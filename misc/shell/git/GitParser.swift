4cdf50d5c9c9f8d785ea725881b59aa665ecd50f
Eonist <unicornhive@gmail.com>
2015-09-30 00:39:56 +0200

    Files modified: 1
    Modified 1 file:misc/shell/git/GitParser.swift

--- misc/shell/git/GitParser.swift
+++ misc/shell/git/GitParser.swift
@@ -2,7 +2,7 @@
  * git diff --name-only --diff-filter=U "outputs: text2.txt"
  * git status -s "outputs UU text2.txt"
  */
-on unmergedFiles(localPath)
+func unMergedFiles(localPath){
 	set unmergedPaths to diff(localPath, "--name-only --diff-filter=U")
 	return paragraphs of unmergedPaths
-end unmerged_files
+}
