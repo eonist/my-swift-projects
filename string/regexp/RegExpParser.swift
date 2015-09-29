1ccefca888576e1e53bd89d7021cc3055f4de228
Eonist <unicornhive@gmail.com>
2015-09-29 21:35:24 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpParser.swift

--- string/regexp/RegExpParser.swift
+++ string/regexp/RegExpParser.swift
@@ -1,19 +1,19 @@
 /*
  * Returns an Array with the matches 
- * EXAMPLE: let string = "🇩🇪€4€9"
- * let matches = matchesForRegexInText("[0-9]", text: string)
+ * EXAMPLE: let string = "€4€9"
+ * let matches = match("[0-9]", text: string)
  * print(matches)
  * // [4, 9]
  * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
  */
-func match(text: String!, pattern: String!) -> [String] {
+func match(text: String!, pattern: String!, options:Array = []) -> [String] {
     do {
         let regex = try NSRegularExpression(pattern: regex, options: [])
         let nsString = text as NSString
         let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
         return results.map { nsString.substringWithRange($0.range)}
     } catch let error as NSError {
-        print("invalid regex: \(error.localizedDescription)")
+        print("invalid regexp: \(error.localizedDescription)")
         return []
     }
 }
