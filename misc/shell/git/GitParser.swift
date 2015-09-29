136f57763bd1e2c526d828155205335be43a134d
Eonist <unicornhive@gmail.com>
2015-09-30 00:41:23 +0200

    Files modified: 1
    Modified 1 file:misc/shell/git/GitParser.swift

--- misc/shell/git/GitParser.swift
+++ misc/shell/git/GitParser.swift
@@ -3,6 +3,6 @@
  * git status -s "outputs UU text2.txt"
  */
 func unMergedFiles(localPath){
-	set unmergedPaths to diff(localPath, "--name-only --diff-filter=U")
-	return paragraphs of unmergedPaths
+	var unmergedPaths = diff(localPath, "--name-only --diff-filter=U")
+	return paragraphs of unmergedPaths//// :TODO: use some sort of linesToArray method here
 }
