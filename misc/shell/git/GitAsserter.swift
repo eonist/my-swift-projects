7845a3251e9290aa954a94df645c788b970101ac
Eonist <unicornhive@gmail.com>
2015-09-30 00:37:05 +0200

    Files modified: 1
    Modified 1 file:misc/shell/git/GitAsserter.swift

--- misc/shell/git/GitAsserter.swift
+++ misc/shell/git/GitAsserter.swift
@@ -3,5 +3,5 @@
  */
 func hasUnMergePaths(localPath)->Boolean{
 	//log length of GitParser's unmerged_files(local_path)
-	return GitParser.unmergedFiles(local_path).count > 0
+	return GitParser.unmergedFiles(localPath).count > 0
 }
