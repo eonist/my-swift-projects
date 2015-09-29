40fff8576e8f8d1094b86f0c181c772d581f2be5
Eonist <unicornhive@gmail.com>
2015-09-30 00:42:51 +0200

    Files modified: 1
    Modified 1 file:misc/shell/git/GitParser.swift

--- misc/shell/git/GitParser.swift
+++ misc/shell/git/GitParser.swift
@@ -6,3 +6,13 @@ func unMergedFiles(localPath){
 	var unmergedPaths = diff(localPath, "--name-only --diff-filter=U")
 	return paragraphs of unmergedPaths//// :TODO: use some sort of linesToArray method here
 }
+/*
+ * "git diff --name-only --diff-filter=U" --returns a list of unmerged files
+ * NOTE: the digits within the @@ and @@ signs represents indices of the lines that changed. Like: @@ -1 +1,3 @@,do a test with numbered lines from 1 - 16 and remove some to see the meaning like in this research: http://stackoverflow.com/questions/10950412/what-does-1-1-mean-in-gits-diff-output
+ * NOTE: git diff returns a result if a file is removed (the removed file will look like this in the returned result: "--- path-to-removed-file")
+ * NOTE: git diff does not reurn a result if a file is added
+ * NOTE: git diff returns a result if a file is changed (the returned result will contain the lines that changed with a "-" preceding the line that is removed and a "+" preceding the line that is added)
+ */
+func diff(local_repo_path, cmd){
+	return ShellUtils.run( "cd " & local_repo_path & ";" & git_path & "git diff " & cmd)
+}
