9857c8da3186c8b273f4b51733d597344946689f
Eonist <unicornhive@gmail.com>
2015-09-29 22:04:31 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpModifier.swift

--- string/regexp/RegExpModifier.swift
+++ string/regexp/RegExpModifier.swift
@@ -10,10 +10,7 @@ Return Value A string with matching regular expressions replaced by the template
  */
 func replace(text: String!, pattern: String!,replacement:String,options:Array = []){
 	//not implemented yet
-	func stringByReplacingMatchesInString(_ string: String,
-                              options options: NSMatchingOptions,
-                                range range: NSRange,
-                         withTemplate templ: String) -> String
+	func stringByReplacingMatchesInString(_ ) -> String
                          
     var original = NSMutableString(string: "<strong>Hell</strong>o, <strong>Hell</strong>o, <strong>Hell</strong>o")
 var search = "<\\/?strong>"
@@ -28,7 +25,7 @@ if (err === nil) {
 >> "*Hell*o, *Hell*o, *Hell*o"
 
 }
-*
+/*
 Flag (Pattern) Description:
 i - If set, matching will take place in a case-insensitive manner.
 x - If set, allow use of white space and #comments within patterns
@@ -36,3 +33,5 @@ s - If set, a "." in a pattern will match a line terminator in the input text. B
 m - Control the behavior of "^" and "$" in a pattern. By default these will only match at the start and end, respectively, of the input text. If this flag is set, "^" and "$" will also match at the start and end of each line within the input text.
 Para - Controls the behavior of \b in a pattern. If set, word boundaries are found according to the definitions of word found in Unicode UAX 29, Text Boundaries. By default, word boundaries are identified by means of a simple classification of characters as either “word” or “non-word”, which approximates traditional regular expression behavior. The results obtained with the two options can be quite different in runs of spaces and other non-word characters.
 */
+
+//string: String,options options: NSMatchingOptions,range  withTemplate templ: String
