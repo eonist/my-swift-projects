a0324e537d4987f1d47005438764017f452b072e
Eonist <unicornhive@gmail.com>
2015-09-29 21:40:40 +0200

    Files modified: 2
    Modified 2 files:string/regexp/RegExpAsserter.swiftstring/regexp/RegExpModifier.swift

--- string/regexp/RegExpAsserter.swift
+++ string/regexp/RegExpAsserter.swift
@@ -1,6 +1,7 @@
 //import RegExpParser.swift
 /*
  * Asserts if a match exists 
+ * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
  */
 func test(text: String!, pattern: String!, options:Array = [])->Boolean{
 	return RegExpParser.match(textpattern,options).count > 0
--- string/regexp/RegExpModifier.swift
+++ string/regexp/RegExpModifier.swift
@@ -1,6 +1,7 @@
 /*
- * 
+ * Replaces all matches with
+ * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
  */
-func replace(text: String!, pattern: String!, options:Array = []){
-
+func replace(text: String!, pattern: String!, options:Array = [],replacement:String){
+	//not implemented yet
 }
