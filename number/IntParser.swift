3ef984fee742e8a2c3b3111a705598b4cec651ec
Eonist <unicornhive@gmail.com>
2015-10-02 22:01:35 +0200

    Files modified: 1
    Modified 1 file:number/IntParser.swift

--- number/IntParser.swift
+++ number/IntParser.swift
@@ -2,12 +2,8 @@ class IntParser{
 	/**
 	 * 
 	 */
-	func kind(){
-		enum Kind {
-	        case , Zero, Positive
-	    }
-	    var kind: Kind {
-	        switch self {
+	func kind(theInt:Int)->Int{
+        switch theInt {
 	        case 0:
 	            return "Zero"
 	        case let x where x > 0:
