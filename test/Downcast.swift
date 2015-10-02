e4a2809670da9f0ce42097e075090cac5ecf98ee
Eonist <unicornhive@gmail.com>
2015-10-02 21:36:12 +0200

    Files modified: 1
    Modified 1 file:test/Downcast.swift

--- test/Downcast.swift
+++ test/Downcast.swift
@@ -32,3 +32,15 @@ for item in controls {
 		picker.datePickerMode = UIDatePickerMode.CountDownTimer
 	}                                      I
 }
+
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: '\(movie.name)', dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: '\(song.name)', by \(song.artist)")
+    }
+}
+
+
+
