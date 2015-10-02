c4d69868c4b6186e0081b4e09dd1979c4eb364ec
Eonist <unicornhive@gmail.com>
2015-10-02 22:08:07 +0200

    Files modified: 1
    Modified 1 file:test/Protocol.swift

--- test/Protocol.swift
+++ test/Protocol.swift
@@ -1,4 +1,5 @@
-//interfaces in swift are called protocols:
+//interfaces in swift are called protocols
+//NOTE: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID267
 protocol Cleaner {
 	// method signatures
 	func c1eanFtoors()
