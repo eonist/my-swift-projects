d91af99af04fa8a9f099f2344000973d10897d49
Eonist <unicornhive@gmail.com>
2015-10-02 21:43:44 +0200

    Files modified: 1
    Modified 1 file:test/Downcast.swift

--- test/Downcast.swift
+++ test/Downcast.swift
@@ -4,6 +4,8 @@
  * Because downcasting can fail, the type cast operator comes in two different forms. The conditional form, as?, returns an optional value of the type you are trying to downcast to. The forced form, as!, attempts the downcast and force-unwraps the result as a single compound action.
  * Use the conditional form of the type cast operator (as?) when you are not sure if the downcast will succeed. This form of the operator will always return an optional value, and the value will be nil if the downcast was not possible. This enables you to check for a successful downcast.
  * Use the forced form of the type cast operator (as!) only when you are sure that the downcast will always succeed. This form of the operator will trigger a runtime error if you try to downcast to an incorrect class type.
+ *
+ * NOTE: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TypeCasting.html#//apple_ref/doc/uid/TP40014097-CH22-ID338
  */
 let myButton    = UIButton()
 let myS1ider    = UISlider()
