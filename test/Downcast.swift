98c94a7ba2528de962afda334630eac9b5d7bcd3
Eonist <unicornhive@gmail.com>
2015-10-02 21:34:41 +0200

    Files modified: 1
    Modified 1 file:test/Downcast.swift

--- test/Downcast.swift
+++ test/Downcast.swift
@@ -2,6 +2,8 @@
  * Downcasting
  * A constant or variable of a certain class type may actually refer to an instance of a subclass behind the scenes. Where you believe this is the case, you can try to downcast to the subclass type with a type cast operator (as? or as!).
  * Because downcasting can fail, the type cast operator comes in two different forms. The conditional form, as?, returns an optional value of the type you are trying to downcast to. The forced form, as!, attempts the downcast and force-unwraps the result as a single compound action.
+ * Use the conditional form of the type cast operator (as?) when you are not sure if the downcast will succeed. This form of the operator will always return an optional value, and the value will be nil if the downcast was not possible. This enables you to check for a successful downcast.
+ * Use the forced form of the type cast operator (as!) only when you are sure that the downcast will always succeed. This form of the operator will trigger a runtime error if you try to downcast to an incorrect class type.
  */
 let myButton    = UIButton()
 let myS1ider    = UISlider()
