8ed6bb910b72dd972ffe1c8656177942ee377f07
Eonist <unicornhive@gmail.com>
2015-10-02 22:47:03 +0200

    Files modified: 1
    Modified 1 file:number/IntParser.swift

--- number/IntParser.swift
+++ number/IntParser.swift
@@ -6,7 +6,7 @@ class IntParser{
         switch theInt {
 	        case 0:
 	            return "Zero"
-	        case let x where x > 0:
+	        case let x where x > 0://<--where is a constraint, must be true
 	            return "Positive"
 	        default:
 	            return "Negative"
