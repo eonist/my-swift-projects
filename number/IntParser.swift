ceaaea0b4b9294d1e0dca98621f6564d204ae725
Eonist <unicornhive@gmail.com>
2015-10-02 21:58:23 +0200

    Files modified: 1
    Modified 1 file:number/IntParser.swift

---
+++ number/IntParser.swift
@@ -0,0 +1,17 @@
+class 
+func kind(){
+	enum Kind {
+        case Negative, Zero, Positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .Zero
+        case let x where x > 0:
+            return .Positive
+        default:
+            return .Negative
+        }
+    }
+}
+}
