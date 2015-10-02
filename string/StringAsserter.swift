84b4d0ed64d2693773a3d558e6e53e4ed355bba2
Eonist <unicornhive@gmail.com>
2015-10-02 15:13:47 +0200

    Files modified: 1
    Modified 1 file:string/StringAsserter.swift

--- string/StringAsserter.swift
+++ string/StringAsserter.swift
@@ -1,5 +1,5 @@
 /*
- * Example: Asserts if a word is duoble quated: is_wrapped_in("\"abc123\"", "\"")--true
+ * Example: Asserts if a word is duoble quated: isWrappedIn("\"abc123\"", "\"")--true
  */
 func isWrappedIn(string:String, char:Character){
 	return (string.first == char) and (string.last == char)
