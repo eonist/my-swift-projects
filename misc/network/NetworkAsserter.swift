af93efd7605ba128bea32320d8de9e9f4fcd7254
Eonist <unicornhive@gmail.com>
2015-10-02 19:01:10 +0200

    Files modified: 1
    Modified 1 file:misc/network/NetworkAsserter.swift

--- misc/network/NetworkAsserter.swift
+++ misc/network/NetworkAsserter.swift
@@ -4,7 +4,6 @@ class NetworkAsserter {
 	 * 
 	 */
 	func isConnectedToNetwork()->Bool{
-	  var Status:Bool = false
 	  let url = NSURL(string: "http://google.com/")
 	  let request = NSMutableURLRequest(URL: url!)
 	  request.HTTPMethod = "HEAD"
@@ -14,9 +13,9 @@ class NetworkAsserter {
 	  var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: nil) as NSData?
 	  if let httpResponse = response as? NSHTTPURLResponse {
 	      if httpResponse.statusCode == 200 {
-	          Status = true
+	          return true
 	      }
 	  }
-	  return Status
+	  return false
 	}
 }
