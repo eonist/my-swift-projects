f076a3e698e92fda49096fe68d3b0205c67353a5
Eonist <unicornhive@gmail.com>
2015-10-03 19:27:26 +0200

    Files modified: 1
    Modified 1 file:array/ArrayParser.swift

--- array/ArrayParser.swift
+++ array/ArrayParser.swift
@@ -1,3 +1,13 @@
 class ArrayParser{
-	
+	/**
+	 * returns the index for item, -1 of none is found
+	 */
+	func index(items: [Any], _ item: Any) -> Int? {
+	    for (index, item) in array.enumerate() {
+	        if value == item {
+	            return index
+	        }
+	    }
+	    return -1
+	}
 }
