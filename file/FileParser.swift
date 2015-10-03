a9ad9b0eb9576572e0dcbee6b0c679c807290273
Eonist <unicornhive@gmail.com>
2015-10-03 18:19:45 +0200

    Files modified: 1
    Modified 1 file:file/FileParser.swift

--- file/FileParser.swift
+++ file/FileParser.swift
@@ -1,19 +1,21 @@
-/*
- * Returns content as a string for filePath
- */
-func string(filePath:String)->String{
-	var err: NSError?
-	let content = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: &err)
-	if (err == nil){
-		return content
-	}else{
-		return nil
+class FileParser{
+	/*
+	 * Returns content as a string for filePath
+	 */
+	func string(filePath:String)->String{
+		var err: NSError?
+		let content = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: &err)
+		if (err == nil){
+			return content
+		}else{
+			return nil
+		}
 	}
-}
-/**
- * Returns the project resource folder
- * NOTE: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation/Classes/NSBundle_Class/
- */
-func resourcePath()->String{
-	return NSBundle.mainBundle().resourcePath
+	/**
+	 * Returns the project resource folder
+	 * NOTE: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation/Classes/NSBundle_Class/
+	 */
+	func resourcePath()->String{
+		return NSBundle.mainBundle().resourcePath
+	}
 }
