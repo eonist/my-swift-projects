336ae43e41f2da7629aa1bdfebde51f015fe1098
Eonist <unicornhive@gmail.com>
2015-10-03 10:22:17 +0200

    Files modified: 1
    Modified 1 file:test/types/Protocol.swift

--- test/types/Protocol.swift
+++ test/types/Protocol.swift
@@ -1,3 +1,5 @@
+//more on protocol conformance: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID283
+
 //interfaces in swift are called protocols
 //NOTE: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID267
 protocol Cleaner {
