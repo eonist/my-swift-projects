0b06d7adbd8c60f53d3b522eb2262c077d6d3aa2
Eonist <unicornhive@gmail.com>
2015-10-03 15:54:25 +0200

    Files modified: 1
    Modified 1 file:misc/network/NetworkParser.swift

--- misc/network/NetworkParser.swift
+++ misc/network/NetworkParser.swift
@@ -1,11 +1,13 @@
-/**
- * 
- */
-func string(URL:String)->String{
-	let theURL = NSURL(string: URL)
-  let rssUrlRequest:NSURLRequest = NSURLRequest(URL:theURL!)
-  let queue:NSOperationQueue = NSOperationQueue()
-  //the bellow is sudo code
-  let result = NSURLConnection.sendAsynchronousRequest(rssUrlRequest, queue: queue) 
-	return result
+class NetworkParser{
+	/**
+	 * Returns a string for a URL
+	 */
+	func string(URL:String)->String{
+		let theURL = NSURL(string: URL)
+		let rssUrlRequest:NSURLRequest = NSURLRequest(URL:theURL!)
+	 	let queue:NSOperationQueue = NSOperationQueue()
+	 	//the bellow is sudo code
+	 	let result = NSURLConnection.sendAsynchronousRequest(rssUrlRequest, queue: queue) 
+		return result
+	}
 }
