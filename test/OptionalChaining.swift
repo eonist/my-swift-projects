f8cf49e988b10ae94c8c331cc4735816408f43d0
Eonist <unicornhive@gmail.com>
2015-10-03 08:32:43 +0200

    Files modified: 1
    Modified 1 file:test/OptionalChaining.swift

--- test/OptionalChaining.swift
+++ test/OptionalChaining.swift
@@ -1,1 +1,13 @@
 if let roomCount = john.residence?.numberOfRooms {
+
+if let johnsStreet = john.residence?.address?.street {
+    print("John's street name is \(johnsStreet).")
+} else {
+    print("Unable to retrieve the address.")
+}
+// prints "Unable to retrieve the address."
+
+let buildingIdentifier = john.residence?.address?.buildingIdentifier() {
+    print("John's building identifier is \(buildingIdentifier).")
+}
+// prints "John's building identifier is The Larches."
