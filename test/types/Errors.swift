086c190f4a24c500cbbd64234dd5db693a67b5cd
Eonist <unicornhive@gmail.com>
2015-10-03 09:34:17 +0200

    New files added: 1, Files modified: 1
    Added 1 file:test/operations/Defer.swiftModified 1 file:test/types/Errors.swift

--- test/types/Errors.swift
+++ test/types/Errors.swift
@@ -130,4 +130,11 @@ if !success {
 }
 
 
+/*
+Disabling Error Propagation
 
+Sometimes you know a throwing function or method won’t, in fact, throw an error at runtime. On those occasions, you can write try! before the expression to disable error propagation and wrap the call in a runtime assertion that no error will be thrown. If an error actually is thrown, you’ll get a runtime error.
+
+For example, the following code uses a loadImage(_:) function, which loads the image resource at a given path or throws an error if the image can’t be loaded. In this case, because the image is shipped with the application, no error will be thrown at runtime, so it is appropriate to disable error propagation.
+*/
+let photo = try! loadImage("./Resources/John Appleseed.jpg")
