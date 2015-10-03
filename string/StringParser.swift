db065c26bd362e96d18c460bfe6160e3a434adb2
Eonist <unicornhive@gmail.com>
2015-10-03 16:53:41 +0200

    Files modified: 2
    Modified 2 files:string/StringModifier.swiftstring/StringParser.swift

--- string/StringParser.swift
+++ string/StringParser.swift
@@ -1,6 +1,14 @@
-/*
- * return array for every line in a string 
- */
-func paragraphs(string:String){
-	string.componentsSeparatedByString("\n")
+class StringParser{
+	/*
+	 * return array for every line in a string 
+	 */
+	func paragraphs(string:String){
+		split(string,"\n")
+	}
+	/**
+	 * NOTE: use "\n" to retrive paragraphs
+	 */
+	func split(string:String,delimiter:String)->Array{
+		return unmMergedPaths.componentsSeparatedByString(delimiter)
+	}
 }
