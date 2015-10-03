ba1c69bb018e81dc035d761bae67d75c4061b56a
Eonist <unicornhive@gmail.com>
2015-10-03 17:10:13 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpAsserter.swift

--- string/regexp/RegExpAsserter.swift
+++ string/regexp/RegExpAsserter.swift
@@ -1,8 +1,10 @@
-//import RegExpParser.swift
-/*
- * Asserts if a match exists 
- * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
- */
-func test(text: String!, pattern: String!, options:Array = [])->Boolean{
-	return RegExpParser.match(textpattern,options).count > 0
+class RegExpAsserter{
+	//import RegExpParser.swift
+	/*
+	 * Asserts if a match exists 
+	 * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+	 */
+	func test(text: String!, pattern: String!, options:Array = [])->Boolean{
+		return RegExpParser.match(textpattern,options).count > 0
+	}
 }
