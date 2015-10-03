4427655c5659fdb9b30c2ccd9bb9d6fdc32b8a80
Eonist <unicornhive@gmail.com>
2015-10-03 09:35:59 +0200

    Files modified: 1
    Modified 1 file:test/operations/Defer.swift

--- test/operations/Defer.swift
+++ test/operations/Defer.swift
@@ -1,16 +1,10 @@
-Disabling Error Propagation
-
-Sometimes you know a throwing function or method won’t, in fact, throw an error at runtime. On those occasions, you can write try! before the expression to disable error propagation and wrap the call in a runtime assertion that no error will be thrown. If an error actually is thrown, you’ll get a runtime error.
-
-For example, the following code uses a loadImage(_:) function, which loads the image resource at a given path or throws an error if the image can’t be loaded. In this case, because the image is shipped with the application, no error will be thrown at runtime, so it is appropriate to disable error propagation.
-
-let photo = try! loadImage("./Resources/John Appleseed.jpg")
+/*
 Specifying Cleanup Actions
 
 You use a defer statement to execute a set of statements just before code execution leaves the current block of code. This statement lets you do any necessary cleanup that should be performed regardless of how execution leaves the current block of code—whether it leaves because an error was thrown or because of a statement such as return or break. For example, you can use a defer statement to ensure that file descriptors are closed and manually allocated memory is freed.
 
 A defer statement defers execution until the current scope is exited. This statement consists of the defer keyword and the statements to be executed later. The deferred statements may not contain any code that would transfer control out of the statements, such as a break or a return statement, or by throwing an error. Deferred actions are executed in reverse order of how they are specified—that is, the code in the first defer statement executes after code in the second, and so on.
-
+*/
 func processFile(filename: String) throws {
     if exists(filename) {
         let file = open(filename)
@@ -23,4 +17,5 @@ func processFile(filename: String) throws {
         // close(file) is called here, at the end of the scope.
     }
 }
-The above example uses a defer statement to ensure that the open(_:) function has a corresponding call to close(_:).
+
+//The above example uses a defer statement to ensure that the open(_:) function has a corresponding call to close(_:).
