f8f6cd6b0be144a63653d8670b816d9fc3169efb
Eonist <unicornhive@gmail.com>
2015-10-03 20:12:58 +0200

    Files modified: 1
    Modified 1 file:research/operations/AdvanceOperators.swift

--- research/operations/AdvanceOperators.swift
+++ research/operations/AdvanceOperators.swift
@@ -8,3 +8,15 @@ struct Vector2D {
 func + (left: Vector2D, right: Vector2D) -> Vector2D {
     return Vector2D(x: left.x + right.x, y: left.y + right.y)
 }
+
+//The operator function is defined as a global function with a function name that matches the operator to be overloaded (+). Because the arithmetic addition operator is a binary operator, this operator function takes two input parameters of type Vector2D and returns a single output value, also of type Vector2D.
+
+//In this implementation, the input parameters are named left and right to represent the Vector2D instances that will be on the left side and right side of the + operator. The function returns a new Vector2D instance, whose x and y properties are initialized with the sum of the x and y properties from the two Vector2D instances that are added together.
+
+//The function is defined globally, rather than as a method on the Vector2D structure, so that it can be used as an infix operator between existing Vector2D instances:
+
+let vector = Vector2D(x: 3.0, y: 1.0)
+let anotherVector = Vector2D(x: 2.0, y: 4.0)
+let combinedVector = vector + anotherVector//amazing!!!
+// combinedVector is a Vector2D instance with values of (5.0, 5.0)
+//This example adds together the vectors (3.0, 1.0) and (2.0, 4.0) to make the vector (5.0, 5.0), as illustrated below.
