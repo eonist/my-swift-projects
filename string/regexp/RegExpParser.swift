89a0354a23a4eae1e876a99cb678912fbadc89ee
Eonist <unicornhive@gmail.com>
2015-10-03 18:08:05 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpParser.swift

--- string/regexp/RegExpParser.swift
+++ string/regexp/RegExpParser.swift
@@ -1,16 +1,18 @@
-/*
- * Returns an Array with the matches 
- * EXAMPLE: match("£4$9", "[0-9]", text: string)//Output: [4, 9]
- * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
- */
-func match(text: String!, pattern: String!, options:Array = []) -> [String] {
-    do {
-        let regex = try NSRegularExpression(pattern: regex, options: [])
-        let nsString = text as NSString
-        let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
-        return results.map { nsString.substringWithRange($0.range)}
-    } catch let error as NSError {
-        print("invalid regexp: \(error.localizedDescription)")
-        return []
-    }
+class RegExpParser{
+	/*
+	 * Returns an Array with the matches 
+	 * EXAMPLE: match("£4$9", "[0-9]", text: string)//Output: [4, 9]
+	 * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+	 */
+	func match(text: String!, pattern: String!, options:Array = []) -> [String] {
+	    do {
+	        let regex = try NSRegularExpression(pattern: regex, options: [])
+	        let nsString = text as NSString
+	        let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
+	        return results.map { nsString.substringWithRange($0.range)}
+	    } catch let error as NSError {
+	        print("invalid regexp: \(error.localizedDescription)")
+	        return []
+	    }
+	}
 }
