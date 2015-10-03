247358dfdccaf48b71fb467f23cd1cdc113d20cd
Eonist <unicornhive@gmail.com>
2015-10-03 19:30:27 +0200

    Files modified: 1
    Modified 1 file:array/ArrayParser.swift

--- array/ArrayParser.swift
+++ array/ArrayParser.swift
@@ -2,7 +2,7 @@ class ArrayParser{
 	/**
 	 * returns the index for item, -1 of none is found
 	 */
-	func index(items: [T], _ item: T) -> Int? {
+	func index<T: Equatable>(array: [T], _ valueToFind: T) -> Int? {//the <T: Equatable> part ensures that only
 	    for (index, item) in array.enumerate() {
 	        if value == item {
 	            return index
