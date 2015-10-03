c243578e7879505911c2ce1a302ce16e61f5beeb
Eonist <unicornhive@gmail.com>
2015-10-03 10:06:52 +0200

    Files modified: 1
    Modified 1 file:test/operations/Downcast.swift

--- test/operations/Downcast.swift
+++ test/operations/Downcast.swift
@@ -52,3 +52,22 @@ if(item as Movie){
 }
 
 
+//downcasting in a loop:
+//Because this array is known to contain only Movie instances, you can downcast and unwrap directly to a nonoptional Movie with the forced version of the type cast operator (as!):
+
+for object in someObjects {
+    let movie = object as! Movie
+    print("Movie: '\(movie.name)', dir. \(movie.director)")
+}
+// Movie: '2001: A Space Odyssey', dir. Stanley Kubrick
+// Movie: 'Moon', dir. Duncan Jones
+// Movie: 'Alien', dir. Ridley Scott
+
+//For an even shorter form of this loop, downcast the someObjects array to a type of [Movie] instead of downcasting each item:
+
+for movie in someObjects as! [Movie] {
+    print("Movie: '\(movie.name)', dir. \(movie.director)")
+}
+// Movie: '2001: A Space Odyssey', dir. Stanley Kubrick
+// Movie: 'Moon', dir. Duncan Jones
+// Movie: 'Alien', dir. Ridley Scott
