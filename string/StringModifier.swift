e474007c03ed612fca5fd0783b02dad7348a156f
Eonist <unicornhive@gmail.com>
2015-10-04 08:55:54 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -9,6 +9,7 @@ class StringModifier{
 	 * returns a reversed strin
 	 * NOTE: does not modify the original string
 	 * NOTE: infered version of the bellow: reversed = names.sort( { s1, s2 in return s1 > s2 } )
+	 * NOTE: even shorter: names.sort( { s1, s2 in s1 > s2 } )
 	 */		
 	class func reverse(string:String)->String{
 		return string.sort( { (s1: String, s2: String) -> Bool in return s1 > s2 } )//closure expression syntax
