b308b53390cfb13f0e1c66b467d4b04083fb2ff2
Eonist <unicornhive@gmail.com>
2015-10-04 10:59:22 +0200

    Files modified: 1
    Modified 1 file:research/SubScript.swift

--- research/SubScript.swift
+++ research/SubScript.swift
@@ -10,3 +10,31 @@ let threeTimesTable = TimesTable(multiplier: 3)
 print("six times three is \(threeTimesTable[6])")
 // prints "six times three is 18"
 
+//
+
+Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue: 0.0)
+    }
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
