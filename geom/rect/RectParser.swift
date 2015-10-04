b28128bfd75b2848e4b39b8ad0c1f539b73a4742
Eonist <unicornhive@gmail.com>
2015-10-04 18:00:54 +0200

    Files modified: 1
    Modified 1 file:geom/rect/RectParser.swift

--- geom/rect/RectParser.swift
+++ geom/rect/RectParser.swift
@@ -4,3 +4,31 @@
 //The size of a geometric shape, perhaps encapsulating a width property and a height property, both of type Double.
 //A way to refer to ranges within a series, perhaps encapsulating a start property and a length property, both of type Int.
 //A point in a 3D coordinate system, perhaps encapsulating x, y and z properties, each of type Double.
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+    size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// prints "square.origin is now at (10.0, 10.0)"
