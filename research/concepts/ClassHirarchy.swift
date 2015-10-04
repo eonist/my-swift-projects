b2c708a28f36768657293d2b4a68764b1e2ec6eb
Eonist <unicornhive@gmail.com>
2015-10-04 21:49:06 +0200

    Files modified: 1
    Modified 1 file:research/concepts/ClassHirarchy.swift

--- research/concepts/ClassHirarchy.swift
+++ research/concepts/ClassHirarchy.swift
@@ -1,2 +1,20 @@
 //here is agood idea: http://stackoverflow.com/questions/25828147/traverse-view-controller-hierarchy-in-swift
 
+
+//You can use the postfix self expression to access a type as a value. For example, SomeClass.self returns SomeClass itself, not an instance of SomeClass. And SomeProtocol.self returns SomeProtocol itself, not an instance of a type that conforms to SomeProtocol at runtime. You can use a dynamicType expression with an instance of a type to access that instance’s dynamic, runtime type as a value, as the following example shows:
+class SomeBaseClass {
+    class func printClassName() {
+        print("SomeBaseClass")
+    }
+}
+class SomeSubClass: SomeBaseClass {
+    override class func printClassName() {
+        print("SomeSubClass")
+    }
+}
+let someInstance: SomeBaseClass = SomeSubClass()
+// The compile-time type of someInstance is SomeBaseClass,
+// and the runtime type of someInstance is SomeBaseClass
+someInstance.dynamicType.printClassName()
+// prints "SomeSubClass"
+
