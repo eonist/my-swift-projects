23329bf10600d35a9f6239f4254d5ae47e83b655
Eonist <unicornhive@gmail.com>
2015-10-04 09:04:00 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -8,17 +8,17 @@ class StringModifier{
 	/**
 	 * returns a reversed strin
 	 * NOTE: does not modify the original string
-	 * NOTE: infered version of the bellow: string.sort( { s1, s2 in return s1 > s2 } )
+	 * NOTE: shorter version of this method: 		return string.sort( { (s1: String, s2: String) -> Bool in return s1 > s2 } )//closure expression syntax
+	 * NOTE: even shorter string.sort( { s1, s2 in return s1 > s2 } )//infered version of the bellow: 
 	 * NOTE: even shorter: string.sort( { s1, s2 in s1 > s2 } )//the return statement is omitted as it is implicit
 	 * NOTE: even shorter: string.sort( { $0 > $1 } )//shorthand argument names, the in is omitted since the expression is made up entirly of its body
 	 * NOTE: even shorther string.sort(>)//operator function
+	 * NOTE: keep the long version around so that its easier to understand
 	 */		
 	class func reverse(string:String)->String{
 		func backwards(s1: String, _ s2: String) -> Bool {
 		    return s1 > s2
 		}
-		var reversed = names.sort(backwards)
-// reversed is equal to ["Ewa", "Daniella", "Chris", "Barry", "Alex"]
-		return string.sort( { (s1: String, s2: String) -> Bool in return s1 > s2 } )//closure expression syntax
+		var reversed:String = string.sort(backwards)
 	}
 } 
