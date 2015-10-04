daa7e9aeada292f685a741d8ff41656e0dbfe53f
Eonist <unicornhive@gmail.com>
2015-10-04 08:49:20 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -5,4 +5,8 @@ class StringModifier{
 	class func wrapWith(string:String,wrapper:String)->String{
 	   return wrapper + string + wrapper
 	}
+	jd2	
+	class func reverse(string:String){
+		string.sort( { (s1: String, s2: String) -> Bool in return s1 > s2 } )//closure expression syntax
+	}
 } 
