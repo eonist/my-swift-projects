3b0f9f0bb9af4501f89ebe894a3dba001764e473
Eonist <unicornhive@gmail.com>
2015-10-04 18:17:58 +0200

    Files modified: 1
    Modified 1 file:geom/rect/RectParser.swift

--- geom/rect/RectParser.swift
+++ geom/rect/RectParser.swift
@@ -32,3 +32,21 @@ let initialSquareCenter = square.center
 square.center = Point(x: 15.0, y: 15.0)
 print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
 // prints "square.origin is now at (10.0, 10.0)"
+
+//Shorthand Setter Declaration
+//If a computed property’s setter does not define a name for the new value to be set, a default name of newValue is used. Here’s an alternative version of the Rect structure, which takes advantage of this shorthand notation:
+struct AlternativeRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
