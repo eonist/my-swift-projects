21f65dea45ef6dc426ecd6b921922d075fda0d51
Eonist <unicornhive@gmail.com>
2015-10-04 17:04:09 +0200

    Files modified: 1
    Modified 1 file:research/concepts/References.swift

--- research/concepts/References.swift
+++ research/concepts/References.swift
@@ -6,3 +6,7 @@ Swift’s String, Array, and Dictionary types are implemented as structures. Thi
 This behavior is different from NSString, NSArray, and NSDictionary in Foundation, which are implemented as classes, not structures. NSString, NSArray, and NSDictionary instances are always assigned and passed around as a reference to an existing instance, rather than as a copy.
 
 */
+
+/**
+ * NOTE: The description above refers to the “copying” of strings, arrays, and dictionaries. The behavior you see in your code will always be as if a copy took place. However, Swift only performs an actual copy behind the scenes when it is absolutely necessary to do so. Swift manages all value copying to ensure optimal performance, and you should not avoid assignment to try to preempt this optimization.
+ */
