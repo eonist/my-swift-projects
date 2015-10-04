f4cefd3f2feeecb835370bd21235296f88bd3f17
Eonist <unicornhive@gmail.com>
2015-10-04 18:19:50 +0200

    Files modified: 1
    Modified 1 file:geom/rect/RectParser.swift

--- geom/rect/RectParser.swift
+++ geom/rect/RectParser.swift
@@ -50,3 +50,14 @@ struct AlternativeRect {
         }
     }
 }
+
+
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+// prints "the volume of fourByFiveByTwo is 40.0"
