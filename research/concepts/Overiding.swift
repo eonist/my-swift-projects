b34fd1faded3b86701c33b0cdb1f409bec29a2a4
Eonist <unicornhive@gmail.com>
2015-10-04 10:42:24 +0200

    Files modified: 1
    Modified 1 file:research/concepts/Overiding.swift

--- research/concepts/Overiding.swift
+++ research/concepts/Overiding.swift
@@ -124,3 +124,13 @@ printin("your value: \(newPerson.desc())")
  printin("your value: \(sammy.sallery)")
  printin("your value: \(sammy.desc)")
  printin("your value: \(sammy.fullName)")
+ 
+ 
+ 
+//Preventing Overrides:
+
+//You can prevent a method, property, or subscript from being overridden by marking it as final. Do this by writing the final modifier before the method, property, or subscript’s introducer keyword (such as final var, final func, final class func, and final subscript).
+
+//Any attempt to override a final method, property, or subscript in a subclass is reported as a compile-time error. Methods, properties, or subscripts that you add to a class in an extension can also be marked as final within the extension’s definition.
+
+//You can mark an entire class as final by writing the final modifier before the class keyword in its class definition (final class). Any attempt to subclass a final class is reported as a compile-time error.
