dfd7643d12519eba4727d625f87114141eb117ac
Eonist <unicornhive@gmail.com>
2015-10-04 10:48:35 +0200

    Files modified: 1
    Modified 1 file:research/SubScript.swift

--- research/SubScript.swift
+++ research/SubScript.swift
@@ -1,1 +1,12 @@
 //NOTE: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Subscripts.html#//apple_ref/doc/uid/TP40014097-CH16-ID305
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+// prints "six times three is 18"
+
