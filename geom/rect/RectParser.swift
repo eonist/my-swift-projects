8ce0cdb883abd1ab36993eec832fb58373e0975d
Eonist <unicornhive@gmail.com>
2015-10-04 16:59:03 +0200

    Files modified: 1
    Modified 1 file:geom/rect/RectParser.swift

--- geom/rect/RectParser.swift
+++ geom/rect/RectParser.swift
@@ -1,1 +1,6 @@
 //math related to the rectangle 
+//Examples of good candidates for structures include:
+
+//The size of a geometric shape, perhaps encapsulating a width property and a height property, both of type Double.
+//A way to refer to ranges within a series, perhaps encapsulating a start property and a length property, both of type Int.
+//A point in a 3D coordinate system, perhaps encapsulating x, y and z properties, each of type Double.
