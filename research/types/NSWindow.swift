2241a34620fb6277038cdf628f6f632393e45a9b
Eonist <unicornhive@gmail.com>
2015-10-05 23:46:48 +0200

    New files added: 1
    Added 1 file:research/types/NSWindow.swift

---
+++ research/types/NSWindow.swift
@@ -0,0 +1,74 @@
+//borderless window
+//http://www.cimgf.com/cocoa-code-snippets/nswindow-snippets/
+
+//fullscrreen: window.setFrame(NSScreen.mainScreen()!.visibleFrame, display: true, animate: true)
+
+//pin to front:  
+[self.window makeKeyAndOrderFront:nil];
+[self.window setLevel:NSStatusWindowLevel];
+
+
+//custom window: in objc: http://www.cocoawithlove.com/2008/12/drawing-custom-window-on-mac-os-x.html
+
+
+
+//create a win in swift osx: http://stackoverflow.com/questions/24068763/create-a-new-window-with-nswindow
+
+
+//docs: nswin swift osx: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSWindow_Class/#//apple_ref/occ/instm/NSWindow/initWithContentRect:styleMask:backing:defer:
+
+//test: http://stackoverflow.com/questions/24045339/how-to-use-swift-playground-to-display-nsview-with-some-drawing
+
+import Cocoa
+import XCPlayground
+
+
+  class CustomView: NSView {
+    init(frame: NSRect) {
+        super.init(frame: frame)
+
+        antibez.moveToPoint(NSPoint(x: 10 , y: 10))
+        for i in 0..25
+        {
+            antibez.lineToPoint(NSPoint(x: 20 + 10 * (25-i), y: 20 + 10 * i))
+            antibez.moveToPoint(NSPoint(x: 10 + 10 * (i), y: 10 ))
+
+        }
+    }
+    override func drawRect(dirtyRect: NSRect) {
+        color.setFill()
+        NSRectFill(self.bounds)
+        antibez.stroke()
+
+    }
+
+    var color = NSColor.greenColor()
+
+    var antibez = NSBezierPath()
+  }
+  var view = CustomView(frame:
+    NSRect(x: 0, y: 0, width: 300, height: 300))
+
+  XCPShowView("chart", view)
+  
+  
+  //http://practicalswift.com/2014/06/27/a-minimal-webkit-browser-in-30-lines-of-swift/
+  
+  //
+  
+  
+  
+  
+  let textField =. NSTextView()
+textView.stringvalue = "Some string"
+textView.frame = CGRectMake(10,20,50,400)
+ mywindow.contentView.addSubview(textView)
+ 
+ 
+ //http://stackoverflow.com/questions/24224886/correct-way-to-create-nswindow-using-swift-and-cocoa
+ 
+ //http://stackoverflow.com/questions/28031732/create-programmatically-nswindow-in-swift
+ 
+ 
+ //vim
+ http://stackoverflow.com/questions/24068763/create-a-new-window-with-nswindow
