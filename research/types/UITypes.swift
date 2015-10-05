db8b15d819079fff8f390fe5c0a99a885b7dcd2d
Eonist <unicornhive@gmail.com>
2015-10-05 09:31:49 +0200

    Files modified: 1
    Modified 1 file:research/types/UITypes.swift

--- research/types/UITypes.swift
+++ research/types/UITypes.swift
@@ -3,3 +3,5 @@ let myTableView: UITableView = UITableView(frame: CGRectZero, style: .Grouped)
 
 //
 let myTextField = UITextField(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
+
+let color = UIColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
