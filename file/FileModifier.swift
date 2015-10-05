b3112ae24c8ed78732f67e59c818b2d5e12fb48f
Eonist <unicornhive@gmail.com>
2015-10-05 14:37:57 +0200

    Files modified: 1
    Modified 1 file:file/FileModifier.swift

--- file/FileModifier.swift
+++ file/FileModifier.swift
@@ -1,6 +1,13 @@
 class FileModifier{
 	/**
-	 * 
+	 * fromURL: "/path/to/old"
+	 * toURL: "/path/to/new"
+	 * additional catch clauses: catch NSCocoaError.FileNoSuchFileError {
+    											print("Error: no such file exists")
+											} catch NSCocoaError.FileReadUnsupportedSchemeError {
+											    print("Error: unsupported scheme (should be 'file://')")
+											}
+
 	 */
 	func move(fromURL:String,toURL:String){
 		let fileManager = NSFileManager.defaultManager()
