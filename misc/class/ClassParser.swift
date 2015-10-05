d73da82baf8ac701bf12c64a2eab8ab71d670628
Eonist <unicornhive@gmail.com>
2015-10-05 21:36:10 +0200

    Files modified: 1
    Modified 1 file:misc/class/ClassParser.swift

--- misc/class/ClassParser.swift
+++ misc/class/ClassParser.swift
@@ -1,7 +1,9 @@
 /**
  * returns a usable class
+ * NOTE: This works: (obj as! NSObject).className
  * Example: let someObj : typeof(anotheraObj) = typeof(anotheraObj)(arguments here)//this creates an instance from the class of another instance
  */
-func getClass(instance:AnyObject){
-	return typeOf(instance)
+func getClass(instance:AnyObject!){
+	return _stdlib_getDemangledTypeName(instance).componentsSeparatedByString(".").last!//This call is subjected to change in future versions of swift
+	//return typeOf(instance)
 }
