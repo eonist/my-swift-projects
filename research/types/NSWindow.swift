c56cc14a35fdfb6b095059c0397f301ab263f241
Eonist <unicornhive@gmail.com>
2015-10-05 16:36:08 +0200

    Files modified: 1
    Modified 1 file:research/types/NSWindow.swift

--- research/types/NSWindow.swift
+++ research/types/NSWindow.swift
@@ -1,6 +1,13 @@
 //borderless window
 //http://www.cimgf.com/cocoa-code-snippets/nswindow-snippets/
 
+//fullscrreen: window.setFrame(NSScreen.mainScreen()!.visibleFrame, display: true, animate: true)
+
+//pin to front:  
+[self.window makeKeyAndOrderFront:nil];
+[self.window setLevel:NSStatusWindowLevel];
+
+
 //custom window: in objc: http://www.cocoawithlove.com/2008/12/drawing-custom-window-on-mac-os-x.html
 
 
