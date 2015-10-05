b8b6cbf82e8e8f787617bc56489fd53fa8a7712d
Eonist <unicornhive@gmail.com>
2015-10-05 16:10:43 +0200

    Files modified: 1
    Modified 1 file:research/types/NSWindow.swift

--- research/types/NSWindow.swift
+++ research/types/NSWindow.swift
@@ -1,2 +1,5 @@
 //borderless window
 //http://www.cimgf.com/cocoa-code-snippets/nswindow-snippets/
+
+//custom window: in objc: http://www.cocoawithlove.com/2008/12/drawing-custom-window-on-mac-os-x.html
+
