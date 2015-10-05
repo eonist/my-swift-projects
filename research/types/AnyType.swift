d6dbbc8ba15f5863a2e44500f5193d79a3c3a17f
Eonist <unicornhive@gmail.com>
2015-10-05 09:53:13 +0200

    New files added: 1, Files deleted: 1
    Added 1 file:research/types/AnyType.swiftDeleted 1 file:research/types/.AnyType.swift.icloud

---
+++ research/types/AnyType.swift
@@ -0,0 +1,12 @@
+let userDefaults = NSUserDefaults.standardUserDefaults()
+let lastRefreshDate: AnyObject? = userDefaults.objectForKey("LastRefreshDate")
+if let date = lastRefreshDate as? NSDate {
+    print("\(date.timeIntervalSinceReferenceDate)")
+}
+
+
+//Alternatively, you can use the forced downcast operator (as!), which returns a non-optional value of the type you are trying to downcast to:
+
+let myDate = lastRefreshDate as! NSDate
+let timeInterval = myDate.timeIntervalSinceReferenceDate
+//However, if a forced downcast fails, a runtime error is triggered:
