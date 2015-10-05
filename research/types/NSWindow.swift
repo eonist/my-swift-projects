dbd0995652bdf7d977935141333f60b1fda631a1
Eonist <unicornhive@gmail.com>
2015-10-05 16:55:07 +0200

    Files modified: 1
    Modified 1 file:research/types/NSWindow.swift

--- research/types/NSWindow.swift
+++ research/types/NSWindow.swift
@@ -18,3 +18,35 @@
 //docs: nswin swift osx: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSWindow_Class/#//apple_ref/occ/instm/NSWindow/initWithContentRect:styleMask:backing:defer:
 
 //test: http://stackoverflow.com/questions/24045339/how-to-use-swift-playground-to-display-nsview-with-some-drawing
+
+import Cocoa
+import XCPlayground
+
+
+  class CustomView: NSView {
+    init(frame: NSRect) {
+        super.init(frame: frame)
+
+        antibez.moveToPoint(NSPoint(x: 10 , y: 10))
+        for i in 0..25
+        {
+            antibez.lineToPoint(NSPoint(x: 20 + 10 * (25-i), y: 20 + 10 * i))
+            antibez.moveToPoint(NSPoint(x: 10 + 10 * (i), y: 10 ))
+
+        }
+    }
+    override func drawRect(dirtyRect: NSRect) {
+        color.setFill()
+        NSRectFill(self.bounds)
+        antibez.stroke()
+
+    }
+
+    var color = NSColor.greenColor()
+
+    var antibez = NSBezierPath()
+  }
+  var view = CustomView(frame:
+    NSRect(x: 0, y: 0, width: 300, height: 300))
+
+  XCPShowView("chart", view)
