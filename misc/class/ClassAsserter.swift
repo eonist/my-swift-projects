02a3e7445c169ad4721cf6feb21e992c72e73972
Eonist <unicornhive@gmail.com>
2015-10-05 21:13:29 +0200

    Files modified: 1
    Modified 1 file:misc/class/ClassAsserter.swift

--- misc/class/ClassAsserter.swift
+++ misc/class/ClassAsserter.swift
@@ -1,6 +1,6 @@
 class ClassAsserter{
 	/**
-	 * 
+	 * NOTE: YOu may try this aswell instance.isKindOfClass(classType)
 	 */
 	class func isOfClass(instance:AnyObject,_ theClass:AnyObject)->Bool{
 		return instance is theClass
