16344395b21841e1da801adc45cd89b21361ca05
Eonist <unicornhive@gmail.com>
2015-10-05 10:11:02 +0200

    Files modified: 1
    Modified 1 file:misc/class/ClassParser.swift

--- misc/class/ClassParser.swift
+++ misc/class/ClassParser.swift
@@ -1,3 +1,19 @@
+/**
+ * returns a usable class
+ * Example: let someObj typeof(anotheraObj) weakSelf = self;
+ */
 func getClass(instance:AnyObject){
+	return typeOf(instance)
+}
 
+extension UIBezierPath {
+    convenience init(triangleSideLength: CGFloat, origin: CGPoint) {
+        self.init()
+        let squareRoot = CGFloat(sqrt(3.0))
+        let altitude = (squareRoot * triangleSideLength) / 2
+        moveToPoint(origin)
+        addLineToPoint(CGPoint(x: origin.x + triangleSideLength, y: origin.y))
+        addLineToPoint(CGPoint(x: origin.x + triangleSideLength / 2, y: origin.y + altitude))
+        closePath()
+    }
 }
