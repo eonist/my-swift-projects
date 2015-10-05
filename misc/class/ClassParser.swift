65bc9407913cdbfd4b94abcaf0f761ebe21d9552
Eonist <unicornhive@gmail.com>
2015-10-05 10:12:30 +0200

    Files modified: 1
    Modified 1 file:misc/class/ClassParser.swift

--- misc/class/ClassParser.swift
+++ misc/class/ClassParser.swift
@@ -1,6 +1,6 @@
 /**
  * returns a usable class
- * Example: let someObj typeof(anotheraObj) weakSelf = self;
+ * Example: let someObj : typeof(anotheraObj) = typeof(anotheraObj)(arguments here)//this creates an instance from the class of another instance
  */
 func getClass(instance:AnyObject){
 	return typeOf(instance)
