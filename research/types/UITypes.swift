18f8f0b96cedfcf97d591f9ff37076a07758beb1
Eonist <unicornhive@gmail.com>
2015-10-05 10:19:53 +0200

    Files modified: 1
    Modified 1 file:research/types/UITypes.swift

--- research/types/UITypes.swift
+++ research/types/UITypes.swift
@@ -7,3 +7,23 @@ let myTextField = UITextField(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height
 let color = UIColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
 
 let image = UIImage(contentsOfFile: "MyImage.png") {
+
+
+//
+import UIKit
+class MyViewController: UIViewController {
+    let myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+    
+    override init?(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        myButton.addTarget(self, action: "tappedButton:", forControlEvents: .TouchUpInside)
+    }
+    
+    func tappedButton(sender: UIButton!) {
+        print("tapped button")
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
