8d3ba768425b678c1d00047ea6d39c115f493082
Eonist <unicornhive@gmail.com>
2015-10-06 17:13:14 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSTableView.swift

--- research/types/ns/NSTableView.swift
+++ research/types/ns/NSTableView.swift
@@ -1,1 +1,3 @@
 // example of tableview with custom design? https://developer.apple.com/library/mac/samplecode/TableViewPlayground/TableViewPlayground.zip
+
+//this should work: http://stackoverflow.com/questions/11886048/adding-nstableview-to-nsview-programmatically
