758c17ec3561559e65cfb1c63f8213ae56ac2d4d
Eonist <unicornhive@gmail.com>
2015-10-06 08:24:47 +0200

    Files modified: 1
    Modified 1 file:geom/rect/Rect.swift

--- geom/rect/Rect.swift
+++ geom/rect/Rect.swift
@@ -12,3 +12,12 @@ struct Rect {
 let defaultRect = Rect()
 let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0),
     size: Size(width: 5.0, height: 5.0))
+    
+    
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
