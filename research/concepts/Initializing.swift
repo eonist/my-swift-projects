5ef1b43406897918807798f900c15e7b903bd26d
Eonist <unicornhive@gmail.com>
2015-10-06 13:41:42 +0200

    New files added: 1
    Added 1 file:research/concepts/Initializing.swift

---
+++ research/concepts/Initializing.swift
@@ -0,0 +1,9 @@
+//Required Initializers
+
+//Write the required modifier before the definition of a class initializer to indicate that every subclass of the class must implement that initializer:
+
+class SomeClass {
+    required init() {
+        // initializer implementation goes here
+    }
+}
