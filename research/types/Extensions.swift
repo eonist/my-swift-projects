8e7f892bd3cc484410232f95f25136ecedbee575
Eonist <unicornhive@gmail.com>
2015-10-06 08:29:18 +0200

    Files modified: 1
    Modified 1 file:research/types/Extensions.swift

--- research/types/Extensions.swift
+++ research/types/Extensions.swift
@@ -23,3 +23,19 @@ var message = "I want to reverse all the words in this string"
 message.reversewords()
 
 //Adding Constraints to Protocol Extensions: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID283
+
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+3.repetitions({
+    print("Hello!")
+})
+// Hello!
+// Hello!
+// Hello!
