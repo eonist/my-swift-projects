18cc94d9e2cbbecf934d87fd81168c1d2af05c1c
Eonist <unicornhive@gmail.com>
2015-10-06 08:23:11 +0200

    New files added: 1
    Added 1 file:geom/rect/Rect.swift

---
+++ geom/rect/Rect.swift
@@ -0,0 +1,14 @@
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0),
+    size: Size(width: 5.0, height: 5.0))
