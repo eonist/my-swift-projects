8091c2b098e7686600bcdc3fd1de9af15dee440d
Eonist <unicornhive@gmail.com>
2015-10-07 09:58:06 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSTableView.swift

--- research/types/ns/NSTableView.swift
+++ research/types/ns/NSTableView.swift
@@ -1,10 +1,13 @@
 // example of tableview with custom design? https://developer.apple.com/library/mac/samplecode/TableViewPlayground/TableViewPlayground.zip
 
-//this should work: http://stackoverflow.com/questions/11886048/adding-nstableview-to-nsview-programmatically
-
-//and this//http://stackoverflow.com/questions/4599747/programmatically-creating-an-nstableview-trouble-getting-the-nsheaderview-to-sh?rq=1
+//and this //http://stackoverflow.com/questions/4599747/programmatically-creating-an-nstableview-trouble-getting-the-nsheaderview-to-sh?rq=1
 
 
 //in ib: http://www.raywenderlich.com/87002/getting-started-with-os-x-and-swift-tutorial-part-1
 
 //apple: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSTableView_Class/
+
+
+//https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Protocols/NSTableDataSource_Protocol/index.html#//apple_ref/occ/intfm/NSTableViewDataSource/tableView:objectValueForTableColumn:row:
+
+//https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/TableView/Introduction/Introduction.html#//apple_ref/doc/uid/10000026i
