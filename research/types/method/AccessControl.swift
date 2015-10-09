45d655035f33c3018a41b80566a71fd46e8e29ce
Eonist <unicornhive@gmail.com>
2015-10-09 07:28:57 +0200

    Files modified: 1
    Modified 1 file:research/types/method/AccessControl.swift

--- research/types/method/AccessControl.swift
+++ research/types/method/AccessControl.swift
@@ -1,3 +1,9 @@
+//Explains everything: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/AccessControl.html#//apple_ref/doc/uid/TP40014097-CH41-ID3
+
+
 //no protected in swift, use public and private
 
 //https://developer.apple.com/swift/blog/?id=11
+
+//a public getter and private settet:
+public private(set) var numberOfEdits = 0
