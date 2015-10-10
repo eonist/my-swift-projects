497e9be3764cb4547c24feb829dbb38ba8e4fa45
Eonist <unicornhive@gmail.com>
2015-10-10 22:35:08 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSTrackingArea.swift

--- research/types/ns/NSTrackingArea.swift
+++ research/types/ns/NSTrackingArea.swift
@@ -13,3 +13,31 @@ struct NSTrackingAreaOptions : OptionSetType {
     static var InVisibleRect: NSTrackingAreaOptions { get }
     static var EnabledDuringMouseDrag: NSTrackingAreaOptions { get }
 }
+
+/*
+ * this may be for ios only:
+ */
+//In viewDidMove, define an NSTrackingArea to track mouse events and add it to the view
+let options = NSTrackingAreaOptions.MouseMoved | NSTrackingAreaOptions.ActiveInKeyWindow
+let trackingArea = NSTrackingArea(rect:view.frame,options:options,owner:self,userInfo:nil)
+view.addTrackingArea(trackingArea)
+
+//Declare a sprite and add it to the scene for testing purposes
+let sprite = SKSpriteNode(imageNamed:"Spaceship")
+sprite.name = "sprite"
+sprite.position = CGPointMake(CGRectGetMidX(view.frame), CGRectGetMidY(view.frame));
+sprite.setScale(0.5)
+self.addChild(sprite)
+
+//Override the mouseMoved method
+override func mouseMoved(event: NSEvent) {
+    // Get mouse position in scene coordinates
+    let location = event.locationInNode(self)
+    // Get node at mouse position
+    let node = self.nodeAtPoint(location)
+    if (node.name == "sprite") {
+        let sprite = node as SKSpriteNode
+        sprite.colorBlendFactor = 1.0
+        sprite.color = SKColor.greenColor()
+    }
+}
