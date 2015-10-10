29ee96313bf760595e0b5cc719889425debf6d99
Eonist <unicornhive@gmail.com>
2015-10-10 08:45:23 +0200

    Files modified: 1
    Modified 1 file:number/NumberParser.swift

---
+++ number/NumberParser.swift
@@ -0,0 +1,18 @@
+class NumbeParser{
+   /*
+    * Returns the floor and ceil of many numbers
+    */
+   class func findRangeFromNumbers(numbers: Number...) -> (min: Int, max: Int)? {
+       if numbers.count > 0 {
+           var min = numbers[0]
+           var max = numbers[0]
+           for number in numbers {
+               if number > max {max = number}
+               if number < min { min = number }
+           }
+           return (min, max)
+       } else {
+           return nil
+       }
+   }
+}
