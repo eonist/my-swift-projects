f1a066212fa385cb4f5eace8b6725b31e91244f0
Eonist <unicornhive@gmail.com>
2015-10-11 19:37:30 +0200

    Files modified: 1
    Modified 1 file:research/types/method/NestedMethods.swift

--- research/types/method/NestedMethods.swift
+++ research/types/method/NestedMethods.swift
@@ -10,3 +10,20 @@ func myFunctionWithNumber(someNumber: Int) {
 
 myFunctionWithNumber(5)
 // The incremented number is 15
+
+
+//Nested Functions
+//Nested functions cannot be referenced outside of their parent function.
+func summer(var num: Double) -> (Double...) -> Double {
+    // Closure: the nested function is able to close over `num`.
+    func internalFunc (numbers: Double...) -> Double {
+        for i in numbers {
+            num += i
+        }
+        return num
+    }
+
+    return internalFunc
+}
+
+summer(1.0)(2.0, 3.0)              // 6.0
