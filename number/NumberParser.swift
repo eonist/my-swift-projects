45bab6098b3e06e7bf12c8e248ba2ba49c31aab7
Eonist <unicornhive@gmail.com>
2015-10-11 11:16:58 +0200

    Files modified: 2
    Modified 2 files:number/NumberParser.swiftresearch/concepts/Delegate.swift

--- number/NumberParser.swift
+++ number/NumberParser.swift
@@ -19,13 +19,14 @@ class NumbeParser{
     * Return odd numbers
     */
     class func odds(numbers:Array<Numbers>)->Array<Numbers>{
-       return let odds = numbers.filter{$0 % 2 == 1}//great example of the filter method
+        let odds = numbers.filter{$0 % 2 == 1}//great example of the filter method
+        return odds
     }
     /*
      * sums numbers with addition
      * // :TODO: add an option for multiplication minus and dividens
      */
-     class func sum(numbers:Array):Number {
+     class func sum(numbers:Array)->Number {
       var sum:Number = 0;
       for number in numbers {
          sum += number
--- research/concepts/Delegate.swift
+++ research/concepts/Delegate.swift
@@ -1,1 +1,1 @@
-https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID267
+//https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID267
