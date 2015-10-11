bd8ab7bbd1558d4288492bcace2a90536631a1e4
Eonist <unicornhive@gmail.com>
2015-10-11 11:07:15 +0200

    Files modified: 1
    Modified 1 file:number/IntParser.swift

--- number/IntParser.swift
+++ number/IntParser.swift
@@ -2,21 +2,33 @@ class IntParser{
 	/**
 	 * 
 	 */
-	class func kind(theInt:Int)->Int{
+    class func kind(theInt:Int)->Int{
         switch theInt {
-	        case 0:
-	            return "Zero"
-	        case let x where x > 0://<--where is a constraint, must be true
-	            return "Positive"
-	        default:
-	            return "Negative"
-	        }
-	    }
-	}
-	/**
-	 * example: minMax([8, -6, 2, 109, 3, 71])//(-6,109)
-	 * example: minMax([8, -6, 2, 109, 3, 71]).min //-6
-	 * example: minMax([8, -6, 2, 109, 3, 71]).max //109
-	 */
-	
+        case 0:
+            return "Zero"
+        case let x where x > 0://<--where is a constraint, must be true
+            return "Positive"
+        default:
+            return "Negative"
+        }
+    }
+    /**
+     * example: minMax([8, -6, 2, 109, 3, 71])//(-6,109)
+     * example: minMax([8, -6, 2, 109, 3, 71]).min //-6
+     * example: minMax([8, -6, 2, 109, 3, 71]).max //109
+     */
+    class func minMax(array: [Int]) -> (min: Int, max: Int)? {
+        if array.isEmpty { return nil }
+        var currentMin = array[0]
+        var currentMax = array[0]
+        for value in array[1..<array.count] {
+            if value < currentMin {
+                currentMin = value
+            } else if value > currentMax {
+                currentMax = value
+            }
+        }
+        return (currentMin, currentMax)
+    }
 }
+
