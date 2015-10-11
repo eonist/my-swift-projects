1778acf9af4112fc544cd66397f42edbd3919392
Eonist <unicornhive@gmail.com>
2015-10-11 18:46:03 +0200

    Files modified: 1
    Modified 1 file:research/types/Enumerations.swift

--- research/types/Enumerations.swift
+++ research/types/Enumerations.swift
@@ -1,26 +1,24 @@
 class EnumTest{//enumerations:
 	//enums are simple classes that can be used like this:
-	enum CarType{
-		case Tractor
-		case Sports
-		case Sedan
-		//or case Tractor, Sports,Sedan
-	}
+    enum Letters: Character {
+        case a = "A"
+        case b = "B"
+        case c = "C"
+    }
+    func test(){
+        var example = Example.A
+        switch example {
+        case .A:
+            print("A")
+        case .B:
+            print("B")                               // B
+        case .C:
+            print("C")
+        case .D:
+            print("D")
+        }
+    }
 	
-	var johnLikes = CarType.Sedan
-	
-	//johnLikes = .Sedan
-	
-	switch johnLikes{
-        case .Sedan:
-			printin("he likes sedan")
-        case .Sports:
-			printin("he likes sport cars")
-        case .Tractor:
-			printin("he likes tractors")
-        default:
-			break;
-	}
 }
 enum Planet {
     case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
