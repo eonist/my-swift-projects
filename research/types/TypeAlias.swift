3e02b32d82115a4ef270a8b49ec1d1f6909b9cfe
Eonist <unicornhive@gmail.com>
2015-10-11 07:19:02 +0200

    Files modified: 1
    Modified 1 file:research/types/TypeAlias.swift

---
+++ research/types/TypeAlias.swift
@@ -0,0 +1,13 @@
+//i dont think you should use type alias too much, but here is a good example: (I wonder what scope type alias works under)
+
+typealias returnedFunctionType = (Int) -> String
+
+func myFuncThatReturnsAFunc() -> returnedFunctionType {
+    return { number in
+        return "The lucky number is \(number)"
+    }
+}
+
+let returnedFunction = myFuncThatReturnsAFunc()
+
+returnedFunction(5) // The lucky number is 5
