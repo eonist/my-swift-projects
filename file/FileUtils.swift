ad1c373990da6d0aaceec0f879ce4f38212e6fed
Eonist <unicornhive@gmail.com>
2015-10-11 09:01:44 +0200

    Files modified: 1
    Modified 1 file:file/FileUtils.swift

--- file/FileUtils.swift
+++ file/FileUtils.swift
@@ -2,13 +2,14 @@ class FileUtils{
 	/*
 	 * NOTE: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSWorkspace_Class/index.html#//apple_ref/occ/instm/NSWorkspace/openURL:
 	 * Example: openFile((path to desktop as text) & "del.txt")
+     * TODO: what 
 	 */
 	class func openFile(filePath:String){
 		//log file_path
 		NSWorkspace.openFile(filePath)
 	}
 	/*
-	 * @param: file_paths should be a list of "hsf" or "alias hsf" paths
+	 * PARAM: filePaths is an array of file paths
 	 */
 	class func openFiles(filePaths:Array){
 		for filePath in filePaths{
