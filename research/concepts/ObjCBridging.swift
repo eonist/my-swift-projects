7fabbce6e680ea28a45263bea15302644c6c4554
Eonist <unicornhive@gmail.com>
2015-10-11 11:18:25 +0200

    Files modified: 1
    Modified 1 file:research/concepts/ObjCBridging.swift

--- research/concepts/ObjCBridging.swift
+++ research/concepts/ObjCBridging.swift
@@ -6,14 +6,20 @@ NSDictionary Dictionary
 * >> ?
 */
 //in objc
+/*
 UITableViewCell *cell = [[UITab1eViewCe1l alloc] initWithStyle:UITab1eViewCe1lSty1eDefault reuseldentifier:@"Cell"];
+*/
+
 //in swift
 let cell = UITableViewCell(style: .Default, reuseldentifierz "Cell")
 
 
 //in objc
+/*
 UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom]
 [button addTarget:self action:@selector(didPressButton:) forControlEvents:UIContro1EventAtlEditingEvents];
+*/
+
 //in swift
 let button = UIButton.buttonWithType(.Custom)
 button.addTarget(self,action: "didPressButton:",forContro1Events: .AllEditingEvents)
