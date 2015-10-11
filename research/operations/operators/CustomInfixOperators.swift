581578b2ba399f85281b754bdc6254e8d2e97a48
Eonist <unicornhive@gmail.com>
2015-10-11 17:37:24 +0200

    Files modified: 1
    Modified 1 file:research/operations/operators/CustomInfixOperators.swift

--- research/operations/operators/CustomInfixOperators.swift
+++ research/operations/operators/CustomInfixOperators.swift
@@ -1,3 +1,4 @@
+/*
 Precedence and Associativity for Custom Infix Operators
 
 Custom infix operators can also specify a precedence and an associativity. See Precedence and Associativity for an explanation of how these two characteristics affect an infix operator’s interaction with other infix operators.
@@ -7,7 +8,7 @@ The possible values for associativity are left, right, and none. Left-associativ
 The associativity value defaults to none if it is not specified. The precedence value defaults to 100 if it is not specified.
 
 The following example defines a new custom infix operator called +-, with left associativity and a precedence of 140:
-
+*/
 infix operator +- { associativity left precedence 140 }
 func +- (left: Vector2D, right: Vector2D) -> Vector2D {
     return Vector2D(x: left.x + right.x, y: left.y - right.y)
@@ -16,4 +17,4 @@ let firstVector = Vector2D(x: 1.0, y: 2.0)
 let secondVector = Vector2D(x: 3.0, y: 4.0)
 let plusMinusVector = firstVector +- secondVector
 // plusMinusVector is a Vector2D instance with values of (4.0, -2.0)
-This operator adds together the x values of two vectors, and subtracts the y value of the second vector from the first. Because it is in essence an “additive” operator, it has been given the same associativity and precedence values (left and 140) as default additive infix operators such as + and -. For a complete list of the default Swift operator precedence and associativity settings, see Expressions.
+//This operator adds together the x values of two vectors, and subtracts the y value of the second vector from the first. Because it is in essence an “additive” operator, it has been given the same associativity and precedence values (left and 140) as default additive infix operators such as + and -. For a complete list of the default Swift operator precedence and associativity settings, see Expressions.
