bb59ebfbbb6b762a4c943a9910eb046f8b731fd5
Eonist <unicornhive@gmail.com>
2015-10-11 17:36:02 +0200

    Files modified: 1
    Modified 1 file:research/operations/literalExpression.swift

--- research/operations/literalExpression.swift
+++ research/operations/literalExpression.swift
@@ -1,3 +1,5 @@
+
+/*
 Literal Expression
 
 A literal expression consists of either an ordinary literal (such as a string or a number), an array or dictionary literal, or one of the following special literals:
@@ -5,6 +7,7 @@ A literal expression consists of either an ordinary literal (such as a string or
 Literal
 Type
 Value
+
 __FILE__
 String
 The name of the file in which it appears.
@@ -19,7 +22,9 @@ String
 The name of the declaration in which it appears.
 Inside a function, the value of __FUNCTION__ is the name of that function, inside a method it is the name of that method, inside a property getter or setter it is the name of that property, inside special members like init or subscript it is the name of that keyword, and at the top level of a file it is the name of the current module.
 
-When used as the default value of a function or method, the special literal’s value is determined when the default value expression is evaluated at the call site.
+*/
+
+//When used as the default value of a function or method, the special literal’s value is determined when the default value expression is evaluated at the call site.
 
 func logFunctionName(string: String = __FUNCTION__) {
     print(string)
@@ -27,6 +32,7 @@ func logFunctionName(string: String = __FUNCTION__) {
 func myFunction() {
     logFunctionName() // Prints "myFunction()".
 }
- 
+ /*
 myFunction()
 namedArgs(1, withJay: 2)
+*/
