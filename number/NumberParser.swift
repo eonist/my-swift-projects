afcbeba078f81a355f29709c4382bc449b24eea5
Eonist <unicornhive@gmail.com>
2015-10-11 07:42:53 +0200

    Files modified: 1
    Modified 1 file:number/NumberParser.swift

--- number/NumberParser.swift
+++ number/NumberParser.swift
@@ -21,4 +21,15 @@ class NumbeParser{
     class func odds(numbers:Array<Numbers>)->Array<Numbers>{
        return let odds = numbers.filter{$0 % 2 == 1}//great example of the filter method
     }
+    /*
+     * sums numbers with addition
+     * // :TODO: add an option for multiplication minus and dividens
+     */
+     class func sum(numbers:Array):Number {
+      var sum:Number = 0;
+      for number in numbers {
+         sum += number
+      }
+      return sum;
+    }
 }
