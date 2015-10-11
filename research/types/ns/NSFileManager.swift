6104174eac896b6307d92e6c133c1f5a43feddfe
Eonist <unicornhive@gmail.com>
2015-10-11 19:01:25 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSFileManager.swift

--- research/types/ns/NSFileManager.swift
+++ research/types/ns/NSFileManager.swift
@@ -1,1 +1,21 @@
 import Foundation
+
+//Excerpt From: Apple Inc. “Using Swift with Cocoa and Objective-C (Swift 2 Prerelease).” iBooks.
+//Example: (from the book)
+/*
+NSFileManager *fileManager = [NSFileManager defaultManager];
+NSURL *URL = [NSURL fileURLWithPath:@"/path/to/file"];
+NSError *error = nil;
+BOOL success = [fileManager removeItemAtURL:URL error:&error];
+if (!success && error){
+NSLog(@"Error: %@", error.domain);
+}
+*/
+
+let fileManager = NSFileManager.defaultManager()
+let URL = NSURL.fileURLWithPath("path/to/file")
+do {
+    try fileManager.removeItemAtURL(URL)
+} catch let error as NSError {
+    print ("Error: \(error.domain)")
+}
