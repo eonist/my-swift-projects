ce59ffcee26ec143e1aa7572829fc37cd49776f7
Eonist <unicornhive@gmail.com>
2015-10-11 08:19:46 +0200

    Files modified: 1
    Modified 1 file:research/operations/For.swift

--- research/operations/For.swift
+++ research/operations/For.swift
@@ -4,17 +4,14 @@ for someItem in allItems{//can also work with strings printing out single chars,
 	//do something eith someItem
 }
 
-for var i = 0; i < 5;i++(
+for var i = 0; i < 5;i++{
 
-)
-//loop with range
-for index in 1...9{//0...someVal will also work, or 0...<5 will do 0-4
-	//do something with index
 }
+
 
 //for loop with a break:
 //break statement exiting a for..in statement
-for count in 1...10 { /I supposed to loop 10 times
+for count in 1...10 { //I supposed to loop 10 times
 	if (count == 5) {
 		break //I terminates Loop if count is 5
 	}
