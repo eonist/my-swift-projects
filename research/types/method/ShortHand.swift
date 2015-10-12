cf90f493306c9ce610bcd65d319b5ff1ed8373af
Eonist <unicornhive@gmail.com>
2015-10-12 10:32:34 +0200

    Files modified: 1
    Modified 1 file:research/types/method/ShortHand.swift

--- research/types/method/ShortHand.swift
+++ research/types/method/ShortHand.swift
@@ -1,1 +1,17 @@
-import Foundation
+
+
+//methods can be simplified:
+//The following closure expressions are equivalent:
+
+myFunction {
+    (x: Int, y: Int) -> Int in
+    return x + y
+}
+
+
+myFunction2 {
+    (x, y) in
+    return x + y
+}
+
+
