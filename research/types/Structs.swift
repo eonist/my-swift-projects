d3379c23f795e4da5c4cc12b1bb7deb2bdce71c9
Eonist <unicornhive@gmail.com>
2015-10-12 23:22:38 +0200

    Files modified: 1
    Modified 1 file:research/types/Structs.swift

--- research/types/Structs.swift
+++ research/types/Structs.swift
@@ -1,20 +1,27 @@
 // CGSize struct
 import Foundation
 
-var size : CGSize = CGSizeMake(100, 100)
+
 // access struct members
-size.height
-size.width
-// CGPoint struct
-var screenLocation : CGPoint = CGPointMake(45,100)
-screenLocat1on.x
-screenLocation.y
-// CGRect struct
-var rectangle : CGRect = CGRectMake(10, 10, 400, 400)
+func test(){
+    let size : CGSize = CGSizeMake(100, 100)
+    print(size.height)
+    print(size.width)
+    
+    // CGPoint struct
+    let screenLocation : CGPoint = CGPointMake(45,100)
+    screenLocation.x
+    screenLocation.y
+    // CGRect struct
+    let rectangle:CGRect = CGRectMake(10, 10, 400, 400)
+    print(rectangle.width)
+    print(rectangle.height)
+    //
+    struct Size {
+        var width = 0.0, height = 0.0
+    }
+    
+    let twoByTwo:Size = Size(width: 2.0, height: 2.0)
+    print(twoByTwo.width)
 
-
-//
-struct Size {
-    var width = 0.0, height = 0.0
 }
-let twoByTwo = Size(width: 2.0, height: 2.0)
