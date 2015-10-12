2a5bd862eaf603ffce8b433dc990c02c207cec2f
Eonist <unicornhive@gmail.com>
2015-10-12 11:10:00 +0200

    Files modified: 1
    Modified 1 file:research/types/method/methodAsArgument.swift

--- research/types/method/methodAsArgument.swift
+++ research/types/method/methodAsArgument.swift
@@ -1,1 +1,12 @@
-import Foundation
+func test(){
+    var array:Array = ["a","c","b"]
+    array.sortInPlace(>)
+    /*
+    
+    not working
+    
+    array.sort({
+        $0.name < $1.name
+    })
+    */
+}
