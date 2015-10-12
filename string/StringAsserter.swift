bb49f746b58544de1a8deb91f0c545ce6b4c1fc4
Eonist <unicornhive@gmail.com>
2015-10-12 21:50:00 +0200

    Files modified: 2
    Modified 2 files:string/StringAsserter.swiftstring/StringParser.swift

--- string/StringAsserter.swift
+++ string/StringAsserter.swift
@@ -5,4 +5,13 @@ class StringAsserter{
 	class func isWrappedWith(string:String, char:Character){
 		return (string.first == char) and (string.last == char)
 	}
+	/**
+	 *	Determines whether the specified string begins with the spcified prefix.
+	 *	@param input: The string that the prefix will be checked against.
+	 *	@param prefix: The prefix that will be tested against the string.
+	 *	@return True if the string starts with the prefix, false if it does not.
+	 */	
+	class func beginsWith(input:String, prefix:String):Boolean{			
+		return (prefix == input.substring(0, prefix.length));
+	}	
 }
--- string/StringParser.swift
+++ string/StringParser.swift
@@ -14,8 +14,25 @@ class StringParser{
 	class func firstWord(string:String)->String {
        return string.componentsSeparatedByString(" ")[0]
    }
-   class lastChar(string:String)->String {
+   class func lastChar(string:String)->String {
         return string[string.length()-1]
     }
+    /**
+	 * Returns an array comprised of two strings derived from splitting @param string at @param index
+	 * splitAt("Hello, playground",5)//["hello"," playground"]
+	 */
+	class func function splitAt(str:String, index:int):Array {
+		return [string.substring(0,index),string.substring(index,string.length)];
+		let a:String = str.substringWithRange(Range<String.Index>(start:0 , end:index )) //"llo, playgroun",str.startIndex.advancedBy(2),str.endIndex.advancedBy(-1)
+		let b:String = str.substringWithRange(Range<String.Index>(start:index , end:str.endIndex )) //"llo, playgroun",str.startIndex.advancedBy(2),str.endIndex.advancedBy(-1)
+		let retVal:Array = [a,b]
+		return retVal
+	}
+	/**
+	 * 
+	 */
+	class func boolean(string:String):Bool{
+		return string == "true";
+	}
 }
     //resolve path extension
