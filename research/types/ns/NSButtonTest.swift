06593441e8913b5da389d09761b52604dff6fcf0
Eonist <unicornhive@gmail.com>
2015-10-12 11:14:10 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSButtonTest.swift

--- research/types/ns/NSButtonTest.swift
+++ research/types/ns/NSButtonTest.swift
@@ -1,16 +1,21 @@
 //https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/Button/Button.html#//apple_ref/doc/uid/10000019i
 import Cocoa
 
-let button = NSButton()
-   button.target = self
-   button.action = "myAction:"
-
-//The above specifies callback on 'self' to be the function
-
-func myAction(obj:AnyObject?){
-   print("press")
-      if let btn = obj as? NSButton{
-           print(btn.title)
-      }
-   
+class NSButtonTest {
+    let button = NSButton()
+    
+    func test(){
+        button.target = self
+        button.action = "myAction:"
+    }
+    
+    //The above specifies callback on 'self' to be the function
+    
+    func myAction(obj:AnyObject?){
+        print("press")
+        if let btn = obj as? NSButton{
+            print(btn.title)
+        }
+        
+    }
 }
