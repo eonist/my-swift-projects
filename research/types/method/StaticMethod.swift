44592694e4c312e99d43a4af751b967406101360
Eonist <unicornhive@gmail.com>
2015-10-12 10:45:21 +0200

    Files modified: 1
    Modified 1 file:research/types/method/StaticMethod.swift

---
+++ research/types/method/StaticMethod.swift
@@ -0,0 +1,20 @@
+/**
+* public static constant methods and properties:
+*/
+class StringUtil{
+    static var STRING_UTIL_NAME = "string util"//this is what is typically refered to as a public staic constant in other languages and is accessible from the class it self, not instances of it
+    /**
+    * Returns a list of text items by splitting a text at every delimiter
+    */
+    class func split(string:String, delimiter:String)->Array<String>{
+        string.componentsSeparatedByString(delimiter)
+    }
+    /**
+    * NOTE: you cant access instance level variables when using public static constants
+    */
+    class func name()-> String{
+        return STRING_UTIL_NAME
+    }
+}
+StringUtil.name//string util
+StringUtil.split("abc 123"," ")[0]//"123"
