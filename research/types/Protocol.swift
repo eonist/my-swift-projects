14e8ebe7277e7bbc5b993b14505ff162288164f1
Eonist <unicornhive@gmail.com>
2015-10-12 23:09:56 +0200

    Files modified: 1
    Modified 1 file:research/types/Protocol.swift

--- research/types/Protocol.swift
+++ research/types/Protocol.swift
@@ -32,3 +32,97 @@ class MyClass : ExampleProtocol {
 }
 
 
+protocol Image {
+    // Protocol defines only a properties'
+    // type and whether it's gettable or settable
+    var filename: String { get set }
+    var filesize: Double { get }
+    var mimetype: String { get }
+    var height: Double { get }
+    var width: Double { get }
+    
+    /*    class var someTypeProperty: Int { get set } */
+    
+    func save ()
+    
+    mutating func resize(width: Double, height: Double)
+}
+
+struct Gif : Image {
+    var filename: String
+    var filesize: Double
+    var mimetype = "image/gif"
+    var height: Double
+    var width: Double
+    
+    func save() {
+        
+    }
+    
+    mutating func resize(width: Double, height: Double) {
+        
+    }
+}
+
+class Png : Image {
+    var filename: String
+    var filesize: Double
+    var mimetype = "image/png"
+    var height: Double
+    var width: Double
+    
+    init(filename: String, filesize: Double, width: Double, height: Double) {
+        self.filename = filename
+        self.filesize = filesize
+        self.width = width
+        self.height = height
+    }
+    
+    func save() {
+        
+    }
+    
+    // `mutating` is not required when the protocol already
+    // declares it as such.
+    func resize(width: Double, height: Double) {
+        
+    }
+}
+
+var gif = Gif(filename: "rainbow.gif", filesize: 2.3, mimetype: "image", height: 50, width: 50)
+var png = Png(filename: "carry.png", filesize: 3.4, width: 100, height: 54)
+
+
+func resizeImage (var img: Image) {
+    img.resize(50.0, height: 50.0)
+}
+
+
+protocol Video {
+    var framerate: Int { get }
+    var resolution: Double { get }
+}
+
+
+struct Media: Image, Video {
+    var filename: String
+    var filesize: Double
+    var mimetype: String
+    var height: Double
+    var width: Double
+    var framerate: Int
+    var resolution: Double
+    
+    func save() {
+        
+    }
+    
+    func resize(width: Double, height: Double) {
+        
+    }
+}
+
+
+func save(media: protocol<Image, Video>) {
+    media.save()
+}
