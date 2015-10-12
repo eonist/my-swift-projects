a12b19e1e80495ae2920e8001c75464b2419403b
Eonist <unicornhive@gmail.com>
2015-10-12 12:09:53 +0200

    Files modified: 2
    Modified 2 files:research/types/NSWindowTest.swiftresearch/types/ns/NSWindowTest.swift

--- research/types/NSWindowTest.swift
+++ research/types/NSWindowTest.swift
@@ -50,4 +50,30 @@ func test(){
  //edit design properties of a window: 
  //window?.backgroundColor = NSColor.clearColor();
 //window?.opaque = false
+
+
+//apple help: https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/WinPanel/Tasks/SizingPlacingWindows.html
+//aplle refs: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSWindow_Class/index.html#//apple_ref/occ/instm/NSWindow/setTitle:
+
+//round borderless,titleless, etc interface window: https://developer.apple.com/library/mac/samplecode/RoundTransparentWindow/Introduction/Intro.html
+
+//borderless window
+//http://www.cimgf.com/cocoa-code-snippets/nswindow-snippets/
+
+//fullscrreen: window.setFrame(NSScreen.mainScreen()!.visibleFrame, display: true, animate: true)
+
+//pin to front:
+/*
+[self.window makeKeyAndOrderFront:nil];
+[self.window setLevel:NSStatusWindowLevel];
+*/
+
+//custom window: in objc: http://www.cocoawithlove.com/2008/12/drawing-custom-window-on-mac-os-x.html
+
+
+//docs: nswin swift osx: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSWindow_Class/#//apple_ref/occ/instm/NSWindow/initWithContentRect:styleMask:backing:defer:
+
+
+
+
         
