d7879f88091b72d7a222abf3b79445d9ee4a0c94
Eonist <unicornhive@gmail.com>
2015-10-12 10:31:13 +0200

    Files modified: 1
    Modified 1 file:research/types/method/InOuts.swift

--- research/types/method/InOuts.swift
+++ research/types/method/InOuts.swift
@@ -1,1 +1,26 @@
-import Foundation
+
+//Here’s an example of a function called swapTwoInts(_:_:), which has two in-out integer parameters called a and b:
+
+func swapTwoInts(inout a: Int, inout _ b: Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+
+
+//The swapTwoInts(_:_:) function simply swaps the value of b into a, and the value of a into b. The function performs this swap by storing the value of a in a temporary constant called temporaryA, assigning the value of b to a, and then assigning temporaryA to b.
+
+//You can call the swapTwoInts(_:_:) function with two variables of type Int to swap their values. Note that the names of someInt and anotherInt are prefixed with an ampersand when they are passed to the swapTwoInts(_:_:) function:
+
+func test(){
+    var someInt = 3
+    var anotherInt = 107
+    swapTwoInts(&someInt, &anotherInt)
+    print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+}
+
+// prints "someInt is now 107, and anotherInt is now 3"
+//The example above shows that the original values of someInt and anotherInt are modified by the swapTwoInts(_:_:) function, even though they were originally defined outside of the function.
+
+//NOTE In-out parameters are not the same as returning a value from a function. The swapTwoInts example above does not define a return type or return a value, but it still modifies the values of someInt and anotherInt. In-out parameters are an alternative way for a function to have an effect outside of the scope of its function body.
