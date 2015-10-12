de1d422a2e6e1fa7dc4f983d00a2d88e6ad954a8
Eonist <unicornhive@gmail.com>
2015-10-12 11:10:00 +0200

    Files modified: 1
    Modified 1 file:research/types/NSFile.swift

--- research/types/NSFile.swift
+++ research/types/NSFile.swift
@@ -1,1 +1,10 @@
 //"~/a.txt".stringByExpandingTildeInPath // prints /Users/<your user>/a.txt
+
+class Job {
+    var title = ""
+}
+
+var job1 = Job()
+var job2 = job1
+job1.title = "singer"
+print(job2.title)
