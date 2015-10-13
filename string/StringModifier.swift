5430a83fccb405272ed489b826bee794bba10add
Eonist <unicornhive@gmail.com>
2015-10-13 19:47:04 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -45,8 +45,8 @@ class StringModifier{
      * NOTE: we use replaceRange in combination with the .capitalizedString method
      * NOTE: string[string.start.index] returns a Character instance. then we cast this as a string
      */
-    class func capFirstChar(var string:String){
-        string.characters[0] + StringParser.range(1,)
+    class func capFirstChar(var str:String){
+        return string.characters[0] + StringParser.subString(1,str.characters.count)
         //string.replaceRange(string.startIndex...string.startIndex, with: String(string[string.startIndex]).capitalizedString)
     }
     /*
