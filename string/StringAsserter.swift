505e5b1f405682e719b099c692e33e231122ad74
Eonist <unicornhive@gmail.com>
2015-10-13 10:36:21 +0200

    Files modified: 2
    Modified 2 files:research/types/Range.swiftstring/StringAsserter.swift

--- string/StringAsserter.swift
+++ string/StringAsserter.swift
@@ -6,19 +6,14 @@ class StringAsserter{
 		return (string.characters.first == char) && (string.characters.last == char)
 	}
 	/**
-	 *	Determines whether the specified string begins with the spcified prefix.
+	 *	Asserts whether the specified string begins with the spcified prefix.
 	 *	@param input: The string that the prefix will be checked against.
 	 *	@param prefix: The prefix that will be tested against the string.
 	 *	@return True if the string starts with the prefix, false if it does not.
 	 */	
-	class func beginsWith(input:String, prefix:String)->Bool{
-		//return (prefix == input.subString(0, prefix.characters.count));
-        let name = "How do you use String.substringWithRange?"
-        let range = prefix.startIndex..<name.startIndex.advancedBy(10)
-        let res:String = name.substringWithRange(range)
-
-        
-        return false
+	class func beginsWith(string:String, prefix:String)->Bool{
+        let range = prefix.startIndex..<prefix.endIndex//create a range
+        return (prefix == string.substringWithRange(range));
 	}
     /*
      *
