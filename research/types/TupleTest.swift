c8e7eb4d19c17ba43d2f8bacb98779201800c2fd
Eonist <unicornhive@gmail.com>
2015-10-13 08:25:54 +0200

    Files modified: 1
    Modified 1 file:research/types/TupleTest.swift

--- research/types/TupleTest.swift
+++ research/types/TupleTest.swift
@@ -1,3 +1,4 @@
+import Foundation
 func testing(){
     //tuples are almost like objects
     var someTuple = ("test",22,10.2,"abc")
@@ -35,7 +36,7 @@ func testing(){
     
     //Toggle different tuple types
     // This url consists of host name.com and path /12345
-    let urlComponents = componentsFromUrlString("http://name.com/12345;param?foo=1&baa=2#fragment")
+    let urlComponents = NSURL.componentsFromUrlString("http://name.com/12345;param?foo=1&baa=2#fragment")
     
     switch (urlComponents.host, urlComponents.path) {
     case let (.Some(host), .Some(path)):
