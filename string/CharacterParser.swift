92eed8ab144de978dc638996318426c03bd61409
Eonist <unicornhive@gmail.com>
2015-10-13 19:27:03 +0200

    Files modified: 1
    Modified 1 file:string/CharacterParser.swift

--- string/CharacterParser.swift
+++ string/CharacterParser.swift
@@ -1,14 +1,16 @@
 import Foundation
 
 class CharacterParser {
-    /*
+    /**
      *
      */
     class func successor(str:String)->Character{
         return str.characters[str.startIndex.successor()]
     }
-    /*
+    /**
      *
      */
-    
+    func predecessor()->Character{
+        return str.characters[str.startIndex.successor()]
+    }
 }
