900d2312d621272bb48b72c5faec9ad99cb06956
Eonist <unicornhive@gmail.com>
2015-10-13 20:17:13 +0200

    Files modified: 2
    Modified 2 files:string/StringModifier.swiftstring/character/CharacterParser.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -45,7 +45,8 @@ class StringModifier{
      * NOTE: Can also be done through replaceRange: string.replaceRange(string.startIndex...string.startIndex, with: String(string[string.startIndex]).capitalizedString) //we use replaceRange in combination with the .capitalizedString method // string[string.start.index] returns a Character instance. then we cast this as a string
      */
     class func capFirstChar(str:String)->String{
-        return str.characters[0] + StringParser.subString(str,1,str.characters.count)
+        let firstChar:Character = str.characters[0]
+        return firstChar + StringParser.subString(str,1,str.characters.count)
     }
     /*
      *
--- string/character/CharacterParser.swift
+++ string/character/CharacterParser.swift
@@ -2,21 +2,21 @@ class CharacterParser {
     /**
      * Returns the char after @param index
      */
-    class func successorAt(str:String,index:Int)->Character{
+    class func successorAt(str:String,_ index:Int)->Character{
         let strIndex = str.startIndex.advancedBy(index)
         return str.characters[strIndex.successor()]
     }
     /**
      * Returns the char before @param index
      */
-    class func predecessorAt(str:String,index:Int)->Character{
+    class func predecessorAt(str:String,_ index:Int)->Character{
         let strIndex = str.startIndex.advancedBy(index)
         return str.characters[strIndex.predecessor()]
     }
     /**
      * Returns the first occurence of @param char in @param str
      */
-    class func indexOf(str:String,char:Character)->Int{
+    class func indexOf(str:String,_ char:Character)->Int{
         if let strIndex = str.characters.indexOf(char) {
             return str.startIndex.distanceTo(strIndex)
         }
@@ -27,7 +27,7 @@ class CharacterParser {
     /**
      * Returns a Character instance at @param index
      */
-    class func charAt(str:String,index:Int)->Character{
+    class func charAt(str:String,_ index:Int)->Character{
         let index = str.startIndex.advancedBy(index) //will call succ n times until index
         let char:Character = str[index] //you can also do: text.characters[index2]
         return char
