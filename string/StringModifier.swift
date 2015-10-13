1e3b39f8b1885670f437d4fe18ecf1982796fb83
Eonist <unicornhive@gmail.com>
2015-10-13 18:57:22 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -41,14 +41,16 @@ class StringModifier{
         return string.uppercaseString
     }
     /*
-     * To capitalize only the first word in a String, we use replaceRange in combination with the .capitalizedString method
+     * Returns @param string with the first character capitalizesed, 
+     * NOTE: we use replaceRange in combination with the .capitalizedString method
      * NOTE: string[string.start.index] returns a Character instance. then we cast this as a string
      */
     class func capFirstChar(var string:String){
-        string.replaceRange(string.startIndex...string.startIndex, with: String(string[string.startIndex]).capitalizedString)
+        string.characters[0] +
+        //string.replaceRange(string.startIndex...string.startIndex, with: String(string[string.startIndex]).capitalizedString)
     }
     /*
      *
      */
-    class func 
+    class func
 } 
