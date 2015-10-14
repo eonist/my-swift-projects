f5737415b15b876b8913e4421d5518a80e95951f
Eonist <unicornhive@gmail.com>
2015-10-14 19:25:18 +0200

    Files modified: 1
    Modified 1 file:file/path/PathParser.swift

--- file/path/PathParser.swift
+++ file/path/PathParser.swift
@@ -3,7 +3,7 @@ class PathParser {
     /**
      * Returns the path to where you can save your app's files. Here it is:
      */
-    class func docPath()->String{
+    class func appDocPath()->String{
         let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
         return paths[0]
     }
