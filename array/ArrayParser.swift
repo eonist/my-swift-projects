c8ad2bb3895c484dcecb4a3f83e42c4391be6e57
Eonist <unicornhive@gmail.com>
2015-10-14 20:06:37 +0200

    Files modified: 1
    Modified 1 file:array/ArrayParser.swift

--- array/ArrayParser.swift
+++ array/ArrayParser.swift
@@ -2,7 +2,7 @@ class ArrayParser{
 	/**
 	 * returns the index for item, -1 of none is found
 	 */
-	class func index<T: Equatable>(array: [T], _ valueToFind: T) -> Int? {//the <T: Equatable> part ensures that the types can use the equal operator ==
+	class func index<T: Equatable>(array: [T], _ value: T) -> Int? {//the <T: Equatable> part ensures that the types can use the equal operator ==
 	    for (index, item) in array.enumerate() {
 	        if value == item {
 	            return index
@@ -10,4 +10,26 @@ class ArrayParser{
 	    }
 	    return -1
 	}
+    
+    /**
+     * EXAMPLE: indexOf(["Apples", "Peaches", "Plums"],"Peaches")
+     * NOTE: you can also do things like {$0 > 5} , {$0 == str}  etc
+     */
+    class func indexOfStr(arr:Array<String>,str:String)->Int{
+        if let index = arr.indexOf(str) {
+            return index
+        }else{
+            return -1//-1 indicates non was found
+        }
+    }
+    /**
+     *
+     */
+    class func indexOfObj<T : Equatable>(arr : [T], _ value:T)->Int{
+        if let index = arr.indexOf(value) {
+            return index
+        }else{
+            return -1//-1 indicates non was found
+        }
+    }
 }
