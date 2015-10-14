e3865139a4c06634fd6109a5ef21ae4396a393e7
Eonist <unicornhive@gmail.com>
2015-10-14 19:17:04 +0200

    Files modified: 1
    Modified 1 file:file/FileModifier.swift

--- file/FileModifier.swift
+++ file/FileModifier.swift
@@ -16,4 +16,20 @@ class FileModifier{
 		    print("Error: \(error.domain)")
 		}
 	}
+    /**
+     *
+     */
+    class func write(path:String,content:String)->Bool{
+        /*
+        let str = "Super long string here"
+        let filename = getDocumentsDirectory().stringByAppendingPathComponent("output.txt")
+        
+        do {
+        try str.writeToFile(filename, atomically: true, encoding: NSUTF8StringEncoding)
+        } catch {
+        // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+
+        */
+    }
 }
