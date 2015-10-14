bd2bf0a263aabda8565db1f8f0bd7c7c4e853289
Eonist <unicornhive@gmail.com>
2015-10-14 19:27:13 +0200

    Files modified: 1
    Modified 1 file:file/FileModifier.swift

--- file/FileModifier.swift
+++ file/FileModifier.swift
@@ -21,12 +21,11 @@ class FileModifier{
      * let str = "Super long string here"
      */
     class func write(path:String,content:String)->Bool{
-        
-        
-        let filename = getDocumentsDirectory().stringByAppendingPathComponent("output.txt")
+        let theURL:NSURL = NSURL(fileURLWithPath: PathParser.appDocPath())
+        let filename = theURL.URLByAppendingPathComponent("output.txt")
         
         do {
-        try str.writeToFile(filename, atomically: true, encoding: NSUTF8StringEncoding)
+            try content.writeToFile(filename, atomically: true, encoding: NSUTF8StringEncoding)
         } catch {
         // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
         }
