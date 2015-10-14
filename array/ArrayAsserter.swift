9f432b9cd8ab60158de6904c1e3fc1f611866e89
Eonist <unicornhive@gmail.com>
2015-10-14 19:45:45 +0200

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -1,3 +1,13 @@
 class ArrayAsserter {
+    /**
+     * EXAMPLE: indexOf(["Apples", "Peaches", "Plums"],"Peaches")
+     */
     
+    class func indexOfObj(){
+        let array = ["Apples", "Peaches", "Plums"]
+        
+        if let index = array.indexOf("Peaches") {
+            print("Found peaches at index \(index)")
+        }
+    }
 }
