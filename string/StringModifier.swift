faa6f081c19363b719071bafd4d8acee53d02b0e
Eonist <unicornhive@gmail.com>
2015-10-14 08:03:48 +0200

    Files modified: 1
    Modified 1 file:string/StringModifier.swift

--- string/StringModifier.swift
+++ string/StringModifier.swift
@@ -72,5 +72,6 @@ class StringModifier{
      */
     class func replaceRange(){
         
+        string.replaceRange(string.startIndex...string.startIndex, with: String(string[string.startIndex]).capitalizedString) //we use replaceRange in combination with the .capitalizedString method // string[string.start.index] returns a Character instance. then we cast this as a string
     }
 } 
