8f72c41617f34f27b59f714a61eecfe583532636
Eonist <unicornhive@gmail.com>
2015-10-14 18:57:53 +0200

    Files modified: 1
    Modified 1 file:misc/network/NetworkParser.swift

--- misc/network/NetworkParser.swift
+++ misc/network/NetworkParser.swift
@@ -1,13 +1,31 @@
+import Foundation
 class NetworkParser{
 	/**
 	 * Returns a string for a URL
 	 */
 	class func string(URL:String)->String{
-		let theURL = NSURL(string: URL)
+		/*
+        let theURL = NSURL(string: URL)
 		let rssUrlRequest:NSURLRequest = NSURLRequest(URL:theURL!)
 	 	let queue:NSOperationQueue = NSOperationQueue()
 	 	//the bellow is sudo code
 	 	let result = NSURLConnection.sendAsynchronousRequest(rssUrlRequest, queue: queue) 
 		return result
+        */
 	}
+    /**
+     * @param url: "https://www.google.com"
+     */
+    class func stringContent(url:String)->String{
+        if let urlObj = NSURL(string: url) {
+            do {
+                let contents = try NSString(contentsOfURL: urlObj, usedEncoding: nil)
+                print(contents)
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // the URL was bad!
+        }
+    }
 }
