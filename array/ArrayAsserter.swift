69b2a4b77ba6c396844eba23c74fd47371e17d8a
Eonist <unicornhive@gmail.com>
2015-10-14 19:57:19 +0200

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -4,12 +4,11 @@ class ArrayAsserter {
      * EXAMPLE: indexOf(["Apples", "Peaches", "Plums"],"Peaches")
      * NOTE: you can also do things like {$0 > 5}
      */
-    class func indexOfObj(arr:Array<String>,str:String)->Int{
+    class func indexOf<T>(arr : [T],str:String)->Int{
         if let index = arr.indexOf({$0 == str}) {
             return index
         }else{
             return -1//-1 indicates non was found
         }
-        
     }
 }
