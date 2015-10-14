81a9446b48c0138e81b7129591610961e3a3a99a
Eonist <unicornhive@gmail.com>
2015-10-14 10:16:10 +0200

    Files modified: 1
    Modified 1 file:string/character/CharacterModifier.swift

--- string/character/CharacterModifier.swift
+++ string/character/CharacterModifier.swift
@@ -2,7 +2,7 @@ class CharacterModifier {
     /**
      *
      */
-    class func removeAt(str:String,index:Int)->String{
-        str.characters
+    class func removeAt(var str:String,_ index:Int)->String{
+        str.removeAtIndex(str.characters.indices[index])
     }
 }
