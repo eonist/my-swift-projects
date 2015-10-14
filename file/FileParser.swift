bea432b453c638e94b939cb56e0f8ce84c849f73
Eonist <unicornhive@gmail.com>
2015-10-14 19:05:37 +0200

    Files modified: 1
    Modified 1 file:file/FileParser.swift

--- file/FileParser.swift
+++ file/FileParser.swift
@@ -1,17 +1,18 @@
+import Foundation
 class FileParser{
 	/*
 	 * Returns string content from a file at file location "filePath"
      * PARAM filePath is the file path to the file
      * Todo: What format is the filePath?
 	 */
-	class func string(filePath:String)->String{
-		var err: NSError?
-		let content = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: &err)
-		if (err == nil){
-			return content
-		}else{
-			return nil
-		}
+	class func string(path:String)->String{
+
+        do{
+            let content = try String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding)
+            return content
+        }catch {
+            return nil
+        }
 	}
 	/**
 	 * Returns the project resource folder
@@ -20,5 +21,19 @@ class FileParser{
 	class func resourcePath()->String{
 		return NSBundle.mainBundle().resourcePath
 	}
-    
+    /**
+     *
+     */
+    class func resourceString(filePath)->String{
+        if let filepath = NSBundle.mainBundle().pathForResource("example", ofType: "txt") {
+            do {
+                let contents = try NSString(contentsOfFile: filepath, usedEncoding: nil) as String
+                print(contents)
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // example.txt not found!
+        }
+    }
 }
