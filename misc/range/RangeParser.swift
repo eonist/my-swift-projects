5a8feba427fbbc5e7ffee0acc8ff4838fa8dffb7
Eonist <unicornhive@gmail.com>
2015-10-14 08:18:47 +0200

    Files modified: 1
    Modified 1 file:misc/range/RangeParser.swift

--- misc/range/RangeParser.swift
+++ misc/range/RangeParser.swift
@@ -2,7 +2,9 @@ class RangeParser {
     /**
      *
      */
-    class func stringRange(str:String,_ beginning:Int,_ end:Int)->Range<String.Index>{
-        
+    class func stringRange(str:String,_ start:Int,_ end:Int)->Range<String.Index>{
+        let startIndex = str.startIndex.advancedBy(index)
+        let endIndex = str.startIndex.advancedBy(index)
+        return Range(,str.startIndex)
     }
 }
