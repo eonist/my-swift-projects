a4d18f9c9a8fb0d2c8f9b0b0e6b27d81b51682b6
Eonist <unicornhive@gmail.com>
2015-10-14 10:21:47 +0200

    Files modified: 1
    Modified 1 file:string/character/CharacterModifier.swift

--- string/character/CharacterModifier.swift
+++ string/character/CharacterModifier.swift
@@ -1,7 +1,7 @@
 class CharacterModifier {
     /**
      * Removes a character at @param index
-    * NOTE: These can be used to remove first and last: str.removeAtIndex(str.characters.indices.first!) // remove first letterstr.removeAtIndex(str.characters.indices.last!) // remove last letter
+     * NOTE: These can be used to remove first and last: str.removeAtIndex(str.characters.indices.first!) // remove first letterstr.removeAtIndex(str.characters.indices.last!) // remove last letter
      */
     class func removeAt(var str:String,_ index:Int)->String{
         str.removeAtIndex(str.startIndex.advancedBy(index))
