3c6456689c27fdef0626252b63a28d05116b1ad0
Eonist <unicornhive@gmail.com>
2015-10-15 09:04:55 +0200

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -1,4 +1,14 @@
 import Foundation
 class ArrayAsserter {
-
+    /**
+     *
+     */
+    class func pop<T>(array:[T],obj:T)->Bool {
+        
+        if array.contains(obj) {
+            return true
+        } else {
+            return false;
+        }
+    }
 }
