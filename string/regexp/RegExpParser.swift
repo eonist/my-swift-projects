12a59f3379630e7737c9a113dbb426612c9305f6
Eonist <unicornhive@gmail.com>
2015-10-15 22:43:32 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpParser.swift

--- string/regexp/RegExpParser.swift
+++ string/regexp/RegExpParser.swift
@@ -1,12 +1,20 @@
 import Foundation
 class RegExpParser{
+    /**
+    *
+    */
+    class func match(){
+        
+    }
+    
+    
 	/*
 	 * Returns an Array with the matches 
 	 * EXAMPLE: match("£4$9", "[0-9]", text: string)//Output: [4, 9]
-	 * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+	 * NOTE: NSRegularExpression. (has overview of the regexp syntax supported) https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
 	 * NOTE: you could try this one too: https://www.hackingwithswift.com/example-code/strings/nsregularexpression-how-to-match-regular-expressions-in-strings
      */
-	class func match(text: String!, searchPattern: String!, options:NSRegularExpressionOptions = []) -> [String] {
+	class func brokenMatch(text: String!, searchPattern: String!, options:NSRegularExpressionOptions = []) -> [String] {
 	    do {
 	        let regex = try NSRegularExpression(pattern: searchPattern, options: options)
 	        let nsString = text as NSString
