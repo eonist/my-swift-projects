58f61e6dd3655e5efa2d38edf8be69134d792112
Eonist <unicornhive@gmail.com>
2015-10-15 09:37:26 +0200

    Files modified: 1
    Modified 1 file:research/types/array/Looping.swift

--- research/types/array/Looping.swift
+++ research/types/array/Looping.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+/**
+*
+*/
+func testing(){
+    let array = ["Apples", "Peaches", "Plums"]
+    
+    for (index, item) in array.reverse().enumerate() {
+        print("Found \(item) at position \(index)")
+    }
+}
