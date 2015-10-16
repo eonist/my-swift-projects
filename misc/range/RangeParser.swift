602c28bcb148fa796aa75dfa3a02b2b53e2b8a00
Eonist <unicornhive@gmail.com>
2015-10-16 09:36:17 +0200

    Files modified: 2
    Modified 2 files:misc/range/RangeParser.swiftstring/StringParser.swift

--- misc/range/RangeParser.swift
+++ misc/range/RangeParser.swift
@@ -1,3 +1,16 @@
 class RangeParser {
-
+    /**
+     * Returns a Range instance for @param str from @param start to @param end
+     */
+    class func stringRange(str:String,_ start:Int,_ end:Int)->Range<String.Index>{
+        return RangeParser.strRange(str,start,end-start)
+    }
+    /**
+     * Returns a Range instance for @param str from @param i to @param i + @param len
+     */
+    class func strRange(str:String,_ i:Int,_ len:Int)->Range<String.Index>{
+        let startIndex = str.startIndex.advancedBy(i)
+        let endIndex = str.startIndex.advancedBy(i + len)
+        return startIndex...endIndex//longhand -> Range(start: startIndex,end: endIndex)
+    }
 }
