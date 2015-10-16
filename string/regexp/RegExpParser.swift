c606c113cfdf3071a19262db421a0492833fca33
Eonist <unicornhive@gmail.com>
2015-10-16 21:45:27 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpParser.swift

--- string/regexp/RegExpParser.swift
+++ string/regexp/RegExpParser.swift
@@ -1,59 +1,93 @@
 import Foundation
 public class RegExpParser{
-    /*
-     * NOTE: NSRegularExpression. (has overview of the regexp syntax supported) https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
-     * EXAMPLE: match("My name is Taylor Swift","My name is (.*)",NSRegularExpressionOptions.CaseInsensitive)
-     */
-    class func match(input: String!, searchPattern: String!, options:NSRegularExpressionOptions = []) -> [String]{
-        do {
-            let regex = try NSRegularExpression(pattern: searchPattern, options: NSRegularExpressionOptions.CaseInsensitive)
-            let matches = regex.matchesInString(input, options: [], range: NSMakeRange(0, input.characters.count))
-            if let match = matches.first {
-                let range = match.rangeAtIndex(1)
-                if let swiftRange = rangeFromNSRange(range, forString: input) {
-                    let name = input.substringWithRange(swiftRange)
-                    return [name]
-                    print(name)
-                }
-            }
-        } catch {
-            // regex was bad!
-        }
-        return []
-    }
     /**
-     * helper function that you should take: rangeFromNSRange(). Annoyingly, regular expression matches demand Swift strings as input then return NSString and NSRange in their output. This function converts from NSRange to Swift string ranges:
-     */
-    private class func rangeFromNSRange(nsRange: NSRange, forString str: String) -> Range<String.Index>? {
-        let fromUTF16 = str.utf16.startIndex.advancedBy(nsRange.location, limit: str.utf16.endIndex)
-        let toUTF16 = fromUTF16.advancedBy(nsRange.length, limit: str.utf16.endIndex)
-        if let from = String.Index(fromUTF16, within: str),
-            let to = String.Index(toUTF16, within: str) {
-                return from ..< to
+    * EXAMPLE: test("hello world","o.*o")//true
+    * NOTE: for simple implimentations:  str.rangeOfString(pattern, options: .RegularExpressionSearch) != nil
+    */
+    public class func test(str:String,_ pattern:String)->Bool{
+        return str.rangeOfString(pattern, options: .RegularExpressionSearch) != nil
+    }
+    /*
+    * NOTE: NSRegularExpression. (has overview of the regexp syntax supported) https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+    * EXAMPLE: match("My name is Taylor Swift","My name is (.*)",NSRegularExpressionOptions.CaseInsensitive)
+    */
+    public class func match(text: String!, _ regex: String!, _ options: NSRegularExpressionOptions = []) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: options)
+            let nsString = text as NSString
+            let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substringWithRange($0.range)}
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
         }
-        return nil
     }
     
-	/*
-	 * Returns an Array with the matches 
-	 * EXAMPLE: match("£4$9", "[0-9]", text: string)//Output: [4, 9]
-	 * NOTE: NSRegularExpression. (has overview of the regexp syntax supported) https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
-	 * NOTE: you could try this one too: https://www.hackingwithswift.com/example-code/strings/nsregularexpression-how-to-match-regular-expressions-in-strings
-     */
-    
+    /*
+    * TODO: The @param text should be inout
+    * TODO: move to RegExpModifer, or just create RegExpUtils for all?
+    */
+    public class func replace(str:String,_ pattern:String,_ replacement:String,_ options:NSRegularExpressionOptions)->String{
+        do {
+            let stringlength = str.characters.count
+            let regex = try NSRegularExpression(pattern:pattern , options: options)
+            let modString = regex.stringByReplacingMatchesInString(str, options: [], range: NSMakeRange(0, stringlength), withTemplate: replacement)
+            //print(modString)
+            return modString
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return ""
+        }
+    }
     /*
     
-	class func brokenMatch(text: String!, searchPattern: String!, options:NSRegularExpressionOptions = []) -> [String] {
-	    do {
-	        let regex = try NSRegularExpression(pattern: searchPattern, options: options)
-	        let nsString = text as NSString
-	        let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
-	        return results.map { nsString.substringWithRange($0.range)}
-	    } catch let error as NSError {
-	        print("invalid regexp: \(error.localizedDescription)")
-	        return []//return empty array
-	    }
-	}
+    //whats wrong with this:
+    
+    public class func replace(text:String, _ regex: String, _ replacement: String,_ options: NSRegularExpressionOptions = []) -> String {
+    do {
+    let regex = try NSRegularExpression(pattern: regex, options: options)
+    let range: NSRange = NSMakeRange(0, text.characters.count)
+    regex.stringByReplacingMatchesInString(text, options: .Anchored, range: range, withTemplate: replacement)
+    return text
+    } catch let error as NSError {
+    print("invalid regex: \(error.localizedDescription)")
+    return ""
+    }
+    /*
+    let matches = match(str,pattern)
+    let range: NSRange = NSMakeRange(0, str.characters.count)
+    
+    return
+    
+    return ""
+    */
+    }
+    
     
     */
+    /*
+    Research:
+    
+    **Wrappers:**
+    http://benscheirman.com/2014/06/regex-in-swift/
+    
+    https://github.com/glaurent/SwiftRegExp/blob/master/RegExp.swift
+    
+    https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
+    
+    //matches:
+    http://stackoverflow.com/questions/27880650/swift-extract-regex-matches
+    
+    //comprehensive tutorial:
+    http://www.raywenderlich.com/86205/nsregularexpression-swift-tutorial
+    
+    //uses an infix =- as a shortcut to regexp match:
+    https://github.com/kasei/SwiftRegex/blob/master/SwiftRegex/SwiftRegex.swift
+    http://nomothetis.svbtle.com/clean-regular-expressions-using-conversions
+    
+    //Extension: has a replacer method:
+    https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
+    
+    */
+    
 }
