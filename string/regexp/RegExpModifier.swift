51b872da2500427a4534f11e93c431842f791bb3
Eonist <unicornhive@gmail.com>
2015-10-17 09:17:05 +0200

    New files added: 1, Files modified: 3
    Added 1 file:string/regexp/RegExpHelp.swiftModified 3 files:string/regexp/RegExpAsserter.swiftstring/regexp/RegExpModifier.swiftstring/regexp/RegExpParser.swift

--- string/regexp/RegExpModifier.swift
+++ string/regexp/RegExpModifier.swift
@@ -1,36 +1,38 @@
 import Foundation
-class RegExpModifier{
+/*
+ * Flag (Pattern) Description:
+ * i - If set, matching will take place in a case-insensitive manner.
+ * x - If set, allow use of white space and #comments within patterns
+ * s - If set, a "." in a pattern will match a line terminator in the input text. By default, it will not. Note that a carriage-return / line-feed pair in text behave as a single line terminator, and will match a single "." in a regular expression pattern
+ * m - Control the behavior of "^" and "$" in a pattern. By default these will only match at the start and end, respectively, of the input text. If this flag is set, "^" and "$" will also match at the start and end of each line within the input text.
+ * Para - Controls the behavior of \b in a pattern. If set, word boundaries are found according to the definitions of word found in Unicode UAX 29, Text Boundaries. By default, word boundaries are identified by means of a simple classification of characters as either “word” or “non-word”, which approximates traditional regular expression behavior. The results obtained with the two options can be quite different in runs of spaces and other non-word characters.
+ */
+
+public class RegExpModifier{
 	/*
 	 * Replaces all matches with the replacment string
-	 * Note: you can use this call replaceMatchesInString to modify the original string, must use nsmurablestring to do this
-	 string The string to search for values within.
-	options The matching options to use. See NSMatchingOptions for possible values.
-	range	The range of the string to search.
-	template	The substitution template used when replacing matching instances.
-	Return Value A string with matching regular expressions replaced by the template string.
-		EXAMPLE: RegExpModifier.replace("<strong>Hell</strong>o, <strong>Hell</strong>o, <strong>Hell</strong>o", "<\\/?strong>",  "*")//Output:  "*Hell*o, *Hell*o, *Hell*o"
-	 * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
-	 *  
-    */
-	class func replace(string: String, searchPattern: String,replacement:String,options:NSMatchingOptions)-> String{
+	 * NOTE: you can use this call replaceMatchesInString to modify the original string, must use nsmutablestring to do this
+	 * @param string The string to search for values within.
+	 * @param options: The matching options to use. See NSMatchingOptions for possible values.
+	 * @param range: The range of the string to search.
+	 * @param replacement: The substitution template used when replacing matching instances.
+     * Returns Value A string with matching regular expressions replaced by the template string.
+	 * EXAMPLE: RegExpModifier.replace("<strong>Hell</strong>o, <strong>Hell</strong>o, <strong>Hell</strong>o", "<\\/?strong>",  "*")//Output:  "*Hell*o, *Hell*o, *Hell*o"
+     * EXAMPLE: RegExpModifier.replace("yeah yeah","(\\b\\w+\\b)", "bla")//bla bla
+     * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+     * TODO: The @param text should be inout
+     * TODO: move to RegExpModifer, or just create RegExpUtils for all?
+     */
+    public class func replace(str:String,_ pattern:String,_ replacement:String,_ options:NSRegularExpressionOptions = NSRegularExpressionOptions.CaseInsensitive)->String{
         do {
-            let expr = try NSRegularExpression(pattern: searchPattern, options: NSRegularExpressionOptions.CaseInsensitive)
-            //let len:Int =
-            let replacement = expr.stringByReplacingMatchesInString(string, options: options, range: NSMakeRange(0, string.characters.count), withTemplate: replacement)
-            return replacement
-        }catch let error as NSError {
-            print("invalid regexp: \(error.localizedDescription)")
-            return ""//return an empty string
+            let stringlength = str.characters.count
+            let regex = try NSRegularExpression(pattern:pattern , options: options)
+            let modString = regex.stringByReplacingMatchesInString(str, options: [], range: NSMakeRange(0, stringlength), withTemplate: replacement)
+            //print(modString)
+            return modString
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return ""
         }
-	}
-	/*
-	Flag (Pattern) Description:
-	i - If set, matching will take place in a case-insensitive manner.
-	x - If set, allow use of white space and #comments within patterns
-	s - If set, a "." in a pattern will match a line terminator in the input text. By default, it will not. Note that a carriage-return / line-feed pair in text behave as a single line terminator, and will match a single "." in a regular expression pattern
-	m - Control the behavior of "^" and "$" in a pattern. By default these will only match at the start and end, respectively, of the input text. If this flag is set, "^" and "$" will also match at the start and end of each line within the input text.
-	Para - Controls the behavior of \b in a pattern. If set, word boundaries are found according to the definitions of word found in Unicode UAX 29, Text Boundaries. By default, word boundaries are identified by means of a simple classification of characters as either “word” or “non-word”, which approximates traditional regular expression behavior. The results obtained with the two options can be quite different in runs of spaces and other non-word characters.
-	*/
-	
-	//string: String,options options: NSMatchingOptions,range  withTemplate templ: String
+    }
 }
--- string/regexp/RegExpParser.swift
+++ string/regexp/RegExpParser.swift
@@ -1,93 +1,55 @@
 import Foundation
+
 public class RegExpParser{
-    /**
-     * EXAMPLE: test("hello world","o.*o")//true
-     * NOTE: for simple implimentations:  str.rangeOfString(pattern, options: .RegularExpressionSearch) != nil
-     */
-    public class func test(str:String,_ pattern:String)->Bool{
-        return str.rangeOfString(pattern, options: .RegularExpressionSearch) != nil
-    }
     /*
      * NOTE: NSRegularExpression. (has overview of the regexp syntax supported) https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
-     * EXAMPLE: match("My name is Taylor Swift","My name is (.*)",NSRegularExpressionOptions.CaseInsensitive)
+     * EXAMPLE: match("My name is Taylor Swift","My name is (.*)")//Swift
+     * EXAMPLE: RegExpParser.match("hello world","(\\b\\w+\\b)")//hello, world
      */
-    public class func match(text: String!, _ regex: String!, _ options: NSRegularExpressionOptions = []) -> [String] {
+    public class func match(text: String!, _ regex: String!, _ options: NSRegularExpressionOptions = NSRegularExpressionOptions.CaseInsensitive) -> [String] {
+        return matches(text, regex).map { (text as NSString).substringWithRange($0.range)}
+    }
+    /**
+     * NOTE: Use this method when doing named capturing group
+     */
+    public class func matches(text: String!, _ regex: String!, _ options: NSRegularExpressionOptions = NSRegularExpressionOptions.CaseInsensitive) -> [NSTextCheckingResult] {
         do {
             let regex = try NSRegularExpression(pattern: regex, options: options)
             let nsString = text as NSString
             let results = regex.matchesInString(text,options: [], range: NSMakeRange(0, nsString.length))
-            return results.map { nsString.substringWithRange($0.range)}
+            return results
         } catch let error as NSError {
             print("invalid regex: \(error.localizedDescription)")
             return []
         }
     }
-    /*
-     * TODO: The @param text should be inout
-     * TODO: move to RegExpModifer, or just create RegExpUtils for all?
-     * NOTE: NSRegularExpressionOptions.DotMatchesLineSeparators
-     */
-    public class func replace(str:String,_ pattern:String,_ replacement:String,_ options:NSRegularExpressionOptions)->String{
-        do {
-            let stringlength = str.characters.count
-            let regex = try NSRegularExpression(pattern:pattern , options: options)
-            let modString = regex.stringByReplacingMatchesInString(str, options: [], range: NSMakeRange(0, stringlength), withTemplate: replacement)
-            //print(modString)
-            return modString
-        } catch let error as NSError {
-            print("invalid regex: \(error.localizedDescription)")
-            return ""
-        }
-    }
-    /*
-    
-    //whats wrong with this:
-    
-    public class func replace(text:String, _ regex: String, _ replacement: String,_ options: NSRegularExpressionOptions = []) -> String {
-    do {
-    let regex = try NSRegularExpression(pattern: regex, options: options)
-    let range: NSRange = NSMakeRange(0, text.characters.count)
-    regex.stringByReplacingMatchesInString(text, options: .Anchored, range: range, withTemplate: replacement)
-    return text
-    } catch let error as NSError {
-    print("invalid regex: \(error.localizedDescription)")
-    return ""
-    }
-    /*
-    let matches = match(str,pattern)
-    let range: NSRange = NSMakeRange(0, str.characters.count)
-    
-    return
-    
-    return ""
-    */
-    }
-    
-    
-    */
-    /*
-    Research:
-    
-    **Wrappers:**
-    http://benscheirman.com/2014/06/regex-in-swift/
-    
-    https://github.com/glaurent/SwiftRegExp/blob/master/RegExp.swift
-    
-    https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
-    
-    //matches:
-    http://stackoverflow.com/questions/27880650/swift-extract-regex-matches
-    
-    //comprehensive tutorial:
-    http://www.raywenderlich.com/86205/nsregularexpression-swift-tutorial
-    
-    //uses an infix =- as a shortcut to regexp match:
-    https://github.com/kasei/SwiftRegex/blob/master/SwiftRegex/SwiftRegex.swift
-    http://nomothetis.svbtle.com/clean-regular-expressions-using-conversions
-    
-    //Extension: has a replacer method:
-    https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
-    
-    */
-    
 }
+/*
+Research:
+
+**Wrappers:**
+http://benscheirman.com/2014/06/regex-in-swift/
+
+https://github.com/glaurent/SwiftRegExp/blob/master/RegExp.swift
+
+https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
+
+//matches:
+http://stackoverflow.com/questions/27880650/swift-extract-regex-matches
+
+//comprehensive tutorial:
+http://www.raywenderlich.com/86205/nsregularexpression-swift-tutorial
+
+//uses an infix =- as a shortcut to regexp match:
+https://github.com/kasei/SwiftRegex/blob/master/SwiftRegex/SwiftRegex.swift
+http://nomothetis.svbtle.com/clean-regular-expressions-using-conversions
+
+//Extension: has a replacer method:
+https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
+
+//find individual groups:
+http://stackoverflow.com/questions/9601551/extract-parts-from-regular-expression-with-nsregularexpression?rq=1
+
+//named capturing groups (sort of)
+http://stackoverflow.com/questions/24814974/named-capture-groups-with-nsregularexpression?lq=1
+*/
