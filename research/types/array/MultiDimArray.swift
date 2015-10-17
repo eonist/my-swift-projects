643ba29327753997e1d8d3161eaf06d2205f9232
Eonist <unicornhive@gmail.com>
2015-10-17 09:40:10 +0200

    Files modified: 1
    Modified 1 file:research/types/array/MultiDimArray.swift

--- research/types/array/MultiDimArray.swift
+++ research/types/array/MultiDimArray.swift
@@ -4,4 +4,10 @@ func testing(){
     let array3D: [[[Int]]] = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
     print(array3D.count)
     
+    //Arrays with dictionaries (method 1):
+    var taskItems:[Dictionary<String,String>] = []
+    taskItems.append(["color":"blue"])
+    //Arrays with dictionaries (method 2):
+    var taskItems2 = [String:String]()
+    taskItems2.append(["color":"blue"])
 }
