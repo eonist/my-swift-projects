8b974c7ff68ffccabfa4158e9bd9dca39b451a28
Eonist <unicornhive@gmail.com>
2015-10-19 11:46:09 +0200

    Files modified: 1
    Modified 1 file:research/types/Objects.swift

--- research/types/Objects.swift
+++ research/types/Objects.swift
@@ -1,1 +1,19 @@
 import Foundation
+
+class Month {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func shortened() -> String {
+        return name[name.startIndex..<advanceBy(name.startIndex, 3)]
+    }
+}
+/**
+*
+*/
+func test(){
+    print(Month(name: "January").shortened())    // Jan
+}
