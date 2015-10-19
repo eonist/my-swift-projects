a51395c13ae920b1bdb6dc4132da97e1f4cfa5d8
Eonist <unicornhive@gmail.com>
2015-10-19 12:42:37 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpUtils.swift

--- string/regexp/RegExpUtils.swift
+++ string/regexp/RegExpUtils.swift
@@ -5,7 +5,7 @@ class RegExpUtils {
     *
     */
     class func value(str:String, _ result:NSTextCheckingResult, _ type:Any)->String{
-        (str as NSString).substringWithRange(result.rangeAtIndex(type.rawValue))
+        (str as NSString).substringWithRange(result.rangeAtIndex(enum(type).rawValue))
         return ""
     }
 }
