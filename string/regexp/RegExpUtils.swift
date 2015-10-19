651e4a12d6f93701dd8f9e3436f923cf1c3fc200
Eonist <unicornhive@gmail.com>
2015-10-19 12:46:31 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpUtils.swift

--- string/regexp/RegExpUtils.swift
+++ string/regexp/RegExpUtils.swift
@@ -2,11 +2,10 @@ import Foundation
 
 class RegExpUtils {
     /**
-    *
-    */
+     * helper method for extracting associate capture groups
+     * TODO: Would be greate if .rawValue was done inside this method
+     */
     class func value(str:String, _ result:NSTextCheckingResult, _ key:Int)->String{
-        
         return (str as NSString).substringWithRange(result.rangeAtIndex(key))
-        
     }
 }
