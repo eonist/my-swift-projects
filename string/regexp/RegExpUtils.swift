7412218d9ef5901bda59c57d4a4560fc3be5f108
Eonist <unicornhive@gmail.com>
2015-10-19 12:36:40 +0200

    Files modified: 1
    Modified 1 file:string/regexp/RegExpUtils.swift

--- string/regexp/RegExpUtils.swift
+++ string/regexp/RegExpUtils.swift
@@ -1,1 +1,11 @@
 import Foundation
+
+class RegExpUtils {
+    /**
+    *
+    */
+    class func value(str:String, _ result:NSTextCheckingResult, _ `enum`:enum)->String{
+        
+        return ""
+    }
+}
