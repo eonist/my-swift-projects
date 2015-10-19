be542473d27ab9db4be96abc1c8f47022268886b
Eonist <unicornhive@gmail.com>
2015-10-19 10:37:53 +0200

    Files modified: 1
    Modified 1 file:research/types/Characters.swift

--- research/types/Characters.swift
+++ research/types/Characters.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+
+func test(){
+    
+    //replace chars in a string withouth regexp etc
+    
+    var aString = "Replace the letter e with *"
+    
+    var replacedString = ""
+    
+    for scalar in aString.unicodeScalars {
+        let char = "\(scalar)"
+        if char == "e" {
+            replacedString = replacedString + "*"
+        } else {
+            replacedString = replacedString + char
+        }
+    }
+}
