ad8d0d4fc10d63dce0be048192fb3c03f7c331cb
Eonist <unicornhive@gmail.com>
2015-10-22 17:31:43 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSButtonTest.swift

--- research/types/ns/NSButtonTest.swift
+++ research/types/ns/NSButtonTest.swift
@@ -1,6 +1,6 @@
 //https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/Button/Button.html#//apple_ref/doc/uid/10000019i
 https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/Button/Button.html
-
+//changing the osx close minimize and max buttons: http://fredandrandall.com/blog/2011/09/22/how-to-make-your-app-window-look-like-the-mac-app-store-twitter-and-chrome/
 /**
 
 you set pos:
@@ -8,6 +8,15 @@ you set pos:
 
 you set size: 
 (buttons[0] as NSButton).setFrameSize(newSize)
+
+
+removing a button:
+removeFromSuperView
+
+to get button size:
+
+//Swift.print("button.frame.width: " + "\(button.frame.width)")
+        //Swift.print("button.frame.height: " + "\(button.frame.height)")
 */
 import Cocoa
 
