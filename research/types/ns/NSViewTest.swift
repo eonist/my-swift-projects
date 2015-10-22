daaf3906d519058eb679649dcafd69356717d75c
Eonist <unicornhive@gmail.com>
2015-10-22 17:39:15 +0200

    Files modified: 1
    Modified 1 file:research/types/ns/NSViewTest.swift

--- research/types/ns/NSViewTest.swift
+++ research/types/ns/NSViewTest.swift
@@ -1,2 +1,15 @@
 //great overview: https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/CocoaViewsGuide/Introduction/Introduction.html
 //translucent window: http://stackoverflow.com/questions/24023183/how-can-i-create-yosemite-style-view-with-translucent-blurry-background
+
+/*
+Sizes:
+ 
+        Swift.print("NSScreen.mainScreen()!.frame.width: " + "\(NSScreen.mainScreen()!.frame.width)")
+        Swift.print("NSScreen.mainScreen()!.frame.height: " + "\(NSScreen.mainScreen()!.frame.height)")
+        
+        Swift.print("self.frame.size.width: " + "\(self.frame.size.width)")
+        Swift.print("self.frame.size.height : " + "\(self.frame.size.height)")//returns the height - the topBar height
+        Swift.print("view.frame.size.width: " + "\(view.frame.size.width)")
+        Swift.print("view.frame.size.height: " + "\(view.frame.size.height)")
+
+*/
