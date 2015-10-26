b9bd54eea5b78559bec20f15adcc4ad9a2ced8a6
Eonist <unicornhive@gmail.com>
2015-10-26 21:30:51 +0100

    Files modified: 1
    Modified 1 file:geom/GraphicStyle.swift

--- geom/GraphicStyle.swift
+++ geom/GraphicStyle.swift
@@ -1,1 +1,32 @@
 import Foundation
+
+public class GraphicStyle {
+    //var fill:IGraphicsFill;
+    //var stroke:IGraphicsStroke;
+    init(_ fill:IGraphicsFill,_ stroke:IGraphicsStroke) {
+        self.fill = fill;
+        self.stroke = stroke;
+    }
+    public var fill: IGraphicsFill {
+        get {
+            return self.fill
+        }
+        set {
+            
+        }
+    }
+    public var stroke: IGraphicsStroke {
+        get {
+            return self.stroke
+        }
+        set {
+            self.stroke = newValue
+        }
+    }
+}
+public class IGraphicsFill {
+    
+}
+public class IGraphicsStroke {
+    
+}
