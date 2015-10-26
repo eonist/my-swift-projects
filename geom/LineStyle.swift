ee8637171cad0b52990cda820793fd0e2a87b81f
Eonist <unicornhive@gmail.com>
2015-10-26 21:45:46 +0100

    Files modified: 1
    Modified 1 file:geom/LineStyle.swift

--- geom/LineStyle.swift
+++ geom/LineStyle.swift
@@ -1,1 +1,29 @@
 import Foundation
+
+
+
+//add borderWidth
+
+
+/*
+
+private var _color:Number;
+private var _alpha:Number;
+public function FillStyle(color:Number = NaN, alpha:Number = 1) {
+_color = color;
+_alpha = !isNaN(alpha) ? alpha:1;/*if alpha is NaN then assign 1*/
+}
+public function get alpha():Number {
+return _alpha;
+}
+public function get color():Number {
+return _color;
+}
+public function set color(color:Number):void{
+_color = color;
+}
+public function set alpha(alpha:Number):void{
+_alpha = alpha;
+}
+
+*/
