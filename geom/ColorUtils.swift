f12739d6cd6a1e1a863d662bca10d17da2ce8def
Eonist <unicornhive@gmail.com>
2015-10-27 17:15:44 +0100

    Files modified: 1
    Modified 1 file:geom/ColorUtils.swift

--- geom/ColorUtils.swift
+++ geom/ColorUtils.swift
@@ -1,10 +1,5 @@
 import Cocoa
 
 class ColorUtils {
-    /**
-    * untested
-    */
-    class func  nsColor(r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat)->NSColor{
-       return NSColor.init(calibratedRed: r/255, green: g/255, blue: b/255, alpha: a)
-    }
+
 }
