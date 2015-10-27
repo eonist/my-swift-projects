0189b3a06858dcc4735b59111a22a651058ed8f3
Eonist <unicornhive@gmail.com>
2015-10-27 12:39:40 +0100

    Files modified: 1
    Modified 1 file:geom/ColorUtils.swift

--- geom/ColorUtils.swift
+++ geom/ColorUtils.swift
@@ -1,14 +1,10 @@
 import Cocoa
 
 class ColorUtils {
-   
     /**
-    *
+    * untested
     */
-    class func  nsColor(r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat){
-        let rgba = NSColor.init(calibratedRed: r/255, green: g/255, blue: b/255, alpha: <#T##CGFloat#>)
-        
-        #define NSColorFromRGB(rgbValue) [NSColor colorWithCalibratedRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+    class func  nsColor(r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat)->NSColor{
+       return NSColor.init(calibratedRed: r/255, green: g/255, blue: b/255, alpha: <#T##CGFloat#>)
     }
-    
 }
