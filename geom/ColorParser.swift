374cf18e8a72b40dd2c8472e81a520d8df2af350
Eonist <unicornhive@gmail.com>
2015-10-27 17:13:44 +0100

    Files modified: 1
    Modified 1 file:geom/ColorParser.swift

--- geom/ColorParser.swift
+++ geom/ColorParser.swift
@@ -42,5 +42,10 @@ class ColorParser {
             return NSColor()
         }
     }
-
+    /**
+    * untested
+    */
+    class func nsColor(r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat)->NSColor{
+        return NSColor.init(calibratedRed: r/255, green: g/255, blue: b/255, alpha: a)
+    }
 }
