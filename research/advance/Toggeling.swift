bc65ae859ef65aeef831d11b9e12f8e897c9c116
Eonist <unicornhive@gmail.com>
2015-10-27 09:54:08 +0100

    New files added: 1
    Added 1 file:research/advance/

---
+++ research/advance/Toggeling.swift
@@ -0,0 +1,17 @@
+//toggeling between osx and ios:
+
+
+/*
+#if os(iOS)
+    import UIKit
+    typealias SWColor = UIColor
+    #else
+    import Cocoa
+    typealias SWColor = NSColor
+#endif
+
+
+see more here: https://github.com/thii/SwiftColors/blob/master/SwiftColors/SwiftColors.swift
+
+
+*/
