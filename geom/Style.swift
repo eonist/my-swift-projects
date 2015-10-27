a80fe3748f6defda07295a4dd5abffa774bc796d
Eonist <unicornhive@gmail.com>
2015-10-27 15:32:33 +0100

    Files modified: 1
    Modified 1 file:geom/Style.swift

--- geom/Style.swift
+++ geom/Style.swift
@@ -3,9 +3,9 @@ protocol IStyle {
     var fill: IFill { get set }
     var stroke: IStroke { get set }
 }
-class Style:IStyle {
-    static var clear:IStyle = Style(Fill.clear,Stroke.clear)
-    static var green:IStyle = Style(Fill(NSColor.greenColor()),Stroke.clear)
+class GraphicStyle:IStyle {
+    static var clear:IStyle = GraphicStyle(Fill.clear,Stroke.clear)
+    static var green:IStyle = GraphicStyle(Fill(NSColor.greenColor()),Stroke.clear)
     var fill: IFill //{ get {return self.fill} set {self.fill = newValue} }
     var stroke: IStroke //{ get {return self.stroke } set {self.stroke = newValue}}
     init(_ fill:IFill = Fill.clear,_ stroke:IStroke = Stroke.clear) {
