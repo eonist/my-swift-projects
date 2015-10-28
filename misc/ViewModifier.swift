926edd1dd05247165cb12b71c0f3e9c971000e96
Eonist <unicornhive@gmail.com>
2015-10-28 08:16:00 +0100

    Files modified: 1
    Modified 1 file:misc/ViewModifier.swift

--- misc/ViewModifier.swift
+++ misc/ViewModifier.swift
@@ -1,1 +1,22 @@
-import Foundation
+import Cocoa
+
+class ViewModifier {
+    /**
+     *
+     */
+    class func applyColor(view:NSView,_ nsFillColor:NSColor = NSColor.clearColor(),_ nsLineColor:NSColor = NSColor.clearColor(),_ lineWidth:Int = 0){
+        let cgFillColor = NSColorParser.cgColor(nsFillColor)
+        if(nsFillColor != NSColor.clearColor()){/*clearColor: 0.0 white, 0.0 alpha */
+            //Swift.print("fill")
+            view.layer!.backgroundColor = cgFillColor
+            
+        }
+        let cgLineColor = NSColorParser.cgColor(nsLineColor)
+        //Swift.print(nsLineColor)
+        if(nsLineColor != NSColor.clearColor()){/*clearColor: 0.0 white, 0.0 alpha */
+            //Swift.print("line")
+            view.layer!.borderColor = cgLineColor
+            view.layer!.borderWidth = CGFloat(lineWidth)
+        }
+    }
+}
