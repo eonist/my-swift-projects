433281c0ca31d48326e16e02b2fdd9416995ab85
Eonist <unicornhive@gmail.com>
2015-10-28 14:19:24 +0100

    Files modified: 1
    Modified 1 file:research/types/Protocol.swift

---
+++ geom/utils/GraphicsModifier.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class GraphicsModifier{
+    /**
+    *
+    */
+    /*
+    class func drawRect(graphics:NSView,rectangle:Rectangle) {
+        graphics.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    }
+*/
+    /**
+    *
+    */
+    /*
+    class func drawRoundRect(graphics:Graphics,rectangle:Rectangle,ellipseWidth:Number,ellipseHeight:Number) {
+        graphics.drawRoundRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height, ellipseWidth, ellipseHeight);
+    }
+*/
+}
--- research/types/Protocol.swift
+++ research/types/Protocol.swift
@@ -1,4 +1,5 @@
 //more on protocol conformance: https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html#//apple_ref/doc/uid/TP40014097-CH25-ID283
+//some cool examples: http://en.swifter.tips/protocol-composition/
 /**
 Checking for Protocol Conformance
 
