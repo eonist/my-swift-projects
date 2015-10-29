1d27e0508cfe3b13a2dd2ee1b29ce10ba5dcaac3
Eonist <unicornhive@gmail.com>
2015-10-29 08:29:12 +0100

    Files modified: 1
    Modified 1 file:geom/path/utils/CGPathParser.swift

--- geom/path/utils/CGPathParser.swift
+++ geom/path/utils/CGPathParser.swift
@@ -1,1 +1,63 @@
 import Foundation
+
+import AppKit
+
+class CGPathParser{
+    /**
+    * Untested
+    */
+    class func nsBezierPath(path:CGPath)->NSBezierPath{
+        let nsBezierPath:NSBezierPath = NSBezierPath()
+        let pathPtr = UnsafeMutablePointer<NSBezierPath>.alloc(1)
+        pathPtr.initialize(nsBezierPath)
+        
+        let infoPtr = UnsafeMutablePointer<Void>(pathPtr)
+        
+        // I hope the CGPathApply call manages the deallocation of the pointers passed to the applier
+        // function, but I'm not sure.
+        CGPathApply(path, infoPtr) { (infoPtr, elementPtr) -> Void in
+            let path = UnsafeMutablePointer<NSBezierPath>(infoPtr).memory
+            let element = elementPtr.memory
+            
+            let pointsPtr = element.points
+            
+            switch element.type {
+            case .MoveToPoint:
+                path.moveToPoint(pointsPtr.memory)
+                
+            case .AddLineToPoint:
+                path.lineToPoint(pointsPtr.memory)
+                
+            case .AddQuadCurveToPoint:
+                let firstPoint = pointsPtr.memory
+                let secondPoint = pointsPtr.successor().memory
+                
+                let currentPoint = path.currentPoint
+                let x = (currentPoint.x + 2 * firstPoint.x) / 3
+                let y = (currentPoint.y + 2 * firstPoint.y) / 3
+                let interpolatedPoint = CGPoint(x: x, y: y)
+                
+                let endPoint = secondPoint
+                
+                path.curveToPoint(endPoint, controlPoint1: interpolatedPoint, controlPoint2: interpolatedPoint)
+                
+            case .AddCurveToPoint:
+                let firstPoint = pointsPtr.memory
+                let secondPoint = pointsPtr.successor().memory
+                let thirdPoint = pointsPtr.successor().successor().memory
+                
+                path.curveToPoint(thirdPoint, controlPoint1: firstPoint, controlPoint2: secondPoint)
+                
+            case .CloseSubpath:
+                path.closePath()
+            }
+            
+            pointsPtr.destroy()
+        }
+    }
+    
+}
+
+
+
+// TODO: Add conversion to CGPath
