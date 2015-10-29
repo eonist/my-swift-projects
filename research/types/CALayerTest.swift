d8292179aa0da393f0112a48a5078d2a7ed7d126
Eonist <unicornhive@gmail.com>
2015-10-29 06:41:56 +0100

    New files added: 1
    Added 1 file:research/types/CALayerTest.swift

---
+++ research/types/CALayerTest.swift
@@ -0,0 +1,22 @@
+/*
+Draw an image in an calayer:
+
+CALayer *sublayer = [CALayer layer];
+sublayer.backgroundColor = [UIColor blueColor].CGColor;
+sublayer.shadowOffset = CGSizeMake(0, 3);
+sublayer.shadowRadius = 5.0;
+sublayer.shadowColor = [UIColor blackColor].CGColor;
+sublayer.shadowOpacity = 0.8;
+sublayer.frame = CGRectMake(30, 30, 128, 192);
+sublayer.borderColor = [UIColor blackColor].CGColor;
+sublayer.borderWidth = 2.0;
+sublayer.cornerRadius = 10.0;
+[self.view.layer addSublayer:sublayer];
+ 
+CALayer *imageLayer = [CALayer layer];
+imageLayer.frame = sublayer.bounds;
+imageLayer.cornerRadius = 10.0;
+imageLayer.contents = (id) [UIImage imageNamed:@"BattleMapSplashScreen.jpg"].CGImage;
+imageLayer.masksToBounds = YES;
+[sublayer addSublayer:imageLayer];
+*/
