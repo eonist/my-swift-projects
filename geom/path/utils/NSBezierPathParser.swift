eaf5856771f81e57e53c551bdaaa6650417a0329
Eonist <unicornhive@gmail.com>
2015-10-29 08:33:10 +0100

    Files modified: 1
    Modified 1 file:geom/path/utils/NSBezierPathParser.swift

--- geom/path/utils/NSBezierPathParser.swift
+++ geom/path/utils/NSBezierPathParser.swift
@@ -4,7 +4,42 @@ class NSBezierPathParser {
     /**
     *
     */
-    class func cgPath(nsBezierPath:NSBezierPath)->CGPath{
+    class func cgPath(nsBezierPath:NSBezierPath)->CGPathRef{
         
+        // Create path
+        var path = CGPathCreateMutable()
+        var points = UnsafeMutablePointer<NSPoint>.alloc(3)
+        let numElements = nsBezierPath.elementCount
+        
+        if numElements > 0 {
+            
+            var didClosePath = true
+            
+            for index in 0..<numElements {
+                
+                let pathType = self.elementAtIndex(index, associatedPoints: points)
+                
+                switch pathType {
+                    
+                case .MoveToBezierPathElement:
+                    CGPathMoveToPoint(path, nil, points[0].x, points[0].y)
+                case .LineToBezierPathElement:
+                    CGPathAddLineToPoint(path, nil, points[0].x, points[0].y)
+                    didClosePath = false
+                case .CurveToBezierPathElement:
+                    CGPathAddCurveToPoint(path, nil, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y)
+                    didClosePath = false
+                case .ClosePathBezierPathElement:
+                    CGPathCloseSubpath(path)
+                    didClosePath = true
+                }
+            }
+            
+            if !didClosePath { CGPathCloseSubpath(path) }
+        }
+        
+        points.dealloc(3)
+        return path
+
     }
 }
