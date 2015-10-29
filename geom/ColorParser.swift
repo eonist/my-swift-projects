75ba1d9e8c7180f0e6b6a53fc70da076ed0975a1
Eonist <unicornhive@gmail.com>
2015-10-29 09:47:40 +0100

    Files modified: 1
    Modified 1 file:geom/ColorParser.swift

--- geom/ColorParser.swift
+++ geom/ColorParser.swift
@@ -1,5 +1,11 @@
 import Cocoa
 class ColorParser {
+    /**
+    * r: 0.0 - 255.0
+    */
+    class func nsColor(r:Int,g:Int,b:Int,a:Int)->NSColor{
+        return NSColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(a) / 100.0)
+    }
     /*
     * Returns NSColor for hex int
     * TODO: use UINT?
