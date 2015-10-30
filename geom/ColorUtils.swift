0efae86bb0745c5f8bb19c4f33a1ff9ca7f06dd8
Eonist <unicornhive@gmail.com>
2015-10-30 08:41:02 +0100

    Files modified: 1
    Modified 1 file:geom/ColorUtils.swift

--- geom/ColorUtils.swift
+++ geom/ColorUtils.swift
@@ -1,5 +1,15 @@
 import Cocoa
 
 class ColorUtils {
-
+    
+    /**
+    *
+    */
+    func randomColor()-> NSColor{
+        let r:CGFloat  = CGFloat(rand() % 255) / 255.0;
+        let g:CGFloat  = CGFloat(rand() % 255) / 255.0;
+        let b:CGFloat  = CGFloat(rand() % 255) / 255.0;
+        let nsColor:NSColor = NSColor(calibratedRed: r, green: g, blue: b, alpha: 1)
+        return nsColor
+    }
 }
