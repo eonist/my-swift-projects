e23e1c7e0771cee094cd792e749195aa6e8efccf
Eonist <unicornhive@gmail.com>
2015-10-31 13:49:21 +0100

    Files modified: 1
    Modified 1 file:geom/cg/context/CGContextModifier.swift

--- geom/cg/context/CGContextModifier.swift
+++ geom/cg/context/CGContextModifier.swift
@@ -1,10 +1,11 @@
+import Cocoa
 class CGContextModifier{
     /**
     *  
     */
-   class func addStar(context:CGContextRef,_ position:CGPoint, _ radius:CGFloat, _ angle:CGFloat){
-      CGFloat x = radius * sinf(angle * M_PI / 5.0) + center.x
-		CGFloat y = radius * cosf(angle * M_PI / 5.0) + center.y
+   class func addStar(context:CGContextRef,_ center:CGPoint, _ radius:CGFloat, _ angle:CGFloat){
+        let x:CGFloat  = radius * sinf(angle * M_PI / 5.0) + center.x
+        let y:CGFloat  = radius * cosf(angle * M_PI / 5.0) + center.y
 		CGContextMoveToPoint(context, x, y)
 		for(var i:Int = 1; i < 5; ++i){
 			let x:CGFloat = radius * sinf((i * 4.0 * M_PI + angle) / 5.0) + center.x;
