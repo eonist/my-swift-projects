4958bec09071bbd3d448103fbc49eaffb381b6b0
Eonist <unicornhive@gmail.com>
2015-11-05 17:29:29 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -1,6 +1,6 @@
 import Cocoa
 public enum GradientType:Int{
-    case Axial = 0, Radial
+    case Axial = 0, Radial//rename Axial to linear
 }
 public class Gradient{
     public var colors:Array<CGColor>
@@ -8,7 +8,7 @@ public class Gradient{
     public var gradientType:GradientType
     public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial){/*,startPoint:CGPoint = CGPoint(),endPoint:CGPoint = CGPoint()*/
         self.colors = colors
-        if locations.count == 0 {
+        if locations.count == 0 {//add support for nil aswell
             Swift.print(colors.count)
             self.locations = CGFloatParser.interpolatedRange(0,  1,  colors.count)
             Swift.print(self.locations)
