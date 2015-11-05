89c5612c52859d7d7dcf9631244768838e8d460d
Eonist <unicornhive@gmail.com>
2015-11-05 16:09:39 +0100

    Files modified: 1
    Modified 1 file:misc/view/FlippedView.swift

--- misc/view/FlippedView.swift
+++ misc/view/FlippedView.swift
@@ -1,9 +1,8 @@
-//
-//  FlippedView.swift
-//  GitSync
-//
-//  Created by Kaleb Skjong on 05/11/15.
-//  Copyright © 2015 beyondnorth. All rights reserved.
-//
-
-import Foundation
+import Cocoa
+class FlippedView:NSView {//Organizes your view from top to bottom
+    override var flipped:Bool {
+        get {
+            return true
+        }
+    }
+}
