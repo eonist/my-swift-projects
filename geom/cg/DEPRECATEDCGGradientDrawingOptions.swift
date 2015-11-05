192b63e2e1b6425e346515707b7e04c0f18eab39
Eonist <unicornhive@gmail.com>
2015-11-05 13:17:19 +0100

    New files added: 5, Files modified: 1, Files deleted: 8
    Added 5 files:geom/context/DEPRECATEDCGContextModifier.txtgeom/style/misc/view/misc/window/string/range/Deleted 8 files:geom/CGContextModifier.swiftgeom/Fill.swiftgeom/GraphicStyle.swiftgeom/Stroke.swiftgeom/cg/DEPRECATEDCGGradientDrawingOptions.swiftmisc/ViewModifier.swiftmisc/WindowHelp.swiftstring/StringRangeParser.swiftModified 1 file:misc/.DS_Store

--- geom/cg/DEPRECATEDCGGradientDrawingOptions.swift
+++
@@ -1,8 +0,0 @@
-import Cocoa
-//CGGradientDrawingOptions
-//this doesnt work, fix it later
-class CGGradientOptions{
-	 static var defaultValue = 0
-     static var extendsPastStart = 1//CGGradientDrawingOptions.kCGGradientDrawsBeforeStartLocation
-	 static var extendsPastEnd = 2//CGGradientDrawingOptions.kCGGradientDrawsAfterEndLocation
-}
