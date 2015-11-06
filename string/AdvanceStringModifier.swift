016e88887506d8b496ae1ed3ff57eaea58fcb49a
Eonist <unicornhive@gmail.com>
2015-11-06 16:44:33 +0100

    Files modified: 1
    Modified 1 file:string/AdvanceStringModifier.swift

--- string/AdvanceStringModifier.swift
+++ string/AdvanceStringModifier.swift
@@ -3,12 +3,10 @@ class AdvanceStringModifier{
      * @param input: a Css String such as "P{color:#00FF00;}"
      * @return a Comment free css string
      */
-    class func removeComments(string:String):String {
-        var pattern:String = "\\/\\*.*?\\*\\/";
-        RegExpModifier.replace(string,pattern, "bla")//bla bla
-        var regExpPattern:RegExp = new RegExp(pattern, "g");
-        var output:String = string.replace(regExpPattern, "");
-        //			trace(input.match(regExpPattern));
+    class func removeComments(string:String)->String {
+        let pattern:String = "\\/\\*.*?\\*\\/";
+        let output:String = RegExpModifier.replace(string,pattern, "")
+        //print(input.match(regExpPattern));
         return output;
     }
 }
