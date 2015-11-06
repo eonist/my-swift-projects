ba83dda22a0f5d5ccc548017558f07cf403e723f
Eonist <unicornhive@gmail.com>
2015-11-06 16:42:27 +0100

    Files modified: 1
    Modified 1 file:string/AdvanceStringModifier.swift

--- string/AdvanceStringModifier.swift
+++ string/AdvanceStringModifier.swift
@@ -1,9 +1,14 @@
-//
-//  AdvanceStringModifier.swift
-//  GitSync
-//
-//  Created by Kaleb Skjong on 06/11/15.
-//  Copyright © 2015 beyondnorth. All rights reserved.
-//
-
-import Foundation
+class AdvanceStringModifier{
+    /**
+     * @param input: a Css String such as "P{color:#00FF00;}"
+     * @return a Comment free css string
+     */
+    class func removeComments(string:String):String {
+        var pattern:String = "\\/\\*.*?\\*\\/";
+        RegExpModifier.replace(string,pattern, "bla")//bla bla
+        var regExpPattern:RegExp = new RegExp(pattern, "g");
+        var output:String = string.replace(regExpPattern, "");
+        //			trace(input.match(regExpPattern));
+        return output;
+    }
+}
