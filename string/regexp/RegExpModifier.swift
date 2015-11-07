61a32bf518eadb1f8f9863f149728311634b35ba
Eonist <unicornhive@gmail.com>
2015-11-07 05:38:09 +0100

    Files modified: 1
    Modified 1 file:string/regexp/RegExpModifier.swift

--- string/regexp/RegExpModifier.swift
+++ string/regexp/RegExpModifier.swift
@@ -11,4 +11,14 @@ public class RegExpModifier{
         //print(input.match(regExpPattern));
         return output;
     }
+    /**
+    * Returns the @param input without whitespace on the left and right side
+    * @Note: writing this function is not easy, check proto website for inspiration
+    * // :TODO: possible rewrite inspiration: test6.match(/(?<=<[tT][iI][tT][lL][eE]>).*(?=<\/[tT][iI][tT][lL][eE]>)/g))
+    */
+    class func removeWrappingWhitespace(input:String)->String {
+        // :TODO: the bellow is wrong , it can be (?<=^|\s)(-|\n)(?=$|\s)  // you dont need to test if there is multiple whitespaces just 1
+        let pattern:String = "[^\\s]+?(?<=\\b)(.|\\n)*?(?=$|\\s*?$)"
+        return RegExp.match(input, pattern)[0];
+    }
 }
