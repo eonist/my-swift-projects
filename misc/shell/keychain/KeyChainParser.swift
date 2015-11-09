9ed31af711ca48a66937ebe49fd86270a9aa7cd9
Eonist <unicornhive@gmail.com>
2015-11-09 10:45:27 +0100

    Files modified: 1, Files renamed: 1
    Modified 1 file:misc/shell/keychain/KeyChainParser.swift

--- misc/shell/keychain/KeyChainParser.swift
+++ misc/shell/keychain/KeyChainParser.swift
@@ -17,7 +17,7 @@ class KeyChainParser {
 	 */	
     class func load(key: String) -> NSData? {
         let query = [kSecClass as String : kSecClassGenericPassword,kSecAttrAccount as String : key, kSecReturnData as String  : kCFBooleanTrue,kSecMatchLimit as String  : kSecMatchLimitOne ]
-        var dataTypeRef :UnsafeMutablePointer<AnyObject?>?
+        let dataTypeRef :UnsafeMutablePointer<AnyObject?>?
         let status: OSStatus = SecItemCopyMatching(query, dataTypeRef!)
         if status == noErr {
             return (dataTypeRef! as! NSData)
