1d8f7c96a2deab69873490921de0d85a22c73caf
Eonist <unicornhive@gmail.com>
2015-11-09 07:53:59 +0100

    Files modified: 1
    Modified 1 file:misc/layout/padding/Padding.swift

--- misc/layout/padding/Padding.swift
+++ misc/layout/padding/Padding.swift
@@ -9,10 +9,10 @@ class Padding {
     var _left:Double = 0;
     init(array:Array<Double>) {
       switch(array.count){
-          case 1: _left = _right = _top = array[0];_bottom = array[0]; break;
-          case 2: _top = _bottom = array[0];_left = _right = array[1]; break;
-          case 3: _top = array[0];_left = _right = array[1];_bottom = array[2];break;
-          case 4: _top = array[0];_right = array[1]; _bottom = array[2];_left = array[3]; break;
+         case 1: _left = array[0]; _right = array[0]; _top = array[0]; _bottom = array[0]; break;
+         case 2: _top = array[0]; _bottom = array[0];_left = array[1]; _right = array[1]; break;
+         case 3: _top = array[0]; _left = array[1]; _right = array[1]; _bottom = array[2];break;
+         case 4: _top = array[0]; _right = array[1]; _bottom = array[2]; _left = array[3]; break;
       }
     }
 }
