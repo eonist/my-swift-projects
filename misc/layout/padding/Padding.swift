3abd5725874645d1e04bd8803452d66ed3b1fc27
Eonist <unicornhive@gmail.com>
2015-11-09 07:51:51 +0100

    Files modified: 1
    Modified 1 file:misc/layout/padding/Padding.swift

--- misc/layout/padding/Padding.swift
+++ misc/layout/padding/Padding.swift
@@ -1,5 +1,18 @@
 import Foundation
-
+/*
+* TopRightBottomLeft (TRBL aka Trouble)
+*/
 class Padding {
-    
+    var _top:Double = 0;
+    var _right:Double = 0;
+    var _bottom:Double = 0;
+    var _left:Double = 0;
+    init(array:Array<Double>) {
+      switch(array.count){
+          case 1: _left = _right = _top = array[0];_bottom = array[0]; break;
+          case 2: _top = _bottom = array[0];_left = _right = array[1]; break;
+          case 3: _top = array[0];_left = _right = array[1];_bottom = array[2];break;
+          case 4: _top = array[0];_right = array[1]; _bottom = array[2];_left = array[3]; break;
+      }
+    }
 }
