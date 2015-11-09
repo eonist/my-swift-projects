abb0926aba0418354051d30e4d5996bb8f655d22
Eonist <unicornhive@gmail.com>
2015-11-09 10:47:34 +0100

    Files modified: 1
    Modified 1 file:misc/shell/keychain/KeyChainParser.swift

--- misc/shell/keychain/KeyChainParser.swift
+++ misc/shell/keychain/KeyChainParser.swift
@@ -16,14 +16,17 @@ class KeyChainParser {
 	 * Returns a keychain item for key
 	 */	
     class func load(key: String) -> NSData? {
+        /*
         let query = [kSecClass as String : kSecClassGenericPassword,kSecAttrAccount as String : key, kSecReturnData as String  : kCFBooleanTrue,kSecMatchLimit as String  : kSecMatchLimitOne ]
-        let dataTypeRef :UnsafeMutablePointer<AnyObject?>?
+        var dataTypeRef :UnsafeMutablePointer<AnyObject?>?
         let status: OSStatus = SecItemCopyMatching(query, dataTypeRef!)
         if status == noErr {
             return (dataTypeRef! as! NSData)
         } else {
             return nil
         }
+        */
+        fatalError("NOT IMPLEMENTED YET")
     }
 	/**
 	 * Deletes a keychain item for key
