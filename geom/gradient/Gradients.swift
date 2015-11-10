a44b64b193cad786ce1ad89556fd3c9ad45185f0
Eonist <unicornhive@gmail.com>
2015-11-10 18:51:52 +0100

    New files added: 1, Files modified: 1
    Added 1 file:geom/gradient/Gradients.swiftModified 1 file:geom/color/Colors.swift

---
+++ geom/gradient/Gradients.swift
@@ -0,0 +1,106 @@
+//FANCY IOS8 Gradients:
+
+
+/*
+#pragma mark - Gradient Colors
+
++ (instancetype)iOS8redGradientStartColor;
+{
+return [UIColor colorWithHexString:@"FF5E3A"];
+}
+
++ (instancetype)iOS8redGradientEndColor;
+{
+return [UIColor colorWithHexString:@"FF2A68"];
+}
+
++ (instancetype)iOS8orangeGradientStartColor;
+{
+return [UIColor colorWithHexString:@"FF9500"];
+}
+
++ (instancetype)iOS8orangeGradientEndColor;
+{
+return [UIColor colorWithHexString:@"FF5E3A"];
+}
+
++ (instancetype)iOS8yellowGradientStartColor;
+{
+return [UIColor colorWithHexString:@"FFDB4C"];
+}
+
++ (instancetype)iOS8yellowGradientEndColor;
+{
+return [UIColor colorWithHexString:@"FFCD02"];
+}
+
++ (instancetype)iOS8greenGradientStartColor;
+{
+return [UIColor colorWithHexString:@"87FC70"];
+}
+
++ (instancetype)iOS8greenGradientEndColor;
+{
+return [UIColor colorWithHexString:@"0BD318"];
+}
+
++ (instancetype)iOS8tealGradientStartColor;
+{
+return [UIColor colorWithHexString:@"52EDC7"];
+}
+
++ (instancetype)iOS8tealGradientEndColor;
+{
+return [UIColor colorWithHexString:@"5AC8FB"];
+}
+
++ (instancetype)iOS8blueGradientStartColor;
+{
+return [UIColor colorWithHexString:@"1AD6FD"];
+}
+
++ (instancetype)iOS8blueGradientEndColor;
+{
+return [UIColor colorWithHexString:@"1D62F0"];
+}
+
++ (instancetype)iOS8violetGradientStartColor;
+{
+return [UIColor colorWithHexString:@"C644FC"];
+}
+
++ (instancetype)iOS8violetGradientEndColor;
+{
+return [UIColor colorWithHexString:@"5856D6"];
+}
+
++ (instancetype)iOS8magentaGradientStartColor;
+{
+return [UIColor colorWithHexString:@"EF4DB6"];
+}
+
++ (instancetype)iOS8magentaGradientEndColor;
+{
+return [UIColor colorWithHexString:@"C643FC"];
+}
+
++ (instancetype)iOS8blackGradientStartColor;
+{
+return [UIColor colorWithHexString:@"4A4A4A"];
+}
+
++ (instancetype)iOS8blackGradientEndColor;
+{
+return [UIColor colorWithHexString:@"2B2B2B"];
+}
+
++ (instancetype)iOS8silverGradientStartColor;
+{
+return [UIColor colorWithHexString:@"DBDDDE"];
+}
+
++ (instancetype)iOS8silverGradientEndColor;
+{
+return [UIColor colorWithHexString:@"898C90"];
+}
+*/
