f655a66b372efb1d39a2147028ce6459cb04a4bb
Eonist <unicornhive@gmail.com>
2015-11-10 18:56:01 +0100

    Files modified: 1
    Modified 1 file:misc/layout/primitives/fillet/Fillet.swift

--- misc/layout/primitives/fillet/Fillet.swift
+++ misc/layout/primitives/fillet/Fillet.swift
@@ -1,5 +1,21 @@
 import Foundation
-
+/**
+ * A fillet is a class that represents the curved corners of a rectangle (DataContainer, Bean style class)
+ * // :TODO: refactor to be TRBL or?
+ */
 class Fillet {
-
+    var _topLeft:Double;
+    var _topRight:Double;
+    var _bottomLeft:Double;
+    var _bottomRight:Double;
+    init(args:String...) {
+    switch(args.length){
+				case 0: _topLeft = _topRight = _bottomLeft = _bottomRight = 0; break;
+				case 1: _topLeft = _topRight = _bottomLeft = _bottomRight = args[0]; break;
+				case 2: _topLeft = _topRight = args[0];_bottomLeft = _bottomRight = args[1]; break;
+				case 3: _topLeft = args[0];_topRight = args[1];_bottomLeft = args[2];_bottomRight = 0; break;
+				case 4: _topLeft = args[0];_topRight = args[1];_bottomLeft = args[2];_bottomRight = args[3]; break;
+				default: throw new Error("Argument length not supported: "+ args.length);
+    }
+    }
 }
