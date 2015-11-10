70b76afcb1bec9f1be362115dca48f7338b82140
Eonist <unicornhive@gmail.com>
2015-11-10 18:58:10 +0100

    Files modified: 1
    Modified 1 file:misc/layout/primitives/fillet/Fillet.swift

--- misc/layout/primitives/fillet/Fillet.swift
+++ misc/layout/primitives/fillet/Fillet.swift
@@ -8,14 +8,14 @@ class Fillet {
     var _topRight:Double;
     var _bottomLeft:Double;
     var _bottomRight:Double;
-    init(args:String...) {
-    switch(args.length){
+    init(args:Double...) {
+        switch(args.count){
 				case 0: _topLeft = _topRight = _bottomLeft = _bottomRight = 0; break;
 				case 1: _topLeft = _topRight = _bottomLeft = _bottomRight = args[0]; break;
 				case 2: _topLeft = _topRight = args[0];_bottomLeft = _bottomRight = args[1]; break;
 				case 3: _topLeft = args[0];_topRight = args[1];_bottomLeft = args[2];_bottomRight = 0; break;
 				case 4: _topLeft = args[0];_topRight = args[1];_bottomLeft = args[2];_bottomRight = args[3]; break;
 				default: throw new Error("Argument length not supported: "+ args.length);
-    }
+        }
     }
 }
