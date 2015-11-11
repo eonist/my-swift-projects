9c072ac3fce57e0923f930a69807ba2e6527e5c7
Eonist <unicornhive@gmail.com>
2015-11-11 20:33:19 +0100

    Files modified: 1
    Modified 1 file:misc/layout/primitives/fillet/Fillet.swift

--- misc/layout/primitives/fillet/Fillet.swift
+++ misc/layout/primitives/fillet/Fillet.swift
@@ -8,7 +8,7 @@ class Fillet {
     var topRight:Double;
     var bottomLeft:Double;
     var bottomRight:Double;
-    init(args:Double...) {
+    init(args:Double...) {//TODO:you may need to add the _ char infront of args
         switch(args.count){
 				case 0: topLeft = 0; topRight = 0; bottomLeft = 0; bottomRight = 0; break;
 				case 1: topLeft = args[0];topRight = args[0];bottomLeft = args[0];bottomRight = args[0]; break;
