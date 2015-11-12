f88cb628efdf1f89203e4b6ba1fdf29fac753b9a
Eonist <unicornhive@gmail.com>
2015-11-12 06:34:18 +0100

    Files modified: 1
    Modified 1 file:misc/layout/primitives/fillet/Fillet.swift

--- misc/layout/primitives/fillet/Fillet.swift
+++ misc/layout/primitives/fillet/Fillet.swift
@@ -3,12 +3,12 @@ import Foundation
  * A fillet is a class that represents the curved corners of a rectangle (DataContainer, Bean style class)
  * // :TODO: refactor to be TRBL or?
  */
-class Fillet {
+class Fillet:ILayout {
     var topLeft:Double;
     var topRight:Double;
     var bottomLeft:Double;
     var bottomRight:Double;
-    init(args:Double...) {//TODO:you may need to add the _ char infront of args
+    required init(_ args:Double...) {//TODO:you may need to add the _ char infront of args
         switch(args.count){
 				case 0: topLeft = 0; topRight = 0; bottomLeft = 0; bottomRight = 0; break;
 				case 1: topLeft = args[0];topRight = args[0];bottomLeft = args[0];bottomRight = args[0]; break;
