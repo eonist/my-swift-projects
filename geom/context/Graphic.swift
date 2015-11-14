2e6e3a14f3cc7322660f7fef04acdf9512b156d0
Eonist <unicornhive@gmail.com>
2015-11-14 08:46:20 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -3,9 +3,8 @@ import Foundation
  * set needsDisplay = true to clear the graphics
  */
 class Graphic:FlippedView{
-    lazy var graphics: Graphics = {
-        return Graphics()
-    }()
+    lazy var graphics: Graphics = {return Graphics()}()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
+
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     init() {
         super.init(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
