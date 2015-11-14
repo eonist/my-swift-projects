1a90589116a1458c1fae4fbd3264b08fddd7bf2c
Eonist <unicornhive@gmail.com>
2015-11-14 08:09:48 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -5,7 +5,7 @@ import Foundation
 class Graphic:FlippedView{
     var graphics:Graphics
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
-    init(_ width:Int = 400, _ height:Int = 400) {
+    init() {
         graphics = Graphics()
         super.init(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
         self.wantsLayer = false//this avoids calling drawLayer() and enables drawingRect()
