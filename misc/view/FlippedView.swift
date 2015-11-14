08c91fbe56a3ffc31008f4b9df79e992f9eba072
Eonist <unicornhive@gmail.com>
2015-11-14 07:48:36 +0100

    Files modified: 1
    Modified 1 file:misc/view/FlippedView.swift

--- misc/view/FlippedView.swift
+++ misc/view/FlippedView.swift
@@ -5,7 +5,7 @@ protocol IView{
     var bounds: NSRect{get}
 }
 class FlippedView:NSView{//Organizes your view from top to bottom
-    override var wantsDefaultClipping:Bool{return false}
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override var flipped:Bool {
         get {
             return true
