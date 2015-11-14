56710e5c323f1e8ba017a4fc576f7085bfc94708
Eonist <unicornhive@gmail.com>
2015-11-14 08:07:55 +0100

    Files modified: 2
    Modified 2 files:geom/context/Graphic.swiftmisc/view/FlippedView.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -1,51 +1,19 @@
 import Foundation
-
-
-//this class is incomplete
-
-
+/*
+ * set needsDisplay = true to clear the graphics
+ */
 class Graphic:FlippedView{
-    var hasClear:Bool = false
     var graphics:Graphics
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     init(_ width:Int = 400, _ height:Int = 400) {
-        let frame = NSRect(x: 0, y: 0, width: width, height: height)
         graphics = Graphics()
-        super.init(frame: frame)
+        super.init(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
         self.wantsLayer = false//this avoids calling drawLayer() and enables drawingRect()
-        
     }
     /*
-    * Required by super class
-    */
+     * Required by super class
+     */
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    /**
-     *
-     */
-    override func drawRect(dirtyRect: NSRect) {
-        Swift.print("drawRect: " + "\(hasClear)")
-        if(!hasClear){
-            var path:CGPath = CGPathParser.rect(200,200)//Shapes
-            //CGPathModifier.translate(&path,CGFloat(x),CGFloat(y))//Transformations
-            //graphics.line(12)//Stylize the line
-            //graphics.fill(color)//Stylize the fill
-            graphics.draw(path)//draw everything
-            hasClear = false;
-        }
-        
-        
-        
-        
-        //super.drawRect(dirtyRect)
-    }
-    /**
-     *
-     */
-    func clear(){
-        Swift.print("clear()")
-        hasClear = true
-        //drawRect(frame)
-        needsDisplay = true
     }
 }
