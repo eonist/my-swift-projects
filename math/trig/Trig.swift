449c49a352b9f0330eb613906a8eb4a4eb86e5a6
Eonist <unicornhive@gmail.com>
2015-11-15 09:39:15 +0100

    Files modified: 1
    Modified 1 file:math/trig/Trig.swift

--- math/trig/Trig.swift
+++ math/trig/Trig.swift
@@ -1,13 +1,17 @@
 class Trig{
+    static var left:Double = 180;
+    static var right:Double = 0;
+    static var bottom:Double = 90;
+    static var top:Double = 270;
     /**
-    * Returns an angle in Degrees
-    * @param angleType A string describing an angle in numeric value
-    * @return angle in degrees (0-360)
-    */
-    class func angleType(angleType:String):Number {
-        if(angleType == "left") return LEFT;
-        else if(angleType == "right") return RIGHT;
-        else if(angleType == "top") return TOP;
-        else /*bottom*/ return BOTTOM;
+     * Returns an angle in Degrees
+     * @param angleType A string describing an angle in numeric value
+     * @return angle in degrees (0-360)
+     */
+    class func angleType(angleType:String)->Double {
+        if(angleType == "left") {return left}
+        else if(angleType == "right") {return right}
+        else if(angleType == "top") {return top}
+        else {/*bottom*/ return bottom}
     }
 }
