8ebfc2f383e7e20190a093ec5566e9174c05b704
Eonist <unicornhive@gmail.com>
2015-11-17 06:11:46 +0100

    Files modified: 1
    Modified 1 file:geom/point/Point.swift

--- geom/point/Point.swift
+++ geom/point/Point.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class Point {
-
+    var x:Double
+    var y:Double
+    init(x:Double = 0, y:Double = 0){
+        self.x = x
+        self.y = y
+    }
+}
+extension Point{
+    
 }
