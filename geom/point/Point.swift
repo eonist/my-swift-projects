78c80db74aa4bb52c66f76872401d5140c47d829
Eonist <unicornhive@gmail.com>
2015-11-17 07:13:21 +0100

    Files modified: 1
    Modified 1 file:geom/point/Point.swift

--- geom/point/Point.swift
+++ geom/point/Point.swift
@@ -1,23 +1,27 @@
 import Foundation
 
-class Point {
+class Point:CGPoint{
+    /*
     var x:Double
     var y:Double
     init(x:Double = 0, y:Double = 0){
-        self.x = x
-        self.y = y
+    self.x = x
+    self.y = y
     }
+    */
 }
 extension Point{
     /**
      *
      */
+    /*
     func add(p:Point){
-        x += p.x
-        y += p.y
+    x += p.x
+    y += p.y
     }
     func subtract(p:Point){
-        x -= p.x
-        y -= p.y
+    x -= p.x
+    y -= p.y
     }
-}
+    
+    */}
