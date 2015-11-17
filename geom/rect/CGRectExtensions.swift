56c08ab323ac799fa1f93bff72770d8d01fbaa5e
Eonist <unicornhive@gmail.com>
2015-11-17 16:03:24 +0100

    Files modified: 1
    Modified 1 file:geom/rect/CGRectExtensions.swift

--- geom/rect/CGRectExtensions.swift
+++ geom/rect/CGRectExtensions.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class CGRectExtensions {
-
+extension CGRect {
+    /**
+     *
+     */
+    func clone()->CGRect{
+        return CGRect(self.origin.x,self.origin.y,self.width,self.height)
+    }
 }
+
+
+//make the conventient init with x,y etc and also implement x,y as get values
