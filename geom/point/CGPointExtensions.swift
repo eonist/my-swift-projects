0f49862228276fde013e8df7b49a6698459a2939
Eonist <unicornhive@gmail.com>
2015-11-17 07:54:24 +0100

    Files modified: 1
    Modified 1 file:geom/point/CGPointExtensions.swift

--- geom/point/CGPointExtensions.swift
+++ geom/point/CGPointExtensions.swift
@@ -76,3 +76,32 @@ public func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
 public func *= (inout point: CGPoint, scalar: CGFloat) {
     point = point * scalar
 }
+
+/**
+ * Divides two CGPoint values and returns the result as a new CGPoint.
+ */
+public func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+/**
+ * Divides a CGPoint by another.
+ */
+public func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+/**
+ * Divides the x and y fields of a CGPoint by the same scalar value and returns
+ * the result as a new CGPoint.
+ */
+public func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+/**
+ * Divides the x and y fields of a CGPoint by the same scalar value.
+ */
+public func /= (inout point: CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
