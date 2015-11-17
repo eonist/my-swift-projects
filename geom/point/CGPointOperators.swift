016d988700c746fae4f8dfaf2c16f7bab8af9ca5
Eonist <unicornhive@gmail.com>
2015-11-17 15:57:30 +0100

    Files modified: 1
    Modified 1 file:geom/point/CGPointOperators.swift

--- geom/point/CGPointOperators.swift
+++ geom/point/CGPointOperators.swift
@@ -1,5 +1,84 @@
 import Foundation
 
-class CGPointOperators {
+/**
+ * Increments a CGPoint with the value of another.
+ */
+public func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
 
+/**
+ * Adds two CGPoint values and returns the result as a new CGPoint.
+ */
+public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+/**
+ * Subtracts two CGPoint values and returns the result as a new CGPoint.
+ */
+public func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+/**
+ * Decrements a CGPoint with the value of another.
+ */
+public func -= (inout left: CGPoint, right: CGPoint) {
+    left = left - right
+}
+/**
+ * Multiplies two CGPoint values and returns the result as a new CGPoint.
+ */
+public func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+/**
+ * Multiplies a CGPoint with another.
+ */
+public func *= (inout left: CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+/**
+ * Multiplies the x and y fields of a CGPoint with the same scalar value and
+ * returns the result as a new CGPoint.
+ */
+public func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+/**
+ * Multiplies the x and y fields of a CGPoint with the same scalar value.
+ */
+public func *= (inout point: CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+/**
+ * Divides two CGPoint values and returns the result as a new CGPoint.
+ */
+public func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+/**
+ * Divides a CGPoint by another.
+ */
+public func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+/**
+ * Divides the x and y fields of a CGPoint by the same scalar value and returns
+ * the result as a new CGPoint.
+ */
+public func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+/**
+ * Divides the x and y fields of a CGPoint by the same scalar value.
+ */
+public func /= (inout point: CGPoint, scalar: CGFloat) {
+    point = point / scalar
 }
