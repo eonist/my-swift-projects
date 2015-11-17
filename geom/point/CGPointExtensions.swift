73380384d29302f5f7c42d8880ad75a083e57cb7
Eonist <unicornhive@gmail.com>
2015-11-17 07:25:03 +0100

    Files modified: 1
    Modified 1 file:geom/point/CGPointExtensions.swift

--- geom/point/CGPointExtensions.swift
+++ geom/point/CGPointExtensions.swift
@@ -13,14 +13,11 @@ extension CGPoint{
     func subtract(p:CGPoint)->CGPoint{
         return CGPointMake(x - p.x, y - p.y)
     }
-    // Extend `CGPoint` to add an initializer from a `float2` representation of a point.
-    
-    // MARK: Initializers
-    
-    /// Initialize with a `float2` type.
+    /**
+     * Init a CGPoint with Double values
+     */
     init(_ x: Double, _ y:Double) {
         self.x = CGFloat(x)
         self.y = CGFloat(y)
     }
-
 }
