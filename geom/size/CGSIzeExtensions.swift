6d8735d5d397c659e3fca4cda2aef7e4a5f17b45
Eonist <unicornhive@gmail.com>
2015-11-17 20:07:44 +0100

    Files modified: 1
    Modified 1 file:geom/size/CGSIzeExtensions.swift

--- geom/size/CGSIzeExtensions.swift
+++ geom/size/CGSIzeExtensions.swift
@@ -1,5 +1,8 @@
 import Foundation
-
-class CGSIzeExtensions {
-
+/*Convenient extensions*/
+extension CGSize {
+    /*Initialization*/
+    init(_ width:CGFloat,_ height:CGFloat){self.width = width;self.height = height;}
+    init(_ width:Double,_ height:Double){self.width = CGFloat(width);self.height = CGFloat(height);}
+    init(_ width:Int,_ height:Int){self.width = CGFloat(width);self.height = CGFloat(height);}
 }
