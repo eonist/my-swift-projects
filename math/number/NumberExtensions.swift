3e2084f6850f89eee9ed705a4619837999a503cc
Eonist <unicornhive@gmail.com>
2015-11-17 08:48:38 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberExtensions.swift

--- math/number/NumberExtensions.swift
+++ math/number/NumberExtensions.swift
@@ -1,5 +1,17 @@
 import Foundation
 
-class NumberExtensions {
-
+/**
+ * Support for addition of CGFLoat and Double
+ * TODO: probably use T or Generics or similar
+ * TODO: add support for returning Double aswell
+ * TODO: add support for Int and Float aswell
+ */
+public func + (left: CGFloat, right: Double) -> CGFloat {
+    return left + CGFloat(right)
+}
+/**
+ * Support for addition of CGFLoat and Double
+ */
+public func + (left: Double, right: CGFloat) -> CGFloat {
+    return CGFloat(left) + right
 }
