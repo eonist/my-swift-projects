963423e184d6769da87688af9a9fad5ad25c1d00
Eonist <unicornhive@gmail.com>
2015-11-17 17:52:09 +0100

    Files modified: 1
    Modified 1 file:geom/rect/CGRectExtensions.swift

--- geom/rect/CGRectExtensions.swift
+++ geom/rect/CGRectExtensions.swift
@@ -8,12 +8,14 @@ extension CGRect {
         return CGRect(self.origin.x,self.origin.y,self.width,self.height)
     }
 }
+/*Convenient extensions*/
 extension CGRect{
     init(_ x:CGFloat,_ y:CGFloat,_ width:CGFloat,_ height:CGFloat){
         self.origin = CGPoint(x,y)
         self.size = CGSize(width: width,height: height)//add initializer to CGSize
     }
-    var x:CGFloat {}
+    var x:CGFloat {set {origin.x = newValue} get {return origin.x} }
+    var y:CGFloat {set {origin.y = newValue} get {return origin.y} }
+        
+    
 }
-
-//make the conventient init with x,y etc and also implement x,y as get values
