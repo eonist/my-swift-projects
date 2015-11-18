a8c0efe3dc7d7ae7a88ee066e5b42e4043aa26e3
Eonist <unicornhive@gmail.com>
2015-11-18 21:57:22 +0100

    Files modified: 1
    Modified 1 file:misc/view/View.swift

--- misc/view/View.swift
+++ misc/view/View.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class View {
+class View :FlippedView{
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
 
+    override init(frame: NSRect) {
+        super.init(frame: frame)
+        self.wantsLayer = false//this avoids calling drawLayer() and enables drawingRect()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
