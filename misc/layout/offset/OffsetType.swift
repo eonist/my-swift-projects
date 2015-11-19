4ce1af9c059f26f0e4b845e16d36e6b553b2580a
Eonist <unicornhive@gmail.com>
2015-11-19 18:59:11 +0100

    Files modified: 1
    Modified 1 file:misc/layout/offset/OffsetType.swift

--- misc/layout/offset/OffsetType.swift
+++ misc/layout/offset/OffsetType.swift
@@ -14,7 +14,7 @@ class OffsetType:ILayout{
     required init(_ args:Any...){
         switch(args.count){
             case 0: left = OffsetType.center; right = OffsetType.center; top = OffsetType.center; bottom = OffsetType.center
-            case 1: Swift.print("args[0]: " + String(args[0])); left = args[0] as! String; right = args[0] as! String; top = args[0] as! String; bottom = args[0] as! String;
+            case 1: left = args[0] as! String; right = args[0] as! String; top = args[0] as! String; bottom = args[0] as! String;
             case 2: left = args[0] as! String; right = args[0] as! String;top = args[1] as! String; bottom = args[1] as! String;
             case 3: left = args[0] as! String;right = args[1] as! String;top = args[2] as! String;bottom = OffsetType.center;
             default: left = args[0] as! String;right = args[1] as! String;top = args[2] as! String;bottom = args[3] as! String; /*length >= 4*/
