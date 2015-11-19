8e6438bdf84a349ba398d5771ffe76ed0acf7582
Eonist <unicornhive@gmail.com>
2015-11-19 06:29:28 +0100

    Files modified: 1
    Modified 1 file:misc/fillet/utils/FilletParser.swift

--- misc/fillet/utils/FilletParser.swift
+++ misc/fillet/utils/FilletParser.swift
@@ -5,11 +5,11 @@ class FilletParser {
      * Returns a fillet configured to @param lineOffset and @param lineStyle
      */
     class func config(fillet:Fillet, offsetType:OffsetType, lineStyle:ILineStyle)->Fillet {/* configure cornerRadius according to offsetType and lineStyle*/
-        var tlr:CGFloat = configRadius(fillet.topLeft, offsetType.left/* || offsetType.top *//*<--this last value is unrelevant since left will always be true*/, lineStyle);//TopLeftRadius
-        var trr:CGFloat = configRadius(fillet.topRight, offsetType.right/* || offsetType.top, lineStyle);*//*TopRightRadius*/
-        var blr:CGFloat = configRadius(fillet.bottomLeft, offsetType.left/* || offsetType.bottom, lineStyle);*//*BottomLeftRadius*/
-        var brr:CGFloat = configRadius(fillet.bottomRight, offsetType.right/* || offsetType.bottom, lineStyle);*//*BottomRightRadius*/
-        return new Fillet(tlr,trr,blr,brr);
+        let tlr:CGFloat = configRadius(fillet.topLeft, offsetType.left/* || offsetType.top *//*<--this last value is unrelevant since left will always be true*/, lineStyle);//TopLeftRadius
+        let trr:CGFloat = configRadius(fillet.topRight, offsetType.right/* || offsetType.top*/, lineStyle)/*TopRightRadius*/
+        let blr:CGFloat = configRadius(fillet.bottomLeft, offsetType.left/* || offsetType.bottom*/, lineStyle)/*BottomLeftRadius*/
+        let brr:CGFloat = configRadius(fillet.bottomRight, offsetType.right/* || offsetType.bottom*/, lineStyle)/*BottomRightRadius*/
+        return Fillet(tlr,trr,blr,brr);
     }
     /**
      * Returns a corner radius with correct radius according to the @param offsetType
