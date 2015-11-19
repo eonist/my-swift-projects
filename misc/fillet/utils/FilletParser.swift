75355237a4a3ea771a6d3341ae7b625674979463
Eonist <unicornhive@gmail.com>
2015-11-19 06:27:33 +0100

    Files modified: 1
    Modified 1 file:misc/fillet/utils/FilletParser.swift

--- misc/fillet/utils/FilletParser.swift
+++ misc/fillet/utils/FilletParser.swift
@@ -5,16 +5,16 @@ class FilletParser {
      * Returns a fillet configured to @param lineOffset and @param lineStyle
      */
     class func config(fillet:Fillet, offsetType:OffsetType, lineStyle:ILineStyle)->Fillet {/* configure cornerRadius according to offsetType and lineStyle*/
-        var tlr:CGFloat = configRadius(fillet.topLeft, offsetType.left || offsetType.top /*<--this last value is unrelevant since left will always be true*/, lineStyle);//TopLeftRadius
-        var trr:CGFloat = configRadius(fillet.topRight, offsetType.right || offsetType.top, lineStyle);/*TopRightRadius*/
-        var blr:CGFloat = configRadius(fillet.bottomLeft, offsetType.left || offsetType.bottom, lineStyle);/*BottomLeftRadius*/
-        var brr:CGFloat = configRadius(fillet.bottomRight, offsetType.right || offsetType.bottom, lineStyle);/*BottomRightRadius*/
+        var tlr:CGFloat = configRadius(fillet.topLeft, offsetType.left/* || offsetType.top *//*<--this last value is unrelevant since left will always be true*/, lineStyle);//TopLeftRadius
+        var trr:CGFloat = configRadius(fillet.topRight, offsetType.right/* || offsetType.top, lineStyle);*//*TopRightRadius*/
+        var blr:CGFloat = configRadius(fillet.bottomLeft, offsetType.left/* || offsetType.bottom, lineStyle);*//*BottomLeftRadius*/
+        var brr:CGFloat = configRadius(fillet.bottomRight, offsetType.right/* || offsetType.bottom, lineStyle);*//*BottomRightRadius*/
         return new Fillet(tlr,trr,blr,brr);
     }
     /**
      * Returns a corner radius with correct radius according to the @param offsetType
      */
-    class func configRadius(cornerRadius:CGFloat, offsetType:String, lineStyle:ILineStyle)->CGFloat{
+    class func configRadius(cornerRadius:CGFloat, _ offsetType:String, _ lineStyle:ILineStyle)->CGFloat{
         var multiplier:CGFloat;
         if(offsetType == OffsetType.OUTSIDE) multiplier = 1;
         else if(offsetType == OffsetType.INSIDE) multiplier = 1;
