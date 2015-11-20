010ce666d77317b5b5e591d042b40bf6946f8ef1
Eonist <unicornhive@gmail.com>
2015-11-20 16:07:12 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFormatConstants.swift

--- text/utils/TextFormatConstants.swift
+++ text/utils/TextFormatConstants.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class TextFormatConstants {
-    static var textFormatPropertyNames:Array<String> = ["align","blockIndent","bold","bullet","color","font","indent","italic","kerning","leading","leftMargin","letterSpacing","rightMargin","size","tabStops","target","underline","url"];
+    static var selectable:String = "selectable"
+    static var background:String = "background"
+    static var backgroundColor:String = "backgroundColor"
+    static var textColor:String = "textColor"
+    static var textFormatPropertyNames:Array<String> = [selectable,background,backgroundColor,textColor]
+    //["align","blockIndent","bold","bullet","color","font","indent","italic","kerning","leading","leftMargin","letterSpacing","rightMargin","size","tabStops","target","underline","url"];
 }
