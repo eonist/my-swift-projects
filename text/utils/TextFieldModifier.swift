455566c50b50bd5676dd596f8a61b8181770452f
Eonist <unicornhive@gmail.com>
2015-11-21 06:47:21 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFieldModifier.swift

--- text/utils/TextFieldModifier.swift
+++ text/utils/TextFieldModifier.swift
@@ -5,11 +5,12 @@ class TextFieldModifier {
      *
      */
     class func applyTextFormat(textField:NSText,_ textFormat:TextFormat){
-        textField.selectable = false
-        textField.drawsBackground = true
-        textField.backgroundColor = NSColor.purpleColor()
-        NSTextAlignment.
-        textField.alignment = NSTextAlignment.Center//Left,Right,Justified,Natural,Center
+        textField.selectable = textFormat.selectable
+        textField.drawsBackground = textFormat.background
+        textField.backgroundColor = textFormat.backgroundColor
+        let align:NSTextAlignment
+        if(){
+        textField.alignment = (textFormat.align)//Left,Right,Justified,Natural,Center
         textField.textColor = NSColor.orangeColor()
     }
 }
