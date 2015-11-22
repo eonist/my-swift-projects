44798b8ff463f196d87b199477adc217228f29bd
Eonist <unicornhive@gmail.com>
2015-11-22 09:03:02 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewExtensions.swift

--- misc/view/utils/NSViewExtensions.swift
+++ misc/view/utils/NSViewExtensions.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 extension NSView {
     /**
-     *
+     * Asserts if locationInWindow is within the NSView frame
      */
     func hitTestPoint(locationInWindow:NSPoint)->Bool{
         let mousePos:NSPoint = convertPoint(locationInWindow, fromView: nil)
