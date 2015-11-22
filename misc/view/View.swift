0ac0a765f0cdcc2a042bafb0fe3410c3f947abf5
Eonist <unicornhive@gmail.com>
2015-11-22 08:49:32 +0100

    Files modified: 1
    Modified 1 file:misc/view/View.swift

--- misc/view/View.swift
+++ misc/view/View.swift
@@ -1,6 +1,7 @@
-import Foundation
+import Cocoa
 
 class View :FlippedView{
+    var interactionEnabled:Bool = true
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override init(frame: NSRect) {
         super.init(frame: frame)
@@ -9,4 +10,5 @@ class View :FlippedView{
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func hitTest(aPoint: NSPoint) -> NSView? {return interactionEnabled ? self : nil}//Avoids covering the graphic behind when dealing with mouse down events
 }
