4d83e9803c7276bff5106d2c9424dcc360de4b7f
Eonist <unicornhive@gmail.com>
2015-11-26 08:38:17 +0100

    Files modified: 1
    Modified 1 file:geom/point/CGPointExtensions.swift

--- geom/point/CGPointExtensions.swift
+++ geom/point/CGPointExtensions.swift
@@ -26,4 +26,4 @@ extension CGPoint{
 public func +(a: CGPoint, b: CGPoint) -> CGPoint { return CGPoint(a.x+b.x, a.y+b.y)}//Adds the coordinates of point p to the coordinates of this point to create a new point
 public func -(a: CGPoint, b: CGPoint) -> CGPoint { return CGPoint(a.x-b.x, a.y-b.y)}//Subtracts the coordinates of point p from the coordinates of this point to create a new point.
 public func +=(inout a: CGPoint, b: CGPoint) {a.x += b.x;a.y += b.y;}//modifies a by adding b
-public func -=(inout a: CGPoint, b: CGPoint) {a.x -= b.x;a.y -= b.y;}//modifies a by adding b
+public func -=(inout a: CGPoint, b: CGPoint) {a.x -= b.x;a.y -= b.y;}//modifies a by substracting b
