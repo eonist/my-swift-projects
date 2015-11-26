b32fe180169b79e3bbe76b1b2f9d7eeb672d8b24
Eonist <unicornhive@gmail.com>
2015-11-26 08:49:46 +0100

    Files modified: 1
    Modified 1 file:geom/point/PointParser.swift

--- geom/point/PointParser.swift
+++ geom/point/PointParser.swift
@@ -27,15 +27,7 @@ class PointParser{
         }
         return CGPoint(x,y);
     }
-    /**
-     * @param rotation must be between -PI and +PI
-     */
-    class func safeRotatePoint(pivot:CGPoint, point:CGPoint, rotation:CGFloat)->CGPoint {
-        let pointAngle:CGFloat = Trig.angle(pivot, point)//find the angle of point
-        let distance:CGFloat = PointParser.distance(pivot, point);//length of point and pivotPoint
-        let rot:CGFloat = Trig.normalize2(pointAngle + rotation);//sum of pointAngle and rotation, normalize this
-        return pivot + PointParser.safePolar(distance, rot);//use Point.polar
-    }
+    
     /**
      * Returns the distance between two points
      * @Note: Math formula for distance of two points is: AB2 = dx2 + dy2 (distance = sqrt(dx2 + dy2)) where one side is dx - the difference in x-coordinates, and the other is dy - the difference in y-coordinates.
