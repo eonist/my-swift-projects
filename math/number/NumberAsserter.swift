6d184ae6bb738ab42fc97ec4508674ae075d8117
Eonist <unicornhive@gmail.com>
2015-11-26 19:38:46 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberAsserter.swift

--- math/number/NumberAsserter.swift
+++ math/number/NumberAsserter.swift
@@ -1,3 +1,9 @@
+import Foundation
 class NumberAsserter{
-    
+    /**
+     * @Note: In most cases you will want to write if(someNumber < 0)
+     */
+    class func negative(number:CGFloat)->Bool {
+        return number < 0;
+    }
 }
