35cc0dd6f2ae50b9226506c6e149e1d7a96ece8e
Eonist <unicornhive@gmail.com>
2015-11-26 08:51:39 +0100

    Files modified: 1, Files renamed: 1
    Modified 1 file:geom/point/CGPointOperators.swift

--- geom/point/CGPointOperators.swift
+++ geom/point/CGPointOperators.swift
@@ -1,36 +1,15 @@
 import Foundation
 
-/**
- * Increments a CGPoint with the value of another.
- */
-public func += (inout left: CGPoint, right: CGPoint) {
-    left = left + right
-}
+public func +(a: CGPoint, b: CGPoint) -> CGPoint { return CGPoint(a.x+b.x, a.y+b.y)}//Adds the coordinates of point p to the coordinates of this point to create a new point
+public func -(a: CGPoint, b: CGPoint) -> CGPoint { return CGPoint(a.x-b.x, a.y-b.y)}//Subtracts the coordinates of point p from the coordinates of this point to create a new point.
+public func +=(inout a: CGPoint, b: CGPoint) {a.x += b.x;a.y += b.y;}//modifies a by adding b
+public func -=(inout a: CGPoint, b: CGPoint) {a.x -= b.x;a.y -= b.y;}//modifies a by substracting b
 
-/**
- * Adds two CGPoint values and returns the result as a new CGPoint.
- */
-public func + (left: CGPoint, right: CGPoint) -> CGPoint {
-    return CGPoint(x: left.x + right.x, y: left.y + right.y)
-}
-
-/**
- * Subtracts two CGPoint values and returns the result as a new CGPoint.
- */
-public func - (left: CGPoint, right: CGPoint) -> CGPoint {
-    return CGPoint(x: left.x - right.x, y: left.y - right.y)
-}
-/**
- * Decrements a CGPoint with the value of another.
- */
-public func -= (inout left: CGPoint, right: CGPoint) {
-    left = left - right
-}
 /**
  * Multiplies two CGPoint values and returns the result as a new CGPoint.
  */
 public func * (left: CGPoint, right: CGPoint) -> CGPoint {
-    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+    
 }
 /**
  * Multiplies a CGPoint with another.
