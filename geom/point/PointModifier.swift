47e97d7a1394b8963d77727eb69acc9ae5b3fcf8
Eonist <unicornhive@gmail.com>
2015-11-26 08:49:46 +0100

    Files modified: 1
    Modified 1 file:geom/point/PointModifier.swift

--- geom/point/PointModifier.swift
+++ geom/point/PointModifier.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class PointModifier {
-
+    /**
+     * @param rotation must be between -PI and +PI
+     */
+    class func safeRotatePoint(pivot:CGPoint, point:CGPoint, rotation:CGFloat)->CGPoint {
+        let pointAngle:CGFloat = Trig.angle(pivot, point)//find the angle of point
+        let distance:CGFloat = PointParser.distance(pivot, point);//length of point and pivotPoint
+        let rot:CGFloat = Trig.normalize2(pointAngle + rotation);//sum of pointAngle and rotation, normalize this
+        return pivot + PointParser.safePolar(distance, rot);//use Point.polar
+    }
 }
