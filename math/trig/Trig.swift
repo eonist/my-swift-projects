3d2394526fbf27ad21b42ab5ade2e4ee39272254
Eonist <unicornhive@gmail.com>
2015-11-26 07:30:42 +0100

    Files modified: 1
    Modified 1 file:math/trig/Trig.swift

--- math/trig/Trig.swift
+++ math/trig/Trig.swift
@@ -37,9 +37,9 @@ class Trig{
     * @Note: we use "while" function type here because angle could be very low at which point Math.PI*2 needs to be contrinuasly added until its above 0 )
     * // :TODO: use modulo like normalize2 does, is that faster ? do an optimization test.
     */
-    class func normalize(angle:CGFloat)->CGFloat {
-        while (angle < 0) angle += Math.PI*2;
-        while (angle >= Math.PI*2) angle -= Math.PI*2;
+    class func normalize(var angle:CGFloat)->CGFloat {
+        while (angle < 0) {angle += π*2};
+        while (angle >= Math.PI*2) {angle -= Math.PI*2};
         return angle;
     }
     /**
