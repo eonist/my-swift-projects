8a79867726ca1c0841d51d251d83de34be3d5eeb
Eonist <unicornhive@gmail.com>
2015-11-27 15:30:01 +0100

    Files modified: 1
    Modified 1 file:geom/path/utils/CGPathParser.swift

--- geom/path/utils/CGPathParser.swift
+++ geom/path/utils/CGPathParser.swift
@@ -5,7 +5,7 @@ public class CGPathParser{
     /**
      * NOTE: We do not need to close this path
      */
-    public class func line(p1:CGPoint,p2:CGPoint)->CGMutablePathRef{
+    public class func line(p1:CGPoint,_ p2:CGPoint)->CGMutablePathRef{
         let linePath:CGMutablePathRef = CGPathCreateMutable()
         CGPathMoveToPoint(linePath, nil, p1.x, p1.y)
         CGPathAddLineToPoint(linePath, nil, p2.x, p2.y)
