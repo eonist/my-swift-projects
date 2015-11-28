55c53a577fa3eed1bfea192bd5a5097bced4ebf9
Eonist <unicornhive@gmail.com>
2015-11-28 19:10:47 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositionable.swift

--- misc/layout/protocols/IPositionable.swift
+++ misc/layout/protocols/IPositionable.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-protocol IPositionable {
+protocol IPositional {
     var position:CGPoint{get set}
 }
-extension IPositionable{
+extension IPositional{
     var x:CGFloat{get{return self.position.x} set{self.position.x = newValue}}
     var y:CGFloat{get{return self.position.y} set{self.position.y = newValue}}
     mutating func setPosition(x:CGFloat,y:CGFloat){
