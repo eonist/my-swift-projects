469c7fdc8a60839cc63cc40f10cc8acd7ea233a7
Eonist <unicornhive@gmail.com>
2015-11-29 10:02:39 +0100

    Files modified: 3
    Modified 3 files:misc/range/Range.swiftmisc/range/RangeAsserter.swiftmisc/range/RangeParser.swift

--- misc/range/Range.swift
+++ misc/range/Range.swift
@@ -8,34 +8,3 @@ class Range<T> {
         self.end = end;
     }
 }
-extension Range{
-    func within(range:Range,number:T)->Bool {
-        return false//(number <= RangeParser.max(range) && number >= RangeParser.min(range));
-    }
-}
-
-/*
-
-class Tree <T> {
-
-// 'T' can now be used as a type
-var value : T
-
-var children : [Tree <T>] = []
-
-init(value : T) {
-self.value = value
-}
-
-func addChild(value : T) -> Tree <T> {”
-
-let newChild = Tree<T>(value: value)
-children.append(newChild)
-return newChild
-}
-}
-
-// Tree of integers
-let integerTree = Tree<Int>(value: 5)
-
-*/
--- misc/range/RangeParser.swift
+++ misc/range/RangeParser.swift
@@ -1,14 +1,3 @@
-class RangeParser<T> {
-    /**
-     * Returns the minimum or smallest value in the range.
-     */
-    class func min(range:Range<T>)->T {
-        return min(range.start, range.end);
-    }
-    /**
-     * Returns The maximum or largest value in the range.
-     */
-    class func max(range:Range<T>)->T {
-        return range.start > range.end ? range.start : range.end
-    }
+class RangeParser {
+    
 }
