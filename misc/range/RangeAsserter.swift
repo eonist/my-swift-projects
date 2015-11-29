202be882413379a3ac6f36a6f39c0b388cb58361
Eonist <unicornhive@gmail.com>
2015-11-29 10:20:51 +0100

    Files modified: 2
    Modified 2 files:math/number/cgfloat/range/CGFloatRangeAsserter.swiftmisc/range/RangeAsserter.swift

--- misc/range/RangeAsserter.swift
+++ misc/range/RangeAsserter.swift
@@ -1,9 +1,3 @@
-class RangeAsserter<T>{
-    /**
-    * Asserts if @param value is within the @param range
-    * // :TODO: a potential bug is that <= max isnt correct, if it is max then its not within, if you think about how integers work, but this may also be correct since if a number range is asserted it may need to also include the max, for now use contained if you deal with integers
-    */
-    class func within(range:Range,number:T)->Bool {
-        return (number <= RangeParser.max(range) && number >= RangeParser.min(range));
-    }
+class RangeAsserter{
+   
 }
