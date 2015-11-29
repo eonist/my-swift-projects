603300fa092c3c4e0cfb0a11ce8ef570a8c9f640
Eonist <unicornhive@gmail.com>
2015-11-29 10:10:46 +0100

    Files modified: 1
    Modified 1 file:math/number/cgfloat/range/CGFloatRangeParser.swift

--- math/number/cgfloat/range/CGFloatRangeParser.swift
+++ math/number/cgfloat/range/CGFloatRangeParser.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class CGFloatRangeParser {
-
+    /**
+     * Returns the minimum or smallest value in the range.
+     */
+    class func miny(range:(CGFloat,CGFloat))->CGFloat {
+        return min(range[0], range[1]);
+    }
+    /**
+     * Returns The maximum or largest value in the range.
+     */
+    class func max(range:Range<T>)->T {
+        return max(range.start, range.end);
+    }
 }
