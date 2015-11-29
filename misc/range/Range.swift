89be7d984ea8611dc3da7fc917954ad9f9b03bb6
Eonist <unicornhive@gmail.com>
2015-11-29 09:52:26 +0100

    Files modified: 1
    Modified 1 file:misc/range/Range.swift

--- misc/range/Range.swift
+++ misc/range/Range.swift
@@ -3,7 +3,12 @@ import Foundation
 class Range<T> {
     var start:T
     var end:T
+    init(start : T, end : T) {
+        self.start = start;
+        self.end = end;
+    }
 }
+
 /*
 
 class Tree <T> {
