b81d4f9560cfaaa581a474203e93460cf7a99f09
Eonist <unicornhive@gmail.com>
2015-11-29 10:00:35 +0100

    Files modified: 1
    Modified 1 file:misc/range/RangeParser.swift

--- misc/range/RangeParser.swift
+++ misc/range/RangeParser.swift
@@ -9,6 +9,6 @@ class RangeParser<T> {
      * Returns The maximum or largest value in the range.
      */
     class func max(range:Range<T>)->T {
-        return max(range.start, range.end);
+        return range.start > range.end ? range.start : range.end
     }
 }
