3e10fc82dad323d422d5799eb2495d1ffe803479
Eonist <unicornhive@gmail.com>
2015-11-30 06:00:25 +0100

    Files modified: 1
    Modified 1 file:misc/layout/margin/Margin.swift

--- misc/layout/margin/Margin.swift
+++ misc/layout/margin/Margin.swift
@@ -1,18 +1,31 @@
 import Foundation
 
-class Margin:ILayout{
+/**
+ * EXAMPLE: Margin(CGFloat(2)).top//2
+ * EXAMPLE: Margin([CGFloat(3)]).top//3
+ * EXAMPLE: Margin().top//0
+ */
+class Margin{
     var top:CGFloat = 0;
     var right:CGFloat = 0;
     var bottom:CGFloat = 0;
     var left:CGFloat = 0;
     required init(_ args:Any...){
         switch(args.count){
-            case 1: left = args[0] as! CGFloat; right = args[0] as! CGFloat; top = args[0] as! CGFloat; bottom = args[0] as! CGFloat; break;
-            case 2: top = args[0] as! CGFloat; bottom = args[0] as! CGFloat;left = args[1] as! CGFloat; right = args[1] as! CGFloat; break;
-            case 3: top = args[0] as! CGFloat; left = args[1] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat;break;
-            case 4: top = args[0] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat; left = args[3] as! CGFloat; break;
-            default:break;
+        case 1: left = args[0] as! CGFloat; right = args[0] as! CGFloat; top = args[0] as! CGFloat; bottom = args[0] as! CGFloat; break;
+        case 2: top = args[0] as! CGFloat; bottom = args[0] as! CGFloat;left = args[1] as! CGFloat; right = args[1] as! CGFloat; break;
+        case 3: top = args[0] as! CGFloat; left = args[1] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat;break;
+        case 4: top = args[0] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat; left = args[3] as! CGFloat; break;
+        default:break;
+        }
+    }
+    init(_ args:Array<CGFloat>){
+        switch(args.count){
+        case 1: left = args[0]; right = args[0]; top = args[0]; bottom = args[0]; break;
+        case 2: top = args[0]; bottom = args[0];left = args[1]; right = args[1]; break;
+        case 3: top = args[0]; left = args[1]; right = args[1]; bottom = args[2];break;
+        case 4: top = args[0]; right = args[1]; bottom = args[2]; left = args[3]; break;
+        default:break;
         }
     }
 }
-
