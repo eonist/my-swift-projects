6b5e4b31c8e064670ef8684b22496d4506f8c1b5
Eonist <unicornhive@gmail.com>
2015-11-30 09:59:01 +0100

    Files modified: 1
    Modified 1 file:text/TextField.swift

--- text/TextField.swift
+++ text/TextField.swift
@@ -1,5 +1,11 @@
 import Cocoa
 
 class TextField:NSText{
-    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
