f69560a5c9810b296b1db1e62b92b425d6db3557
Eonist <unicornhive@gmail.com>
2015-12-01 06:24:10 +0100

    Files modified: 1
    Modified 1 file:text/TextExtension.swift

--- text/TextExtension.swift
+++ text/TextExtension.swift
@@ -1,12 +1,5 @@
 import Cocoa
 
-class TextField:NSText{
-    override init(frame frameRect: NSRect) {
-        Swift.print("TextField.init()")
-        super.init(frame: frameRect)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+extension NSText : IPositional{
+    
 }
