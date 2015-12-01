0797093626d827a7da4b8882e904325e19b9f7a1
Eonist <unicornhive@gmail.com>
2015-12-01 14:32:55 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositional.swift

--- misc/layout/protocols/IPositional.swift
+++ misc/layout/protocols/IPositional.swift
@@ -9,8 +9,9 @@ protocol IPositional {
  */
 extension IPositional{
     //var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting mutating variables via extensions*/
-    var x:CGFloat{get{return self.position.x} set{self.position.x = newValue}}
-    var y:CGFloat{get{return self.position.y} set{self.position.y = newValue}}
+    var x:CGFloat{get{return getPosition().x} set{setPosition(CGPoint(newValue,getPosition().y))}}
+    var y:CGFloat{get{return getPosition().y} set{setPosition(CGPoint(getPosition().x,newValue))}}
+    //var y:CGFloat{get{return getPosition().y} set{self.position.y = newValue}}
     /*func getPosition() -> CGPoint {
     return position
     }*/
