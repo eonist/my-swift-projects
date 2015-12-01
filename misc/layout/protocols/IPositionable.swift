3606fafe16b1b8c984dcb1e380879d3f58ae0452
Eonist <unicornhive@gmail.com>
2015-12-01 11:56:32 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositionable.swift

--- misc/layout/protocols/IPositionable.swift
+++ misc/layout/protocols/IPositionable.swift
@@ -2,16 +2,18 @@ import Foundation
 
 protocol IPositional {
     var position:CGPoint{get set}
+    /*
     func setPosition(position:CGPoint)
     func setPosition(x:CGFloat,y:CGFloat)
+    */
 }
 extension IPositional{
+    var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting variables*/
     var x:CGFloat{get{return self.position.x} set{self.position.x = newValue}}
     var y:CGFloat{get{return self.position.y} set{self.position.y = newValue}}
     func getPosition() -> CGPoint {
         return position
     }
-    /*
     mutating func setPosition(x:CGFloat,y:CGFloat){
         self.x = x
         self.y = y
@@ -20,9 +22,6 @@ extension IPositional{
         self.position.x = position.x
         self.position.y = position.y
     }
-    */
-    
-    //var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting variables*/
 }
 
 
