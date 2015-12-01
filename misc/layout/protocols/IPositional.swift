84fe1cbf8e3233c8afe1209921899cd7b0e0c661
Eonist <unicornhive@gmail.com>
2015-12-01 14:17:09 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositional.swift

--- misc/layout/protocols/IPositional.swift
+++ misc/layout/protocols/IPositional.swift
@@ -1,7 +1,8 @@
 import Foundation
 protocol IPositional {
     var position:CGPoint{get set}
-    //func setPosition(position:CGPoint)//unsure if this can be here when you have that same method in the extension
+    func setPosition(x:CGFloat,y:CGFloat)
+    func getPosition() -> CGPoint
 }
 /**
  * CAUTION: These extensions can only be used if you dont need to cast the instance to ISizeable
@@ -10,17 +11,17 @@ extension IPositional{
     //var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting mutating variables via extensions*/
     var x:CGFloat{get{return self.position.x} set{self.position.x = newValue}}
     var y:CGFloat{get{return self.position.y} set{self.position.y = newValue}}
-    func getPosition() -> CGPoint {
-        return position
-    }
-    mutating func setPosition(x:CGFloat,y:CGFloat){
-        self.x = x
-        self.y = y
-    }
-    mutating func setPosition(position:CGPoint){
-        self.position.x = position.x
-        self.position.y = position.y
-    }
+    /*func getPosition() -> CGPoint {
+    return position
+    }*/
+    /*mutating func setPosition(x:CGFloat,y:CGFloat){
+    self.x = x
+    self.y = y
+    }*/
+    /*mutating func setPosition(position:CGPoint){
+    self.position.x = position.x
+    self.position.y = position.y
+    }*/
 }
 
 
