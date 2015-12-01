2f19ebec5ea626343f4b0d35e30dd1e4569441b0
Eonist <unicornhive@gmail.com>
2015-12-01 13:00:43 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositionable.swift

--- misc/layout/protocols/IPositionable.swift
+++ misc/layout/protocols/IPositionable.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+//continue here: create IPositionalGraphic, that uses mutating methods, the IPositional class shouldnt use mutating methods to avoid the problem with setting variables directly
 protocol IPositional {
     var position:CGPoint{get set}
     /*
