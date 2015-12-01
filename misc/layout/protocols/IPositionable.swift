908dbbd36ef3fb82536b5b84771484a3b120c72b
Eonist <unicornhive@gmail.com>
2015-12-01 11:40:56 +0100

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositionable.swift

--- misc/layout/protocols/IPositionable.swift
+++ misc/layout/protocols/IPositionable.swift
@@ -21,3 +21,5 @@ extension IPositional{
     
     var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting variables*/
 }
+
+
