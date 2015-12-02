e0529d82aa70682b07833e665947856a001face5
Eonist <unicornhive@gmail.com>
2015-12-02 16:01:09 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewExtensions.swift

--- misc/view/utils/NSViewExtensions.swift
+++ misc/view/utils/NSViewExtensions.swift
@@ -8,11 +8,13 @@ extension NSView {
     func hitTestToView(locationInWindow:NSPoint, _ toView:NSView? = nil)->Bool{
         let mousePos:NSPoint = convertPoint(locationInWindow, toView: toView)
         Swift.print("hitTestToView.mousePos(): " + String(mousePos))
+        Swift.print("hitTestToView.frame: " + String(frame))
         return NSPointInRect(mousePos, frame)
     }
     func hitTestFromView(locationInWindow:NSPoint, _ fromView:NSView? = nil)->Bool{
         let mousePos:NSPoint = convertPoint(locationInWindow, fromView: fromView)
         Swift.print("hitTestFromView.mousePos(): " + String(mousePos))
+        Swift.print("hitTestFromView.frame: " + String(frame))
         return NSPointInRect(mousePos, frame)
     }
 }
