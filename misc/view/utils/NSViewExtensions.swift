883b206bcb4e52d043c87e6fd9a141261901c52f
Eonist <unicornhive@gmail.com>
2015-12-02 15:29:44 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewExtensions.swift

--- misc/view/utils/NSViewExtensions.swift
+++ misc/view/utils/NSViewExtensions.swift
@@ -3,9 +3,14 @@ import Cocoa
 extension NSView {
     /**
      * Asserts if locationInWindow is within the NSView frame
+     * NOTE: seem to work different if the view isnt flipped (which some arent) try the alternate method with the "fromView" param
      */
-    func hitTestPoint(locationInWindow:NSPoint)->Bool{
-        let mousePos:NSPoint = convertPoint(locationInWindow, toView: self)
+    func hitTestPoint(locationInWindow:NSPoint, _ toView:NSView? = nil)->Bool{
+        let mousePos:NSPoint = convertPoint(locationInWindow, toView: toView)
+        return NSPointInRect(mousePos, frame)
+    }
+    func hitTestPoint(locationInWindow:NSPoint, _ fromView:NSView? = nil)->Bool{
+        let mousePos:NSPoint = convertPoint(locationInWindow, fromView: fromView)
         return NSPointInRect(mousePos, frame)
     }
 }
