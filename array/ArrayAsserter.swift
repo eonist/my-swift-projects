1ad78eff22ae524f90b69bdcad86aaef3c0f3905
Eonist <unicornhive@gmail.com>
2015-12-03 10:32:49 +0100

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -6,7 +6,8 @@ class ArrayAsserter {
     * @param b: the array to check against
     * @param strict: toggle the ability that the items must be equal to the array, (almost the same functionality as the isEqual function, although equal function the array must have the same order)
     * @return: a boolean indicating if the items is apart of the array
-    * TODO:  write an example
+    * EXAMPLE: ArrayAsserter.contains([1,2,3], [1,2], false)//true
+    * EXAMPLE: ArrayAsserter.contains([1,2,3], [1,2], true)//false, the length of a and b must be the same
     * TODO: create a better description
     * NOTE: one can also use the similar native method: array.contains(obj)
     */
@@ -15,5 +16,4 @@ class ArrayAsserter {
         for (var i:Int = 0 , n:Int = a.count ;i < n ;i++) {if (b.indexOf(a[i]) >= 0 ){ score++ }}
         return score == (strict ? b.count:a.count)
     }
-    
 }
