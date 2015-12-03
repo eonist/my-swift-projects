c6024e186177ad6dc97964650c9f79abcc2a8252
Eonist <unicornhive@gmail.com>
2015-12-03 10:28:56 +0100

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -1,23 +1,19 @@
 import Foundation
 class ArrayAsserter {
     /**
-     *
-     */
-    class func contains<T:Equatable>(array:[T],obj:T)->Bool {
-        
-       return array.contains(obj)
-        //continue here
-        
-        //needs the equatable syntax
-        /*
-        
-        if array.contains(obj) {
-            return true
-        } else {
-            return false;
-        }
-
-        */
-        return false
+    * Asserts if the items in @param a is also present in the array @param b
+    * @param a: an array of items to check if is apart of the @param b
+    * @param b: the array to check against
+    * @param strict: toggle the ability that the items must be equal to the array, (almost the same functionality as the isEqual function, although equal function the array must have the same order)
+    * @return: a boolean indicating if the items is apart of the array
+    * TODO:  write an example
+    * TODO: create a better description
+    * NOTE: one can also use the similar native method: array.contains(obj)
+    */
+    class func contains<T:Equatable>(a:[T], _ b:[T], _ strict:Bool = false)->Bool {
+        var score:Int = 0;
+        for (var i:Int = 0 , n:Int = a.count ;i < n ;i++) {if (b.indexOf(a[i]) >= 0 ){ score++ }}
+        return score == (strict ? b.count:a.count)
     }
+    
 }
