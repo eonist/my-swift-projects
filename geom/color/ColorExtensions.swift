f2eb67ba4c389ea208886473c39e26e2ff169ea8
Eonist <unicornhive@gmail.com>
2015-12-06 09:24:10 +0100

    Files modified: 1
    Modified 1 file:geom/color/ColorExtensions.swift

--- geom/color/ColorExtensions.swift
+++ geom/color/ColorExtensions.swift
@@ -1,5 +1,10 @@
-import Foundation
+import Cocoa
 
-class ColorExtensions {
-
+extension NSColor{
+    /**
+     * EXAMPLE: :NSColor(NSColor.blackColor(),0.5)//outputs a black color with 50% transperancy
+     */
+    convenience init(_ color:NSColor,_ alpha:CGFloat/*0.0 - 1.0*/){
+        self.init(red: color.redComponent, green: color.greenComponent, blue: color.blueComponent, alpha: alpha)
+    }
 }
