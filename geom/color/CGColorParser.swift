422e75638cc004c3ce0eee4878efed292df2d8d8
Eonist <unicornhive@gmail.com>
2015-12-08 13:21:54 +0100

    Files modified: 1
    Modified 1 file:geom/color/CGColorParser.swift

--- geom/color/CGColorParser.swift
+++ geom/color/CGColorParser.swift
@@ -20,5 +20,13 @@ class CGColorParser {
         let nsColor = NSColor(CIColor: ciColor)//convert the ci to ns
         return nsColor
     }
-    
+    /**
+     * NOTE: I think you can also just do: NSColor.redColor().CGColor, which renders this method obsolete
+     * NOTE: This method is nice to have around for reference
+     */
+    class func cgColor(nsColor:NSColor)->CGColor{
+        let ciColor:CIColor = CIColor(color: nsColor)!
+        let cgColor = CIColorParser.cgColor(ciColor)
+        return cgColor
+    }
 }
