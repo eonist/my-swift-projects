de00255d866ca1c84cbb44df8c68df528121fd01
Eonist <unicornhive@gmail.com>
2015-12-08 15:04:13 +0100

    Files modified: 1
    Modified 1 file:math/number/uint/UIntParser.swift

--- math/number/uint/UIntParser.swift
+++ math/number/uint/UIntParser.swift
@@ -5,15 +5,17 @@ class UIntParser {
      * Adds a zero infront of a number
      * // :TODO: Add more advance features to this function, when you have time!
      */
-    class func digit(n:UInt,prefix:String){
+    class func digit(n:UInt)->String{
         
         //continue here: complete this method to also support 3 digits, and continue with the nsColorToHexColor code
         
         //return n < 10 ? "0"+aNr : String(aNr);
-        if(n < 10){
-            prefix + String(n)
-        }else {//(n < 100)
-            prefix + String(n)
+        if(n < 10){//0-9
+            return "0" + String(n)
+        }else if(n >= 10 && n < 100){//10-99
+            return prefix + String(n)
+        }else{
+            return String(n)
         }
     }
 }
