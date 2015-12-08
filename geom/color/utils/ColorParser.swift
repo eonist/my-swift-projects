90aa6644b89ae01f38b31723807f7f88a51b5656
Eonist <unicornhive@gmail.com>
2015-12-08 17:09:45 +0100

    Files modified: 1
    Modified 1 file:geom/color/utils/ColorParser.swift

--- geom/color/utils/ColorParser.swift
+++ geom/color/utils/ColorParser.swift
@@ -2,37 +2,6 @@ import Cocoa
 
 class ColorParser {/*Covers returning hex colors etc*/
     /**
-     *
-     */
-    class func hexColor2(nsColor:NSColor)->String{
-        let rgba = nsColor.rgba
-        /*
-        let rgbString:String = "\(UInt(rgba.r * 255))"
-        Swift.print("rgbString: " + rgbString)
-        let rgbValue = (rgba.r + rgba.g + rgba.b)
-        Swift.print("rgbValue" + "\(rgbValue)")
-        */
-        let tempRGB = StringParser.color("FF0000")
-        Swift.print("tempRGB: " + "\(tempRGB)")
-        
-        let hexValue = String(format:"%X", Int(rgba.r * 255)) + String(format:"%X", Int(rgba.g * 255)) + String(format:"%X", Int(rgba.b * 255))
-        Swift.print("hexValue: " + "\(hexValue)")
-        
-        let stringR = UIntParser.digit(UInt(rgba.r * 255), 3)
-        Swift.print("stringR: " + "\(stringR)")
-        let stringG = UIntParser.digit(UInt(rgba.g * 255), 3)
-        Swift.print("stringG: " + "\(stringG)")
-        let stringB = UIntParser.digit(UInt(rgba.b * 255), 3)
-        Swift.print("stringB: " + "\(stringB)")
-        let temp:String = stringR + stringG + stringB
-        Swift.print("temp: " + "\(temp)")
-        let numericTemp:Double = Double(temp)!
-        let floatTemp:Float = Float(numericTemp)
-        let intTemp:Int = Int(floatTemp)
-        let uintTemp:UInt = UInt(intTemp)
-        return ColorUtils.hexString(uintTemp)
-    }
-    /**
      * Converts an RGB color value into a hexidecimal String representation.
      * @param r: A uint from 0 to 255 representing the red color value.
      * @param g: A uint from 0 to 255 representing the green color value.
@@ -58,5 +27,11 @@ class ColorParser {/*Covers returning hex colors etc*/
     class func rgba(nsColor:NSColor)->(r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat){
         let ciColor:CIColor = CIColor(color: nsColor)!
         return (ciColor.red,ciColor.green,ciColor.blue,ciColor.alpha)
+    }
+}
+
+extension ColorParser{
+    func hexColor(r:CGFloat,g:CGFloat,b:CGFloat)->String{
+        
     }
 }
