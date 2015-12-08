62480599aa666143e44db9c70156e731796eb274
Eonist <unicornhive@gmail.com>
2015-12-08 05:51:43 +0100

    Files modified: 1
    Modified 1 file:geom/context/DropShadow.swift

--- geom/context/DropShadow.swift
+++ geom/context/DropShadow.swift
@@ -5,11 +5,13 @@ class DropShadow {
     var offsetX:CGFloat
     var offsetY:CGFloat
     var blurRadius:CGFloat
-    init(_ color:NSColor,_ offsetX:CGFloat,_ offsetY:CGFloat,_ blurRadius:CGFloat){
+    var inner:Bool
+    init(_ color:NSColor,_ offsetX:CGFloat,_ offsetY:CGFloat,_ blurRadius:CGFloat,inner:Bool){
         self.color = color
         self.offsetX = offsetX
         self.offsetY = offsetY
         self.blurRadius = blurRadius
+        self.inner = inner
     }
 }
 /*
