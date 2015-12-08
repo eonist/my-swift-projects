3829c9b492d1db9daa59ee66a082e2fdbcc3a30c
Eonist <unicornhive@gmail.com>
2015-12-08 15:39:18 +0100

    Files modified: 1
    Modified 1 file:math/number/uint/UIntParser.swift

--- math/number/uint/UIntParser.swift
+++ math/number/uint/UIntParser.swift
@@ -4,6 +4,7 @@ class UIntParser {
     /**
      * Adds a zero infront of a number
      * TODO: refactor this code when you have time
+     * EXAMPLE: Swift.print(UIntParser.digit(9, 2))
      */
     class func digit(n:UInt,_ numOfDigits:Int)->String{
         let stringNum:String = String(n)
