5d2f93deba0855ad454b36bd83bf5f4799bf82e8
Eonist <unicornhive@gmail.com>
2015-12-08 18:29:11 +0100

    Files modified: 1
    Modified 1 file:geom/context/DropShadow.swift

--- geom/context/DropShadow.swift
+++ geom/context/DropShadow.swift
@@ -6,7 +6,7 @@ class DropShadow : CustomStringConvertible{
     var offsetY:CGFloat
     var blurRadius:CGFloat
     var inner:Bool
-    var description: String { get{return "Color: " + String(color) + "\n" + "offsetX: " + String(offsetX) + "\n" + "offsetY: " + String(offsetY)} }
+    var description: String { get{return "Color: " +  + "\n" + "offsetX: " + String(offsetX) + "\n" + "offsetY: " + String(offsetY)} }
     init(_ color:NSColor,_ offsetX:CGFloat,_ offsetY:CGFloat,_ blurRadius:CGFloat,_ inner:Bool){
         self.color = color
         self.offsetX = offsetX
