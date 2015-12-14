9d8aaf04bf3bc1b102ede3ed46ca87d78bd50a4f
Eonist <unicornhive@gmail.com>
2015-12-14 13:34:42 +0100

    Files modified: 1
    Modified 1 file:misc/view/temp/TempNSView.swift

--- misc/view/temp/TempNSView.swift
+++ misc/view/temp/TempNSView.swift
@@ -4,15 +4,13 @@ class TempNSView :FlippedView{
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
-        let trackingArea = NSTrackingArea(rect: self.bounds,
-            options: [NSTrackingAreaOptions.ActiveWhenFirstResponder,NSTrackingAreaOptions.MouseEnteredAndExited],
-            owner: self, userInfo: nil)
-        self.addTrackingArea(trackingArea)
-        //addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
-    }
-
-    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         
+        
+        addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
+    }
+    
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+      override var acceptsFirstResponder: Bool { return true }
     
     override func drawRect(dirtyRect: NSRect) {
         Swift.print("TempNSView.drawRect()")
