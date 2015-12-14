e282a090d3528281e9771a85cbb9cd5d8fb981f7
Eonist <unicornhive@gmail.com>
2015-12-14 13:48:01 +0100

    Files modified: 1
    Modified 1 file:misc/view/temp/TempNSView.swift

--- misc/view/temp/TempNSView.swift
+++ misc/view/temp/TempNSView.swift
@@ -8,7 +8,10 @@ class TempNSView :FlippedView{
         
         addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
     }
-    
+    override func hitTest(aPoint: NSPoint) -> NSView? {
+        Swift.print("hitTest")
+        return self
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
       override var acceptsFirstResponder: Bool { return true }
     
