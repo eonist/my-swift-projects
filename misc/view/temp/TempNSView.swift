ea66f485426707691023e1ac9e05ae2576ed7bd5
Eonist <unicornhive@gmail.com>
2015-12-14 13:11:26 +0100

    Files modified: 1
    Modified 1 file:misc/view/temp/TempNSView.swift

--- misc/view/temp/TempNSView.swift
+++ misc/view/temp/TempNSView.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
 
-class TempNSView {
-
+class TempNSView :FlippedView{
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    override func drawRect(dirtyRect: NSRect) {
+        Swift.print("drawRect()")
+        let graphicsContext = NSGraphicsContext.currentContext()!
+        let context = graphicsContext.CGContext/* Get the handle to the current context */
+        let path = CGRect(0,0,100,100).path
+        CGContextAddPath(context, path)
+        CGContextSetFillColorWithColor(context,NSColor.blueColor().CGColor)
+        CGContextDrawPath(context, CGPathDrawingMode.Fill)
+    }
 }
