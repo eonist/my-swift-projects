497244b33e75138b6849da1c06af67433d25e2e3
Eonist <unicornhive@gmail.com>
2015-12-16 12:50:46 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -17,7 +17,7 @@ class Graphic:FlippedView,IGraphic{
         self.lineStyle = lineStyle
         self.lineOffsetType = lineOffsetType
         super.init(frame:NSRect(0,0,100,100))//<---move this into the arguments
-        //self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
+        self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
         layer?.addSublayer(fillShape)
         layer?.addSublayer(lineShape)
     }
