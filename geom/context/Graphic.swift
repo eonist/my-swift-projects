5e0136661df6858e3dc895579362e09d5a142322
Eonist <unicornhive@gmail.com>
2015-12-16 06:28:03 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -10,18 +10,12 @@ class Graphic:FlippedView{
     var fillStyle:IFillStyle?;
     var lineStyle:ILineStyle?;
     var lineOffsetType:OffsetType?;
-    
-    
-    lazy var graphics: Graphics = Graphics()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
-    var path:CGMutablePath = CGPathCreateMutable()
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
         layer?.addSublayer(fillShape)
-        layer?.addSublayer(lineShape)
-        
-        //continue here, create the CALayer class, should hold a path and the graphics instance
+        layer?.addSublayer(lineShape) 
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
     
