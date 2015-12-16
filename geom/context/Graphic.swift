b4068581c0273c7e001ff415a753b4993acf19db
Eonist <unicornhive@gmail.com>
2015-12-16 05:56:36 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -1,13 +1,16 @@
-import Foundation
+import Cocoa
 /**
  * set needsDisplay = true to clear the graphics
  * TODO: Write an example
  * NOTE: Example is in the Graphics class
  */
 class Graphic:FlippedView{
+    var fillShape:CALayer = CALayer()
+    var lineShape:CALayer = CALayer()
     lazy var graphics: Graphics = Graphics()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
     var path:CGMutablePath = CGPathCreateMutable()
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    
     init() {
         super.init(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
         self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
