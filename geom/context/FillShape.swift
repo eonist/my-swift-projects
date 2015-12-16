d69450925b3f109e024f1c2d19f4d29e6f07691d
Eonist <unicornhive@gmail.com>
2015-12-16 19:16:39 +0100

    Files modified: 2
    Modified 2 files:geom/context/FillShape.swiftgeom/context/LineShape.swift

--- geom/context/FillShape.swift
+++ geom/context/FillShape.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class FillShape :Shape{
-
+    override func drawInContext(ctx: CGContext) {
+        Swift.print("TempShape.drawInContext")
+        graphics.context = ctx
+        self.graphics.fill(fillStyle.color)//Stylize the fill
+        self.graphics.draw(path)//draw everything
+        
+    }
 }
--- geom/context/LineShape.swift
+++ geom/context/LineShape.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class LineShape :Shape{
-
+    var fillStyle:IFillStyle?;
+    var lineStyle:ILineStyle?;
+    override func drawInContext(ctx: CGContext) {
+        Swift.print("TempShape.drawInContext")
+        graphics.context = ctx
+        self.graphics.line(lineStyle!.thickness)//Stylize the line
+        self.graphics.draw(thePath)//draw everything
+    }
 }
