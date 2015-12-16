e912d166e330b528396ea0d440d62c2b18ab7f83
Eonist <unicornhive@gmail.com>
2015-12-16 07:20:28 +0100

    Files modified: 1
    Modified 1 file:geom/context/IGraphic.swift

--- geom/context/IGraphic.swift
+++ geom/context/IGraphic.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class IGraphic {
-
+    var lineStyle:ILineStyle?{get}
+    var fillStyle:IFillStyle?{get}
+    func setProperties(fillStyle:IFillStyle?, lineStyle:ILineStyle?)
+    var lineOffsetType:OffsetType{get set}
+    var path:CGMutablePath{get set}
+    var linePath:CGMutablePath{get set}
 }
