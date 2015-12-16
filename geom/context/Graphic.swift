f27e48f34776a80127dc0e166ed07eca33af4110
Eonist <unicornhive@gmail.com>
2015-12-16 05:58:32 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -10,17 +10,13 @@ class Graphic:FlippedView{
     lazy var graphics: Graphics = Graphics()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
     var path:CGMutablePath = CGPathCreateMutable()
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
-    
-    init() {
-        super.init(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
         self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
     }
-    /**
-     * Required by super class
-     */
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
+        
+    
     /**
      *
      */
