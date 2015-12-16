e49681a48bfb4f66a7031c68676633908030e894
Eonist <unicornhive@gmail.com>
2015-12-16 19:14:36 +0100

    Files modified: 1
    Modified 1 file:geom/context/TempShape.swift

--- geom/context/TempShape.swift
+++ geom/context/TempShape.swift
@@ -4,12 +4,5 @@ class TempShape:Shape{
     override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
         Swift.print("TempShape.drawLayer")
     }
-    override func drawInContext(ctx: CGContext) {
-        Swift.print("TempShape.drawInContext")
-        graphics.context = ctx
-        self.graphics.fill(NSColor.blueColor())//Stylize the fill
-        self.graphics.draw(path)//draw everything
-        self.graphics.line(lineStyle!.thickness)//Stylize the line
-        self.graphics.draw(thePath)//draw everything
-    }
+   
 }
