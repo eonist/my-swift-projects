686b772b558473f8cfb5a94daa68ec780483e1d8
Eonist <unicornhive@gmail.com>
2015-12-17 07:45:14 +0100

    Files modified: 1
    Modified 1 file:geom/context/LineShape.swift

--- geom/context/LineShape.swift
+++ geom/context/LineShape.swift
@@ -5,7 +5,7 @@ class LineShape :Shape{
     override func drawInContext(ctx: CGContext) {
         Swift.print("LineShape.drawInContext")
         graphics.context = ctx
-        self.graphics.line(lineStyle!.thickness,lineStyle!.color)//Stylize the line
+        self.graphics.line(lineStyle!.thickness,lineStyle!.color,lineStyle!.lineCap, lineStyle!.lineJoin, lineStyle!.miterLimit)//Stylize the line
         self.graphics.draw(path)//draw everything
     }
 }
