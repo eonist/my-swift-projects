927029d4f076d281d3e99580d7dcc28a27327916
Eonist <unicornhive@gmail.com>
2015-12-17 07:49:08 +0100

    Files modified: 2
    Modified 2 files:geom/context/FillShape.swiftgeom/context/LineShape.swift

--- geom/context/FillShape.swift
+++ geom/context/FillShape.swift
@@ -6,7 +6,7 @@ class FillShape :Shape{
         
     
     override func drawInContext(ctx: CGContext) {
-        Swift.print("FillShape.drawInContext")
+        //Swift.print("FillShape.drawInContext")
         graphics.context = ctx
         self.graphics.fill(fillStyle!.color)//Stylize the fill
         self.graphics.draw(path)//draw everything
--- geom/context/LineShape.swift
+++ geom/context/LineShape.swift
@@ -3,7 +3,7 @@ import Foundation
 class LineShape :Shape{
     var lineStyle:ILineStyle?;
     override func drawInContext(ctx: CGContext) {
-        Swift.print("LineShape.drawInContext")
+        //Swift.print("LineShape.drawInContext")
         graphics.context = ctx
         self.graphics.line(lineStyle!.thickness,lineStyle!.color,lineStyle!.lineCap, lineStyle!.lineJoin, lineStyle!.miterLimit)//Stylize the line
         self.graphics.draw(path)//draw everything
