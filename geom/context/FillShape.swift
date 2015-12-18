7ffa649c6b0eab1b451d4680ecb583150a866295
Eonist <unicornhive@gmail.com>
2015-12-18 20:18:37 +0100

    Files modified: 1
    Modified 1 file:geom/context/FillShape.swift

--- geom/context/FillShape.swift
+++ geom/context/FillShape.swift
@@ -8,6 +8,9 @@ class FillShape :Shape{
     override func drawInContext(ctx: CGContext) {
         Swift.print("FillShape.drawInContext")
         graphics.context = ctx
+        
+        
+        
         self.graphics.fill(fillStyle!.color)//Stylize the fill
         self.graphics.draw(path)//draw everything
         
