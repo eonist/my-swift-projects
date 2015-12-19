ea7c554f106c7428de2f3050bfc4cdf83e4c00f2
Eonist <unicornhive@gmail.com>
2015-12-19 12:14:47 +0100

    Files modified: 1
    Modified 1 file:geom/context/FillShape.swift

--- geom/context/FillShape.swift
+++ geom/context/FillShape.swift
@@ -5,7 +5,7 @@ class FillShape :Shape{
 
     
     override func drawInContext(ctx: CGContext) {
-        Swift.print("FillShape.drawInContext")
+        //Swift.print("FillShape.drawInContext")
         graphics.context = ctx
         
         //TODO:you only need to call the draw method from here, the fill setting etc can be done in the decoratable classes
