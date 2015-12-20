09c36296450977d9e83bca92b63a9cb065d5fa22
Eonist <unicornhive@gmail.com>
2015-12-20 18:53:54 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -23,8 +23,9 @@ class Graphic:FlippedView,IGraphic{
         super.init(frame:NSRect(0,0,0/*<- was 1*/,0/*<- was 1*/))//<---move this into the arguments/*the width and the height arent clipped*/
         
         //layerContentsRedrawPolicy = NSViewLayerContentsRedrawPolicy.OnSetNeedsDisplay//this is new, but apple recomends it, more about it here: https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/CoreAnimation_guide/SettingUpLayerObjects/SettingUpLayerObjects.html#//apple_ref/doc/uid/TP40004514-CH13-SW4
-        wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
         layer = CALayer()//TempCALayer(layer: layer!)
+        wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
+        
         
         /*layer!.shadowColor = NSColor.blackColor().CGColor;
         layer!.shadowOpacity = 1.0;
