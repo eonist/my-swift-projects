07d32cf477b80f6d36dfa76c2383d3b8a5e7f402
Eonist <unicornhive@gmail.com>
2015-12-20 06:01:45 +0100

    Files modified: 1
    Modified 1 file:geom/path/utils/CGPathExtensions.swift

--- geom/path/utils/CGPathExtensions.swift
+++ geom/path/utils/CGPathExtensions.swift
@@ -1,5 +1,16 @@
 import Foundation
 
-class CGPathExtensions {
-
+extension CGMutablePath{
+    /**
+     * Note its probably better to move this method into the Graphic class since then it can be overriden by subclasses, which you cant do with this extension method, really?
+     */
+    func add(path:CGPath){
+        CGPathAddPath(self, nil, path)
+    }
+    /**
+     * Returns a copy of it self
+     */
+    func copy()->CGMutablePathRef {//possibly return CGMutablePathRef
+        return CGPathCreateMutableCopy(self)!
+    }
 }
