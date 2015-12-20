6a88b8996b1d49b9f5bc616c5ba8bc6a828ec0f5
Eonist <unicornhive@gmail.com>
2015-12-20 08:13:10 +0100

    Files modified: 1
    Modified 1 file:misc/view/InteractiveView.swift

--- misc/view/InteractiveView.swift
+++ misc/view/InteractiveView.swift
@@ -12,11 +12,13 @@ class InteractiveView:FlippedView{
     var hasMouseEntered:Bool = false/*you should hit test this on init*/
     var isInteractive:Bool = true//why is this here?
     var hasHandCursor:Bool = false
-    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
-    override var wantsUpdateLayer:Bool{return false;}
+    //override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    //override var wantsUpdateLayer:Bool{return false;}
     override init(frame:NSRect) {
         super.init(frame:frame)
         self.wantsLayer = true//setting this to false avoids calling drawLayer() and enables drawingRect()
+        layer = CALayer()
+        layer!.masksToBounds = false
         let trackingArea:NSTrackingArea = NSTrackingArea(rect: bounds, options: [NSTrackingAreaOptions.ActiveAlways, NSTrackingAreaOptions.MouseMoved,NSTrackingAreaOptions.MouseEnteredAndExited], owner: self, userInfo: nil)
         addTrackingArea(trackingArea)
     }
