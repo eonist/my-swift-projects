66cdd0362ee21355d9d93fdd19c6be1d5dda8294
Eonist <unicornhive@gmail.com>
2015-12-21 14:28:02 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -4,13 +4,14 @@ public enum GradientType:Int{//TODO:move to its own class?
 }
 /**
  * EXAMPLE: let gradient:Gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor])
+ * TODO: create two Gradient classes the extends a common Gradient class. LinearGradient and RadialGradient, ILinearGradient, IRadialGradient etc
  */
 public class Gradient:IGradient{
     public var colors:Array<CGColor>
     public var locations:Array<CGFloat>
     public var gradientType:GradientType
     public var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix*/
-    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,startPoint:CGPoint = CGPoint(),endPoint:CGPoint = CGPoint()*/
+    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
             //Swift.print(colors.count)
