2454a4bbad2e7b621068153d579950fa45cc49c0
Eonist <unicornhive@gmail.com>
2015-12-21 15:54:43 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -10,16 +10,16 @@ public enum GradientType:Int{//TODO:move to its own class?
  * NOTE: Why are we using realtive values for the radial gradient when xcode supports absolute points? Because absolute values cant be applied to different graphics. Absolute values only work with one exact graphic.
  * NOTE: the scalar values are subjected to change, if -1 to +1 makes more sense when doing the polar point calculations etc
  */
-class Gradient:IGradient{
-    var colors:Array<CGColor>
-    var locations:Array<CGFloat>
-    var gradientType:GradientType
-    var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix, for now its fine*/
-    var relativeStartCenter:CGPoint?//0 to 1 (x:0.5 means half way accross the boundingbox etc) (from the intersection of the normal and an edge in the boundingbox)
-    var relativeEndCenter:CGPoint?//0 to 1 (same as relativeEndCenter)
-    var relativeStartRadius:CGSize?//0 to 1 (x:0.5 means half the size of the current cross-section in the x-axis) the x-axis is determined by the rotation
-    var relativeEndRadius:CGSize?//0 to 1 (same as relativeStartRadius)
-    init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/,_ relativeStartCenter:CGPoint? = nil,_ relativeEndCenter:CGPoint? = nil,_ relativeStartRadius:CGSize? = nil,_ relativeEndRadius:CGSize? = nil){/*,*/
+public class Gradient:IGradient{
+    public var colors:Array<CGColor>
+    public var locations:Array<CGFloat>
+    public  gradientType:GradientType
+    public var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix, for now its fine*/
+    public var relativeStartCenter:CGPoint?//0 to 1 (x:0.5 means half way accross the boundingbox etc) (from the intersection of the normal and an edge in the boundingbox)
+    public var relativeEndCenter:CGPoint?//0 to 1 (same as relativeEndCenter)
+    public var relativeStartRadius:CGSize?//0 to 1 (x:0.5 means half the size of the current cross-section in the x-axis) the x-axis is determined by the rotation
+    public var relativeEndRadius:CGSize?//0 to 1 (same as relativeStartRadius)
+    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/,_ relativeStartCenter:CGPoint? = nil,_ relativeEndCenter:CGPoint? = nil,_ relativeStartRadius:CGSize? = nil,_ relativeEndRadius:CGSize? = nil){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
             //Swift.print(colors.count)
