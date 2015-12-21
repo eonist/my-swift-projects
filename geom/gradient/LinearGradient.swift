ebf0cd357f3acb2d446febcedd3488f05f4c6d1d
Eonist <unicornhive@gmail.com>
2015-12-21 15:34:58 +0100

    Files modified: 2
    Modified 2 files:geom/gradient/LinearGradient.swiftgeom/gradient/RadialGradient.swift

--- geom/gradient/LinearGradient.swift
+++ geom/gradient/LinearGradient.swift
@@ -1,11 +1,4 @@
 import Foundation
 
 class LinearGradient :Gradient2{
-    /**
-     *
-     */
-    override init(_ colors: Array<CGColor>, _ locations: Array<CGFloat>, _ rotation: CGFloat) {
-        super.init(colors, locations, rotation)
-        
-    }
 }
