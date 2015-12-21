11ef6a0d78673b07cdde40c5382321105725360b
Eonist <unicornhive@gmail.com>
2015-12-21 14:43:45 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -11,8 +11,10 @@ public class Gradient:IGradient{
     public var locations:Array<CGFloat>
     public var gradientType:GradientType
     public var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix*/
-    public var relativeStartCenter:CGPoint?//0 to 1 (0 means )
-    public var relativeEndCenter:CGPoint?
+    public var relativeStartCenter:CGPoint?//0 to 1 (x:0.5 means half way accross the boundingbox etc)
+    public var relativeEndCenter:CGPoint?//0 to 1
+    public var relativeStartRadius:CGSize?//0 to 1 (x:0.5)
+    public var relativeEndRadius:CGSize?//0 to 1
     public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
