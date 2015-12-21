3d6a898f5e6216f2ddec3695ab601e848042fd68
Eonist <unicornhive@gmail.com>
2015-12-21 15:36:58 +0100

    Files modified: 1, Files deleted: 3
    Deleted 3 files: geom/gradient/Gradient2.swift geom/gradient/LinearGradient.swift geom/gradient/RadialGradient.swiftModified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -5,13 +5,19 @@ public enum GradientType:Int{//TODO:move to its own class?
 /**
  * The overall goal of this class is to provide a universal way to define gradients on a graphic. IMPORTANT ->>> So that if you scale the graphic the gradient will not look strange. <<<- IMPORTANT
  * EXAMPLE: let gradient:Gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor])
+ * TODO: create two Gradient classes the extends a common Gradient class. LinearGradient and RadialGradient, ILinearGradient, IRadialGradient etc
+ * NOTE: the radial scalar values can also be more or less than 0 and 1. think width * 1.2 etc. Works for both pos and size. (the only thing that is capped is the size if it goes under 0, then it is capped to 0)
+ * NOTE: Why are we using realtive values for the radial gradient when xcode supports absolute points? Because absolute values cant be applied to different graphics. Absolute values only work with one exact graphic.
  */
 class Gradient:IGradient{
     var colors:Array<CGColor>
     var locations:Array<CGFloat>
     var gradientType:GradientType
     var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix, for now its fine*/
-    
+    var relativeStartCenter:CGPoint//0 to 1 (x:0.5 means half way accross the boundingbox etc) (from the intersection of the normal and an edge in the boundingbox)
+    var relativeEndCenter:CGPoint?//0 to 1 (same as relativeEndCenter)
+    var relativeStartRadius:CGSize?//0 to 1 (x:0.5 means half the size of the current cross-section in the x-axis) the x-axis is determined by the rotation
+    var relativeEndRadius:CGSize?//0 to 1 (same as relativeStartRadius)
     init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
