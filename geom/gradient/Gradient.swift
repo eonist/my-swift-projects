8d5f7d422277cb1a344c2c1caaab2579fd9acfbe
Eonist <unicornhive@gmail.com>
2015-12-21 15:01:01 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -5,6 +5,7 @@ public enum GradientType:Int{//TODO:move to its own class?
 /**
  * EXAMPLE: let gradient:Gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor])
  * TODO: create two Gradient classes the extends a common Gradient class. LinearGradient and RadialGradient, ILinearGradient, IRadialGradient etc
+ * The overall goal of this class is to provide a universal way to define gradients on a graphic. IMPORTANT ->>> So that if you scale the graphic the gradient will not look strange. <<<- IMPORTANT 
  */
 public class Gradient:IGradient{
     public var colors:Array<CGColor>
@@ -13,7 +14,7 @@ public class Gradient:IGradient{
     public var rotation:CGFloat;/*this doesnt belong here, you apply rotations in the matrix*/
     public var relativeStartCenter:CGPoint?//0 to 1 (x:0.5 means half way accross the boundingbox etc) (from the intersection of the normal and an edge in the boundingbox)
     public var relativeEndCenter:CGPoint?//0 to 1
-    public var relativeStartRadius:CGSize?//0 to 1 (x:0.5 means half the size of the current cross-section of ) (the radius of a rectangle is defined from the center to the topleft corner)
+    public var relativeStartRadius:CGSize?//0 to 1 (x:0.5 means half the size of the current cross-section in the x-axis) the x-axis is determined by the rotation
     public var relativeEndRadius:CGSize?//0 to 1
     public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Axial,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,*/
         self.colors = colors
