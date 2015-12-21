166f8ae854b698164e110f3e475c6701a4c16a36
Eonist <unicornhive@gmail.com>
2015-12-21 15:14:30 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:geom/gradient/RadialGradient.swift

--- geom/gradient/RadialGradient.swift
+++ geom/gradient/RadialGradient.swift
@@ -1,9 +1,9 @@
 import Foundation
-/*
-* TODO: create two Gradient classes the extends a common Gradient class. LinearGradient and RadialGradient, ILinearGradient, IRadialGradient etc
-* NOTE: the radial scalar values can also be more or less than 0 and 1. think width * 1.2 etc. Works for both pos and size. (the only thing that is capped is the size if it goes under 0, then it is capped to 0)
-* NOTE: Why are we using realtive values for the radial gradient when xcode supports absolute points? Because absolute values cant be applied to different graphics. Absolute values only work with one exact graphic.
-*/
+/**
+ * TODO: create two Gradient classes the extends a common Gradient class. LinearGradient and RadialGradient, ILinearGradient, IRadialGradient etc
+ * NOTE: the radial scalar values can also be more or less than 0 and 1. think width * 1.2 etc. Works for both pos and size. (the only thing that is capped is the size if it goes under 0, then it is capped to 0)
+ * NOTE: Why are we using realtive values for the radial gradient when xcode supports absolute points? Because absolute values cant be applied to different graphics. Absolute values only work with one exact graphic.
+ */
 class RadialGradient {
     var relativeStartCenter:CGPoint?//0 to 1 (x:0.5 means half way accross the boundingbox etc) (from the intersection of the normal and an edge in the boundingbox)
     var relativeEndCenter:CGPoint?//0 to 1 (same as relativeEndCenter)
