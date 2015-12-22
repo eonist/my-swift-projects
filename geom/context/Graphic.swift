0a18041aca30a5f5e88380e30f6a06b43c70b06c
Eonist <unicornhive@gmail.com>
2015-12-22 06:14:11 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -1,7 +1,6 @@
 import Cocoa
 import QuartzCore
 /**
- * set needsDisplay = true to clear the graphics
  * TODO: Write an example
  * NOTE: Example is in the Graphics class
  * NOTE: you can set the position by calling: graphic.frame.origin = CGPoint()
@@ -18,21 +17,13 @@ class Graphic:FlippedView,IGraphic{
         //Swift.print("Graphic.init()")
         self.fillStyle = fillStyle
         self.lineStyle = lineStyle
-        
         self.lineOffsetType = lineOffsetType
         super.init(frame:NSRect(0,0,0/*<- was 1*/,0/*<- was 1*/))//<---move this into the arguments/*the width and the height arent clipped*/
-        
         //layerContentsRedrawPolicy = NSViewLayerContentsRedrawPolicy.OnSetNeedsDisplay//this is new, but apple recomends it, more about it here: https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/CoreAnimation_guide/SettingUpLayerObjects/SettingUpLayerObjects.html#//apple_ref/doc/uid/TP40004514-CH13-SW4
         wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
         layer = CALayer()//TempCALayer(layer: layer!)
-        //layer!.frame = NSRect(0,0,1,1)
-        //layer?.display()
-        //layer?.needsLayout()
-        
-        
         layer!.masksToBounds = false//this is needed!!!
         layer?.addSublayer(fillShape)
-        //fillShape.delegate = self
         layer?.addSublayer(lineShape)
     }
     /**
@@ -50,28 +41,6 @@ class Graphic:FlippedView,IGraphic{
             lineShape.graphics.drawLine(lineShape.path)
         }
     }
-    /**
-     * If you do not implement this method, the layer calls the drawLayer:inContext: method instead.
-     * NOTE: you can probably derive the cgcontext from inside this method. by utilizing the graphics.contect etc. no you cant
-     */
-    /*
-     override func  displayLayer(layer: CALayer){
-        Swift.print("Graphic.displayLayer: " + "\(layer)")
-     }
-    */
-    /*
-    override func updateLayer(){
-        Swift.print("Graphic.updateLayer()")
-    }
-    override func layout(){
-        Swift.print("Graphic.layout()")
-    }
-     */
-    /*
-    override init(frame frameRect: NSRect) {
-    super.init(frame: frameRect)
-    }
-    */
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
     /**
      * Convenince implicit setter
