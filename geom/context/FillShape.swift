5d0481b4b51ff4a02a2d4a29a5e07f33f3487918
Eonist <unicornhive@gmail.com>
2015-12-22 07:36:29 +0100

    Files modified: 1
    Modified 1 file:geom/context/FillShape.swift

--- geom/context/FillShape.swift
+++ geom/context/FillShape.swift
@@ -6,5 +6,13 @@ class FillShape :Shape{
         super.init()
         self.fillStyle = fillStyle
     }
+    override func drawInContext(ctx: CGContext) {
+        if(fillStyle != nil){
+            self.graphics.context = ctx
+            self.graphics.fill(fillStyle!.color)
+            //fillShape.graphics.gradientFill((a.fillStyle as! GradientFillStyle).gradient)
+            self.graphics.drawFill(self.path)
+        }
+    }
     required init?(coder aDecoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
