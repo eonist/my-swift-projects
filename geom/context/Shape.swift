6c206eb0e3bf1209c59b9811ab3e369297f4f245
Eonist <unicornhive@gmail.com>
2015-12-22 07:18:56 +0100

    Files modified: 1
    Modified 1 file:geom/context/Shape.swift

--- geom/context/Shape.swift
+++ geom/context/Shape.swift
@@ -5,7 +5,9 @@ import Cocoa
 class Shape : CALayer{
     lazy var graphics: Graphics = Graphics()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
     var path:CGMutablePath = CGPathCreateMutable()
-    
+    /**
+     * drawInContext is the method that gets called after a setNeedsDisplay() is called
+     */
     /*
     override func drawInContext(ctx: CGContext) {
         Swift.print("Shape.drawInContext()")
