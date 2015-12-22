cdb2cf1add1d3104d191a9851be7392493adedd0
Eonist <unicornhive@gmail.com>
2015-12-22 06:25:52 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -26,22 +26,7 @@ class Graphic:FlippedView,IGraphic{
         layer?.addSublayer(fillShape)
         layer?.addSublayer(lineShape)
     }
-    /**
-     * NOTE: if you set the delegate then this method wont be called on graphic.setNeedsDisplay()
-     * TODO: Maybe create the LineShape and FillShape again and add this method and the FillStyle LineStyle to them. 
-     */
-    override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
-        fillShape.graphics.context = ctx
-        if(fillStyle != nil){
-            fillShape.graphics.fill(fillStyle!.color)
-            //fillShape.graphics.gradientFill((a.fillStyle as! GradientFillStyle).gradient)
-            fillShape.graphics.drawFill(fillShape.path)
-        }
-        if(lineStyle != nil){
-            lineShape.graphics.line(lineStyle!.thickness, lineStyle!.color, lineStyle!.lineCap, lineStyle!.lineJoin, lineStyle!.miterLimit)
-            lineShape.graphics.drawLine(lineShape.path)
-        }
-    }
+    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
     /**
      * Convenince implicit setter
