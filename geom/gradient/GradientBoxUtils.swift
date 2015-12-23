e1737a529949de86b59fd789a5650abfadd0234f
Eonist <unicornhive@gmail.com>
2015-12-23 15:57:42 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/GradientBoxUtils.swift

--- geom/gradient/GradientBoxUtils.swift
+++ geom/gradient/GradientBoxUtils.swift
@@ -9,21 +9,21 @@ class GradientBoxUtils{
         //Swift.print("GradientBoxUtils.angle: " + "\(angle)")
         var cornerPoint:CGPoint = CGPoint()
         switch true{
-        case CGFloatRangeAsserter.within(Trig.tl, angle):
-            //Swift.print("Q1")
-            cornerPoint = rect.topLeft
-        case CGFloatRangeAsserter.within(Trig.tr, angle):
-            //Swift.print("Q2")
-            cornerPoint = rect.topRight
-        case CGFloatRangeAsserter.within(Trig.br, angle):
-            //Swift.print("Q3")
-            cornerPoint = rect.bottomRight
-        case CGFloatRangeAsserter.within(Trig.bl, angle):
-            //Swift.print("Q4")
-            cornerPoint = rect.bottomLeft
-        default:
-            fatalError("Angle is out of the allowed range (-π to π): " + "\(angle)")
-            break;
+            case CGFloatRangeAsserter.within(Trig.tl, angle):
+                //Swift.print("Q1")
+                cornerPoint = rect.topLeft
+            case CGFloatRangeAsserter.within(Trig.tr, angle):
+                //Swift.print("Q2")
+                cornerPoint = rect.topRight
+            case CGFloatRangeAsserter.within(Trig.br, angle):
+                //Swift.print("Q3")
+                cornerPoint = rect.bottomRight
+            case CGFloatRangeAsserter.within(Trig.bl, angle):
+                //Swift.print("Q4")
+                cornerPoint = rect.bottomLeft
+            default:
+                fatalError("Angle is out of the allowed range (-π to π): " + "\(angle)")
+                break;
         }
         let distPoint = PointParser.directionalAxisDistance(rect.center, cornerPoint, angle)
         //Swift.print("distPoint: " + String(distPoint))
