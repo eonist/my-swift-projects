e0458c1f14193388716eb0e010af72c8846077bc
Eonist <unicornhive@gmail.com>
2015-12-23 08:39:25 +0100

    Files modified: 1
    Modified 1 file:misc/layout/primitives/fillet/Fillet.swift

--- misc/layout/primitives/fillet/Fillet.swift
+++ misc/layout/primitives/fillet/Fillet.swift
@@ -10,13 +10,16 @@ class Fillet:ILayout {
     var bottomLeft:CGFloat;
     var bottomRight:CGFloat;
     required init(_ args:Any...) {//TODO:you may need to add the _ char infront of args
+        
+    }
+    init(_ args:Array<CGFloat>){
         switch(args.count){
-				case 0: topLeft = 0; topRight = 0; bottomLeft = 0; bottomRight = 0; break;
-				case 1: topLeft = args[0] as! CGFloat;topRight = args[0] as! CGFloat;bottomLeft = args[0] as! CGFloat;bottomRight = args[0] as! CGFloat; break;
-				case 2: topLeft = args[0] as! CGFloat; topRight = args[0] as! CGFloat;bottomLeft = args[1] as! CGFloat; bottomRight = args[1] as! CGFloat; break;
-				case 3: topLeft = args[0] as! CGFloat;topRight = args[1] as! CGFloat;bottomLeft = args[2] as! CGFloat;bottomRight = 0; break;
-				case 4: topLeft = args[0] as! CGFloat;topRight = args[1] as! CGFloat;bottomLeft = args[2] as! CGFloat;bottomRight = args[3] as! CGFloat; break;
-				default: fatalError("Argument length not supported: " + "\(args.count)");
+            case 0: topLeft = 0; topRight = 0; bottomLeft = 0; bottomRight = 0; break;
+            case 1: topLeft = args[0];topRight = args[0];bottomLeft = args[0];bottomRight = args[0]; break;
+            case 2: topLeft = args[0]; topRight = args[0];bottomLeft = args[1]; bottomRight = args[1]; break;
+            case 3: topLeft = args[0];topRight = args[1];bottomLeft = args[2];bottomRight = 0; break;
+            case 4: topLeft = args[0];topRight = args[1];bottomLeft = args[2];bottomRight = args[3]; break;
+            default: fatalError("Argument length not supported: " + "\(args.count)");
         }
     }
 }
