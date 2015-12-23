b940f164ab99897d1c790cfded83391f3c754e48
Eonist <unicornhive@gmail.com>
2015-12-23 15:51:50 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -222,7 +222,7 @@ private class Utils{
         let startRadius:CGFloat = min( ((boundingBox.size.width/2.0)/* - 2.0*/),((boundingBox.size.height/2.0)/* - 2.0*/) )
         let endCenter:CGPoint = boundingBox.bottom// + CGPoint(0,100)
         let endRadius:CGFloat = 0.0
-        CGContextDrawRadialGradient(context, cgGradient, startCenter, startRadius, endCenter, endRadius, [CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation])//CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
+        CGContextDrawRadialGradient(context, cgGradient, startCenter, startRadius, endCenter, endRadius, [])//CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
     }
 }
 
