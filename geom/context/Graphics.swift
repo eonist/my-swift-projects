159438a27e0946df22fa3dba04c8175221783133
Eonist <unicornhive@gmail.com>
2015-12-23 15:49:52 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -218,10 +218,10 @@ private class Utils{
         
         //continue here think, draw in illustrator etc
         
-        let startCenter:CGPoint = boundingBox.top//NSMakePoint(NSMidX(boundingBox), NSMidY(boundingBox))
-        let startRadius:CGFloat = min( ((boundingBox.size.width/2.0)),((boundingBox.size.height/2.0) ) )
+        let startCenter:CGPoint = NSMakePoint(NSMidX(boundingBox), NSMidY(boundingBox))
+        let startRadius:CGFloat = min( ((boundingBox.size.width/2.0)/* - 2.0*/),((boundingBox.size.height/2.0)/* - 2.0*/) )
         let endCenter:CGPoint = boundingBox.bottom// + CGPoint(0,100)
-        let endRadius:CGFloat = 20
+        let endRadius:CGFloat = 0.0
         CGContextDrawRadialGradient(context, cgGradient, startCenter, startRadius, endCenter, endRadius, [CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation])//CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
     }
 }
