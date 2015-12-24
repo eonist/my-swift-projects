4fb0a7b6714066f4d5b62cfa4b6f3d61257a191b
Eonist <unicornhive@gmail.com>
2015-12-24 11:49:43 +0100

    Files modified: 1
    Modified 1 file:geom/context/utils/CGAffineTransformExtensions.swift

--- geom/context/utils/CGAffineTransformExtensions.swift
+++ geom/context/utils/CGAffineTransformExtensions.swift
@@ -2,6 +2,12 @@ import Foundation
 
 extension CGAffineTransform {
     /**
+     *
+     */
+    mutating func rotate(rotation:CGFloat){
+        self = CGAffineTransformRotate(self, rotation)
+    }
+    /**
      * Returns a matrix that you can get an objects position clockwise from the pivot, can also futher be manipulated if the input matrix has variables.
      * NOTE: The equivelent code in Matrix Trasform form: //let transform:CGAffineTransform = CGAffineTransformMake(cos(a),sin(a),-sin(a),cos(a),x - x * cos(a)+y * sin(a),y - x * sin(a) - y * cos(a))
      * // :TODO: we could return the matrix for method chaining
@@ -9,7 +15,7 @@ extension CGAffineTransform {
      */
     static func rotateAroundExternalPoint(inout transform:CGAffineTransform,_ pivot:CGPoint, _ rotation:CGFloat){
         transform = CGAffineTransformTranslate(transform, pivot.x, pivot.y)/*<-this looks strage, but you sort of set the point here*/
-        transform = CGAffineTransformRotate(transform, rotation);
+        transform.rotate(rotation)// = CGAffineTransformRotate(transform, rotation);
         transform = CGAffineTransformTranslate(transform,-pivot.x,-pivot.y)/*then you reset the offset to the original position*/
     }
     /**
