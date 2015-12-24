1ddd64a808756f2047785ee36221c8de45729265
Eonist <unicornhive@gmail.com>
2015-12-24 12:41:20 +0100

    Files modified: 1
    Modified 1 file:geom/context/utils/CGAffineTransformExtensions.swift

--- geom/context/utils/CGAffineTransformExtensions.swift
+++ geom/context/utils/CGAffineTransformExtensions.swift
@@ -31,18 +31,20 @@ extension CGAffineTransform {
      * // :TODO: we could return the matrix for method chaining
      * // :TODO: rename to just rotate? for simplicity?
      */
-    static func rotateAroundPoint(inout transform:CGAffineTransform,_ rotation:CGFloat,_ pivot:CGPoint){
+    static func rotateAroundPoint(inout transform:CGAffineTransform,_ rotation:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
         transform.translate(pivot.x, pivot.y)/*<-this looks strage, but you sort of set the point here*/
         transform.rotate(rotation)// = CGAffineTransformRotate(transform, rotation);
         transform.translate(-pivot.x,-pivot.y)/*then you reset the offset to the original position*/
+        return transform
     }
     /**
      *
      */
-    static func scaleFromPoint(inout transform:CGAffineTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint){
+    static func scaleFromPoint(inout transform:CGAffineTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
         transform.translate(pivot.x, pivot.y)/*<-this looks strage, but you sort of set the point here*/
         transform.scale(xScale,yScale)// = CGAffineTransformRotate(transform, rotation);
         transform.translate(-pivot.x,-pivot.y)/*then you reset the offset to the original position*/
+        return transform
     }
     /**
      * NOTE: The result may vary if you change the order of how the translations are applied. This method does not work if you need to rotate and scale around a point for instance, then you need to change the order
