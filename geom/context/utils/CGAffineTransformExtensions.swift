2c5266e8439c07f7166bb7f954c197ddecc176ae
Eonist <unicornhive@gmail.com>
2015-12-24 11:23:00 +0100

    Files modified: 1
    Modified 1 file:geom/context/utils/CGAffineTransformExtensions.swift

--- geom/context/utils/CGAffineTransformExtensions.swift
+++ geom/context/utils/CGAffineTransformExtensions.swift
@@ -2,7 +2,9 @@ import Foundation
 
 extension CGAffineTransform {
     /**
-     *
+     * Returns a matrix that you can get an objects position clockwise from the pivot, can also futher be manipulated if the input matrix has variables.
+     * // :TODO: we could return the matrix for method chaining
+     * // :TODO: rename to just rotate? for simplicity?
      */
     static func rotateAroundExternalPoint(transform:CGAffineTransform, pivot:CGPoint, rotation:CGFloat){
         transform
