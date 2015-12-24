497160f4b54d2d03438316c7afb7eb498d1eed62
Eonist <unicornhive@gmail.com>
2015-12-24 12:16:34 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -278,7 +278,7 @@ private class Utils{
         */
         var transform:CGAffineTransform = CGAffineTransformIdentity//CGAffineTransformMakeTranslation(x, y);
         //CGAffineTransform.rotateAroundExternalPoint(&transform, CGPoint(x,y), a)
-        CGAffineTransform.transformWithPivot(&transform, CGPoint(1.2,1), a, CGPoint(0,0), CGPoint(boundingBox.width/2,boundingBox.height/2))
+        CGAffineTransform.transformWithPivot(&transform, CGPoint(1.5,1), a, CGPoint(0,0), CGPoint(boundingBox.width/2,boundingBox.height/2))
         //let transform:CGAffineTransform = CGAffineTransformMake(cos(a),sin(a),-sin(a),cos(a),x - x * cos(a)+y * sin(a),y - x * sin(a) - y * cos(a))
         CGContextConcatCTM(context, transform)
         //CGContextSaveGState(context)
