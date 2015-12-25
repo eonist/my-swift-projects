921456cf186626432714d60293e371b8e2363906
Eonist <unicornhive@gmail.com>
2015-12-25 10:04:57 +0100

    Files modified: 2
    Modified 2 files:geom/color/extensions/CGColorExtensions.swiftgeom/gradient/Gradients.swift

--- geom/color/extensions/CGColorExtensions.swift
+++ geom/color/extensions/CGColorExtensions.swift
@@ -4,4 +4,10 @@ extension CGColor {
     static func color(hexColor:UInt, _ alpha: CGFloat = 1.0)->CGColor{
         return NSColorParser.cgColor(hexColor,alpha)
     }
+    /**
+     * EXAMPLE: NSColor.redColor().cgColor.alpha(0.5)//Output: a black color with 50% transparancy
+     */
+    func alpha(alpha:CGFloat)->CGColor{
+        return CGColorCreateCopyWithAlpha(self,alpha)!
+    }
 }
