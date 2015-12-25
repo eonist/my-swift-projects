c7b12b167d85c1c2f1ba9f5a582f1f7b8d55271a
Eonist <unicornhive@gmail.com>
2015-12-25 09:53:25 +0100

    Files modified: 1
    Modified 1 file:geom/color/Colors.swift

--- geom/color/Colors.swift
+++ geom/color/Colors.swift
@@ -192,14 +192,14 @@ extension Colors{
 }
 /*FANCY IOS Colors - NSColor:*/
 extension Colors{
-    static var red:NSColor = NSColorParser.nsColor(Colors.red)
-    static var orange:NSColor = NSColorParser.nsColor(Colors.orange)
-    static var yellow:NSColor = NSColorParser.nsColor(Colors.yellow)
-    static var green:NSColor = NSColorParser.nsColor(Colors.green)
-    static var lightBlue:NSColor = NSColorParser.nsColor(Colors.lightBlue)
-    static var darkBlue:NSColor = NSColorParser.nsColor(Colors.darkBlue)
-    static var purple:NSColor = NSColorParser.nsColor(Colors.purple)
-    static var pink:NSColor = NSColorParser.nsColor(Colors.pink)
-    static var darkGray:NSColor = NSColorParser.nsColor(Colors.darkGray)
-    static var lightGray:NSColor = NSColorParser.nsColor(Colors.red)
+    static var red:NSColor = NSColorParser.nsColor(Colors.red())
+    static var orange:NSColor = NSColorParser.nsColor(Colors.orange())
+    static var yellow:NSColor = NSColorParser.nsColor(Colors.yellow())
+    static var green:NSColor = NSColorParser.nsColor(Colors.green())
+    static var lightBlue:NSColor = NSColorParser.nsColor(Colors.lightBlue())
+    static var darkBlue:NSColor = NSColorParser.nsColor(Colors.darkBlue())
+    static var purple:NSColor = NSColorParser.nsColor(Colors.purple())
+    static var pink:NSColor = NSColorParser.nsColor(Colors.pink())
+    static var darkGray:NSColor = NSColorParser.nsColor(Colors.darkGray())
+    static var lightGray:NSColor = NSColorParser.nsColor(Colors.red())
 }
