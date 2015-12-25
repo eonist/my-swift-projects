f73a9f6665958fbe14781c974031f22d620de716
Eonist <unicornhive@gmail.com>
2015-12-25 08:33:11 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradients.swift

--- geom/gradient/Gradients.swift
+++ geom/gradient/Gradients.swift
@@ -3,30 +3,33 @@ import Cocoa
 class Gradients{
     static var redStart:UInt = 0xFF5E3A
     static var redEnd:UInt = 0xFF2A68
-    static var greenGradientStartColor:UInt = 0x87FC70
-    static var greenGradientEndColor:UInt = 0x0BD318
-    static var orangeGradientStartColor:UInt = 0xFF9500
-    static var orangeGradientEndColor:UInt = 0xFF5E3A
-    static var yellowGradientStartColor:UInt = 0xFFDB4C
-    static var yellowGradientEndColor:UInt = 0xFFCD02
-    static var tealGradientStartColor:UInt = 0x52EDC7
-    static var tealGradientEndColor:UInt = 0x5AC8FB
-    static var blueGradientStartColor:UInt = 0x1AD6FD
-    static var blueGradientEndColor:UInt = 0x1D62F0
-    static var violetGradientStartColor:UInt = 0xC644FC
-    static var violetGradientEndColor:UInt = 0x5856D6
-    static var magentaGradientStartColor:UInt = 0xEF4DB6
-    static var magentaGradientEndColor:UInt = 0xC643FC
-    static var blackGradientStartColor:UInt = 0x4A4A4A
-    static var blackGradientEndColor:UInt = 0x2B2B2B
-    static var silverGradientStartColor:UInt = 0xDBDDDE
-    static var silverGradientEndColor:UInt = 0x898C90
+    static var greenStart:UInt = 0x87FC70
+    static var greenEnd:UInt = 0x0BD318
+    static var orangeStart:UInt = 0xFF9500
+    static var orangeEnd:UInt = 0xFF5E3A
+    static var yellowStart:UInt = 0xFFDB4C
+    static var yellowEnd:UInt = 0xFFCD02
+    static var tealStart:UInt = 0x52EDC7
+    static var tealEnd:UInt = 0x5AC8FB
+    static var blueStart:UInt = 0x1AD6FD
+    static var blueEnd:UInt = 0x1D62F0
+    static var violetStart:UInt = 0xC644FC
+    static var violetEnd:UInt = 0x5856D6
+    static var magentaStart:UInt = 0xEF4DB6
+    static var magentaEnd:UInt = 0xC643FC
+    static var blackStart:UInt = 0x4A4A4A
+    static var blackEnd:UInt = 0x2B2B2B
+    static var silverStart:UInt = 0xDBDDDE
+    static var silverEnd:UInt = 0x898C90
 }
 extension Gradients{
-    static func red()->(start:NSColor,end:NSColor){return (NSColorParser.nsColor(redGradientStartColor),NSColorParser.nsColor(redGradientEndColor))}
+    //static func red()->(start:NSColor,end:NSColor){return (NSColorParser.nsColor(redStart),NSColorParser.nsColor(redEnd))}
         
     
-    static func red()->Array<CGColor>{return [NSColorParser.cgColor(<#T##hexColor: UInt##UInt#>)]}
+    static func red()->Array<CGColor>{
+        let c = NSColorParser.cgColor(Gradients.red)
+        return [c]
+    }
         
     
     
