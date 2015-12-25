4733dd022dd1a2f9a707a516774815bb58842a3e
Eonist <unicornhive@gmail.com>
2015-12-25 06:06:48 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradients.swift

--- geom/gradient/Gradients.swift
+++ geom/gradient/Gradients.swift
@@ -9,84 +9,16 @@ class Gradients{
     static var iOS8orangeGradientEndColor:UInt = 0xFF5E3A
     static var iOS8yellowGradientStartColor:UInt = 0xFFDB4C
     static var iOS8yellowGradientEndColor:UInt = 0xFFCD02
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
-    static var :UInt = 0x
+    static var iOS8tealGradientStartColor:UInt = 0x52EDC7
+    static var iOS8tealGradientEndColor:UInt = 0x5AC8FB
+    static var iOS8blueGradientStartColor:UInt = 0x1AD6FD
+    static var iOS8blueGradientEndColor:UInt = 0x1D62F0
+    static var iOS8violetGradientStartColor:UInt = 0xC644FC
+    static var iOS8violetGradientEndColor:UInt = 0x5856D6
+    static var iOS8magentaGradientStartColor:UInt = 0xEF4DB6
+    static var iOS8magentaGradientEndColor:UInt = 0xC643FC
+    static var iOS8blackGradientStartColor:UInt = 0x4A4A4A
+    static var iOS8blackGradientEndColor:UInt = 0x2B2B2B
+    static var iOS8silverGradientStartColor:UInt = 0xDBDDDE
+    static var iOS8silverGradientEndColor:UInt = 0x898C90
 }
-/*
-
-+ (instancetype)iOS8tealGradientStartColor;
-{
-return [UIColor colorWithHexString:@"52EDC7"];
-}
-
-+ (instancetype)iOS8tealGradientEndColor;
-{
-return [UIColor colorWithHexString:@"5AC8FB"];
-}
-
-+ (instancetype)iOS8blueGradientStartColor;
-{
-return [UIColor colorWithHexString:@"1AD6FD"];
-}
-
-+ (instancetype)iOS8blueGradientEndColor;
-{
-return [UIColor colorWithHexString:@"1D62F0"];
-}
-
-+ (instancetype)iOS8violetGradientStartColor;
-{
-return [UIColor colorWithHexString:@"C644FC"];
-}
-
-+ (instancetype)iOS8violetGradientEndColor;
-{
-return [UIColor colorWithHexString:@"5856D6"];
-}
-
-+ (instancetype)iOS8magentaGradientStartColor;
-{
-return [UIColor colorWithHexString:@"EF4DB6"];
-}
-
-+ (instancetype)iOS8magentaGradientEndColor;
-{
-return [UIColor colorWithHexString:@"C643FC"];
-}
-
-+ (instancetype)iOS8blackGradientStartColor;
-{
-return [UIColor colorWithHexString:@"4A4A4A"];
-}
-
-+ (instancetype)iOS8blackGradientEndColor;
-{
-return [UIColor colorWithHexString:@"2B2B2B"];
-}
-
-+ (instancetype)iOS8silverGradientStartColor;
-{
-return [UIColor colorWithHexString:@"DBDDDE"];
-}
-
-+ (instancetype)iOS8silverGradientEndColor;
-{
-return [UIColor colorWithHexString:@"898C90"];
-}
-*/
