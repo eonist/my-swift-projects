8aea71dc23f03a8e5b8f76988c1b73de7f92dd71
Eonist <unicornhive@gmail.com>
2015-12-25 06:10:37 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradients.swift

--- geom/gradient/Gradients.swift
+++ geom/gradient/Gradients.swift
@@ -1,24 +1,24 @@
-//FANCY IOS8 Gradients:
+//FANCY  Gradients:
 
 class Gradients{
-    static var iOS8redGradientStartColor:UInt = 0xFF5E3A
-    static var iOS8redGradientEndColor:UInt = 0xFF2A68
-    static var iOS8greenGradientStartColor:UInt = 0x87FC70
-    static var iOS8greenGradientEndColor:UInt = 0x0BD318
-    static var iOS8orangeGradientStartColor:UInt = 0xFF9500
-    static var iOS8orangeGradientEndColor:UInt = 0xFF5E3A
-    static var iOS8yellowGradientStartColor:UInt = 0xFFDB4C
-    static var iOS8yellowGradientEndColor:UInt = 0xFFCD02
-    static var iOS8tealGradientStartColor:UInt = 0x52EDC7
-    static var iOS8tealGradientEndColor:UInt = 0x5AC8FB
-    static var iOS8blueGradientStartColor:UInt = 0x1AD6FD
-    static var iOS8blueGradientEndColor:UInt = 0x1D62F0
-    static var iOS8violetGradientStartColor:UInt = 0xC644FC
-    static var iOS8violetGradientEndColor:UInt = 0x5856D6
-    static var iOS8magentaGradientStartColor:UInt = 0xEF4DB6
-    static var iOS8magentaGradientEndColor:UInt = 0xC643FC
-    static var iOS8blackGradientStartColor:UInt = 0x4A4A4A
-    static var iOS8blackGradientEndColor:UInt = 0x2B2B2B
-    static var iOS8silverGradientStartColor:UInt = 0xDBDDDE
-    static var iOS8silverGradientEndColor:UInt = 0x898C90
+    static var redGradientStartColor:UInt = 0xFF5E3A
+    static var redGradientEndColor:UInt = 0xFF2A68
+    static var greenGradientStartColor:UInt = 0x87FC70
+    static var greenGradientEndColor:UInt = 0x0BD318
+    static var orangeGradientStartColor:UInt = 0xFF9500
+    static var orangeGradientEndColor:UInt = 0xFF5E3A
+    static var yellowGradientStartColor:UInt = 0xFFDB4C
+    static var yellowGradientEndColor:UInt = 0xFFCD02
+    static var tealGradientStartColor:UInt = 0x52EDC7
+    static var tealGradientEndColor:UInt = 0x5AC8FB
+    static var blueGradientStartColor:UInt = 0x1AD6FD
+    static var blueGradientEndColor:UInt = 0x1D62F0
+    static var violetGradientStartColor:UInt = 0xC644FC
+    static var violetGradientEndColor:UInt = 0x5856D6
+    static var magentaGradientStartColor:UInt = 0xEF4DB6
+    static var magentaGradientEndColor:UInt = 0xC643FC
+    static var blackGradientStartColor:UInt = 0x4A4A4A
+    static var blackGradientEndColor:UInt = 0x2B2B2B
+    static var silverGradientStartColor:UInt = 0xDBDDDE
+    static var silverGradientEndColor:UInt = 0x898C90
 }
