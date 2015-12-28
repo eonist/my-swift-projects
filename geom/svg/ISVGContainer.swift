796c8482f03d7dd4a3f23fa4e8d1d779f8ceac08
Eonist <unicornhive@gmail.com>
2015-12-28 09:55:14 +0100

    Files modified: 1
    Modified 1 file:geom/svg/ISVGContainer.swift

--- geom/svg/ISVGContainer.swift
+++ geom/svg/ISVGContainer.swift
@@ -3,5 +3,7 @@ import Foundation
  * interface for SVGContainer
  */
 protocol ISVGContainer : ISVGElement{
-
+    func add(element:ISVGElement)
+    func get items() : Array
+    func getItem(id:String):*
 }
