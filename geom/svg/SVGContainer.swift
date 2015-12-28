9665bf9a2cd371366010710c24c7955bb3824360
Eonist <unicornhive@gmail.com>
2015-12-28 09:31:48 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGContainer.swift

--- geom/svg/SVGContainer.swift
+++ geom/svg/SVGContainer.swift
@@ -1,5 +1,9 @@
 import Foundation
-
+/**
+ * SVGContainer serves as a Sprite but also has an id and an array that keeps track of the added items
+ * // :TODO: could we omit the _items and just use the display-stack as a record of items added?
+ * // :TODO: we need a remove method to compliment the add method
+ */
 class SVGContainer {
 
 }
