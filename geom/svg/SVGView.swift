38af8f25b29b5256794630f271bfc694d4261c0e
Eonist <unicornhive@gmail.com>
2015-12-28 10:25:41 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGView.swift

--- geom/svg/SVGView.swift
+++ geom/svg/SVGView.swift
@@ -1,5 +1,7 @@
-import Foundation
-
-class SVGView {
+import Cocoa
+/**
+ * SVGDisplayObject impliments the same interface as ISVGElement and also the style params in ISVGDisplayObject and extends Sprite so that it can be seen on stage
+ */
+class SVGView : NSView{
 
 }
