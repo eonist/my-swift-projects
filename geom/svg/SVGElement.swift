45722cc48b8294b766017997baadf2b034496ade
Eonist <unicornhive@gmail.com>
2015-12-28 10:19:58 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGElement.swift

--- geom/svg/SVGElement.swift
+++ geom/svg/SVGElement.swift
@@ -2,6 +2,9 @@ import Foundation
 /**
  * SVGElement serves as a building block for other SVG items and only has an id value
  */
-class SVGElement {
-    
+class SVGElement :ISVGElement{
+    var id : String;
+    init(_ id:String) {
+        self.id = id;
+    }
 }
