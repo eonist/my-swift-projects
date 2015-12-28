2154841f02cfaa2622d526d1c36198a06e415d9c
Eonist <unicornhive@gmail.com>
2015-12-28 10:27:35 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGView.swift

--- geom/svg/SVGView.swift
+++ geom/svg/SVGView.swift
@@ -3,5 +3,12 @@ import Cocoa
  * SVGDisplayObject impliments the same interface as ISVGElement and also the style params in ISVGDisplayObject and extends Sprite so that it can be seen on stage
  */
 class SVGView : NSView{
+    var style : SVGStyle;
+    var id : String;
+    init(style:SVGStyle, id:String) {
+        self.style = style;
+        self.id = id;
+    }
 
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
