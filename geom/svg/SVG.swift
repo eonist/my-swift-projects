3e123fcb4ab0adec7864574c92c705ba19740183
Eonist <unicornhive@gmail.com>
2015-12-28 09:31:48 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVG.swift

--- geom/svg/SVG.swift
+++ geom/svg/SVG.swift
@@ -1,5 +1,14 @@
 import Foundation
-
-class SVG {
-
+/**
+ * SVG class is where pther svg elemnts is added
+ * // :TODO: impliment xmlns:xlink,viewBox,enable-background,xml:space,
+ * //preserveAspectRatio="xMinYMin meet"
+ * //viewBox="0 0 90 90"
+ * @example
+ * <?xml version="1.0"?><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="768px" height="768px">
+ * 		<rect x="64" y="64" fill="#0000FF" stroke="#000000" stroke-miterlimit="10" width="512" height="512"/>
+ * </svg>
+ */
+class SVG {// :TODO: (SVG should be split into SVGDisplayObject and SVG...something els)
+    
 }
