0012a5102633115c02288647d2601858479dd8ef
Eonist <unicornhive@gmail.com>
2015-12-29 08:05:42 +0100

    Files modified: 1
    Modified 1 file:geom/SVGRect.swift

--- geom/SVGRect.swift
+++ geom/SVGRect.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class SVGRect {
-    //continue here
+    width : Number;
+    height : Number;
+    x:Number;
+    y : Number;
+    rx : Number;/*for round rect, radius*/
+    ry : Number;/*for round rect, radius*/
 }
