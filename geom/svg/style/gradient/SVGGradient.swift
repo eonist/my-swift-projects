25e98483ba160a9ce755ea3d9f47eb0a9288a4bd
Eonist <unicornhive@gmail.com>
2015-12-29 14:01:25 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:geom/svg/style/gradient/SVGGradient.swift

--- geom/svg/style/gradient/SVGGradient.swift
+++ geom/svg/style/gradient/SVGGradient.swift
@@ -1,5 +1,8 @@
 import Foundation
-
+/*
+ * // :TODO: add an example here
+ * 13.06.19 10:19 - Added support for gradientTransform for SVGLinearGradient by moving gradientTransform from SVGRadialGradient into this class
+ */
 class SVGGradient {
 
 }
