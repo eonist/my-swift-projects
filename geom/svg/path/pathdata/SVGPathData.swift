c1052dfe1a4309dc89a68f140594c9af5f2b8a3c
Eonist <unicornhive@gmail.com>
2015-12-29 12:25:45 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/pathdata/SVGPathData.swift

--- geom/svg/path/pathdata/SVGPathData.swift
+++ geom/svg/path/pathdata/SVGPathData.swift
@@ -1,5 +1,12 @@
 import Foundation
-
+/*
+ * this class serves as a data holder of SVGPath data, other SVGPath types extends this class to get access to its methods
+ */
 class SVGPathData {
-
+	var commands:Array<Int>
+	var parameters:Array<CGFloat>
+	init(_ commands:Array<Int>,_ parameters:Array<CGFloat>) {
+		self.commands = commands
+		self.parameters = parameters
+	}
 }
