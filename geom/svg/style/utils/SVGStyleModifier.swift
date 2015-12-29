561c31c7ce2a3c34070ea94a629250d388f60771
Eonist <unicornhive@gmail.com>
2015-12-29 15:02:51 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleModifier.swift

--- geom/svg/style/utils/SVGStyleModifier.swift
+++ geom/svg/style/utils/SVGStyleModifier.swift
@@ -8,7 +8,7 @@ class SVGStyleModifier {
     class func merge(a:SVGStyle,_ b:SVGStyle){
         //Swift.print("a.fill: " + a.fill);
         //Swift.print("b.fill: " + b.fill);
-        if((a.fill.isNaN) && !(b.fill.isNaN)) {a.fill = b.fill}/*Fill*/
+        if((a.fill as? Double).isNaN && !(b.fill as! Double).isNaN) {a.fill = b.fill}/*Fill*/
 		if(isNaN(a.fillOpacity) && !isNaN(b.fillOpacity)) a.fillOpacity = b.fillOpacity;
 		if(a.fillRule == null && b.fillRule != null) a.fillRule = b.fillRule;
 		if(isNaN(a.stroke) && !isNaN(b.stroke)) a.stroke = b.stroke;/*Stroke*/
