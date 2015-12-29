427f555e0ed2a2aeba34437de29df87c8b3d91c0
Eonist <unicornhive@gmail.com>
2015-12-29 09:08:01 +0100

    Files modified: 2
    Modified 2 files:geom/svg/SVGPolygon.swiftgeom/svg/path/ISVGPolyLine.swift

--- geom/svg/SVGPolygon.swift
+++ geom/svg/SVGPolygon.swift
@@ -1,5 +1,22 @@
 import Foundation
+/**
+ * Draws a polygon based on points, closes it self to the first point
+ */
+class SVGPolygon:SVGGraphic,ISVGPolyLine{
+    var points : Array<CGPoint>;
+    init(_ points:Array, _ style : SVGStyle? = nil, _ id : String? = nil) {
+        _points = points;
+        super.init(style, id);
+    }
 
-class SVGPolygon {
-
+    
+        
+    
+    override func draw()  {
+        //swift.print("SVGPolygon.draw"+_points);
+        graphics.moveTo((points[0] as Point).x, (points[0] as Point).y);
+        for (var i : int = 1; i < points.length; i++) graphics.lineTo((points[i] as Point).x, (points[i] as Point).y);
+        graphics.lineTo((points[0] as Point).x, (points[0] as Point).y);/*closes it self to the start position*/
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
--- geom/svg/path/ISVGPolyLine.swift
+++ geom/svg/path/ISVGPolyLine.swift
@@ -1,5 +1,7 @@
 import Foundation
-
-class ISVGPolyLine {
-
+/**
+ * Interface for the SVGPolyLine class
+ */
+protocol ISVGPolyLine {
+    var points:Array<CGPoint> {get set}
 }
