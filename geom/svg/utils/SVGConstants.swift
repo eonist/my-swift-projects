4ec88b4d8a73da8973bd84b52df61468e9cc6c67
Eonist <unicornhive@gmail.com>
2015-12-29 16:52:48 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGConstants.swift

--- geom/svg/utils/SVGConstants.swift
+++ geom/svg/utils/SVGConstants.swift
@@ -1,5 +1,18 @@
 import Foundation
-
+/*
+ * These constants represents the names in the SVG specification 
+ */
 class SVGConstants {
-
+	static var rect:String = "rect"
+	static var circle:String = "circle"
+	static var ellipse:String = "ellipse"
+	static var path:String = "path"
+	static var polyLine:String = "polyline"
+	static var polygon:String = "polygon"
+	static var line:String = "line"
+	static var group:String = "g"
+	static var data:String = "d"
+	static var points:String = "points"
+	static var linearGradient:String = "linearGradient"
+	static var radialGradient:String = "radialGradient"
 }
