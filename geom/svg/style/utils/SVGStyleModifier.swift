ba820333ab22e2912beccdae359ef3f55764f75d
Eonist <unicornhive@gmail.com>
2015-12-29 15:12:32 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleModifier.swift

--- geom/svg/style/utils/SVGStyleModifier.swift
+++ geom/svg/style/utils/SVGStyleModifier.swift
@@ -8,17 +8,14 @@ class SVGStyleModifier {
     class func merge(a:SVGStyle,_ b:SVGStyle){
         //Swift.print("a.fill: " + a.fill);
         //Swift.print("b.fill: " + b.fill);
-        //
-        
         if((!(a.fill is Double) && !(b.fill is Double)) || ((a.fill as! Double).isNaN && !(b.fill as! Double).isNaN)) {a.fill = b.fill}/*Fill*/
         if((a.fillOpacity.isNaN) && !(b.fillOpacity.isNaN)) {a.fillOpacity = b.fillOpacity}
         if(a.fillRule == nil && b.fillRule != nil) {a.fillRule = b.fillRule}
-        
         if(a.stroke.isNaN && !(b.stroke.isNaN)) {a.stroke = b.stroke}/*Stroke*/
         if((a.strokeWidth.isNaN) && !(b.strokeWidth.isNaN)) {a.strokeWidth = b.strokeWidth}
-        if(isNaN(a.strokeOpacity) && !isNaN(b.strokeOpacity)) {a.strokeOpacity = b.strokeOpacity}
-		if(a.strokeLineCap == null && b.strokeLineCap != null) a.strokeLineCap = b.strokeLineCap;
-		if(a.strokeLineJoin == null && b.strokeLineJoin != null) a.strokeLineJoin = b.strokeLineJoin;
-		if(isNaN(a.strokeMiterLimit) && isNaN(b.strokeMiterLimit)) a.strokeMiterLimit = b.strokeMiterLimit;
+        if((a.strokeOpacity.isNaN) && !(b.strokeOpacity.isNaN)) {a.strokeOpacity = b.strokeOpacity}
+		if(a.strokeLineCap == nil && b.strokeLineCap != nil) {a.strokeLineCap = b.strokeLineCap}
+		if(a.strokeLineJoin == nil && b.strokeLineJoin != nil) {a.strokeLineJoin = b.strokeLineJoin}
+		if((a.strokeMiterLimit.isNaN) && (b.strokeMiterLimit.isNaN)) {a.strokeMiterLimit = b.strokeMiterLimit}
     }
 }
