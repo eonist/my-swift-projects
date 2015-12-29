9e4e4c889903ae64ca0b569b4e7b57f497c38d2b
Eonist <unicornhive@gmail.com>
2015-12-29 17:21:52 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -7,15 +7,15 @@ class SVGModifier {
 	 */
 	class func offsetItems(svg:SVG, offset:CGPoint) {
 		for (var i : Int = 0; i < svg.items.count; i++) {
-			var svgGraphic:SVGGraphic = svg.items[i];
+			let svgGraphic:SVGGraphic = svg.items[i] as! SVGGraphic;
 			if(svgGraphic is SVGLine){
-				(svgGraphic as SVGLine).x1 += offset.x;
-				(svgGraphic as SVGLine).x2 += offset.x;
-				(svgGraphic as SVGLine).y1 += offset.y;
-				(svgGraphic as SVGLine).y2 += offset.y;
+				(svgGraphic as! SVGLine).x1 += offset.x;
+				(svgGraphic as! SVGLine).x2 += offset.x;
+				(svgGraphic as! SVGLine).y1 += offset.y;
+				(svgGraphic as! SVGLine).y2 += offset.y;
 			}else if(svgGraphic is SVGRect){
-				(svgGraphic as SVGRect).x += offset.x;
-				(svgGraphic as SVGRect).y += offset.y;
+				(svgGraphic as! SVGRect).x += offset.x;
+				(svgGraphic as! SVGRect).y += offset.y;
 			}
 		}
 	}
