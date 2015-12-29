44805a531522434c8a43b7e568cf2abdeabe65ef
Eonist <unicornhive@gmail.com>
2015-12-29 17:11:53 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -1,5 +1,22 @@
 import Foundation
 
 class SVGModifier {
-
+	/**
+	 * Offsets each item in a svg by @param offset (Point)
+	 * // :TODO: add support for other SVG elements as well, use switch method instead of if
+	 */
+	class func offsetItems(svg:SVG, offset:CGPoint) {
+		for (var i : Int = 0; i < svg.items.count; i++) {
+			var svgGraphic:SVGGraphic = svg.items[i];
+			if(svgGraphic is SVGLine){
+				(svgGraphic as SVGLine).x1 += offset.x;
+				(svgGraphic as SVGLine).x2 += offset.x;
+				(svgGraphic as SVGLine).y1 += offset.y;
+				(svgGraphic as SVGLine).y2 += offset.y;
+			}else if(svgGraphic is SVGRect){
+				(svgGraphic as SVGRect).x += offset.x;
+				(svgGraphic as SVGRect).y += offset.y;
+			}
+		}
+	}
 }
