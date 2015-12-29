024f6f481f1853ff5171fc5e9d2035a73b55b917
Eonist <unicornhive@gmail.com>
2015-12-29 14:58:58 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleModifier.swift

--- geom/svg/style/utils/SVGStyleModifier.swift
+++ geom/svg/style/utils/SVGStyleModifier.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class SVGStyleModifier {
-
+    /**
+     * Merges if a value isnt present already
+     * // :TODO: a boolean flag should toggle overide if a value exists already
+     */
+    class func merge(){
+        
+    }
 }
