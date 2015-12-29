98f3b7ddedcd38b35766ea03d36f2c0550d95892
Eonist <unicornhive@gmail.com>
2015-12-29 14:47:36 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/SVGRadialGradient.swift

--- geom/svg/style/gradient/SVGRadialGradient.swift
+++ geom/svg/style/gradient/SVGRadialGradient.swift
@@ -1,5 +1,20 @@
 import Foundation
-
+/*
+ * // :TODO: add an example here
+ * // :TODO: add fx="98.7581" fy="63.338" 
+ */
 class SVGRadialGradient {
-    
+    var cx:CGFloat
+    var cy:CGFloat
+    var r:CGFloat/*radius*/
+    var fx:CGFloat
+    var fy:CGFloat
+    init(offsets:Array, colors:Array, opacities:Array, gradientUnits:String, spreadMethod:String, id:String, cx:CGFLoat, cy:CGFloat, r:CGFloat, fx:CGFloat, fy:CGFloat/*, gradientTransform:Matrix*/){
+        self.cx = cx;
+        self.cy = cy;
+        self.r = r;
+        self.fx = fx;
+        self.fy = fy;
+        super(offsets,colors,opacities,spreadMethod,id,gradientUnits,gradientTransform);
+    }
 }
