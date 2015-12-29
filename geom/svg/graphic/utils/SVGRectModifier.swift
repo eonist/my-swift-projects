b7c61d74c788b172af0ed1c68b10057040d93e79
Eonist <unicornhive@gmail.com>
2015-12-29 20:45:50 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGRectModifier.swift

--- geom/svg/graphic/utils/SVGRectModifier.swift
+++ geom/svg/graphic/utils/SVGRectModifier.swift
@@ -3,14 +3,14 @@ import Foundation
 class SVGRectModifier {
 	/**
 	 * Scales the SVGRect instance from @param pivot to a specific @param scale scalar value (0-1)
-	 * // :TODO: impliment support for scaling the rx and ry values in the SVGRect instance
+	 * // :TODO: impliment support for scaling the rx and ry values in the SVGRect instance, you have code for this Think fillet classes
 	 */
-	class func scale(rect:SVGRect,_ pivot:CGPoint,_ scale:CGPoint):void {
-		var position:CGPoint = PointModifier.scalePoint(CGPoint(rect.x,rect.y), pivot, scale);
-		var size:CGPoint = CGPoint(rect.width * scale.x, rect.height * scale.y);
+	class func scale(rect:SVGRect,_ pivot:CGPoint,_ scale:CGPoint) {
+		let position:CGPoint = PointModifier.scale(rect.position, pivot, scale);
+		let size:CGSize = CGSize(rect.width * scale.x, rect.height * scale.y);//TODO:use CGRect scale method instead
 		rect.x = position.x;
 		rect.y = position.y;
-		rect.width = size.x;
-		rect.height = size.y;
+		rect.width = size.width;
+		rect.height = size.height;
 	}
 }
