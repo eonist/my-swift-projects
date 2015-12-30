6f1e048f4169e3c26cdf6470e70434745bfaced6
Eonist <unicornhive@gmail.com>
2015-12-30 13:18:45 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGParser.swift

--- geom/svg/utils/SVGParser.swift
+++ geom/svg/utils/SVGParser.swift
@@ -8,14 +8,14 @@ class SVGParser {
      * @Note the regular expression removes the PX suffix
      */
     class func svg(xml:NSXMLElement)->SVG {
-        var x:CGFloat = SVGPropertyParser.digit(xml,"x");
-        var y:CGFloat = SVGPropertyParser.digit(xml,"y");
-        var width:CGFloat = SVGPropertyParser.digit(xml,"width");
-        var height:CGFloat = SVGPropertyParser.digit(xml,"height");
-        var version:CGFloat = SVGPropertyParser.value(SVGPropertyParser.property(xml, "version"));
-        var nameSpace:String = ""//xml.namespaceDeclarations().toString();//TODO: implement this later
-        var id:String = SVGPropertyParser.id(xml);
-        var doc:SVG = SVG([],x,y,width,height,version,nameSpace,id);
+        let x:CGFloat = SVGPropertyParser.digit(xml,"x");
+        let y:CGFloat = SVGPropertyParser.digit(xml,"y");
+        let width:CGFloat = SVGPropertyParser.digit(xml,"width");
+        let height:CGFloat = SVGPropertyParser.digit(xml,"height");
+        let version:CGFloat = SVGPropertyParser.value(SVGPropertyParser.property(xml, "version"));
+        let nameSpace:String = ""//xml.namespaceDeclarations().toString();//TODO: implement this later
+        let id:String = SVGPropertyParser.id(xml);
+        let doc:SVG = SVG([],x,y,width,height,version,nameSpace,id);
         let children:NSArray = xml.children!
         let count = children.count//or use rootElement.childCount TODO: test this
         for (var i = 0; i < count; i++) {
@@ -36,17 +36,18 @@ class SVGParser {
         var style:SVGStyle = SVGPropertyParser.style(xml, container)
         if(container is SVGGroup && (container as! SVGGroup).style != nil) {SVGStyleModifier.merge(style, (container as! SVGGroup).style!)}/*parent style is inherited down to sub elements*/
         var id:String = SVGPropertyParser.id(xml);
-        switch(xml.localName){
-            case SVGConstants.RECT: element = rect(xml,style,id); break;
-            case SVGConstants.POLY_LINE: element =  polyLine(xml,style,id); break;
-            case SVGConstants.POLYGON: element = polygon(xml,style,id); break;
-            case SVGConstants.PATH: element = path(xml,style,id); break;
-            case SVGConstants.LINE: element = line(xml,style,id); break;
-            case SVGConstants.CIRCLE: element = circle(xml,style,id); break;
-            case SVGConstants.ELLIPSE: element = ellipse(xml,style,id); break;
-            case SVGConstants.GROUP: element = group(xml,style,id); break;
-            case SVGConstants.LINEAR_GRADIENT: element = SVGGradientParser.linearGradient(xml); break;
-            case SVGConstants.RADIAL_GRADIENT: element = SVGGradientParser.radialGradient(xml); break;
+        switch(xml.localName!){
+            case SVGConstants.rect: element = rect(xml,style,id); break;
+            case SVGConstants.polyLine: element =  polyLine(xml,style,id); break;
+            case SVGConstants.polygon: element = polygon(xml,style,id); break;
+            case SVGConstants.path: element = path(xml,style,id); break;
+            case SVGConstants.line: element = line(xml,style,id); break;
+            case SVGConstants.circle: element = circle(xml,style,id); break;
+            case SVGConstants.ellipse: element = ellipse(xml,style,id); break;
+            case SVGConstants.group: element = group(xml,style,id); break;
+            case SVGConstants.linearGradient: element = SVGGradientParser.linearGradient(xml); break;
+            case SVGConstants.radialGradient: element = SVGGradientParser.radialGradient(xml); break;
+            default: break;
         }
         return element;
     }
