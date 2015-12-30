16edf3ca9354551a1aaf41055e02cc7695dad3e7
Eonist <unicornhive@gmail.com>
2015-12-30 19:39:10 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/utils/SVGGradientParser.swift

--- geom/svg/style/gradient/utils/SVGGradientParser.swift
+++ geom/svg/style/gradient/utils/SVGGradientParser.swift
@@ -10,16 +10,16 @@ class SVGGradientParser {
 	 */
 	class func linearGradient(xml:NSXMLElement)->SVGLinearGradient{
 		// trace("linearGradient ");
-		var x1Str:String = SVGPropertyParser.property(xml,"x1");
+		var x1Str:String = SVGPropertyParser.property(xml,"x1")!;
 		// trace("x1: " + x1);
 		let x1:CGFloat = StringAsserter.percentage(x1Str) ? StringParser.percentage(x1Str) : SVGPropertyParser.value(x1Str);
-		var y1Str:String = SVGPropertyParser.property(xml,"y1");
+		var y1Str:String = SVGPropertyParser.property(xml,"y1")!;
 		let y1:CGFloat = StringAsserter.percentage(y1Str) ? StringParser.percentage(y1Str) : SVGPropertyParser.value(y1Str);
-		var x2Str:String = SVGPropertyParser.property(xml,"x2");
+		var x2Str:String = SVGPropertyParser.property(xml,"x2")!;
 		let x2:CGFloat = StringAsserter.percentage(x2Str) ? StringParser.percentage(x2Str) : SVGPropertyParser.value(x2Str);
-		var y2Str:String = SVGPropertyParser.property(xml,"y2");
+		var y2Str:String = SVGPropertyParser.property(xml,"y2")!;
 		let y2:CGFloat = StringAsserter.percentage(y2) ? StringParser.percentage(y2Str) : SVGPropertyParser.value(y2Str);
 		var svgGradient:SVGGradient = Utils.gradient(xml);
-		return new SVGLinearGradient(svgGradient.offsets,svgGradient.colors,svgGradient.opacities,x1,y1,x2,y2,svgGradient.gradientUnits,svgGradient.spreadMethod,svgGradient.id,svgGradient.gradientTransform);
+		return SVGLinearGradient(svgGradient.offsets,svgGradient.colors,svgGradient.opacities,x1,y1,x2,y2,svgGradient.gradientUnits,svgGradient.spreadMethod,svgGradient.id,svgGradient.gradientTransform);
 	}
 }
