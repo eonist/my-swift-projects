b4273f3a620bd848808ce7870d0de04c6846db29
Eonist <unicornhive@gmail.com>
2015-12-30 12:33:19 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleParser.swift

--- geom/svg/style/utils/SVGStyleParser.swift
+++ geom/svg/style/utils/SVGStyleParser.swift
@@ -5,11 +5,11 @@ class SVGStyleParser {
 	 * // :TODO: why do we need the @param container here?
 	 */
 	class func style(prop:Any,_ container:ISVGContainer)->SVGStyle {
-		var inlineStyle:Object = inlineStyle(prop);
+		var inlineStyle:Dictionary<String, String> = SVGStyleParser.inlineStyle(prop as! String);
 //			ObjectDescriber.describe(inlineStyle);
 		let fill:Any = SVGStyleParser.fill(inlineStyle["fill"], container)
 		let fillOpacity:CGFloat = SVGPropertyParser.value(inlineStyle["fill-opacity"])
-		let fillRule:String = inlineStyle["fill-rule"]
+		let fillRule:String = inlineStyle["fill-rule"]!
 		let stroke:CGFloat = SVGStyleParser.stroke(inlineStyle["stroke"])
 		let strokeWidth:CGFloat = SVGPropertyParser.value(inlineStyle["stroke-width"])
 		let strokeOpacity:CGFloat = SVGPropertyParser.value(inlineStyle["stroke-opacity"])
