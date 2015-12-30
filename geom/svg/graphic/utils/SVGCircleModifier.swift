e879e42c4afb95a1d1dfc7f8c791d1193bb9a504
Eonist <unicornhive@gmail.com>
2015-12-30 07:21:25 +0100

    Files modified: 2
    Modified 2 files:geom/svg/graphic/utils/SVGCircleModifier.swiftgeom/svg/style/gradient/utils/SVGGradientModifier.swift

--- geom/svg/graphic/utils/SVGCircleModifier.swift
+++ geom/svg/graphic/utils/SVGCircleModifier.swift
@@ -5,7 +5,7 @@ class SVGCircleModifier {
      * Scales the SVGCircle instance from @param pivot to a specific @param scale scalar value (0-1)
      */
     class func scale(circle:SVGCircle,pivot:CGPoint,scale:CGPoint) {
-        let circleCenter:CGPoint = PointModifier.scale(CGPoint(circle.cx,circle.cy), pivot, scale);
+        let circleCenter:CGPoint = PointModifier.scale(circle.center, pivot, scale);
         let circleRadius:CGFloat = circle.r * scale.x;
         circle.center = circleCenter;
         circle.r = circleRadius;
--- geom/svg/style/gradient/utils/SVGGradientModifier.swift
+++ geom/svg/style/gradient/utils/SVGGradientModifier.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class SVGGradientModifier {
-
+	/**
+	 * Scales @param gradient at @param pivot with @param scalar (0-1)
+	 * @Note works for SVGRadialGradient and SVGLinearGradient
+	 */
+	class func scale(gradient:SVGGradient,pivot:CGPoint,scale:CGPoint) {
+		if(gradient.gradientTransform != null) MatrixModifier.scale(gradient.gradientTransform, pivot, scale);/*Scale the current applied matrix*/
+		else gradient.gradientTransform = MatrixModifier.scale(new Matrix(), pivot, scale);/*if there is no gradientTransform allready applied to the Gradient instnace, then apply a new Matrix instance w/ the correct scale*/
+	}
 }
