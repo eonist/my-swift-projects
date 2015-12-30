75eb876145256439f643400c421922a48938b0c2
Eonist <unicornhive@gmail.com>
2015-12-30 07:56:24 +0100

    Files modified: 1
    Modified 1 file:xml/XMLModifier.swift

--- xml/XMLModifier.swift
+++ xml/XMLModifier.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class XMLModifier {
     /**
-     * Adding attributes to an element: http://stackoverflow.com/questions/25540023/swift-creating-an-nsxmlnode
+     * Adding attributes to an element: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation/Classes/NSXMLElement_Class/
      * more here: https://gist.github.com/brentdax/caaaa134c500e00efd36
      * worth a look (constructing html): https://gist.github.com/brentdax/caaaa134c500e00efd36
      */
