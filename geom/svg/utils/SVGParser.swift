44be439b24b87ccb9fe4fb803a4bed8e7ec390c7
Eonist <unicornhive@gmail.com>
2015-12-30 12:39:08 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGParser.swift

--- geom/svg/utils/SVGParser.swift
+++ geom/svg/utils/SVGParser.swift
@@ -7,7 +7,7 @@ class SVGParser {
      * Returns an SVGDoc instance derived from @param xml
      * @Note the regular expression removes the PX suffix
      */
-    class func svg(xml:XML)->SVG {
+    class func svg(xml:NSXMLElement)->SVG {
         var x:CGFloat = SVGPropertyParser.digit(xml,"x");
         var y:CGFloat = SVGPropertyParser.digit(xml,"y");
         var width:CGFloat = SVGPropertyParser.digit(xml,"width");
@@ -15,7 +15,7 @@ class SVGParser {
         var version:CGFloat = SVGPropertyParser.value(SVGPropertyParser.property(xml, "version"));
         var nameSpace:String = xml.namespaceDeclarations().toString();
         var id:String = SVGPropertyParser.id(xml);
-        var doc:SVG = new SVG([],x,y,width,height,version,nameSpace,id);
+        var doc:SVG = SVG([],x,y,width,height,version,nameSpace,id);
         
         for each (child : XML in xml.children()) doc.add(element(child,doc));//print("Import - child.toXMLString(): " + child.toXMLString());
         return SVG()/*doc*/;
