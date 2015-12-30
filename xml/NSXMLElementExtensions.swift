799037d8fd43394a0a305b8a303efea5f4a33010
Eonist <unicornhive@gmail.com>
2015-12-30 07:48:38 +0100

    Files modified: 1
    Modified 1 file:xml/NSXMLElementExtensions.swift

--- xml/NSXMLElementExtensions.swift
+++ xml/NSXMLElementExtensions.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class NSXMLElementExtensions {
-
+    /*Easy Access to corners*/
+    subscript(key: String) -> CGPoint {
+        get {
+            
+        }
+        set {
+            fatalError("UNSUPORTED CORNER TYPE: " + key + " WITH VALUE: " + String(newValue))
+        }
+    }
 }
