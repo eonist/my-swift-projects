07f8fc4c4e372c8bf91cf99cbdaab93dc876e39a
Eonist <unicornhive@gmail.com>
2015-12-30 12:43:19 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGParser.swift

--- geom/svg/utils/SVGParser.swift
+++ geom/svg/utils/SVGParser.swift
@@ -13,11 +13,11 @@ class SVGParser {
         var width:CGFloat = SVGPropertyParser.digit(xml,"width");
         var height:CGFloat = SVGPropertyParser.digit(xml,"height");
         var version:CGFloat = SVGPropertyParser.value(SVGPropertyParser.property(xml, "version"));
-        var nameSpace:String = xml.namespaceDeclarations().toString();
+        var nameSpace:String = ""//xml.namespaceDeclarations().toString();//TODO: implement this later
         var id:String = SVGPropertyParser.id(xml);
         var doc:SVG = SVG([],x,y,width,height,version,nameSpace,id);
         
-        for each (child : XML in xml.children()) doc.add(element(child,doc));//print("Import - child.toXMLString(): " + child.toXMLString());
+        for child : XML in xml.children() doc.add(element(child,doc));//print("Import - child.toXMLString(): " + child.toXMLString());
         return SVG()/*doc*/;
     }
 }
