5da950914b1be3a0dc73ce408ebafb37637d76d2
Eonist <unicornhive@gmail.com>
2015-12-30 07:46:42 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:geom/svg/utils/SVGPropertyParser.swift

--- geom/svg/utils/SVGPropertyParser.swift
+++ geom/svg/utils/SVGPropertyParser.swift
@@ -5,7 +5,7 @@ class SVGPropertyParser {
 	 * @Note could also be named attributeValue or value
 	 * @param key is the that matches the returned value among the attributes in the xml item
 	 */
-	class func property(xml:NSXMLElement,key:String):* {
-		return xml.hasOwnProperty("@"+key) ? xml["@"+key] : null;
+	class func property(xml:NSXMLElement,key:String)->String? {
+		return XMLAsserter.hasAttribute(xml,"@"+key) ? XMLParser.attribute(xml, "@"+key) : nil;
 	}
 }
