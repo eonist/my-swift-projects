4f04ef11a395393533d55057de43a2b56606df59
Eonist <unicornhive@gmail.com>
2015-12-30 19:35:16 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/utils/SVGGradientParser.swift

--- geom/svg/style/gradient/utils/SVGGradientParser.swift
+++ geom/svg/style/gradient/utils/SVGGradientParser.swift
@@ -8,17 +8,17 @@ class SVGGradientParser {
 	 * @param xml (<linearGradient id="skyline"><stop offset="0" style="stop-color:blue"/><stop offset="1" style="stop-color:red"/></linearGradient>)
 	 * other attributes: gradientTransform,gradientUnits,xlink:href
 	 */
-	class func linearGradient(xml:XML)->SVGLinearGradient{
+	class func linearGradient(xml:NSXMLElement)->SVGLinearGradient{
 		// trace("linearGradient ");
-		var x1:* = SVGPropertyParser.property(xml,"x1");
+		var x1Str:String = SVGPropertyParser.property(xml,"x1");
 		// trace("x1: " + x1);
-		x1 = StringAsserter.percentage(x1) ? StringParser.percentage(x1) : SVGPropertyParser.value(x1);
-		var y1:* = SVGPropertyParser.property(xml,"y1");
-		y1 = StringAsserter.percentage(y1) ? StringParser.percentage(y1) : SVGPropertyParser.value(y1);
-		var x2:* = SVGPropertyParser.property(xml,"x2");
-		x2 = StringAsserter.percentage(x2) ? StringParser.percentage(x2) : SVGPropertyParser.value(x2);
-		var y2:* = SVGPropertyParser.property(xml,"y2");
-		y2 = StringAsserter.percentage(y2) ? StringParser.percentage(y2) : SVGPropertyParser.value(y2);
+		let x1:CGFloat = StringAsserter.percentage(x1Str) ? StringParser.percentage(x1Str) : SVGPropertyParser.value(x1Str);
+		var y1Str:String = SVGPropertyParser.property(xml,"y1");
+		let y1:CGFloat = StringAsserter.percentage(y1Str) ? StringParser.percentage(y1Str) : SVGPropertyParser.value(y1Str);
+		var x2Str:String = SVGPropertyParser.property(xml,"x2");
+		let x2:CGFloat = StringAsserter.percentage(x2Str) ? StringParser.percentage(x2Str) : SVGPropertyParser.value(x2Str);
+		var y2Str:String = SVGPropertyParser.property(xml,"y2");
+		let y2:CGFloat = StringAsserter.percentage(y2) ? StringParser.percentage(y2Str) : SVGPropertyParser.value(y2Str);
 		var svgGradient:SVGGradient = Utils.gradient(xml);
 		return new SVGLinearGradient(svgGradient.offsets,svgGradient.colors,svgGradient.opacities,x1,y1,x2,y2,svgGradient.gradientUnits,svgGradient.spreadMethod,svgGradient.id,svgGradient.gradientTransform);
 	}
