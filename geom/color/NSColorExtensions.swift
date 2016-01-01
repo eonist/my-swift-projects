fa2f99d8266ca6fd53e352992ddfdaf9e6f518dc
Eonist <unicornhive@gmail.com>
2016-01-01 14:23:05 +0100

    Files modified: 1
    Modified 1 file:geom/color/NSColorExtensions.swift

--- geom/color/NSColorExtensions.swift
+++ geom/color/NSColorExtensions.swift
@@ -6,7 +6,7 @@ extension NSColor{
     /**
      * EXAMPLE: :NSColor(NSColor.blackColor(),0.5)//outputs: a black color with 50% transparancy
      */
-    convenience init(_ color:NSColor,_ alpha:CGFloat/*0.0 - 1.0*/){
+    convenience init(_ color:NSColor,_ alpha:CGFloat){/*0.0 - 1.0*/
         let ciColor:CIColor = CIColor(color: color)!
         self.init(red: ciColor.red, green: ciColor.green, blue: ciColor.blue, alpha: alpha)
     }
