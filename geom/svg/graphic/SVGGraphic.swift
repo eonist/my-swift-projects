184e6504f4ee2d35133e512a3f3128c8c6893518
Eonist <unicornhive@gmail.com>
2016-01-01 18:41:35 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/SVGGraphic.swift

--- geom/svg/graphic/SVGGraphic.swift
+++ geom/svg/graphic/SVGGraphic.swift
@@ -70,7 +70,8 @@ class SVGGraphic : SVGView,ISVGGraphic{
      * @Note Updates only if style exists and fill is a number
      */
     func beginFill(){
-        Swift.print("SVGGraphic.beginFill()" + "\(style!.fill))")
+        let temp:Double! = Double(String(style!.fill))
+        Swift.print("SVGGraphic.beginFill()" + "\(temp))")
         if(style != nil && style!.fill is Double/* && style!.fill != "none"*/) {
             Swift.print("SVGGraphic.beginFill() color")
             let colorVal:Double = !(style!.fill as! Double).isNaN ? style!.fill as! Double : Double(0x000000)
