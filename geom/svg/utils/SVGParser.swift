aea213d8e9c1ff720f6d53bfe8effd920c294062
Eonist <unicornhive@gmail.com>
2016-01-01 15:17:20 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGParser.swift

--- geom/svg/utils/SVGParser.swift
+++ geom/svg/utils/SVGParser.swift
@@ -122,7 +122,7 @@ class SVGParser {
 		print("SVGParser.polygon()");
         if(!xml.hasAttribute(SVGConstants.points)) {return nil}
         let pointsString:String = xml[SVGConstants.points]!
-		print("SVGParser.polygon() pointsString: " + pointsString);
+		//print("SVGParser.polygon() pointsString: " + pointsString);
         var points:Array<CGPoint> = []
         var parameters:Array<CGFloat> = SVGPathParser.parameters(pointsString);
         //print("SVGPArser.polygon() parameters: " + "\(parameters)");
