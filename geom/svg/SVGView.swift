5528a6561763bee84591d9363cd3c1e31e8bd17d
Eonist <unicornhive@gmail.com>
2016-01-01 15:48:14 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGView.swift

--- geom/svg/SVGView.swift
+++ geom/svg/SVGView.swift
@@ -2,7 +2,7 @@ import Cocoa
 /**
  * SVGDisplayObject impliments the same interface as ISVGElement and also the style params in ISVGDisplayObject and extends Sprite so that it can be seen on stage
  */
-class SVGView : NSView,ISVGView{
+class SVGView : FlippedView,ISVGView{
     var style : SVGStyle?;
     var id : String;
     init(_ style:SVGStyle, _ id:String) {
