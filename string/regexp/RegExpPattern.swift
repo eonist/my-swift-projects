0a19f49829c6ecec430fe45a45f292d7cf2f7673
Eonist <unicornhive@gmail.com>
2016-01-01 06:32:27 +0100

    Files modified: 1
    Modified 1 file:string/regexp/RegExpPattern.swift

--- string/regexp/RegExpPattern.swift
+++ string/regexp/RegExpPattern.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 class RegExpPattern {
-
+    static var digitAssertPattern:String{
+        //let g1:String = "(?=\\s|^)"/*must be preceeded by a space char or the begining*/
+        let g2:String = "\\-*\\d+?"/*optional minus sign followed by one or more digits*/
+        let g3:String = "(?=\\.\\d|\\s|,|$)"/*Must be followed by a dot with a digit or space, comma or end*/
+        let g4:String = "((?<=\\d)\\.(?=\\d))*"/*zero or one dot char that must be preceeded with a digit and subseeded wutha digit*/
+        let g5:String = "((?<=\\d\\.)\\d+?)*"/*zero or more digits that must be preceeded with a digit and a char*/
+        //let g6:String = "(?=$)"/*followed by an end or */
+        
+        let pattern:String = /*g1 +*/ g2 + g3 + g4 + g5 /*+ g6*/
+        return pattern
+    }
 }
