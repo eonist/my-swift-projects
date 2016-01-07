5cac8023883b33bb195f8882141b94a49dc7fe9d
Eonist <unicornhive@gmail.com>
2016-01-07 21:42:08 +0100

    Files modified: 2
    Modified 2 files:geom/style/GraphicStyle.swiftgeom/svg/SVGPolygon.swift

--- geom/style/GraphicStyle.swift
+++ geom/style/GraphicStyle.swift
@@ -1,6 +1,6 @@
 import Cocoa
 protocol IGraphicStyle {
-    var fill: IFill? { get set }
+    var fill: IFill { get set }
     var stroke: IStroke { get set }
 }
 class GraphicStyle:IGraphicStyle {
@@ -8,7 +8,7 @@ class GraphicStyle:IGraphicStyle {
     static var green:IGraphicStyle = GraphicStyle(Fill(NSColor.greenColor()),Stroke.clear)
     var fill: IFill? //{ get {return self.fill} set {self.fill = newValue} }
     var stroke: IStroke //{ get {return self.stroke } set {self.stroke = newValue}}
-    init(_ fill:IFill? = Fill.clear/*<-maybe not use clear here, nil works aswell*/,_ stroke:IStroke = Stroke.clear) {
+    init(_ fill:IFill = Fill.clear/*<-maybe not use clear here, nil works aswell*/,_ stroke:IStroke = Stroke.clear) {
         self.fill = fill;
         self.stroke = stroke;
     }
