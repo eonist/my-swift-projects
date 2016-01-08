30af95f7aff54232daa2d8d5374e8cb90a0aec98
Eonist <unicornhive@gmail.com>
2016-01-08 12:44:51 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -7,14 +7,13 @@ class SVGGraphicModifier {
     */
     class func applyStrokeStyle(graphics:Graphics, style:SVGStyle) {
         var strokeWidth:CGFloat = !(style.strokeWidth.isNaN) ? style.strokeWidth : 0;
-        var strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
         var strokeMiterLimit:CGFloat = !(style.strokeMiterLimit.isNaN) ? style.strokeMiterLimit : 1.414;
         var strokeLineCap:String = style.strokeLineCap! == "" ? style.strokeLineCap! : "none";
         var strokeLineJoin:String = style.strokeLineJoin == "" ? style.strokeLineJoin! : "miter";
         /*color*/
         let colorVal:Double = !(style.fill as! Double).isNaN ? style.fill as! Double : Double(0x000000)
-        let opacity:CGFloat = !style.fillOpacity.isNaN ? style!.fillOpacity : 1
-        let color:NSColor = NSColorParser.nsColor(UInt(colorVal), opacity)
-        graphics.line(strokeWidth, style.stroke, strokeLineCap, strokeLineJoin, strokeMiterLimit)
+        var strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
+        let color:NSColor = NSColorParser.nsColor(UInt(colorVal), strokeOpacity)
+        graphics.line(strokeWidth, color, strokeLineCap, strokeLineJoin, strokeMiterLimit)
     }
 }
