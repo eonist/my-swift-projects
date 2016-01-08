374c0da1ef89d7370c7a346a40c74936c6883b75
Eonist <unicornhive@gmail.com>
2016-01-08 12:28:45 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class SVGGraphicModifier {
-    //continue here: add the applyStrokeStyle method at first
+    /**
+    * Applies the @param strokeStyle to the @param graphics
+    * @Note: If @param strokeStyle Does not have a variable, the default flash variable is applied
+    */
+    class func applyStrokeStyle(graphics:Graphics, style:SVGStyle) {
+        var strokeWidth:CGFloat = !(style.strokeWidth.isNaN) ? style.strokeWidth : 0;
+        var strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
+        var strokeMiterLimit:CGFloat = !(style.strokeMiterLimit.isNaN) ? style.strokeMiterLimit : 1.414;
+        var strokeLineCap:String = style.strokeLineCap == "" ? style.strokeLineCap : "none";
+        var strokeLineJoin:String = style.strokeLineJoin == "" ? style.strokeLineJoin : "miter";
+        graphics.lineStyle(strokeWidth , style.stroke, strokeOpacity, false/*<-pixelHinting*/, "normal"/*<-lineScaleMode*/, strokeLineCap, strokeLineJoin , strokeMiterLimit);
+    }
 }
