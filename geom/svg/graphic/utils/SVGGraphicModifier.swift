802ab7faf857007398b9af6e7d6b1da2fb73ec80
Eonist <unicornhive@gmail.com>
2016-01-08 14:17:33 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -6,13 +6,13 @@ class SVGGraphicModifier {
     * @Note: If @param strokeStyle Does not have a variable, the default flash variable is applied
     */
     class func applyStrokeStyle(graphics:Graphics, style:SVGStyle) {
-        var strokeWidth:CGFloat = !(style.strokeWidth.isNaN) ? style.strokeWidth : 0;
-        var strokeMiterLimit:CGFloat = !(style.strokeMiterLimit.isNaN) ? style.strokeMiterLimit : 1.414;
-        var strokeLineCap:String = style.strokeLineCap! == "" ? style.strokeLineCap! : "none";
-        var strokeLineJoin:String = style.strokeLineJoin == "" ? style.strokeLineJoin! : "miter";
+        let strokeWidth:CGFloat = !(style.strokeWidth.isNaN) ? style.strokeWidth : 0;
+        let strokeMiterLimit:CGFloat = !(style.strokeMiterLimit.isNaN) ? style.strokeMiterLimit : 1.414;
+        let strokeLineCap:String = style.strokeLineCap! == "" ? style.strokeLineCap! : "none";
+        let strokeLineJoin:String = style.strokeLineJoin == "" ? style.strokeLineJoin! : "miter";
         /*color*/
         let colorVal:Double = !(style.fill as! Double).isNaN ? style.fill as! Double : Double(0x000000)
-        var strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
+        let strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
         let color:NSColor = NSColorParser.nsColor(UInt(colorVal), strokeOpacity)
         graphics.line(strokeWidth, color, Utils.strokeLineCap(strokeLineCap), Utils.strokeLineJoin(strokeLineJoin), strokeMiterLimit)
     }
