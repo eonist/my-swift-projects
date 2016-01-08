cc65ec6b2c0302147b5bb9dcff80959dfea46b0e
Eonist <unicornhive@gmail.com>
2016-01-08 21:25:37 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGPolygon.swift

--- geom/svg/SVGPolygon.swift
+++ geom/svg/SVGPolygon.swift
@@ -12,17 +12,14 @@ class SVGPolygon:SVGGraphic,ISVGPolyLine{
         Swift.print("SVGPolygon.drawFill()")
         
         let boundingBox:CGRect = PointParser.rectangle(points)/*We need the bounding box in order to set the frame*/
-        fillShape.path = CGPathParser.lines(points,true,CGPoint(-boundingBox.x,-boundingBox.y))
+        fillShape.path = CGPathParser.lines(points,true,CGPoint(-boundingBox.x,-boundingBox.y))/*<--We offset so that the lines draw from 0,0 relative to the frame*/
         Swift.print("fillShape.path: " + "\(fillShape.path)")
-        fillShape.frame = boundingBox
+        fillShape.frame = boundingBox/*The positioning happens in the frame*/
         Swift.print("SVGPolygon.draw() boundingBox: " + "\(boundingBox)")
         /*line*/
-        let lineOffsetRect = RectGraphicUtils.lineOffsetRect(rect, style!.strokeWidth, OffsetType(OffsetType.center))
+        let lineOffsetRect = RectGraphicUtils.lineOffsetRect(boundingBox, style!.strokeWidth, OffsetType(OffsetType.center))
         lineShape.frame = lineOffsetRect.lineFrameRect
-        lineShape.path = lineOffsetRect.lineRect.path
-        
-    
+        lineShape.path = fillShape.path.copy()
     }
-    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
