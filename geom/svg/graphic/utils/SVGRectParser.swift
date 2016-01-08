a7f53d0939d4fe42d53991a83336e89e688ae172
Eonist <unicornhive@gmail.com>
2016-01-08 09:39:40 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGRectParser.swift

--- geom/svg/graphic/utils/SVGRectParser.swift
+++ geom/svg/graphic/utils/SVGRectParser.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 class SVGRectParser {
-
+    /**
+     * Returns an Rectangle instance with data derived frompa svgRect
+     * @Note: if the svgRect x and or y is of the value NaN, then these are transfered as 0
+     */
+    class func rectangle(svgRect:SVGRect)->CGRect {
+        
+        //continue here
+        
+        return CGRect(!isNaN(svgRect.x) ? svgRect.x : 0, !isNaN(svgRect.y) ? svgRect.y : 0, svgRect.width, svgRect.height);
+    }
 }
