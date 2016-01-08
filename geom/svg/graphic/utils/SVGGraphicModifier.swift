3b19247b2767c06ff0c30a02f8d109ddb15143e7
Eonist <unicornhive@gmail.com>
2016-01-08 16:38:39 +0100

    Files modified: 2
    Modified 2 files:geom/svg/graphic/utils/SVGGraphicModifier.swiftgeom/svg/style/SVGStyle.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -12,7 +12,7 @@ class SVGGraphicModifier {
         let strokeLineJoin:String = style.strokeLineJoin != nil && style.strokeLineJoin! == "" ? style.strokeLineJoin! : "miter";
         /*color*/
         Swift.print("style.fill: " + "\(style.fill)")
-        let colorVal:Double = !(style.stroke as! Double).isNaN ? style.fill as! Double : Double(0x000000)
+        let colorVal:Double = !(style.stroke.isNaN) ? style.fill as! Double : Double(0x000000)
         Swift.print("colorVal: " + "\(colorVal)")
         let strokeOpacity:CGFloat = !(style.strokeOpacity.isNaN) ? style.strokeOpacity : 1;
         Swift.print("strokeOpacity: " + "\(strokeOpacity)")
--- geom/svg/style/SVGStyle.swift
+++ geom/svg/style/SVGStyle.swift
@@ -7,7 +7,7 @@ class SVGStyle {
     var fillOpacity : CGFloat/*A number ranging from 0.0 to 1.0; 0.0 is entirely transparent, 1.0 is entirely opaque.*/
     var fillRule : String?/*not implimented yet, This attribute can have the values "nonzero" or "evenodd", which apply different rules for determining whether a point is inside or outside a shape. These rules generate different effects only when a shape has intersecting lines or "holes" in it. Details are in "Filling Polygons That Have Intersecting Lines" earlier in this chapter.*/
     var strokeWidth : CGFloat/*Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
-    var stroke : Any?;
+    var stroke : Double;/*TODO: Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
     var strokeOpacity : CGFloat;
     var strokeLineCap : String?;
     var strokeLineJoin : String?;
