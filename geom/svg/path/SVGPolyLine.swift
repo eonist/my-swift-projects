a5d14e87a38c8737eb41418d410029bedf2a35d8
Eonist <unicornhive@gmail.com>
2016-01-09 17:01:55 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/SVGPolyLine.swift

--- geom/svg/path/SVGPolyLine.swift
+++ geom/svg/path/SVGPolyLine.swift
@@ -1,19 +1,24 @@
 import Foundation
-
-class SVGPolyLine : SVGGraphic, ISVGPolyLine{
-    var points : Array<CGPoint>;
-    init(_ points:Array<CGPoint>, _ style : SVGStyle? = nil, _ id : String? = nil) {
-        self.points = points;
-        super.init(style, id);
+/**
+ * this is basically Polygon without the fill part
+ */
+class SVGPolyLine : SVGPolygon{
+    //var points : Array<CGPoint>;
+    override init(_ points:Array<CGPoint>, _ style : SVGStyle? = nil, _ id : String? = nil) {
+        //self.points = points;
+        super.init(points,style, id);
     }
     
-    override func draw()  {
+    //override func draw()  {
         //swift.print("SVGPolygon.draw"+_points);
         //graphic.lineShape = CGPathParser.lines(points)
         
         
-        //continue here: this is basically Polygon without the fill part
+        //continue here:
         
+    //}
+    override func drawFill() {
+        //we override fill because we only need to draw a stroke
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
