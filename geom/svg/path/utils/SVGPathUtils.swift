4f556fd4771268c45846c94b5f3368c98ca38b87
Eonist <unicornhive@gmail.com>
2016-01-09 21:30:36 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/utils/SVGPathUtils.swift

--- geom/svg/path/utils/SVGPathUtils.swift
+++ geom/svg/path/utils/SVGPathUtils.swift
@@ -9,16 +9,16 @@ class SVGPathUtils {
         var path:IPath = Path()
         var i:Int = 0;/*parameterIndex*/
         var prevP:CGPoint = CGPoint();
-        var prevM:CGPoint;/*previous MoveTo pos*/
-        var prevC:CGPoint;/*previous ControlPoint*/
+        //var prevM:CGPoint/*previous MoveTo pos*/
+        var prevC:CGPoint/*previous ControlPoint*/
         for (var e : Int = 0; e < commands.count; e++) {
-            var command:String = commands[e];
-            var isLowerCase:Bool = StringAsserter.lowerCase(command);
+            let command:String = commands[e];
+            let isLowerCase:Bool = StringAsserter.lowerCase(command);
             var pos:CGPoint = isLowerCase ? prevP.copy() : CGPoint();/*the current end pos*/
             switch(command.lowercaseString){
                 case SVGPathCommand.m: //moveTo
                     pos += CGPoint(params[i],params[i+1]);
-                    prevM = pos.copy()
+                    //prevM = pos.copy()
                     path.commands.append(PathCommand.MOVE_TO);
                     path.pathData += [pos.x,pos.y]/*<-- used to be path.pathData.append(pos.x,pos.y);*/
                     i += 2;
@@ -43,7 +43,7 @@ class SVGPathUtils {
                     break;
                 case SVGPathCommand.c://cubicCurveTo
                     pos += CGPoint(params[i+4],params[i+5]);
-                    var controlP1:CGPoint = isLowerCase ? CGPoint(prevP.x + CGFloat(params[i]),prevP.y+CGFloat(params[i+1])) : CGPoint(params[i],params[i+1]);
+                    let controlP1:CGPoint = isLowerCase ? CGPoint(prevP.x + CGFloat(params[i]),prevP.y+CGFloat(params[i+1])) : CGPoint(params[i],params[i+1]);
                     prevC = isLowerCase ? CGPoint(prevP.x+CGFloat(params[i+2]),prevP.y+CGFloat(params[i+3])) : CGPoint(params[i+2],params[i+3]);/*aka controlP2*/
                     path.commands.append(PathCommand.CUBIC_CURVE_TO);
                     path.pathData += [controlP1.x,controlP1.y, prevC.x,prevC.y, pos.x,pos.y]
@@ -51,7 +51,7 @@ class SVGPathUtils {
                     break;
                 case SVGPathCommand.s://smoothCubicCurveTo
                     pos += CGPoint(params[i+2],params[i+3]);
-                    var cP1:CGPoint = CGPoint(2 * prevP.x - prevC.x,2 * prevP.y - prevC.y);/*x2 = 2 * x - x1 and y2 = 2 * y - y1*/
+                    let cP1:CGPoint = CGPoint(2 * prevP.x - prevC.x,2 * prevP.y - prevC.y);/*x2 = 2 * x - x1 and y2 = 2 * y - y1*/
                     prevC = isLowerCase ? CGPoint(CGFloat(params[i])+prevP.x,CGFloat(params[i+1])+prevP.y) : CGPoint(params[i],params[i+1]);
                     path.commands.append(PathCommand.CUBIC_CURVE_TO);
                     path.pathData += [cP1.x,cP1.y, prevC.x,prevC.y, pos.x,pos.y]
@@ -76,10 +76,10 @@ class SVGPathUtils {
 //						DisplayArc4Modifier.arcTo(graphics, path.params[i], path.params[i+1], path.params[i+2], path.params[i+3],path.params[i+4], path.params[i+5],path.params[i+6]);
 //						i += 7;
 //						break;
-                default: fatalError("");
+                default: fatalError("type not supported");
             }
-            if(e < commands.length-1 /*&& StringAsserter.lowerCase(commands[i+1])*/) {// :TODO: check for z?
-                prevP = pos.clone();
+            if(e < commands.count-1 /*&& StringAsserter.lowerCase(commands[i+1])*/) {// :TODO: check for z?
+                prevP = pos.copy();
             }
         }
         return path;
