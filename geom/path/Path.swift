a5a2832f8c46b9e087f643868afc5bfe6568eb30
Eonist <unicornhive@gmail.com>
2016-01-09 21:02:46 +0100

    Files modified: 1
    Modified 1 file:geom/path/Path.swift

--- geom/path/Path.swift
+++ geom/path/Path.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class Path {
-
+    var commands:Array<Int>/* = Array<Int>();*/// :TODO: does this need to be created? isnt it passed through argument
+    var pathData:Array<CGFloat>/* = Array<CGFloat>();*/// :TODO: does this need to be created? isnt it passed through argument
+    var rotation:CGFloat;/*in degrees*/// :TODO: is this much used?// :TODO: if we can have roation why cant we have matrix?
+    var name:String = "";/*temp*/// :TODO: why is this temp?
+    init(commands:Array<Int>,pathData:Array<CGFloat>,rotation:CGFloat = 0) {
+        self.commands = commands;
+        self.pathData = pathData;
+        self.rotation = rotation;
+    }
 }
