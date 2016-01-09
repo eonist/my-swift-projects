be324cdb65682047a3e7334389d2cd66d5af5cb6
Eonist <unicornhive@gmail.com>
2016-01-09 13:24:40 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -24,25 +24,7 @@ class SVGGraphicModifier {
         graphics.line(strokeWidth, color, Utils.strokeLineCap(strokeLineCap), Utils.strokeLineJoin(strokeLineJoin), strokeMiterLimit)
     }
 }
-/**
- * DISCUSSION:Probably move this into a more central place, but in the spirit of moving on!, Or not since its probably only related to how svg names these variables
- * TODO: try to find a method in swift that can extract enum values by providing a string
- */
+
 private class Utils{
     
-    //continue here: add these to an utility class, and then write a note that you should probably convert these values upon style creation and use nil to symbolize no value
-    //then continue with the SVGPolygon
-    
-    class func strokeLineCap(strokeLineCap:String)->CGLineCap{
-        if(strokeLineCap == "butt"){return CGLineCap.Butt}
-        else if(strokeLineCap == "round"){return CGLineCap.Round}
-        else if(strokeLineCap == "square"){return CGLineCap.Square}
-        else{fatalError(strokeLineCap + " not supported")}
-    }
-    class func strokeLineJoin(strokeLineJoin:String)->CGLineJoin{
-        if(strokeLineJoin == "miter"){return CGLineJoin.Miter}
-        else if(strokeLineJoin == "round"){return CGLineJoin.Round}
-        else if(strokeLineJoin == "Bevel"){return CGLineJoin.Bevel}
-        else{fatalError(strokeLineJoin + " not supported")}
-    }
 }
