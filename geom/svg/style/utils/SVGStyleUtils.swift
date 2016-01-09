5b8cf64b88aa458477c497349bf341a3f53d4272
Eonist <unicornhive@gmail.com>
2016-01-09 13:58:00 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleUtils.swift

--- geom/svg/style/utils/SVGStyleUtils.swift
+++ geom/svg/style/utils/SVGStyleUtils.swift
@@ -12,17 +12,17 @@ class SVGStyleUtils {
         if(strokeLineCap == "butt"){return CGLineCap.Butt}
         else if(strokeLineCap == "round"){return CGLineCap.Round}
         else if(strokeLineCap == "square"){return CGLineCap.Square}
-        else{fatalError("lineCap not supported")}
+        else{fatalError("this lineCap type is not supported")}
     }
     /**
      *
      */
-    class func lineJoin(lineJoin:String)->CGLineJoin{
+    class func lineJoin(lineJoin:String?)->CGLineJoin{
         let strokeLineJoin = lineJoin != nil && lineJoin != "" ? lineJoin : "miter";
         if(strokeLineJoin == "miter"){return CGLineJoin.Miter}
         else if(strokeLineJoin == "round"){return CGLineJoin.Round}
         else if(strokeLineJoin == "Bevel"){return CGLineJoin.Bevel}
-        else{fatalError(strokeLineJoin + " not supported")}
+        else{fatalError("this strokeLineJoin type is not supported")}
     }
     /**
      *
