668fe43498da9b06fd95d634ec718ce6a934e1ad
Eonist <unicornhive@gmail.com>
2016-01-09 13:40:25 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleUtils.swift

--- geom/svg/style/utils/SVGStyleUtils.swift
+++ geom/svg/style/utils/SVGStyleUtils.swift
@@ -1,20 +1,28 @@
 import Foundation
-
+/**
+ * DISCUSSION:Probably move this into a more central place, but in the spirit of moving on!, Or not since its probably only related to how svg names these variables
+ * NOTE: these methods provide a central place to convert values from the svg value scheme to the quartz value scheme
+ */
 class SVGStyleUtils {
     /**
-     * DISCUSSION:Probably move this into a more central place, but in the spirit of moving on!, Or not since its probably only related to how svg names these variables
      * TODO: try to find a method in swift that can extract enum values by providing a string
      */
-    class func strokeLineCap(strokeLineCap:String)->CGLineCap{
+    class func lineCap(strokeLineCap:String)->CGLineCap{
         if(strokeLineCap == "butt"){return CGLineCap.Butt}
         else if(strokeLineCap == "round"){return CGLineCap.Round}
         else if(strokeLineCap == "square"){return CGLineCap.Square}
         else{fatalError(strokeLineCap + " not supported")}
     }
-    class func strokeLineJoin(strokeLineJoin:String)->CGLineJoin{
+    class func lineJoin(strokeLineJoin:String)->CGLineJoin{
         if(strokeLineJoin == "miter"){return CGLineJoin.Miter}
         else if(strokeLineJoin == "round"){return CGLineJoin.Round}
         else if(strokeLineJoin == "Bevel"){return CGLineJoin.Bevel}
         else{fatalError(strokeLineJoin + " not supported")}
+    }
+    /**
+     * 
+     */
+    class func miterLimit(strokeMiterLimit:CGFloat)->CGFloat{
+        return !strokeMiterLimit.isNaN ? strokeMiterLimit : 10/*<--was 1.414*/;
     }
 }
