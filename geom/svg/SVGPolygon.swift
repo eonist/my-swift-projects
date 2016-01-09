dc4004efbaa3f7e0d703a771b1d2346eeb774ea2
Eonist <unicornhive@gmail.com>
2016-01-09 18:12:16 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGPolygon.swift

--- geom/svg/SVGPolygon.swift
+++ geom/svg/SVGPolygon.swift
@@ -13,13 +13,6 @@ class SVGPolygon:SVGGraphic,ISVGPolyLine{
      * @NOTE: this method calls two sub methods so that SVGPolyLine can use this class as a base method
      */
     override func draw() {
-        drawFill()
-        drawLine(true)
-    }
-    /**
-     *
-     */
-    func drawFill(){
         //Swift.print("SVGPolygon.drawFill()")
         /*Fill*/
         let boundingBox:CGRect = PointParser.rectangle(points)/*We need the bounding box in order to set the frame*/
@@ -29,8 +22,6 @@ class SVGPolygon:SVGGraphic,ISVGPolyLine{
         fillShape.frame = boundingBox/*The positioning happens in the frame*/
         //Swift.print("SVGPolygon.draw() boundingBox: " + "\(boundingBox)")
         /*line*/
-        let boundingBox:CGRect = PointParser.rectangle(points)/*We need the bounding box in order to set the frame*/
-        
         let strokeBoundingBox:CGRect = Utils.boundingBox(fillShape.path, style!)// + boundingBox.origin
         //Swift.print("strokeBoundingBox: " + "\(strokeBoundingBox)")
         
@@ -39,10 +30,9 @@ class SVGPolygon:SVGGraphic,ISVGPolyLine{
         
         //let lineOffsetRect = RectGraphicUtils.lineOffsetRect(strokeBoundingBox, style!.strokeWidth, OffsetType(OffsetType.center))
         lineShape.frame = (strokeBoundingBox + boundingBox.origin).copy()
-        lineShape.path = CGPathParser.lines(points,close,CGPoint(-boundingBox.x,-boundingBox.y) + linePathOffset)
+        lineShape.path = CGPathParser.lines(points,true,CGPoint(-boundingBox.x,-boundingBox.y) + linePathOffset)
         
     }
-
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
 private class Utils{
