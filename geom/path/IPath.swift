215b24bccb9af07870742f55b4025143a468e37b
Eonist <unicornhive@gmail.com>
2016-01-09 21:04:45 +0100

    Files modified: 1
    Modified 1 file:geom/path/IPath.swift

--- geom/path/IPath.swift
+++ geom/path/IPath.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class IPath {
-
+protocol IPath {
+    var commands:Array<Int>{get set}
+    var pathData:Array<CGFloat>{get set}
+    var rotation:CGFloat{get set}
+    var name:String{get set}
 }
