9dbfd195ced860e6b2180a50990e273d80b40ecf
Eonist <unicornhive@gmail.com>
2016-01-10 18:44:12 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/utils/SVGPathUtils.swift

--- geom/svg/path/utils/SVGPathUtils.swift
+++ geom/svg/path/utils/SVGPathUtils.swift
@@ -73,8 +73,8 @@ class SVGPathUtils {
                     break;
                 case SVGPathCommand.z:
                     Swift.print("close path")
-                    CGPathMoveToPoint(path, nil, prevM.x, prevM.y);
                     CGPathCloseSubpath(path);
+                    CGPathMoveToPoint(path, nil, prevM.x, prevM.y);/*<--unsure if this is needed?*/
                     break;/*closes it self to the prev MT pos*/
                     //					case PathCommand.ARC_TO:
                     //						DisplayArc4Modifier.arcTo(graphics, path.params[i], path.params[i+1], path.params[i+2], path.params[i+3],path.params[i+4], path.params[i+5],path.params[i+6]);
