9f7504fcfbac403e9e9e46c5e782a5b965a44e23
Eonist <unicornhive@gmail.com>
2016-01-10 13:22:15 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/SVGPath.swift

--- geom/svg/path/SVGPath.swift
+++ geom/svg/path/SVGPath.swift
@@ -38,9 +38,9 @@ class SVGPath :SVGGraphic{
         //Swift.print("strokeBoundingBox: " + "\(strokeBoundingBox)")
         let linePathOffset:CGPoint = PointParser.difference(strokeBoundingBox.origin,CGPoint(0,0))
         Swift.print("linePathOffset: " + "\(linePathOffset)")
-        //let lineOffsetRect = RectGraphicUtils.lineOffsetRect(strokeBoundingBox, style!.strokeWidth, OffsetType(OffsetType.center))
         lineShape.frame = (strokeBoundingBox + boundingBox.origin).copy()
-        lineShape.path = offsetPath.copy()
+        lineShape.path = fillShape.path.copy()
+    
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
