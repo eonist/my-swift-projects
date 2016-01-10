fc1d0c06080ab03f9f50037b199873a23dd44191
Eonist <unicornhive@gmail.com>
2016-01-10 10:59:19 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleParser.swift

--- geom/svg/style/utils/SVGStyleParser.swift
+++ geom/svg/style/utils/SVGStyleParser.swift
@@ -9,7 +9,7 @@ class SVGStyleParser {
 		//ObjectDescriber.describe(inlineStyle);
 		let fill:Any = SVGStyleParser.fill(inlineStyle["fill"], container)
 		let fillOpacity:CGFloat = SVGPropertyParser.value(inlineStyle["fill-opacity"])
-		let fillRule:String = inlineStyle["fill-rule"]!
+		let fillRule:String? = inlineStyle["fill-rule"]!
 		let stroke:Double = SVGStyleParser.stroke(inlineStyle["stroke"])
 		let strokeWidth:CGFloat = SVGPropertyParser.value(inlineStyle["stroke-width"])
 		let strokeOpacity:CGFloat = SVGPropertyParser.value(inlineStyle["stroke-opacity"])
