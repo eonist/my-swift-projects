80d817f438c5d09734bdd32fcf6d6b21d48d1331
Eonist <unicornhive@gmail.com>
2016-01-11 12:48:24 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -2,12 +2,12 @@ import Cocoa
 
 class SVGGraphicModifier {
     /**
-    * Applies the @param strokeStyle to the @param graphics
-    * @Note: If @param strokeStyle Does not have a variable, the default flash variable is applied
-    * @NOTE: For example, a miter limit of 1.414 converts miters to bevels for theta less than 90 degrees, a limit of 4.0 converts them for theta less than approximately 29 degrees, and a limit of 10.0 converts them for theta less than approximately 11.5 degrees.
-    * @NOTE: miterLength / stroke-width = 1 / sin ( theta / 2 ) The ratio of miter length (distance between the outer tip and the inner corner of the miter) to stroke-width is directly related to the angle (theta) between the segments in user space by the formula:
+     * Applies the @param strokeStyle to the @param graphics
+     * @Note: If @param strokeStyle Does not have a variable, the default flash variable is applied
+     * @NOTE: For example, a miter limit of 1.414 converts miters to bevels for theta less than 90 degrees, a limit of 4.0 converts them for theta less than approximately 29 degrees, and a limit of 10.0 converts them for theta less than approximately 11.5 degrees.
+     * @NOTE: miterLength / stroke-width = 1 / sin ( theta / 2 ) The ratio of miter length (distance between the outer tip and the inner corner of the miter) to stroke-width is directly related to the angle (theta) between the segments in user space by the formula:
      * @NOTE: miterlimit The limit on the ratio of the miter length to the stroke-width. The value of <miterlimit> must be a <number> greater than or equal to 1.
-    */
+     */
     class func applyStrokeStyle(graphics:Graphics, _ style:SVGStyle) {
         let strokeWidth:CGFloat = SVGStyleUtils.strokeWidth(style.strokeWidth)
         let strokeMiterLimit:CGFloat = SVGStyleUtils.miterLimit(style.strokeMiterLimit)
@@ -56,6 +56,7 @@ class SVGGraphicModifier {
             //there is also this: gradientTransform="rotate(-50), there is also: gradientTransform="rotate(90, 50, 30)" the origin of the rotation would be 50, 30
             
             //The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.
+            
             
             //<linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
             
