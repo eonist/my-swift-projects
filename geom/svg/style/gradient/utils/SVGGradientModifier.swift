433ec01348130a6959df5741d2fa9361f615bba8
Eonist <unicornhive@gmail.com>
2016-01-11 15:06:24 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/utils/SVGGradientModifier.swift

--- geom/svg/style/gradient/utils/SVGGradientModifier.swift
+++ geom/svg/style/gradient/utils/SVGGradientModifier.swift
@@ -6,6 +6,7 @@ class SVGGradientModifier {
 	 * @Note works for SVGRadialGradient and SVGLinearGradient
 	 */
 	class func scale(gradient:SVGGradient,_ pivot:CGPoint,_ scale:CGPoint) {
+        fatalError("not implemented yet")
         //if(gradient.gradientTransform != nil) {MatrixModifier.scale(gradient.gradientTransform, pivot, scale)}/*Scale the current applied matrix*/
         //else {gradient.gradientTransform = MatrixModifier.scale(new Matrix(), pivot, scale)}/*if there is no gradientTransform allready applied to the Gradient instnace, then apply a new Matrix instance w/ the correct scale*/
 	}
