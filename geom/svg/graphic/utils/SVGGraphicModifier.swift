038e5595890d8a09bae0dc4ae15e962333df79ac
Eonist <unicornhive@gmail.com>
2016-01-11 14:50:24 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -46,8 +46,8 @@ class SVGGraphicModifier {
             
             let userSpaceOnUse:Bool = gradient.gradientUnits == "userSpaceOnUse";////The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.
             
-            let p1:CGPoint? = userSpaceOnUse && !gradient.x1.isNaN && !gradient.y1.isNaN ? CGPoint(gradient.x1,gradient.y1) :nil
-            let p2:CGPoint? = userSpaceOnUse && !gradient.x2.isNaN && !gradient.y2.isNaN ? CGPoint(gradient.x2,gradient.y2) :nil
+            var p1:CGPoint? = userSpaceOnUse && !gradient.x1.isNaN && !gradient.y1.isNaN ? CGPoint(gradient.x1,gradient.y1) :nil
+            var p2:CGPoint? = userSpaceOnUse && !gradient.x2.isNaN && !gradient.y2.isNaN ? CGPoint(gradient.x2,gradient.y2) :nil
             
             if(userSpaceOnUse){
                 p1 -= shape.frame.origin
