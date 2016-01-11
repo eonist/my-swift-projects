a0da22bd5ba7823fe9ef6bb6ff224d41bd0bfa14
Eonist <unicornhive@gmail.com>
2016-01-11 20:01:16 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGPolygonModifier.swift

--- geom/svg/graphic/utils/SVGPolygonModifier.swift
+++ geom/svg/graphic/utils/SVGPolygonModifier.swift
@@ -2,11 +2,11 @@ import Foundation
 
 class SVGPolygonModifier {
     /**
-     *
+     * @NOTE: we need to scale the 
      */
     class func scale(svgPolygon:SVGPolygon,_ pivot:CGPoint,_ scalePoint:CGPoint) {
         if(svgPolygon.style != nil && svgPolygon.style!.fill is SVGGradient){
-            (svgPolygon.style!.fill as! SVGGradient).gradientTransform
+            SVGGradientModifier.scaleGradient(&(svgPolygon.style!.fill as! SVGGradient).gradientTransform,pivot,scalePoint)
         }
         svgPolygon.points = PointModifier.scalePoints(svgPolygon.points, pivot, scalePoint)
     }
