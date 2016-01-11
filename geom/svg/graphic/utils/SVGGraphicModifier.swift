744e2b0494692168b932ba2ba36348348d249ab6
Eonist <unicornhive@gmail.com>
2016-01-11 12:08:27 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -53,7 +53,7 @@ class SVGGraphicModifier {
             
             //objectBoundingBox — x1, y1, x2, y2 represent coordinates in a system established by the bounding box of the element to which the gradient is applied. In other words the gradient scales with the element it’s applied to.
             
-            //gradientTransform="rotate(-50)
+            //there is also this: gradientTransform="rotate(-50)
             
             //The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.
             
