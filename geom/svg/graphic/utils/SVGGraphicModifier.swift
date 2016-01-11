9ff76573abc3b1818f14e69b3a396faa93980825
Eonist <unicornhive@gmail.com>
2016-01-11 15:00:12 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -20,6 +20,11 @@ class SVGGraphicModifier {
      * Begins a gradient fill on @param graphics with @param gradient
      * @NOTE: we use the Shape instance here because we need the frame offset to calculate the correct gradient p1 and p2 when using userspace
      * @NOTE: userspace uses real coordinates, nonuserspace uses relative coordinates 0 - 1 etc
+     * @NOTE: userSpaceOnUse — x1, y1, x2, y2 represent coordinates in the current user coordinate system. In other words the values in the gradient are absolute values.
+     * @NOTE: objectBoundingBox — x1, y1, x2, y2 represent coordinates in a system established by the bounding box of the element to which the gradient is applied. In other words the gradient scales with the element it’s applied to.
+     * TODO: there is also: gradientTransform="rotate(90, 50, 30)" the origin of the rotation would be 50, 30
+     * @NOTE: <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+     * @NOTE: The cx, cy and r attributes define the outermost circle and the fx and fy define the innermost circle
      */
     class func beginGradientFill(shape:Shape,_ gradient:SVGGradient) {
         let graphics:Graphics = shape.graphics
@@ -31,9 +36,9 @@ class SVGGraphicModifier {
             //matrix.concat(gradient.gradientTransform)
         }
         //var spreadMethod:String = gradient.spreadMethod || SpreadMethod.PAD;
-        //			trace("spreadMethod: " + spreadMethod);
+        //Swift.print("spreadMethod: " + spreadMethod);
         //var interpolationMethod:String = InterpolationMethod.RGB;/*InterpolationMethod.LINEAR_RGB*/
-        //			trace("interpolationMethod: " + interpolationMethod);
+        //Swift.print("interpolationMethod: " + interpolationMethod);
         //let focalPointRatio:CGFloat = 0;/*from -1 to 1;*/
         if(gradient is SVGRadialGradient && !((gradient as! SVGRadialGradient).fx).isNaN) {
             //focalPointRatio = Utils.focalPointRatio(gradient as! SVGRadialGradient);
@@ -55,20 +60,6 @@ class SVGGraphicModifier {
                 p2 -= shape.frame.origin
             }
             
-            
-            //userSpaceOnUse — x1, y1, x2, y2 represent coordinates in the current user coordinate system. In other words the values in the gradient are absolute values.
-            
-            //objectBoundingBox — x1, y1, x2, y2 represent coordinates in a system established by the bounding box of the element to which the gradient is applied. In other words the gradient scales with the element it’s applied to.
-            
-            //there is also this: gradientTransform="rotate(-50), there is also: gradientTransform="rotate(90, 50, 30)" the origin of the rotation would be 50, 30
-            
-            
-            
-            
-            //
-            //<radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-            
-            //The cx, cy and r attributes define the outermost circle and the fx and fy define the innermost circle
             //Swift.print("gradient.colors: " + "\(gradient.colors)")
             //Swift.print("gradient.offsets: " + "\(gradient.offsets)")
             
