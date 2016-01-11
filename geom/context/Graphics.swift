31598e875d8ba4185132f662298f62122672cf24
Eonist <unicornhive@gmail.com>
2016-01-11 17:54:53 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -200,9 +200,9 @@ private class Utils{
         //Swift.print("drawAxialGradient.rotation: " + "\(rotation)")
         Swift.print("gradient.p1: " + "\(gradient.p1)")
         Swift.print("gradient.p2: " + "\(gradient.p2)")
-        let points:(start:CGPoint,end:CGPoint) = gradient.p1 != nil && gradient.p2 != nil ? (start:gradient.p1!,end:gradient.p2!) : GradientBoxUtils.points(boundingBox, gradient.rotation) /*GradientBox*/
+        var points:(start:CGPoint,end:CGPoint) = gradient.p1 != nil && gradient.p2 != nil ? (start:gradient.p1!,end:gradient.p2!) : GradientBoxUtils.points(boundingBox, gradient.rotation) /*GradientBox*/
         Swift.print("points: " + "\(points)")
-        CGRectApplyAffineTransform(<#T##rect: CGRect##CGRect#>, <#T##t: CGAffineTransform##CGAffineTransform#>)
+        CGPointApplyAffineTransform(points.start, gradient.transformation)
         CGContextDrawLinearGradient(context, cgGradient, points.start, points.end , [CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation])//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
     }
     /**
