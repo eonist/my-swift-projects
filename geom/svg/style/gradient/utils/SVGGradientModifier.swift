e8cbccd56e5880c6837617aa77215b32b87904ca
Eonist <unicornhive@gmail.com>
2016-01-11 19:19:11 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/utils/SVGGradientModifier.swift

--- geom/svg/style/gradient/utils/SVGGradientModifier.swift
+++ geom/svg/style/gradient/utils/SVGGradientModifier.swift
@@ -8,19 +8,19 @@ class SVGGradientModifier {
 	class func scale(gradient:SVGGradient,_ pivot:CGPoint,_ scale:CGPoint) {
         Swift.print("SVGGradientModifier.scale()")
         //fatalError("not implemented yet")
-        
+        scaleGradient(&gradient.gradientTransform,pivot,scale)
         Swift.print("gradient.gradientTransform: " + "\(gradient.gradientTransform)")
     }
     /**
      *
      */
-    class func scaleGradient(gradientTransform:CGAffineTransform?,_ pivot:CGPoint,_ scale:CGPoint)->CGAffineTransform{
+    class func scaleGradient(inout gradientTransform:CGAffineTransform?,_ pivot:CGPoint,_ scale:CGPoint){
         Swift.print("scaleGradient")
         if(gradientTransform != nil) {
-            gradientTransform?.scaleFromPoint(scale.x, scale.y, pivot)
+            gradientTransform!.scaleFromPoint(scale.x, scale.y, pivot)
         }/*Scale the current applied matrix*/
         else {
-            gradientTransform = CGAffineTransform.scaleFromPoint(CGAffineTransformIdentity, scale.x, scale.y, pivot)
+            CGAffineTransform.scaleFromPoint(CGAffineTransformIdentity, scale.x, scale.y, pivot)
         }/*if there is no gradientTransform allready applied to the Gradient instnace, then apply a new Matrix instance w/ the correct scale*/
     }
 }
