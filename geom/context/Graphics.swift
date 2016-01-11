8f47f591b32445dcd4bdba11b1c5f6010c675f7e
Eonist <unicornhive@gmail.com>
2016-01-11 10:03:01 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -198,7 +198,7 @@ private class Utils{
      */
     class func drawAxialGradient(path:CGPath,_ context:CGContextRef,_ cgGradient:CGGradientRef?, _ boundingBox:CGRect, _ gradient:IGradient){
         //Swift.print("drawAxialGradient.rotation: " + "\(rotation)")
-        let points = gradient.p1 != nil && gradient.p2 != nil ? GradientBoxUtils.points(boundingBox, gradient.rotation) : [gradient.p1,gradient.p2]/*GradientBox*/
+        let points = gradient.p1 != nil && gradient.p2 != nil ? GradientBoxUtils.points(boundingBox, gradient.rotation) : (start:gradient.p1,end:gradient.p2)/*GradientBox*/
         CGContextDrawLinearGradient(context, cgGradient, points.start, points.end , [CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation])//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
     }
     /**
