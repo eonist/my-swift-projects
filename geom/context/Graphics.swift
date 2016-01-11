21a006d83a3b03458c7591a0c4bf61ed8e000437
Eonist <unicornhive@gmail.com>
2016-01-11 17:38:27 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -202,6 +202,7 @@ private class Utils{
         Swift.print("gradient.p2: " + "\(gradient.p2)")
         let points:(start:CGPoint,end:CGPoint) = gradient.p1 != nil && gradient.p2 != nil ? (start:gradient.p1!,end:gradient.p2!) : GradientBoxUtils.points(boundingBox, gradient.rotation) /*GradientBox*/
         Swift.print("points: " + "\(points)")
+        
         CGContextDrawLinearGradient(context, cgGradient, points.start, points.end , [CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation])//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
     }
     /**
