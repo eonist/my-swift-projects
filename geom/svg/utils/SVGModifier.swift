5ccd081b16baab823566e7c4f9fbfd811aba8dcb
Eonist <unicornhive@gmail.com>
2016-01-11 21:12:13 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -30,9 +30,17 @@ class SVGModifier {
 		switch(true){
 			case element is SVGPolyLine:(element as! SVGPolyLine).points = PointModifier.scalePoints((element as! SVGPolyLine).points, pivot, scale);/*SVGPolyLine,SVGPolygon*/break;
             case element is SVGPolygon:
-                Swift.print("points:" + "\((element as! SVGPolygon).points)")
-                
-                SVGPolygonModifier.scale(element as! SVGPolygon, pivot, scale);/*SVGPolyLine,SVGPolygon*/
+                //Swift.print("points:" + "\((element as! SVGPolygon).points)")
+                if(svgPolygon.style != nil && (element as! SVGPolygon).style!.fill is SVGGradient){
+                    SVGGradientModifier.scaleGradient(&(svgPolygon.style!.fill as! SVGGradient).gradientTransform,pivot,scale)
+                }
+                if(svgPolygon.style != nil && (element as! SVGPolygon).style!.fill is SVGGradient){
+                    SVGGradientModifier.scaleGradient(&(svgPolygon.style!.fill as! SVGGradient).gradientTransform,pivot,scale)
+                }
+                //Swift.print("svgPolygon.points: " + "\(svgPolygon.points.count)")
+                //Swift.print("pivot: " + "\(pivot)")
+                //Swift.print("scalePoint: " + "\(scalePoint)")
+                svgPolygon.points = PointModifier.scalePoints(svgPolygon.points, pivot, scalePoint)
                 
                 break;
 			case element is SVGRect:SVGRectModifier.scale(element as! SVGRect, pivot, scale);break;
