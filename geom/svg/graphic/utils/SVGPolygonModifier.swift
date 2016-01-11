ffc6e5aa06432d951f441cff2981a376dbfa374f
Eonist <unicornhive@gmail.com>
2016-01-11 20:33:36 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGPolygonModifier.swift

--- geom/svg/graphic/utils/SVGPolygonModifier.swift
+++ geom/svg/graphic/utils/SVGPolygonModifier.swift
@@ -4,11 +4,16 @@ class SVGPolygonModifier {
     /**
      * @NOTE: if the style.fill is a Gradient then we need to scale the transform matrix to reflect the new scale of the shape
      * TODO: we need to scale the gradient.transform of the stroke aswell if it exists
+     * TODO: the scaling of the style should be abstracted to the SVGGraphic so you can reuse the code.
      */
     class func scale(svgPolygon:SVGPolygon,_ pivot:CGPoint,_ scalePoint:CGPoint) {
+        Swift.print("SVGPolygonModifier.scale()")
         if(svgPolygon.style != nil && svgPolygon.style!.fill is SVGGradient){
             SVGGradientModifier.scaleGradient(&(svgPolygon.style!.fill as! SVGGradient).gradientTransform,pivot,scalePoint)
         }
+        Swift.print("svgPolygon.points: " + "\(svgPolygon.points)")
+        Swift.print("pivot: " + "\(pivot)")
+        Swift.print("scalePoint: " + "\(scalePoint)")
         svgPolygon.points = PointModifier.scalePoints(svgPolygon.points, pivot, scalePoint)
     }
 }
