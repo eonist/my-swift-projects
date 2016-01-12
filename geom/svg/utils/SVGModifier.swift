478b7f935fba7ff63fa22a69a00f84091173877b
Eonist <unicornhive@gmail.com>
2016-01-12 10:53:14 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -64,11 +64,11 @@ class SVGModifier {
 	 */
 	class func update(graphic:ISVGGraphic) {
         //WARNING: this method is incomplete, needs correct order of calls etc
-        
+        graphic.draw();/*<--draws the path with the new params*/
         graphic.beginFill();//we need to set this before we scale the frame etc, to get the right values
 		graphic.applyLineStyle();
 		
-		graphic.draw();/*<--draws the path with the new params*/
+		
 		//graphic.endFill();
         graphic.fillShape.setNeedsDisplay()/*there needs to be an update to the beginFill and applyLineStyll since gradient matrices may have changed etc, but the call must be a request not a direct call since the context isnt ready yet*/
         graphic.lineShape.setNeedsDisplay()
