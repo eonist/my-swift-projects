1bf076f761ac4c23d6f86337742121c049d39ec9
Eonist <unicornhive@gmail.com>
2016-01-12 08:37:58 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -74,6 +74,8 @@ class SVGGraphicModifier {
             Swift.print("points after offset: " + "\([p1,p2])")
             let grad:IGradient = Gradient(gradient.colors,gradient.offsets,gradientType,0,nil,nil,nil,nil,p1,p2,!userSpaceOnUse/*,gradient.gradientTransform*/)
             graphics.gradientFill(grad)
+            
+            //continue here: you need to calc the grad b4 you draw the path, simple as that
         }
     }
 }
