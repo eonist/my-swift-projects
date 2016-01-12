a47f81f1bbd74c68c15d87bb30df7a1cad3227bf
Eonist <unicornhive@gmail.com>
2016-01-12 08:29:56 +0100

    Files modified: 2
    Modified 2 files:geom/svg/style/gradient/SVGLinearGradient.swiftgeom/svg/style/gradient/utils/SVGGradientParser.swift

--- geom/svg/style/gradient/utils/SVGGradientParser.swift
+++ geom/svg/style/gradient/utils/SVGGradientParser.swift
@@ -22,7 +22,7 @@ class SVGGradientParser {
 		let y2:CGFloat = StringAsserter.percentage(y2Str) ? StringParser.percentage(y2Str) : SVGPropertyParser.value(y2Str);
 		let svgGradient:SVGGradient = Utils.gradient(xml);
         //Swift.print("SVGGradientParser.linearGradient svgGradient.id: " + "\(svgGradient.id)")
-		return SVGLinearGradient(svgGradient.offsets,svgGradient.colors/*svgGradient.opacities*/,x1,y1,x2,y2,svgGradient.gradientUnits,svgGradient.spreadMethod,svgGradient.id/*,svgGradient.gradientTransform*/);
+		return SVGLinearGradient(svgGradient.offsets,svgGradient.colors/*svgGradient.opacities*/,x1,y1,x2,y2,svgGradient.gradientUnits,svgGradient.spreadMethod,svgGradient.id,svgGradient.gradientTransform);
 	}
 	/**
 	 * Returns an gradient instance with data derived from @param xml 
