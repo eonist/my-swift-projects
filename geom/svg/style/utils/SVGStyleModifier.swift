696148f23285f97121965d39203f40a5def4ccfc
Eonist <unicornhive@gmail.com>
2016-01-12 11:39:16 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleModifier.swift

--- geom/svg/style/utils/SVGStyleModifier.swift
+++ geom/svg/style/utils/SVGStyleModifier.swift
@@ -11,7 +11,7 @@ class SVGStyleModifier {
         if((!(a.fill is Double) && !(b.fill is Double)) || ((a.fill as! Double).isNaN && !(b.fill as! Double).isNaN)) {a.fill = b.fill}/*Fill*/
         if((a.fillOpacity.isNaN) && !(b.fillOpacity.isNaN)) {a.fillOpacity = b.fillOpacity}
         if(a.fillRule == nil && b.fillRule != nil) {a.fillRule = b.fillRule}
-        if(a.stroke.isNaN && !(b.stroke.isNaN)) {a.stroke = b.stroke}/*Stroke*/
+        if((!(a.stroke is Double) && !(b.stroke is Double)) || ((a.stroke as! Double).isNaN && !(b.stroke as! Double).isNaN)) {a.stroke = b.stroke}/*Stroke*/
         if((a.strokeWidth.isNaN) && !(b.strokeWidth.isNaN)) {a.strokeWidth = b.strokeWidth}
         if((a.strokeOpacity.isNaN) && !(b.strokeOpacity.isNaN)) {a.strokeOpacity = b.strokeOpacity}
 		if(a.strokeLineCap == nil && b.strokeLineCap != nil) {a.strokeLineCap = b.strokeLineCap}
