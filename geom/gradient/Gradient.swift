2da6ea0375c56a669d1041d32c8a89a60402a6f2
Eonist <unicornhive@gmail.com>
2016-01-14 20:05:50 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -39,7 +39,7 @@ TODO: then start refactoring the Graphics class related to the Gradient config m
 */
  
 //Continue here: in order to move forward we need to seperate radial and linear gradient types. this class should provide the common denominator of the two possible three (concial gradient)
-//Radial gradient should maybe suport a dual system of focal and 2 point system
+
 //Linear and Radial should support both absolute and relative values and also both boundingbox and ...or only bounding box? Think!
 
 
