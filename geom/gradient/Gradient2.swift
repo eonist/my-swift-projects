e313d3643e6f226a9efa9bf57638458bdd862904
Eonist <unicornhive@gmail.com>
2016-01-14 20:09:51 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient2.swift

--- geom/gradient/Gradient2.swift
+++ geom/gradient/Gradient2.swift
@@ -11,7 +11,7 @@ public class Gradient2:IGradient2 {/*<---its public so that it works in playgrou
     public var colors:Array<CGColor>
     public var locations:Array<CGFloat>/*same as color stops*/
     public var transformation:CGAffineTransform?
-    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ gradientType:GradientType = GradientType.Linear,_ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/,_ relativeStartCenter:CGPoint? = nil,_ relativeEndCenter:CGPoint? = nil,_ relativeStartRadius:CGSize? = nil,_ relativeEndRadius:CGSize? = nil, _ p1:CGPoint? = nil, _ p2:CGPoint? = nil,_ useBoundingBox:Bool = false, _ transformation:CGAffineTransform? = nil){/*,*/
+    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [], _ transformation:CGAffineTransform? = nil){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
             //Swift.print(colors.count)
