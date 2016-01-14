ea1fc59d1d593a6dd1fd51a662ffcdc3b37b652b
Eonist <unicornhive@gmail.com>
2016-01-14 20:01:44 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient2.swift

--- geom/gradient/Gradient2.swift
+++ geom/gradient/Gradient2.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class Gradient2 {
-    //continue here: move code from Gradient1 to here
+public class Gradient2:IGradient2 {/*<---its public so that it works in playground*/
+    public var colors:Array<CGColor>
+    public var locations:Array<CGFloat>/*same as color stops*/
+    public var transformation:CGAffineTransform?
 }
