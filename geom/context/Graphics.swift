312b1dfb17ccaba24ae03a26d0b5b575c0235667
Eonist <unicornhive@gmail.com>
2016-01-14 17:34:21 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -290,7 +290,8 @@ private class RadialGradientUtils{
     /**
      * @NOTE: needs better explinations
      * @NOTE: in order to get a squashed radial gradient we need to use the transform. 
-     * @NOTE: the matrix that is provided needs to 
+     * @NOTE: the matrix that is provided needs to be applied inorder to keep the init ratio (this matrix should be calulated when from the graphic point of view not here)
+     * @NOTE: 
      */
     class func radialGradient(boundingBox:CGRect,_ gradient:IGradient)->(startCenter:CGPoint,endCenter:CGPoint,startRadius:CGFloat,endRadius:CGFloat,transform:CGAffineTransform){
         let startCenter:CGPoint = CGPoint(boundingBox.width/2 ,boundingBox.height/2)/*Find the center of the boundingbox, the pivot*/
