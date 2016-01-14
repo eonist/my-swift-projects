c4c8f4fe33e8dd79cf796890bcca650db4f4273e
Eonist <unicornhive@gmail.com>
2016-01-14 17:47:00 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/IGradient2.swift

--- geom/gradient/IGradient2.swift
+++ geom/gradient/IGradient2.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 protocol IGradient2 {
-
+    var colors:Array<CGColor>{get set}
+    var locations:Array<CGFloat>{get set}
+    var transformation:CGAffineTransform?{get set}
 }
