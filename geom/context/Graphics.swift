721837a8e23a017bac46765b1699c949ada7f5c8
Eonist <unicornhive@gmail.com>
2016-01-15 13:13:35 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -343,8 +343,21 @@ CGContextClip(context);
  * @param: transformation: we need transformation as this is the only way to achieve the squashed gradient look
  */
 public class GraphicsGradient{
+    public var colors:Array<CGColor>
+    public var locations:Array<CGFloat>/*same as color stops*/
     public var transformation:CGAffineTransform?
-    init(_ transformation:CGAffineTransform? = nil){
+    init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ transformation:CGAffineTransform? = nil){
+        self.colors = colors
+        if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
+            //Swift.print(colors.count)
+            self.locations = CGFloatParser.interpolatedRange(0,  1,  colors.count)
+            //Swift.print(self.locations)
+        }else{
+            self.locations = locations
+        }
         self.transformation = transformation
     }
+}
+class LinearGraphicsGradient{
+    
 }
