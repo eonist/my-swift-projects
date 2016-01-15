44083b5c70db95865b43404b0174406d6bc0d0fd
Eonist <unicornhive@gmail.com>
2016-01-15 07:09:11 +0100

    Files modified: 2
    Modified 2 files:geom/gradient/IGradient2.swiftgeom/gradient/RadialGradient.swift

--- geom/gradient/IGradient2.swift
+++ geom/gradient/IGradient2.swift
@@ -7,7 +7,7 @@ protocol IGradient2 {
 }
 extension IGradient2{
     func copy() -> IGradient2 {
-        if(self is LinearGradient){return LinearGradient((self as! LinearGradient).p1,(self as! LinearGradient).p2,colors,locations,transformation)}
-        else{return RadialGradient((self as! RadialGradient).startCenter,(self as! RadialGradient).endCenter,(self as! RadialGradient).startRadius,(self as! RadialGradient).endRadius,colors,locations,transformation)}
+        if(self is LinearGradient){return LinearGradient(colors,locations,transformation,(self as! LinearGradient).p1,(self as! LinearGradient).p2)}
+        else{return RadialGradient(colors,locations,transformation,(self as! RadialGradient).startCenter,(self as! RadialGradient).endCenter,(self as! RadialGradient).startRadius,(self as! RadialGradient).endRadius)}
     }
 }
--- geom/gradient/RadialGradient.swift
+++ geom/gradient/RadialGradient.swift
@@ -5,7 +5,7 @@ public class RadialGradient:Gradient2 {
     public var endCenter:CGPoint?
     public var startRadius:CGSize?
     public var endRadius:CGSize?
-    init(_ startCenter:CGPoint? = nil,_ endCenter:CGPoint? = nil,_ startRadius:CGSize? = nil,_ endRadius:CGSize? = nil,_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ transformation:CGAffineTransform? = nil){
+    init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ transformation:CGAffineTransform? = nil,_ startCenter:CGPoint? = nil,_ endCenter:CGPoint? = nil,_ startRadius:CGSize? = nil,_ endRadius:CGSize? = nil){
         super.init(colors, locations, transformation)
         self.startCenter = startCenter
         self.endCenter = endCenter
