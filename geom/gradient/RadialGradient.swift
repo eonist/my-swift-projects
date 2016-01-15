f749f765a33468debe89adf4ebb55fb3441ded87
Eonist <unicornhive@gmail.com>
2016-01-15 07:01:11 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/RadialGradient.swift

--- geom/gradient/RadialGradient.swift
+++ geom/gradient/RadialGradient.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-public class RadialGradient {
+public class RadialGradient:Gradient2 {
     public var startCenter:CGPoint?
     public var endCenter:CGPoint?
     public var startRadius:CGSize?
     public var endRadius:CGSize?
     init(_ startCenter:CGPoint? = nil,_ endCenter:CGPoint? = nil,_ startRadius:CGSize? = nil,_ endRadius:CGSize? = nil,_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ transformation:CGAffineTransform? = nil){
-        
+        super.init(colors, locations, transformation)
+        self.startCenter = startCenter
+        self.endCenter = endCenter
+        self.startRadius = startRadius
+        self.endRadius = endRadius
     }
 }
