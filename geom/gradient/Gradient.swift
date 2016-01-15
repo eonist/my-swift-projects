d6c124d16baf1e3e91d459a3868b39456f4253d2
Eonist <unicornhive@gmail.com>
2016-01-15 21:30:30 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -7,7 +7,7 @@ import Foundation
  * NOTE: Linear and Radial should support both absolute and relative values that operate on the path boundingbox
  * TODO: Find that medium article on axial gradient. the one that looks like a "spread out patonefan"
  */
-public class Gradient2:IGradient2 {/*<---its public so that it works in playground*/
+public class Gradient:IGradient {/*<---its public so that it works in playground*/
     public var colors:Array<CGColor>
     public var locations:Array<CGFloat>/*same as color stops*/
     public var rotation:CGFloat;/*must be between -π and π or it will fail*/
