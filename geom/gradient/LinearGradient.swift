b7b8c72037a4ed6ad486d00cb24a15049eb98d9a
Eonist <unicornhive@gmail.com>
2016-01-15 06:53:17 +0100

    Files modified: 1
    Modified 1 file:geom/gradient/LinearGradient.swift

--- geom/gradient/LinearGradient.swift
+++ geom/gradient/LinearGradient.swift
@@ -3,7 +3,7 @@ import Foundation
 public class LinearGradient:Gradient2 {
     public var p1:CGPoint?/*<--temp*/
     public var p2:CGPoint?
-    init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ p1:CGPoint? = nil, _ p2:CGPoint? = nil){
+    init(_ p1:CGPoint? = nil, _ p2:CGPoint? = nil,_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ transformation:CGAffineTransform? = nil){
         super.init(colors, locations, transformation)
         self.p1 = p1
         self.p2 = p2
