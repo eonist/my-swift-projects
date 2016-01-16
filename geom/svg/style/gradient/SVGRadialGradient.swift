1b5951449eaf47855b59902ade4d303c73901610
Eonist <unicornhive@gmail.com>
2016-01-16 10:49:57 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/gradient/SVGRadialGradient.swift

--- geom/svg/style/gradient/SVGRadialGradient.swift
+++ geom/svg/style/gradient/SVGRadialGradient.swift
@@ -2,6 +2,7 @@ import Foundation
 /*
  * // :TODO: add an example here
  * // :TODO: add fx="98.7581" fy="63.338" 
+ * @NOTE radius represents the half the width and hight of the inner circle
  */
 class SVGRadialGradient:SVGGradient {
     var cx:CGFloat
@@ -9,12 +10,12 @@ class SVGRadialGradient:SVGGradient {
     var r:CGFloat/*radius*/
     var fx:CGFloat
     var fy:CGFloat
-    init(_ offsets:Array<CGFloat>,_ colors:Array<CGColor>,/*_ opacities:Array<CGFloat>,*/ _ gradientUnits:String, _ spreadMethod:String, _ id:String, _ cx:CGFloat, _ cy:CGFloat, _ r:CGFloat, _ fx:CGFloat, _ fy:CGFloat/*, gradientTransform:Matrix*/){
+    init(_ offsets:Array<CGFloat>,_ colors:Array<CGColor>,/*_ opacities:Array<CGFloat>,*/ _ gradientUnits:String, _ spreadMethod:String, _ id:String, _ cx:CGFloat, _ cy:CGFloat, _ r:CGFloat, _ fx:CGFloat, _ fy:CGFloat, _ gradientTransform:CGAffineTransform? = nil/**/){
         self.cx = cx;
         self.cy = cy;
         self.r = r;
         self.fx = fx;
         self.fy = fy;
-        super.init(offsets,colors,/*opacities*/spreadMethod,id,gradientUnits/*gradientTransform*/);
+        super.init(offsets,colors,/*opacities*/spreadMethod,id,gradientUnits,gradientTransform);
     }
 }
