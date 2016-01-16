9d0c883063e822328a7e91976a187d084aea8a63
Eonist <unicornhive@gmail.com>
2016-01-16 21:40:20 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/SVGCircle.swift

--- geom/svg/graphic/SVGCircle.swift
+++ geom/svg/graphic/SVGCircle.swift
@@ -23,18 +23,19 @@ class SVGCircle : SVGGraphic{
         
         if(!r.isNaN) {
             /*Fill*/
-            let x:CGFloat = (!cx.isNaN ? cx : 0) - r
-            let y:CGFloat = (!cy.isNaN ? cy : 0) - r
-            let rect:CGRect = CGRect(x, y, r*2, r*2)
-            let fillFrame = (style!.stroke is Double && !(style!.stroke as! Double).isNaN) || style!.stroke is SVGGradient  ?  RectGraphicUtils.fillFrame(rect, style!.strokeWidth, OffsetType(OffsetType.center)) : rect
-            Swift.print("fillFrame: " + "\(fillFrame)")
-            fillShape.frame = fillFrame/*,position and set the size of the frame*/
-            fillShape.path = CGPathParser.circle(r,r,r)/*<--the path is positioned relative to the frame, remeber the circle is drawn from the center not from 0,0 which is what we want when it concerns the SVGCircle*//*CGPathParser.ellipse(CGRect(0,0,rect.width,rect.height))*/
-            
+            if(style!.fill != nil){
+                let x:CGFloat = (!cx.isNaN ? cx : 0) - r
+                let y:CGFloat = (!cy.isNaN ? cy : 0) - r
+                let rect:CGRect = CGRect(x, y, r*2, r*2)
+                let fillFrame = (style!.stroke is Double && !(style!.stroke as! Double).isNaN) || style!.stroke is SVGGradient  ?  RectGraphicUtils.fillFrame(rect, style!.strokeWidth, OffsetType(OffsetType.center)) : rect
+                Swift.print("fillFrame: " + "\(fillFrame)")
+                fillShape.frame = fillFrame/*,position and set the size of the frame*/
+                fillShape.path = CGPathParser.circle(r,r,r)/*<--the path is positioned relative to the frame, remeber the circle is drawn from the center not from 0,0 which is what we want when it concerns the SVGCircle*//*CGPathParser.ellipse(CGRect(0,0,rect.width,rect.height))*/
+            }
             /*Line*/
             Swift.print("style!.stroke: " + "\(style!.stroke)")
             if(style!.stroke != nil){//checks if there is a stroke in style
-                let lineOffsetRect = RectGraphicUtils.lineOffsetRect(rect, style!.strokeWidth, OffsetType(OffsetType.center))
+                let lineOffsetRect = RectGraphicUtils.lineOffsetRect(CGRect(x, y, r*2, r*2), style!.strokeWidth, OffsetType(OffsetType.center))
                 Swift.print("lineOffsetRect: " + "\(lineOffsetRect)")
                 lineShape.frame = lineOffsetRect.lineFrameRect
                 Swift.print("lineShape.frame: " + "\(lineShape.frame)")
