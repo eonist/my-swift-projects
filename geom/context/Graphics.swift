9cc4ba159ce0b1836723a3fe87fbf31f52d58c9e
Eonist <unicornhive@gmail.com>
2016-01-16 22:34:38 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -213,11 +213,11 @@ private class Utils{
      */
     class func drawRadialGradient(path:CGPath,_ context:CGContextRef,_ cgGradient:CGGradientRef?, /*_ boundingBox:CGRect,*/_ gradient:RadialGraphicsGradient){
         //Swift.print("Graphics.drawRadialGradient")
-        //let boundingBox:CGRect = CGPathGetBoundingBox(path)/*<-temp, find a faster way*/
+        let boundingBox:CGRect = CGPathGetBoundingBox(path)/*<-temp, find a faster way*/
         
-        //CGContextAddPath(context,boundingBox.path)
+        CGContextAddPath(context,boundingBox.path)
         
-        CGContextAddPath(context,path)//Adds the path to the context
+        //CGContextAddPath(context,path)//Adds the path to the context
         
         CGContextSetFillColorWithColor(context,gradient.colors[gradient.colors.count-1])/*Sets the background to the same color as the first gradient color, this is needed to fill the entire path*/
         CGContextDrawPath(context, CGPathDrawingMode.Fill)/*draws the background color to the context*/
