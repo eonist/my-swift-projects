e7626243fbb9ff073159711ae878ada3e42af41d
Eonist <unicornhive@gmail.com>
2016-01-16 10:25:18 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGRectParser.swift

--- geom/svg/graphic/utils/SVGRectParser.swift
+++ geom/svg/graphic/utils/SVGRectParser.swift
@@ -6,6 +6,11 @@ class SVGRectParser {
      * @Note: if the svgRect x and or y is of the value NaN, then these are transfered as 0
      */
     class func rectangle(svgRect:SVGRect)->CGRect {
+        //Swift.print("svgRect: " + "\(svgRect)")
+        //Swift.print("svgRect.x: " + "\(svgRect.x)")
+        //Swift.print("svgRect.y: " + "\(svgRect.y)")
+        //Swift.print("svgRect.width: " + "\(svgRect.width)")
+        //Swift.print("svgRect.height: " + "\(svgRect.height)")
         return CGRect(!svgRect.x.isNaN ? svgRect.x : 0, !svgRect.y.isNaN ? svgRect.y : 0, svgRect.width, svgRect.height);
     }
 }
