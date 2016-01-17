2e413130c4d35d033c033ac80dbdd6ed4492d3e6
Eonist <unicornhive@gmail.com>
2016-01-17 21:00:21 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -173,7 +173,7 @@ class SVGUtils {
         //graphic.draw();/*<--draws the path with the new params*/
         
         //graphic.endFill();
-        graphic.fillShape.setNeedsDisplay()/*there needs to be an update to the beginFill and applyLineStyll since gradient matrices may have changed etc, but the call must be a request not a direct call since the context isnt ready yet*/
-        graphic.lineShape.setNeedsDisplay()
+        Swift.print("graphic.fillShape: " + "\(graphic.fillShape)")/*there needs to be an update to the beginFill and applyLineStyll since gradient matrices may have changed etc, but the call must be a request not a direct call since the context isnt ready yet*/
+        Swift.print("graphic.lineShape: " + "\(graphic.lineShape)")
     }
 }
