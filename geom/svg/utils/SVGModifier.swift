0e5eb9de24c643fcb409e025943adbaec4510bad
Eonist <unicornhive@gmail.com>
2016-01-17 18:10:49 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -142,7 +142,6 @@ class SVGPathModifier {
         var i:Int = 0;/*parameterIndex*/
         //var commands:Array<String> = path.commands;
         //var params:Array<CGFloat> = path.parameters;
-        let path:SVGPath = SVGPath([],[])
         var p:CGPoint;
         var c1:CGPoint;
         var c2:CGPoint;
@@ -152,18 +151,18 @@ class SVGPathModifier {
             switch(command.lowercaseString){
             case SVGPathCommand.l,SVGPathCommand.m://<-this may need testing since it may be || instead of ,
                 p = PointModifier.scale(CGPoint(params[i],params[i+1]), pivot, scalePoint);
-                path.parameters[i] = p.x;
-                path.parameters[i+1] = p.y;
+                parameters[i] = p.x;
+                parameters[i+1] = p.y;
                 i += 2;
                 break;
             case SVGPathCommand.h:
                 p = PointModifier.scale(CGPoint(params[i],0), pivot, scalePoint);
-                path.parameters[i] = p.x;
+                parameters[i] = p.x;
                 i++;
                 break;
             case SVGPathCommand.v:
                 p = PointModifier.scale(CGPoint(0,params[i]), pivot, scalePoint);
-                path.parameters[i] = p.y;
+                parameters[i] = p.y;
                 i++;
                 break;
             case SVGPathCommand.c:
@@ -171,36 +170,36 @@ class SVGPathModifier {
                 c2 = PointModifier.scale(CGPoint(params[i+2],params[i+3]), pivot, scalePoint);
                 a2 = PointModifier.scale(CGPoint(params[i+4],params[i+5]), pivot, scalePoint);
                 //						trace(e+" scale.c c1: "+c1 + " c2:"+c2+" a2:" + a2);
-                path.parameters[i] = c1.x;
-                path.parameters[i+1] = c1.y;
-                path.parameters[i+2] = c2.x;
-                path.parameters[i+3] = c2.y;
-                path.parameters[i+4] = a2.x;
-                path.parameters[i+5] = a2.y;
+                parameters[i] = c1.x;
+                parameters[i+1] = c1.y;
+                parameters[i+2] = c2.x;
+                parameters[i+3] = c2.y;
+                parameters[i+4] = a2.x;
+                parameters[i+5] = a2.y;
                 i+=6;
                 break;
             case SVGPathCommand.s:
                 c2 = PointModifier.scale(CGPoint(params[i],params[i+1]), pivot, scalePoint);
                 a2 = PointModifier.scale(CGPoint(params[i+2],params[i+3]), pivot, scalePoint);
-                path.parameters[i] = c2.x;
-                path.parameters[i+1] = c2.y;
-                path.parameters[i+2] = a2.x;
-                path.parameters[i+3] = a2.y;
+                parameters[i] = c2.x;
+                parameters[i+1] = c2.y;
+                parameters[i+2] = a2.x;
+                parameters[i+3] = a2.y;
                 i+=4;
                 break;
             case SVGPathCommand.q:
                 c1 = PointModifier.scale(CGPoint(params[i],params[i+1]), pivot, scalePoint);
                 a2 = PointModifier.scale(CGPoint(params[i+2],params[i+3]), pivot, scalePoint);
-                path.parameters[i] = c1.x;
-                path.parameters[i+1] = c1.y;
-                path.parameters[i+2] = a2.x;
-                path.parameters[i+3] = a2.y;
+                parameters[i] = c1.x;
+                parameters[i+1] = c1.y;
+                parameters[i+2] = a2.x;
+                parameters[i+3] = a2.y;
                 i+=4;
                 break;
             case SVGPathCommand.t:
                 a2 = PointModifier.scale(CGPoint(params[i],params[i+1]), pivot, scalePoint);
-                path.parameters[i] = a2.x;
-                path.parameters[i+1] = a2.y;
+                parameters[i] = a2.x;
+                parameters[i+1] = a2.y;
                 i+=2;
                 break;
             case SVGPathCommand.z: 
