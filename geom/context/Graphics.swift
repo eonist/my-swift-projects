1abb96daf294a8f820f5486cb5fa407d68ef3efc
Eonist <unicornhive@gmail.com>
2016-01-17 09:25:43 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -216,6 +216,10 @@ private class Utils{
         
         
         
+        let boundingBox:CGRect = CGPathGetBoundingBox(path)/*<-temp, find a faster way*/
+        Swift.print("boundingBox: " + "\(boundingBox)")
+        CGContextAddPath(context,boundingBox.path)//Adds the path to the context
+        /**/
         var newPath:CGPath = CGPathCreateMutableCopy(path)!
         newPath = CGPathModifier.scale(&newPath, -1, -1)
         CGContextAddPath(context,newPath)
