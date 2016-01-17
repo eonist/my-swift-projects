b7475da0ed90c5591697689f125a1e6ff8dddebd
Eonist <unicornhive@gmail.com>
2016-01-17 18:34:21 +0100

    Files modified: 2
    Modified 2 files:geom/svg/utils/SVGContainerModifier.swiftgeom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGContainerModifier.swift
+++ geom/svg/utils/SVGContainerModifier.swift
@@ -4,16 +4,16 @@ class SVGContainerModifier {
 	/**
 	 * scales the SVGContainer 
 	 */
-	class func scale(inout container:SVGContainer,_ pivot:CGPoint,_ scale:CGPoint) {
+	class func scale(container:SVGContainer,_ pivot:CGPoint,_ scale:CGPoint) {
 		let position:CGPoint = PointModifier.scale(container.frame.origin, pivot, scale);
 		let size:CGSize = CGSize(container.frame.width * scale.x, container.frame.height * scale.y);
 		container.frame.origin = position;
 		container.frame.size = size;
         for var i = 0; i < container.items.count-1; ++i{
-            var element : ISVGElement = container.items
-            print(i)
+            //var element : ISVGElement = container.items[i]
+            SVGModifier.scale(container.items[i], pivot, scale)
         }
-        for element : ISVGElement in container.items{SVGModifier.scale(&element, pivot, scale)}/**/
+        //for element : ISVGElement in container.items{SVGModifier.scale(&element, pivot, scale)}/**/
 	}
     
 }
