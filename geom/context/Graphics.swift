f9600814e1b31079e63690efe2b62c26c64f8fd9
Eonist <unicornhive@gmail.com>
2016-01-17 10:29:40 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -237,15 +237,15 @@ private class Utils{
         
         
         
-        CGContextSetFillColorWithColor(context,gradient.colors[gradient.colors.count-1])/*Sets the background to the same color as the first gradient color, this is needed to fill the entire path*/
+        //CGContextSetFillColorWithColor(context,gradient.colors[gradient.colors.count-1])/*Sets the background to the same color as the first gradient color, this is needed to fill the entire path*/
         CGContextDrawPath(context, CGPathDrawingMode.Fill)/*draws the background color to the context*/
         CGContextRestoreGState(context)//restore the graphic mask
-        /*
+        
         CGContextSaveGState(context)/*save the current context*/
         if(gradient.transformation != nil) {CGContextConcatCTM(context, gradient.transformation!)}/*transform the current context*/
         CGContextDrawRadialGradient(context, cgGradient, gradient.startCenter, gradient.startRadius, gradient.endCenter, gradient.endRadius, [])/*Draw the actual radial graphics*///CGGradientDrawingOptions.DrawsBeforeStartLocation,CGGradientDrawingOptions.DrawsAfterEndLocation//CGGradientDrawingOptions.DrawsBeforeStartLocation or CGGradientDrawingOptions.DrawsAfterEndLocation
         CGContextRestoreGState(context)/*restore the context that was saved*/
-        */
+        /**/
         /**/
         
         //continue here: figure out how to deal with this problem: the last color shouldnt always be the the background fill, sometimes you need it to be the first, think transperancy etc, so you need to either make a square with a hole in it: this could be easily done with two paths one being drawn with reverese winding.
