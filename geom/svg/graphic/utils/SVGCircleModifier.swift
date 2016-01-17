10e35ee0e0cf95a0d482554524720fc51e07b523
Eonist <unicornhive@gmail.com>
2016-01-17 16:54:25 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGCircleModifier.swift

--- geom/svg/graphic/utils/SVGCircleModifier.swift
+++ geom/svg/graphic/utils/SVGCircleModifier.swift
@@ -5,9 +5,12 @@ class SVGCircleModifier {
      * Scales the SVGCircle instance from @param pivot to a specific @param scale scalar value (0-1)
      */
     class func scale(circle:SVGCircle,_ pivot:CGPoint,_ scale:CGPoint) {
+        Swift.print("SVGCircleModifier.circle() circle: " + "\(circle)")
+        Swift.print("circle.r: " + "\(circle.r)")
         let circleCenter:CGPoint = PointModifier.scale(circle.center, pivot, scale);
         let circleRadius:CGFloat = circle.r * scale.x;
         circle.center = circleCenter;
         circle.r = circleRadius;
+        circle.r
     }
 }
