60f7984817ade50dd9866da5b5b6d2691b172c01
Eonist <unicornhive@gmail.com>
2016-01-17 20:58:23 +0100

    Files modified: 2
    Modified 2 files:geom/svg/utils/SVGModifier.swiftgeom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -164,5 +164,16 @@ class SVGUtils {
             }
         }
     }
-    
+    /**
+     * updates an SVGGraphic
+     */
+    private class func update(graphic:SVGGraphic) {
+        Swift.print("update")
+        //WARNING: this method is incomplete, needs correct order of calls etc
+        //graphic.draw();/*<--draws the path with the new params*/
+        
+        //graphic.endFill();
+        graphic.fillShape.setNeedsDisplay()/*there needs to be an update to the beginFill and applyLineStyll since gradient matrices may have changed etc, but the call must be a request not a direct call since the context isnt ready yet*/
+        graphic.lineShape.setNeedsDisplay()
+    }
 }
