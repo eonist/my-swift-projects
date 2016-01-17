b1679d72268c429035a93913b0296c682ed85088
Eonist <unicornhive@gmail.com>
2016-01-17 14:05:25 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -36,7 +36,9 @@ class SVGModifier {
 			case element is SVGPath:SVGPathModifier.scale(element as! SVGPath, pivot, scale);break;
 			case element is SVGCircle:SVGCircleModifier.scale(element as! SVGCircle, pivot, scale);break;
 			case element is SVGEllipse:SVGEllipseModifier.scale(element as! SVGEllipse, pivot, scale);break;
-			case element is SVGContainer:SVGContainerModifier.scale(&(element as! SVGContainer),pivot,scale);break;
+			case element is SVGContainer:
+                var container:SVGContainer = element as! SVGContainer
+                SVGContainerModifier.scale(&container,pivot,scale);break;
 			case element is SVGGradient:
                 Swift.print("(element as! SVGGradient).gradientTransform: " + "\((element as! SVGGradient).gradientTransform)")
                 SVGGradientModifier.scale(element as! SVGGradient, pivot, scale);
