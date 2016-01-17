5b425c91d90f78ba1b45c0ac39b3ca582efddfad
Eonist <unicornhive@gmail.com>
2016-01-17 20:26:50 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGModifier.swift

--- geom/svg/utils/SVGModifier.swift
+++ geom/svg/utils/SVGModifier.swift
@@ -20,6 +20,18 @@ class SVGModifier {
         }
     }
     /**
+     * updates an SVGGraphic
+     */
+    class func update(graphic:ISVGGraphic) {
+        Swift.print("update")
+        //WARNING: this method is incomplete, needs correct order of calls etc
+        graphic.draw();/*<--draws the path with the new params*/
+        
+        //graphic.endFill();
+        graphic.fillShape.setNeedsDisplay()/*there needs to be an update to the beginFill and applyLineStyll since gradient matrices may have changed etc, but the call must be a request not a direct call since the context isnt ready yet*/
+        graphic.lineShape.setNeedsDisplay()
+    }
+    /**
      * Styles an @param element with @param style
      * // :TODO: rename to stylize?
      * @Note this method is recursive
