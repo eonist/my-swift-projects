853b4c834d821bdf7befea7181cdbaaeaf9ed1eb
Eonist <unicornhive@gmail.com>
2016-01-17 21:43:02 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -153,13 +153,16 @@ class SVGUtils {
     class func dsc(inout svg:SVGContainer,_ pivot:CGPoint,_ scalePoint:CGPoint){
         for var i = 0; i < svg.items.count; ++i{
             if(svg.items[i] is SVGContainer){
-                let svgContainer:SVGContainer = svg.items[i] as! SVGContainer
+                //let svgContainer:SVGContainer = svg.items[i] as! SVGContainer
                 for var e = 0; e < svg.items.count; ++e{
                     if((svg.items[i] as! SVGContainer).items[e] is SVGPath){
                         //Swift.print((svgContainer.items[e] as! SVGPath).parameters)
-                        dsc(&(svg.items[i] as SVGContainer),pivot,scalePoint)
+                        var svgCon = svg.items[i] as! SVGContainer
+                        dsc(&svgCon,pivot,scalePoint)
                     }
                 }
+            }else if(){
+                
             }else{
                 fatalError("no")
             }
