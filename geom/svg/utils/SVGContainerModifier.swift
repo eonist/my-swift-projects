e94eafdc389d83dadca5a7b8c1d929d33e7e675b
Eonist <unicornhive@gmail.com>
2016-01-17 22:09:07 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGContainerModifier.swift

--- geom/svg/utils/SVGContainerModifier.swift
+++ geom/svg/utils/SVGContainerModifier.swift
@@ -10,9 +10,9 @@ class SVGContainerModifier {
 		let size:CGSize = CGSize(container.frame.width * scale.x, container.frame.height * scale.y);
 		container.frame.origin = position;
 		container.frame.size = size;
-        fatalError("uncomment the line bellow")
+        //fatalError("uncomment the line bellow")
         //for var i = 0; i < container.items.count; ++i{SVGModifier.scale(container.items[i], pivot, scale)}
-        //for element : ISVGElement in container.items{SVGModifier.scale(&element, pivot, scale)}/**/
+        for element : ISVGElement in container.items{SVGModifier.scale(element, pivot, scale)}/**/
 	}
     
 }
