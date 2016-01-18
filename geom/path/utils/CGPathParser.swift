27001201f3e8dcf3cefd6d1d5d176c00a9a3f2b8
Eonist <unicornhive@gmail.com>
2016-01-18 09:05:23 +0100

    Files modified: 1
    Modified 1 file:geom/path/utils/CGPathParser.swift

--- geom/path/utils/CGPathParser.swift
+++ geom/path/utils/CGPathParser.swift
@@ -118,8 +118,8 @@ extension CGPathParser{
     /**
      * Draws an ellipse from the center
      */
-    class func roundRect(rect:CGRect,)->CGMutablePathRef{
-        return roundRect(rect.x, rect.y, rect.width, <#T##h: CGFloat##CGFloat#>, <#T##topLeftRadius: CGFloat##CGFloat#>, <#T##topRightRadius: CGFloat##CGFloat#>, <#T##bottomLeftRadius: CGFloat##CGFloat#>, <#T##bottomRightRadius: CGFloat##CGFloat#>)
+    class func roundRect(rect:CGRect,_ fillet:Fillet)->CGMutablePathRef{
+        return roundRect(rect.x, rect.y, rect.width, rect.height, fillet.topLeft, fillet.topRight, fillet.bottomLeft, fillet.bottomRight)
     }
     //CGPathCreateWithRoundedRect
 }
