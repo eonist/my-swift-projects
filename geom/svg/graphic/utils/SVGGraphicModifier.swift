66a15c3e1b162442e77ad926b68c822f88dc0180
Eonist <unicornhive@gmail.com>
2016-01-20 19:53:10 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/utils/SVGGraphicModifier.swift

--- geom/svg/graphic/utils/SVGGraphicModifier.swift
+++ geom/svg/graphic/utils/SVGGraphicModifier.swift
@@ -9,7 +9,7 @@ class SVGGraphicModifier {
      * @NOTE: miterlimit The limit on the ratio of the miter length to the stroke-width. The value of <miterlimit> must be a <number> greater than or equal to 1.
      */
     class func applyStrokeStyle(graphics:Graphics, _ style:SVGStyle) {
-        Swift.print("SVGGraphicModifier.applyStrokeStyle()")
+        //Swift.print("SVGGraphicModifier.applyStrokeStyle()")
         let strokeWidth:CGFloat = SVGStyleUtils.strokeWidth(style.strokeWidth!)
         let strokeMiterLimit:CGFloat = SVGStyleUtils.miterLimit(style.strokeMiterLimit!)
         let strokeLineCap:CGLineCap = SVGStyleUtils.lineCap(style.strokeLineCap)
@@ -22,7 +22,7 @@ class SVGGraphicModifier {
      * TODO: when you scale the lineGradient, remember to consider that you dont scale the lineWidth and so the gradient that is applied may not cover as it should. This means that you cant simply scale the SVGGradient.transformation, instead you have to scale the gradient that is attached to the indeviduel SVGGraphic, and you have to take into consideration that the strokeThickness is now different than the new relative size. Even though its the same, the relative difference between the strokeThickness and the size of the shape is different. So you have to recalculate the gradient.transformation matrix a little bit. Probably by insetting it by some relative value
      */
     class func applyGradientStrokeStyle(shape:Shape,_ style:SVGStyle){
-        Swift.print("applyGradientStrokeStyle: " + "\(applyGradientStrokeStyle)")
+        //Swift.print("applyGradientStrokeStyle: " + "\(applyGradientStrokeStyle)")
         SVGGraphicModifier.applyStrokeStyle(shape.graphics,style)/*call the BaseGraphic to set the stroke-width, cap, joint etc*/
         let gradient:SVGGradient = (style.stroke! as! SVGGradient)
         let userSpaceOnUse:Bool = gradient.gradientUnits == "userSpaceOnUse";//The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.
@@ -80,7 +80,7 @@ class SVGGraphicModifier {
      */
     class func beginGradientFill(shape:Shape,_ gradient:SVGGradient) {
         //let graphics:Graphics = shape.graphics
-        Swift.print("SVGGraphicModifier.beginGradientFill");
+        //Swift.print("SVGGraphicModifier.beginGradientFill");
         //let gradientType = gradient is SVGLinearGradient ? GradientType.Linear : GradientType.Radial;
         let userSpaceOnUse:Bool = gradient.gradientUnits == "userSpaceOnUse";////The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.
         //Swift.print("gradientType: " + gradientType);
@@ -106,18 +106,18 @@ class SVGGraphicModifier {
             //TODO: add support for relative values, see old code, you need to use the bounding box etc and test how relative values work in svg etc
             var p1:CGPoint = /*userSpaceOnUse && !gradient.x1.isNaN && !gradient.y1.isNaN ? */CGPoint((gradient as! SVGLinearGradient).x1,(gradient as! SVGLinearGradient).y1).copy()/* :nil*/
             var p2:CGPoint = /*userSpaceOnUse && !gradient.x2.isNaN && !gradient.y2.isNaN ? */CGPoint((gradient as! SVGLinearGradient).x2,(gradient as! SVGLinearGradient).y2).copy()/* :nil*/
-            Swift.print("p1: " + "\(p1)")
-            Swift.print("shape.frame.origin: " + "\(shape.frame.origin)")
+            //Swift.print("p1: " + "\(p1)")
+            //Swift.print("shape.frame.origin: " + "\(shape.frame.origin)")
             
             //TODO:  the problem is that you do the offset on values that are not yet sccaled. so either do scaling with matrix here or think of something els
             //maybe the graphic gradient is only absolute and you do matrix here instead?, since the offset will always be a problem etc, try this
-            Swift.print("points: " + "\([p1,p2])")
+            //Swift.print("points: " + "\([p1,p2])")
             
-            Swift.print("points after: " + "\([p1,p2])")
+            //Swift.print("points after: " + "\([p1,p2])")
             
             if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
                 if(gradient.gradientTransform != nil){
-                    Swift.print("drawAxialGradient() gradient.transformation()")
+                    //Swift.print("drawAxialGradient() gradient.transformation()")
                     //i think you should do tje matrix in Graphics not here
                     p1 = CGPointApplyAffineTransform(p1, gradient.gradientTransform!)
                     p2 = CGPointApplyAffineTransform(p2, gradient.gradientTransform!)
@@ -134,7 +134,7 @@ class SVGGraphicModifier {
                 //Swift.print("p1: " + "\(p1)")
                 //Swift.print("p2: " + "\(p2)")
             }
-            Swift.print("points after offset: " + "\([p1,p2])")
+            //Swift.print("points after offset: " + "\([p1,p2])")
             let linearGraphicsGradient:IGraphicsGradient = LinearGraphicsGradient(gradient.colors,gradient.offsets,nil/*gradient.gradientTransform*/,p1,p2)
             //Gradient(gradient.colors,gradient.offsets,0,nil,nil,nil,nil,p1,p2,!userSpaceOnUse/*,gradient.gradientTransform*/)
             //fatalError("implment the bellow first")
@@ -145,9 +145,9 @@ class SVGGraphicModifier {
          * @TODO: lets try to scale radial gradient aswell
          */
         else{/*gradient is SVGRadialGradient */
-            Swift.print("drawRadialGradient()")
+            //Swift.print("drawRadialGradient()")
             let radialGradient:SVGRadialGradient = gradient as! SVGRadialGradient
-            Swift.print("radialGradient.gradientTransform: " + "\(radialGradient.gradientTransform)")
+            //Swift.print("radialGradient.gradientTransform: " + "\(radialGradient.gradientTransform)")
             let startRadius:CGFloat = 0
             var endRadius:CGFloat = radialGradient.r
             //Swift.print("endRadius: " + "\(endRadius)")
@@ -160,11 +160,11 @@ class SVGGraphicModifier {
             var transformation:CGAffineTransform = CGAffineTransformIdentity
             
             if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
-                Swift.print("userSpaceOnUse")
+                //Swift.print("userSpaceOnUse")
                 if(radialGradient.gradientTransform != nil) {
-                    Swift.print("drawRadialGradient() gradient.transformation()")
+                    //Swift.print("drawRadialGradient() gradient.transformation()")
                     transformation = radialGradient.gradientTransform!.copy()
-                    Swift.print("transformation: " + "\(transformation)")
+                    //Swift.print("transformation: " + "\(transformation)")
                     //matrix.concat(gradient.gradientTransform)
                     //startCenter = CGPointApplyAffineTransform(startCenter, gradient.gradientTransform!)
                     //endCenter = CGPointApplyAffineTransform(endCenter, gradient.gradientTransform!)
@@ -172,7 +172,7 @@ class SVGGraphicModifier {
                 //startCenter -= shape.frame.origin
                 //endCenter -= shape.frame.origin
                 transformation.concat(CGAffineTransformMakeTranslation(-shape.frame.origin.x, -shape.frame.origin.y))
-                Swift.print("transformation: " + "\(transformation)")
+                //Swift.print("transformation: " + "\(transformation)")
             }else{/*objectBoundingBox*/
                 //if(radialGradient.gradientTransform != nil) {fatalError("not supported yet")} 
                 //TODO: we dont use any transform yet, you need to sort out the scaling first see todolist in the basic svg support article
@@ -185,12 +185,10 @@ class SVGGraphicModifier {
                 let minAxis:CGFloat = min(boundingBox.width,boundingBox.height)/*We need the smallest axis length, either width or height*/
                 let minRadius:CGFloat = minAxis/2/*Radius is half the axis length*/
                 endRadius = minRadius * (endRadius/100*2)//needs to be half of minwidth of boundingbox, multiply by 2 since we are using radius not diameter, this can be optimized
-                Swift.print("endRadius: " + "\(endRadius)")
-                Swift.print("startCenter: " + "\(startCenter)")
-                Swift.print("endCenter: " + "\(endCenter)")
+                //Swift.print("endRadius: " + "\(endRadius)")
+                //Swift.print("startCenter: " + "\(startCenter)")
+                //Swift.print("endCenter: " + "\(endCenter)")
             }
-            
-            
             let radialGraphicsGradient:IGraphicsGradient = RadialGraphicsGradient(radialGradient.colors,radialGradient.offsets,transformation/*nil*/,startCenter,endCenter,startRadius,endRadius)
             shape.graphics.gradientFill(radialGraphicsGradient)
         }
