9c0f45e72099442ebb8d3c922bd707a9e3bedf5d
Eonist <unicornhive@gmail.com>
2016-01-20 16:25:16 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: geom/svg/style/utils/SVGStyleDescriber.swiftModified 1 file:geom/svg/style/utils/SVGStyleParser.swift

--- geom/svg/style/utils/SVGStyleParser.swift
+++ geom/svg/style/utils/SVGStyleParser.swift
@@ -65,4 +65,31 @@ class SVGStyleParser {
 	class func stroke(property:Any?, _ container:ISVGContainer)->Any? {//<-- this doesnt have to be Any! it can be Any?
         return SVGStyleParser.fill(property, container)/*we use the fill parser here as it has the same features*/
 	}
+    /**
+     * // :TODO: doesn't this method also exist in the a parser class?
+     */
+    class func describe(style:SVGStyle) {
+        Swift.print("describe");
+        if(style.fill is Double) {
+            Swift.print("style.fill: " + "\(style.fill)")
+            Swift.print("style.fillOpacity: " + "\(style.fillOpacity)")
+        }else if(style.fill is SVGLinearGradient) {
+            let linearGradient:SVGLinearGradient = style.fill as! SVGLinearGradient;
+            Swift.print("linearGradient.colors: " + "\(linearGradient.colors)");
+            //Swift.print("linearGradient.opacities: " + "\(linearGradient.opacities)");
+            Swift.print("linearGradient.offsets: " + "\(linearGradient.offsets)");
+            Swift.print("linearGradient.gradientUnits: " + "\(linearGradient.gradientUnits)");
+            Swift.print("linearGradient.spreadMethod: " + "\(linearGradient.spreadMethod)");
+            Swift.print("linearGradient.x1: " + "\(linearGradient.x1)");
+            Swift.print("linearGradient.y1: " + "\(linearGradient.y1)");
+            Swift.print("linearGradient.x2: " + "\(linearGradient.x2)");
+            Swift.print("linearGradient.y2: " + "\(linearGradient.y2)");
+        }
+        Swift.print("style.fillRule: " + "\(style.fillRule)")
+        Swift.print("style.strokeWidth: " + "\(style.strokeWidth)")
+        Swift.print("style.stroke: " + "\(style.stroke)")
+        Swift.print("style.strokeLineCap: " + "\(style.strokeLineCap)")
+        Swift.print("style.strokeLineJoin: " + "\(style.strokeLineJoin)")
+        Swift.print("style.strokeMiterLimit: " + "\(style.strokeMiterLimit)")
+    }
 }
