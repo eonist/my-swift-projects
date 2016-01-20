a44c256c35735c5e1c3006d30c827f4f81bf9259
Eonist <unicornhive@gmail.com>
2016-01-20 18:37:06 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/SVGCircle.swift

--- geom/svg/graphic/SVGCircle.swift
+++ geom/svg/graphic/SVGCircle.swift
@@ -28,7 +28,7 @@ class SVGCircle : SVGGraphic{
             let rect:CGRect = CGRect(x, y, r*2, r*2)
             /*Fill*/
             if(style!.fill != nil){
-                let fillFrame = (style!.stroke != nil && style!.stroke! is Double && !(style!.stroke! as! Double).isNaN) || style!.stroke! is SVGGradient  ?  RectGraphicUtils.fillFrame(rect, style!.strokeWidth!, OffsetType(OffsetType.center)) : rect
+                let fillFrame = (style!.stroke != nil && style!.stroke! is Double && !(style!.stroke! as! Double).isNaN) || style!.stroke != nil && style!.stroke! is SVGGradient  ?  RectGraphicUtils.fillFrame(rect, style!.strokeWidth!, OffsetType(OffsetType.center)) : rect
                 Swift.print("fillFrame: " + "\(fillFrame)")
                 fillShape.frame = fillFrame/*,position and set the size of the frame*/
                 fillShape.path = CGPathParser.circle(r,r,r)/*<--the path is positioned relative to the frame, remeber the circle is drawn from the center not from 0,0 which is what we want when it concerns the SVGCircle*//*CGPathParser.ellipse(CGRect(0,0,rect.width,rect.height))*/
