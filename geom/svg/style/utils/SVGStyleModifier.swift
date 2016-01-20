ddb9b5166e7bbdefd3f493624da9606de683011c
Eonist <unicornhive@gmail.com>
2016-01-20 15:32:42 +0100

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleModifier.swift

--- geom/svg/style/utils/SVGStyleModifier.swift
+++ geom/svg/style/utils/SVGStyleModifier.swift
@@ -9,14 +9,14 @@ class SVGStyleModifier {
         //Swift.print("a.fill: " + a.fill);
         //Swift.print("b.fill: " + b.fill);
         if((!(a.fill is Double) && !(b.fill is Double)) || ((a.fill as! Double).isNaN && !(b.fill as! Double).isNaN)) {a.fill = b.fill}/*Fill*/
-        if((a.fillOpacity.isNaN) && !(b.fillOpacity.isNaN)) {a.fillOpacity = b.fillOpacity}
+        if((a.fillOpacity == nil || a.fillOpacity!.isNaN) && (b.fillOpacity != nil && !b.fillOpacity!.isNaN)) {a.fillOpacity = b.fillOpacity}
         if(a.fillRule == nil && b.fillRule != nil) {a.fillRule = b.fillRule}
         if((!(a.stroke is Double) && !(b.stroke is Double)) || ((a.stroke as! Double).isNaN && !(b.stroke as! Double).isNaN)) {a.stroke = b.stroke}/*Stroke*/
-        if((a.strokeWidth.isNaN) && !(b.strokeWidth.isNaN)) {a.strokeWidth = b.strokeWidth}
-        if((a.strokeOpacity.isNaN) && !(b.strokeOpacity.isNaN)) {a.strokeOpacity = b.strokeOpacity}
+        if((a.strokeWidth == nil || a.strokeWidth!.isNaN) && (a.strokeWidth != nil && !b.strokeWidth!.isNaN)) {a.strokeWidth = b.strokeWidth}
+        if((a.strokeOpacity == nil || a.strokeOpacity!.isNaN) && (a.strokeOpacity != nil && !b.strokeOpacity!.isNaN)) {a.strokeOpacity = b.strokeOpacity}
 		if(a.strokeLineCap == nil && b.strokeLineCap != nil) {a.strokeLineCap = b.strokeLineCap}
 		if(a.strokeLineJoin == nil && b.strokeLineJoin != nil) {a.strokeLineJoin = b.strokeLineJoin}
-		if(a.strokeMiterLimit.isNaN && b.strokeMiterLimit.isNaN) {a.strokeMiterLimit = b.strokeMiterLimit}
+		if(a.strokeMiterLimit a.strokeMiterLimit.isNaN && b.strokeMiterLimit.isNaN) {a.strokeMiterLimit = b.strokeMiterLimit}
             
         
     }
