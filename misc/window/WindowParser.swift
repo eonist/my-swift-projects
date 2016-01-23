fe4c255385d801b6ce65e7f66cc83272dd4f3e9d
Eonist <unicornhive@gmail.com>
2016-01-23 15:27:06 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: misc/window/WindowHelp.swiftModified 1 file:misc/window/WindowParser.swift

--- misc/window/WindowParser.swift
+++ misc/window/WindowParser.swift
@@ -1,5 +1,13 @@
-import Foundation
+import Cocoa
 
 class WindowParser {
-
+    /**
+     *
+     */
+    class func describeWindows(app:NSApplicationDelegate){
+        for w in app.windows{
+            print("windowNumber: " + "\(w.windowNumber)")
+            app.windowWithWindowNumber(w.windowNumber)//this is how you can manage windows
+        }
+    }
 }
