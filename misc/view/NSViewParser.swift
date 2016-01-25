5f175213d4ffc5a280085952a203f39438113266
Eonist <unicornhive@gmail.com>
2016-01-25 11:16:45 +0100

    Files modified: 1
    Modified 1 file:misc/view/NSViewParser.swift

--- misc/view/NSViewParser.swift
+++ misc/view/NSViewParser.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 class NSViewParser {
-
+    /**
+     * Returns an array of all the children in @param displayObjectContainer that is of type @param classType
+     * // :TODO: rename to childrenByClassType
+     */
+    func childrenOfType(view:NSView,classType:AnyClass)->Array<NSView> {
+    var children:Array<> = [];
+        
+        for subView in view.subviews {
+            subView.removeFromSuperview()
+            if(subView is classType) children.push(subView);
+        }
+    
+    return children;
+    }
 }
