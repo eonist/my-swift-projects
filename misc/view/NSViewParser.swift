83b03ea63a424b726fc7f449a688a5228913f603
Eonist <unicornhive@gmail.com>
2016-01-25 16:00:04 +0100

    Files modified: 1
    Modified 1 file:misc/view/NSViewParser.swift

--- misc/view/NSViewParser.swift
+++ misc/view/NSViewParser.swift
@@ -4,18 +4,13 @@ class NSViewParser {
     /**
      * Returns an array of all the children in @param view that is of type @param classType
      * // :TODO: rename to childrenByClassType, maybe not?
+     * NOTE: this works with classes and protocols
      */
-    func childrenOfType(view:NSView, classType:AnyClass?, protocolType:Protocol?)->Array<NSView> {
-        var children:Array<NSView> = []
+    func childrenOfType<T>(view:NSView, type:T)->Array<T> {
+        var children:Array<T> = []
         for subView in view.subviews {
-            let isOfClassType:Bool = classType != nil && subView.isKindOfClass(classType!)
-            let isOfProtocolType:Bool = String(subView.dynamicType) == NSStringFromProtocol(protocolType!)
-            if(isOfClassType && isOfProtocolType) {children.append(subView)}
+            if(subView as? T != nil){children.append(subView as! T)}
         }
         return children;
-    }
-    func ofType<T>(instance:Any?,_ type:T.Type) -> T?{/*<--we use the ? char so that it can also return a nil*/
-        if(instance as? T != nil){return instance as? T}
-        return nil
     }
 }
