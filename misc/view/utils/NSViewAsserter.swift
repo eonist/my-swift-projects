54d7d566a7be30a461a7311e27f914ed55ab70a5
Eonist <unicornhive@gmail.com>
2016-01-25 16:51:05 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewAsserter.swift

--- misc/view/utils/NSViewAsserter.swift
+++ misc/view/utils/NSViewAsserter.swift
@@ -1,5 +1,11 @@
-import Foundation
+import Cocoa
 
 class NSViewAsserter {
-
+    /**
+     *
+     */
+    class func contains(parent:NSView,view:NSView)->Bool{
+        for subView in view.subviews {if(view === subView){return true}}
+        return false
+    }
 }
