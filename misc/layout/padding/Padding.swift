cb582833fe81b850b688751007b3bce43c6b1f79
Eonist <unicornhive@gmail.com>
2016-01-25 18:37:09 +0100

    Files modified: 1
    Modified 1 file:misc/layout/padding/Padding.swift

--- misc/layout/padding/Padding.swift
+++ misc/layout/padding/Padding.swift
@@ -2,18 +2,30 @@ import Foundation
 /*
  * TopRightBottomLeft (TRBL aka Trouble)
  */
-class Padding {//TODO: this is incomplete
-    var top:Double = 0;
-    var right:Double = 0;
-    var bottom:Double = 0;
-    var left:Double = 0;
-    init(array:Array<Double>) {
-      switch(array.count){
-         case 1: left = array[0]; right = array[0]; top = array[0]; bottom = array[0]; break;
-         case 2: top = array[0]; bottom = array[0];left = array[1]; right = array[1]; break;
-         case 3: top = array[0]; left = array[1]; right = array[1]; bottom = array[2];break;
-         case 4: top = array[0]; right = array[1]; bottom = array[2]; left = array[3]; break;
-         default:break;
-      }
+class Padding {
+    var top:CGFloat = 0;
+    var right:CGFloat = 0;
+    var bottom:CGFloat = 0;
+    var left:CGFloat = 0;
+    required init(_ args:Any...){
+        switch(args.count){
+        case 1: left = args[0] as! CGFloat; right = args[0] as! CGFloat; top = args[0] as! CGFloat; bottom = args[0] as! CGFloat; break;
+        case 2: top = args[0] as! CGFloat; bottom = args[0] as! CGFloat;left = args[1] as! CGFloat; right = args[1] as! CGFloat; break;
+        case 3: top = args[0] as! CGFloat; left = args[1] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat;break;
+        case 4: top = args[0] as! CGFloat; right = args[1] as! CGFloat; bottom = args[2] as! CGFloat; left = args[3] as! CGFloat; break;
+        default:break;
+        }
+    }
+    /**
+     * TODO: Could this be added through an extension? seems like not!
+     */
+    init(_ args:Array<CGFloat>){
+        switch(args.count){
+        case 1: left = args[0]; right = args[0]; top = args[0]; bottom = args[0]; break;
+        case 2: top = args[0]; bottom = args[0];left = args[1]; right = args[1]; break;
+        case 3: top = args[0]; left = args[1]; right = args[1]; bottom = args[2];break;
+        case 4: top = args[0]; right = args[1]; bottom = args[2]; left = args[3]; break;
+        default:break;
+        }
     }
 }
