850204ac44041772d0a52b46b99809990d3ecb17
Eonist <unicornhive@gmail.com>
2016-01-25 12:10:03 +0100

    Files modified: 1
    Modified 1 file:misc/view/NSViewParser.swift

--- misc/view/NSViewParser.swift
+++ misc/view/NSViewParser.swift
@@ -8,7 +8,8 @@ class NSViewParser {
     func childrenOfType(view:NSView, classType:AnyClass?, protocolType:Protocol?)->Array<NSView> {
         var children:Array<NSView> = []
         for subView in view.subviews {
-            let isOfClassTypeOrProtocol:Bool = (classType != nil && subView.isKindOfClass(classType!)) || (protocolType != nil && view is protocolType!)
+            classType.Type
+            let isOfClassTypeOrProtocol:Bool = (classType != nil && subView.isKindOfClass(classType!)) || (protocolType != nil && protocol_isEqual(protocolType!, protocolType!))
             
             
             
