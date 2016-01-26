f0bbf71c87c8c202478d8f313b18ae97cf10b8fa
Eonist <unicornhive@gmail.com>
2016-01-26 12:08:27 +0100

    Files modified: 1
    Modified 1 file:math/Math.swift

--- math/Math.swift
+++ math/Math.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class Math {
-
+    /**
+     *
+     */
+    class func max<T:Comparable>(a:Element<T>,b:Element<T>){
+        
+    }
+    /**
+     *
+     */
+    class func min(){
+        
+    }
 }
