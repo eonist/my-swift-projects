f9822226518469f9c825f49dd3eafbeb4ec0beed
Eonist <unicornhive@gmail.com>
2016-01-26 12:06:28 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:misc/range/RangeParser.swift

--- misc/range/RangeParser.swift
+++ misc/range/RangeParser.swift
@@ -2,13 +2,13 @@ class RangeParser {
     /**
      * Returns the minimum or smallest value in the range.
      */
-    class func min(range:Range)->CGFloat {
-        return Math.min(range.start, range.end);
+    class func min<T:Comparable>(range:Range<T>)->T {
+        return min(range.start, range.end);
     }
     /**
      * Returns The maximum or largest value in the range.
      */
-    class func max(range:Range)->CGFloat {
-        return Math.max(range.start, range.end);
+    class func max<T:Comparable>(range:Range<T>)->T {
+        return max(range.start, range.end);
     }
 }
