4190736c1f8064312ee28a530bd2ba8c7832210d
Eonist <unicornhive@gmail.com>
2016-01-26 13:18:36 +0100

    Files modified: 1
    Modified 1 file:misc/range/RangeExtensions.swift

--- misc/range/RangeExtensions.swift
+++ misc/range/RangeExtensions.swift
@@ -3,4 +3,7 @@ import Foundation
 extension Range {
     var start:Element {return self.startIndex}
     var end:Element {return self.endIndex}
+    init(_ start:Element,_ end:Element){
+        self.init(start:start,end:end)
+    }
 }
