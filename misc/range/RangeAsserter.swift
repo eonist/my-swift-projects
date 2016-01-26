a0046c78f5ba78c6087fe14c185b4061a91cdb1b
Eonist <unicornhive@gmail.com>
2016-01-26 12:02:31 +0100

    Files modified: 1
    Modified 1 file:misc/range/RangeAsserter.swift

--- misc/range/RangeAsserter.swift
+++ misc/range/RangeAsserter.swift
@@ -3,7 +3,13 @@ class RangeAsserter{
      * Asserts if @param a equals @param b
      */
     class func equals<T:Comparable>(a:Range<T>,b:Range<T>)->Bool {
-        return a.startIndex == b.startIndex && a.endIndex == b.endIndex;
+        return a.start == b.start && a.end == b.end;
+    }
+    /**
+     * Asserts if @param a contains @param a or @param b contains @param a
+     */
+    class func contains<T:Comparable>(a:Range<T>,b:Range<T>)->Bool {
+        return a.start <= b.start && a.end >= b.end;
     }
 }
 
