a29ae57334fe07ef68d5c46bf585adff53fbd389
Eonist <unicornhive@gmail.com>
2016-01-26 12:12:25 +0100

    Files modified: 2
    Modified 2 files:math/Math.swiftmisc/range/RangeParser.swift

--- math/Math.swift
+++ math/Math.swift
@@ -4,13 +4,14 @@ class Math {
     /**
      *
      */
-    class func max<T:Comparable>(a:T,b:T)->Bool{
-        return max(a, b: b)
+    class func max<T:Comparable>(x:T,_ y:T)->Bool{
+        return max(x, y)
     }
     /**
-     * 
+     *
      */
-    class func min<T:Comparable>(a:T,b:T)->Bool{
-        return min(a, b: b)
+    /* class func min<T:Comparable>(a:T,_ b:T)->Bool{
+    return Swift.min(a, b: b)
     }
+    */
 }
