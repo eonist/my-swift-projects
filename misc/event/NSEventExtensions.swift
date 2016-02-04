bf4d2ca4f5ff66f0111a8241d02d9290f715b825
Eonist <unicornhive@gmail.com>
2016-02-04 10:35:48 +0100

    Files modified: 1
    Modified 1 file:misc/event/NSEventExtensions.swift

--- misc/event/NSEventExtensions.swift
+++ misc/event/NSEventExtensions.swift
@@ -1,5 +1,11 @@
-import Foundation
+import Cocoa
 
-class NSEventExtensions {
-
+extension NSEvent {
+    /**
+     * Returns localPosition in a view (converts a global position to a local position)
+     * TODO: hopefully this method also works if the view is not 0,0 in the window
+     */
+    func localPos(view:NSView)->CGPoint{
+        return view.convertPoint(self.locationInWindow,fromView:nil)
+    }
 }
