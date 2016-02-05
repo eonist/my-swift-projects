9f6106f82e9962bef1d2e3efbff9db5669a1ce3b
Eonist <unicornhive@gmail.com>
2016-02-05 06:01:33 +0100

    Files modified: 2
    Modified 2 files:geom/context/Shape.swiftgeom/svg/graphic/utils/SVGAsserter.swift

--- geom/svg/graphic/utils/SVGAsserter.swift
+++ geom/svg/graphic/utils/SVGAsserter.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class SVGAsserter {
-
+    /**
+     * Asserts if a point is within the SVG (empty holes will return false)
+     */
+    class func isPointWithin(svg:SVG){
+        //CGPathContainsPoint(path,transform,point)
+    }
 }
