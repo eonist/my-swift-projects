b01589e80186b9ae5ee5b4e3719a4fe348cea68c
Eonist <unicornhive@gmail.com>
2016-02-06 13:19:37 +0100

    Files modified: 2
    Modified 2 files:misc/event/EventSender.swiftmisc/event/IEventSender.swift

--- misc/event/EventSender.swift
+++ misc/event/EventSender.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class EventSender:IEventSender {
-    var event: ((event:Event) -> ())?/*this holds any method assigned to it that has its type*/
+    var event: ((Event) -> ())?/*this holds any method assigned to it that has its type*/
     /**
     * EXAMPLE: override onEvent in a subClass then assert origin === thumb && event.type == ButtonEvent.down
     */
