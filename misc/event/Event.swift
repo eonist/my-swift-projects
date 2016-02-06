8688b7fafdba93fcdc6c20eab362ec056b2fe05d
Eonist <unicornhive@gmail.com>
2016-02-06 14:49:30 +0100

    Files modified: 1
    Modified 1 file:misc/event/Event.swift

--- misc/event/Event.swift
+++ misc/event/Event.swift
@@ -2,9 +2,9 @@ import Cocoa
 
 class Event{
     var type:String/**/
-    var origin:NSView//origin sender of event, this could also be weak if you discover a memory leak
+    var origin:AnyObject//origin sender of event, this could also be weak if you discover a memory leak
     /*var immidiate:Any?*///prev sender of event
-    init(_ type:String = "", _ origin:NSView/*, immidiate:Any? = nil*/){
+    init(_ type:String = "", _ origin:AnyObject/*, immidiate:Any? = nil*/){
         self.type = type/**/
         self.origin = origin
     }
