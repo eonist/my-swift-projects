834ed68a2ce10362267d2371c4108beca5c9b571
Eonist <unicornhive@gmail.com>
2016-02-06 14:27:22 +0100

    Files modified: 1
    Modified 1 file:misc/event/IEventSender.swift

--- misc/event/IEventSender.swift
+++ misc/event/IEventSender.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 protocol IEventSender:class {
-    var event: ((Event) -> ())?{get set}
+    var event: ((Event) -> ())?{get set}//maybe rename to eventRelay?
     func onEvent(event:Event)
 }
