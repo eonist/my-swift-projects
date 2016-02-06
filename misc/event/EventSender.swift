48c60ab81a05f1a106c2e6d12394a7ff0f3358a3
Eonist <unicornhive@gmail.com>
2016-02-06 12:10:32 +0100

    Files modified: 1
    Modified 1 file:misc/event/EventSender.swift

--- misc/event/EventSender.swift
+++ misc/event/EventSender.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class EventSender {
+class EventSender:IEventSender {
     var selector: ((sender: AnyObject?, type:String) -> ())?/*this holds any method assigned to it that has its type*/
+    /**
+    * EXAMPLE: override onEvent in a subClass then assert origin === thumb && event.type == ButtonEvent.down
+    */
+    func onEvent(event:Event){
+        if(self.superview is IInteractiveView){(self.superview as! IInteractiveView).onEvent(event)}
+    }
 }
