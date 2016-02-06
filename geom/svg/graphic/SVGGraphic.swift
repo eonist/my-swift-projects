82226ed92a67b7951abb5b693c173626395caade
Eonist <unicornhive@gmail.com>
2016-02-06 18:14:55 +0100

    Files modified: 1
    Modified 1 file:geom/svg/graphic/SVGGraphic.swift

--- geom/svg/graphic/SVGGraphic.swift
+++ geom/svg/graphic/SVGGraphic.swift
@@ -13,9 +13,9 @@ class SVGGraphic : SVGView,ISVGGraphic{
         fillShape = Shape()
         lineShape = Shape()
         super.init(style!,id!);
-        wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
-        layer = CALayer()//TempCALayer(layer: layer!)
-        layer!.masksToBounds = false//this is needed!!!
+        //wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
+        //layer = CALayer()//TempCALayer(layer: layer!)
+        //layer!.masksToBounds = false//this is needed!!!
         layer?.addSublayer(fillShape)
         layer?.addSublayer(lineShape)
         self.fillShape.delegate = self/*this is needed in order to be able to retrive the context and use it whithin the decoratable methods, or else the context would reside isolated inside the Graphic.fillShape, and Graphic.lineShape*/
