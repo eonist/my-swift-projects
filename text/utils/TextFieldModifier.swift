1e0bba21d5ccb7ee1b2f62723154d363ee094554
Eonist <unicornhive@gmail.com>
2016-02-07 23:49:32 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFieldModifier.swift

--- text/utils/TextFieldModifier.swift
+++ text/utils/TextFieldModifier.swift
@@ -5,13 +5,13 @@ class TextFieldModifier {
      *
      */
     class func applyTextFormat(textField:NSText,_ textFormat:TextFormat){
-        textField.selectable = true//textFormat.selectable
+        textField.selectable = textFormat.selectable
         textField.drawsBackground = textFormat.background
         textField.backgroundColor = textFormat.backgroundColor
         textField.alignment = Utils.alignment(textFormat.align)//Left,Right,Justified,Natural,Center
         textField.textColor = textFormat.color
         textField.font = Utils.font(textFormat.font,textFormat.size)
-        textField.editable = true//textFormat.type == "input"
+        textField.editable = textFormat.type == "input"
         //textField.bordered = false//<--maybe implement later
     }
     /**
