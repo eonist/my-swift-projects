ca9cbaf8fcd77c958f38b986915071ae0e296735
Eonist <unicornhive@gmail.com>
2016-02-08 14:11:06 +0100

    Files modified: 1
    Modified 1 file:text/TextField.swift

--- text/TextField.swift
+++ text/TextField.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
 
-class TextField {
-
+class TextField:NSTextField{
+    /**
+     * NOTE: You must use InteractiveView as a parent for this class to work
+     * NOTE: the hitTesting bellow is the only combination I found that will give a correct hit. the x can also be derived from the
+     */
+    override func hitTest(aPoint: NSPoint) -> NSView? {
+        Swift.print("CustomText: hitTest()" + "\(aPoint)" + " localPos(): " + "\(localPos())")
+        let tempPos = self.convertPoint(aPoint,fromView:nil)
+        Swift.print("tempPos: " + "\(tempPos)")
+        return super.hitTest(CGPoint(aPoint.x,localPos().y))
+    }
 }
