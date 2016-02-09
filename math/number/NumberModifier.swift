9415755ee001de9e8ec15a42f05b8f3bfb074c99
Eonist <unicornhive@gmail.com>
2016-02-09 13:05:19 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberModifier.swift

--- math/number/NumberModifier.swift
+++ math/number/NumberModifier.swift
@@ -6,4 +6,13 @@ class NumberModifier{
     class func swap(inout a:Double, inout _ b:Double) {
         ObjectModifier.swapObjects(&a, &b)
     }
+    /**
+    * Returns a number that is incremented by @param incrementation
+    * @Note: The multiplication and division is a floating bug fix, optimized by looking at how many decimal points the @param number has
+    */
+    public static function increment(number:Number,incrementation:Number):Number {
+    var decimalCount:int = NumberParser.decimals(number);
+    var modifier:Number =  Math.pow(10, decimalCount);
+    return ((number*modifier) + (incrementation*modifier))/modifier;
+    }
 }
