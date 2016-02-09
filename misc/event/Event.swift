30be64aae50d984eca5559a340ceeb3dc4a8f635
Eonist <unicornhive@gmail.com>
2016-02-09 19:04:48 +0100

    Files modified: 1
    Modified 1 file:misc/event/Event.swift

--- misc/event/Event.swift
+++ misc/event/Event.swift
@@ -1,7 +1,6 @@
 import Cocoa
-
+class EventType{static var change:String = "eventChange"}
 class Event{
-    static var change:String = "eventChange"
     var type:String/**/
     var origin:AnyObject//origin sender of event, this could also be weak if you discover a memory leak
     /*var immidiate:Any?*///prev sender of event
