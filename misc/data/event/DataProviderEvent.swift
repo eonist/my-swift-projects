10aafa6c86e0e9ece18a43d63104756bcd993eea
Eonist <unicornhive@gmail.com>
2016-02-13 20:17:04 +0100

    Files modified: 1
    Modified 1 file:misc/data/event/DataProviderEvent.swift

--- misc/data/event/DataProviderEvent.swift
+++ misc/data/event/DataProviderEvent.swift
@@ -1,5 +1,10 @@
 import Foundation
-
+/**
+ * NOTE: we dont add items here, we get access to items from the DataProvider instance instead. 
+ */
 class DataProviderEvent :Event{
-
+    //continue here 
+    init(_ type: String, startIndex:Int = -1, endIndex:Int = -1, _ origin: AnyObject) {
+        super.init(type, origin)
+    }
 }
