e226e34772f6e0c6dacd8d13ea2eec2d3e3e0d53
Eonist <unicornhive@gmail.com>
2016-02-13 07:26:30 +0100

    Files modified: 1
    Modified 1 file:misc/event/Event.swift

--- misc/event/Event.swift
+++ misc/event/Event.swift
@@ -1,14 +1,15 @@
 import Foundation
 /*
- * TODO: Implement the immediate variable if its needed (it would be a way to get assert the 1-level down immediate child an event came from, rather than the origin child which can be many levels deeper in the hirarchy)
+ * TODO: Implement the immediate variable if its needed (it would be a way to get assert the 1-level down immediate child an event came from, rather than the origin child which can be many levels deeper in the hierarchy)
  */
 class Event{
-    static var update:String = "eventUpdate"/*Idealy I would name this change but aparantly then subclasses can name their const the same*/
+    static var update:String = "eventUpdate"/*Ideally I would name this change but apparently then subclasses can name their const the same*/
     var type:String/**/
     var origin:AnyObject/*origin sender of event, this could also be weak if you discover a memory leak*/
-    /*var immidiate:Any?*///prev sender of event
-    init(_ type:String = "", _ origin:AnyObject/*, immidiate:Any? = nil*/){
-        self.type = type/**/
+    var immediate:AnyObject/*prev sender of event*/
+    init(_ type:String = "", _ origin:AnyObject, immediate:AnyObject){
+        self.type = type
         self.origin = origin
+        self.immediate = immediate
     }
 }
