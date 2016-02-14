942b69ec4dc2e09ba08ca13092e4c08148ecd74e
Eonist <unicornhive@gmail.com>
2016-02-14 06:50:11 +0100

    Files modified: 1
    Modified 1 file:misc/data/event/DataProviderEvent.swift

--- misc/data/event/DataProviderEvent.swift
+++ misc/data/event/DataProviderEvent.swift
@@ -4,8 +4,13 @@ import Foundation
  */
 class DataProviderEvent :Event{
     
-    
-    //continue here: add types
+    public static const TYPES:Array = [ADD,DATA_CHANGE,REMOVE,REMOVE_ALL,REPLACE,SORT];
+    public static const ADD:String = "add";
+    public static const REMOVE:String = "remove";
+    public static const REMOVE_ALL:String = "removeAll";
+    public static const REPLACE:String = "replace";
+    public static const SORT:String = "sort";
+    public static const DATA_CHANGE:String = "dataChange";
     
     
     init(_ type: String, startIndex:Int = -1, endIndex:Int = -1, _ origin: AnyObject) {
