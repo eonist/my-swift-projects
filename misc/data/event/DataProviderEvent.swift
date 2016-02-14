9a2a638b6121f44143f9e3bb22b4bde98a52e990
Eonist <unicornhive@gmail.com>
2016-02-14 06:52:24 +0100

    Files modified: 1
    Modified 1 file:misc/data/event/DataProviderEvent.swift

--- misc/data/event/DataProviderEvent.swift
+++ misc/data/event/DataProviderEvent.swift
@@ -3,17 +3,16 @@ import Foundation
  * NOTE: we dont add items here, we get access to items from the DataProvider instance instead.
  */
 class DataProviderEvent :Event{
-    
-    public static const TYPES:Array = [ADD,DATA_CHANGE,REMOVE,REMOVE_ALL,REPLACE,SORT];
-    public static const ADD:String = "add";
-    public static const REMOVE:String = "remove";
-    public static const REMOVE_ALL:String = "removeAll";
-    public static const REPLACE:String = "replace";
-    public static const SORT:String = "sort";
-    public static const DATA_CHANGE:String = "dataChange";
-    
-    
+    static var types:Array = [add,dataChange,remove,removeAll,replace,sort];
+    static var add:String = "add";
+    static var remove:String = "remove";
+    static var removeAll:String = "removeAll";
+    static var replace:String = "replace";
+    static var sort:String = "sort";
+    static var dataChange:String = "dataChange";
     init(_ type: String, startIndex:Int = -1, endIndex:Int = -1, _ origin: AnyObject) {
+        self.startIndex = startIndex;
+        self.endIndex = endIndex;
         super.init(type, origin)
     }
 }
