ccd5456ef946471569bd9962a16188c5fc4bd287
Eonist <unicornhive@gmail.com>
2016-02-14 16:50:49 +0100

    Files modified: 1
    Modified 1 file:misc/data/utils/DataProviderParser.swift

--- misc/data/utils/DataProviderParser.swift
+++ misc/data/utils/DataProviderParser.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class DataProviderParser {
-    
+    /**
+     * TODO: Use a real Range type here
+     */
+    class func itemsInRange(start:Int, end:Int,_ items:[Dictionary<String,String>]){
+        var theItems:[Dictionary<String,String>] = []
+        for var i = start; i < end; ++i{
+            theItems.append(items[i])
+        }
+    }
 }
