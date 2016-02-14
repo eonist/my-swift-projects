aabdec0af3a47a3d6ffa718b3213529c5168453d
Eonist <unicornhive@gmail.com>
2016-02-14 09:40:04 +0100

    Files modified: 1
    Modified 1 file:array/ArrayExtension.swift

--- array/ArrayExtension.swift
+++ array/ArrayExtension.swift
@@ -42,4 +42,7 @@ extension Array {
     func index<T : Equatable>(arr : [T], _ value:T)->Int{
         return ArrayParser.index(arr, value)
     }
+    func indexOf(arr:Array<AnyObject>,_ item:AnyObject)-> Int{
+        return ArrayParser.indexOf(arr,item)
+    }
 }
