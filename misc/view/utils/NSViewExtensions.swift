84d52235b1fced0765da7e1d19949302ad9c4643
Eonist <unicornhive@gmail.com>
2016-02-14 15:39:20 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewExtensions.swift

--- misc/view/utils/NSViewExtensions.swift
+++ misc/view/utils/NSViewExtensions.swift
@@ -39,6 +39,9 @@ extension NSView {
         self.addSubview(view)
         return view
     }
+    func addSubviewAt(view: NSView,_ index:Int){
+        self.addSubview(view, positioned: <#T##NSWindowOrderingMode#>, relativeTo: <#T##NSView?#>)(view)
+    }
     /**
      * Asserts if @param view is a subView of @param parent
      */
