ab488fc70ea9ab3a85ba8de5b17bb81850ff9ecc
Eonist <unicornhive@gmail.com>
2016-02-15 06:48:34 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/depth/DepthModifier.swift

--- misc/view/utils/depth/DepthModifier.swift
+++ misc/view/utils/depth/DepthModifier.swift
@@ -1,5 +1,10 @@
-import Foundation
+import Cocoa
 
 class DepthModifier {
-
+    /**
+     *
+     */
+    class func sortByList(container:NSView, _ containerFieldName:String, _ sortListFieldName:String, _ sortList:[Dictionary<String,String>]){
+        fatalError("not implemented yet")
+    }
 }
