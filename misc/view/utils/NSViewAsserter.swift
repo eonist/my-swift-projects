993cab73c48f1c7fec43628f63fd051824a0d8df
Eonist <unicornhive@gmail.com>
2016-02-17 08:04:21 +0100

    Files modified: 1
    Modified 1 file:misc/view/utils/NSViewAsserter.swift

--- misc/view/utils/NSViewAsserter.swift
+++ misc/view/utils/NSViewAsserter.swift
@@ -5,7 +5,12 @@ class NSViewAsserter {
      * Asserts if @param view is a subView of @param parent
      */
     class func contains(parent:NSView,_ view:NSView)->Bool{
-        for subView in view.subviews {if(view === subView){return true}}
+        for subView in view.subviews {
+            Swift.print("subView: " + "\(subView)")
+            if(view === subView){
+                return true
+            }
+        }
         return false
     }
 }
