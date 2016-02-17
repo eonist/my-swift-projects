dcb9a87c77f1f8ebb788f49f5a5b6c6ce6243a12
Eonist <unicornhive@gmail.com>
2016-02-17 15:52:37 +0100

    Files modified: 1
    Modified 1 file:dictionary/DictionaryParser.swift

--- dictionary/DictionaryParser.swift
+++ dictionary/DictionaryParser.swift
@@ -1,3 +1,13 @@
 class DictionaryParser{
-    
+    /**
+     * Describes properties in an object
+     * // :TODO: maybe write a method that works on Class instances aswell? you probably could use that Class method that returns an xml of data about the object
+     */
+    class func describe(dictionary:Dictionary){
+        //print("object is: " + typeof(dictionary) + ": " + dictionary);
+        for(var val:* in dictionary) {
+            trace(val + ' => ' + dictionary[val] + '   [' + typeof(dictionary[val]) + '] ');
+            if(typeof(dictionary[val]) == "dictionary") describe(dictionary[val]);
+        }
+    }
 }
