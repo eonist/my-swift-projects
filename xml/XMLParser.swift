3d41deca107481647839274f6e1e5fab0185837b
Eonist <unicornhive@gmail.com>
2016-02-17 15:45:53 +0100

    Files modified: 1
    Modified 1 file:xml/XMLParser.swift

--- xml/XMLParser.swift
+++ xml/XMLParser.swift
@@ -106,16 +106,14 @@ public class XMLParser{
         for (var i = 0; i < count; i++) {
             let child:NSXMLElement = XMLParser.childAt(xml.children!, i)!
             //print("Import - child.toXMLString(): " + child.toXMLString());
-            var item:Dictionary<String,String>
+            var item:Dictionary<String,String> = Dictionary<String,String>()
             let attributes:[Dictionary<String,String>] = XMLParser.attributes(child)
             for attribute in attributes {
-                item[attribute["name"]!] = attribute["value"]
+                item[attribute["name"]!] = attribute["value"]!
             }
-            if(child.stringValue) { item["xml"] = child.stringValue }// :TODO: this may need to be rolled back to previouse code state
+            if(child.stringValue != nil) { item["xml"] = child.stringValue! }// :TODO: this may need to be rolled back to previouse code state
             items.append(item);
-
         }
-       
         return items;
     }
 }
