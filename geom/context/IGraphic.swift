18630a64c3d96a64328cb6a8ac94d539e268890b
Eonist <unicornhive@gmail.com>
2016-02-20 12:01:39 +0100

    Files modified: 1
    Modified 1 file:geom/context/IGraphic.swift

--- geom/context/IGraphic.swift
+++ geom/context/IGraphic.swift
@@ -3,8 +3,8 @@ import Cocoa
 protocol IGraphic {
     var fillShape:Shape{get}
     var lineShape:Shape{get}
-    var lineStyle:ILineStyle?{get}
-    var fillStyle:IFillStyle?{get}
+    var lineStyle:ILineStyle?{get set}
+    var fillStyle:IFillStyle?{get set}
     func setProperties(fillStyle:IFillStyle?, lineStyle:ILineStyle?)
     var lineOffsetType:OffsetType{get set}
 }
