45f4b47b498ee7bfb532aaaad714d6dd2b221bc6
Eonist <unicornhive@gmail.com>
2016-02-23 14:48:23 +0100

    Files modified: 1
    Modified 1 file:misc/view/FlippedView.swift

--- misc/view/FlippedView.swift
+++ misc/view/FlippedView.swift
@@ -6,11 +6,15 @@ protocol IView:class{/*the :class enables you to set variables via extension*/
     var frame: NSRect{get set}
     func addSubview(aView: NSView)
 }
-class FlippedView:NSOpenGLView{
+class FlippedView:NSView{
+    override var flipped:Bool {return true}/*Organizes your view from top to bottom*/
+}
+class FlippedView2:NSOpenGLView{
+
     override init?(frame frameRect: NSRect, pixelFormat format: NSOpenGLPixelFormat?) {
         super.init(frame: frameRect, pixelFormat: NSOpenGLView.defaultPixelFormat())
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
