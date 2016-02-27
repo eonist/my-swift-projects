39a1cf64ccbe9f3131a9b8301a002c55bb3cfc97
Eonist <unicornhive@gmail.com>
2016-02-27 07:11:03 +0100

    Files modified: 1
    Modified 1 file:misc/date/Timer.swift

--- misc/date/Timer.swift
+++ misc/date/Timer.swift
@@ -1,7 +1,10 @@
+import CoreFoundation
+import CoreGraphics
+import Darwin
 import Foundation
 
 class Timer:NSTimer {
-    init(_ timeInterval:NSTimeInterval,_ target:AnyObject,_ selector:Selector,_ userInfo:AnyObject?,_ repeats:Bool ){
-        super.init(timeInterval: timeInterval, target: target, selector: selector, userInfo: userInfo, repeats: repeats)
+    init(_ timeInterval:NSTimeInterval,_ target:AnyObject,_ aSelector:'Selector',_ userInfo:AnyObject?,_ repeats:Bool ){
+        super.init(timeInterval: timeInterval, target: target, selector: aSelector, userInfo: userInfo, repeats: repeats)
     }
 }
