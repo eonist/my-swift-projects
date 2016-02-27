fee48588d657b4ed3208f055b95beb4207bdb652
Eonist <unicornhive@gmail.com>
2016-02-27 12:58:13 +0100

    New files added: 1
    Added 1 file:misc/view/IInteractiveView.swift

---
+++ misc/view/IInteractiveView.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+protocol IInteractiveView:IEventSender {
+    //func onEvent(event:Event)
+    func mouseMoved(event:MouseEvent)
+    func mouseOver(event:MouseEvent)
+    func mouseOut(event:MouseEvent)
+    func mouseDown(event:MouseEvent)
+    func mouseUpInside(event: MouseEvent)
+    func mouseUpOutside(event: MouseEvent)
+    func mouseUp(event: MouseEvent)/*convenient*/
+}
