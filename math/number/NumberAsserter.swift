e76fa4e343ed7c90f39b69c71edd768b8c6d7e07
Eonist <unicornhive@gmail.com>
2016-03-01 08:17:29 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberAsserter.swift

--- math/number/NumberAsserter.swift
+++ math/number/NumberAsserter.swift
@@ -19,12 +19,12 @@ class NumberAsserter{
      * print(NumberAsserter.isNear(-1.8650465545944293 , -1.8650465545944273, NumberAsserter.EPSILON));//true, result is 10e-15 which is less than 10e-12
      * NOTE: i think this could be written simpler: Math.abs(a - b) <= epsilon, test different cases to verify
      */
-    class func isNear(a:CGFloat,b:CGFloat,epsilon:CGFloat)->Bool {//this could also be named almostEqual
+    class func isNear(a:CGFloat,_ b:CGFloat,_ epsilon:CGFloat)->Bool {//this could also be named almostEqual
         if(a == b){ return true}
         else{
-            let result:CGFloat = Swift.max(a, b) - Swift.min(a, b);
+            let result:CGFloat = Swift.max(a, b) - Swift.min(a, b)
             //print("result: " + result);
-            return result < epsilon;
+            return result < epsilon
         }
     }
 }
