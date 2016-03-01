c5835ab01861c4a88a6a865fcadaed632c68a65c
Eonist <unicornhive@gmail.com>
2016-03-01 08:13:16 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberAsserter.swift

--- math/number/NumberAsserter.swift
+++ math/number/NumberAsserter.swift
@@ -1,4 +1,7 @@
 import Foundation
+/**
+ * Definition of Asserter: state a fact or belief confidently and forcefully
+ */
 class NumberAsserter{
     /**
      * @Note: In most cases you will want to write if(someNumber < 0)
@@ -11,5 +14,18 @@ class NumberAsserter{
      */
     class func positive(number:CGFloat)->Bool {
         return number > 0;
+    }
+    /**
+     * print(NumberAsserter.isNear(-1.8650465545944293 , -1.8650465545944273, NumberAsserter.EPSILON));//true, result is 10e-15 which is less than 10e-12
+     * NOTE: i think this could be written simpler: Math.abs(a - b) <= epsilon, test different cases to verify
+     */
+    class func isNear(a:CGFloat,b:CGFloat,epsilon:CGFloat)->Bool {//this could also be named almostEqual
+        if(a == b){
+            return true;
+        }else{
+            var result:CGFloat = Math.max(a, b) - Math.min(a, b);
+            //print("result: " + result);
+            return result < epsilon;
+        }
     }
 }
