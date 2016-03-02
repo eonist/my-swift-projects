4e0ec4e2d3cb682fec4e5b6c71218ebc514c5d3a
Eonist <unicornhive@gmail.com>
2016-03-02 14:41:26 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: math/number/NumberExtensions.swiftModified 1 file:math/number/cgfloat/CGFloatExtension.swift

--- math/number/cgfloat/CGFloatExtension.swift
+++ math/number/cgfloat/CGFloatExtension.swift
@@ -5,3 +5,31 @@ extension CGFloat {
         return NumberModifier.toFixed(self, places)
     }
 }
+/**
+ * Support for addition of CGFLoat and Double
+ * TODO: probably use T or Generics or similar
+ * TODO: add support for returning Double aswell
+ * TODO: add support for Int and Float aswell
+ */
+public func + (left: CGFloat, right: Double) -> CGFloat {
+    return left + CGFloat(right)
+}
+/**
+ * Support for addition of CGFLoat and Double
+ */
+public func + (left: Double, right: CGFloat) -> CGFloat {
+    return CGFloat(left) + right
+}
+/**
+ *
+ */
+public func * (left: Int, right: CGFloat) -> CGFloat {
+    return CGFloat(left) + right
+}
+
+/**
+ *
+ */
+public func * (left: CGFloat, right: Int) -> CGFloat {
+    return left + CGFloat(right)
+}
