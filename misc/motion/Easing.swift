664b60ca690cece0256306ed95b0872689e6bb75
Eonist <unicornhive@gmail.com>
2016-03-06 13:30:49 +0100

    Files modified: 1
    Modified 1 file:misc/motion/Easing.swift

--- misc/motion/Easing.swift
+++ misc/motion/Easing.swift
@@ -28,8 +28,10 @@ class Easing{
 		t = t/d-1
 		return c*(t*t*t*t*t + 1) + b
 	}
-	class func easeInOutQuint = ^CGFloat(CGFloat t, CGFloat b, CGFloat c, CGFloat d) {
-		if ((t/=d/2) < 1) {return c/2*t*t*t*t*t + b}
+	class func easeInOutQuint(var t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat) {
+		if ((t/=d/2) < 1) {
+            return c/2*t*t*t*t*t + b
+        }
 		return {c/2*((t-=2)*t*t*t*t + 2) + b}
 	}
 }
