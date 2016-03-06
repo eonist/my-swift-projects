c63c96e504e3559dea1bff71cee9757b5a0e69e4
Eonist <unicornhive@gmail.com>
2016-03-06 14:07:11 +0100

    Files modified: 1
    Modified 1 file:misc/motion/Easing.swift

--- misc/motion/Easing.swift
+++ misc/motion/Easing.swift
@@ -45,7 +45,12 @@ class Easing{
 		t = (t/d)-1//<--the brackets are important
 		return -c * (t*t*t*t - 1) + b
 	}
-    
+    class func easeInOutQuart(var t:CGFloat, b:CGFloat, c:CGFloat, d:CGFloat) -> CGFloat{
+		t = t/(d/2)//<--the brackets are important
+		if (t < 1) {return c/2*t*t*t*t + b}
+		t = t-2
+		return -c/2 * (t*t*t*t - 2) + b;
+	}
 }
 
 /*
