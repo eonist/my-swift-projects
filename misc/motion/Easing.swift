1c04f81acb9d537281c87e73a25efbe77ed2c6a5
Eonist <unicornhive@gmail.com>
2016-03-06 14:05:09 +0100

    Files modified: 1
    Modified 1 file:misc/motion/Easing.swift

--- misc/motion/Easing.swift
+++ misc/motion/Easing.swift
@@ -4,6 +4,7 @@ import Cocoa
  * b: begin (the value it is at the begining)
  * c: change (end value - begining value) sort of the amount to change, this can also be described as the difference between two values
  * d: duration (total frames in anim) this can also be actual time
+ * TODO:
  */
 class Easing{
     //No easing, linear animation
