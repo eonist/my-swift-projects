735b4ad36aff2a5512da694d2ce5f7873ff187de
Eonist <unicornhive@gmail.com>
2016-03-07 21:19:31 +0100

    Files modified: 2
    Modified 2 files:geom/context/Graphic.swiftmisc/motion/BaseAnimation.swift

--- misc/motion/BaseAnimation.swift
+++ misc/motion/BaseAnimation.swift
@@ -2,6 +2,7 @@ import Cocoa
 
 class BaseAnimation {
     var view:AnimatableView//ref to where the displayLink recides
+    var drawCall:(()->Void)?
     init(_ view:AnimatableView){
         self.view = view
     }
