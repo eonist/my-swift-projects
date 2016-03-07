7a593e34c3242431a4f5becdd7e1e3ae0945fbfd
Eonist <unicornhive@gmail.com>
2016-03-07 10:27:34 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -109,7 +109,7 @@ class Graphic:InteractiveView2,IGraphic{
      * PARAM: owner is the instance that receives the interaction event
      * NOTE: we could keep the trackingArea in graphic so its always easy to access, but i dont think it needs to be easily accesible atm.
      */
-    func updateTrackingArea() {//TODO:you dont have to store the trackingarea in this class you can get and set the trackingarea from NSView
+    override func updateTrackingAreas() {//TODO:you dont have to store the trackingarea in this class you can get and set the trackingarea from NSView
         //Swift.print("updateTrackingArea: " + "\(fillShape.frame)")
         //Swift.print("\(NSViewParser.parents(self))" + ".updateTrackingArea: " + "\(fillShape.frame)")
         if(trackingArea != nil) {self.removeTrackingArea(trackingArea!)}//remove old trackingArea if it exists
