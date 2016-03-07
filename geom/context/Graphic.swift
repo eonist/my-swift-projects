81294f15122b5363d933430bc088a9bdb94e67ef
Eonist <unicornhive@gmail.com>
2016-03-07 07:37:06 +0100

    Files modified: 1
    Modified 1 file:geom/context/Graphic.swift

--- geom/context/Graphic.swift
+++ geom/context/Graphic.swift
@@ -108,7 +108,7 @@ class Graphic:InteractiveView2,IGraphic{
     override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
         //Swift.print("Graphic.drawLayer(layer,inContext)")
         selector!(layer: layer,ctx: ctx)/*call the selector*/
-        updateTrackingArea()
+        updateTheTrackingArea()
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
 }
@@ -127,9 +127,9 @@ extension Graphic{
      * PARAM: owner is the instance that receives the interaction event
      * NOTE: we could keep the trackingArea in graphic so its always easy to access, but i dont think it needs to be easily accesible atm.
      */
-    func updateTrackingArea() {//TODO:you dont have to store the trackingarea in this class you can get and set the trackingarea from NSView
-        //Swift.print("updateTrackingArea: " + "\(fillShape.frame)")
-        Swift.print("\(NSViewParser.parents(self))" + ".updateTrackingArea: " + "\(fillShape.frame)")
+    func updateTheTrackingArea() {//TODO:you dont have to store the trackingarea in this class you can get and set the trackingarea from NSView
+        Swift.print("updateTrackingArea: " + "\(fillShape.frame)")
+        //Swift.print("\(NSViewParser.parents(self))" + ".updateTrackingArea: " + "\(fillShape.frame)")
         if(trackingArea != nil) {self.removeTrackingArea(trackingArea!)}//remove old trackingArea if it exists
         trackingArea = NSTrackingArea(rect: fillShape.frame, options: [NSTrackingAreaOptions.ActiveAlways, NSTrackingAreaOptions.MouseMoved,NSTrackingAreaOptions.MouseEnteredAndExited], owner: self, userInfo: nil)
         self.addTrackingArea(trackingArea!)//<---this will be in the Skin class in the future and the owner will be set to Element to get interactive events etc
