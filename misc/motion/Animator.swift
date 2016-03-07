afa151ae8d1db98a06e7a1c1e98828ae32d2f859
Eonist <unicornhive@gmail.com>
2016-03-07 11:01:21 +0100

    Files modified: 2
    Modified 2 files:misc/motion/Animator.swiftmisc/motion/BaseAnimation.swift

--- misc/motion/Animator.swift
+++ misc/motion/Animator.swift
@@ -9,7 +9,6 @@ import Cocoa
 
 class Animator:BaseAnimation{
     let fps:CGFloat = 60//this should be pulled from a device variable
-    var view:AnimatableView//ref to where the displayLink recides
     var duration:CGFloat/*in seconds*/
     var from:CGFloat//from this value
     var to:CGFloat//to this value
@@ -40,21 +39,5 @@ class Animator:BaseAnimation{
             stop()
         }
         self.currentFrameCount++
-    }
-    /**
-     * Start the animation
-     */
-    func start(){
-        //Swift.print("start")
-        if(!CVDisplayLinkIsRunning(view.displayLink)){CVDisplayLinkStart(view.displayLink)}//start the displayLink if it isnt already running
-        view.animators.append(self)//add your self to the list of animators that gets the onFrame call
-    }
-    /**
-     * Stop the animation
-     */
-    func stop(){
-        //Swift.print("stop")
-        view.animators.removeAt(view.animators.indexOf(self))
-        if(view.animators.count == 0 && CVDisplayLinkIsRunning(view.displayLink)){CVDisplayLinkStop(view.displayLink)}//stops the frame ticker if there is not active running animators
     }
 }
--- misc/motion/BaseAnimation.swift
+++ misc/motion/BaseAnimation.swift
@@ -1,7 +1,27 @@
-import Foundation
+import Cocoa
 
 class BaseAnimation {
+    var view:AnimatableView//ref to where the displayLink recides
+    init(view:AnimatableView){
+        self.view = view
+    }
     func onFrame(){
         fatalError("Must be overwritten in subclass")
+    }
+    /**
+     * Start the animation
+     */
+    func start(){
+        //Swift.print("start")
+        if(!CVDisplayLinkIsRunning(view.displayLink)){CVDisplayLinkStart(view.displayLink)}//start the displayLink if it isnt already running
+        view.animators.append(self)//add your self to the list of animators that gets the onFrame call
+    }
+    /**
+     * Stop the animation
+     */
+    func stop(){
+        //Swift.print("stop")
+        view.animators.removeAt(view.animators.indexOf(self))
+        if(view.animators.count == 0 && CVDisplayLinkIsRunning(view.displayLink)){CVDisplayLinkStop(view.displayLink)}//stops the frame ticker if there is not active running animators
     }
 }
