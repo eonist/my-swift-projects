c961a7b63e0ae5caf1e002618a145d7482c49bef
Eonist <unicornhive@gmail.com>
2016-03-08 18:45:32 +0100

    Files modified: 1
    Modified 1 file:misc/motion/Easing.swift

--- misc/motion/Easing.swift
+++ misc/motion/Easing.swift
@@ -127,9 +127,27 @@ class Easing{
         /*if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
         return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;*/
     }
-    private class func (){
-    
+    //Bounce
+    private class func easeInBounce(){
+        //return c - easeOutBounce (d - t, 0, c, d) + b;
     }
+    private class func easeOutBounce(){
+        /*if ((t/=d) < (1/2.75)) {
+        return c*(7.5625*t*t) + b;
+        } else if (t < (2/2.75)) {
+        return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+        } else if (t < (2.5/2.75)) {
+        return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+        } else {
+        return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+        }*/
+    }
+    private class func easeInOutBounce(){
+        /*if (t < d/2) return easeInBounce (t*2, 0, c, d) * 0.5 + b;
+        else return easeOutBounce (t*2-d, 0, c, d) * 0.5 + c*.5 + b;*/
+    }
+    
+    
 }
 
 /*
