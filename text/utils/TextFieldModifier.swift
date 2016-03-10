efc7464a4816d3b0a3c978bb6d154e25e3b02d9b
Eonist <unicornhive@gmail.com>
2016-03-10 16:00:47 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFieldModifier.swift

--- text/utils/TextFieldModifier.swift
+++ text/utils/TextFieldModifier.swift
@@ -16,11 +16,11 @@ class TextFieldModifier {
         textField.focusRingType = NSFocusRingType.None//<- implement suport for this if needed, Personally i dont like it. You can use the focus state instead and have your own focus style applied
         textField.bordered = textFormat.border//<--maybe implement later
         //textField.maximumNumberOfLines = 1//<---cant get these to work yet
-        textField.usesSingleLineMode = false//<---cant get these to work yet
-        textField.lineBreakMode = .ByWordWrapping
+        textField.usesSingleLineMode = true//<---cant get these to work yet
+        //textField.lineBreakMode = .ByWordWrapping
         
         textField.cell?.wraps = true//textFormat.wordWrap//wordwrap enables the text to be in one line basically
-        textField.setContentCompressionResistancePriority(250, forOrientation: .Horizontal)
+        textField.setContentCompressionResistancePriority(180, forOrientation: .Horizontal)
 
         textField.cell?.scrollable = textFormat.wordWrap//i guess this is connected to wordWrap
     }
