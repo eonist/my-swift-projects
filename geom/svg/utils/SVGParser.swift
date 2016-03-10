b6ff17515235b40ac6892709f056cd11da97590b
Eonist <unicornhive@gmail.com>
2016-03-10 13:21:51 +0100

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGParser.swift

--- geom/svg/utils/SVGParser.swift
+++ geom/svg/utils/SVGParser.swift
@@ -1,6 +1,14 @@
 import Foundation
 /**
- * // :TODO: impliment text (<text x="60" y="165" style="font-family: sans-serif; font-size: 14pt; stroke: none; fill: black;">Cat</text>)
+ * SVG class is where pther svg elemnts is added
+ * // :TODO: impliment xmlns:xlink,viewBox,enable-background,xml:space,
+ * //preserveAspectRatio="xMinYMin meet"
+ * //viewBox="0 0 90 90"
+ * @example
+ * <?xml version="1.0"?><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="768px" height="768px">
+ * 		<rect x="64" y="64" fill="#0000FF" stroke="#000000" stroke-miterlimit="10" width="512" height="512"/>
+ * </svg>
+ * TODO: impliment text (<text x="60" y="165" style="font-family: sans-serif; font-size: 14pt; stroke: none; fill: black;">Cat</text>)
  */
 class SVGParser {
     /**
