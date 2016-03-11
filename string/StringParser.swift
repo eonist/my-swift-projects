dcc8b82ac24f8727591e2e335d99dd002f7830ae
Eonist <unicornhive@gmail.com>
2016-03-11 12:54:02 +0100

    Files modified: 1
    Modified 1 file:string/StringParser.swift

--- string/StringParser.swift
+++ string/StringParser.swift
@@ -131,4 +131,22 @@ class StringParser{
             return uintColor;//green, blue, orange etc// :TODO: support for all of w3c color types// :TODO: move this to a method named webColor?
         }
     }
+    /**
+     * Returns a file path, excluding the file name and file-suffix
+     * @param input: usually formated like: /Users/James/Downloads/PanelView.png
+     * @return a string formatted like: /Users/James/Downloads/
+     * @Example path(file://Users/User/Desktop/main.css);//file://Users/User/Desktop/
+     * @Note you can also do this another way:
+     * var match : Array = input.split(".");
+     * var path:String = String(match[0]).substring(0, String(match[0]).lastIndexOf("/"));
+     */
+    class func path(url:String)->String {
+        return url.match("^.*?\\/(?=\\w*?\\.\\w*?$)")[0]
+    }
+    /**
+     *
+     */
+    class func fileName(url:String)->String {
+        return url.match("^.*?\\/(\\w*?\\.\\w*?$)")[1]
+    }
 }
