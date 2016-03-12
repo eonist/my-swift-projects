fd89fe0fc8412350c0a8ebcc0f9021b777b5ce47
Eonist <unicornhive@gmail.com>
2016-03-12 20:44:48 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFieldConstants.swift

--- text/utils/TextFieldConstants.swift
+++ text/utils/TextFieldConstants.swift
@@ -1,5 +1,7 @@
 import Foundation
-
+/*
+ * NOTE: It may be tempting to make variables of each of these types, don't its just more code
+ */
 class TextFieldConstants {
     static var textFieldPropertyNames:Array<String> = ["alwaysShowSelection","antiAliasType","autoSize","alpha","background","backgroundColor","border","borderColor","condenseWhite","displayAsPassword","embedFonts","gridFitType","height","htmlText","maxChars","mouseWheelEnabled","mouseEnabled","multiline","restrict","sharpness","selectable","thickness","type","textColor","useRichTextClipboard","width","wordWrap"];
 }
