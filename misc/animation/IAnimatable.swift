0dd89d00048ab122b1e66f564d575d1887852279
Eonist <unicornhive@gmail.com>
2016-03-13 08:32:30 +0100

    New files added: 1
    Added 1 file:misc/animation/IAnimatable.swift

---
+++ misc/animation/IAnimatable.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+protocol IAnimatable:class {//this should probably be renamed to IAnimatableView
+    func onFrame()
+    var animators:Array<BaseAnimation>{get set}
+    //var drawCalls:Array<()->Void>{get set}
+    var displayLink: CVDisplayLink{get}
+}
