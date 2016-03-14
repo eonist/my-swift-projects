6f8d39292f73d1121708381e3607697b6cd0d04c
Eonist <unicornhive@gmail.com>
2016-03-14 19:05:56 +0100

    Files modified: 1
    Modified 1 file:text/utils/TextFieldModifier.swift

--- text/utils/TextFieldModifier.swift
+++ text/utils/TextFieldModifier.swift
@@ -37,12 +37,12 @@ class TextFieldModifier {
 }
 private class Utils{
     /**
-     *
+     * TODO: The paragraph style stuff doesnt seem to work. more research needed
      */
     class func font(fontName:String,_ size:CGFloat) -> NSFont{
-        let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()//NSMutableParagraphStyle.defaultParagraphStyle()
-        paragraphStyle.lineSpacing = 200.0//other paragraph options: paragraphSpacing,alignment,firstLineHeadIndent,lineBreakMode,minimumLineHeight,maximumLineHeight,baseWritingDirection,lineHeightMultiple,paragraphSpacingBefore
-        let fontDescriptor:NSFontDescriptor = NSFontDescriptor(fontAttributes: [NSFontNameAttribute:fontName,NSParagraphStyleAttributeName:paragraphStyle])//NSForegroundColorAttributeName:NSColor.redColor(),
+        //let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()//NSMutableParagraphStyle.defaultParagraphStyle()
+        //paragraphStyle.lineSpacing = 10.0//other paragraph options: paragraphSpacing,alignment,firstLineHeadIndent,lineBreakMode,minimumLineHeight,maximumLineHeight,baseWritingDirection,lineHeightMultiple,paragraphSpacingBefore
+        let fontDescriptor:NSFontDescriptor = NSFontDescriptor(fontAttributes: [NSFontNameAttribute:fontName/*,NSParagraphStyleAttributeName:paragraphStyle*/])//NSForegroundColorAttributeName:NSColor.redColor(),
         let font = NSFont(descriptor: fontDescriptor, size: size)
         return font!
     }
