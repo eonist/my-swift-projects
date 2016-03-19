056479af3f7e746f77d1ca4939f09e904e933873
Eonist <unicornhive@gmail.com>
2016-03-19 13:41:41 +0100

    Files modified: 2
    Modified 2 files:misc/shell/keychain/KeyChainModifier.swiftmisc/shell/keychain/KeyChainParser.swift

--- misc/shell/keychain/KeyChainModifier.swift
+++ misc/shell/keychain/KeyChainModifier.swift
@@ -1,5 +1,32 @@
 import Foundation
 
 class KeyChainModifier {
-
+    /**
+     * Save keychain data for key
+     * TODO: move to KeyChainModifier.swift
+     */
+    class func save(key: String, _ data: NSData) -> Bool {
+        let query = [kSecClass as String : kSecClassGenericPassword as String, kSecAttrAccount as String : key,  kSecValueData as String   : data ]
+        SecItemDelete(query as CFDictionaryRef)
+        let status: OSStatus = SecItemAdd(query as CFDictionaryRef, nil)
+        return status == noErr
+    }
+    /**
+     * Deletes a keychain item for key
+     */
+    class func delete(key: String) -> Bool {
+        let query = [ kSecClass as String : kSecClassGenericPassword,kSecAttrAccount as String : key ]
+        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
+        return status == noErr
+    }
+    /**
+     * what does this method do? research needed
+     * TODO: move to KeyChainModifier.swift
+     * EXAMPLE: Keychain.clear()
+     */
+    class func clear() -> Bool {
+        let query = [ kSecClass as String : kSecClassGenericPassword ]
+        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
+        return status == noErr
+    }
 }
--- misc/shell/keychain/KeyChainParser.swift
+++ misc/shell/keychain/KeyChainParser.swift
@@ -18,16 +18,7 @@ class KeyChainParser {
         let keyChainData:Dictionary<String,String> = ["accountName":"eonist","accountPassword":"12345"]
         return keyChainData
     }
-    /**
-     * Save keychain data for key
-     * TODO: move to KeyChainModifier.swift
-     */
-    class func save(key: String, _ data: NSData) -> Bool {
-        let query = [kSecClass as String : kSecClassGenericPassword as String, kSecAttrAccount as String : key,  kSecValueData as String   : data ]
-        SecItemDelete(query as CFDictionaryRef)
-        let status: OSStatus = SecItemAdd(query as CFDictionaryRef, nil)
-        return status == noErr
-    }
+    
 	/**
 	 * Returns a keychain item for key
 	 */	
@@ -43,25 +34,6 @@ class KeyChainParser {
         return nil
     }
     
-	/**
-	 * Deletes a keychain item for key
-	 * TODO: move to KeyChainModifier.swift
-	 */
-    class func delete(key: String) -> Bool {
-        let query = [ kSecClass as String : kSecClassGenericPassword,kSecAttrAccount as String : key ]
-        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
-        return status == noErr
-    }
-    /**
-	  * what does this method do? research needed
-	  * TODO: move to KeyChainModifier.swift
-      * EXAMPLE: Keychain.clear()
-	  */
-    class func clear() -> Bool {
-        let query = [ kSecClass as String : kSecClassGenericPassword ]
-        let status: OSStatus = SecItemDelete(query as CFDictionaryRef)
-        return status == noErr
-    }
 }
 
 
