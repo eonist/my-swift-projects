3d0c389740249eb95a93fd25092e9397fade4979
Eonist <unicornhive@gmail.com>
2016-03-19 13:20:15 +0100

    Files modified: 1
    Modified 1 file:misc/shell/keychain/KeyChainUtils.swift

--- misc/shell/keychain/KeyChainUtils.swift
+++ misc/shell/keychain/KeyChainUtils.swift
@@ -3,12 +3,15 @@ import Foundation
 class KeyChainUtils {
     
     /**
-    *
-    */
+     *
+     */
     class func dataValue(string:String)->NSData{
         return string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
     }
-
+    /**
+     *
+     */
+    class func stringValue(data:NSData)->String{
+        return NSString(data: data, encoding: NSUTF8StringEncoding)!
     }
-    
 }
