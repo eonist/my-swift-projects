749acb902e4a6b27d78de204b21dfd0f103583f7
Eonist <unicornhive@gmail.com>
2016-03-19 15:06:50 +0100

    Files modified: 1
    Modified 1 file:misc/shell/keychain/KeyChainParser.swift

--- misc/shell/keychain/KeyChainParser.swift
+++ misc/shell/keychain/KeyChainParser.swift
@@ -2,23 +2,6 @@ import Security
 import Foundation
 //examples here: https://gist.github.com/s-aska/e7ad24175fb7b04f78e7
 class KeyChainParser {
-    /**
-     *
-     * Returns a record with account name and password by querrying keychain with the "keychain item name" of the password-keychain-item
-     * Note: the_keychain_item_name is the "name" of the keychain-password-item
-     * Caution: Make sure your keychain item is unique, or else it may return the wrong keychain, this includes secure notes
-     * Note: appending find-generic-password -a  & account_name &  -g will retrive keychain itm name and pass from account name
-     * Example: keychain_data("flowerpower2")--{keychain_item_name:"flowerpower2", account_name:"John", the_password:"HereIsJohnny2015"}
-     * Note: to access a record use the_password of keychain_data
-     * Todo: impliment support for retriving the comment in the keychain item
-     * Caution: If there are unusual characters in the password, it isn't output as plain text, it's output encoded in hex. Here's a python script I've been using which covers that case: http://blog.macromates.com/2006/keychain-access-from-shell/
-     * Caution: If the password contains special chars, the password will be returned as hex values, Use ShellUtil's hex_to_ascii(hex_text
-     */
-    class func keychainData(keychainItemName:String)->Dictionary<String,String>{//TODO:return a tuple
-        let keyChainData:Dictionary<String,String> = ["accountName":"eonist","accountPassword":"12345"]
-        return keyChainData
-    }
-    
 	/**
 	 * Returns a keychain item for key
 	 */	
@@ -35,7 +18,6 @@ class KeyChainParser {
         }
         return nil
     }
-    
 }
 
 
