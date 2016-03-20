3277b4a0c1d989f1e87984ff0394c7aa4efa5c59
Eonist <unicornhive@gmail.com>
2016-03-20 12:34:10 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: misc/shell/keychain/KeyChainUtils.swiftModified 1 file:misc/shell/keychain/KeyChainParser.swift

--- misc/shell/keychain/KeyChainParser.swift
+++ misc/shell/keychain/KeyChainParser.swift
@@ -27,8 +27,27 @@ class KeyChainParser {
     }
 }
 
+class KeyChainUtils {
+    /**
+     *
+     */
+    class func dataValue(string:String)->NSData{
+        return string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+    }
+    /**
+     *
+     */
+    class func stringValue(data:NSData)->String{
+        return NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+    }
+}
+extension String {
+    public var dataValue: NSData {return KeyChainUtils.dataValue(self)}
+}
 
-
+extension NSData {
+    public var stringValue: String {return KeyChainUtils.stringValue(self)}
+}
 
 
 
