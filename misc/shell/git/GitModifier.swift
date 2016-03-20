151cb3478a2d8375666fc33b762e87f41de9143c
Eonist <unicornhive@gmail.com>
2016-03-20 05:37:44 +0100

    Files modified: 1
    Modified 1 file:misc/shell/git/GitModifier.swift

--- misc/shell/git/GitModifier.swift
+++ misc/shell/git/GitModifier.swift
@@ -33,7 +33,7 @@ class GitModifier{
     */
    class func commit(localRepoPath:String, _ messageTitle:String, _ messageDescription:String)->String{
    	//log ("GitModifier's commit(" + message_title + ")")
-   	let shellScript:String = /*"cd " + localRepoPath + ";" + */gitPath + "git commit" + " " + "-m" + " '" + messageTitle + "' " + "-m" + " '" + messageDescription + "'"
+   	let shellScript:String = /*"cd " + localRepoPath + ";" + */gitPath + "git commit" + " -m '" + messageTitle.encode() + "' -m '" + messageDescription.encode() + "'"
     Swift.print("shellScript: " + "\(shellScript)")
    	return ShellUtils.run(shellScript,localRepoPath)
    }
