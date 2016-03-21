13dc557b95b16c374e42e5554cda8ba1443f3e40
Eonist <unicornhive@gmail.com>
2016-03-21 20:40:53 +0100

    Files modified: 1
    Modified 1 file:xml/XMLParser.swift

--- xml/XMLParser.swift
+++ xml/XMLParser.swift
@@ -70,7 +70,8 @@ public class XMLParser{
      */
     class func childAt(xml:NSXMLElement?,index:Array<Int>)->NSXMLElement? {
         if(index.count == 0 && xml != nil) {return xml}
-        //else if(index.count == 1 && xml != nil && xml!.children![index[0]] != nil) {return xml!.children[index]}// :TODO: if index.length is 1 you can just ref index
+        //xml!.children![0]
+        else if(index.count == 1 && xml != nil && xml!.children![0] != nil) {return xml!.children[index]}// :TODO: if index.length is 1 you can just ref index
         //else if(index.count > 1 && xml.children().length() > 0) return childAt(xml.children()[index[0]],index.slice(1,index.length))
         return nil
     }
