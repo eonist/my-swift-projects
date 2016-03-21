77f516df1ba55fa2141fa3bce6eca82a17eebae8
Eonist <unicornhive@gmail.com>
2016-03-21 17:48:38 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/Node.swift

--- misc/data/node/Node.swift
+++ misc/data/node/Node.swift
@@ -1,5 +1,27 @@
 import Foundation
-
-class Node {
+/**
+ * // :TODO: addItemsAt
+ * // :TODO: removeAll
+ * // :TODO: replaceItemAt
+ * // :TODO: setNameAt
+ * // :TODO: removeRange(range)
+ * @example:
+ * // :TODO: Node should have some core methods for easy access
+ * // :TODO: add a method for setting the xml setXML that also dispatches an event
+ */
+class Node {// :TODO: this should stricly be a DataClass, all none basic functions that are not excplicit get or set should be moved to Parser,Modifier, asserter classes, also make a not of thi sin the java doc
 
 }
+
+
+/*
+In math terms:
+Root – The top node in a tree.
+Child – A node directly connected to another node when moving away from the Root.
+Parent – The converse notion of a child.
+Siblings – Nodes with the same parent.
+Descendant – A node reachable by repeated proceeding from parent to child.
+Ancestor – A node reachable by repeated proceeding from child to parent.
+Leaf – A node with no children.
+Degree – Number of sub trees of a node.
+*/
