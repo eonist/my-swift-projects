6e04bc5f3c64e6a01b40d357b666ef26d04bf78a
Eonist <unicornhive@gmail.com>
2016-03-21 21:15:35 +0100

    Files modified: 2
    Modified 2 files:misc/data/node/Node.swiftmisc/data/node/utils/NodeModifier.swift

--- misc/data/node/utils/NodeModifier.swift
+++ misc/data/node/utils/NodeModifier.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 class NodeModifier {
-    /**
-     * Removes the item @param index in @param database
-     */
-    func removeItemAt(database:Node, index:Array<Int>) -> NSXMLElement {
-        return XMLModifier.removeChildAt(database.xml, index);
-    }
+    
 }
