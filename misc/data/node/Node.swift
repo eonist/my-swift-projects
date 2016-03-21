04d846e15eebfb8f44a2b684a8ab8bdfd283d828
Eonist <unicornhive@gmail.com>
2016-03-21 18:26:21 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/Node.swift

--- misc/data/node/Node.swift
+++ misc/data/node/Node.swift
@@ -14,8 +14,8 @@ class Node {// :TODO: this should stricly be a DataClass, all none basic functio
     init(xml:NSXMLElement) {
         self.xml = xml;
     }
-    func removeAt(index:Array):NSXMLElement{// :TODO: do we still need the event dispatching, cant the calling method do this?
-        var removedXML:NSXMLElement = NodeModifier.removeItemAt(this, index);
+    func removeAt(index:Array)->NSXMLElement{// :TODO: do we still need the event dispatching, cant the calling method do this?
+        var removedXML:NSXMLElement = NodeModifier.removeItemAt(self, index);
         dispatchEvent(new DatabaseEvent(DatabaseEvent.REMOVE_AT,index,removedXML));// :TODO: we could dispatch from the DatabaseModifier method
         return removedXML;
     }
