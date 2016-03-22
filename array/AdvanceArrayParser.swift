c82628d22c866955d477989df7c3d03be394c9ec
Eonist <unicornhive@gmail.com>
2016-03-22 07:38:18 +0100

    Files modified: 1
    Modified 1 file:array/AdvanceArrayParser.swift

--- array/AdvanceArrayParser.swift
+++ array/AdvanceArrayParser.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 class AdvanceArrayParser {
-
+    /**
+     * Returns the branch at @param index
+     * @Note this function is recursive
+     * @Note to find the children of the root use an empty array as the index value
+     */
+    class func childAt<T>(children:Array<T>,index:Array<Int>)->T{
+        if(index.count == 0 && children.count >= index[])
+    }
 }
+
+
+if(index.length == 0 && xml != null) return xml;
+else if(index.length == 1 && xml != null && xml.children()[index[0]] != null) return xml.children()[index];// :TODO: if index.length is 1 you can just ref index
+else if(index.length > 1 && xml.children().length() > 0) return childAt(xml.children()[index[0]],index.slice(1,index.length));
+return null;
