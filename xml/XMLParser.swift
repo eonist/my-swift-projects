09c228f8235b9da3f20c75ad01d1fad3240a5e9a
Eonist <unicornhive@gmail.com>
2016-03-22 18:51:54 +0100

    Files modified: 1
    Modified 1 file:xml/XMLParser.swift

--- xml/XMLParser.swift
+++ xml/XMLParser.swift
@@ -136,20 +136,16 @@ public class XMLParser{
      */
     class func toXML(content:AnyObject,_ name:String)->NSXMLElement{
         let xml:NSXMLElement = try! NSXMLElement("<"+name+"></"+name+">")
-        if(content is [String:AnyObject]){
+        if(content is [String:AnyObject]){//content is a dictionary
             let dict = content as! [String:AnyObject]
             for (theKey,theValue) in dict{
                 print("key: \(theKey) value: \(theValue)")
                 if(theValue is [String:AnyObject]) {xml.appendChild(toXML(dict,theKey))}
                 else {xml[theKey] = theValue as? String}
-                //
-                //if(theValue is Dictionary) {describe(theValue)}
             }
-
-        }else{
-            //add to stringValue
+        }else{//content is string
+            xml.stringValue = content as? String
         }
-                //xml["x1"] = "\(line.x1)";
         return xml
     }
 }
