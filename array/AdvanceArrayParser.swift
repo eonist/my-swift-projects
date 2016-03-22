3c830d38139b9d2b89de05bd97250f4483707337
Eonist <unicornhive@gmail.com>
2016-03-22 07:57:46 +0100

    Files modified: 1
    Modified 1 file:array/AdvanceArrayParser.swift

--- array/AdvanceArrayParser.swift
+++ array/AdvanceArrayParser.swift
@@ -9,17 +9,7 @@ class AdvanceArrayParser {
     class func childAt<T>(children:Array<T>,_ index:Array<Int>)->[T]?{
         if(index.count == 0 && children.count >= 1){return children}/*returns the root*/
         else if(index.count == 1 && children.count >= index[0]){return [children[index[0]]]}/*returns a branch, packaged as a branch*/
-        else if(index.count > 1 && children.count > 0) {
-            return AdvanceArrayParser.childAt(children[index[0]] as! Array<T>,index.slice(1,index.count))
-        }
+        else if(index.count > 1 && children.count > 0) {return AdvanceArrayParser.childAt(children[index[0]] as! Array<T>,index.slice(1,index.count))}
         return nil
     }
 }
-
-/*
-if(index.length == 0 && xml != null) return xml;
-else if(index.length == 1 && xml != null && xml.children()[index[0]] != null) return xml.children()[index];// :TODO: if index.length is 1 you can just ref index
-else if(index.length > 1 && xml.children().length() > 0) return childAt(xml.children()[index[0]],index.slice(1,index.length));
-return null;
-
-*/
