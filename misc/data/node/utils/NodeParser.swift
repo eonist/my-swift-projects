082c30b9c91579954226d6d7a9278121837bce43
Eonist <unicornhive@gmail.com>
2016-03-22 17:15:04 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeParser.swift

--- misc/data/node/utils/NodeParser.swift
+++ misc/data/node/utils/NodeParser.swift
@@ -58,8 +58,8 @@ class NodeParser {
             }
             if(child.stringValue != nil && child.stringValue!.count > 0) { node.content = child.stringValue! }// :TODO: this may need to be rolled back to previouse code state
             
-            if(/*child.kind = NSXMLNodeKind. && */child.childCount > 0){
-                Swift.print("child.kind: " + "\(child.kind)")
+            if(child.childCount > 0){
+                Swift.print("child.kind: " + "\(child.children![0])")
                 NodeParser.node(child, node)//this makes the method recursive
             }
             root.children.append(node)
