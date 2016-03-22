d6f3f8044f58702c30ef0a09165f9e79efe700f0
Eonist <unicornhive@gmail.com>
2016-03-22 07:00:51 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeModifier.swift

--- misc/data/node/utils/NodeModifier.swift
+++ misc/data/node/utils/NodeModifier.swift
@@ -4,12 +4,12 @@ class NodeModifier {
     /**
      * Removes the item @param index in @param node
      */
-    func removeAt(inout node:Node, _ index:Array<Int>) -> Node {
+    func removeAt(inout node:Node, _ index:Array<Int>) -> Node/**/ {
         
         //continue here: check if you have a removeAt that is recursive
-        if(index.count == 1 && node.children.count > index[0]){removeAt(&node, index[0])}
+        if(index.count == 1 && node.children.count > index[0]){return removeAt(&node, index[0])}
         else if(index.count > 1 && node.children.count > index[0] && node.children[index[0]].children.count > 0){/*recursive*/
-            node = removeAt()
+            node = removeAt(node.children[index[0]],index.slice(1,index.count))
         }
         /*
         }else if(index.length > 1 && xml.children().length() > 0 && (xml.children()[index[0]] as XML).toXMLString() != null){
@@ -19,7 +19,7 @@ class NodeModifier {
         return node
         
     }
-    func removeAt(inout node:Node, _ index:Int){
-        ArrayModifier.removeAt(&node.children, index)
+    func removeAt(inout node:Node, _ index:Int)->Node{
+        return ArrayModifier.removeAt(&node.children, index)
     }
 }
