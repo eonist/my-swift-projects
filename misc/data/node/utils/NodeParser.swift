257e0a4a9066953d0149b70e493e4ffd72204f55
Eonist <unicornhive@gmail.com>
2016-03-22 11:32:59 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeParser.swift

--- misc/data/node/utils/NodeParser.swift
+++ misc/data/node/utils/NodeParser.swift
@@ -2,17 +2,19 @@ import Foundation
 
 class NodeParser {
     /**
-     * EXAMPLE: nodeAt(Node())
+     * Returns the branch at @param index
+     * @Note this function is recursive
      */
     class func nodeAt(node:Node,_ index:Array<Int>)->Node?{
-        if(index.count == 1 && node.children.count >= index[0]){return nodeAt(node, index[0])}/*the index is at its end point, cut of the branch*/
+        if(index.count == 0){return node}/*returns the root*/
+        else if(index.count == 1 && node.children.count >= index[0]){return nodeAt(node, index[0])}/*the index is at its end point, cut of the branch*/
         else if(index.count > 1 && node.children.count >= index[0] && node.children[index[0]].children.count > 0){/*recursive*/
             return nodeAt(node.children[index[0]],index.slice2(1,index.count))
         }
-        return node
+        return nil
     }
     /**
-     *
+     * 
      */
     class func nodeAt(node:Node,_ index:Int) -> Node?{
         return node.children[index]
