97483ed5a166a8bbeb2fd275aa8fac36389dd82c
Eonist <unicornhive@gmail.com>
2016-03-22 07:38:18 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeModifier.swift

--- misc/data/node/utils/NodeModifier.swift
+++ misc/data/node/utils/NodeModifier.swift
@@ -6,8 +6,8 @@ class NodeModifier {
      */
     class func removeAt(inout node:Node, _ index:Array<Int>) -> Node {
         Swift.print("UNTESTED")
-        if(index.count == 1 && node.children.count > index[0]){return removeAt(&node, index[0])}/*the index is at its end point, cut of the branch*/
-        else if(index.count > 1 && node.children.count > index[0] && node.children[index[0]].children.count > 0){/*recursive*/
+        if(index.count == 1 && node.children.count >= index[0]){return removeAt(&node, index[0])}/*the index is at its end point, cut of the branch*/
+        else if(index.count > 1 && node.children.count >= index[0] && node.children[index[0]].children.count > 0){/*recursive*/
             node = removeAt(&node.children[index[0]],index.slice(1,index.count))
         }
         return node
