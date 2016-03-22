b923eeffe53625405d9aa02c65ad3fc3151874a1
Eonist <unicornhive@gmail.com>
2016-03-22 06:45:01 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeModifier.swift

--- misc/data/node/utils/NodeModifier.swift
+++ misc/data/node/utils/NodeModifier.swift
@@ -8,12 +8,16 @@ class NodeModifier {
         
         //continue here: check if you have a removeAt that is recursive
         if(index.count == 1 && node.children.count > index[0]){removeAt(&node, index[0])}
+        else if(index.count > 1){
             
+        }
+         /*
             
         }else if(index.length > 1 && xml.children().length() > 0 && (xml.children()[index[0]] as XML).toXMLString() != null){
             xml = replaceChildAt(xml.children()[index[0]],index.slice(1,index.length),replacement);
         }
-        return xml;
+        */
+        return node
         
     }
     func removeAt(inout node:Node, _ index:Int){
