2698b03c2f7fff4ec9c60b4c5dee082276440b26
Eonist <unicornhive@gmail.com>
2016-03-22 14:50:17 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/events/NodeEvent.swift

--- misc/data/node/events/NodeEvent.swift
+++ misc/data/node/events/NodeEvent.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class NodeEvent:Event{
+    
+    static var addAt:String = "databaseEventAddAt"//we mergeed add_at and add since its essentialy the same
+    static var removeAt:String = "databaseEventRemoveAt"
+    static var removeAll:String = "nodeEventRemoveAll"
+    static var removeAll:String = "nodeEventRemoveAll"
+    
+    databaseEventSetAttributeAt
 
 }
