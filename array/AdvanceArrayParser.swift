2177877957d171a166f937481902621ec3a995cc
Eonist <unicornhive@gmail.com>
2016-03-22 08:07:51 +0100

    Files modified: 1
    Modified 1 file:array/AdvanceArrayParser.swift

--- array/AdvanceArrayParser.swift
+++ array/AdvanceArrayParser.swift
@@ -11,7 +11,8 @@ class AdvanceArrayParser {
         if(index.count == 0 && children.count >= 1){return children}/*returns the root*/
         else if(index.count == 1 && children.count >= index[0]){return [children[index[0]]]}/*the index is at its end point, cut of the branch and packaged as a branch*/
         else if(index.count > 1 && children.count > 0) {
-            return AdvanceArrayParser.childAt(children[index[0]],index.slice(1,index.count))
+            let temp = children[index[0]]
+            return AdvanceArrayParser.childAt(temp,index.slice(1,index.count))
         }/*here is where the recursive magic happens*/
         return nil
     }
