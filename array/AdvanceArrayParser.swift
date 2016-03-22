f7f4ed3ba70b3428c48bd10a9ce0bb955e3ea6eb
Eonist <unicornhive@gmail.com>
2016-03-22 08:11:47 +0100

    Files modified: 1
    Modified 1 file:array/AdvanceArrayParser.swift

--- array/AdvanceArrayParser.swift
+++ array/AdvanceArrayParser.swift
@@ -7,12 +7,11 @@ class AdvanceArrayParser {
      * @Note to find the children of the root use an empty array as the index value
      * EXAMPLE: childAt([[red,green][[four,five][1,2,3]]],[1,0,1])//[five]
      */
-    class func childAt(children:Array<Any>,_ index:Array<Int>)->Array<Any>?{
+    class func childAt(children:Array<AnyObject>,_ index:Array<Int>)->Array<AnyObject>?{
         if(index.count == 0 && children.count >= 1){return children}/*returns the root*/
         else if(index.count == 1 && children.count >= index[0]){return [children[index[0]]]}/*the index is at its end point, cut of the branch and packaged as a branch*/
         else if(index.count > 1 && children.count > 0) {
-            let temp = children[index[0]] as! Array<Any>
-            return AdvanceArrayParser.childAt(temp,index.slice(1,index.count))
+            return AdvanceArrayParser.childAt(children[index[0]] as! Array<AnyObject>,index.slice(1,index.count))
         }/*here is where the recursive magic happens*/
         return nil
     }
