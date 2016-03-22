77f999aa1009e936f534fddafa7afe29e4f7bdac
Eonist <unicornhive@gmail.com>
2016-03-22 11:54:41 +0100

    Files modified: 1
    Modified 1 file:misc/data/node/utils/NodeAsserter.swift

--- misc/data/node/utils/NodeAsserter.swift
+++ misc/data/node/utils/NodeAsserter.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class NodeAsserter {
-
+    /**
+     * Asserts if a Node has children at @param index 
+     */
+    class func hasChildrenAt(node:Node,_ index:Array<Int>) -> Bool {
+        return NodeParser.countAt(node,index) > 0
+    }
 }
