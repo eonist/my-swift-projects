a3a4ae51979b8f2bd7854148c8572246b2aff06d
Eonist <unicornhive@gmail.com>
2016-03-23 18:23:12 +0100

    Files modified: 1
    Modified 1 file:misc/data/database/utils/DatabaseParser.swift

--- misc/data/database/utils/DatabaseParser.swift
+++ misc/data/database/utils/DatabaseParser.swift
@@ -12,7 +12,14 @@ class DatabaseParser {
      * @example: DatabaseParser.attributesAt(database,[0,0])["title"]
      */
     class func attributesAt(db:Database, _ index:Array<Int>) -> [[String:String]]?{// :TODO: rename to objAt?
-        var xml:NSXMLElement? = childAt(db,index)
-        return xml != nil ? XMLParser.attributes(xml) : nil
+        let xml:NSXMLElement? = childAt(db,index)
+        return xml != nil ? XMLParser.attributes(xml!) : nil
+    }
+    /**
+     * Returns a value of an attribute at @param index in @param database by the key @param name
+     * @Note: undefined is returned if attribute does not exist
+     */
+    func attributeAt(database:Database, index:Array,name:String) ->  {
+        return attributesAt(database, index)[name];
     }
 }
