8ec6ee549cbe97f66852b1deacccbd95df151d3f
Eonist <unicornhive@gmail.com>
2016-03-23 18:06:25 +0100

    Files modified: 3
    Modified 3 files:misc/data/database/Database.swiftmisc/data/database/utils/DatabaseAsserter.swiftmisc/data/database/utils/DatabaseParser.swift

--- misc/data/database/Database.swift
+++ misc/data/database/Database.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 class Database {
-
+    var xml : NSXMLElement;
+    init(xml:NSXMLElement) {
+        self.xml = xml;
+    }
 }
--- misc/data/database/utils/DatabaseAsserter.swift
+++ misc/data/database/utils/DatabaseAsserter.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class DatabaseModifier {
+class DatabaseAsserter {
 
 }
