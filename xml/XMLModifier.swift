577aa51ffd5c565ef83e7c066cf34a6f41b92b3a
Eonist <unicornhive@gmail.com>
2016-03-23 20:58:06 +0100

    Files modified: 1
    Modified 1 file:xml/XMLModifier.swift

--- xml/XMLModifier.swift
+++ xml/XMLModifier.swift
@@ -7,7 +7,19 @@ public class XMLModifier {
      * worth a look (constructing html): https://gist.github.com/brentdax/caaaa134c500e00efd36
      */
     public class func test(){
-        //xml.addChild(<#T##child: NSXMLNode##NSXMLNode#>)
+        //xml.addChild()
+        
+        /**
+         * Adds @param child to @param xml at an array index @param index
+         * // :TODO: needs some refactoring can we use hasComplexContent as an asserter? research
+         */
+        public static function addChildAt(xml:XML,index:Array,child:XML):XML {// :TODO: rename to addAt?
+            if(index.length == 1) xml = insertAt(xml, index[0], child);
+            else if(index.length > 1 && xml.children().length() > 0 && (xml.children()[index[0]] as XML).toXMLString() != null){
+                xml = addChildAt(xml.children()[index[0]],index.slice(1,index.length),child);
+            }
+            return xml;
+        }
     }
 }
 
