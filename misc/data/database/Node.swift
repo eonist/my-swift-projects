d7c5be93a931d32ae43b6ccfabe7b9dd578a4df3
Eonist <unicornhive@gmail.com>
2016-03-24 15:33:28 +0100

    Files modified: 1
    Modified 1 file:misc/data/database/Node.swift

--- misc/data/database/Node.swift
+++ misc/data/database/Node.swift
@@ -20,7 +20,7 @@ class Node : EventSender{
      */
     func addAt(index:Array<Int>,xml:NSXMLElement){// :TODO: shouldnt the arguments be in this order: xml, index// :TODO: do we still need the event dispatching, cant the calling method do this?
         XMLModifier.addChildAt(self.xml, index, xml);
-        onEvent(DatabaseEvent(DatabaseEvent.addAt,index,self))
+        onEvent(NodeEvent(NodeEvent.addAt,index,self))
     }
     /**
      * @example setAttributeAt([0], ["title":"someTitle"]);
@@ -28,13 +28,13 @@ class Node : EventSender{
      */
     func setAttributeAt(index:Array<Int>,_ attributes:Dictionary<String,String>){// :TODO: do we still need the event dispatching, cant the calling method do this?
         XMLModifier.setAttributeAt(xml, index, attributes)
-        onEvent(DatabaseEvent(DatabaseEvent.setAttributeAt,index,self))
+        onEvent(NodeEvent(NodeEvent.setAttributeAt,index,self))
     }
     /**
-     * Removes the item @param index in @param database
+     * Removes the item @param index
      */
     func removeAt(index:Array<Int>){// :TODO: do we still need the event dispatching, can't the calling method do this?
         XMLModifier.removeChildAt(xml, index)
-        onEvent(DatabaseEvent(DatabaseEvent.removeAt,index,self))
+        onEvent(NodeEvent(NodeEvent.removeAt,index,self))
     }
 }
