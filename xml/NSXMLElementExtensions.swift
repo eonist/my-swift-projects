86d4ea15ea82bcb3434da62eed371a3817e80444
Eonist <unicornhive@gmail.com>
2016-03-24 08:17:10 +0100

    Files modified: 1
    Modified 1 file:xml/NSXMLElementExtensions.swift

--- xml/NSXMLElementExtensions.swift
+++ xml/NSXMLElementExtensions.swift
@@ -42,4 +42,8 @@ extension NSXMLElement {
     func childAt(index:Array<Int>)->NSXMLElement?{
         return XMLParser.childAt(self, index)
     }
+    public func childAtIndex(index:Int)->NSXMLElement?{
+        return self.childAtIndex(<#T##index: Int##Int#>)
+    }
+    
 }
