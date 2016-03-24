42b37aa5d96358e17d6fa77cebc9dd38879a912a
Eonist <unicornhive@gmail.com>
2016-03-24 13:42:14 +0100

    Files modified: 1
    Modified 1 file:misc/data/database/Database.swift

--- misc/data/database/Database.swift
+++ misc/data/database/Database.swift
@@ -5,4 +5,11 @@ class Database {
     init(xml:NSXMLElement) {
         self.xml = xml
     }
+    /**
+     * // :TODO: consider renaming to appendAt
+     */
+    func addAt(index:Array<Int>,xml:NSXMLElement){// :TODO: shouldnt the arguments be in this order: xml, index// :TODO: do we still need the event dispatching, cant the calling method do this?
+        DatabaseModifier.addAt(self, index, xml);
+        dispatchEvent(new DatabaseEvent(DatabaseEvent.ADD_AT,index,xml,true));
+    }
 }
