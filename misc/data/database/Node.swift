89f9f85a80fabc2ce1189d442bdb523e2145d93b
Eonist <unicornhive@gmail.com>
2016-03-24 19:56:01 +0100

    Files modified: 1
    Modified 1 file:misc/data/database/Node.swift

--- misc/data/database/Node.swift
+++ misc/data/database/Node.swift
@@ -37,3 +37,24 @@ class Node : EventSender{//this should probably extend NSXMLElement, and just im
         onEvent(NodeEvent(NodeEvent.removeAt,index,self))
     }
 }
+/*
+Terminologies used in Trees:
+
+Root – The top node in a tree.
+Child – A node directly connected to another node when moving away from the Root.
+Parent – The converse notion of a child.
+Siblings – Nodes with the same parent.
+Descendant – A node reachable by repeated proceeding from parent to child.
+Ancestor – A node reachable by repeated proceeding from child to parent.
+Leaf – A node with no children.
+Internal node – A node with at least one child
+External node – A node with no children.
+Degree – Number of sub trees of a node.
+Edge – Connection between one node to another.
+Path – A sequence of nodes and edges connecting a node with a descendant.
+Level – The level of a node is defined by 1 + (the number of connections between the node and the root).
+Height of node – The height of a node is the number of edges on the longest path between that node and a leaf.
+Height of tree – The height of a tree is the height of its root node.
+Depth – The depth of a node is the number of edges from the node to the tree's root node.
+Forest – A forest is a set of n ≥ 0 disjoint trees.
+*/
