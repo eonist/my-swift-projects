b2416c5e27da934dfb994267942ca1741bc1b42c
Eonist <unicornhive@gmail.com>
2016-03-24 13:26:40 +0100

    Files modified: 1
    Modified 1 file:xml/XMLAsserter.swift

--- xml/XMLAsserter.swift
+++ xml/XMLAsserter.swift
@@ -20,6 +20,20 @@ public class XMLAsserter {
     class func hasChildren(xml:NSXMLElement) -> Bool {
         return xml.childCount > 0
     }
+    /**
+     * Checks whether the specified string is valid and well formed XML.
+     * @param data The string that is being checked to see if it is valid XML.
+     * @return A Boolean value indicating whether the specified string is
+     * valid XML.
+     */
+    class func validXML(xmlStr:String)->Bool{
+        let xmlDoc:NSXMLDocument? = try? NSXMLDocument(XMLString: xmlStr, options: 0)
+        
+
+        catch(e:Error){return false;}
+        if(xml.nodeKind() != XMLConstants.ELEMENT){return false;}
+        return true;
+    }
 }
 
 
