6e9b69a8fcc3f0b1595aa5a6508eaf0504aef1f5
Eonist <unicornhive@gmail.com>
2016-03-24 13:44:10 +0100

    Files modified: 2
    Modified 2 files:misc/data/database/Database.swiftmisc/data/database/events/DatabaseEvent.swift

--- misc/data/database/Database.swift
+++ misc/data/database/Database.swift
@@ -9,7 +9,7 @@ class Database {
      * // :TODO: consider renaming to appendAt
      */
     func addAt(index:Array<Int>,xml:NSXMLElement){// :TODO: shouldnt the arguments be in this order: xml, index// :TODO: do we still need the event dispatching, cant the calling method do this?
-        DatabaseModifier.addAt(self, index, xml);
-        dispatchEvent(new DatabaseEvent(DatabaseEvent.ADD_AT,index,xml,true));
+        XMLModifier.addChildAt(self.xml, index, xml);
+        DatabaseEvent(DatabaseEvent.addAt,index,xml,true)
     }
 }
--- misc/data/database/events/DatabaseEvent.swift
+++ misc/data/database/events/DatabaseEvent.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class DatabaseEvent {
-
+    static var addAt:String = "databaseEventAddAt"//we mergeed add_at and add since its essentialy the same
+    static var removeAt:String = "databaseEventRemoveAt"
+    static var removeAll:String = "databaseEventRemoveAll"
+    static var changeAt:String = "databaseEventChangeAt"
+    var index:Array<Int>
+    init(_ type: String, _ index:Array<Int>, _ origin: AnyObject) {
+        self.index = index;
+        super.init(type, origin)
+    }
 }
