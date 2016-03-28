1c2ea51228d7302188c7fdd46722b42c5fa60969
Eonist <unicornhive@gmail.com>
2016-03-28 12:37:21 +0200

    Files modified: 1
    Modified 1 file:dictionary/DictionaryExtensions.swift

--- dictionary/DictionaryExtensions.swift
+++ dictionary/DictionaryExtensions.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class DictionaryExtensions {
-
+extension Dictionary {
+    func combine<K,V>(b:Dictionary<K,V>)->Dictionary<K,V>{//// :TODO: rename to combine since merge implies that the passed obj is also changed
+        return DictionaryModifier.combine(self, b)
+    }
 }
