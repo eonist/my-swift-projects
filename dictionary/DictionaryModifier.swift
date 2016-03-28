034f2d5f7bf5fe3ba3b81ff7022072a09539021f
Eonist <unicornhive@gmail.com>
2016-03-28 12:35:20 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:dictionary/DictionaryModifier.swift

--- dictionary/DictionaryModifier.swift
+++ dictionary/DictionaryModifier.swift
@@ -1,11 +1,10 @@
-
 class DictionaryModifier{
 	/**
 	 * Merges 2 arrays, changes the left array, the right is untouched
 	 * Returns the left dictionary for convenience purposes
 	 */
-	class func merge <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, _ right: Dictionary<KeyType, ValueType>)-> Dictionary<KeyType, ValueType>{
-	    for (k, v) in right { left.updateValue(v, forKey: k)}
+	class func merge<KeyType,ValueType> (inout left:Dictionary<KeyType,ValueType>, _ right: Dictionary<KeyType,ValueType>)-> Dictionary<KeyType,ValueType>{
+	    for (k,v) in right {left.updateValue(v,forKey:k)}
 	    return left
 	}
 	/**
@@ -18,10 +17,10 @@ class DictionaryModifier{
 	 * var c:Object = ObjectModifier.merge(a, b);
 	 * ObjectDescriber.describe2(c)//Output:    [number] index => 2, [string] color => red, [string] name => john
 	 */
-	class func combine<T>(a:Dictionary<String,T>, _ b:Dictionary<String,T>)->Dictionary<String,T>{//// :TODO: rename to combine since merge implies that the passed obj is also changed
-		var c:Dictionary<String,T> = [:]//use generics in the future: T and <T>
+	class func combine<K,V>(a:Dictionary<K,V>, _ b:Dictionary<K,V>)->Dictionary<K,V>{//// :TODO: rename to combine since merge implies that the passed obj is also changed
+		var c:Dictionary<K,V> = [:]//use generics in the future: T and <T>
         for keyA in a.keys{c[keyA] = a[keyA]}//copies the items in a to the return object
-        for keyB in b.keys {c[keyB] = b[keyB]}//assigns the items in b to the return object, if they already exist s they are overriden
+        for keyB in b.keys{c[keyB] = b[keyB]}//assigns the items in b to the return object, if they already exist s they are overriden
 		return c
 	}
 }
