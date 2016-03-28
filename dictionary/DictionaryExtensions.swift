1fbc1bc6a94e6b278e803277c836c149c3ed1d8c
Eonist <unicornhive@gmail.com>
2016-03-28 12:47:27 +0200

    Files modified: 1
    Modified 1 file:dictionary/DictionaryExtensions.swift

--- dictionary/DictionaryExtensions.swift
+++ dictionary/DictionaryExtensions.swift
@@ -1,10 +1,11 @@
 import Foundation
-
-extension Dictionary where Key: StringLiteralConvertible, Value: AnyObject{
-    func combine<K,V>(b:Dictionary<K,V>)->Dictionary<K,V>{//// :TODO: rename to combine since merge implies that the passed obj is also changed
+/**
+ * TODO: Try to make this extension Generic, currently its only working with String key value pairs
+ */
+extension Dictionary where Key: String, Value: String{
+    func combine<K,V>(b:Dictionary<K,V>)->Dictionary<K,V>{
         return DictionaryModifier.combine(self, b)
     }
-    
-    
+    //TODO: add merge aswell
 }
 
