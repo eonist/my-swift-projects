b5bdd202bef3e2e0ca0c6503c31ce523065904d5
Eonist <unicornhive@gmail.com>
2016-04-03 12:24:42 +0200

    Files modified: 2
    Modified 2 files:geom/color/types/HSB.swiftgeom/color/types/HSBColor.swift

--- geom/color/types/HSB.swift
+++ geom/color/types/HSB.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class HSB {
-
+    private var h : UInt;/*0-360*/
+    private var s : UInt;/*0-1*/
+    private var b : UInt;/*0-1*/
+    init(h:UInt = 0,_ s:UInt = 0,_ b:UInt = 0) {
+        self.h = h;
+        self.s = s;
+        self.b = b;
+    }
 }
--- geom/color/types/HSBColor.swift
+++ geom/color/types/HSBColor.swift
@@ -1,5 +1,36 @@
 import Foundation
 
 class HSBColor {
-
+    /**
+     * The hue value for the HSB color. This represents an angle, in
+     * degrees, around the HSB cone. The supplied value will be modulated
+     * by 360 so that the stored value of hue will be in the range [0,360).
+     */
+    private var hueValue:CGFloat
+    /**
+     * The saturation parameter for this HSB color. This is a value between
+     * 0 (black) and 1 (full saturation), which represents the distance
+     * from the center in the HSB cone.
+     */
+    private var saturation:CGFloat
+    /**
+     * The brightness parameter for this HSB color. This is a value between
+     * 0 (black) and 1 (full brightness), which represents the distance
+     * from the apex of the HSB cone.
+     */
+    private var brightness:CGFloat
+    /**
+     *  Constructs an HSBColor with optional parameters.
+     *  @param hue The hue.
+     *  @param saturation The saturation.
+     *  @param brightness The brightness.
+     */
+    init(hue:CGFloat = NaN, _ saturation:CGFloat = NaN, _ brightness:CGFloat = NaN) {
+        self.hueValue = hue
+        self.saturation = saturation
+        self.brightness = brightness
+    }
+}
+extension HSBColor{
+    var hue:CGFloat{get{return self.hueValue}set{self.hueValue = newValue % 360}}
 }
