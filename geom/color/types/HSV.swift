d00077c066497553f2d1cc77a4c7b726a10b8ce6
Eonist <unicornhive@gmail.com>
2016-04-03 12:32:46 +0200

    Files modified: 2
    Modified 2 files:geom/color/types/HSV.swiftgeom/color/types/RGB.swift

--- geom/color/types/HSV.swift
+++ geom/color/types/HSV.swift
@@ -3,7 +3,7 @@ class HSV {
 	var h:UInt/*0-240*/
 	var s:UInt/*0-240*/
 	var v:UInt/*0-240*/
-	public function HSV(h:uint = 0, _ s:uint = 0, _ v:uint = 0) {
+	init(h:UInt = 0, _ s:UInt = 0, _ v:UInt = 0) {
 		self.h = h
 		self.s = s
 		self.v = v
