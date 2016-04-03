a72f9021ac5ddaf16f0b5209cee1815eb7f7da6b
Eonist <unicornhive@gmail.com>
2016-04-03 15:37:26 +0200

    Files modified: 2
    Modified 2 files:geom/color/types/HSV.swiftgeom/color/utils/ColorParser.swift

--- geom/color/types/HSV.swift
+++ geom/color/types/HSV.swift
@@ -1,9 +1,9 @@
 import Foundation
 class HSV {
-	var h:UInt/*0-240*/
-	var s:UInt/*0-240*/
-	var v:UInt/*0-240*/
-	init(_ h:UInt = 0, _ s:UInt = 0, _ v:UInt = 0) {
+	var h:CGFloat/*0-240*/
+	var s:CGFloat/*0-240*/
+	var v:CGFloat/*0-240*/
+	init(_ h:CGFloat = 0, _ s:CGFloat = 0, _ v:CGFloat = 0) {
 		self.h = h
 		self.s = s
 		self.v = v
--- geom/color/utils/ColorParser.swift
+++ geom/color/utils/ColorParser.swift
@@ -69,8 +69,8 @@ class ColorParser {/*Covers returning hex colors etc*/
      * @return h: 0-240, s: 0-1, v 0-1
      */
     class func hsv(r:CGFloat, _ g:CGFloat, _ b:CGFloat)->HSV{
-        var max:CGFloat = Swift.max(r, g, b)
-        var min:CGFloat = Swift.min(r, g, b)
+        let max:CGFloat = Swift.max(r, g, b)
+        let min:CGFloat = Swift.min(r, g, b)
         var hue:CGFloat = 0
         var saturation:CGFloat = 0
         var value:CGFloat = 0
@@ -79,7 +79,7 @@ class ColorParser {/*Covers returning hex colors etc*/
         else if(max == g){hue = (60 * (b-r) / (max-min) + 120)}
         else if(max == b){hue = (60 * (r-g) / (max-min) + 240)}
         value = max/*get Value*/
-        saturation = max == 0 ? 0 : saturation = (max - min) / max /*get Saturation*/
+        saturation = max == 0 ? 0 : (max - min) / max /*get Saturation*/
         return HSV(hue, saturation, (value / 255))
     }
     /**
