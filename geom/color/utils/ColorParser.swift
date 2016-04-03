dc782c51e5d57f54d4c20d839027547fee3be382
Eonist <unicornhive@gmail.com>
2016-04-03 12:47:07 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/ColorParser.swift

--- geom/color/utils/ColorParser.swift
+++ geom/color/utils/ColorParser.swift
@@ -37,19 +37,28 @@ class ColorParser {/*Covers returning hex colors etc*/
      *  @param rgb The RGB color.
      *  @return The HSBColor object representing the RGB color. Hue:0-360, Saturate:0-1, brightness:0-1
      */
+    
+     /*
+     
+     let r:UInt = rgb >> 16;
+     //Swift.print("r: " + "\(r)")
+     let g:UInt = (rgb ^ (r << 16)) >> 8;
+     //Swift.print("g: " + "\(g)")
+     let b:UInt = (rgb ^ (r << 16)) ^ (g << 8);
+     
+     
+     */
     class func hsbByRgb(rgb:UInt)->HSBColor {
-        
-        let someColor:NSColor = NSColor
-        
+        let someColor:NSColor = NSColor.redColor()
         var hue:CGFloat, saturation:CGFloat, brightness:CGFloat;
-        var r:CGFloat = ((rgb >> 16) & 0xff) / 255;
-        var g:CGFloat = ((rgb >> 8) & 0xff) / 255;
-        var b:CGFloat = (rgb & 0xff) / 255;
-        var max:CGFloat = Math.max(r, Math.max(g, b));
-        var min:CGFloat = Math.min(r, Math.min(g, b));
-        var delta:CGFloat = max - min;
-        brightness = max;
-        saturation = max != 0 ? delta / max : 0;
+        var r:UInt = ((rgb >> 16) & 0xff) / 255
+        var g:UInt = ((rgb >> 8) & 0xff) / 255
+        var b:UInt = (rgb & 0xff) / 255
+        var max:UInt = Swift.max(r, Swift.max(g, b))
+        var min:UInt = Swift.min(r, Swift.min(g, b))
+        var delta:UInt = max - min
+        brightness = max
+        saturation = max != 0 ? delta / max : 0
         if (saturation == 0) hue = 0; /*this was set to NaN, but 0 seemed more suitable*/
         else {
     				if (r == max) hue = (g - b) / delta;
