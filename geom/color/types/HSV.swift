abae1710e4863ba92de87b01b29adf2b8405bdc9
Eonist <unicornhive@gmail.com>
2016-04-03 12:28:41 +0200

    Files modified: 2
    Modified 2 files:geom/color/types/HSV.swiftgeom/color/types/RGB.swift

--- geom/color/types/HSV.swift
+++ geom/color/types/HSV.swift
@@ -1,5 +1,11 @@
 import Foundation
-
 class HSV {
-
+	var h:UInt/*0-240*/
+	var s:UInt/*0-240*/
+	var v:UInt/*0-240*/
+	public function HSV(h:uint = 0, _ s:uint = 0, _ v:uint = 0) {
+		self.h = h
+		self.s = s
+		self.v = v
+	}
 }
--- geom/color/types/RGB.swift
+++ geom/color/types/RGB.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class RGB {
-
+	var r:UInt/*0-255*/
+	var g:UInt/*0-255*/
+	var b:UInt/*0-255*/
+	public function RGB(r:UInt = 0,_ g:UInt = 0,_ b:UInt = 0) {
+		self.r = r
+		self.g = g
+		self.b = b
+	}
 }
