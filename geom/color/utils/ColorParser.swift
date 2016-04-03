7cd6baccdcc707db57535fb7a79c7db4db43cc7c
Eonist <unicornhive@gmail.com>
2016-04-03 15:09:24 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/ColorParser.swift

--- geom/color/utils/ColorParser.swift
+++ geom/color/utils/ColorParser.swift
@@ -71,6 +71,35 @@ class ColorParser {/*Covers returning hex colors etc*/
         var rgbObj:[String:CGFloat] = ColorParser.rgbByHex(rgb)
         return ColorParser.hlsByRgb(rgbObj["rb"],rgbObj["gb"],rgbObj["bb"])
     }
+    /**
+    * This was created with references to a few anonymous calculations written in other languages, None of which were very accurate
+    * @Note // You can compare these values to those produced in the Windows Color Picker (MS Paint, etc)
+    * @Example
+    * var r:uint = 145;
+    * var g:uint = 100;
+    * var b:uint = 120;
+    * var hls:Object = hlsByRgb(r,g,b);
+    * trace(hls.h);   // outputs a scale of 0-240
+    * trace(hls.l);   // outputs a scale of 0-240
+    * trace(hls.s);   // outputs a scale of 0-240
+    */
+    class func hlsByRgb(r:CGFloat,g:CGFloat,b:CGFloat)->HLS {
+        var h:CGFloat
+        var l:CGFloat
+        var s:CGFloat
+        var max:CGFloat = (Swift.max(Swift.max(r, g), b))/255
+        var min:CGFloat = (Swift.min(Swift.min(r, g), b))/255
+        var delta:CGFloat = max-min
+        l = (max+min)/2
+        s = (max == min) ? 0 : ((l <= 0.5) ? delta/l/2 : delta/(2-l*2))
+        if(r/255 == max) {h = (g-b)/delta/255}
+        else if(g/255 == max) {h = 2+(b-r)/delta/255}
+        else if(b/255 == max) {h = 4+(r-g)/delta/255}
+        h *= 40
+        if(h < 0) h += 240
+        h = Swift.round(h)
+        return HLS(((h.isNaN) ? 0 : h), Swift.round(l*240), Swift.round(s*240))
+    }
 }
 
 extension ColorParser{
