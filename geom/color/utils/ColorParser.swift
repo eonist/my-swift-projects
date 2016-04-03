8bb585c000455f76250614dd43652b399da780a8
Eonist <unicornhive@gmail.com>
2016-04-03 11:56:56 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/ColorParser.swift

--- geom/color/utils/ColorParser.swift
+++ geom/color/utils/ColorParser.swift
@@ -1,5 +1,8 @@
 import Cocoa
-
+/**
+ * HSB, HSL and HSV , hex, cmyk, rgb
+ * // :TODO: create new Classes called RGB,HLS,HSV,HSB, internal at first and maybe public later? its benefitial, when you want to use RGB as an argument etc nice!, use ,g,b etc as variables, not red,green,blue etc since these values will be in the calculations and they are easier to read if they are shorthand
+ */
 class ColorParser {/*Covers returning hex colors etc*/
     /**
      * Converts an RGB color value into a hexidecimal String representation.
@@ -13,13 +16,13 @@ class ColorParser {/*Covers returning hex colors etc*/
      * TODO: maybe make another method that takes int from 0-255?
      */
     class func hexColor(r:CGFloat,_ g:CGFloat,_ b:CGFloat)->String{
-        var rr:String = String(format:"%X", Int(r * 255));
-        var gg:String = String(format:"%X", Int(g * 255));
-        var bb:String = String(format:"%X", Int(b * 255));
-        rr = (rr.count == 1) ? "0" + rr : rr;
-        gg = (gg.count == 1) ? "0" + gg : gg;
-        bb = (bb.count == 1) ? "0" + bb : bb;
-        return (rr + gg + bb)/*.toUpperCase()*/;
+        var rr:String = String(format:"%X", Int(r * 255))
+        var gg:String = String(format:"%X", Int(g * 255))
+        var bb:String = String(format:"%X", Int(b * 255))
+        rr = (rr.count == 1) ? "0" + rr : rr
+        gg = (gg.count == 1) ? "0" + gg : gg
+        bb = (bb.count == 1) ? "0" + bb : bb
+        return (rr + gg + bb)/*.toUpperCase()*/
     }
     /**
      * EXAMPLE: rgba(NSColor.redColor()).r//Outputs //1.0
