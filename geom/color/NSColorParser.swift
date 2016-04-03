3a3235dd20b7eeaba6d60186b645da84c680a9c7
Eonist <unicornhive@gmail.com>
2016-04-03 12:45:06 +0200

    Files modified: 1
    Modified 1 file:geom/color/NSColorParser.swift

--- geom/color/NSColorParser.swift
+++ geom/color/NSColorParser.swift
@@ -5,10 +5,10 @@ class NSColorParser {
         nsColor(250.0, 0, 0,100)//output: red NSColor
     }
     /**
-    * r: from 0.0 to 1.0
-    * Note: research: CGColorCreateGenericGray(gray: CGFloat, _ alpha: CGFloat) -> CGColor
-    * Note: research: CGColorCreateGenericCMYK(cyan: CGFloat, _ magenta: CGFloat, _ yellow: CGFloat, _ black: CGFloat, _ alpha: CGFloat) -> CGCol
-    */
+     * r: from 0.0 to 1.0
+     * Note: research: CGColorCreateGenericGray(gray: CGFloat, _ alpha: CGFloat) -> CGColor
+     * Note: research: CGColorCreateGenericCMYK(cyan: CGFloat, _ magenta: CGFloat, _ yellow: CGFloat, _ black: CGFloat, _ alpha: CGFloat) -> CGCol
+     */
     class func cgColor(r:CGFloat = 0.0, _ g:CGFloat = 0.0, _ b:CGFloat = 0.0, _ a:CGFloat = 1.0)->CGColor{
         return CGColorCreateGenericRGB(r,g,b,a)
     }
