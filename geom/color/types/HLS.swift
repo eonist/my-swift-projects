e05fc03e98a766c5209e147ad50d9238159f2b80
Eonist <unicornhive@gmail.com>
2016-04-03 12:26:42 +0200

    Files modified: 1
    Modified 1 file:geom/color/types/HLS.swift

--- geom/color/types/HLS.swift
+++ geom/color/types/HLS.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class HLS {
-
+    var h:UInt/*0-240*/
+    var l:UInt/*0-240*/
+    var s:UInt/*0-240*/
+    init(h:uint = 0,_ l:uint = 0,_ s:uint = 0) {
+        self.h = h
+        self.l = l
+        self.s = s
+    }
 }
