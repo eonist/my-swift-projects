4f8d9bbc990ff67e57dae81eca44111cd9e1febd
Eonist <unicornhive@gmail.com>
2016-04-04 11:50:40 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HexModifier.swift

--- geom/color/utils/HexModifier.swift
+++ geom/color/utils/HexModifier.swift
@@ -15,4 +15,19 @@ class HexModifier {
         c = (((first & 0xff0000) >> 16) * (1 - ratio) + ((second & 0xff0000) >> 16) * ratio);
         return UInt((a) | (b << 8) | (c << 16))
     }
+    /**
+     * Brighten (or darken) a color by a scalar amount.
+     * @param	color	The color to brighten/darken.
+     * @param	modifier	The scalar to modify the brightness, 0 = no change, ie. 0.9, 1.1
+     * @return	The modified color
+     */
+    class func brighten(color:UInt, modifier:UInt) -> UInt {
+        // :TODO: rename?
+        var z:UInt = 0xff * modifier;
+        var a:UInt; var b:UInt; var c:UInt;
+        a = (a = (color & 0xff) + z) > 0xFF ? 0xFF : ( a < 0 ? 0 : a)
+        b = (b = ((color & 0xff00) >> 8) + z) > 0xFF ? 0xFF : ( b < 0 ? 0 : b)
+        c = (c = ((color & 0xff0000) >> 16) + z) > 0xFF ? 0xFF : ( c < 0 ? 0 : c)
+        return ((a) | (b << 8) | (c << 16))
+    }
 }
