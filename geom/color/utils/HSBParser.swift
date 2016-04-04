6a6752b7e1ccca38107b4a3531f015788c1fd0b8
Eonist <unicornhive@gmail.com>
2016-04-04 16:55:34 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HSBParser.swift

--- geom/color/utils/HSBParser.swift
+++ geom/color/utils/HSBParser.swift
@@ -2,19 +2,17 @@ import Cocoa
 
 class HSBParser {
     /**
-     *  Converts a color from RGB format into an HSBColor.
-     *  @param rgb The RGB color.
-     *  @return The HSBColor object representing the RGB color. Hue:0-360, Saturate:0-1, brightness:0-1
+     * Converts a color from RGB format into an HSBColor.
+     * @param rgb The RGB color.
+     * @return The HSBColor object representing the RGB color. Hue:0-360, Saturate:0-1, brightness:0-1
      */
-    class func hsb(color:NSColor)->HSBColor {
-        //let someColor:NSColor = NSColor.redColor()
-        let rgb:UInt = HexParser.rgb(color)
+    class func hsb(rgb:RGB)->HSBColor {
         var hue:UInt = 0//<--the zero was recently added to get the code to compile. Shouldnt be there
         var saturation:UInt
         var brightness:UInt
-        let r:UInt = ((rgb >> 16) & 0xff) / 255
-        let g:UInt = ((rgb >> 8) & 0xff) / 255
-        let b:UInt = (rgb & 0xff) / 255
+        let r:UInt = rgb.r / 255
+        let g:UInt = rgb.g / 255
+        let b:UInt = rgb.b / 255
         let max:UInt = Swift.max(r, Swift.max(g, b))
         let min:UInt = Swift.min(r, Swift.min(g, b))
         let delta:UInt = max - min
