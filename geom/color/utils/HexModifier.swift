1a463737baab98ab1b0df3d702192b85e0c7aaf3
Eonist <unicornhive@gmail.com>
2016-04-04 11:46:33 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HexModifier.swift

--- geom/color/utils/HexModifier.swift
+++ geom/color/utils/HexModifier.swift
@@ -8,12 +8,11 @@ class HexModifier {
      * @param	ratio	The ratio of the second color to blend, .5 = equal ratio, .9 = 1:9, .2 = 8:2
      * @return	The color value as a uint.
      */
-    class func blend(first:uint, second:uint, ratio:CGFloat) -> uint {
-        // :TODO: rename?
-        var a:CGFloat; var b:CGFloat; var c:CGFloat;
+    class func blend(first:UInt, second:UInt, ratio:UInt) -> UInt {// :TODO: rename?
+        var a:UInt; var b:UInt; var c:UInt;
         a = ((first & 0xff) * (1 - ratio) + (second & 0xff) * ratio);
         b = (((first & 0xff00) >> 8) * (1 - ratio) + ((second & 0xff00) >> 8) * ratio);
         c = (((first & 0xff0000) >> 16) * (1 - ratio) + ((second & 0xff0000) >> 16) * ratio);
-        return uint((a) | (b << 8) | (c << 16));
+        return UInt((a) | (b << 8) | (c << 16))
     }
 }
