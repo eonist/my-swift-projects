34659c62e0c7489d2dca926d106f0ec4662403c9
Eonist <unicornhive@gmail.com>
2016-04-04 10:41:15 +0200

    Files modified: 1
    Modified 1 file:geom/color/types/CMYK.swift

--- geom/color/types/CMYK.swift
+++ geom/color/types/CMYK.swift
@@ -1,5 +1,13 @@
 import Foundation
-
 class CMYK {
-
+    var c:CGFloat
+    var m:CGFloat
+    var y:CGFloat
+    var k:CGFloat
+    init(_ c:CGFloat = 0, _ m:CGFloat = 0, _ y:CGFloat = 0, _ k:CGFloat = 0) {
+        self.c = c
+        self.m = m
+        self.y = y
+        self.k = k
+    }
 }
