0040b285d13a20b29fce81b3d9898ca9d42930fe
Eonist <unicornhive@gmail.com>
2016-04-04 13:29:04 +0200

    Files modified: 1
    Modified 1 file:geom/color/types/RGBA.swift

--- geom/color/types/RGBA.swift
+++ geom/color/types/RGBA.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 class RGBA {
-
+    var r:UInt/*0-255*/
+    var g:UInt/*0-255*/
+    var b:UInt/*0-255*/
+    var a:UInt
+    init(_ r:UInt = 0,_ g:UInt = 0,_ b:UInt = 0,_ a:UInt) {
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+    }
+}
+extension RGBA{
+    convenience init(_ r:CGFloat = 0,_ g:CGFloat = 0,_ b:CGFloat = 0, _ a:CGFloat = 0){
+        self.init(r.uint,g.uint,b.uint,a.uint)
+    }
 }
