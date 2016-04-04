b2a3f159e80df72557f0638910d5498f6a01add0
Eonist <unicornhive@gmail.com>
2016-04-04 11:21:07 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HEXParser.swift

--- geom/color/utils/HEXParser.swift
+++ geom/color/utils/HEXParser.swift
@@ -1,5 +1,24 @@
-import Foundation
+import Cocoa
 
 class HEXParser {
-    //continue here: add the hex parser methods
+    /**
+     * EXAMPLE: hexNumber(FF0000)//Output:16711680
+     */
+    class func hexNumber(hex:String) -> UInt{
+        return UInt(Float(hex)!)
+    }
+    /**
+     * EXAMPLE: hexString(16711680)//Output: FF0000
+     */
+    class func hexString(hex:UInt) -> String{
+        return NSString(format: "%2X", hex) as String
+    }
+    
+    /**
+     * Convenience method (since extensions can only return one type of each var)
+     * EXAMPLE: rgb(NSColor.redColor())//16711680
+     */
+    class func rgb(color:NSColor) -> UInt{
+        return UInt(CGFloat(color.rgb!))
+    }
 }
