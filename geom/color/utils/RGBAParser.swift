29ddef6b2b4cca3a24de33f02ec38d26d65d40d4
Eonist <unicornhive@gmail.com>
2016-04-04 16:02:06 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/RGBAParser.swift

--- geom/color/utils/RGBAParser.swift
+++ geom/color/utils/RGBAParser.swift
@@ -1,5 +1,11 @@
-import Foundation
+import Cocoa
 
 class RGBAParser {
-
+    /**
+     * EXAMPLE: rgba(NSColor.redColor()).r//Outputs //1.0
+     */
+    class func color(nsColor:NSColor)->RGBA{//<--was: (r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat)
+        let ciColor:CIColor = CIColor(color: nsColor)!
+        return RGBA(ciColor.red,ciColor.green,ciColor.blue,ciColor.alpha)
+    }
 }
