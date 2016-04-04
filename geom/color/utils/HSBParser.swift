b82f40dee919b5e283b6badf60958572bd6bb62f
Eonist <unicornhive@gmail.com>
2016-04-04 16:57:35 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HSBParser.swift

--- geom/color/utils/HSBParser.swift
+++ geom/color/utils/HSBParser.swift
@@ -7,12 +7,10 @@ class HSBParser {
      * @return The HSBColor object representing the RGB color. Hue:0-360, Saturate:0-1, brightness:0-1
      */
     class func hsb(rgb:RGB)->HSBColor {
+        let r:UInt = rgb.r / 255;let g:UInt = rgb.g / 255;let b:UInt = rgb.b / 255;
         var hue:UInt = 0//<--the zero was recently added to get the code to compile. Shouldnt be there
         var saturation:UInt
         var brightness:UInt
-        let r:UInt = rgb.r / 255
-        let g:UInt = rgb.g / 255
-        let b:UInt = rgb.b / 255
         let max:UInt = Swift.max(r, Swift.max(g, b))
         let min:UInt = Swift.min(r, Swift.min(g, b))
         let delta:UInt = max - min
