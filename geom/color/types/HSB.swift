e8a64b49f3f92eb215316db707d705ed7b4bedc3
Eonist <unicornhive@gmail.com>
2016-04-04 17:20:16 +0200

    Files modified: 2
    Modified 2 files:geom/color/types/HSB.swiftgeom/color/types/HSBColor.swift

--- geom/color/types/HSB.swift
+++ geom/color/types/HSB.swift
@@ -1,9 +1,29 @@
 import Foundation
-
 class HSB {
+    /**
+     * The hue value for the HSB color. This represents an angle, in
+     * degrees, around the HSB cone. The supplied value will be modulated
+     * by 360 so that the stored value of hue will be in the range [0,360).
+     */
     var h : UInt;/*0-360*/
+    /**
+     * The saturation parameter for this HSB color. This is a value between
+     * 0 (black) and 1 (full saturation), which represents the distance
+     * from the center in the HSB cone.
+     */
     var s : UInt;/*0-1*/
+    /**
+     * The brightness parameter for this HSB color. This is a value between
+     * 0 (black) and 1 (full brightness), which represents the distance
+     * from the apex of the HSB cone.
+     */
     var b : UInt;/*0-1*/
+    /**
+     *  Constructs an HSBColor with optional parameters.
+     *  @param hue The hue.
+     *  @param saturation The saturation.
+     *  @param brightness The brightness.
+     */
     init(_ h:UInt = 0,_ s:UInt = 0,_ b:UInt = 0) {
         self.h = h;
         self.s = s;
@@ -11,5 +31,4 @@ class HSB {
     }
 }
 extension HSB{
-    
 }
--- geom/color/types/HSBColor.swift
+++ geom/color/types/HSBColor.swift
@@ -1,31 +1,11 @@
 import Foundation
 
 class HSBColor {
-    /**
-     * The hue value for the HSB color. This represents an angle, in
-     * degrees, around the HSB cone. The supplied value will be modulated
-     * by 360 so that the stored value of hue will be in the range [0,360).
-     */
+   
     private var hueValue:CGFloat
-    /**
-     * The saturation parameter for this HSB color. This is a value between
-     * 0 (black) and 1 (full saturation), which represents the distance
-     * from the center in the HSB cone.
-     */
-    var saturation:CGFloat
-    /**
-     * The brightness parameter for this HSB color. This is a value between
-     * 0 (black) and 1 (full brightness), which represents the distance
-     * from the apex of the HSB cone.
-     */
-    var brightness:CGFloat
-    /**
-     *  Constructs an HSBColor with optional parameters.
-     *  @param hue The hue.
-     *  @param saturation The saturation.
-     *  @param brightness The brightness.
-     */
-    init(_ hue:CGFloat = NaN, _ saturation:CGFloat = NaN, _ brightness:CGFloat = NaN) {
+        var saturation:CGFloat
+        var brightness:CGFloat
+        init(_ hue:CGFloat = NaN, _ saturation:CGFloat = NaN, _ brightness:CGFloat = NaN) {
         self.hueValue = hue
         self.saturation = saturation
         self.brightness = brightness
