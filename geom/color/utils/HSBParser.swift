956416be65ebcf32cd3e118ae88f0d9e2d5ab32f
Eonist <unicornhive@gmail.com>
2016-04-05 15:30:32 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/HSBParser.swift

--- geom/color/utils/HSBParser.swift
+++ geom/color/utils/HSBParser.swift
@@ -7,32 +7,27 @@ class HSBParser {
      * @return The HSBColor object representing the RGB color. Hue:0-360, Saturate:0-1, brightness:0-1
      */
     class func hsb(rgb:RGB)->HSB{
-        let r:CGFloat = rgb.r.cgFloat / 255;let g:CGFloat = rgb.g.cgFloat / 255;let b:CGFloat = rgb.b.cgFloat / 255;
-        
-        var hue:CGFloat = 0//<--the zero was recently added to get the code to compile. Shouldnt be there
-        var saturation:CGFloat
-        var brightness:CGFloat
-        let max:CGFloat = Swift.max(r, Swift.max(g, b))
-        let min:CGFloat = Swift.min(r, Swift.min(g, b))
-        let delta:CGFloat = max - min
-        
+        let r:UInt = rgb.r / 255;let g:UInt = rgb.g / 255;let b:UInt = rgb.b / 255;
+        var hue:UInt = 0//<--the zero was recently added to get the code to compile. Shouldnt be there
+        var saturation:UInt
+        var brightness:UInt
+        let max:UInt = Swift.max(r, Swift.max(g, b))
+        let min:UInt = Swift.min(r, Swift.min(g, b))
+        let delta:UInt = max - min
         brightness = max
         saturation = max != 0 ? delta / max : 0
         if (saturation == 0) {hue = 0} /*this was set to NaN, but 0 seemed more suitable*/
-        else {
+        else if(g == max){
+            hue = 2 + (b - r) / delta
+        }else if(b == max){
+            hue = 4 + (r - g) / delta
+            hue = hue * 60
+            if(hue < 0) {hue += 360}
+        }else {
             if(r == max){
-                Swift.print("g: " + "\(g)")
-                Swift.print("b: " + "\(b)")
-                Swift.print("delta: " + "\(delta)")
                 hue = (g - b) / delta
-            }else if(g == max){
-                hue = 2 + (b - r) / delta
-            }else if(b == max){
-                hue = 4 + (r - g) / delta
-                hue = hue * 60
-                if(hue < 0) {hue += 360}
             }
         }
-        return HSB(hue.uint, saturation.uint, brightness.uint)
+        return HSB(hue, saturation, brightness)
     }
 }
