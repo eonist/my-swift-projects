dc2e9e78de4a2ce8ee51005191260bcbdd44ee8b
Eonist <unicornhive@gmail.com>
2016-04-09 09:03:30 +0200

    Files modified: 1
    Modified 1 file:file/FileWatcher.swift

--- file/FileWatcher.swift
+++ file/FileWatcher.swift
@@ -79,7 +79,7 @@ public class FileWatcher {
         if(hasStarted){return}/*<--only start if its not already started*/
         var context = FSEventStreamContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
         context.info = UnsafeMutablePointer<Void>(unsafeAddressOf(self))
-        let flags = UInt32(kFSEventStreamCreateFlagFileEvents)
+        let flags = UInt32(kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagFileEvents)
         streamRef = FSEventStreamCreate(kCFAllocatorDefault, eventCallback, &context, filePaths, lastEventId, 0, flags)//Creates an FSEventStream.
         FSEventStreamScheduleWithRunLoop(streamRef!, CFRunLoopGetMain(), kCFRunLoopDefaultMode)// Schedules an FSEventStream on a runloop, like CFRunLoopAddSource() does for a CFRunLoopSourceRef.
         FSEventStreamStart(streamRef!)
