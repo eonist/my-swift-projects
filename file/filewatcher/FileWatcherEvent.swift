ffe51f2de8f76de59e8f3eba28603cbd2f4fcb27
Eonist <unicornhive@gmail.com>
2016-04-10 12:20:26 +0200

    Files modified: 1
    Modified 1 file:file/filewatcher/FileWatcherEvent.swift

--- file/filewatcher/FileWatcherEvent.swift
+++ file/filewatcher/FileWatcherEvent.swift
@@ -2,10 +2,16 @@ import Foundation
 
 class FileWatcherEvent:Event{
     static var change:String = "fileWatcherEventChange"
+    var eventId: FSEventStreamEventId
+    var eventPath:String
+    var eventFlags: FSEventStreamEventFlags
     init(_ type:String = "", _ origin:AnyObject, _ eventId: FSEventStreamEventId, _ eventPath: String, _ eventFlags: FSEventStreamEventFlags){
+        self.eventId = eventId
+        self.eventPath = eventPath
+        self.eventFlags = eventFlags
         super.init(type,origin)
     }
 }
 extension FileWatcherEvent{
-    
+    var fileChange:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsFile)) != 0}
 }
