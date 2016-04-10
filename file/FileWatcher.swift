258dacf3c49808528b709b9b6b099ac8ba182ffb
Eonist <unicornhive@gmail.com>
2016-04-10 07:25:36 +0200

    Files modified: 1
    Modified 1 file:file/FileWatcher.swift

--- file/FileWatcher.swift
+++ file/FileWatcher.swift
@@ -21,6 +21,9 @@ class FileWatcher/*:NSView*//*:EventSender*/{
      * NOTE: Starts receiving events and servicing them from the client's runloop(s) using the callback supplied by the client when the stream was created. If a value was supplied for the sinceWhen parameter then "historical" events will be sent via your callback first, then a HistoryDone event, then "contemporary" events will be sent on an ongoing basis (as though you had supplied kFSEventStreamEventIdSinceNow for sinceWhen).
      * NOTE: FSEvents now supports file-level granularity, use kFSEventStreamCreateFlagFileEvents flag when creating events stream to get informed about changes to particular files.
      */
+    
+    public let queue: dispatch_queue_t?
+    
     func start() {
         Swift.print("start - has started: " + "\(hasStarted)")
         if(hasStarted){return}/*<--only start if its not already started*/
@@ -32,7 +35,9 @@ class FileWatcher/*:NSView*//*:EventSender*/{
         FSEventStreamScheduleWithRunLoop(streamRef!, NSRunLoop.currentRunLoop().getCFRunLoop()/*CFRunLoopGetMain()*/, kCFRunLoopDefaultMode)// Schedules an FSEventStream on a runloop, like CFRunLoopAddSource() does for a CFRunLoopSourceRef., you could also use a different runloop here: NSRunLoop.currentRunLoop().getCFRunLoop() for instance
         FSEventStreamStart(streamRef!)
         
-        streamRef?.setDispatchQueue(nil)
+
+        
+        FSEventStreamSetDispatchQueue(streamRef, <#T##q: dispatch_queue_t?##dispatch_queue_t?#>)
         
         hasStarted = true
         
