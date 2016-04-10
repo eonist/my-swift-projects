8a105d97815ec385ee566e6b1301a4bf657ca44e
Eonist <unicornhive@gmail.com>
2016-04-10 12:30:39 +0200

    Files modified: 1
    Modified 1 file:file/filewatcher/FileWatcherEvent.swift

--- file/filewatcher/FileWatcherEvent.swift
+++ file/filewatcher/FileWatcherEvent.swift
@@ -17,9 +17,32 @@ class FileWatcherEvent:Event{
  * NOTE: Be aware that .DS_STORE changes frequently when other files change
  */
 extension FileWatcherEvent{
+    /*File*/
     var fileChange:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsFile)) != 0}
     var fileCreated:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCreated)) != 0}
     var fileRemoved:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRemoved)) != 0}
     var fileRenamed:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRenamed)) != 0}
     var fileModified:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0}
+    /*Directory*/
+    var dirChange:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsDir)) != 0}
+    
 }
+
+
+/*
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsDir)) != 0 {/*directory change*/
+Swift.print("directory change")
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCreated)) != 0 {/*directory created*/
+Swift.print("directory created: \(eventPath)")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRemoved)) != 0 {/*directory removed*/
+Swift.print("directory removed: \(eventPath)")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRenamed)) != 0 {/*directory renamed*/
+Swift.print("directory renamed: \(eventPath)")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0 {/*directory modified*/
+Swift.print("directory modified: \(eventPath)")
+}
+
+}*/
