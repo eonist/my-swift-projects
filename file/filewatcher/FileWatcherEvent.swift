5ed984b121196888e4287a27e2ce1a0ca384aeaf
Eonist <unicornhive@gmail.com>
2016-04-10 12:22:25 +0200

    Files modified: 1
    Modified 1 file:file/filewatcher/FileWatcherEvent.swift

--- file/filewatcher/FileWatcherEvent.swift
+++ file/filewatcher/FileWatcherEvent.swift
@@ -14,4 +14,24 @@ class FileWatcherEvent:Event{
 }
 extension FileWatcherEvent{
     var fileChange:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsFile)) != 0}
+    var fileCreated:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCreated)) != 0}
+    var fileRemoved:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRemoved)) != 0}
+    var fileRenamed:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRenamed)) != 0}
+    var fileModified:Bool {return (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0}
 }
+/*
+
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCreated)) != 0 {/*file created*/
+    Swift.print("file created")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRemoved)) != 0 {/*file removed*/
+    Swift.print("File removed: \(eventPath) - \(eventId)")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRenamed)) != 0 {/*file renamed*/
+    Swift.print("file renamed")
+}
+if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0 {/*file modified*/
+    Swift.print("File modified: \(eventPath) - \(eventId)")
+}
+
+*/
