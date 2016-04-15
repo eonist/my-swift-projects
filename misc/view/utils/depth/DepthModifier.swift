d14f71a27fc91ee3ef79f47d62bcfecd9f59a3d6
Eonist <unicornhive@gmail.com>
2016-04-15 20:12:16 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/depth/DepthModifier.swift

--- misc/view/utils/depth/DepthModifier.swift
+++ misc/view/utils/depth/DepthModifier.swift
@@ -12,13 +12,11 @@ class DepthModifier {
      * @Note indices contain an array of indec values that is in a spessific order, so if the first index is 6 that means the instance at depth 6 will now have depth 0
      */
     class func sortByIndices(container:NSView, _ indices:Array<Int>) {
-        //print("sortByIndices");
-        var indicesLen:Int = indices.count
+        Swift.print("DepthModifier.sortByIndices()");
         
         let children:Array<NSView> = NSViewParser.childrenOfType(container, NSView.self)
         
-        //continue here: Figure out how to use sortSubviewsUsingFunction
-        
-        for(var i:Int = 0;i < indicesLen;i++) {container.addSubviewAt(<#T##view: NSView##NSView#>, <#T##i: Int##Int#>)(children[indices[i]], i)}
+        let indicesLen:Int = indices.count
+        for(var i:Int = 0;i < indicesLen;i++) {container.addSubviewAt(children[indices[i]], i)}
     }
 }
