bb4b2f5f344f8885b95cf47accd5d33da50c9551
Eonist <unicornhive@gmail.com>
2016-04-15 19:18:27 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/depth/DepthModifier.swift

--- misc/view/utils/depth/DepthModifier.swift
+++ misc/view/utils/depth/DepthModifier.swift
@@ -7,4 +7,14 @@ class DepthModifier {
     class func sortByList(container:NSView, _ containerFieldName:String, _ sortListFieldName:String, _ sortList:[Dictionary<String,String>]){
         fatalError("not implemented yet")
     }
+    /**
+     * Arranges children in @param container according to @param indices
+     * @Note indices contain an array of indec values that is in a spessific order, so if the first index is 6 that means the instance at depth 6 will now have depth 0
+     */
+    class func sortByIndices(container:NSView, indices:Array<Int>) {
+        //			trace("sortByIndices");
+        var indicesLen:Int = indices.count
+        var children:Array = DisplayObjectParser.children(container);
+        for(var i:int = 0;i < indicesLen;i++) container.setChildIndex(children[indices[i]], i);
+    }
 }
