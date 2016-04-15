cb5ff91080ecc880c64a26f606065410ac32c580
Eonist <unicornhive@gmail.com>
2016-04-15 19:22:44 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/depth/DepthModifier.swift

--- misc/view/utils/depth/DepthModifier.swift
+++ misc/view/utils/depth/DepthModifier.swift
@@ -11,12 +11,12 @@ class DepthModifier {
      * Arranges children in @param container according to @param indices
      * @Note indices contain an array of indec values that is in a spessific order, so if the first index is 6 that means the instance at depth 6 will now have depth 0
      */
-    class func sortByIndices(container:NSView, indices:Array<Int>) {
-        //			trace("sortByIndices");
+    class func sortByIndices(container:NSView, _ indices:Array<Int>) {
+        //print("sortByIndices");
         var indicesLen:Int = indices.count
         
-        let children:Array<NSView> = NSViewParser.childrenOfType(list.lableContainer!, NSView.self)
+        let children:Array<NSView> = NSViewParser.childrenOfType(container, NSView.self)
         
-        for(var i:int = 0;i < indicesLen;i++) {container.setChildIndex(children[indices[i]], i)}
+        for(var i:Int = 0;i < indicesLen;i++) {container.setChildIndex(children[indices[i]], i)}
     }
 }
