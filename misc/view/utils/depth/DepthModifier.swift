6f5e62a806b4d6b312b594e1b91198b70f804c18
Eonist <unicornhive@gmail.com>
2016-04-15 21:04:12 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/depth/DepthModifier.swift

--- misc/view/utils/depth/DepthModifier.swift
+++ misc/view/utils/depth/DepthModifier.swift
@@ -17,7 +17,7 @@ class DepthModifier {
         let children:Array<NSView> = NSViewParser.childrenOfType(container, NSView.self)
         
         let indicesLen:Int = indices.count
-        for(var i:Int = 0;i < indicesLen;i++) {container.addSubviewAt(children[indices[i]], i)}/*<--apparently you don't need to remove a view before adding it again*///TODO: ideally we would use sortSubviewsUsingFunction. But there are no clear examples of anyone using this method in swift yet. It is possibe though, but will require some research
+        for(var i:Int = 0;i < indicesLen;i++) {container.addSubviewAt(children[indices[i]], i)}/*<--apparently you don't need to remove a view before adding it again, animation-wise it may be better to remove, did get some ghosting on the stroke*///TODO: ideally we would use sortSubviewsUsingFunction. But there are no clear examples of anyone using this method in swift yet. It is possibe though, but will require some research
     }
 }
 //forward
