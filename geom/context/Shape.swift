850541a6444744ec05dc95b66ce623fd595e33b1
Eonist <unicornhive@gmail.com>
2016-04-24 21:19:52 +0200

    Files modified: 1
    Modified 1 file:geom/context/Shape.swift

--- geom/context/Shape.swift
+++ geom/context/Shape.swift
@@ -12,22 +12,11 @@ class Shape : CALayer{
     lazy var graphics: Graphics = Graphics()//Delays the creation of graphics until it is needed, keep in mind that you cant create this instance before drawRect is called
     var path:CGMutablePath = CGPathCreateMutable()
     /**
-     * drawInContext is the method that gets called after a setNeedsDisplay() is called
-     */
-    /*
-    override func drawInContext(ctx: CGContext) {
-        Swift.print("Shape.drawInContext()")
-    }
-    */
-     
-    /**
      * NOTE: If you zoom this class in. you need to change the contentScale appropriatly. Zoom 2x on a retina screen: (2x2=4) Zoom 4x on a retina screen: (2x2=4) etc
      */
     override init(){
         super.init()
         
-        self.autoresizingMask = [.LayerNotSizable/*, .LayerHeightSizable*/]//CAAutoresizingMask
-            //NSAutoresizingMaskOptions.NSViewWidthSizable.union(NSAutoresizingMaskOptions.NSViewHeightSizable)
         self.needsDisplayOnBoundsChange = true
         self.contentsScale = 2.0/*<--Makes lines smooth on retina screens, you may want to consider toggeling this on non-retina displays. From apple docs: The default value of this property is 1.0. For layers attached to a view, the view changes the scale factor automatically to a value that is appropriate for the current screen. For layers you create and manage yourself, you must set the value of this property yourself based on the resolution of the screen and the content you are providing. Core Animation uses the value you specify as a cue to determine how to render your content.*/
     }
