52f45cbe213a004950b9ee935c1e97f65610295b
Eonist <unicornhive@gmail.com>
2016-04-24 21:22:02 +0200

    Files modified: 1
    Modified 1 file:geom/context/Shape.swift

--- geom/context/Shape.swift
+++ geom/context/Shape.swift
@@ -20,12 +20,6 @@ class Shape : CALayer{
         self.needsDisplayOnBoundsChange = true
         self.contentsScale = 2.0/*<--Makes lines smooth on retina screens, you may want to consider toggeling this on non-retina displays. From apple docs: The default value of this property is 1.0. For layers attached to a view, the view changes the scale factor automatically to a value that is appropriate for the current screen. For layers you create and manage yourself, you must set the value of this property yourself based on the resolution of the screen and the content you are providing. Core Animation uses the value you specify as a cue to determine how to render your content.*/
     }
-    /*
-    override func display() {
-        Swift.print("Shape.display()")
-        super.display()
-    }
-    */
     override func hitTest(p: CGPoint) -> CALayer? {
         return super.hitTest(p)
     }
