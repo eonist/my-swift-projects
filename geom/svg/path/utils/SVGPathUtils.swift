0d5fde228292ddd08b8162d1c9daa610a59ca1b9
Eonist <unicornhive@gmail.com>
2016-04-26 15:19:12 +0200

    Files modified: 1
    Modified 1 file:geom/svg/path/utils/SVGPathUtils.swift

--- geom/svg/path/utils/SVGPathUtils.swift
+++ geom/svg/path/utils/SVGPathUtils.swift
@@ -171,6 +171,13 @@ class SVGPathUtils {
         }
         return path;
     }
+    /**
+     * Returns an Rectangle instance with data derived from a svgRect
+     * @Note: if the svgRect x and or y is of the value NaN, then these are transfered as 0
+     */
+    class func rectangle(svgRect:SVGRect) -> CGRect {
+        return CGRect(!svgRect.x.isNaN ? svgRect.x : 0, !svgRect.y.isNaN ? svgRect.y : 0, svgRect.width, svgRect.height);
+    }
 }
 /**
 CGPathAddCurveToPoint
