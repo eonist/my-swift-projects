9f18c1e8250f451f097dc214f52a0557f9c89933
Eonist <unicornhive@gmail.com>
2016-04-27 09:03:15 +0200

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -161,6 +161,18 @@ class SVGUtils {
     /**
      *
      */
+    class func fillColor(style:SVGStyle)->NSColor{
+        //Swift.print("SVGGraphic.beginFill() color")
+        let colorVal:Double = !(style.fill as! Double).isNaN ? style.fill as! Double : Double(0x000000)
+        //Swift.print("colorVal: " + "\(colorVal)")
+        let opacity:CGFloat = style.fillOpacity != nil && !style.fillOpacity!.isNaN ? style.fillOpacity! : 1
+        let color:NSColor = NSColorParser.nsColor(UInt(colorVal), opacity)
+        //Swift.print("color: " + "\(color)")
+        return color
+    }
+    /**
+     *
+     */
     class func lineStyle(style:SVGStyle)->ILineStyle{
         var lineStyle:ILineStyle = LineStyle()
         lineStyle.thickness = SVGStyleUtils.strokeWidth(style.strokeWidth!)//let strokeWidth:CGFloat
