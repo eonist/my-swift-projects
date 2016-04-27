957a9bec9c96bed8e1d2b708ca6bae878cba5508
Eonist <unicornhive@gmail.com>
2016-04-27 10:17:17 +0200

    Files modified: 1
    Modified 1 file:geom/gradient/Gradient.swift

--- geom/gradient/Gradient.swift
+++ geom/gradient/Gradient.swift
@@ -10,8 +10,9 @@ import Foundation
 public class Gradient:IGradient {/*<---its public so that it works in playground*/
     public var colors:Array<CGColor>
     public var locations:Array<CGFloat>/*same as color stops between 0 & 1*/
-    public var rotation:CGFloat;/*must be between -π and π or it will fail*/
-    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [], _ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/){/*,*/
+    public var rotation:CGFloat;/*must be between -π and π or it will fail*/ /*<---this doesnt belong here, you apply rotations in the matrix*/
+    public var transformation:CGAffineTransform?
+    public init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [], _ rotation:CGFloat = 1.5707963267949/*4.71238898038469*/, transformation:CGAffineTransform? = nil){/*,*/
         self.colors = colors
         if (locations.count == 0/* && colors.count > 0*/) {//add support for nil aswell
             //Swift.print(colors.count)
@@ -21,5 +22,6 @@ public class Gradient:IGradient {/*<---its public so that it works in playground
             self.locations = locations
         }
         self.rotation = rotation
+        self.transformation = transformation
     }
 }
