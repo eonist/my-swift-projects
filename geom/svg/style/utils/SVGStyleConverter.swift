6e7960ed6a038263b41148ac768d8bf70456418e
Eonist <unicornhive@gmail.com>
2016-04-27 19:49:28 +0200

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGStyleConverter.swift

--- geom/svg/style/utils/SVGStyleConverter.swift
+++ geom/svg/style/utils/SVGStyleConverter.swift
@@ -6,13 +6,15 @@ class SVGStyleConverter {
      */
     class func graphicStyle(svgStyle:SVGStyle,_ shape:Shape)->IGraphicStyle{
         
-        let lineStyle:ILineStyle = SVGLineStyleConverter.lineStyle(svgStyle, shape)
-        let fillStyle:IFillStyle = 
+        let lineStyle:ILineStyle? = SVGLineStyleConverter.lineStyle(svgStyle, shape)
+        let fillStyle:IFillStyle =
         //continue here: add support for Fill and LineStyle (remember to add Gradient support. and remember that you have all the code for this, just look at the actual drawing of the SVGGraphic. it converts to the format you need. )
         //this method should be made but the You have to merge IGradient and IGraphicsGradient somehow, you could create a third class named GraphicsStyle
         
         return GraphicStyle()
     }
+}
+private class SVGFillStyleConverter{
     /**
      *
      */
@@ -25,10 +27,6 @@ class SVGStyleConverter {
         //Swift.print("color: " + "\(color)")
         return color
     }
-    
-}
-private class SVGFillStyleConverter{
-    
 }
 private class SVGLineStyleConverter{
     /**
