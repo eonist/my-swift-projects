f564cd50ca917294ebec6567b189a980ad77c497
Eonist <unicornhive@gmail.com>
2016-04-27 20:37:13 +0200

    Files modified: 1
    Modified 1 file:geom/graphic/style/IGraphicStyle.swift

--- geom/graphic/style/IGraphicStyle.swift
+++ geom/graphic/style/IGraphicStyle.swift
@@ -4,3 +4,7 @@ protocol IGraphicStyle {
     var fillStyle:IFillStyle? { get set }
     var lineStyle:ILineStyle? { get set }
 }
+extension IGraphicStyle{/*just for convenince*/
+    var fill:IFillStyle? {get{return fillStyle} set{fillStyle = newValue}}
+    var line:ILineStyle? {get{return lineStyle} set{lineStyle = newValue}}
+}
