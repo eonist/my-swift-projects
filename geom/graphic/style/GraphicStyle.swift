cfdd58a41ca670eaeb55b147468d4f6abeea81b4
Eonist <unicornhive@gmail.com>
2016-04-27 20:20:12 +0200

    Files modified: 1
    Modified 1 file:geom/graphic/style/GraphicStyle.swift

--- geom/graphic/style/GraphicStyle.swift
+++ geom/graphic/style/GraphicStyle.swift
@@ -1,5 +1,15 @@
-import Foundation
-
-class GraphicStyle {
-
+import Cocoa
+protocol IGraphicStyle {
+    var fill: IFill? { get set }
+    var stroke: IStroke { get set }
+}
+class GraphicStyle:IGraphicStyle {
+    static var clear:IGraphicStyle = GraphicStyle(Fill.clear,Stroke.clear)
+    static var green:IGraphicStyle = GraphicStyle(Fill(NSColor.greenColor()),Stroke.clear)
+    var fill: IFill? //{ get {return self.fill} set {self.fill = newValue} }
+    var stroke: IStroke //{ get {return self.stroke } set {self.stroke = newValue}}
+    init(_ fill:IFill? = Fill.clear/*<-maybe not use clear here, nil works aswell*/,_ stroke:IStroke = Stroke.clear) {
+        self.fill = fill;
+        self.stroke = stroke;
+    }
 }
