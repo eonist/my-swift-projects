1216a964dfa7b73d5adc476c1f3f11cbc55ca743
Eonist <unicornhive@gmail.com>
2016-04-27 19:38:40 +0200

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -147,58 +147,7 @@ class SVGUtils {
 		 // :TODO: add support for fillOpacity,fillRule,strokeOpacity,strokeLineCap,strokeLineJoin,strokeMiterLimit, (Get ques from the old SVGPropertyParser)
 		 return xml;
 	 }
-    /**
-     *
-     */
-    class func style(svgStyle:SVGStyle,_ shape:Shape)->IGraphicStyle{
-        
-        var lineStyle:ILineStyle
-        if(svgStyle.stroke is Double) {
-            lineStyle = SVGUtils.lineStyle(svgStyle)
-        }else if(svgStyle.stroke is SVGGradient){
-            //Swift.print("gradient")
-            lineStyle = SVGUtils.lineStyle(svgStyle)
-            let graphicsGradient:IGraphicsGradient = SVGUtils.lineGraphicsGradient(shape, svgStyle)
-            
-            
-            //Continue here: convert graphicsGradient to Gradient
-            
-            
-            lineStyle = GradientLineStyle()
-        }else{/*clear*/
-            //Swift.print("no stroke")
-            //fatalError("not implemented yet " + "\(style!.stroke)")
-        }
-        //continue here: add support for Fill and LineStyle (remember to add Gradient support. and remember that you have all the code for this, just look at the actual drawing of the SVGGraphic. it converts to the format you need. )
-        //this method should be made but the You have to merge IGradient and IGraphicsGradient somehow, you could create a third class named GraphicsStyle
-        
-        return GraphicStyle()
-    }
-    /**
-     *
-     */
-    class func fillColor(style:SVGStyle)->NSColor{
-        //Swift.print("SVGGraphic.beginFill() color")
-        let colorVal:Double = !(style.fill as! Double).isNaN ? style.fill as! Double : Double(0x000000)
-        //Swift.print("colorVal: " + "\(colorVal)")
-        let opacity:CGFloat = style.fillOpacity != nil && !style.fillOpacity!.isNaN ? style.fillOpacity! : 1
-        let color:NSColor = NSColorParser.nsColor(UInt(colorVal), opacity)
-        //Swift.print("color: " + "\(color)")
-        return color
-    }
-    /**
-     *
-     */
-    class func lineStyle(style:SVGStyle)->ILineStyle{
-        var lineStyle:ILineStyle = LineStyle()
-        lineStyle.thickness = SVGStyleUtils.strokeWidth(style.strokeWidth!)//let strokeWidth:CGFloat
-        lineStyle.miterLimit = SVGStyleUtils.miterLimit(style.strokeMiterLimit!)
-        lineStyle.lineCap = SVGStyleUtils.lineCap(style.strokeLineCap)
-        lineStyle.lineJoin = SVGStyleUtils.lineJoin(style.strokeLineJoin)
-        let strokeOpacity:CGFloat = style.fillOpacity != nil && !style.fillOpacity!.isNaN ? style.fillOpacity! : 1/*<-- this line is new, used to be done inline*/
-        lineStyle.color = style.stroke != nil && style.stroke! is Double && !(style.stroke! as! Double).isNaN ? SVGStyleUtils.strokeColor(style.stroke! as! Double, strokeOpacity) : NSColor.clearColor()//if color is NaN or nil then set this to clear color
-        return lineStyle
-    }
+    
     /**
      * TODO: when you scale the lineGradient, remember to consider that you dont scale the lineWidth and so the gradient that is applied may not cover as it should. This means that you cant simply scale the SVGGradient.transformation, instead you have to scale the gradient that is attached to the indeviduel SVGGraphic, and you have to take into consideration that the strokeThickness is now different than the new relative size. Even though its the same, the relative difference between the strokeThickness and the size of the shape is different. So you have to recalculate the gradient.transformation matrix a little bit. Probably by insetting it by some relative value
      */
