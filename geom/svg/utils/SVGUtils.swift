67214115a0ef4e6f417ca372b4941eec41c03959
Eonist <unicornhive@gmail.com>
2016-04-27 09:05:30 +0200

    Files modified: 1
    Modified 1 file:geom/svg/utils/SVGUtils.swift

--- geom/svg/utils/SVGUtils.swift
+++ geom/svg/utils/SVGUtils.swift
@@ -154,7 +154,7 @@ class SVGUtils {
         
         
         //continue here: add support for Fill and LineStyle (remember to add Gradient support. and remember that you have all the code for this, just look at the actual drawing of the SVGGraphic. it converts to the format you need. )
-        
+        //this method should be made but the You have to merge IGradient and IGraphicsGradient somehow, you could create a third class named GraphicsStyle
         
         return GraphicStyle()
     }
