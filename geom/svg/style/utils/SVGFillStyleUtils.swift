5e1eb8f222786ed4cf984d4fa79352821d50f882
Eonist <unicornhive@gmail.com>
2016-04-28 10:40:14 +0200

    Files modified: 1
    Modified 1 file:geom/svg/style/utils/SVGFillStyleUtils.swift

--- geom/svg/style/utils/SVGFillStyleUtils.swift
+++ geom/svg/style/utils/SVGFillStyleUtils.swift
@@ -12,9 +12,9 @@ class SVGFillStyleUtils{
             let color:NSColor = SVGFillStyleUtils.fillColor(style)
             //Swift.print("color: " + "\(color)")
             fillStyle = FillStyle(color)
-        }else if(style.fill != nil && style.fill! is ISVGGradient){//<- may need to use dynamixtype to assert this?!?
-            //Swift.print("trans: " + "\((style!.fill as! SVGGradient).gradientTransform)")
-           SVGFillStyleUtils.gradientFillStyle(style, shape)
+        }else if(style.fill != nil && style.fill is ISVGGradient){//<- may need to use dynamixtype to assert this?!?
+            Swift.print("WORKS")
+            SVGFillStyleUtils.gradientFillStyle(style, shape)
 
         }else{
             //clear
