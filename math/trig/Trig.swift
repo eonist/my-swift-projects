48cb2fdab0b0ffc4e2576407c6ffcf78c244ed47
Eonist <unicornhive@gmail.com>
2016-04-30 09:37:32 +0200

    Files modified: 1
    Modified 1 file:math/trig/Trig.swift

--- math/trig/Trig.swift
+++ math/trig/Trig.swift
@@ -37,42 +37,11 @@ class Trig{
      * @return angle in degrees (0-360)
      */
     class func angleType(angleType:String)->CGFloat {
-        if(angleType == "left") {return left}
-        else if(angleType == "right") {return right}
-        else if(angleType == "top") {return top}
-        else {/*bottom*/ return bottom}
+        return TrigParser.angleType(angleType)
     }
-    /**
-     * Returns a radian to be between 0 and Math.PI*2 Radian (0 - 6.28)
-     * @param theta: An radian in degrees typically 0 - Math.PI*2
-     * @Note: we use "while" function type here because angle could be very low at which point Math.PI*2 needs to be contrinuasly added until its above 0 )
-     * // :TODO: use modulo like normalize2 does, is that faster ? do an optimization test.
-     */
-    class func normalize(var angle:CGFloat)->CGFloat {
-        while (angle < 0) {angle += π*2};
-        while (angle >= π*2) {angle -= π*2};
-        return angle;
-    }
-    /**
-     * Returns an radian to be between -Math.PI(-3.14) and Math.PI(3.14)
-     * @param theta: An radian in degrees typically 0 - Math.PI*2
-     */
-    class func normalize2(angle:CGFloat)->CGFloat {
-        if(angle < -π){ return π + (angle % π)};
-        if(angle > π){ return -π + (angle % π)};
-        return angle;
-    }
-
-    func cartesianToPolar(point:CGPoint)->(radius:CGFloat,angle:CGFloat){return TrigParser.cartesianToPolar(point)}
-
-        
-    
-
+    class func normalize(angle:CGFloat)->CGFloat {return TrigParser.normalize(angle)}
+    class func normalize2(angle:CGFloat)->CGFloat {return TrigParser.normalize2(angle)}
+    //func cartesianToPolar(point:CGPoint)->(radius:CGFloat,angle:CGFloat){return TrigParser.cartesianToPolar(point)}
     class func degrees(radians:CGFloat) -> CGFloat {return TrigParser.degrees(radians)}
-        
-    
     class func radians(degrees:CGFloat) -> CGFloat {return TrigParser.radians(degrees)}
-        
-    
-
 }
