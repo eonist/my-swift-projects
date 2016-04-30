4f2aa41f892ac98253d7d6900a01a1f4af4629b7
Eonist <unicornhive@gmail.com>
2016-04-30 09:41:48 +0200

    Files modified: 1
    Modified 1 file:math/trig/TrigConverter.swift

--- math/trig/TrigConverter.swift
+++ math/trig/TrigConverter.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class TrigConverter {
-
+    /**
+     * Needs code
+     */
+    class func cartesianToPolar(point:CGPoint)->(radius:CGFloat,angle:CGFloat){
+        /* do some math here to get radius and angle */
+        let radius = CGFloat(0.0)
+        let angle = CGFloat(0.0)
+        return (radius,angle)
+    }
 }
