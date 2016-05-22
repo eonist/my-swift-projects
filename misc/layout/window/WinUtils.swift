a5ff373f18ab0c1342553288fc5ed22037a9ea92
Eonist <unicornhive@gmail.com>
2016-05-22 18:31:30 +0200

    Files modified: 1
    Modified 1 file:misc/layout/window/WinUtils.swift

--- misc/layout/window/WinUtils.swift
+++ misc/layout/window/WinUtils.swift
@@ -1,19 +1,19 @@
 import Cocoa
 
-class WindowUtils {
+class WinUtils {
     /**
-     * Positions a window
+     * Positions a window to @param position
      */
-    class func position(window:NSWindow, position:CGPoint){
-        window.setFrame(NSRect(position.x,position.y,window.frame.width,window.frame.height), display: true)/*<--unsure what the display var does*/
+    class func position(win:NSWindow, _ position:CGPoint){
+        window.setFrame(NSRect(position.x,position.y,win.frame.width,win.frame.height), display: true)/*<--unsure what the display var does*/
     }
     /**
      * Aligns a window to an alignment type
      * NOTE: The screen aligns from the bottom up (so use bottom for top and top for bottom)
      */
-    class func align(panel:NSWindow,_ canvasAlignment:String,_ viewAlignment:String,_ offset:CGPoint = CGPoint(0,0)) {
-        let alignmentPoint:CGPoint = Align.alignmentPoint(CGSize(panel.frame.width,panel.frame.height), CGSize(NSScreen.mainScreen()!.visibleFrame.width,NSScreen.mainScreen()!.visibleFrame.height),canvasAlignment,viewAlignment,offset)
+    class func align(win:NSWindow,_ canvasAlignment:String,_ viewAlignment:String,_ offset:CGPoint = CGPoint(0,0)) {
+        let alignmentPoint:CGPoint = Align.alignmentPoint(CGSize(win.frame.width,win.frame.height), CGSize(NSScreen.mainScreen()!.visibleFrame.width,NSScreen.mainScreen()!.visibleFrame.height),canvasAlignment,viewAlignment,offset)
         //Swift.print("ScreenUtils.alignmentPoint: " + "\(alignmentPoint)")
-        panel.setFrame(NSRect(alignmentPoint.x,alignmentPoint.y,panel.frame.width,panel.frame.height), display: true)/*<--unsure what the display var does*/
+        WinUtils.position(win, alignmentPoint)
     }
 }
