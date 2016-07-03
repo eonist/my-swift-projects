2cec929af12573235ab1edcc2dbca43187e00d5b
Eonist <unicornhive@gmail.com>
2016-07-03 19:56:52 +0200

    Files modified: 2
    Modified 2 files:misc/application/AppExtension.swiftmisc/layout/window/WinExtensions.swift

--- misc/application/AppExtension.swift
+++ misc/application/AppExtension.swift
@@ -1,5 +1,15 @@
-import Foundation
+import Cocoa
 
-class ApplicationExtension {
-
+extension NSApplication{
+    
+    var activeWindow:NSWindow {
+        
+        //(app.windows[0] as! )
+        for win in self.windows{
+            print("windowNumber: " + "\(win.windowNumber)")
+            app.windowWithWindowNumber(win.windowNumber)//this is how you can manage windows
+            
+        }
+        return nil
+    }
 }
