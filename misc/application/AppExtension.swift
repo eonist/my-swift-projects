62b5ba78fad163b9dc06f113e67ae5b8efd2771e
Eonist <unicornhive@gmail.com>
2016-07-03 20:05:27 +0200

    Files modified: 1
    Modified 1 file:misc/application/AppExtension.swift

--- misc/application/AppExtension.swift
+++ misc/application/AppExtension.swift
@@ -4,13 +4,12 @@ extension NSApplication{
     /**
      *
      */
-    var focusedWindow:NSWindow {
+    var focusedWindow:NSWindow? {
         //(app.windows[0] as! )
         for win in self.windows{
-            print("windowNumber: " + "\(win.windowNumber)")
-            win.isAccessibilityFocused()
+            //print("windowNumber: " + "\(win.windowNumber)")
             //app.windowWithWindowNumber(win.windowNumber)//this is how you can manage windows
-            
+            if(win.isAccessibilityFocused()){return win}
         }
         return nil
     }
