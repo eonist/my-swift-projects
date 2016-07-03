8c43360aabf244cc52901c699c53fe1b6f2f9330
Eonist <unicornhive@gmail.com>
2016-07-03 19:37:20 +0200

    Files modified: 1
    Modified 1 file:misc/layout/window/WinParser.swift

--- misc/layout/window/WinParser.swift
+++ misc/layout/window/WinParser.swift
@@ -8,9 +8,9 @@ class WinParser {
      */
     class func describeWindows(app:NSApplication){
         Swift.print("app.windows.count: " + "\(app.windows.count)")
-        for w in app.windows{
-            print("windowNumber: " + "\(w.windowNumber)")
-            app.windowWithWindowNumber(w.windowNumber)//this is how you can manage windows
+        for win in app.windows{
+            print("windowNumber: " + "\(win.windowNumber)")
+            app.windowWithWindowNumber(win.windowNumber)//this is how you can manage windows
         }
     }
     /**
