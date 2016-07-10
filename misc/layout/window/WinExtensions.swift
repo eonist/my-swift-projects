f3c9557bf7e312c42378e84ec68a8220d7c0030b
Eonist <unicornhive@gmail.com>
2016-07-10 19:18:36 +0200

    Files modified: 1
    Modified 1 file:misc/layout/window/WinExtensions.swift

--- misc/layout/window/WinExtensions.swift
+++ misc/layout/window/WinExtensions.swift
@@ -1,5 +1,17 @@
-import Foundation
+import Cocoa
 
-class WinExtensions {
-    
+extension NSWindow {
+    /**
+     * Shows @param: win
+     */
+    func show(){
+        self.makeKeyAndOrderFront(self)//<- should be self, but nil may work
+        NSApp.activateIgnoringOtherApps(true)
+    }
+    /**
+     * Hides @param: win
+     */
+    func hide(win:NSWindow){
+        self.orderOut(self)//<- should be self, but nil may work
+    }
 }
