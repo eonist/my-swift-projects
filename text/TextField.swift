f0d15c2f4acef2b483baafb0813e01c46bdb668e
Eonist <unicornhive@gmail.com>
2016-07-26 23:14:05 +0200

    Files modified: 1
    Modified 1 file:text/TextField.swift

--- text/TextField.swift
+++ text/TextField.swift
@@ -21,9 +21,7 @@ class TextField:NSTextField{
         Swift.print("onMouseDownOutside " + "localPos: " + "\(event.localPos(self))")
         if(hitTest(event.localPos(self)) == nil){//if you click outside the NSTextField then this will take care of resiging the caret of the text
             //Swift.print("resign")
-            if(globalMouseDownHandler != nil) {
-                NSEvent.removeMonitor(globalMouseDownHandler!)
-            }//we remove the evenListener as its done its job
+            if(globalMouseDownHandler != nil) {NSEvent.removeMonitor(globalMouseDownHandler!)}//we remove the evenListener as its done its job
             self.window?.makeFirstResponder(nil)//resigns the NSTextField caret focus
         }
         return event
