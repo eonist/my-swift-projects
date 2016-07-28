c0904b05f7a35b1d9693e13cc999d76ce5519aa8
Eonist <unicornhive@gmail.com>
2016-07-28 20:58:26 +0200

    Files modified: 1
    Modified 1 file:misc/layout/protocols/IPositional.swift

--- misc/layout/protocols/IPositional.swift
+++ misc/layout/protocols/IPositional.swift
@@ -1,7 +1,7 @@
 import Foundation
 protocol IPositional:class {//<--new extends class
     //pos was recently commented out because it isn't needed!?!?
-    //var pos:CGPoint{get set}/*<-- this is named pos, because the name position is effectivly blocked when using implicit getter and setter names*/
+    
     func setPosition(position:CGPoint)
     func getPosition() -> CGPoint
 }
@@ -10,8 +10,9 @@ protocol IPositional:class {//<--new extends class
  */
 extension IPositional{
     //var positional:IPositional {get{return self as IPositional}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting mutating variables via extensions*/
-    var x:CGFloat{get{return self.getPosition().x} set{setPosition(CGPoint(newValue,getPosition().y))}}
-    var y:CGFloat{get{return self.getPosition().y} set{setPosition(CGPoint(getPosition().x,newValue))}}
+    var pos:CGPoint{get{return self.getPosition()} set{self.setPosition(newValue)}}/*<-- this is named pos, because the name position is effectivly blocked when using implicit getter and setter names*/
+    var x:CGFloat{get{return self.pos.x} set{self.pos.x = newValue}}
+    var y:CGFloat{get{return self.pos.y} set{self.pos.y = newValue}}
     /*func getPosition() -> CGPoint {
     return position
     }*/
