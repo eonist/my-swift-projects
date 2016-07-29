1dab440d3b61bd96c2aeb2798cc8acf85dae6755
Eonist <unicornhive@gmail.com>
2016-07-29 17:20:52 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/InteractiveViewExtensions.swift

--- misc/view/utils/InteractiveViewExtensions.swift
+++ misc/view/utils/InteractiveViewExtensions.swift
@@ -1,5 +1,20 @@
-import Foundation
+import Cocoa
 
-class InteractiveViewExtensions {
-
+/**
+ * TODO: add to private utils class, maybe!?!?
+ */
+extension InteractiveView2{
+    /**
+     * Returns a correctly flipped coordinate of the mouse in window space 0,0
+     * @NOTE: there may be a problem with this if the window has a titleBar, then the height of this may need to be included
+     */
+    var winMousePos:CGPoint {
+        var pos = (window?.mouseLocationOutsideOfEventStream)!//convertPoint((window?.mouseLocationOutsideOfEventStream)!, fromView: nil)/*converts the p to local coordinates*/
+        pos.y = WinParser.height(window!) - pos.y/*flips the window coordinates*/
+        return pos
+    }
+    var viewUnderMouse:NSView?{
+        let theHitView = window!.contentView?.hitTest((window?.mouseLocationOutsideOfEventStream)!)
+        return theHitView
+    }
 }
