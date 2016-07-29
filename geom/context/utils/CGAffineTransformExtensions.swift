f9269d19369c2d793359f210549f0ef1733adcf6
Eonist <unicornhive@gmail.com>
2016-07-29 22:02:30 +0200

    Files modified: 1
    Modified 1 file:geom/context/utils/CGAffineTransformExtensions.swift

--- geom/context/utils/CGAffineTransformExtensions.swift
+++ geom/context/utils/CGAffineTransformExtensions.swift
@@ -44,36 +44,16 @@ extension CGAffineTransform {
      * // :TODO: rename to just rotate? for simplicity?
      */
     static func rotateAroundPoint(var transform:CGAffineTransform,_ rotation:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
-        transform.translate(pivot.x, pivot.y)/*<-this looks strage, but you sort of set the point here*/
-        transform.rotate(rotation)// = CGAffineTransformRotate(transform, rotation);
-        transform.translate(-pivot.x,-pivot.y)/*then you reset the offset to the original position*/
-        return transform
+        return CGAffineTransformParser.rotateAroundPoint(transform, rotation, pivot)
     }
-    /**
-     * NOTE: You can chain scaleFromPoint and rotatAroundPoint and eventually skewFromPoint. This is a great way to get different transform results quickly
-     */
-    static func scaleFromPoint(var transform:CGAffineTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
-        transform.translate(pivot.x, pivot.y)/*<-this looks strage, but you sort of set the point here*/
-        transform.scale(xScale,yScale)// = CGAffineTransformRotate(transform, rotation);
-        transform.translate(-pivot.x,-pivot.y)/*then you reset the offset to the original position*/
-        return transform
+    static func scaleFromPoint(transform:CGAffineTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
+        return CGAffineTransformParser.scaleFromPoint(transform, xScale, yScale, pivot)
     }
-    /**
-     *
-     */
-    static func translate(var transform:CGAffineTransform,_ x:CGFloat,_ y:CGFloat)->CGAffineTransform{
-        transform.translate(x, y);
-        return transform
+    static func translate(transform:CGAffineTransform,_ x:CGFloat,_ y:CGFloat)->CGAffineTransform{
+        return CGAffineTransformParser.translate(transform, x, y)
     }
-    /**
-     * NOTE: this method is used in conjunction with the radial gradient matrix transformation of context
-     * NOTE: the special thing about it is that the scale value follows the axis of the rotation. defualt rotation is 0 and thus scale.y becomes the width sort of
-     */
-    static func transformAroundPoint(var transform:CGAffineTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint)->CGAffineTransform{
-        transform = CGAffineTransform.translate(transform,offset.x,offset.y)//transform,minRadius*gradient.relativeStartCenter!.x,minRadius*gradient.relativeStartCenter!.y
-        transform = CGAffineTransform.rotateAroundPoint(transform, rotation, pivot)
-        transform = CGAffineTransform.scaleFromPoint(transform, scale.y/**/,  scale.x/**/, pivot)
-        return transform
+    static func transformAroundPoint(transform:CGAffineTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint)->CGAffineTransform{
+        return CGAffineTransformParser.transformAroundPoint(transform, scale, rotation, offset, pivot)
     }
     static func transformWithPivot(inout transform:CGAffineTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint,_ initRotation:CGFloat = 0) {
         CGAffineTransformModifier.transformWithPivot(&transform,scale,rotation,offset,pivot,initRotation)
@@ -81,16 +61,10 @@ extension CGAffineTransform {
     static func copy(transform:CGAffineTransform)->CGAffineTransform{
         return CGAffineTransformParser.copy(transform)
     }
-    /**
-     *
-     */
     static func concat(a:CGAffineTransform,_ b:CGAffineTransform)->CGAffineTransform{
         return CGAffineTransformParser.concat(a, b)
     }
-    /**
-     * Applys a AffineTransformation on a point (This method is mostly for convenience)
-     */
     func transformPoint(point:CGPoint) -> CGPoint{
-        return CGPointApplyAffineTransform(point, self)
+        return CGAffineTransformModifier.transformPoint(point, self)
     }
 }
