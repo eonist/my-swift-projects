42e499a65f771e1a4cc0f84a650778886560c799
Eonist <unicornhive@gmail.com>
2016-07-29 17:23:09 +0200

    Files modified: 1
    Modified 1 file:misc/view/InteractiveView2.swift

--- misc/view/InteractiveView2.swift
+++ misc/view/InteractiveView2.swift
@@ -184,27 +184,3 @@ class InteractiveView2:FlippedView,IInteractiveView{
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
-
-/**
- * Reasearch, these can be deprecated
- */
-class InteractionUtils{
-    /**
-     * This method finds the immediate origin. Aka the first descendant of current
-     */
-    func immediate(current:NSView,_ origin:NSView)->NSView{//origin may be a CALayer, test this later
-        var view:NSView = origin
-        while(view.superview != current) {
-            view = view.superview!
-        }
-        return view;
-    }
-    /**
-     * origin equals target or origin descendes from target
-     * @param origin: the result of hitTest()
-     * @param target: i.e button1 or slider or some other UI component.
-     */
-    func withinScope(origin:NSView,_ target:NSView)->Bool{
-        return origin === target || origin.isDescendantOf(target)
-    }
-}
