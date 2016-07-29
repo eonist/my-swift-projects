d95f96433d2a037fc728f466d149da7407c67545
Eonist <unicornhive@gmail.com>
2016-07-29 18:17:35 +0200

    Files modified: 1
    Modified 1 file:misc/view/utils/mouse/MouseUtils.swift

--- misc/view/utils/mouse/MouseUtils.swift
+++ misc/view/utils/mouse/MouseUtils.swift
@@ -1,5 +1,11 @@
-import Foundation
+import Cocoa
 
 class MouseUtils {
-
+    /**
+     * Returns a Point for the mouse position in the coordinate space of the PARAM: view
+     */
+    class func point(view:NSView) -> CGPoint {
+        return view.localPos()
+    }
+    //isOver
 }
