95e37d8eb398deda7b2daef8d4c6c8bff7c74144
Eonist <unicornhive@gmail.com>
2016-07-29 16:43:32 +0200

    Files modified: 1
    Modified 1 file:misc/layout/utils/ClosableModifier.swift

--- misc/layout/utils/ClosableModifier.swift
+++ misc/layout/utils/ClosableModifier.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class ClosableModifier {
-
+    /**
+     *
+     */
+    class func closeAll(scope:*) {
+        if(scope as DisplayObjectContainer){ scope = DisplayObjectParser.childrenOfType(scope, IClosable)}
+        else if((scope is Array) == false)  {throw new IllegalOperationError(scope+" type not supported")}
+        let sub
+        for closable : IClosable in scope{ closable.close()}
+    }
 }
