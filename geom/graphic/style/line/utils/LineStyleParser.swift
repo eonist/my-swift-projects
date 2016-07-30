167c69a9bb22a052070fe56ae949cb0bb4fda83c
Eonist <unicornhive@gmail.com>
2016-07-30 22:03:59 +0200

    Files modified: 1
    Modified 1 file:geom/graphic/style/line/utils/LineStyleParser.swift

--- geom/graphic/style/line/utils/LineStyleParser.swift
+++ geom/graphic/style/line/utils/LineStyleParser.swift
@@ -18,24 +18,16 @@ class LineStyleParser {
      *
      */
     class func lineCapType(lineCap:CGLineCap)->String{
-        if(lineCap == CGLineCap.Butt){
-            return "butt"
-        }else if(lineCap == CGLineCap.Round){
-            return "round"
-        }else{//Square
-            return "square"
-        }
+        if(lineCap == CGLineCap.Butt){return "butt"}
+        else if(lineCap == CGLineCap.Round){return "round"}
+        else{return "square"}//Square
     }
     /**
      *
      */
     class func lineJoinType(lineJoin:CGLineJoin)->String{
-        if(lineJoin == CGLineJoin.Miter){
-            return "miter"
-        }else if(lineJoin == CGLineJoin.Round){
-            return "round"
-        }else{//Bevel
-            return "bevel"
-        }
+        if(lineJoin == CGLineJoin.Miter){return "miter"}
+        else if(lineJoin == CGLineJoin.Round){return "round"}
+        else{return "bevel"}//Bevel
     }
 }
