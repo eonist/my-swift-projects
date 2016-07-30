6b01813d8fef51dfa5c6e73721449a1f5e445fe3
Eonist <unicornhive@gmail.com>
2016-07-30 14:06:06 +0200

    Files modified: 1
    Modified 1 file:geom/path/IPath.swift

--- geom/path/IPath.swift
+++ geom/path/IPath.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol IPath:class{
+protocol IPath:class{/*<-when you extend class you can mutate variables directly when casting to IPath*/
     var commands:Array<Int>{get set}
     var pathData:Array<CGFloat>{get set}
     var rotation:CGFloat{get set}
