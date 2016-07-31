b3737a7fab88a5070a914d30c22b408c7eef68ec
Eonist <unicornhive@gmail.com>
2016-07-31 21:00:46 +0200

    Files modified: 1
    Modified 1 file:misc/view/AdvanceNSViewParser.swift

--- misc/view/AdvanceNSViewParser.swift
+++ misc/view/AdvanceNSViewParser.swift
@@ -10,4 +10,27 @@ class AdvanceNSViewParser {
         for view : NSView in views {indices.append(NSViewParser.index(container, view)!)}/*<--This should be correct*/
         return indices
     }
+    /**
+     * Returns an NSView instance at @param index in @param container
+     * @Note this function is recursive
+     * TODO: rename index to map?, move to AdvanceDisplayObjectParser?
+     */
+    class func getAt(container:NSView?, _ index:Array<Int>) -> NSView? {
+        if(index.count == 0 && container != nil) {
+            Swift.print("a")
+            return container!
+        }
+        else if(index.count == 1 && container!.numSubViews > 0) {
+            Swift.print("b")
+            return container?.getSubViewAt(index[0])!
+        }
+        else if(index.count > 1 && container!.numSubViews > 0 && container!.getSubViewAt(index[0]) is NSView/*<--this last assert isn't needed*/) {
+            Swift.print("c")
+            return getAt(displayObjectContainer.getChildAt(index[0]) as DisplayObjectContainer,index.slice(1,index.length));
+        }
+        else{
+            trace("d");
+            return null;
+        }
+    }
 }
