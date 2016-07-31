9cab9c0e75ba89f9cb24d6944cc5e8a3dcdb144e
Eonist <unicornhive@gmail.com>
2016-07-31 19:31:57 +0200

    Files modified: 1
    Modified 1 file:array/AdvanceArrayModifier.swift

--- array/AdvanceArrayModifier.swift
+++ array/AdvanceArrayModifier.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 class AdvanceArrayModifier {
-
+    /**
+     *
+     */
+    class func sortToHighestDepths(indices:Array<Int>) -> Array<Int>{
+        var sortedIndices:Array<Int> = []
+        var numOfindices:Int = indices.count
+        for (var i : Int = 0; i < numOfindices; i++) {
+            var index:Array<Int> = indices[i]
+            var indicesIndex:Int = AdvanceArrayParser.index(index, indices)
+            ArrayModifier.addAt(sortedIndices, index, indicesIndex)
+        }
+        return sortedIndices;
+    }
 }
