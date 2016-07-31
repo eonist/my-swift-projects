51cb657f84ac96d9b454974e2395a5f3ae5db9c5
Eonist <unicornhive@gmail.com>
2016-07-31 13:31:28 +0200

    Files modified: 1
    Modified 1 file:geom/point/PointAsserter.swift

--- geom/point/PointAsserter.swift
+++ geom/point/PointAsserter.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class PointAsserter {
-
+    /**
+     * Asserts if p1 is in the same position as p2
+     * @Note: You can also use the native function point1.equales(point2)
+     * @Note: unlike the native point.equals method this method supports NaN values
+     * // :TODO: does NaN equal NaN and what is its native behaviour
+     */
+    class func equals(p1:CGPoint, _ p2:CGPoint) -> Bool {
+        return NumberAsserter.equals(p1.x, p2.x) && NumberAsserter.equals(p1.y, p2.y)
+    }
 }
