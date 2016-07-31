d99e16acbb0c8327c1eac4f1427f14ee25ab78e7
Eonist <unicornhive@gmail.com>
2016-07-31 15:02:26 +0200

    Files modified: 2
    Modified 2 files:geom/line/utils/LineAsserter.swiftgeom/point/PointAsserter.swift

--- geom/line/utils/LineAsserter.swift
+++ geom/line/utils/LineAsserter.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class LineAsserter {
-
+    /**
+     *
+     */
+    class func intersects(a:Line,_ b:Line) -> Bool {// :TODO: rename to isSomeName
+        return PointAsserter.intersects(a.p1, a.p2, b.p1, b.p2)
+    }
 }
--- geom/point/PointAsserter.swift
+++ geom/point/PointAsserter.swift
@@ -10,4 +10,20 @@ class PointAsserter {
     class func equals(p1:CGPoint, _ p2:CGPoint) -> Bool {
         return NumberAsserter.equals(p1.x, p2.x) && NumberAsserter.equals(p1.y, p2.y)
     }
+    /**
+     * Asserts if two lines intersects (p1 and p2 is line1, p3 and p4 is line2)
+     * @Note if eigther a1 or a2 is "CoLinear" and within with b1 and b2 then it intersects, does it, yes?
+     * @Note if line a touches the start or end of line b then it intersects
+     * @Note if the end of line a is equal to the end of line b then it returns false, this can in some cases be undesirable // :TODO: you could add a point check to mitigate this problem
+     */
+    class func intersects(a1:Point ,  _ a2:Point,  _ b1:Point,  _ b2:Point) -> Bool {
+        var q:Number = (a1.y - b1.y) * (b2.x - b1.x) - (a1.x - b1.x) * (b2.y - b1.y);
+        var d:Number = (a2.x - a1.x) * (b2.y - b1.y) - (a2.y - a1.y) * (b2.x - b1.x);
+        if( d == 0 )  return false;
+        var r:Number = q / d;
+        q = (a1.y - b1.y) * (a2.x - a1.x) - (a1.x - b1.x) * (a2.y - a1.y);
+        var s:Number = q / d;
+        if( r < 0 || r > 1 || s < 0 || s > 1 ) return false;
+        return true;
+    }
 }
