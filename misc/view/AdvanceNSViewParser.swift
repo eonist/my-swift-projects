2b41b8e75850bb68e21b7391c6be394f86f4f918
Eonist <unicornhive@gmail.com>
2016-07-31 19:29:45 +0200

    Files modified: 1
    Modified 1 file:misc/view/AdvanceNSViewParser.swift

--- misc/view/AdvanceNSViewParser.swift
+++ misc/view/AdvanceNSViewParser.swift
@@ -1,5 +1,13 @@
-import Foundation
+import Cocoa
 
 class AdvanceNSViewParser {
-
+    /**
+     * TODO: not optimized very well
+     * Returns depths of every DisplayObject instance in @param displayObject
+     */
+    class func indicesOfViews(container:NSView,_ views:Array<NSView>) -> Array<[Int]> {
+        var indices:Array<[Int]> = []
+        for view : NSView in views {indices.append(NSViewParser.index(container, view)!)}/*<--This should be correct*/
+        return indices
+    }
 }
