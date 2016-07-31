8649438cec25b150e68d0138272a5e1c9b46ead0
Eonist <unicornhive@gmail.com>
2016-07-31 14:29:43 +0200

    Files modified: 1
    Modified 1 file:geom/line/Line.swift

--- geom/line/Line.swift
+++ geom/line/Line.swift
@@ -1,5 +1,12 @@
 import Foundation
-
+/**
+ * NOTE: Used to be named: PrimitiveLine
+ */
 class Line {
-
+    var p1:CGPoint
+    var p2:CGPoint
+    init(_ p1:CGPoint,_ p2:CGPoint) {
+        self.p1 = p1
+        self.p2 = p2
+    }
 }
