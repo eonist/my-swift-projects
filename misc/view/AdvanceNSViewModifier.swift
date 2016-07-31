ab32829b5612d6042f072a80e14d5493b2b4d994
Eonist <unicornhive@gmail.com>
2016-07-31 21:17:57 +0200

    Files modified: 1
    Modified 1 file:misc/view/AdvanceNSViewModifier.swift

--- misc/view/AdvanceNSViewModifier.swift
+++ misc/view/AdvanceNSViewModifier.swift
@@ -5,7 +5,7 @@ class AdvanceNSViewModifier {
      * Adds an DisplayObject instance to @param displayObjectContainer at @param index (array index)
      * // :TODO: return the DisplayObject
      */
-    class func addAt(container:NSView, item:NSView, index:Array<Int>) {
+    class func addAt(container:NSView, _ item:NSView, _ index:Array<Int>) {
         if(index.count == 1) {container.addSubviewAt(item, index[0])}
         else if(index.count > 1 && AdvanceNSViewParser.getAt(container,index.slice2(0,index.count-1))?.numSubViews > 0){//<-- the last assert is untested
             AdvanceNSViewParser.getAt(container,index.slice2(0,index.count-1))?.addSubviewAt(item, index[index.count-1])
