baaae86b1ab03432c175f921aa43976ef4bf3e65
Eonist <unicornhive@gmail.com>
2016-07-31 20:53:41 +0200

    Files modified: 1
    Modified 1 file:misc/view/AdvanceNSViewModifier.swift

--- misc/view/AdvanceNSViewModifier.swift
+++ misc/view/AdvanceNSViewModifier.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
 
 class AdvanceNSViewModifier {
-
+    /**
+     * Adds an DisplayObject instance to @param displayObjectContainer at @param index (array index)
+     * // :TODO: return the DisplayObject
+     */
+    class func addAt(container:NSView, item:NSView, index:Array<Int>) {
+        if(index.count == 1) {container.addSubviewAt(item, index[0])}
+        else if(index.count > 1 && DisplayObjectParser.getAt(container,index.slice(0,index.length-1)) is NSView){//<-- here you probably need to assert if NSView has children instead of an is assert
+            (DisplayObjectParser.getAt(displayObjectContainer,index.slice(0,index.length-1)) as DisplayObjectContainer).addChildAt(displayObject, index[index.length-1])
+        }
+    }
 }
