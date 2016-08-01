9d8dfec7328d26c0425c195d320bfbfb063c8713
Eonist <unicornhive@gmail.com>
2016-08-01 14:12:36 +0200

    New files added: 1

---
+++ geom/arc/AngleArc.swift
@@ -0,0 +1,26 @@
+import Foundation
+/**
+ * Data container class
+ */
+class AngleArc {
+    var start:CGFloat
+    var end:CGFloat
+    var xRadii:CGFloat
+    var yRadii:CGFloat
+    var rotation:CGFloat
+    var center:CGPoint
+    init(_ start:CGFloat,_ end:CGFloat,_ xRadii:CGFloat,_ yRadii:CGFloat, _ center:CGPoint,_ rotation:CGFloat = 0) {
+        self.start = start
+        self.end = end
+        self.xRadii = xRadii
+        self.yRadii = yRadii
+        self.rotation = rotation
+        self.center = center
+    }
+}
+extension AngleArc{
+    var x:CGFloat {get {return center.x} set {center.x = newValue}}/*convenience*/
+    var y:CGFloat {get {return center.y} set {center.y = newValue}}/*convenience*/
+    var transform:CGAffineTransform {get {return CGAffineTransformMakeScale(xRadii/yRadii, yRadii/xRadii)} /*set{} */}
+    var delta:CGFloat {return TrigParser.angleSpan(start, end)}
+}
