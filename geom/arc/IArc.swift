600f610873527449627acf8b47ac0177199d3804
Eonist <unicornhive@gmail.com>
2016-08-01 14:17:02 +0200

    Files modified: 1
    Modified 1 file:geom/arc/IArc.swift

--- geom/arc/IArc.swift
+++ geom/arc/IArc.swift
@@ -13,5 +13,14 @@ protocol IArc {
 extension IArc{
     var x:CGFloat {get {return center!.x} set {center!.x = newValue}}/*convenience*/
     var y:CGFloat {get {return center!.y} set {center!.y = newValue}}/*convenience*/
-    var pathData:Array<CGFloat> {return ArcParser.pathData(self)}
+    var pathData:Array<CGFloat> {return BasicArcParser.pathData(self)}
+}
+private class BasicArcParser{
+    /**
+     * Returns the the pathData that represents an Arc Instance (sans the start point)
+     * NOTE: Arc-path-data-structure: xRadii,yRadii,rotation,largeArcFlag,sweepFlag,end.x,end.y,center.x,center.y
+     */
+    class func pathData(arc:IArc)->Array<CGFloat> {
+        return Array<CGFloat>([arc.xRadii,arc.yRadii,arc.rotation,arc.largeArcFlag ? 1:0,arc.sweepFlag ? 1:0,arc.end.x,arc.end.y,arc.center!.x,arc.center!.y]);
+    }
 }
