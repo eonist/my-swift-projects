3a738ad4bca1bdd56c1406e5dcbea3215dc3fd39
Eonist <unicornhive@gmail.com>
2016-08-01 13:55:46 +0200

    Files modified: 1
    Modified 1 file:geom/path/utils/CGPathUtils.swift

--- geom/path/utils/CGPathUtils.swift
+++ geom/path/utils/CGPathUtils.swift
@@ -64,3 +64,72 @@ class CGPathUtils {
     }
 }
 
+private class BasicPathParser{
+    /**
+     *
+     */
+    class func arcAt(path:IPath,_ commandIndex:Int) -> IArc{
+        let pathDataIndex:Int = PathDataParser.index(path.commands, commandIndex)
+        let start:CGPoint = commandIndex > 0 ? PathDataParser.end(path, commandIndex-1) : CGPoint()
+        return PathDataParser.arcAt(path.pathData, pathDataIndex, start)
+    }
+}
+private class BasicPathDataParser{
+    /**
+     * Returns the pathDataIndex based on the @param commandIndex
+     */
+    class func index(commands:Array<Int>,_ commandIndex:Int) -> Int {
+        var pathDataIndex:Int = 0;
+        for (var i : Int = 0; i < commandIndex; i++) {pathDataIndex += CommandParser.commandLength(commands[i])}
+        return pathDataIndex;
+    }
+    /**
+     * Returns the destination end position of a given command at @param commandIndex in @param commands
+     * @param index the index of the command
+     * @Note this is cpu intensive to call if you are iterating over an array
+     */
+    class func end(path:IPath, _ commandIndex:Int) -> CGPoint {// :TODO: rename to position?!? or maybe point?
+        //START USING END2 which supports CLOSE
+        let command:Int = path.commands[commandIndex];
+        let pathDataIndex:Int = BasicPathDataParser.index(path.commands, commandIndex);
+        return endAt(path.pathData, pathDataIndex, command);
+        
+        /*
+        var pathData:Vector.<Number> = pathDataAt(path, commandIndex);
+        if(command == PathCommand.MOVE_TO || command == PathCommand.LINE_TO || command == PathCommand.WIDE_MOVE_TO || command == PathCommand.WIDE_LINE_TO) return new Point(pathData[0],pathData[1]);
+        else if(command == PathCommand.ARC_TO) return new Point(pathData[5],pathData[6]);
+        else if(command == PathCommand.CURVE_TO) return new Point(pathData[2],pathData[3]);
+        else {
+        throw new Error("PathCommand not yet supported");
+        return null;//PathCommand.CUBIC_CURVE_TO// :TODO: not supported yet
+        }
+        */
+    }
+}
+private class Utils{
+    /**
+     * Very temp, remove if the other solution works
+     */
+    class func arcTo(path:IPath,cgPath:CGMutablePathRef,index:Int,prevEnd:CGPoint){
+        //CGPathAddArcToPoint(cgPath, nil, prevEnd.x, prevEnd.y, path.pathData[index+5], path.pathData[index+6], path.pathData[index+0])
+        let center:CGPoint = CGPoint(path.pathData[index+7],path.pathData[index+8])
+        let start:CGPoint = prevEnd.copy()
+        let end:CGPoint = CGPoint(path.pathData[index+5],path.pathData[index+6])
+        let startAngle:CGFloat = TrigParser.angle(center, start)
+        
+        Swift.print("DisplayPathUtils.compile() startAngle: " + "\(startAngle)")
+        let endAngle:CGFloat = TrigParser.angle(center, end)
+        
+        Swift.print("DisplayPathUtils.compile() endAngle: " + "\(endAngle)")
+        let clockwise:Bool =  path.pathData[index+4] == 1
+        Swift.print("DisplayPathUtils.compile() clockwise: " + "\(clockwise)")
+        let delta = TrigParser.angleSpan2(startAngle, endAngle, clockwise)//use angleSpan to find the diff between start and end angles
+        Swift.print("DisplayPathUtils.compile() delta: " + "\(delta)")
+        
+        //CGPathAddArc(cgPath, nil, center.x,center.y, path.pathData[index+0]/*<-radius*/, startAngle,endAngle ,clockwise/**//*<-clockwise*/)
+        cgPath.addRelativeArc(center, path.pathData[index+0], startAngle, delta, nil)
+        //CGPathAddLineToPoint(cgPath,nil,path.pathData[index+5],path.pathData[index+6])
+        
+    }
+}
+
