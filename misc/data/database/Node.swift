a6d9e4e55c8363404710063f8361cbdc35a608a6
Eonist <unicornhive@gmail.com>
2016-08-02 14:57:31 +0200

    Files modified: 1
    Modified 1 file:misc/data/database/Node.swift

--- misc/data/database/Node.swift
+++ misc/data/database/Node.swift
@@ -15,7 +15,7 @@ class Node : EventSender{//this should probably extend NSXMLElement, and just im
         self.xml = xml
     }
     /**
-     * // :TODO: consider renaming to appendAt
+     * // :TODO: consider renaming to appendAt 
      */
     func addAt(index:Array<Int>,_ xml:NSXMLElement){// :TODO: shouldnt the arguments be in this order: xml, index// :TODO: do we still need the event dispatching, cant the calling method do this?
         XMLModifier.addChildAt(self.xml, index, xml)
