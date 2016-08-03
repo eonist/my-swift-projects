b0d31d208009ce71faa6bf69cb7276f7859c95ec
Eonist <unicornhive@gmail.com>
2016-08-03 19:06:51 +0200

    Files modified: 1
    Modified 1 file:misc/clipboard/ClipboardModifier.swift

--- misc/clipboard/ClipboardModifier.swift
+++ misc/clipboard/ClipboardModifier.swift
@@ -4,9 +4,15 @@ class ClipboardModifier {
     /**
      *
      */
-    class func setString(string:String){
+    class func setStringData(string:String,_ clipboardFormat:String)  {
         let thePasteboard = NSPasteboard.generalPasteboard()
-        thePasteboard.clearContents()
-        thePasteboard.setString(string, forType: NSPasteboardTypeString)
+        thePasteboard.clearContents()/*Clear any previous data, since a sys clipboard can have 2 version of the same data etc*/
+        thePasteboard.setString(string, forType: clipboardFormat)
+    }
+    /**
+     *
+     */
+    class func setString(string:String){
+        return setStringData(string, NSPasteboardTypeString)
     }
 }
