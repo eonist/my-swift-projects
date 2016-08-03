069f78f07767f92bd29d916f56a43100fd95180b
Eonist <unicornhive@gmail.com>
2016-08-03 19:17:57 +0200

    Files modified: 1
    Modified 1 file:misc/clipboard/ClipboardAsserter.swift

--- misc/clipboard/ClipboardAsserter.swift
+++ misc/clipboard/ClipboardAsserter.swift
@@ -1,5 +1,17 @@
-import Foundation
+import Cocoa
 
 class ClipboardAsserter {
-
+    /**
+     * @param clipboardFormat ClipboardFormats.TEXT_FORMAT , ClipboardFormats.HTML_FORMAT, etc
+     */
+    class func hasData(clipboardFormat:String) -> Bool {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        return pasteboard.availableTypeFromArray([clipboardFormat])
+    }
+    /**
+     *
+     */
+    class func hasString() -> Bool {
+        return hasData(NSPasteboardTypeString)
+    }
 }
