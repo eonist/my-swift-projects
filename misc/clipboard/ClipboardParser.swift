39a191893cdb2d228b199b5fa16066d9dc4d9f87
Eonist <unicornhive@gmail.com>
2016-08-03 18:57:43 +0200

    Files modified: 2
    Modified 2 files:misc/clipboard/ClipboardParser.swiftmisc/clipboard/ClipboardsModifier.swift

--- misc/clipboard/ClipboardParser.swift
+++ misc/clipboard/ClipboardParser.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
 
 class ClipboardParser {
-
+    
+    /**
+     *
+     */
+    class func getClipboardText()->String{
+        let pasteboard = NSPasteboard.generalPasteboard()
+        let theContent:String = pasteboard.stringForType(NSPasteboardTypeString)!
+        Swift.print("theContent: " + theContent)
+        return theContent
+    }
 }
--- misc/clipboard/ClipboardsModifier.swift
+++ misc/clipboard/ClipboardsModifier.swift
@@ -1,5 +1,12 @@
-import Foundation
+import Cocoa
 
 class ClipboardsModifier {
-
+    /**
+     *
+     */
+    class func setString(string:String){
+        let thePasteboard = NSPasteboard.generalPasteboard()
+        thePasteboard.clearContents()
+        thePasteboard.setString(string, forType: NSPasteboardTypeString)
+    }
 }
