44418ebd0d1849f87516cd20a22db0fbee11f926
Eonist <unicornhive@gmail.com>
2016-08-03 18:59:58 +0200

    Files modified: 1
    Modified 1 file:misc/clipboard/ClipboardParser.swift

--- misc/clipboard/ClipboardParser.swift
+++ misc/clipboard/ClipboardParser.swift
@@ -1,11 +1,17 @@
 import Cocoa
 
 class ClipboardParser {
-    
+    /**
+     * @Note remember to assert if the clipboard has the requested format first by using ClipboardAsserter.hasData etc
+     * @param textFormat ClipboardFormats.TEXT_FORMAT etc
+     */
+    class func data(textFormat:String) -> Any {
+        return Clipboard.generalClipboard.getData(textFormat);
+    }
     /**
      *
      */
-    class func getClipboardText()->String{
+    class func getString()->String{
         let pasteboard = NSPasteboard.generalPasteboard()
         let theContent:String = pasteboard.stringForType(NSPasteboardTypeString)!
         Swift.print("theContent: " + theContent)
