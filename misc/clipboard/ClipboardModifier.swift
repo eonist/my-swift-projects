dce700e4ce4dbbce8f2e4565bc78e05c2c805a11
Eonist <unicornhive@gmail.com>
2016-08-03 19:09:21 +0200

    Files modified: 1
    Modified 1 file:misc/clipboard/ClipboardModifier.swift

--- misc/clipboard/ClipboardModifier.swift
+++ misc/clipboard/ClipboardModifier.swift
@@ -1,5 +1,15 @@
 import Cocoa
-
+/**
+ * ClipboardFormats.TEXT_FORMAT	String	string data
+ * ClipboardFormats.HTML_FORMAT	String	HTML string data
+ * ClipboardFormats.URL_FORMAT	String	URL string (AIR only)
+ * ClipboardFormats.RICH_TEXT_FORMAT	ByteArray	Rich Text Format data
+ * ClipboardFormats.BITMAP_FORMAT	BitmapData	bitmap data (AIR only)
+ * ClipboardFormats.FILE_LIST_FORMAT	array of File	an array of files (AIR only)
+ * Custom format name	any	object reference and serialized clone
+ * Note: On Mac OS, when you set the format parameter to ClipboardFormats.URL_FORMAT, the URL is transferred only if it is a valid URL. Otherwise, the Clipboard object is emptied (and calling getData() returns null).
+ * @Note Clipboard.generalClipboard.setDataHandler can set a method and when the data is pasted this method is called and then it returns some data that ultimaltly ends up as the pasted data
+ */
 class ClipboardModifier {
     /**
      *
