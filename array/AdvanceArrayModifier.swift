bd90252a0a246e926d575c534d9ee74e5ee90d5b
Eonist <unicornhive@gmail.com>
2016-08-04 16:02:21 +0200

    Files modified: 1
    Modified 1 file:array/AdvanceArrayModifier.swift

--- array/AdvanceArrayModifier.swift
+++ array/AdvanceArrayModifier.swift
@@ -1,10 +1,6 @@
 import Foundation
 
-class AdvanceArrayModifier {
-    
-    //Continue here make a method that finds the highestDepth, so in [[4,3],[6,1],[6,2],[0,0],] the [6,2] should be the highestDepth. This is much more efficient than sorting an entire array and picking the index:0 after (The algo should use temp variables ) 
-    
-    
+class AdvanceArrayModifier {    
     /**
      * I think this method sorts each node in an array-tree to highest index like [[1,0][0,1]] -> [[0,1],[1,0]]  (<---I'm not sure if this example is correct)
      * NOTE: This method can't sort arrays which are partial, all indecies must exist from 0 to the end etc (It probably can sort partial arrays, but that requires the addAt method to support adding items to lengths that are beyond the length of the array, To add this support you need to make test to make sure its fail safe)
