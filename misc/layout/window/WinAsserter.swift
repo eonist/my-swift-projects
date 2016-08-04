61b4dbf43060e714c2a2d687b823ca161d432322
Eonist <unicornhive@gmail.com>
2016-08-04 21:56:49 +0200

    Files modified: 1
    Modified 1 file:misc/layout/window/WinAsserter.swift

--- misc/layout/window/WinAsserter.swift
+++ misc/layout/window/WinAsserter.swift
@@ -1,5 +1,10 @@
 import Cocoa
 
 class WinAsserter {
-    
+    /**
+     *
+     */
+    class func windowOfTypeExists() -> Bool{
+        return WinParser.windowsOfType(PositionWin).count > 0
+    }
 }
