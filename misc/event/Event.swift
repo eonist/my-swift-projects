89709df3593b8054940cbfdb7b8243ec7bf5b5a7
Eonist <unicornhive@gmail.com>
2016-08-05 10:25:00 +0200

    Files modified: 1
    Modified 1 file:misc/event/Event.swift

--- misc/event/Event.swift
+++ misc/event/Event.swift
@@ -6,7 +6,7 @@ class Event{
     static var update:String = "eventUpdate"/*Ideally I would name this change but apparently then subclasses can name their const the same*/
     var type:String/**/
     var origin:AnyObject/*origin sender of event, this could also be weak if you discover a memory leak*/
-    var immediate:AnyObject/*prev sender of event*/
+    var immediate:AnyObject/*previouse sender of event*/
     init(_ type:String = "", _ origin:AnyObject/*, _ immediate:AnyObject*/){
         self.type = type
         self.origin = origin
