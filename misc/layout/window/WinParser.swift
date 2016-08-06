7c696d91cc65e56000be61e0342a5431bd6ec403
Eonist <unicornhive@gmail.com>
2016-08-06 16:45:29 +0200

    Files modified: 1
    Modified 1 file:misc/layout/window/WinParser.swift

--- misc/layout/window/WinParser.swift
+++ misc/layout/window/WinParser.swift
@@ -36,7 +36,7 @@ class WinParser {
     /**
      * Returns an array of NSWindow of type T in the current app
      */
-    class func windowsOfType<T:NSWindow>(type:T.Type)-> [T] {
+    class func windowsOfType<T>(type:T.Type)-> [T] {
         var windows:Array<T> = []
         for window : NSWindow in NSApp.windows { if(window as? T != nil) {windows.append(window as! T)}}
         return windows
