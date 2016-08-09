03c0505dbc42d3bc0d30b509d41affe040b63f51
Eonist <unicornhive@gmail.com>
2016-08-09 20:20:45 +0200

    Files modified: 1
    Modified 1 file:geom/path/Path.swift

--- geom/path/Path.swift
+++ geom/path/Path.swift
@@ -1,4 +1,7 @@
 import Foundation
+/**
+ * NOTE: Compound paths needs to be closed
+ */
 class Path:IPath {
     var commands:Array<Int>/* = Array<Int>();*/// :TODO: does this need to be created? isnt it passed through argument
     var pathData:Array<CGFloat>/* = Array<CGFloat>();*/// :TODO: does this need to be created? isnt it passed through argument
