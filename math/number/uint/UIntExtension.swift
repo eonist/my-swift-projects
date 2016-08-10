bb404230db3d99cd35683cd911a6b7227fd69544
Eonist <unicornhive@gmail.com>
2016-08-10 21:35:42 +0200

    Files modified: 1
    Modified 1 file:math/number/uint/UIntExtension.swift

--- math/number/uint/UIntExtension.swift
+++ math/number/uint/UIntExtension.swift
@@ -3,4 +3,5 @@ import Foundation
 extension UInt {
     var cgFloat:CGFloat {return CGFloat(self)}//Convenince
     var int:Int{return Int(self)}//Convenince
+    var double:Double{return Double(self)}//Convenince
 }
