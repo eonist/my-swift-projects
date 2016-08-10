a792b03d6f8a1bca44c69662e3049d13b4269b14
Eonist <unicornhive@gmail.com>
2016-08-10 20:52:15 +0200

    Files modified: 1
    Modified 1 file:geom/path/Path.swift

--- geom/path/Path.swift
+++ geom/path/Path.swift
@@ -3,7 +3,7 @@ class Path:IPath {
     var commands:Array<Int>/* = Array<Int>();*/// :TODO: does this need to be created? isnt it passed through argument
     var pathData:Array<CGFloat>/* = Array<CGFloat>();*/// :TODO: does this need to be created? isnt it passed through argument
     var rotation:CGFloat;/*in degrees*/// :TODO: is this much used?// :TODO: if we can have roation why cant we have matrix?
-    var name:String = "";/*temp*/// :TODO: why is this temp?
+    var name:String = "";/*temp*/// :TODO: why is this temp? because it probably needs to be moved to DisplayPath etc
     init(_ commands:Array<Int> = Array<Int>(),_ pathData:Array<CGFloat> = Array<CGFloat>(),_ rotation:CGFloat = 0) {
         self.commands = commands
         self.pathData = pathData
