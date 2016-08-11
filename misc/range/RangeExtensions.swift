4b3b8af51b8406a5c34bae55578cc34f41a825c0
Eonist <unicornhive@gmail.com>
2016-08-11 12:29:00 +0200

    Files modified: 1
    Modified 1 file:misc/range/RangeExtensions.swift

--- misc/range/RangeExtensions.swift
+++ misc/range/RangeExtensions.swift
@@ -3,6 +3,7 @@ import Foundation
 extension Range {
     var start:Element {return self.startIndex}/*convenince*/
     var end:Element {return self.endIndex}/*convenince*/
+    var length:Element.Distance {return self.count}/*convenince*/
     init(_ start:Element,_ end:Element){/*Conveninence initializer*/
         self.init(start:start,end:end)
     }
