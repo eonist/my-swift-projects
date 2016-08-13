dd4496dd5f3c4b23a0bce665c155c2fc74494503
Eonist <unicornhive@gmail.com>
2016-08-13 18:27:31 +0200

    Files modified: 1
    Modified 1 file:geom/point/CGPointExtensions.swift

--- geom/point/CGPointExtensions.swift
+++ geom/point/CGPointExtensions.swift
@@ -29,6 +29,7 @@ extension CGPoint{
     /*parsers*/
     func distance(p:CGPoint) -> CGFloat { return CGPoint.distance(self,p) }//distance from self to p
     func polarPoint(radius:CGFloat, _ angle:CGFloat) -> CGPoint { return self + CGPoint.polarPoint(radius, angle) }//polarPoint from self
+    func polar(radius:CGFloat, _ angle:CGFloat) -> CGPoint { return polarPoint(radius, angle) }//convenience and legacy support
     func interpolate(p:CGPoint,_ scalar:CGFloat) -> CGPoint { return CGPoint.interpolate(self,p,scalar) }//interpolate from self to b by scalar
     func copy()->CGPoint{return CGPoint(self.x,self.y)}
     func clone()->CGPoint{return CGPoint(self.x,self.y)}
