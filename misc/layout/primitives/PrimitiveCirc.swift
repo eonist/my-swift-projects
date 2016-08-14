8c6607ceed3096da622133329af059fbb93157cb
Eonist <unicornhive@gmail.com>
2016-08-14 19:56:16 +0200

    Files modified: 1
    Modified 1 file:misc/layout/primitives/PrimitiveCirc.swift

--- misc/layout/primitives/PrimitiveCirc.swift
+++ misc/layout/primitives/PrimitiveCirc.swift
@@ -4,7 +4,7 @@ class PrimitiveCirc {
     var x:CGFloat = 0
     var y:CGFloat = 0
     var radius:CGFloat = 0
-    init(x:CGFloat = 0,_ y:CGFloat = 0, _ radius:CGFloat = 0) {
+    init(_ x:CGFloat = 0,_ y:CGFloat = 0, _ radius:CGFloat = 0) {
         self.x = x
         self.y = y
         self.radius = radius
