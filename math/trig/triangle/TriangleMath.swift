016a058e7b6cadbb4f1eac1352ef57e2456a5368
Eonist <unicornhive@gmail.com>
2016-08-14 19:40:53 +0200

    Files modified: 1
    Modified 1 file:math/trig/triangle/TriangleMath.swift

--- math/trig/triangle/TriangleMath.swift
+++ math/trig/triangle/TriangleMath.swift
@@ -27,28 +27,28 @@ import Foundation
  */
 class TriangleMath {
     /**
-    * The incenter  is the center of the incircle for a polygon or insphere for a polyhedron (when they exist). (The corresponding radius of the incircle or insphere is known as the inradius)
-    * @Note The incenter can be constructed as the intersection of angle bisectors. It is also the interior point for which distances to the sides of the triangle are equal.
-    * @Note It has trilinear coordinates 1:1:1, i.e., triangle center function
-    * @Note a characteristic is that the normal from each line to the center of the circle is the same
-    * @Note the circle will fit exactly inside the triangle
-    * @Note each line from p1,p2,p3 will hit the center of the circle (these lines are called angle bisectors)
-    * @Note Cr : incenter (center of circle tangent to all three sides)
-    * For a triangle with Cartesian vertices , , , the Cartesian coordinates of the incenter are given by:
-    * Equation:
-    * x = ((a*x1)+(b*x2)+(c*x3))/(a+b+c)
-    * y = ((a*y1)+(b*y2)+(c*y3))/(a+b+c)
-    * @param a: side a in the triangle (Side can also be defined as AB, or the side opposite of angle BAC)
-    * @param b: side b in the triangle (Side can also be defined as AC, or the side opposite of angle ABC)
-    * @param c: side c in the triangle (Side can also be defined as BC, or the side opposite of angle ACB)
-    * @param p1: the A point in the triangle (Angle can also be defined as BAC)
-    * @param p2: The B point in the triangle (Angle can also be defined as ABC)
-    * @param p3: The C point in the triangle (Angle can also be defined as ACB)
-    */
-    public static function inCenter(a:Number, b:Number, c:Number,p1:Point,p2:Point,p3:Point):Point {
-    var x:Number = ((a*p1.x)+(b*p2.x)+(c*p3.x))/(a+b+c);
-    var y:Number = ((a*p1.y)+(b*p2.y)+(c*p3.y))/(a+b+c);
-    return new Point(x,y);
+     * The incenter  is the center of the incircle for a polygon or insphere for a polyhedron (when they exist). (The corresponding radius of the incircle or insphere is known as the inradius)
+     * @Note The incenter can be constructed as the intersection of angle bisectors. It is also the interior point for which distances to the sides of the triangle are equal.
+     * @Note It has trilinear coordinates 1:1:1, i.e., triangle center function
+     * @Note a characteristic is that the normal from each line to the center of the circle is the same
+     * @Note the circle will fit exactly inside the triangle
+     * @Note each line from p1,p2,p3 will hit the center of the circle (these lines are called angle bisectors)
+     * @Note Cr : incenter (center of circle tangent to all three sides)
+     * For a triangle with Cartesian vertices , , , the Cartesian coordinates of the incenter are given by:
+     * Equation:
+     * x = ((a*x1)+(b*x2)+(c*x3))/(a+b+c)
+     * y = ((a*y1)+(b*y2)+(c*y3))/(a+b+c)
+     * @param a: side a in the triangle (Side can also be defined as AB, or the side opposite of angle BAC)
+     * @param b: side b in the triangle (Side can also be defined as AC, or the side opposite of angle ABC)
+     * @param c: side c in the triangle (Side can also be defined as BC, or the side opposite of angle ACB)
+     * @param p1: the A point in the triangle (Angle can also be defined as BAC)
+     * @param p2: The B point in the triangle (Angle can also be defined as ABC)
+     * @param p3: The C point in the triangle (Angle can also be defined as ACB)
+     */
+    class func inCenter(a:CGFloat, _ b:CGFloat, _ c:CGFloat,_ p1:CGPoint,_ p2:CGPoint,_ p3:CGPoint) -> CGPoint {
+        let x:CGFloat = ((a*p1.x)+(b*p2.x)+(c*p3.x))/(a+b+c)
+        let y:CGFloat = ((a*p1.y)+(b*p2.y)+(c*p3.y))/(a+b+c)
+        return CGPoint(x,y)
     }
     //----------------------------------
     //  Law of sin
