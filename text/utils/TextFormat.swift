5b1abd85228721c8495fcd78ad03790e0882618f
Eonist <unicornhive@gmail.com>
2016-08-17 14:10:30 +0200

    Files modified: 1
    Modified 1 file:text/utils/TextFormat.swift

--- text/utils/TextFormat.swift
+++ text/utils/TextFormat.swift
@@ -13,7 +13,7 @@ class TextFormat {
     var multiline:Bool = false
     var wordWrap:Bool = true
     var scrollable:Bool = true
-    var leading:CGFloat = 5/*line-spacing*///TODO:find apples default value by tracing it, could be something else than 10
+    var leading:CGFloat = 0/*line-spacing*///TODO:find apples default value by tracing it, could be something else than 10
     //autoSize can be implemented, check stackoverflow
     init(){}
     subscript(key: String) -> Any {
