65929d3cf512bb092b6c8d77f2b0e55cf08ed5e4
Eonist <unicornhive@gmail.com>
2016-08-17 13:54:52 +0200

    Files modified: 1
    Modified 1 file:text/utils/TextFieldModifier.swift

--- text/utils/TextFieldModifier.swift
+++ text/utils/TextFieldModifier.swift
@@ -41,9 +41,9 @@ class TextFieldModifier {
         textField.selectable = textFormat.selectable
         textField.backgroundColor = textFormat.background ? textFormat.backgroundColor : NSColor.clearColor()
         textField.drawsBackground = true//textFormat.background//<--this is a temp fix so that one can add or remove a background while the app is running, this should ideally be done automatically when the text is re-rendered, more research needed
-        textField.alignment = Utils.alignment(textFormat.align)//Left,Right,Justified,Natural,Center
+        textField.alignment = TextFieldParser.alignment(textFormat.align)//Left,Right,Justified,Natural,Center
         textField.textColor = textFormat.color
-        textField.font = Utils.font(textFormat.font,textFormat.size)
+        textField.font = TextFieldParser.font(textFormat.font,textFormat.size)
         textField.editable = textFormat.type == "input"
         textField.focusRingType = NSFocusRingType.None//<- implement suport for this if needed, Personally i dont like it. You can use the focus state instead and have your own focus style applied
         textField.bordered = textFormat.border//<--This doesnt work i Live Edit mode
@@ -64,30 +64,5 @@ class TextFieldModifier {
         textField.frame.width = width;/*SkinParser.width(this);*/
         textField.frame.height = height;/*SkinParser.height(this);*/
         //textField.setTextFormat(StylePropertyParser.textFormat(this));
-    }
-}
-private class Utils{
-    /**
-     * TODO: The paragraph style stuff doesnt seem to work. more research needed
-     */
-    class func font(fontName:String,_ size:CGFloat) -> NSFont{
-        //let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()//NSMutableParagraphStyle.defaultParagraphStyle()
-        //paragraphStyle.lineSpacing = 10.0//other paragraph options: paragraphSpacing,alignment,firstLineHeadIndent,lineBreakMode,minimumLineHeight,maximumLineHeight,baseWritingDirection,lineHeightMultiple,paragraphSpacingBefore
-        let fontDescriptor:NSFontDescriptor = NSFontDescriptor(fontAttributes: [NSFontNameAttribute:fontName/*,NSParagraphStyleAttributeName:paragraphStyle*/])//NSForegroundColorAttributeName:NSColor.redColor(),
-        let font = NSFont(descriptor: fontDescriptor, size: size)
-        return font!
-    }
-    /**
-     *
-     */
-    class func alignment(alignType:String) -> NSTextAlignment{
-        let align:NSTextAlignment
-        if(alignType == "left"){align = NSTextAlignment.Left}
-        else if(alignType == "right"){align = NSTextAlignment.Right}
-        else if(alignType == "center"){align = NSTextAlignment.Center}
-        else if(alignType == "justified"){align = NSTextAlignment.Justified}
-        else if(alignType == "natural"){align = NSTextAlignment.Natural}
-        else {fatalError("ALIGN TYPE NOT SUPPORTED: " + alignType)}
-        return align
     }
 }
