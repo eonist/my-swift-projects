39f66a12ab9cb00bada67f2d96d63f806e464670
Eonist <unicornhive@gmail.com>
2016-08-17 14:20:59 +0200

    Files modified: 1
    Modified 1 file:text/utils/TextFormat.swift

--- text/utils/TextFormat.swift
+++ text/utils/TextFormat.swift
@@ -66,10 +66,10 @@ extension TextFormat{
         let font:NSFont = TextFieldParser.font(self.font,self.size)
         let textColor:NSColor = self.color
         let textParagraph:NSMutableParagraphStyle = NSMutableParagraphStyle()
-        //textParagraph.lineSpacing = -25//self.leading/*this sets the space BETWEEN lines to 10points*/
+        textParagraph.lineSpacing = 20//self.leading/*this sets the space BETWEEN lines to 10points*/
         textParagraph.alignment = TextFieldParser.alignment(self.align)//Left,Right,Justified,Natural,Center
-        textParagraph.maximumLineHeight = 10.0/*this sets the MAXIMUM height of the lines to 12points*/
-        //textParagraph.minimumLineHeight = 1.0
+        textParagraph.maximumLineHeight = 5/*this sets the MAXIMUM height of the lines to 12points*/
+        textParagraph.minimumLineHeight = 1.0
         let attribs = [NSFontAttributeName:font,NSForegroundColorAttributeName:textColor,NSParagraphStyleAttributeName:textParagraph]
         let attrString:NSAttributedString = NSAttributedString.init(string: stringValue, attributes: attribs)
         return attrString
