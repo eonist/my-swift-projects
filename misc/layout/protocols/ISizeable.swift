c836e15dbe72352cab32e181d810c9237a5591f4
Eonist <unicornhive@gmail.com>
2016-08-18 16:50:53 +0200

    Files modified: 1
    Modified 1 file:misc/layout/protocols/ISizeable.swift

--- misc/layout/protocols/ISizeable.swift
+++ misc/layout/protocols/ISizeable.swift
@@ -10,7 +10,15 @@ protocol ISizeable:class {//<--new extends class
  */
 extension ISizeable{
     //var sizeable:ISizeable {get{return self as ISizeable}set{}}/*This method provides support for returning a direct pointer when casting to protocol, which swift doesnt do, it only provides an immutable reference, which is unusable when setting mutating variables via extensions*/
-    var width:CGFloat{get{return self.size.width} set{self.width = newValue}}
+    var width:CGFloat{
+        get{
+            if(self.size.width.isNaN){ fatalError("width can't be NaN")}
+            if(self.size.width.isNaN){fatalError("height can't be NaN")}
+            return self.size.width
+        } set{
+            self.width = newValue
+        }
+    }
     var height:CGFloat{get{return self.size.height} set{self.height = newValue}}
     /*mutating func setSize(width:CGFloat,height:CGFloat){
     self.width = width
