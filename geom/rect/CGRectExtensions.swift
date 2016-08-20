263f412ed79302136be2b56012fbdb82dcb2a112
Eonist <unicornhive@gmail.com>
2016-08-20 20:15:57 +0200

    Files modified: 1
    Modified 1 file:geom/rect/CGRectExtensions.swift

--- geom/rect/CGRectExtensions.swift
+++ geom/rect/CGRectExtensions.swift
@@ -1,7 +1,8 @@
 import Foundation
 //lots of great stuff here: https://github.com/nschum/SwiftCGRectExtensions/blob/master/CGRectExtensions/CGRectExtensions.swift
 /**
- * MOve the logic into CGRectModifier and CGRectParser and CGRectAsserter
+ * Move the logic into CGRectModifier and CGRectParser and CGRectAsserter
+ * For rect.isWithin(point) use: rect.contains(point)
  */
 extension CGRect {
     /**
