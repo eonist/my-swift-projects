80b617af0681f90910d87d97fcbc886aaf10d102
Eonist <unicornhive@gmail.com>
2016-08-25 11:26:55 +0200

    Files modified: 1
    Modified 1 file:math/number/int/IntExtension.swift

--- math/number/int/IntExtension.swift
+++ math/number/int/IntExtension.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Int {
     var cgFloat:CGFloat {return CGFloat(self)}//Convenince
     var float:Float {return Float(self)}//Convenince
+    var string:String{return String(self)}//Convenince
 }
