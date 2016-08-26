e82aa0fb17856ac8d17cd6f2e95dc25811481b5f
Eonist <unicornhive@gmail.com>
2016-08-26 18:28:34 +0200

    Files modified: 1
    Modified 1 file:geom/svg/style/SVGStyle.swift

--- geom/svg/style/SVGStyle.swift
+++ geom/svg/style/SVGStyle.swift
@@ -6,27 +6,27 @@ import Foundation
   * // :TODO: impliment stroke-dasharray
   */
 class SVGStyle {
-    var fill : Any?/*The fill color or SVGLinearGradient instance*/
-    var fillOpacity : CGFloat?/*A number ranging from 0.0 to 1.0; 0.0 is entirely transparent, 1.0 is entirely opaque.*/
-    var fillRule : String?/*not implimented yet, This attribute can have the values "nonzero" or "evenodd", which apply different rules for determining whether a point is inside or outside a shape. These rules generate different effects only when a shape has intersecting lines or "holes" in it. Details are in "Filling Polygons That Have Intersecting Lines" earlier in this chapter.*/
-    var strokeWidth : CGFloat?/*Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
-    var stroke : Any?/*TODO: Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
-    var strokeOpacity : CGFloat?
-    var strokeLineCap : String?
-    var strokeLineJoin : String?
-    var strokeMiterLimit : CGFloat?
+    var fill:Any?/*The fill color or SVGLinearGradient instance*/
+    var fillOpacity:CGFloat?/*A number ranging from 0.0 to 1.0; 0.0 is entirely transparent, 1.0 is entirely opaque.*/
+    var fillRule:String?/*not implimented yet, This attribute can have the values "nonzero" or "evenodd", which apply different rules for determining whether a point is inside or outside a shape. These rules generate different effects only when a shape has intersecting lines or "holes" in it. Details are in "Filling Polygons That Have Intersecting Lines" earlier in this chapter.*/
+    var strokeWidth:CGFloat?/*Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
+    var stroke:Any?/*TODO: Stroke needs to be of type Any? since it needs to support gradient stroke aswell*/
+    var strokeOpacity:CGFloat?
+    var strokeLineCap:String?
+    var strokeLineJoin:String?
+    var strokeMiterLimit:CGFloat?
     
     //TODO: all values should be nil, since we dont want none values by default as these may get exported, and if they didnt exist on the import they should exist on export. nil means no value, none or NaN means variable with none value
     
     init(_ fill:Any? = nil,_ fillOpacity:CGFloat? = nil,_ fillRule:String? = nil,_ strokeWidth:CGFloat? = nil,_ stroke:Any? = nil ,_ strokeOpacity:CGFloat? = nil,_ strokeLineCap:String? = nil,_ strokeLineJoin:String? = nil, _ strokeMiterLimit:CGFloat? = nil) {
-        self.fill = fill;
-        self.fillOpacity = fillOpacity;
-        self.fillRule = fillRule;
-        self.strokeWidth = strokeWidth;
-        self.stroke = stroke;
-        self.strokeOpacity = strokeOpacity;
-        self.strokeLineCap = strokeLineCap;
-        self.strokeLineJoin = strokeLineJoin;
-        self.strokeMiterLimit = strokeMiterLimit;
+        self.fill = fill
+        self.fillOpacity = fillOpacity
+        self.fillRule = fillRule
+        self.strokeWidth = strokeWidth
+        self.stroke = stroke
+        self.strokeOpacity = strokeOpacity
+        self.strokeLineCap = strokeLineCap
+        self.strokeLineJoin = strokeLineJoin
+        self.strokeMiterLimit = strokeMiterLimit
     }
 }
