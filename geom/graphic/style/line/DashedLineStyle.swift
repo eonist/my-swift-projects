0b6def2c7f573b22c769bee95dfee23947d09788
Eonist <unicornhive@gmail.com>
2016-08-27 12:10:50 +0200

    Files modified: 1
    Modified 1 file:geom/graphic/style/line/DashedLineStyle.swift

--- geom/graphic/style/line/DashedLineStyle.swift
+++ geom/graphic/style/line/DashedLineStyle.swift
@@ -1,5 +1,11 @@
-import Foundation
+import Cocoa
 
-class DashedLineStyle {
-
+class DashedLineStyle:LineStyle {
+    var phase: CGFloat/*start of the dash*/
+    var lengths: Array<CGFloat>/*gap,length,gap,length, etc*/
+    init(_ thickness: CGFloat, _ color: NSColor, _ lineCap: CGLineCap, _ lineJoin: CGLineJoin, _ miterLimit: CGFloat, _ phase:CGFloat, _ lengths:Array<CGFloat>) {
+        self.phase = phase
+        self.lengths = lengths
+        super.init(thickness, color, lineCap, lineJoin, miterLimit)
+    }
 }
