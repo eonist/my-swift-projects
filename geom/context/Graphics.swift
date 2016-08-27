e8cc3f84a15b11b8a59ae458d839dea93c0eb086
Eonist <unicornhive@gmail.com>
2016-08-27 12:10:50 +0200

    Files modified: 1
    Modified 1 file:geom/context/Graphics.swift

--- geom/context/Graphics.swift
+++ geom/context/Graphics.swift
@@ -74,7 +74,7 @@ public class Graphics{
         CGContextSetLineCap(context, lineCap)/*Butt is the default, CGLineCap.Square,CGLineCap.Round,CGLineCap.Butt*/
         CGContextSetLineJoin(context, lineJoin)/*Miter is the default, CGLineJoin.Round,CGLineJoin.Bevel,CGLineJoin.Miter*/
         CGContextSetMiterLimit(context, miterLimit)//The default Quartz miter limit is 10
-        //CGContextSetLineDash(context, 0, [2,2], 2)
+        CGContextSetLineDash(context, 0, [2,2], 2)
     }
     /**
      * Set the current  gradient line style
