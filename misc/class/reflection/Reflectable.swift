169ae9c607b42fd04b6fc284e38d4f756fcf5d73
Eonist <unicornhive@gmail.com>
2016-09-11 19:48:55 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/Reflectable.swift

--- misc/class/reflection/Reflectable.swift
+++ misc/class/reflection/Reflectable.swift
@@ -1,5 +1,36 @@
 import Foundation
 
-class Reflectable {
-
+protocol Reflectable {
+    func properties()->[(label:String,value:Any)]
+}
+/**
+ * NOTE: does not work with computed properties like: var something:String{return ""}
+ * NOTE: does not work with methods
+ * NOTE: only works with regular variables
+ * NOTE: some limitations with inheritance
+ * NOTE: works with struct and class
+ */
+extension Reflectable{
+    func properties()->[(label:String,value:Any)]{
+        var properties = [(label:String,value:Any)]()
+        Mirror(reflecting: self).children.forEach{
+            if let name = $0.label{
+                properties.append((name,$0.value))
+            }
+        }
+        return properties
+    }
+    //try to parse an instance into xml:
+    
+    //<Selectors>
+        //<Selector element="" id="">
+            //<states>
+                //<state>over</state>
+            //</states>
+            //<classIds></classIds>
+        //</Selector>
+    //</Selectors>
+    func xml(instance:Any)->XML{
+        return XML()
+    }
 }
