5c82c77db9eaff5a9b0965543ffee6ff56e4520e
Eonist <unicornhive@gmail.com>
2016-09-22 00:26:38 +0200

    Files modified: 1
    Modified 1 file:math/number/float/FloatExtensions.swift

--- math/number/float/FloatExtensions.swift
+++ math/number/float/FloatExtensions.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Float {
     var int:Int{return Int(self)}
     var cgFloat:CGFloat {return CGFloat(self)}//Convenince
+    var double:Double {return Double(self)}//Convenince
 }
