c49b624159f6910aad89841a72c3b7a2f36e7f45
Eonist <unicornhive@gmail.com>
2016-09-23 17:44:07 +0200

    Files modified: 2
    Modified 2 files:geom/context/utils/CGAffineTransformExtensions.swiftmisc/class/reflection/Reflectable.swift

--- geom/context/utils/CGAffineTransformExtensions.swift
+++ geom/context/utils/CGAffineTransformExtensions.swift
@@ -1,5 +1,5 @@
 import Foundation
-//typealias Matrix = CGAffineTransform/*Convenience*/
+typealias CGTransform = CGAffineTransform/*Convenience*/
 extension CGAffineTransform {
     /**
      *
@@ -14,10 +14,10 @@ extension CGAffineTransform {
         self = CGAffineTransformTranslate(self, x, y)
     }
     mutating func rotateAroundPoint(rotation:CGFloat, _ pivot:CGPoint){/*Convenience*/
-        self = CGAffineTransform.rotateAroundPoint(self, rotation,pivot)
+        self = CGTransform.rotateAroundPoint(self, rotation,pivot)
     }
     mutating func scaleFromPoint(xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint){/*Convenience*/
-        self = CGAffineTransform.scaleFromPoint(self, xScale,yScale,pivot)
+        self = CGTransform.scaleFromPoint(self, xScale,yScale,pivot)
     }
     /**
      * Scales a matrix and returns the result. The skewSide parameter lets the user determine which side to skew (right or bottom).
@@ -26,40 +26,40 @@ extension CGAffineTransform {
         //see old code for this implementation
     }
     
-    static func rotateAroundPoint(transform:CGAffineTransform,_ rotation:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
+    static func rotateAroundPoint(transform:CGTransform,_ rotation:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
         return CGAffineTransformParser.rotateAroundPoint(transform, rotation, pivot)
     }
-    static func scaleFromPoint(transform:CGAffineTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
+    static func scaleFromPoint(transform:CGTransform, _ xScale:CGFloat,_ yScale:CGFloat,_ pivot:CGPoint)->CGAffineTransform{
         return CGAffineTransformParser.scaleFromPoint(transform, xScale, yScale, pivot)
     }
-    static func translate(transform:CGAffineTransform,_ x:CGFloat,_ y:CGFloat)->CGAffineTransform{
+    static func translate(transform:CGTransform,_ x:CGFloat,_ y:CGFloat)->CGAffineTransform{
         return CGAffineTransformParser.translate(transform, x, y)
     }
-    static func transformAroundPoint(transform:CGAffineTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint)->CGAffineTransform{
+    static func transformAroundPoint(transform:CGTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint)->CGAffineTransform{
         return CGAffineTransformParser.transformAroundPoint(transform, scale, rotation, offset, pivot)
     }
-    static func transformWithPivot(inout transform:CGAffineTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint,_ initRotation:CGFloat = 0) {
+    static func transformWithPivot(inout transform:CGTransform, _ scale:CGPoint, _ rotation:CGFloat, _ offset:CGPoint, _ pivot:CGPoint,_ initRotation:CGFloat = 0) {
         CGAffineTransformModifier.transformWithPivot(&transform,scale,rotation,offset,pivot,initRotation)
     }
-    static func copy(transform:CGAffineTransform)->CGAffineTransform{
+    static func copy(transform:CGTransform)->CGTransform{
         return CGAffineTransformParser.copy(transform)
     }
     /**
      *
      */
-    mutating func concat(transformation:CGAffineTransform){
+    mutating func concat(transformation:CGTransform){
         self = CGAffineTransformParser.concat(self, transformation)
     }
     /**
      *
      */
-    static func concat(a:CGAffineTransform,_ b:CGAffineTransform)->CGAffineTransform{
+    static func concat(a:CGTransform,_ b:CGTransform)->CGTransform{
         return CGAffineTransformParser.concat(a, b)
     }
     /**
      *
      */
-    func copy()->CGAffineTransform{
+    func copy()->CGTransform{
         return CGAffineTransformParser.copy(self)
     }
     func transformPoint(point:CGPoint) -> CGPoint{
--- misc/class/reflection/Reflectable.swift
+++ misc/class/reflection/Reflectable.swift
@@ -13,17 +13,8 @@ extension NSColor:Reflectable{
         return (type:"NSColor",value:self.hexString)
     }
 }
-extension CGAffineTransform{
-    /*
-    public var a: CGFloat
-    public var b: CGFloat
-    public var c: CGFloat
-    public var d: CGFloat
-    public var tx: CGFloat
-    public var ty: CGFloat
-    */
+extension CGTransform:Reflectable{
     var reflection:(type:String,value:String) {
-        let transformIdentity:String =  //a,b,c,d,tx,ty
-        return (type:"CGTransform",value:"")
+        return (type:"CGTransform",value:self.identity)
     }
 }
