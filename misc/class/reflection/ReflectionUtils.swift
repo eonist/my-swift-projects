3b389a2eaef08d3623915f9b0e6e10e6c5b97a81
Eonist <unicornhive@gmail.com>
2016-09-24 18:01:44 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/ReflectionUtils.swift

--- misc/class/reflection/ReflectionUtils.swift
+++ misc/class/reflection/ReflectionUtils.swift
@@ -6,9 +6,13 @@ class ReflectionUtils {
      */
     class func toType(value:String, _ type:String) -> Any{
         switch(true) {
-            case type == "CGFloat":return value.cgFloat
             case type == "String":return value
-            case type == "Bool":return value
+            case type == "CGFloat":return value.cgFloat
+            case type == "Double":return value.double
+            case type == "Int":return value.int
+            case type == "UInt":return value.uint
+            case type == "Bool":return value.bool
+            case type == "DropShaodw":return value
             default : fatalError("TYPE NOT SUPPORTED: " + "\(type)" + " value: " + "\(value)")
         }
         
