454bc7eee2743571fa8094333ff88cda194e59c1
Eonist <unicornhive@gmail.com>
2016-09-24 18:33:35 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/UnWrappable.swift

--- misc/class/reflection/UnWrappable.swift
+++ misc/class/reflection/UnWrappable.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class UnWrappable {
-
+protocol UnWrappable {
+    //Continue here: what you do is that you extend the Types you want to unWrap. 
+    //And use inference similar to the way you made that cast method. 
+    //For more complex types see if they them selfs are UnWrappable. NICE!
 }
+
