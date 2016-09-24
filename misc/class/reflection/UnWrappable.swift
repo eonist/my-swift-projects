d1c2c906da53b415cdae29c76e9606b529987dca
Eonist <unicornhive@gmail.com>
2016-09-25 00:16:59 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/UnWrappable.swift

--- misc/class/reflection/UnWrappable.swift
+++ misc/class/reflection/UnWrappable.swift
@@ -21,31 +21,12 @@ extension UnWrappable{
      * NOTE: looks at the type and converts that the value into a type
      */
     func unWrap<T:UnWrappable>(xml:XML,_ key:String) -> T?{
-        
-        
         if(xml.childCount == 0 || xml.value.count == 0){//return nil if the node has no value and no subNodes
             return nil
         }
+        //let type:String = xml.firstNode(key)!["type"]!//<-- type not important anymore since we use T
         let value:String = xml.firstNode(key)!.value
-        
-        /*
-        let type:String = xml["type"]!
-        
-        switch(true) {
-        case type == "String":return value as? T
-        case type == "CGFloat":return value.cgFloat as? T
-        case type == "Double":return value.double as? T
-        case type == "Int":return value.int as? T
-        case type == "UInt":return value.uint as? T
-        case type == "Bool":return value.bool as? T
-        case type == "DropShadow":return value as? T
-            //if T is UNWrappable then (T as unwrappaple).unwrap(xml)
-            //TODO: add the types above to extensions instead
-        default : fatalError("TYPE NOT SUPPORTED: " + "\(type)" + " value: " + "\(value)")
-        }
-
-        */
-        return nil
+        return unWrap(value)//use T to your advantage when converting the value (A protocol extension switch, polymorphism)
     }
 }
 extension CGFloat:UnWrappable{
@@ -54,4 +35,20 @@ extension CGFloat:UnWrappable{
     }
 }
 
+/*
+old code:
 
+switch(true) {
+case type == "String":return value as? T
+case type == "CGFloat":return value.cgFloat as? T
+case type == "Double":return value.double as? T
+case type == "Int":return value.int as? T
+case type == "UInt":return value.uint as? T
+case type == "Bool":return value.bool as? T
+case type == "DropShadow":return value as? T
+//if T is UNWrappable then (T as unwrappaple).unwrap(xml)
+//TODO: add the types above to extensions instead
+default : fatalError("TYPE NOT SUPPORTED: " + "\(type)" + " value: " + "\(value)")
+}
+
+*/
