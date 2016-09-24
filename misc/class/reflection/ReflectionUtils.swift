4ff811440afb872e1fd601b33e193e5647d1c4fd
Eonist <unicornhive@gmail.com>
2016-09-24 17:57:50 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/ReflectionUtils.swift

--- misc/class/reflection/ReflectionUtils.swift
+++ misc/class/reflection/ReflectionUtils.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 class ReflectionUtils {
-
+    /**
+     *
+     */
+    class func toType(value:String, _ type:String){
+        switch(true) {
+            case type == "CGFloat":{return value.cgFloat}
+            
+            default : fatalError("CSSPropertyParser.property() THE: " + string + " PROPERTY IS NOT SUPPORTED")
+        }
+        
+    }
 }
