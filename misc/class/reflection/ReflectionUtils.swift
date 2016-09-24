c068ee1e7ee75276d4345c31784d00e633f75623
Eonist <unicornhive@gmail.com>
2016-09-24 18:33:35 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/ReflectionUtils.swift

--- misc/class/reflection/ReflectionUtils.swift
+++ misc/class/reflection/ReflectionUtils.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class ReflectionUtils {
     
-    //Continue here: what you do is that you extend the Types you want to unWrap. And use inference similar to the way you made that cast method. For more complex types see if they them selfs are UnWrappable. NICE!
+    
     
     /**
      * NOTE: looks at the type and converts that the value into a type
