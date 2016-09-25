a355cfbee75782780e4d26b8a5ae16609b94ca6d
Eonist <unicornhive@gmail.com>
2016-09-26 00:49:50 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/UnWrappable.swift

--- misc/class/reflection/UnWrappable.swift
+++ misc/class/reflection/UnWrappable.swift
@@ -4,21 +4,28 @@ import Cocoa
 //For more complex types see if they them selfs are UnWrappable. NICE!
 /**
  * NOTE: Using init with extension, protocol and classes is a bit troublesome. So a method is used instead of init
+ * NOTE: We are accessing the classtype and casting it as UNWrappable and then calling unwrap on the correct type (this requires usage of static methods, but its the most elegant)
  */
 protocol UnWrappable {
-    static func unWrap<T>(xml:XML) -> T?//add to custom classes
-    static func unWrap<T:UnWrappable>(xml:XML,_ key:String) -> T?//used to unWrap values
+    static func unWrap<T>(xml:XML) -> T?
+    static func unWrap<T:UnWrappable>(xml:XML,_ key:String) -> T?
     static func unWrap<T>(value:String) -> T?
 }
 extension UnWrappable{
-    
+    /**
+     * This would be similar to an init method (add to custom classes)
+     */
     static func unWrap<T>(xml:XML) -> T?{
         fatalError("must be overridden in subClass")
     }
+    /**
+     * non-nested values (NSColor,Int,CGFloat etc)
+     */
     static func unWrap<T>(value:String) -> T? {
         fatalError("must be overridden in subClass")
     }
     /**
+     * NOTE: used to unWrap nested values (DropShadow)
      * NOTE: looks at the type and converts that the value into a type
      */
     static func unWrap<T:UnWrappable>(xml:XML,_ key:String) -> T?{
@@ -28,6 +35,7 @@ extension UnWrappable{
         //let type:String = xml.firstNode(key)!["type"]!//<-- type not important anymore since we use T
         let value:String = xml.firstNode(key)!.value
         Swift.print("value: " + "\(value)")
+        
         
         return T.unWrap(value) //use T to your advantage when converting the value (A protocol extension switch, polymorphism)
     }
