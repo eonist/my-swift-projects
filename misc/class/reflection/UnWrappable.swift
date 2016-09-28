03e20595d32e62711803d753d35bcef889b061f8
Eonist <unicornhive@gmail.com>
2016-09-28 17:40:00 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/UnWrappable.swift

--- misc/class/reflection/UnWrappable.swift
+++ misc/class/reflection/UnWrappable.swift
@@ -89,7 +89,7 @@ extension Gradient:UnWrappable{
         let colors:Array<CGColor?> = unWrap(xml, "colors")
         let locations:Array<CGFloat?> = unWrap(xml, "locations")
         let rotation:CGFloat = unWrap(xml, "rotation")!
-        let transformation:CGTransform? = unWrap(xml, "transformation")
+        let transformation:CGTransform = unWrap(xml, "transformation")
         return Gradient(colors.flatMap{$0}, locations.flatMap{$0}, rotation) as? T
     }
 }
