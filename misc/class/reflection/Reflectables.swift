f7c045b509f4babbe9f45f371bdf1e139781ae54
Eonist <unicornhive@gmail.com>
2016-10-04 23:03:00 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/Reflectables.swift

--- misc/class/reflection/Reflectables.swift
+++ misc/class/reflection/Reflectables.swift
@@ -1,27 +1,5 @@
 import Cocoa
 
-extension IStyleProperty{
-    /**
-     * Converts xml to a Selector
-     */
-    static func styleProperty(xml:XML)->IStyleProperty{
-        //Swift.print("xml.string: " + "\(xml.string)")
-        /* let name:String = xml.firstNode("name")!.value
-        //Swift.print("element: " + "\(element)")
-        let valueXML:XML = xml.firstNode("value")!
-        Swift.print("valueXML.XMLString: " + "\(valueXML.XMLString)")
-        
-        
-        
-        let value:Any? = nil//ReflectionUtils.toType(valueXML)
-        
-        let depth:Int = xml.firstNode("depth")!.value.int
-        */
-        fatalError("this is out of order atm")
-        
-        //return StyleProperty(name,value,depth)
-    }
-}
 extension CGColorRef:Reflectable{
     var reflection:(type:String,value:String) {
         return (type:"CGColor",value:self.nsColor.hexString)
@@ -45,5 +23,27 @@ extension CGPoint:Reflectable{
 extension CGSize:Reflectable{
     var reflection:(type:String,value:String) {
         return (type:"CGSize",value:self.width.string + "," + self.height.string)
+    }
+}
+extension IStyleProperty{
+    /**
+     * Converts xml to a Selector
+     */
+    static func styleProperty(xml:XML)->IStyleProperty{
+        //Swift.print("xml.string: " + "\(xml.string)")
+        /* let name:String = xml.firstNode("name")!.value
+        //Swift.print("element: " + "\(element)")
+        let valueXML:XML = xml.firstNode("value")!
+        Swift.print("valueXML.XMLString: " + "\(valueXML.XMLString)")
+        
+        
+        
+        let value:Any? = nil//ReflectionUtils.toType(valueXML)
+        
+        let depth:Int = xml.firstNode("depth")!.value.int
+        */
+        fatalError("this is out of order atm, and its not needed as this class is handled automatically")
+        
+        //return StyleProperty(name,value,depth)
     }
 }
