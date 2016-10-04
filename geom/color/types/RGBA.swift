bedb598e184969d5f443ba6550d78dcf788b9643
Eonist <unicornhive@gmail.com>
2016-10-04 21:35:47 +0200

    Files modified: 1
    Modified 1 file:geom/color/types/RGBA.swift

--- geom/color/types/RGBA.swift
+++ geom/color/types/RGBA.swift
@@ -1,10 +1,10 @@
-import Foundation
+import Cocoa
 
 class RGBA {//<--TODO: could be a struct
     var r:UInt/*0-255*/
     var g:UInt/*0-255*/
     var b:UInt/*0-255*/
-    var a:UInt
+    var a:UInt/*0-100*/
     init(_ r:UInt = 0,_ g:UInt = 0,_ b:UInt = 0,_ a:UInt) {
         self.r = r
         self.g = g
@@ -16,4 +16,5 @@ extension RGBA{
     convenience init(_ r:CGFloat = 0,_ g:CGFloat = 0,_ b:CGFloat = 0, _ a:CGFloat = 0){
         self.init(r.uint,g.uint,b.uint,a.uint)
     }
+    var nsColor:NSColor{return NSColorParser.nsColor(r.cgFloat,g.cgFloat,b.cgFloat,a.cgFloat)}
 }
