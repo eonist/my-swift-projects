94d3cf941558fd9a9e93ba0deb008561ad67464d
Eonist <unicornhive@gmail.com>
2016-10-05 21:26:34 +0200

    Files modified: 1
    Modified 1 file:geom/color/utils/RGBAParser.swift

--- geom/color/utils/RGBAParser.swift
+++ geom/color/utils/RGBAParser.swift
@@ -3,25 +3,19 @@ import Cocoa
 class RGBAParser {
     /**
      * EXAMPLE: rgba(NSColor.redColor()).r//Outputs //1.0
-     * IMPORTANT: does this return 0-1.0 values
+     * IMPORTANT: this return 0-1.0 values
      */
     static func rgba(nsColor:NSColor)->RGBA{//<--was: (r:CGFloat,g:CGFloat,b:CGFloat,a:CGFloat)
         let ciColor:CIColor = CIColor(color: nsColor)!
-        return RGBA(ciColor.red,ciColor.green,ciColor.blue,ciColor.alpha)//<--you could just do: color.redComponent.uint etc
+        return RGBA(ciColor.red,ciColor.green,ciColor.blue,ciColor.alpha)//<--you could just do: color.redComponent.uint etc, nopp you can't, redComponent requires colorspace etc see: http://stackoverflow.com/questions/15682923/convert-nscolor-to-rgb/34115587#34115587
     }
     /**
      * Returns values like: FF0000FF (which is blue with 100% opacity)
      */
     static func hex(color:NSColor)->String{
-        let r:UInt = color.redComponent.uint*255
-        //Swift.print("r: " + "\(r)")
-        let g:UInt = color.greenComponent.uint*255
-        //Swift.print("g: " + "\(g)")
-        let b:UInt = color.blueComponent.uint*255
-        //Swift.print("b: " + "\(b)")
-        let a:UInt = color.alphaComponent.uint*255
+        let rgba:RGBAParser.rgba(color)
         //Swift.print("a: " + "\(a)")
-        let hex:String = HexParser.hexString(a,r,g,b)
+        let hex:String = HexParser.hexString(rgba.a,rgba.r,rgba.g,rgba.b)
         return hex
     }
     /**
