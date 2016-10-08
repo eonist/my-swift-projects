e922090c4df9b26533eacfcec56f6de10f6a59fc
Eonist <unicornhive@gmail.com>
2016-10-08 21:44:16 +0200

    Files modified: 1
    Modified 1 file:file/FileAsserter.swift

--- file/FileAsserter.swift
+++ file/FileAsserter.swift
@@ -1,7 +1,7 @@
 import Cocoa
 class FileAsserter{
     /**
-     * EXAMPLE: FileAsserter.exists("~/Desktop/del.txt".tildePath)//true or false
+     * EXAMPLE: FileAsserter.exists("~/Desktop/del.txt".tildePath)//true or false (remember to expand the tildePath)
      */
     class func exists(path: String) -> Bool {
         return NSFileManager().fileExistsAtPath(path)
