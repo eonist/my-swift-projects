c29bb2dd17b5d071606d8ea394862d4e77a015a0
Eonist <unicornhive@gmail.com>
2016-10-08 23:39:14 +0200

    Files modified: 1
    Modified 1 file:misc/class/reflection/UnWrappables.swift

--- misc/class/reflection/UnWrappables.swift
+++ misc/class/reflection/UnWrappables.swift
@@ -25,7 +25,7 @@ extension Bool:UnWrappable{
         return value.bool as? T
     }
 }
-extension NSColor:UnWrappable{
+extension NSColor:UnWrappable{/* we use 32 bit RGBA values when storing color data (This also stores the alpha value)*/
     static func unWrap<T>(value:String) -> T? {
         //Swift.print("NSColor.unWrap()")
         return NSColorParser.color(value) as? T//turns string values like: FF0000FF into NSColor blue
