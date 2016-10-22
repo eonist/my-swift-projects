8e5fd211ffd33edb04cb3f3391527e42d252b0ef
Eonist <unicornhive@gmail.com>
2016-10-22 13:19:49 +0200

    Files modified: 1
    Modified 1 file:misc/layout/utils/Resizer.swift

--- misc/layout/utils/Resizer.swift
+++ misc/layout/utils/Resizer.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 class Resizer {
-
+    func fit(size:CGSize, ratio:CGFloat){
+        //Scale to ratio:
+        let ratio:CGFloat = 4/3
+        Swift.print("ratio: " + "\(ratio)")
+        Swift.print("(w/h): " + "\((w/h))")
+        if((w/h) > ratio){
+            Swift.print("w is wider than ratio allows")
+            graphArea!.setSize(h*ratio,h)
+        }else if((w/h) < ratio){
+            Swift.print("h is taller than ratio allows")
+            graphArea!.setSize(w,w*ratio)
+        }else{
+            graphArea!.setSize(w,h)
+        }
+    }
 }
