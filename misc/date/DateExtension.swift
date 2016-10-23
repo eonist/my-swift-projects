d0fe63a3d582369af95e922c5d85f83da9e14b21
Eonist <unicornhive@gmail.com>
2016-10-23 21:32:53 +0200

    Files modified: 1
    Modified 1 file:misc/date/DateExtension.swift

--- misc/date/DateExtension.swift
+++ misc/date/DateExtension.swift
@@ -1,5 +1,29 @@
 import Foundation
 
-class DateExtension {
-
+extension NSDate {
+    /**
+     * beta
+     */
+    static func createDate(year:Int? = nil,_ month:Int? = nil,_ day:Int? = nil,_ hour:Int? = nil,_ minute:Int? = nil,_ second:Int? = nil)->NSDate?{
+        let calendar = NSCalendar.currentCalendar()
+        let components = NSDateComponents()
+        if(year != nil){components.year = year!}
+        if(month != nil){components.month = month!}
+        if(day != nil){components.day = day!}
+        if(hour != nil){components.hour = hour!}
+        if(second != nil){components.second = second!}
+        return calendar.dateFromComponents(components)
+    }
+    /**
+     * beta
+     */
+    var numOfDaysInMonth : Int{
+        //parts to NSDate:
+        let cal = NSCalendar.currentCalendar()
+        let days:NSRange = cal.rangeOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.Month, forDate: self)
+        //Swift.print("days: " + "\(days)")
+        //Swift.print("days.location: " + "\(days.location)")
+        //Swift.print("days.length: " + "\(days.length)")
+        return days.length
+    }
 }
