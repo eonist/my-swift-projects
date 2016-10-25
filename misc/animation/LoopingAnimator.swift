f452c6deaf95f13f661f9736f7f605a868514005
Eonist <unicornhive@gmail.com>
2016-10-25 16:00:17 +0200

    Files modified: 1
    Modified 1 file:misc/animation/LoopingAnimator.swift

--- misc/animation/LoopingAnimator.swift
+++ misc/animation/LoopingAnimator.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 class LoopingAnimator {
-
+    
+    //extends Animator
+    
+    //overide onFrame
+    
+    //reset currentFrame when at endFrame
+    
+    //duration
+    
+    //easing
+    
+    //
+    
+    //onFrame
+    
 }
