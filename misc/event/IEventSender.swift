1daac10b4a054371939df94137dce90392e20677
Eonist <unicornhive@gmail.com>
2016-10-28 13:51:05 +0200

    Files modified: 1
    Modified 1 file:misc/event/IEventSender.swift

--- misc/event/IEventSender.swift
+++ misc/event/IEventSender.swift
@@ -1,6 +1,6 @@
 import Foundation
 /**
- * All elements in the hierarchy that wants to propegate the event must implement this protocol 
+ * All elements in the hierarchy that want's to propegate the event must implement this protocol 
  */
 protocol IEventSender:class {
     var event: ((Event) -> ())?{get set}//maybe rename to eventRelay?
