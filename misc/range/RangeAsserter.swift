29afa0aeb54ca55b54fe04f50208d6b9eded3371
Eonist <unicornhive@gmail.com>
2016-10-29 14:14:57 +0200

    Files modified: 1
    Modified 1 file:misc/range/RangeAsserter.swift

--- misc/range/RangeAsserter.swift
+++ misc/range/RangeAsserter.swift
@@ -11,7 +11,8 @@ class RangeAsserter{
     }
     /**
      * Asserts if @param value is within the @param range
-     * // :TODO: a potential bug is that <= max isnt correct, if it is max then its not within, if you think about how integers work, but this may also be correct since if a number range is asserted it may need to also include the max, for now use contained if you deal with integers
+     * TODO: Add some examples
+     * // :TODO: a potential bug is that <= max isn't correct, if it is max then its not within, if you think about how integers work, but this may also be correct since if a number range is asserted it may need to also include the max, for now use contained if you deal with integers
      */
     class func within<T:Comparable>(range:Range<T>,_ number:T)->Bool {
         return (number <= RangeParser.max(range) && number >= RangeParser.min(range))
