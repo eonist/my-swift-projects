689fa40e01bd245f1b4870a8a9702901624de2d8
Eonist <unicornhive@gmail.com>
2016-11-02 16:26:52 +0100

    Files modified: 1
    Modified 1 file:misc/data/utils/DataProviderModifier.swift

--- misc/data/utils/DataProviderModifier.swift
+++ misc/data/utils/DataProviderModifier.swift
@@ -1,4 +1,11 @@
 import Foundation
 class DataProviderModifier {
+    /**
+    * Adds an item to @param dataProvider at an array index @param index
+    */
+
+    static func addItemAtIndex(){
     
+    }
+    //, see legacy code
 }
