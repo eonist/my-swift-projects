8d1d9d07208fce8f1d68bb1ecd7db2e0864bb45f
Eonist <unicornhive@gmail.com>
2016-11-03 22:47:50 +0100

    Files modified: 1
    Modified 1 file:misc/animation/motion/RubberBand.swift

--- misc/animation/motion/RubberBand.swift
+++ misc/animation/motion/RubberBand.swift
@@ -3,6 +3,8 @@ import Cocoa
  * NOTE: this rubberBand tween is cheating a bit. The perfect way to implement this would be to add a half circle easing curve
  * NOTE: I think essentialy this is a SpringSolver. YOu can find an example of the SpringSolver in books and also in facebook pop
  * TODO: move these clases into the motion folder in swift-utils
+ * PARAM: frame: represents the visible part of the content //TODO: could be ranmed to maskRect
+ * PARAM: itemsRect: represents the total size of the content //TODO: could be ranmed to contentRect
  */
 class RubberBand:Mover{
     let epsilon:CGFloat = 0.15/*twips 20th of a pixel*/
