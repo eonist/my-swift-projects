017a73a9d4f871b92cda73e099f95e398dd25195
Eonist <unicornhive@gmail.com>
2016-11-04 17:03:36 +0100

    Files modified: 1
    Modified 1 file:misc/data/utils/DataProviderModifier.swift

--- misc/data/utils/DataProviderModifier.swift
+++ misc/data/utils/DataProviderModifier.swift
@@ -4,7 +4,8 @@ class DataProviderModifier {
      * Adds an item to @param dataProvider at an array index @param index
      */
     static func addItemAtIndex(dataProvider:DataProvider,_ index:Array<Int>,_ item:Dictionary<String,String>)->DataProvider{
-        //see legacy code for instructions on how to inplement this
-        return DataProvider()
+        //see legacy code for instructions on how to implement this
+        fatalError("not implemented yet")
+        //return DataProvider()
     }
 }
