985f14dc08e2304a24750f772bf5e65f527252ba
Eonist <unicornhive@gmail.com>
2016-11-21 01:43:41 +0100

    Files modified: 1
    Modified 1 file:geom/svg/path/utils/SVGPathParser.swift

--- geom/svg/path/utils/SVGPathParser.swift
+++ geom/svg/path/utils/SVGPathParser.swift
@@ -9,8 +9,8 @@ class SVGPathParser {
 	 * @example SVGPathDataUtils.pathData("M10,10pxH110");//commands: M,H parameters: 10,10,110
 	 */
 	class func pathData(data:String)->SVGPathData {
-		var parameters:Array<CGFloat> = [];
-		var commands:Array<String> = [];
+		var parameters:Array<CGFloat> = []
+		var commands:Array<String> = []
         ///(?P<cmnd>[MmLlHhVvCcSsQqTtZzAa])(?P<params>[\d\.\-\s\,px]*?)(?=[MmLlHhVvCcSsQqTtZzAa]|$)/g;
 		let pattern:String = "([MmLlHhVvCcSsQqTtZzAa])([\\d\\.\\-\\s\\,px]*?)(?=[MmLlHhVvCcSsQqTtZzAa]|$)"//Capturing groups: ?P<cmnd>,?P<params>
         let matches = data.matches(pattern)
@@ -23,13 +23,13 @@ class SVGPathParser {
             commands.append(cmnd);//command()
             let params = (data as NSString).substringWithRange(match.rangeAtIndex(2))//capturing group 2
             //Swift.print("params: >" + params+"<");
-            let array:Array<CGFloat> = SVGPathParser.parameters(params);
+            let array:Array<CGFloat> = SVGPathParser.parameters(params)
             //Swift.print("pathData.parameters: " + array);
             parameters += array//<---this is the same as concat
         }
 //		Swift.print("pathData.commands: " + commands);
 //		Swift.print("pathData.parameters: " + parameters);
-		return SVGPathData(commands,parameters);
+		return SVGPathData(commands,parameters)
 	}
     /**
 	 * Returns an array comprised of values "sans" its prefix and or suffix
@@ -57,7 +57,7 @@ class SVGPathParser {
 	 */
 	class func end(path:SVGPath, index:Int)->CGPoint? {// :TODO: rename to position?!?
 		let command:String = path.commands[index].lowercaseString
-		var parameters:Array = SVGPathDataParser.pathData(path, index);
+		var parameters:Array = SVGPathDataParser.pathData(path, index)
         if(command == "m" || command == "l") {return CGPoint(parameters[0],parameters[1])}
         else if(command == "c") {return CGPoint(parameters[2],parameters[3])}
         else { return nil}//Arc4Parser.end(PathDataParser.arc(pathData));/*PathCommand.ARC_TO*/
@@ -69,60 +69,60 @@ class SVGPathParser {
 	 * // :TODO: cubic and quad curve may have more params and they may have t and s  impliment this
 	 */
 	class func points(path:SVGPath)->Array<CGPoint> {
-		var commands:Array = path.commands;
-		var params:Array = path.parameters;
-		var positions:Array<CGPoint> = [];
+		var commands:Array = path.commands
+		var params:Array = path.parameters
+		var positions:Array<CGPoint> = []
 		var i:Int = 0;/*parameterIndex*/
-		var prevP:CGPoint = CGPoint();
+		var prevP:CGPoint = CGPoint()
 		for (var e : Int = 0; e < commands.count; e++) {
-			let command:String = commands[e];
-			let isLowerCase:Bool = StringAsserter.lowerCase(command);
-			var pos:CGPoint = isLowerCase ? prevP.copy() : CGPoint();
+			let command:String = commands[e]
+			let isLowerCase:Bool = StringAsserter.lowerCase(command)
+			var pos:CGPoint = isLowerCase ? prevP.copy() : CGPoint()
 			switch(command.lowercaseString){
 				case SVGPathCommand.m,SVGPathCommand.l: //lineTo,moveTo
-					pos += CGPoint(params[i+0],params[i+1]);
-					i += 2;
+					pos += CGPoint(params[i+0],params[i+1])
+					i += 2
 					break;
 				case SVGPathCommand.h: //horizontalLineTo
-					pos += CGPoint(params[i],isLowerCase ? 0 : prevP.y);
-					i++;
+					pos += CGPoint(params[i],isLowerCase ? 0 : prevP.y)
+					i++
 					break;
 				case SVGPathCommand.v: //verticalLineTo
-					pos += CGPoint(isLowerCase ? 0 : prevP.x,params[i]);
-					i++;
+					pos += CGPoint(isLowerCase ? 0 : prevP.x,params[i])
+					i++
 					break;
 				case SVGPathCommand.c:/*cubicCurveTo*/ // :TODO: this isnt tested!!
-					pos += CGPoint(params[i+4],params[i+5]);
-					i += 6;
+					pos += CGPoint(params[i+4],params[i+5])
+					i += 6
 					break;
 				case SVGPathCommand.s://smooth Cubic curve command
-					pos += CGPoint(params[i+2],params[i+3]);
-					i += 4;
+					pos += CGPoint(params[i+2],params[i+3])
+					i += 4
 					break;
 				case SVGPathCommand.q://quadCurveTo
-					pos += CGPoint(params[i+2],params[i+3]);
-					i += 4;
+					pos += CGPoint(params[i+2],params[i+3])
+					i += 4
 					break;
 				case SVGPathCommand.t://smooth quadratic curve command
-					pos += CGPoint(params[i],params[i+1]);
-					i += 2;
+					pos += CGPoint(params[i],params[i+1])
+					i += 2
 					break;
                 default:break;
 			}
 			positions.append(pos);
 			if(e < commands.count-1 /*&& StringAsserter.lowerCase(commands[i+1])*/) {// :TODO: check for z?
-				prevP = pos.copy();
+				prevP = pos.copy()
 			}
 		}
 //		Swift.print("positions: " + positions);
-		return positions;
+		return positions
 	}
 	/**
 	 * Returns an Rectangle instance with points derived from @param path
 	 * // :TODO: arcs and curve bounding boxes will be dificult,but you have code for this, see notebooks
 	 */
 	class func rectangle(path:SVGPath) -> CGRect {
-		let points:Array<CGPoint> = SVGPathParser.points(path);
-		return PointParser.rectangle(points);
+		let points:Array<CGPoint> = SVGPathParser.points(path)
+		return PointParser.rectangle(points)
 	}
 }
