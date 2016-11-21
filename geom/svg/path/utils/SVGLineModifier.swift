9fe4ca3fdd1d1d348c9fb52bb92a97241350abe4
Eonist <unicornhive@gmail.com>
2016-11-21 01:41:53 +0100

    Files modified: 2
    Modified 2 files:geom/svg/path/utils/SVGLineModifier.swiftgeom/svg/path/utils/SVGPathModifier.swift

--- geom/svg/path/utils/SVGLineModifier.swift
+++ geom/svg/path/utils/SVGLineModifier.swift
@@ -4,8 +4,8 @@ class SVGLineModifier {
 	 * Scales the SVGLine element at @param pivot with @param scalar (0-1)
 	 */
 	class func scale(line:SVGLine, _ pivot:CGPoint, _ scale:CGPoint){
-		let p1:CGPoint = PointModifier.scale(line.p1.copy(), pivot, scale);
-		let p2:CGPoint = PointModifier.scale(line.p2.copy(), pivot, scale);
+		let p1:CGPoint = PointModifier.scale(line.p1.copy(), pivot, scale)
+		let p2:CGPoint = PointModifier.scale(line.p2.copy(), pivot, scale)
 		line.p1 = p1
 		line.p2 = p2
 	}
