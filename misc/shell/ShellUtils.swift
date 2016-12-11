614a0553aeb937a910e8b29f50b2c124412ba6d5
Eonist <unicornhive@gmail.com>
2016-12-11 22:28:47 +0100

    Files modified: 1
    Modified 1 file:misc/shell/ShellUtils.swift

--- misc/shell/ShellUtils.swift
+++ misc/shell/ShellUtils.swift
@@ -29,10 +29,9 @@ class ShellUtils{
         
         var arguments = input.componentsSeparatedByString(" ")
         Swift.print("arguments.count: " + "\(arguments.count)")
-        arguments = arguments.map {$0.encode()!.decode()!}/*<--the encode part was neccesary*/
-        arguments.forEach{
-            Swift.print("$0: " + "\($0)")
-        }
+        arguments = arguments.map {$0.encode()!.decode()!}/*<--the encode part was necessary to allow % chars*/
+        //arguments.forEach{Swift.print("$0: " + "\($0)")}
+            
         let task = NSTask()
         task.currentDirectoryPath = cd
         task.launchPath = "/usr/bin/env"
