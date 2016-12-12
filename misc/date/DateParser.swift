f2c9d6378c1751feb86c1694b77664c13f4d1c31
Eonist <unicornhive@gmail.com>
2016-12-12 13:42:59 +0100

    Files modified: 1
    Modified 1 file:misc/date/DateParser.swift

--- misc/date/DateParser.swift
+++ misc/date/DateParser.swift
@@ -4,6 +4,7 @@ class DateParser {
     /**
      * Returns relative time like: 2y, 11m, 3w, 4h, 2d, 5m,  3sec (aka: time ago)
      * NOTE: the approche bellow is Naive. There are more elegant ways of doing this. But this offers more customization in the future.
+     * EXAMPLE: DateParser.relativeTime(NSDate(),today.offsetByDays(-3))//Output: [(3,"d")] 
      */
     static func relativeTime(a:NSDate,_ b:NSDate) -> [(value:Int,type:String)]{
         let calendar = NSCalendar.currentCalendar()
