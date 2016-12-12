b9cb1b30890d7e6359855b24179a68d7a1d6246f
Eonist <unicornhive@gmail.com>
2016-12-12 13:36:55 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:misc/date/DateExtension.swift

--- misc/date/DateExtension.swift
+++ misc/date/DateExtension.swift
@@ -61,32 +61,6 @@ extension NSDate {
         return convertedDate
     }
     /**
-     * Returns relative time like: 2y, 11m, 3w, 4h, 2d, 5m,  3sec (aka: time ago)
-     * NOTE: the approche bellow is Naive. There are more elegant ways of doing this. But this offers more customization in the future.
-     */
-    func relativeTime(a:NSDate,_ b:NSDate)->[(value:Int,type:String)]{
-        let calendar = NSCalendar.currentCalendar()
-        let aComponents = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: a)
-        let bComponents = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: b)
-        let yearsAgo:Int = aComponents.year - bComponents.year//cur year - year = 0
-        let monthsAgo:Int = aComponents.month - bComponents.month//cur month - month = 0
-        let daysAgo:Int = aComponents.day - bComponents.day//cur day - day = 0
-        var weeksAgo:Int = 0
-        if(daysAgo >= 7){
-            weeksAgo = daysAgo / 7
-        }
-        let hoursAgo:Int = aComponents.hour - bComponents.hour//cur hour - hour = 0
-        let minutesAgo:Int = aComponents.minute - bComponents.minute//cur min - min = 0
-        let secondsAgo:Int = aComponents.second - bComponents.second//cur sec - sec = 4
-        //result: 4s ago
-        let timeUnits:[(value:Int,type:String)] = [(yearsAgo,"y"),(monthsAgo,"m"),(weeksAgo,"w"),(daysAgo,"d"),(hoursAgo,"h"),(minutesAgo,"m"),(secondsAgo,"s")]
-        var relativeTime:[(value:Int,type:String)] = []
-        timeUnits.forEach{
-            if($0.value > 0){relativeTime.append($0)}
-        }
-        return relativeTime
-    }
-    /**
      * NSDate().offsetByDays(-3)//an NSDate instance that is 3 days ago
      */
     func offsetByDays(days:Int)->NSDate{
