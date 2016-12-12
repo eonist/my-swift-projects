aa94f891afd3e045d903ff7331c6829566e88511
Eonist <unicornhive@gmail.com>
2016-12-12 19:43:43 +0100

    Files modified: 1
    Modified 1 file:misc/shell/git/log/GitLogParser.swift

--- misc/shell/git/log/GitLogParser.swift
+++ misc/shell/git/log/GitLogParser.swift
@@ -27,4 +27,49 @@ class GitLogParser {
         let date:NSDate = NSDate.createDate(year.int, month.int, day.int, hour.int, min.int, sec.int)!
         return date
     }
+    /**
+     * EXAMPLE: parts("Author:Eonist \n Date:2015-12-03 16:59:09 +0100 \n Subject:'abc' \n Body:'123'")//Output:
+     */
+    
+    "Author:Eonist" + "\n"
+    testString += "Date:2015-12-03 16:59:09 +0100" + "\n"
+    testString += "Subject:'Files modified: 1'" + "\n"
+    testString += "Body:'" + "\n"
+    testString += "" + "\n"
+    testString += "Modified 1 file:" + "\n"
+    testString += "README.md" + "\n"
+    testString += "'"
+
+    static func parts(testString:String) -> (author:String,date:String,subject:String,body:String){
+        let firstIdx:Int = testString.indexOf("\n")
+        Swift.print("firstIdx: " + "\(firstIdx)")
+        let firstPart:String = testString.subString(0,firstIdx)
+        Swift.print("firstPart: " + "\(firstPart)")
+        //Swift.print("firstPart: " + "\(firstPart)")
+        
+        let secondIdx:Int = firstIdx+1 + testString.subString(firstIdx+1,testString.count).indexOf("\n")
+        Swift.print("secondIdx: " + "\(secondIdx)")
+        
+        let secondPart:String = testString.subString(firstIdx+1,secondIdx)
+        Swift.print("secondPart: " + "\(secondPart)")
+        
+        let thirdIdx:Int = secondIdx+1 + testString.subString(secondIdx+1,testString.count).indexOf("\n")
+        Swift.print("thirdIdx: " + "\(thirdIdx)")
+        
+        let thirdPart:String = testString.subString(secondIdx+1,thirdIdx)
+        Swift.print("thirdPart: " + "\(thirdPart)")
+        
+        let fourthPart:String = testString.subString(thirdIdx+1,testString.count)
+        Swift.print("fourthPart: " + "\(fourthPart)")
+        
+        var commitData = (author:firstPart,date:secondPart,subject:thirdPart,body:fourthPart)
+        commitData.author = commitData.author.subString("Author:".count, commitData.author.count)
+        commitData.date = commitData.date.subString("Date:".count, commitData.date.count)
+        commitData.subject = commitData.subject.subString("Subject:".count, commitData.subject.count)
+        commitData.body = commitData.body.subString("Body:".count, commitData.body.count)
+        Swift.print("commitData.author: " + "\(commitData.author)")
+        Swift.print("commitData.date: " + "\(commitData.date)")
+        Swift.print("commitData.subject: " + "\(commitData.subject)")
+        Swift.print("commitData.body: " + "\(commitData.body)")
+    }
 }
