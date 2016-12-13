a4f247d90b00de76be57cefcc8c4df7322ff3b4e
Eonist <unicornhive@gmail.com>
2016-12-13 15:48:40 +0100

    Files modified: 1
    Modified 1 file:misc/data/DataProvider.swift

--- misc/data/DataProvider.swift
+++ misc/data/DataProvider.swift
@@ -166,6 +166,9 @@ extension DataProvider{
      */
     func sortOn(key:String,_ ascending:Bool = true){/*names:AnyObject, _ options:Int = 0,_ args:AnyObject...*/
         fatalError("not implemented yet")
+        self.items.sortInPlace { (a, b) -> Bool in
+            return a.date < b.date
+        }
         //self.items.sortOn(names, options,args);
         //onEvent(DataProviderEvent(DataProviderEvent.sort, /*[_items],*/ 0,self.items.count,self));
     }
