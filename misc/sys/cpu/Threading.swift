f507e96a45607937e290ee1573ebfddc5c284e4f
Eonist <unicornhive@gmail.com>
2016-12-26 14:07:10 +0100

    Files modified: 1
    Modified 1 file:misc/sys/cpu/Threading.swift

--- misc/sys/cpu/Threading.swift
+++ misc/sys/cpu/Threading.swift
@@ -1,5 +1,8 @@
 import Foundation
-
-class Threading {
-
-}
+/**
+ * Makes working with threads, queues, serial and concurrent tasks easier by using shorter/less cryptic names
+ */
+var bgQueue = {return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)}()//swift 3-> let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+//there is also these: DISPATCH_QUEUE_PRIORITY_DEFAULT,DISPATCH_QUEUE_PRIORITY_HIGH,DISPATCH_QUEUE_PRIORITY_LOW
+var mainQueue = {return dispatch_get_main_queue()}()
+func async(queue:dispatch_queue_t , _ block:dispatch_block_t) -> Void {return dispatch_async(queue, block)}
