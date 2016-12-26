aba0b57d923c36b2c2fb238f1bf497dd659fcd9b
Eonist <unicornhive@gmail.com>
2016-12-26 18:42:11 +0100

    Files modified: 1
    Modified 1 file:misc/date/Timer.swift

--- misc/date/Timer.swift
+++ misc/date/Timer.swift
@@ -25,13 +25,19 @@ class Timer {
      *
      */
     func start(){
-        timer = NSTimer.scheduledTimerWithTimeInterval(interval/*0.01*/, target: target, selector: selector, userInfo: nil, repeats: repeats)
+        timer = NSTimer.scheduledTimerWithTimeInterval(interval/*0.01*/, target: self, selector: "update", userInfo: nil, repeats: repeats)
     }
     /**
      *
      */
     func stop(){
         if(timer != nil){timer!.invalidate()}
+    }
+    /**
+     *
+     */
+    func update(){
+        Swift.print("tick")
     }
 }
 
