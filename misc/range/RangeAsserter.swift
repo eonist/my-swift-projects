ccf9df199e2b81e63d5ad5f2b2302a11d6edf95e
Eonist <unicornhive@gmail.com>
2017-01-03 12:57:57 +0100

    Files modified: 1
    Modified 1 file:misc/range/RangeAsserter.swift

--- misc/range/RangeAsserter.swift
+++ misc/range/RangeAsserter.swift
@@ -15,20 +15,20 @@ class RangeAsserter{
      * TODO: a potential bug is that <= max isn't correct, if it is max then its not within, if you think about how integers work, but this may also be correct since if a number range is asserted it may need to also include the max, for now use contained if you deal with integers
      */
     static func within<T:Comparable>(range:Range<T>,_ number:T)->Bool {
-        return (number <= RangeParser.max(range) && number >= RangeParser.min(range))
+        return number <= RangeParser.max(range) && number >= RangeParser.min(range)
     }
     /**
      * NOTE: this method is supplimentary to the within method, concerning the "max" problem
      * NOTE: another name for this could be absolutlyWithin
      */
     static func contained<T:Comparable>(range:Range<T>,_ number:T)->Bool {
-        return (number < RangeParser.max(range) && number >= RangeParser.min(range))
+        return number >= RangeParser.min(range) && number < RangeParser.max(range)
     }
     /**
      * Asserts if @param a overlaps @param b
      */
     static func overlaps<T:Comparable>(a:Range<T>,_ b:Range<T>)->Bool {
-        return (RangeAsserter.equals(a,b) || contains(a,b) || contains(b,a) || within(a,b.start) || within(a,b.end))
+        return RangeAsserter.equals(a,b) || contains(a,b) || contains(b,a) || within(a,b.start) || within(a,b.end)
     }
     /**
      * Asserts if PARAM: a contains PARAM: a or PARAM: b contains PARAM: a
