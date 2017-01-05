1e38cf7c1e341fc9207671b444ae432b9cccd90a
Eonist <unicornhive@gmail.com>
2017-01-05 22:11:45 +0100

    Files modified: 1
    Modified 1 file:geom/rect/CGRectAsserter.swift

--- geom/rect/CGRectAsserter.swift
+++ geom/rect/CGRectAsserter.swift
@@ -3,22 +3,22 @@ class CGRectAsserter {
     /**
      *
      */
-    class func containsOrIntersectsLine(rectangle:CGRect,_ line:Line) -> Bool {
+    struct func containsOrIntersectsLine(rectangle:CGRect,_ line:Line) -> Bool {
         return containsLine(rectangle, line) || intersectsLine(rectangle, line)
     }
     /**
-     * Asserts if @param rectangle contains the line @param p1 to @param p2
+     * Asserts if PARAM: rectangle contains the line PARAM: p1 to PARAM: p2
      */
-    class func containsLine(rectangle:CGRect,_ line:Line) -> Bool {
+    struct func containsLine(rectangle:CGRect,_ line:Line) -> Bool {
         return rectangle.contains(line.p1) && rectangle.contains(line.p2)
     }
     /**
-     * Asserts if @param rectangle intersects the line @param p1 to @param p2
-     * @Note to assert if a line intersects or is contained within a rectangle use both of the methods in this class named intersectsLine and containsLine
-     * @Note the for loop does not call RectangleParser.sides more than once
-     * @example RectangleAsserter.intersectsLine(new Rectangle(200,200,500,500),new PrimitiveLine(new Point(0,0), new Point(60,60)));//false
+     * Asserts if PARAM: rectangle intersects the line PARAM: p1 to PARAM: p2
+     * NOTE: to assert if a line intersects or is contained within a rectangle use both of the methods in this class named intersectsLine and containsLine
+     * NOTE: the for loop does not call RectangleParser.sides more than once
+     * EXAMPLE: RectangleAsserter.intersectsLine(CGRect(200,200,500,500),PrimitiveLine(CGPoint(0,0), CGPoint(60,60)));//false
      */
-    class func intersectsLine(rectangle:CGRect,_ line:Line) -> Bool {
+    struct func intersectsLine(rectangle:CGRect,_ line:Line) -> Bool {
         for side:Line in CGRectParser.sides(rectangle) {if(LineAsserter.intersects(side, line)) {return true}}
         return false
     }
