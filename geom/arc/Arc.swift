45cab3f9e081da5affd54457504a5c55a6ed828a
Eonist <unicornhive@gmail.com>
2017-01-05 19:05:39 +0100

    Files modified: 1
    Modified 1 file:geom/arc/Arc.swift

--- geom/arc/Arc.swift
+++ geom/arc/Arc.swift
@@ -2,12 +2,12 @@ import Foundation
 
 class Arc:IArc{
     /**
-     * @Note the largeFlag is always true if the span of the arc is more than 180Degrees or PI
-     * @Note this version of the Arc class might be more precise when editing a path comprised of 1 or more arcs since you have control over the start and end point, like in a regular bezierpath, with the Arc4 you have less controll over the start and end,
-     * @Note in XML the arc is defined as [startX,startY,xRadii,yRadii,rotation,largeArcFlag,sweepFlag,endX,endY,centerX,centerY]
-     * // :TODO: the reason Arc4 exists is because it can work with axises that are 0, if you want to Make Arc3 the primary arc class again you need to make sure it can also work with axis that are 0, the benefit of Ar3 is that its absolute precis while editing the arc instance start and end positions.
-     * // :TODO: a largeflag two that indicates wheter the start point is on the left or right side when one of the axises is 0 ould be a solution to the problem with 0 axises
-     * // :TODO: you might also want to consider including a position argument, for better performance and ease. this is just an idea
+     * NOTE: the largeFlag is always true if the span of the arc is more than 180Degrees or PI
+     * NOTE: this version of the Arc class might be more precise when editing a path comprised of 1 or more arcs since you have control over the start and end point, like in a regular bezierpath, with the Arc4 you have less controll over the start and end,
+     * NOTE: in XML the arc is defined as [startX,startY,xRadii,yRadii,rotation,largeArcFlag,sweepFlag,endX,endY,centerX,centerY]
+     * TODO: the reason Arc4 exists is because it can work with axises that are 0, if you want to Make Arc3 the primary arc class again you need to make sure it can also work with axis that are 0, the benefit of Ar3 is that its absolute precis while editing the arc instance start and end positions.
+     * TODO: a largeflag two that indicates wheter the start point is on the left or right side when one of the axises is 0 ould be a solution to the problem with 0 axises
+     * TODO: you might also want to consider including a position argument, for better performance and ease. this is just an idea
      */
     var start:CGPoint
     var xRadii:CGFloat// :TODO: i think the xRadii and yRadii cant be negative, confirm please and write it up as a note
