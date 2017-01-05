4741df6d427a04bb609b18182f56c13410b1ba63
Eonist <unicornhive@gmail.com>
2017-01-05 22:36:03 +0100

    Files modified: 2
    Modified 2 files:geom/svg/graphic/SVGCircle.swiftgeom/svg/graphic/utils/SVGRectModifier.swift

--- geom/svg/graphic/SVGCircle.swift
+++ geom/svg/graphic/SVGCircle.swift
@@ -1,7 +1,7 @@
 import Foundation
 /**
  * Creates a circle instance
- * @example <circle cx="30" cy="30" r="20" style="stroke: black; fill: none;" />
+ * EXAMPLE: <circle cx="30" cy="30" r="20" style="stroke: black; fill: none;" />
  */
 class SVGCircle : SVGGraphic{
     var cx : CGFloat;//center x
@@ -14,10 +14,10 @@ class SVGCircle : SVGGraphic{
       super.init(style, id);
     }
     /**
-     * @Note If the radius is zero, no shape will be displayed; it is an error to provide a negative radius.
-     * @Note if the cx or cy is omitted (nan), it is presumed to be zero.
-     * @Note it is an error to provide a negative radius.
-     * @NOTE: strokeWidth should always be >= 0 if there is a lineStyle (asserting if there is a linestyle is done by the caller of this method)
+     * NOTE: If the radius is zero, no shape will be displayed; it is an error to provide a negative radius.
+     * NOTE: if the cx or cy is omitted (nan), it is presumed to be zero.
+     * NOTE: it is an error to provide a negative radius.
+     * NOTE: strokeWidth should always be >= 0 if there is a lineStyle (asserting if there is a linestyle is done by the caller of this method)
      */
     override func draw(){
         //Swift.print("SVGCircle.draw() + r: " + "\(r)")
@@ -44,8 +44,6 @@ class SVGCircle : SVGGraphic{
                 
                 lineShape.path = CGPathParser.ellipse(lineOffsetRect.lineRect)/*<--why arent we using the circle method here?, well this works aswell*/
             }
-            
-            
         }
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
