204c0b75f9dfd1bcfbdc30ea01dcce2d92b55a91
Eonist <unicornhive@gmail.com>
2017-01-06 00:36:05 +0100

    Files modified: 1
    Modified 1 file:math/algebra/Algebra.swift

--- math/algebra/Algebra.swift
+++ math/algebra/Algebra.swift
@@ -7,7 +7,7 @@ class Algebra{
 	 * EXAMPLE: Algebra.quadratic(0.5, 6, -68));//7.114877048604001
 	 */
 	static func quadratic(a:CGFloat,_ b:CGFloat,_ c:CGFloat)->CGFloat {
-		var numerator:CGFloat = -b+Math.sqrt(b*b-4*a*c)
+		var numerator:CGFloat = -b+sqrt(b*b-4*a*c)
 		var denominator:CGFloat = 2*a
 		return numerator/denominator/*fraction*/
 	}
