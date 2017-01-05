03670a13000636d1983bfc91c107bca1526dae06
Eonist <unicornhive@gmail.com>
2017-01-06 00:28:12 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVGContainer.swift

--- geom/svg/SVGContainer.swift
+++ geom/svg/SVGContainer.swift
@@ -1,8 +1,8 @@
 import Cocoa
 /**
  * SVGContainer serves as a Sprite but also has an id and an array that keeps track of the added items
- * // :TODO: could we omit the _items and just use the display-stack as a record of items added?
- * // :TODO: we need a remove method to compliment the add method
+ * TODO: could we omit the _items and just use the display-stack as a record of items added?
+ * TODO: we need a remove method to compliment the add method
  */
 class SVGContainer : InteractiveView2, ISVGContainer{
     var id : String
