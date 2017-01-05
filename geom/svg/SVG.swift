19c89ef799588ff0a40c0eb21d6a47cad3defb82
Eonist <unicornhive@gmail.com>
2017-01-06 00:26:12 +0100

    Files modified: 1
    Modified 1 file:geom/svg/SVG.swift

--- geom/svg/SVG.swift
+++ geom/svg/SVG.swift
@@ -4,7 +4,7 @@ import Foundation
  * // :TODO: impliment xmlns:xlink,viewBox,enable-background,xml:space,
  * //preserveAspectRatio="xMinYMin meet"
  * //viewBox="0 0 90 90"
- * @example
+ * EXAMPLE:
  * <?xml version="1.0"?><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="768px" height="768px">
  * 		<rect x="64" y="64" fill="#0000FF" stroke="#000000" stroke-miterlimit="10" width="512" height="512"/>
  * </svg>
