6c864201bbf12ea1d95df1801972da2e638b7b42
Eonist <unicornhive@gmail.com>
2017-01-06 16:53:49 +0100

    Files modified: 1
    Modified 1 file:math/trig/TrigConverter.swift

--- math/trig/TrigConverter.swift
+++ math/trig/TrigConverter.swift
@@ -4,7 +4,7 @@ class TrigConverter {
     /**
      * Needs code
      */
-    class func cartesianToPolar(point:CGPoint)->(radius:CGFloat,angle:CGFloat){
+    static func cartesianToPolar(point:CGPoint)->(radius:CGFloat,angle:CGFloat){
         /* do some math here to get radius and angle */
         let radius = CGFloat(0.0)
         let angle = CGFloat(0.0)
@@ -15,14 +15,14 @@ class TrigConverter {
      * NOTE: its easier to just do: 45*㎭
      * NOTE: can also be defined: rad * (180 / π)
      */
-    class func degrees(radians:CGFloat) -> CGFloat {
+    static func degrees(radians:CGFloat) -> CGFloat {
         return π * radians / 180.0
     }
     /**
      * Converts an angle in radians to degrees.
      * NOTE: can also be defined degrees * (π / 180)
      */
-    class func radians(degrees:CGFloat) -> CGFloat {
+    static func radians(degrees:CGFloat) -> CGFloat {
         return degrees / 180.0 * π
     }
 }
