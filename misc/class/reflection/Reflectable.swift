78ea63bd5570223be727bd42a1c97f94ba5bcea3
Eonist <unicornhive@gmail.com>
2017-01-06 18:35:13 +0100

    Files modified: 3
    Modified 3 files:misc/border/Border.swiftmisc/class/reflection/Reflectable.swiftmisc/class/reflection/Reflectables.swift

--- misc/class/reflection/Reflectable.swift
+++ misc/class/reflection/Reflectable.swift
@@ -1,6 +1,6 @@
 import Cocoa
 /**
- * NOTE: for special types like CGAffineTransform etc, you should make an protocol named: Reflectable that you extend CGColor CGTransform etc. that package these types into an organized xml child
+ * NOTE: For special types like CGAffineTransform etc, you should make an protocol named: Reflectable that you extend CGColor CGTransform etc. that package these types into an organized xml child
  */
 protocol Reflectable {
     var reflection:(type:String,value:String) {get}
