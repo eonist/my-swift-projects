fbe03681e51115cb899a7e615f154b16461e0b08
Eonist <unicornhive@gmail.com>
2017-01-07 03:08:49 +0100

    Files modified: 1
    Modified 1 file:xml/XMLExtensions.swift

--- xml/XMLExtensions.swift
+++ xml/XMLExtensions.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-typealias XML = NSXMLElement/*makes refering to NSXMLElement simpler, just use XML instead*/
+typealias XML = NSXMLElement/*Makes refering to NSXMLElement simpler, just use XML instead*/
 typealias XMLDoc = NSXMLDocument
 extension NSXMLElement {
     /**
-     * convenience
+     * Convenience
      */
     convenience init(_ xmlString:String){//<---recently removed the throw, and changed try to try!
         try! self.init(XMLString:xmlString)
@@ -17,16 +17,16 @@ extension NSXMLElement {
         get {return XMLParser.attribute(self, key)}
         set {self.setAttributesWithDictionary(DictionaryModifier.combine(self.attribs,[key:newValue!]))}
     }
-    func hasAttribute(attrName:String)->Bool{//Convenience
+    func hasAttribute(attrName:String)->Bool{/*Convenience*/
         return XMLAsserter.hasAttribute(self,attrName)
     }
-    func appendChild(xml:XML){//convenince
+    func appendChild(xml:XML){/*convenince*/
         self.addChild(xml)
     }
-    func removeChildAt(index:Array<Int>){//Convenience
+    func removeChildAt(index:Array<Int>){/*Convenience*/
         XMLModifier.removeChildAt(self, index)
     }
-    func removeAt(index:Int)->XML{//Convenience
+    func removeAt(index:Int)->XML{/*Convenience*/
         return XMLModifier.removeChildAt(self, index)
     }
     /**
@@ -38,57 +38,57 @@ extension NSXMLElement {
     var hasSimpleContent:Bool{
         return XMLAsserter.hasSimpleContent(self)
     }
-    func valueAt(index:Array<Int>)->String?{//Convenience
+    func valueAt(index:Array<Int>)->String?{/*Convenience*/
         return XMLParser.valueAt(self, index)
     }
     var value:String {return XMLParser.value(self)}/*Convenience*/
     /**
      * NOTE: xml.XMLString does the same as this var
      */
-    var string:String{//Convenience
+    var string:String{/*Convenience*/
         return XMLParser.string(self)
     }
-    func childAt(index:Array<Int>)->XML?{//Convenience
+    func childAt(index:Array<Int>)->XML?{/*Convenience*/
         return XMLParser.childAt(self, index)
     }
-    func childByIndex(index:Int)->XML?{//Convenience
+    func childByIndex(index:Int)->XML?{/*Convenience*/
         return self.childAtIndex(index) as? XML
     }
     var attribs:Dictionary<String,String> {//Convenience, the name attributes is occupied so attribs it is
        return XMLParser.attribs(self)
     }
-    func addAt(child:XML,_ index:Int)->XML{//Convenience
+    func addAt(child:XML,_ index:Int)->XML{/*Convenience*/
         return XMLModifier.insertAt(self, index, child)
     }
-    func addchildAt(child:XML,_ index:Array<Int>)->XML{//Convenience
+    func addchildAt(child:XML,_ index:Array<Int>)->XML{/*Convenience*/
         return XMLModifier.addChildAt(self, index, child)
     }
-    func setNameAt(index:Array<Int>, _ name:String) -> XML {//Convenience
+    func setNameAt(index:Array<Int>, _ name:String) -> XML {/*Convenience*/
         return XMLModifier.setNameAt(self, index, name)
     }
-    func setAttributeAt(xml:XML,_ index:Array<Int>, _ key:String,_ value:String) -> XML {//Convenience
+    func setAttributeAt(xml:XML,_ index:Array<Int>, _ key:String,_ value:String) -> XML {/*Convenience*/
         return XMLModifier.setAttributeAt(self, index, key, value)
     }
-    func setAttributeAt(xml:XML,_ index:Array<Int>, _ attributes:Dictionary<String,String>) -> XML {//Convenience
+    func setAttributeAt(xml:XML,_ index:Array<Int>, _ attributes:Dictionary<String,String>) -> XML {/*Convenience*/
         return XMLModifier.setAttributeAt(xml, index, attributes)
     }
-    func replaceAt(xml:XML,_ index:Int,_ replacement:XML) -> XML {//Convenience
+    func replaceAt(xml:XML,_ index:Int,_ replacement:XML) -> XML {/*Convenience*/
         return XMLModifier.replaceAt(xml, index, replacement)
     }
-    func replaceChildAt(xml:XML,_ index:Array<Int>,_ replacement:XML) -> XML {//Convenience
+    func replaceChildAt(xml:XML,_ index:Array<Int>,_ replacement:XML) -> XML {/*Convenience*/
         return XMLModifier.replaceAt(xml, index, replacement)
     }
-    func firstNode(nodeName:String) -> XML? {//Convenience
+    func firstNode(nodeName:String) -> XML? {/*Convenience*/
         return XMLParser.firstNode(self, nodeName)
     }
     //deprecated:
-    //func value()->String?{return XMLParser.value(self)}//Convenience
+    //func value()->String?{return XMLParser.value(self)}/*Convenience*/
 }
 /**
  * Multiplies a string
  * EXAMPLE: NSXMLElement("<items><items/>") += "<coffe><coffe/>" //<items><coffe><coffe/><items/>
  */
-public func += (left: NSXMLElement, right: NSXMLElement) -> NSXMLElement {//Convenience
+public func += (left: NSXMLElement, right: NSXMLElement) -> NSXMLElement {/*Convenience*/
     left.appendChild(right)
     return left
 }
