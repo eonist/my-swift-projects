70701b9044694d48dfed46d6d88c5ed3cac9b0ee
Eonist <unicornhive@gmail.com>
2017-01-08 23:20:58 +0100

    Files modified: 1
    Modified 1 file:array/ArrayModifier.swift

--- array/ArrayModifier.swift
+++ array/ArrayModifier.swift
@@ -261,9 +261,12 @@ class ArrayModifier{
         // :TODO: this doesnt work , you need to use a combination of splice, concat and unshift also make this function not return anything, it seem to work actually
         //it cant work look at the .pop and .count
         var arrayB:Array<T> = []
-        let arrayLength:Int = array.count
-        for(var i:Int=index+1 ; i<arrayLength ; i++) {
-            arrayB.append(array.pop()!)//pop removes last and returns it
+        //let arrayLength:Int = array.count
+        var i:Int = index+1
+        while(i < array.count){
+        //for(var i:Int=index+1 ; i<arrayLength ; i++) {
+            arrayB.unshift(array.pop()!)//pop removes last and returns it
+            i += 1
         }
         let retVal:([T],[T]) = (array,arrayB.reverse())
         return retVal
