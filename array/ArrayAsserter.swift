94dc4146f5f578ef352f17f21b734c5dd4002155
Eonist <unicornhive@gmail.com>
2017-01-09 17:30:07 +0100

    Files modified: 1
    Modified 1 file:array/ArrayAsserter.swift

--- array/ArrayAsserter.swift
+++ array/ArrayAsserter.swift
@@ -1,29 +1,33 @@
 import Foundation
 class ArrayAsserter {
     /**
-     * Asserts if the items in @param a is also present in the array @param b
-     * PARAM: a: an array of items to check if is apart of the @param b
+     * Asserts if the items in PARAM: a is also present in the array PARAM: b
+     * PARAM: a: an array of items to check if is apart of the PARAM: b
      * PARAM: b: the array to check against
      * PARAM: strict: toggle the ability that the items must be equal to the array, (almost the same functionality as the isEqual function, although equal function the array must have the same order)
      * RETURNS: a boolean indicating if the items is apart of the array
      * EXAMPLE: ArrayAsserter.contains([1,2,3], [1,2], false)//true
      * EXAMPLE: ArrayAsserter.contains([1,2,3], [1,2], true)//false, the length of a and b must be the same
-     * TODO: create a better description
      * NOTE: one can also use the similar native method: array.contains(obj)
      * NOTE: This method compares if the instance has the same variables, it does not compare if the instance has the same referene point, which may be suitable in some situations, maybe make a method for this?
+     * TODO: create a better description
      */
     static func contains<T:Equatable>(a:[T], _ b:[T], _ strict:Bool = false)->Bool {
         var score:Int = 0
-        for (var i:Int = 0 , n:Int = a.count ;i < n ;i++) {if (b.indexOf(a[i]) >= 0 ){ score++ }}
+        for (var i:Int = 0 , n:Int = a.count ;i < n ;i++) {
+            if (b.indexOf(a[i]) >= 0 ){
+                score++
+            }
+        }
         return score == (strict ? b.count:a.count)
     }
     /**
      * Asserts if an array has an item
      * NOTE: Determines whether the specified array contains the specified value
      * NOTE: Only works with Equatable types
-     * @param arr The array that will be checked for the specified value.
-     * @param value The object which will be searched for within the array
-     * @return True if the array contains the value, False if it does not.
+     * PARAM: arr The array that will be checked for the specified value.
+     * PARAM: value The object which will be searched for within the array
+     * RETURN: True if the array contains the value, False if it does not.
      * EXAMPLE: Swift.print(ArrayAsserter.has(["e","f","g"], "f"))//true
      * EXAMPLE: Swift.print(ArrayAsserter.has(["e","f","g"], "a"))//false
      */
