8f403a616ea9d4da900c2a11cbdb4d8cf78fae53
Eonist <unicornhive@gmail.com>
2017-01-09 16:31:28 +0100

    Files modified: 1
    Modified 1 file:array/ArrayParser.swift

--- array/ArrayParser.swift
+++ array/ArrayParser.swift
@@ -149,8 +149,10 @@ class ArrayParser{
         for (var a:Int = start; a <= end; a++) {numbers.append(a)}
         var randomNumbers:Array<Int> = []
         let len:Int = numbers.count
+        Swift.print("len: " + "\(len)")
         for (var e:Int=0; e<len; e++) {
             let randomNr:Int = Int(floor(Float(Int(arc4random()) * (len-e))))// possibly use this line instead: Math.floor(Math.random()*(array.length-0.1));
+            Swift.print("randomNr: " + "\(randomNr)")
             randomNumbers.append(numbers[randomNr])
             numbers.splice2(randomNr, 1)
         }
