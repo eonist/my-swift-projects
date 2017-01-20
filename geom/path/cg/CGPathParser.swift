b145a32a89da2e603d7ae08af7c395abd71aeec7
Eonist <unicornhive@gmail.com>
2017-01-20 16:44:11 +0100

    Files modified: 1
    Modified 1 file:geom/path/cg/CGPathParser.swift

--- geom/path/cg/CGPathParser.swift
+++ geom/path/cg/CGPathParser.swift
@@ -8,7 +8,7 @@ class CGPathParser{
      * NOTE: effectivly it creates a PolyLine, 
      * TODO: rename to polyLine
      */
-    static func lines(_ points:Array<CGPoint>,_ close:Bool = false,_ offset:CGPoint = CGPoint(0,0))->CGMutablePath{
+    static func polyLine(_ points:Array<CGPoint>,_ close:Bool = false,_ offset:CGPoint = CGPoint(0,0))->CGMutablePath{
         let path:CGMutablePath = CGMutablePath()
         if(points.count > 0) { path.move(to: CGPoint(points[0].x+offset.x, points[0].y+offset.y))}
         for i in 1..<points.count{
@@ -140,4 +140,6 @@ extension CGPathParser{
         path.addRoundedRect(in: rect, cornerWidth: cornerWidth, cornerHeight: cornerheight)
         return path
     }
+    //DEPRECATED: 
+    static func lines(_ points:Array<CGPoint>,_ close:Bool = false,_ offset:CGPoint = CGPoint(0,0))->CGMutablePath{return polyLine(points,close,offset)}//deprecated
 }
