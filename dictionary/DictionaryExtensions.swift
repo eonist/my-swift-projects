457146e0a339fde9d911949eed7f2b5791894a79
Eonist <unicornhive@gmail.com>
2017-01-27 22:13:13 +0100

    Files modified: 1
    Modified 1 file:dictionary/DictionaryExtensions.swift

--- dictionary/DictionaryExtensions.swift
+++ dictionary/DictionaryExtensions.swift
@@ -3,20 +3,3 @@ import Foundation
 protocol AnyDictionary{}//see ArrayExtensions for description
 extension Dictionary:AnyDictionary{}
 extension NSDictionary:AnyDictionary{}
-
-
-/**
- * TODO: Try to make this extension Generic, currently its only working with String key value pairs
- */
-
-/*
-
-//this doesnt work, figure it out, something wrong with the generics
-
-extension Dictionary where Key: String, Value: String{
-    func combine(b:Dictionary<String,String>)->Dictionary<String,String>{
-        return DictionaryModifier.combine(self, b)
-    }
-    //TODO: add merge aswell
-}
-*/
