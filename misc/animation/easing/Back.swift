81eb5113c3b2d8ad41ba697eb355738af3207bc4
Eonist <unicornhive@gmail.com>
2017-01-27 13:52:26 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Back.swift

--- misc/animation/easing/Back.swift
+++ misc/animation/easing/Back.swift
@@ -1,15 +1,31 @@
 import Foundation
 
 class Back {
-    //Back
     static func easeInBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        return c*(t/=d)*t*((s+1)*t - s) + b
+        var s: CGFloat? = nil
+        if s == nil { s = 1.70158 }
+        var t = t
+        t = t / d
+        return c * t * t * ((s! + 1 ) * t - s!) + b
     }
     static func easeOutBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+        var s: CGFloat? = nil
+        if s == nil { s = 1.70158 }
+        var t = t
+        t = t / d - 1
+        return c * (t * t * ((s! + 1) * t + s!) + 1) + b
     }
     static func easeInOutBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b
-         return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b
+        var s: CGFloat? = nil
+        if s == nil { s = 1.70158 }
+        s = s! * 1.525
+        var t = t
+        t = t / d * 2
+        if t < 1 {
+            return c / 2 * (t * t * ((s! + 1) * t - s!)) + b
+        } else {
+            t = t - 2
+            return c / 2 * (t * t * ((s! + 1) * t + s!) + 2) + b
+        }
     }
 }
