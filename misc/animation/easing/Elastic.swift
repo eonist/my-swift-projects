ec28e20722fe8b1d6dcd03673b40843840639224
Eonist <unicornhive@gmail.com>
2017-01-27 14:05:31 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Elastic.swift

--- misc/animation/easing/Elastic.swift
+++ misc/animation/easing/Elastic.swift
@@ -2,11 +2,22 @@ import Foundation
 /*Elastic*/
 class Elastic {
     private static func easeInElastic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        var s:Number;
-         if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-         if (!a || a < abs(c)) { a=c; s=p/4; }
-         else s = p/PI_M2 * Math.asin (c/a);
-         return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*PI_M2/p )) + b;
+        var t = t
+        var a: CGFloat? = nil
+        var p: CGFloat? = nil
+        if t == 0 { return b }
+        t = t / d
+        if t == 1 { return b + c }
+        if p == nil { p = d * 0.3 }
+        var s: CGFloat
+        if a == nil || a! < abs( c ) {
+            a = c
+            s = p! / 4
+        } else {
+            s = p! / (2 * Float(M_PI)) * asin(c / a!)
+        }
+        t = t - 1
+        return -(a! * pow(2, 10 * t) * sin((t * d - s) * (2 * M_PI) / p!)) + b
     }
     private static func easeOutElastic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
         /*var s:Number;
