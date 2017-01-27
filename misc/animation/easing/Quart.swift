f3245c99f4df87c46d6c2ea9533c5531c6898813
Eonist <unicornhive@gmail.com>
2017-01-27 13:33:38 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Quart.swift

--- misc/animation/easing/Quart.swift
+++ misc/animation/easing/Quart.swift
@@ -1,5 +1,21 @@
 import Foundation
-
+//Quartic
 class Quart {
-
+    static func easeInQuart(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        var t = t
+        t = t/d
+        return c*t*t*t*t + b
+    }
+    static func easeOutQuart(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        var t = t
+        t = (t/d)-1//<--the brackets are important
+        return -c * (t*t*t*t - 1) + b
+    }
+    static func easeInOutQuart(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat) -> CGFloat{
+        var t = t
+        t = t/(d/2)//<--the brackets are important
+        if (t < 1) {return c/2*t*t*t*t + b}
+        t = t-2
+        return -c/2 * (t*t*t*t - 2) + b
+    }
 }
