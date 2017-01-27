70c07ef8678ee14651962e26cf7448aa08811a6f
Eonist <unicornhive@gmail.com>
2017-01-28 00:09:18 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file:geom/style/line/utils/LineStyleModifier.swiftModified 1 file:geom/style/fill/FillStyle.swift

--- geom/style/fill/FillStyle.swift
+++ geom/style/fill/FillStyle.swift
@@ -1,5 +1,7 @@
 import Cocoa
-
+/**
+ * TODO: Candidate for struct. Use IFillStyle as a base protocol
+ */
 class FillStyle:IFillStyle {
     var color:NSColor
     init(_ color:NSColor = NSColor.clear){
