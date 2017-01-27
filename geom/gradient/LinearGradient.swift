1dc8e8020e55e7a289733c72cd8b6a85a50510bb
Eonist <unicornhive@gmail.com>
2017-01-27 23:19:00 +0100

    Files modified: 3
    Modified 3 files:geom/gradient/LinearGradient.swiftgeom/gradient/RadialGradient.swiftgeom/graphic/BaseGraphic.swift

--- geom/gradient/LinearGradient.swift
+++ geom/gradient/LinearGradient.swift
@@ -1,7 +1,7 @@
 import Foundation
 /**
- * NOTE: there is no TopLeft or BottomRight etc in the CSS sepcs so no need to have more complex relative positioning in this class
- * NOTE: why do we have this class when its basically the same as Gradient? Because the alternative is to ad another value to to Gradient to represent Gradient Type.
+ * NOTE: There is no TopLeft or BottomRight etc in the CSS sepcs so no need to have more complex relative positioning in this class
+ * NOTE: Why do we have this class when its basically the same as Gradient? Because the alternative is to ad another value to to Gradient to represent Gradient Type.
  */
 public class LinearGradient:Gradient {
     override init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ rotation:CGFloat = 1.5707963267949, _ transformation:CGAffineTransform? = nil){
--- geom/gradient/RadialGradient.swift
+++ geom/gradient/RadialGradient.swift
@@ -1,12 +1,10 @@
 import Foundation
-/**
- * NOTE: Focal length:
- */
-public class RadialGradient:Gradient {
-    public var startCenter:CGPoint?/*should be 0.5,0.5 to mimic the focal ratio radial system*/
-    public var endCenter:CGPoint?/*y = focalRatio (-1 to +1) */
-    public var startRadius:CGSize?
-    public var endRadius:CGSize?/*should be 0,0 to mimic the focal ratio radial gradient system*/
+
+class RadialGradient:Gradient {
+    var startCenter:CGPoint?/*should be 0.5,0.5 to mimic the focal ratio radial system*/
+    var endCenter:CGPoint?/*y = focalRatio (-1 to +1) */
+    var startRadius:CGSize?
+    var endRadius:CGSize?/*should be 0,0 to mimic the focal ratio radial gradient system*/
     init(_ colors:Array<CGColor> = [], _ locations:Array<CGFloat> = [],_ rotation:CGFloat = 1.5707963267949, _ startCenter:CGPoint? = nil,_ endCenter:CGPoint? = nil,_ startRadius:CGSize? = nil,_ endRadius:CGSize? = nil,_ transformation:CGAffineTransform? = nil){
         super.init(colors, locations,rotation, transformation)
         self.startCenter = startCenter
