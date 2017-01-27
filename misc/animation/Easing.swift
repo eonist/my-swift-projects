30b58807892346d0800b6f2c9d5bc8e86455d904
Eonist <unicornhive@gmail.com>
2017-01-27 12:49:33 +0100

    Files modified: 1
    Modified 1 file:misc/animation/Easing.swift

--- misc/animation/Easing.swift
+++ misc/animation/Easing.swift
@@ -147,16 +147,21 @@ class Easing{
     private static func easeInBounce(){
         //return c - easeOutBounce (d - t, 0, c, d) + b;
     }
-    private static func easeOutBounce(){
-        /*if ((t/=d) < (1/2.75)) {
-        return c*(7.5625*t*t) + b;
-        } else if (t < (2/2.75)) {
-        return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-        } else if (t < (2.5/2.75)) {
-        return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+    static func easeOutBounce(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat) -> CGFloat{
+        var t = t
+        t = t / d
+        if t < 1 / 2.75 {
+            return c * (7.5625 * t * t) + b
+        } else if t < 2 / 2.75 {
+            t = t - (1.5 / 2.75)
+            return c * (7.5625 * t * t + 0.75) + b
+        } else if t < 2.5 / 2.75 {
+            t = t - (2.25 / 2.75 )
+            return c * (7.5625 * t * t + 0.9375) + b
         } else {
-        return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
-        }*/
+            t = t - (2.625 / 2.75 )
+            return c * (7.5625 * t * t + 0.984375) + b
+        }
     }
     private static func easeInOutBounce(){
         /*if (t < d/2) return easeInBounce (t*2, 0, c, d) * 0.5 + b;
