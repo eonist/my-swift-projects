d16c46c5ed9009aa88e083ec219884392ba08c46
Eonist <unicornhive@gmail.com>
2017-01-27 13:46:53 +0100

    Files modified: 1
    Modified 1 file:misc/animation/Easing.swift

--- misc/animation/Easing.swift
+++ misc/animation/Easing.swift
@@ -8,14 +8,24 @@ import Cocoa
  * NOTE: robertpenner.com has lots of tutorials and pdfs on how easing work
  */
 class Easing{
+    static var back:Back.Type {return Back.self}
     static var bounce:Bounce.Type {return Bounce.self}
+    static var circ:Circ.Type {return Circ.self}
+    static var cubic:Cubic.Type {return Cubic.self}
+    static var elastic:Elastic.Type {return Elastic.self}
     static var expo:Expo.Type {return Expo.self}
+    static var linear:Linear.Type {return Linear.self}
+    static var quad:Quad.Type {return Quad.self}
+    static var quart:Quart.Type {return Quart.self}
+    static var quint:Quint.Type {return Quint.self}
+    static var sine:Sine.Type {return Sine.self}
 }
 
-/*
+
 /**
 * NOTE: If you decrease the decimal variable you increase the friction effect
 */
+/*
 static func easeOut(value : CGFloat, _ from:CGFloat, _ to:CGFloat) -> CGFloat {
 let distToGoal:CGFloat = NumberParser.relativeDifference(value, to)
 let val:CGFloat = 0.2 * distToGoal
