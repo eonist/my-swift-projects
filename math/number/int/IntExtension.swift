e2c90b96890732e27a345feaab925219ddd4b27c
Eonist <unicornhive@gmail.com>
2017-01-27 16:59:51 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:math/number/int/IntExtension.swift

--- math/number/int/IntExtension.swift
+++ math/number/int/IntExtension.swift
@@ -6,6 +6,7 @@ extension Int {
     var string:String{return String(self)}/*Convenince*/
     var double:Double {return Double(self)}/*Convenince*/
     func minMax(_ min:Int,_ max:Int)->Int{return NumberParser.minMax(self, min, max)}/*Convenince*/
+    func clip(_ min:Int,_ max:Int)->Int{return NumberParser.clip(self, min, max)}/*Convenince*/
     var negative:Bool{return IntAsserter.negative(self)}
     var positive:Bool{return IntAsserter.positive(self)}
 }
