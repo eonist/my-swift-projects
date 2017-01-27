4572ce9c12459cf57bed9ecdfa6fbe876b8dd062
Eonist <unicornhive@gmail.com>
2017-01-27 23:23:20 +0100

    Files modified: 1
    Modified 1 file:geom/graphic/concrete/form/EllipseGraphic.swift

--- geom/graphic/concrete/form/EllipseGraphic.swift
+++ geom/graphic/concrete/form/EllipseGraphic.swift
@@ -6,13 +6,11 @@ import Cocoa
  */
 class EllipseGraphic:SizeableGraphic{
     override func drawFill() {
-        //Swift.print("EllipseGraphic.drawFill()")
         let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType) : CGRect(x,y,width,height)
         graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
         graphic.fillShape.path = CGPathParser.ellipse(CGRect(0,0,width,height))
     }
     override func drawLine() {
-        //Swift.print("EllipseGraphic.drawLine()")
         let lineOffsetRect = RectGraphicUtils.lineOffsetRect(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType)
         graphic.lineShape.frame = lineOffsetRect.lineFrameRect
         //Swift.print("lineOffsetRect.lineFrameRect: " + "\(lineOffsetRect.lineFrameRect)")
