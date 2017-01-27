150b85ace668dfac53291ef3af830087002a0735
Eonist <unicornhive@gmail.com>
2017-01-27 14:03:35 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Elastic.swift

--- misc/animation/easing/Elastic.swift
+++ misc/animation/easing/Elastic.swift
@@ -1,21 +1,21 @@
 import Foundation
 /*Elastic*/
 class Elastic {
-    private static func easeInElastic(){
-        /*var s:Number;
+    private static func easeInElastic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        var s:Number;
          if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-         if (!a || a < Math.abs(c)) { a=c; s=p/4; }
+         if (!a || a < abs(c)) { a=c; s=p/4; }
          else s = p/PI_M2 * Math.asin (c/a);
-         return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*PI_M2/p )) + b;*/
+         return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*PI_M2/p )) + b;
     }
-    private static func easeOutElastic(){
+    private static func easeOutElastic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
         /*var s:Number;
          if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
          if (!a || a < Math.abs(c)) { a=c; s=p/4; }
          else s = p/PI_M2 * Math.asin (c/a);
          return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*PI_M2/p ) + c + b);*/
     }
-    private static func easeInOutElastic(){
+    private static func easeInOutElastic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
         /*var s:Number;
          if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
          if (!a || a < Math.abs(c)) { a=c; s=p/4; }
