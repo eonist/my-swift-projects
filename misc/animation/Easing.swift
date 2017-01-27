26e6be04499be380b495617138eb4fa00fae9282
Eonist <unicornhive@gmail.com>
2017-01-27 13:41:00 +0100

    Files modified: 2
    Modified 2 files:misc/animation/Easing.swiftmisc/animation/easing/Quad.swift

--- misc/animation/Easing.swift
+++ misc/animation/Easing.swift
@@ -8,38 +8,7 @@ import Cocoa
  * NOTE: robertpenner.com has lots of tutorials and pdfs on how easing work
  */
 class Easing{
-    
-    //Continue here:
-        //Separate out all the easing classes.
-        //Try to do Easing.Bounce.easeInBounce etc. maybe?
-    
-    
-    
-	//Quadratic
-    static func easeInQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
-        var t = t
-        t = t/d
-		return c*t*t + b
-	}
-    static func easeOutQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
-        var t = t
-        t = t/d
-		return -c*t*(t-2) + b
-	}
-    static func easeInOutQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
-        var t = t
-        t = t / d * 2
-        if t < 1 {
-            return c / 2 * t * t + b
-        }
-        return -c / 2 * ((t - 1) * (t - 3) - 1) + b
-	}
-    static func easeOutInQuad ( _ t: CGFloat, _ b: CGFloat, _ c: CGFloat, _ d: CGFloat) -> CGFloat {
-        if t < d / 2 {
-            return easeOutQuad(t * 2,  b, c / 2,  d)
-        }
-        return easeInQuad((t * 2) - d,  b + c / 2,  c / 2,  d)
-    }
+    static var Bounce:Bounce {return Bounce.self}
 }
 
 /*
--- misc/animation/easing/Quad.swift
+++ misc/animation/easing/Quad.swift
@@ -1,5 +1,30 @@
 import Foundation
 
 class Quad {
+    //Quadratic
+    static func easeInQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
+        var t = t
+        t = t/d
+        return c*t*t + b
+    }
+    static func easeOutQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
+        var t = t
+        t = t/d
+        return -c*t*(t-2) + b
+    }
+    static func easeInOutQuad(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)-> CGFloat{
+        var t = t
+        t = t / d * 2
+        if t < 1 {
+            return c / 2 * t * t + b
+        }
+        return -c / 2 * ((t - 1) * (t - 3) - 1) + b
+    }
+    static func easeOutInQuad ( _ t: CGFloat, _ b: CGFloat, _ c: CGFloat, _ d: CGFloat) -> CGFloat {
+        if t < d / 2 {
+            return easeOutQuad(t * 2,  b, c / 2,  d)
+        }
+        return easeInQuad((t * 2) - d,  b + c / 2,  c / 2,  d)
+    }
 
 }
