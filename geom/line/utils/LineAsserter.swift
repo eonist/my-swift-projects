416215ec689038cb5bb07d089f3fe0bad5f884c5
Eonist <unicornhive@gmail.com>
2017-01-27 23:46:10 +0100

    Files modified: 1
    Modified 1 file:geom/line/utils/LineAsserter.swift

--- geom/line/utils/LineAsserter.swift
+++ geom/line/utils/LineAsserter.swift
@@ -5,27 +5,15 @@ typealias CGLineAsserter = LineAsserter
  * There is about 20 other lineAssert methods in legacy code
  */
 class LineAsserter {
-    /**
-     *
-     */
     static func intersects(_ a:Line,_ b:Line) -> Bool {// :TODO: rename to isSomeName
         return PointAsserter.intersects(a.p1, a.p2, b.p1, b.p2)
     }
-    /**
-     *
-     */
     static func oppositeDirectional(_ bisector:CGLine,_ a:CGLine,_ b:CGLine) -> Bool {// :TODO: rename to isSomeName
         return PointAsserter.oppositeDirectional(bisector.p1,bisector.p2,a.p1,a.p2,b.p1,b.p2)
     }
-    /**
-     *
-     */
     static func isContraDirectional(_ a:CGLine,_ b:CGLine) -> Bool {
         return PointAsserter.isContraDirectional(a.p1,a.p2,b.p1,b.p2)
     }
-    /**
-     *
-     */
     static func isCoDirectional(_ a:CGLine,_ b:CGLine) -> Bool {
         return PointAsserter.isCoDirectional(a.p1, a.p2, b.p1, b.p2)
     }
