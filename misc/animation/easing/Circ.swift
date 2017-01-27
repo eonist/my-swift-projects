1efaf8980743f8f1052021676607c09c60b1af72
Eonist <unicornhive@gmail.com>
2017-01-27 13:56:06 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Circ.swift

--- misc/animation/easing/Circ.swift
+++ misc/animation/easing/Circ.swift
@@ -1,7 +1,6 @@
 import Foundation
-
+/*Circular*/
 class Circ {
-    /*Circular*/
     static func easeInCircular(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
         var t = t
         t = t / d
@@ -13,9 +12,20 @@ class Circ {
         return (c * sqrt(1 - pow(t, 2)) + b)
     }
     static func easeInOutCircular(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        
-         if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-         return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
-        
+        var t = t
+        t = t / d * 2
+        if t < 1 {
+            return -c / 2 * (sqrt( 1 - t * t) - 1) + b
+        } else {
+            t = t - 2
+            return c / 2 * (sqrt( 1 - t * t) + 1) + b
+        }
+    }
+    static func easeOutInCirc(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        if t < d / 2 {
+            return easeOutCirc(t * 2,  b,  c / 2,  d)
+        } else {
+            return easeInCirc(( t * 2 ) - d, b + c / 2,  c / 2,  d)
+        }
     }
 }
