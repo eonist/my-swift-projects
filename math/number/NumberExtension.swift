4d007f7b3cd9155d7d250a23613ee4109c54e85d
Eonist <unicornhive@gmail.com>
2017-01-27 17:16:24 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberExtension.swift

--- math/number/NumberExtension.swift
+++ math/number/NumberExtension.swift
@@ -1,15 +1,21 @@
 import Foundation
 
-extension Comparable{
-    <
-}
 
 extension NumberType{
-    func clip(_ min:NumberType,_ max:NumberType)->NumberType{return NumberParser.clip(self, min, max)}/*Convenince*/
+    static func <(lhs: NumberType, rhs: NumberType) -> Bool {
+        return true
+    }
+    
+    // MARK: Equatable
+    
+    static func ==(lhs: NumberType, rhs: NumberType) -> Bool {
+        return false
+    }
+    //func clip(_ min:NumberType,_ max:NumberType)->NumberType{return NumberParser.clip(self, min, max)}/*Convenince*/
 }
 
-protocol NumberType{
-    
+protocol NumberType:Comparable{
+   
 }
 extension CGFloat:NumberType{
     
