7842c58b977afb4da542f4a250916667c7f47936
Eonist <unicornhive@gmail.com>
2017-01-27 13:48:44 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Back.swift

--- misc/animation/easing/Back.swift
+++ misc/animation/easing/Back.swift
@@ -2,14 +2,14 @@ import Foundation
 
 class Back {
     //Back
-    private static func easeInBack(){
-        //return c*(t/=d)*t*((s+1)*t - s) + b;
+    static func easeInBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        return c*(t/=d)*t*((s+1)*t - s) + b
     }
-    private static func easeOutBack(){
-        //return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+    static func easeOutBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
     }
-    private static func easeInOutBack(){
-        /*if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-         return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;*/
+    static func easeInOutBack(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
+        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b
+         return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b
     }
 }
