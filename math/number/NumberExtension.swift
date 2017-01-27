a193f3960c73a0712df9fc53e2d4fd857444edd9
Eonist <unicornhive@gmail.com>
2017-01-27 17:10:57 +0100

    Files modified: 1
    Modified 1 file:math/number/NumberExtension.swift

--- math/number/NumberExtension.swift
+++ math/number/NumberExtension.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 extension Comparable{
-    func clip(_ min:Comparable,_ max:Comparable)->Comparable{return NumberParser.clip(self, min, max)}/*Convenince*/
+    //func clip(_ min:Comparable,_ max:Comparable)->Comparable{return NumberParser.clip(self, min, max)}/*Convenince*/
+}
+
+extension IntegerType{
+    
 }
