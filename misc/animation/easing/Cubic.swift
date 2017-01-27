6ceef8ea87acb8099d4b7d7ac7200eabf6c55592
Eonist <unicornhive@gmail.com>
2017-01-27 13:59:49 +0100

    Files modified: 1
    Modified 1 file:misc/animation/easing/Cubic.swift

--- misc/animation/easing/Cubic.swift
+++ misc/animation/easing/Cubic.swift
@@ -7,10 +7,18 @@ class Cubic {
         return c * pow(t, 3) + b
     }
     private static func easeOutCubic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        return c*((t=t/d-1)*t*t + 1) + b
+        var t = t
+        t = t / d - 1
+        return c * (pow(t, 3) + 1) + b
     }
     private static func easeInOutCubic(_ t:CGFloat, _ b:CGFloat, _ c:CGFloat, _ d:CGFloat)->CGFloat{
-        if ((t/=d/2) < 1) return c/2*t*t*t + b;
-         return c/2*((t-=2)*t*t + 2) + b
+        var t = t
+        t = t / d * 2
+        if t < 1 {
+            return c / 2 * t * t * t + b
+        } else {
+            t = t - 2
+            return c / 2 * ( t * t * t + 2 ) + b
+        }
     }
 }
