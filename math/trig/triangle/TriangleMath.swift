a0ec17de63f42fd532287264e701691e5a0a28e7
Eonist <unicornhive@gmail.com>
2017-01-28 23:38:21 +0100

    Files modified: 1
    Modified 1 file:math/trig/triangle/TriangleMath.swift

--- math/trig/triangle/TriangleMath.swift
+++ math/trig/triangle/TriangleMath.swift
@@ -344,7 +344,19 @@ class TriangleMath {
      static func polarToAngle(_ x:CGFloat, _ y:CGFloat) -> CGFloat {
          return atan(y/x)
      }
+     /**
+      * 
+      */
+     public static function withinTriangle(_ x:CGFloat, _ y:CGFloat, _ x1:CGFloat, _ y1:CGFloat, _ x2:CGFloat, _ y2:CGFloat, _ x3:CGFloat, _ y3:CGFloat) -> Boolean {
+        let fab:CGFloat = ((y - y1) * (x2 - x1)) - ((x - x1) * (y2 - y1))
+        let fbc:CGFloat = ((y - y2) * (x3 - x2)) - ((x - x2) * (y3 - y2))
+        let fca:CGFloat = ((y - y3) * (x1 - x3)) - ((x - x3) * (y1 - y3))
+        return ( fab * fbc > 0 ) && ( fbc * fca > 0 )
+     }
 }
-
-
-//TODO: Implement the missting methods above, see legacy code
+//is_equilateral,Are all the sides the same length?
+//is_isosceles, Are two or more of the sides the same length?
+//is_scalene(self):"""Are all the sides of the triangle of different lengths?
+//is_right(self): """Is the triangle right-angled.
+//altitudes(self):"""The altitudes of the triangle. An altitude of a triangle is a segment through a vertex,perpendicular to the opposite side, with length being the, height of the vertex measured from the line containing the side.
+//medial(self):"""The medial triangle of the triangle., The triangle which is formed from the midpoints of the three sides.
