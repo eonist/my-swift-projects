a8bde3fee7e1cb70fdf496403953dfaacaf3ec8a
Eonist <unicornhive@gmail.com>
2017-01-29 23:50:51 +0100

    Files modified: 2
    Modified 2 files:math/number/NumberAsserter.swiftmath/number/cgfloat/CGFloatModifier.swift

--- math/number/NumberAsserter.swift
+++ math/number/NumberAsserter.swift
@@ -8,6 +8,7 @@ class NumberAsserter{
     }
     /**
      * NOTE: In most cases you will want to write if(someNumber >
+     * TODO: to avoid zero, we could pass zero to an internal private static method that then returns true or false
      */
     static func positive<T:Comparable>(_ number:T , _ zero:T) -> Bool {
         return number > zero
