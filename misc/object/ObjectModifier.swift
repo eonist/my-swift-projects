f89e737d140c86c370e7b5b3387ba12e5ed3fc4a
Eonist <unicornhive@gmail.com>
2017-01-30 00:49:42 +0100

    Files modified: 1
    Modified 1 file:misc/object/ObjectModifier.swift

--- misc/object/ObjectModifier.swift
+++ misc/object/ObjectModifier.swift
@@ -1,15 +1,8 @@
 class ObjectModifier {
     /**
-     *
+     * Use native swap()
      */
     static func swapObjects<T>(_ a: inout T, _ b: inout T) {
-        /*
-        let temporaryA = a
-        a = b
-        b = temporaryA
-        */
-        var a:String = ""
-        var b:String = ""
         swap(&a, &b)
     }
 }
