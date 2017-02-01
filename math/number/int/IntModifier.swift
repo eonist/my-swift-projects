ca3b22dc39a07ac81fbd412c093c308622b77a6e
Eonist <unicornhive@gmail.com>
2017-02-01 23:21:59 +0100

    Files deleted: 1
    Deleted 1 file: math/number/int/IntModifier.swift

--- math/number/int/IntModifier.swift
+++
@@ -1,9 +0,0 @@
-class IntModifier {
-    /**
-     * Directly replaces a with b, and b with a
-     * NOTE: Modifies the original a and b Doubles
-     */
-    static func swap(_ a:inout Int, _ b:inout Int) {
-        ObjectModifier.swapObjects(&a, &b)
-    }
-}
