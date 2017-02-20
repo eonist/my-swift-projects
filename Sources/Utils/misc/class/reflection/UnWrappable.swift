da25d39ff4a9e75151a10aa1cb805901a1e26253
Eonist <unicornhive@gmail.com>
2017-02-20 11:36:17 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/class/reflection/UnWrappable.swift

--- Sources/Utils/misc/class/reflection/UnWrappable.swift
+++ Sources/Utils/misc/class/reflection/UnWrappable.swift
@@ -104,7 +104,7 @@ extension UnWrappable{
         let child:XML = xml.firstNode(key)!//<--this should probably be asserted first, but should we return nil or empty array then?
         if(child.childCount > 0){
             XMLParser.children(child).forEach{
-                array.append(unWrapDict($0 as! XML))//$0.hasComplexContent ? .. : nil
+                array.append(unWrapDict($0))//$0.hasComplexContent ? .. : nil
             }
         }
         return array
