cc0e94ed198a129cf976b2f5f9e5cdacd6bc9742
Eonist <30n1st@gmail.com>
2017-02-24 16:24:59 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryExtensions.swift

--- Sources/Utils/dictionary/DictionaryExtensions.swift
+++ Sources/Utils/dictionary/DictionaryExtensions.swift
@@ -5,8 +5,8 @@ extension Dictionary:AnyDictionary{}
 extension NSDictionary:AnyDictionary{}
 
 
-extension CollectionType where Self: DictionaryLiteralConvertible, Self.Key == String, Self.Value == AnyObject, Generator.Element == (Self.Key, Self.Value) {
+extension Dictionary {
     var xml:XML{
-        return DictionaryParser.xml(self)
+        return DictionaryParser.xml(self as! Dictionary<String, String>)
     }
 }
