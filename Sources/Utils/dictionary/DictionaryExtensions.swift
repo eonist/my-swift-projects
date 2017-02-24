9c6857c039b8e1501dd35634dec4d8799a7efde5
Eonist <30n1st@gmail.com>
2017-02-24 17:13:10 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryExtensions.swift

--- Sources/Utils/dictionary/DictionaryExtensions.swift
+++ Sources/Utils/dictionary/DictionaryExtensions.swift
@@ -3,9 +3,10 @@ import Foundation
 protocol AnyDictionary{}//see ArrayExtensions for description
 extension Dictionary:AnyDictionary{}
 extension NSDictionary:AnyDictionary{}
+protocol AnyDict{}
+extension Dictionary:AnyDict{}
 
-
-extension AnyDictionary {
+extension AnyDict {
     var xml:XML{
         return DictionaryParser.xml(self as! Dictionary<String, String>)
     }
