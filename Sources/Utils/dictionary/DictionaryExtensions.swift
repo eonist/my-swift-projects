da365de7ab1d8a9c3efae992372939f43da5b612
Eonist <30n1st@gmail.com>
2017-02-24 15:47:03 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryExtensions.swift

--- Sources/Utils/dictionary/DictionaryExtensions.swift
+++ Sources/Utils/dictionary/DictionaryExtensions.swift
@@ -4,7 +4,7 @@ protocol AnyDictionary{}//see ArrayExtensions for description
 extension Dictionary:AnyDictionary{}
 extension NSDictionary:AnyDictionary{}
 
-extension Dictionary<String,String>{
+extension Dictionary where Element:String{
     var xml:XML{
         return DictionaryParser.xml(self)
     }
