ae97837d8ad42eaa77d2a46349fb48f722f62e0d
Eonist <30n1st@gmail.com>
2017-02-27 13:16:18 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/date/DateModifier.swift

--- Sources/Utils/misc/date/DateModifier.swift
+++ Sources/Utils/misc/date/DateModifier.swift
@@ -2,11 +2,13 @@ import Foundation
 
 class DateModifier {
     /**
+     * Returns a Date instance for a date and dayOffset
      * EXAMPLE: NSDate().offsetByDays(-3)//an NSDate instance that is 3 days ago
      */
     static func offsetByDays(_ date:Date, _ days:Int)->Date{
         //swift 3 update. may work, may not work. its not including: Calendar.Options.matchStrictly
-        let newDate:Date = Calendar.current.date(byAdding: .day, value: days, to: date)!//let newDate:Date = NSCalendar.current.dateByAddingUnit(NSCalendar.Unit.Day, value: days, toDate: date, options: Calendar.Options.matchStrictly)!
+        let calendar = Calendar.current
+        let newDate:Date = calendar.date(byAdding: .day, value: days, to: date)!//let newDate:Date = NSCalendar.current.dateByAddingUnit(NSCalendar.Unit.Day, value: days, toDate: date, options: Calendar.Options.matchStrictly)!
         return newDate
     }
 }
