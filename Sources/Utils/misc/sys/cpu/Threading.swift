573e4d089eb0ca41a920fbfc129652a79be6b849
Eonist <30n1st@gmail.com>
2017-02-28 14:46:51 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/sys/cpu/Threading.swift

--- Sources/Utils/misc/sys/cpu/Threading.swift
+++ Sources/Utils/misc/sys/cpu/Threading.swift
@@ -5,9 +5,11 @@ import Foundation
 //swift 3 update, remember that threading is simpler in swift 3
 
 var bgQueue = {return DispatchQueue.global(qos: DispatchQoS.QoSClass.background)}()//swift 3-> let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+var bg:DispatchQueue {return bgQueue}/*Convenience*/
 //there is also these: DISPATCH_QUEUE_PRIORITY_DEFAULT,DISPATCH_QUEUE_PRIORITY_HIGH,DISPATCH_QUEUE_PRIORITY_LOW
-var mainQueue = {return DispatchQueue.main}()
 
+var mainQueue = {return DispatchQueue.main}()
+var main:DispatchQueue {return mainQueue}/*Convenience*/
 //swift 3 update, The bellow line may work, but maybe not. Do more swift 3 async research
 func async(_ queue:DispatchQueue , _ block:@escaping () -> Void) -> Void {return queue.async(execute: block)}
 
