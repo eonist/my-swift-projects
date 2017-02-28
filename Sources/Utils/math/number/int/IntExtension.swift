ebea3a0de0d0a54bf4a11b8ee3b396effe8790e5
Eonist <30n1st@gmail.com>
2017-02-28 14:34:19 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/math/number/int/IntExtension.swift

--- Sources/Utils/math/number/int/IntExtension.swift
+++ Sources/Utils/math/number/int/IntExtension.swift
@@ -5,6 +5,7 @@ extension Int {
     var float:Float {return Float(self)}/*Convenince*/
     var string:String{return String(self)}/*Convenince*/
     var double:Double {return Double(self)}/*Convenince*/
+    var uint32:UInt32 {return UInt32(self)}/*Convenince*/
     func minMax(_ min:Int,_ max:Int)->Int{return NumberParser.minMax(self, min, max)}/*Convenince*/
     func clip(_ min:Int,_ max:Int)->Int{return NumberParser.clip(self, min, max)}/*Convenince*/
     var negative:Bool{return NumberAsserter.negative(self,0)}
