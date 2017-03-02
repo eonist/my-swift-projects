e10e9b89e91ae292a1ed586db51a33452b571e44
Eonist <30n1st@gmail.com>
2017-03-02 22:05:16 +0100

    'Files modified: 1'
    '
    
    Modified 1 file:
    Sources/Utils/math/number/int/IntExtension.swift
    '

--- Sources/Utils/math/number/int/IntExtension.swift
+++ Sources/Utils/math/number/int/IntExtension.swift
@@ -1,5 +1,7 @@
 import Foundation
-
+/**
+ * Don't add Int.init code that auto casts. like: let i:Int = 40.5//40 via ExpressibleByFloatLiteral and FloatLiteralType. It will accidentally break I.E Animator.swift
+ */
 extension Int {
     var cgFloat:CGFloat {return CGFloat(self)}/*Convenince*/
     var float:Float {return Float(self)}/*Convenince*/
@@ -10,12 +12,4 @@ extension Int {
     func clip(_ min:Int,_ max:Int)->Int{return NumberParser.clip(self, min, max)}/*Convenince*/
     var negative:Bool{return NumberAsserter.negative(self,0)}
     var positive:Bool{return NumberAsserter.positive(self,0)}
-}
-/**
- * EXAMPLE: let i:Int = 40.5//40
- */
-extension Int : ExpressibleByFloatLiteral {
-    public init(floatLiteral value: FloatLiteralType) {
-        self.init(Int(value))
-    }
 }
