ac079ef85c5c2d911af422fc14959e7c55328b50
Eonist <30n1st@gmail.com>
2017-03-04 19:00:38 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/Event.swift

--- Sources/Utils/misc/event/Event.swift
+++ Sources/Utils/misc/event/Event.swift
@@ -14,35 +14,3 @@ class Event{
         self.immediate = origin
     }
 }
-extension Event{
-    /**
-     * The immediate previouse sender of event
-     * NOTE: we return the event as its convenient when chaining method calls, great for attaching self to incoming events
-     */
-    func setImmediate(_ immediate:AnyObject)->Event{
-        self.immediate = immediate
-        return self
-    }
-    /**
-     * NOTE: we return the event as its convenient when chaining method calls
-     */
-    func setType(_ type:String)->Event{
-        self.type = type
-        return self
-    }
-    func cast<T>() -> T{/*Convenience*/
-        return self as! T
-    }
-    /**
-     * TODO: It would be cool if you could also add a call-back method as an argument, also if it could cary arguments, maybe asking too much
-     */
-    func assert(_ type:String,_ origin:AnyObject?) -> Bool{
-        return self.type == type && self.origin === origin
-    }
-    /**
-     * Convenience method for asserting event target
-     */
-    func assert(_ type:String, immediate:AnyObject?) -> Bool{
-        return self.type == type && self.immediate === immediate
-    }
-}
