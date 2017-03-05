5c988258fd53445647355a15705a44494b2bb4bf
Eonist <30n1st@gmail.com>
2017-03-05 17:03:37 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Mover.swift

--- Sources/Utils/misc/animation/movers/Mover.swift
+++ Sources/Utils/misc/animation/movers/Mover.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
-class Mover:BaseAnimation {
-    var value:CGFloat = 0/*the value that should be applied to the target*/
+public class Mover:BaseAnimation {
+    public var value:CGFloat = 0/*the value that should be applied to the target*/
     var velocity:CGFloat
     init(_ animatable:IAnimatable, _ value:CGFloat, _ velocity:CGFloat = 0){
         self.value = value
