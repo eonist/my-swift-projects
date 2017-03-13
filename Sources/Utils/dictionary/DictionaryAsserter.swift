0cb15e9e17cbc9931ffe0e8e342b3d4bb4889535
Eonist <30n1st@gmail.com>
2017-03-13 16:32:12 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryAsserter.swift

--- Sources/Utils/dictionary/DictionaryAsserter.swift
+++ Sources/Utils/dictionary/DictionaryAsserter.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class DictionaryAsserter {
-
+    /**
+     * Asserts if PARAM: dict has PARAM: key
+     */
+    static func hasKey<Key, Value>(_ dict:[Key: Value], _ key:Key) -> Bool{
+        return dict[key] != nil
+    }
 }
