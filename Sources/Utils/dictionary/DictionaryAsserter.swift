2f529612b820251800dd6f10c713aa580ac407e2
Eonist <30n1st@gmail.com>
2017-03-13 19:20:48 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryAsserter.swift

--- Sources/Utils/dictionary/DictionaryAsserter.swift
+++ Sources/Utils/dictionary/DictionaryAsserter.swift
@@ -7,4 +7,12 @@ class DictionaryAsserter {
     static func hasKey<Key, Value>(_ dict:[Key: Value], _ key:Key) -> Bool{
         return dict[key] != nil
     }
+    static func hasKeys<Key, Value>(_ dict:[Key: Value], _ keys:[Key]) -> Bool{
+        for myKey  in toppings.keys{
+            toppings[myKey] = toppings[myKey]! * 1.10
+        }
+        
+        dict.
+        //return dict[key] != nil
+    }
 }
