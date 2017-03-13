105d54704a4ce591a7b787f0f7cbce02f53f9aef
Eonist <30n1st@gmail.com>
2017-03-13 19:22:52 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryAsserter.swift

--- Sources/Utils/dictionary/DictionaryAsserter.swift
+++ Sources/Utils/dictionary/DictionaryAsserter.swift
@@ -7,9 +7,9 @@ class DictionaryAsserter {
     static func hasKey<Key, Value>(_ dict:[Key: Value], _ key:Key) -> Bool{
         return dict[key] != nil
     }
-    static func hasKeys<Key, Value>(_ dict:[Key: Value], _ keys:[Key]) -> Bool{
-        for myKey  in toppings.keys{
-            toppings[myKey] = toppings[myKey]! * 1.10
+    static func contains<Key, Value>(_ dict:[Key: Value], _ keys:[Key]) -> Bool{
+        for k in keys{
+            return dict.hasKey(k)
         }
         
         dict.
