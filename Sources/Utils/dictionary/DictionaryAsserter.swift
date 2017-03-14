4547168c38839906c4bd52782a71f5b58e82aa5f
Eonist <30n1st@gmail.com>
2017-03-14 11:15:23 +0100

    'Files modified: 1'
    '
    
    Modified 1 file:
    Sources/Utils/dictionary/DictionaryAsserter.swift
    '

--- Sources/Utils/dictionary/DictionaryAsserter.swift
+++ Sources/Utils/dictionary/DictionaryAsserter.swift
@@ -7,6 +7,9 @@ class DictionaryAsserter {
     static func hasKey<Key, Value>(_ dict:[Key: Value], _ key:Key) -> Bool{
         return dict[key] != nil
     }
+    /**
+     * TODO: It's possible to add a bool flag to make it more precise. see legacy code for implimentation
+     */
     static func contains<Key, Value>(_ dict:[Key: Value], _ keys:[Key]) -> Bool{
         for k in keys{
             return dict.hasKey(k)
