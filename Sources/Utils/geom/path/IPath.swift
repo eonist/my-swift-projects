6b7430d947d1979780ba2d091d8b0d1d82a95159
Eonist <30n1st@gmail.com>
2017-03-18 12:18:03 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/path/IPath.swift

--- Sources/Utils/geom/path/IPath.swift
+++ Sources/Utils/geom/path/IPath.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 protocol IPath:class{/*<-When you extend class you can mutate variables directly when casting to IPath, why because scruts don't allow this and so on..*/
-    var commands:Array<Int>{get set}
-    var pathData:Array<CGFloat>{get set}
+    var commands:[Int]{get set}
+    var pathData:[CGFloat]{get set}
     var rotation:CGFloat{get set}
     var name:String{get set}
 }
