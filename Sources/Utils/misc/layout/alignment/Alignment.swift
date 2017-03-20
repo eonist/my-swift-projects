f886ba56344cbc0540608e1300c4464b2d66e2ef
Eonist <30n1st@gmail.com>
2017-03-20 13:38:16 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/alignment/Alignment.swift

--- Sources/Utils/misc/layout/alignment/Alignment.swift
+++ Sources/Utils/misc/layout/alignment/Alignment.swift
@@ -5,13 +5,13 @@ import Foundation
 class Alignment {
     static var horisontal:String = "horisontal"
     static var vertical:String = "vertical"
-    
+    /**/
     static var left:String = "left"
     static var right:String = "right"
     static var top:String = "top"
     static var bottom:String = "bottom"
     static var center:String = "center"
-    
+    /**/
     static var topLeft:String = "topLeft"
     static var topCenter:String = "topCenter"
     static var topRight:String = "topRight"
@@ -21,6 +21,6 @@ class Alignment {
     static var centerLeft:String = "centerLeft"
     static var centerRight:String = "centerRight"
     static var centerCenter:String = "centerCenter" // :TODO: remove when transitioning to new lib// we have this when we use center in y and x axis? should be remved, or should it be removed?
-    
+    /**/
     static var none:String = "none"
 }
