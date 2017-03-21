61686fb3af7659a6d6f486e84d192bc666ad0dbf
Eonist <30n1st@gmail.com>
2017-03-21 14:55:22 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/SnapFriction.swift

--- Sources/Utils/misc/animation/movers/SnapFriction.swift
+++ Sources/Utils/misc/animation/movers/SnapFriction.swift
@@ -6,7 +6,6 @@ class SnapFriction:Friction {
     init(_ view:IAnimatable,  _ callBack:@escaping (CGFloat)->Void,_ value:CGFloat, _ velocity:CGFloat = 0, _ frictionStrength:CGFloat = 0.98, _ snap:CGFloat = 0){
         self.snap = snap
         super.init(view, callBack, value, velocity, frictionStrength)
-        
     }
     override func applyFriction() {
         //keep some velocity alive
