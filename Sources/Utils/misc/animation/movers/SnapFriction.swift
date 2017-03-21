21bb6cf03a2972ee61ccc8bf0839e83db38e9448
Eonist <30n1st@gmail.com>
2017-03-21 14:27:51 +0100

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/SnapFriction.swift

--- Sources/Utils/misc/animation/movers/SnapFriction.swift
+++ Sources/Utils/misc/animation/movers/SnapFriction.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class SnapFriction {
-
+class SnapFriction:Friction {
+    init(_ view:IAnimatable,  _ callBack:@escaping (CGFloat)->Void,_ value:CGFloat, _ velocity:CGFloat = 0, _ frictionStrength:CGFloat = 0.98, _ ){
+        self.frictionStrength = frictionStrength
+        super.init(view, callBack, value, velocity)
+        
+    }
+    func applyFriction() {
+        velocity *= frictionStrength
+        velocity *= slowDownFriction/*ad-hock way to slow things down outside normal friction*/
+    }
 }
