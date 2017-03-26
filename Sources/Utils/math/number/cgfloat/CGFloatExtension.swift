36086b6e0ec2c50c42e62e48cd60f73fb41476f6
Eonist <30n1st@gmail.com>
2017-03-26 19:26:15 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/math/number/cgfloat/CGFloatExtension.swift

--- Sources/Utils/math/number/cgfloat/CGFloatExtension.swift
+++ Sources/Utils/math/number/cgfloat/CGFloatExtension.swift
@@ -13,7 +13,9 @@ extension CGFloat {
     var isPositive:Bool{return NumberAsserter.positive(self,0)}
     func clip(_ min:CGFloat,_ max:CGFloat)->CGFloat{return NumberParser.clip(self, min, max)}/*Convenince*/
     func interpolate(_ to:CGFloat, _ fraction:CGFloat) -> CGFloat{return CGFloatParser.interpolate(self,to,fraction)}
-    var average:CGFloat {CGFloat}
+}
+extension Array where Element:CGFloat{
+    var average:CGFloat {CGFloatParser.average(self)}
 }
 /**
  * Swift 3 removed the possibility to cast CGFloat to Bool This method brings back this functionality.
