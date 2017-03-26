68dbd7a999cef8a04fde3df25ceaa441262377fd
Eonist <30n1st@gmail.com>
2017-03-26 22:42:45 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/size/CGSizeExtensions.swift

--- Sources/Utils/geom/size/CGSizeExtensions.swift
+++ Sources/Utils/geom/size/CGSizeExtensions.swift
@@ -6,6 +6,22 @@ extension CGSize {
     init(_ width:Int,_ height:Int){self.width = CGFloat(width);self.height = CGFloat(height)}
     var w:CGFloat {set {self.width = newValue} get {return self.width}}
     var h:CGFloat {set {self.height = newValue} get {return self.height}}
+    
+    subscript(dir:Dir) -> CGFloat {/*Convenience*/
+        get {
+            if(dir == .hor){
+                return self.width
+            }else if(dir == .ver){
+                return self.height
+            }else{fatalError("not supported")}
+        }set {
+            if(dir == .hor){
+                self.width = newValue
+            }else if(dir == .ver){
+                self.height = newValue
+            }else{fatalError("not supported")}
+        }
+    }
 }
 public func +(a: CGSize, b: CGSize) -> CGSize { return CGSize(a.width + b.width,a.height + b.height)}
 public func -(a: CGSize, b: CGSize) -> CGSize { return CGSize(a.width - b.width,a.height - b.height)}
