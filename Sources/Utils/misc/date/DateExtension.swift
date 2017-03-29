8ea05272b01829d9f9373037aacd00a01d461da9
Eonist <30n1st@gmail.com>
2017-03-29 22:19:55 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/date/DateExtension.swift

--- Sources/Utils/misc/date/DateExtension.swift
+++ Sources/Utils/misc/date/DateExtension.swift
@@ -1,25 +1,11 @@
 import Foundation
 
 extension Date {
-    /**
-     * Returns a date instance for time components like: 2016,11,03,15,49,59
-     */
-    static func createDate(_ year:Int? = nil,_ month:Int? = nil,_ day:Int? = nil,_ hour:Int? = nil,_ minute:Int? = nil,_ second:Int? = nil)->Date?{
+    static func createDate(_ year:Int? = nil,_ month:Int? = nil,_ day:Int? = nil,_ hour:Int? = nil,_ minute:Int? = nil,_ second:Int? = nil)->Date?{/*Convenience*/
         return DateParser.createDate(year, month, day, hour, minute, second)
     }
-    /**
-     * beta
-     * TODO: move into DateParser
-     */
-    var numOfDaysInMonth:Int{
-        //parts to NSDate:
-        let cal = Calendar.current
-        //range(of: NSCalendar.Unit.day, in: NSCalendar.Unit.month, for: self)
-        let days = cal.range(of: .day, in: .month, for: self)
-        //Swift.print("days: " + "\(days)")
-        //Swift.print("days.location: " + "\(days.location)")
-        //Swift.print("days.length: " + "\(days.length)")
-        return days!.length//swift 3 issue<-fix the RangeExtensions.swift and this will work
+    var numOfDaysInMonth:Int{/*Convenience*/
+        return DateParser.numOfDaysInMonth(self)
     }
     var dayName:String? {/*Convenience*/
         return DateParser.dayName(self)
