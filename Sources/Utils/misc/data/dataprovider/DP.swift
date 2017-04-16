25de4fe2a8257cf7b7a1382e9012d8f0e987c5a5
Eonist <30n1st@gmail.com>
2017-04-16 21:16:56 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/data/dataprovider/DP.swift

--- Sources/Utils/misc/data/dataprovider/DP.swift
+++ Sources/Utils/misc/data/dataprovider/DP.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 struct DP:DataProvidable,EventSendable {
-    var event:EventCallBack/*this holds any method assigned to it that has its type*/
-    var eventCall:EventCallBack {//TODO:this should probably be private
-        return {
-            (event:Event) -> Void in
-        }
+    var event:EventCallBack = {return{(event:Event) -> Void in}}()
+    func onEvent(_ event:Event){
+        //self.event!(event.setImmediate(self))
     }
     var items:[[String:String]]//ideally it should be string,AnyObject//TODO:Maybe make this public getter private setter
     var count:Int{return self.items.count}
     
     init(_ items:[[String:String]] = []){
         self.items = items
-        event = eventCall
+        //event = eventCall
     }
 }
