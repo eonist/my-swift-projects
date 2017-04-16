f89216a646659aef2afb14254af0e295fab462f4
Eonist <30n1st@gmail.com>
2017-04-16 21:11:57 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/data/dataprovider/DP.swift

--- Sources/Utils/misc/data/dataprovider/DP.swift
+++ Sources/Utils/misc/data/dataprovider/DP.swift
@@ -1,10 +1,17 @@
 import Foundation
 
 struct DP:DataProvidable,IEventSender {
+    var event:EventCallBack/*this holds any method assigned to it that has its type*/
+    var eventCall:EventCallBack {//TODO:this should probably be private
+        return {
+            (event:Event) -> Void in
+        }
+    }
     var items:[[String:String]]//ideally it should be string,AnyObject//TODO:Maybe make this public getter private setter
     var count:Int{return self.items.count}
     
     init(_ items:[[String:String]] = []){
         self.items = items
+        event = eventCall
     }
 }
