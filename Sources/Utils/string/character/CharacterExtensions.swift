3d10b6ac1dbdd07bee65d0cd6cc937b2806775e1
Eonist <30n1st@gmail.com>
2017-04-16 14:31:36 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/character/CharacterExtensions.swift

--- Sources/Utils/string/character/CharacterExtensions.swift
+++ Sources/Utils/string/character/CharacterExtensions.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class CharacterExtensions {
-
+extension Character {
+    var cgFloat:CGFloat{return CGFloat(Double(String(self))!)}
+    var double:Double{return Double(String(self))!}
+    var uint:UInt{return UInt(Float(String(self))!)}
+    var int:Int{return Int(String(self))!}
 }
