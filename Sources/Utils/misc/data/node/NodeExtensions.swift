e51db121539b8ded82ec3f9f43274371ec92a6ae
Eonist <30n1st@gmail.com>
2017-04-17 14:59:32 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/data/node/NodeExtensions.swift

--- Sources/Utils/misc/data/node/NodeExtensions.swift
+++ Sources/Utils/misc/data/node/NodeExtensions.swift
@@ -1,5 +1,27 @@
 import Foundation
 
-class NodeExtensions {
-
+extension Node{
+    /**
+     * TODO: consider renaming to appendAt
+     */
+    func addAt(_ index:[Int],_ xml:XML){// :TODO: shouldn't the arguments be in this order: xml, index// :TODO: do we still need the event dispatching, cant the calling method do this?
+        _ = XMLModifier.addChildAt(self.xml, index, xml)
+        onEvent(NodeEvent(NodeEvent.addAt,index,self))
+    }
+    /**
+     * EXAMPLE: setAttributeAt([0], ["title":"someTitle"]);
+     * TODO: rename to changeAttribute? or editAttribute?
+     */
+    func setAttributeAt(_ index:[Int],_ attributes:[String:String]){// :TODO: do we still need the event dispatching, cant the calling method do this?
+        _ = XMLModifier.setAttributeAt(xml, index, attributes)
+        onEvent(NodeEvent(NodeEvent.setAttributeAt,index,self))
+    }
+    /**
+     * Removes the item PARAM: index
+     */
+    func removeAt(_ index:[Int])->XML{// :TODO: do we still need the event dispatching, can't the calling method do this?
+        let removedXML:XML = XMLModifier.removeChildAt(xml, index)
+        onEvent(NodeEvent(NodeEvent.removeAt,index,self))
+        return removedXML
+    }
 }
