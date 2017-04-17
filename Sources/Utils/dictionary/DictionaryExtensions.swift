7ca932e3ec97a3c7992b9be9fb1cb681f2fe30d9
Eonist <30n1st@gmail.com>
2017-04-17 15:31:34 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/dictionary/DictionaryExtensions.swift

--- Sources/Utils/dictionary/DictionaryExtensions.swift
+++ Sources/Utils/dictionary/DictionaryExtensions.swift
@@ -8,7 +8,7 @@ extension Dictionary:AnyDict{}
 
 extension AnyDict {
     var xml:XML{
-        return DictionaryParser.xml(self as! Dictionary<String, String>)
+        return DictionaryParser.xml(self as! [String:String])
     }
 }
 /**
