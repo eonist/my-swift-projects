37ea96bb2c5c1f58ebae865fd83e7d3c9be774fe
Eonist <30n1st@gmail.com>
2017-04-22 15:59:18 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/view/utils/NSViewParser.swift

--- Sources/Utils/misc/view/utils/NSViewParser.swift
+++ Sources/Utils/misc/view/utils/NSViewParser.swift
@@ -8,11 +8,11 @@ class NSViewParser {
      * TODO: rename to childrenByClassType, maybe not?, I say not!
      */
     static func childrenOfType<T>(_ view:NSView, _ type:T.Type)->[T] {
-        
-        return view.subviews.filter() {
-            if($0 as? T != nil){ return $0 as! T}
+        var arr:[T] = []
+        view.subviews.forEach {
+            if($0 as? T != nil){arr.append($0 as! T)}
         }
-     
+        return arr
     }
     /**
      * Returns a list of parents (this works, tested)
