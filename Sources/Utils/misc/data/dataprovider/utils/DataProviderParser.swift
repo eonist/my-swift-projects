084f36821dd0660ab629073e8754bda84bf40f8a
Eonist <30n1st@gmail.com>
2017-04-24 15:57:10 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/data/dataprovider/utils/DataProviderParser.swift

--- Sources/Utils/misc/data/dataprovider/utils/DataProviderParser.swift
+++ Sources/Utils/misc/data/dataprovider/utils/DataProviderParser.swift
@@ -5,11 +5,6 @@ class DataProviderParser {
      * TODO: Use a real Range type here
      */
     static func itemsInRange(_ start:Int, _ end:Int,_ items:[[String:String]]) -> [[String:String]]{
-        /*var theItems:[Dictionary<String,String>] = []
-         for i in start..<end{//swift 3 upgrade, we could also use subRange here -> arr[start..<end]
-            theItems.append(items[i])
-         }
-         return theItems*/
         return items.slice2(start, end)
     }
     /**
