328e4a5135e56af199158d61ee639f228100274c
Eonist <30n1st@gmail.com>
2017-04-25 15:21:50 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/utils/SVGLineStyleUtils.swift

--- Sources/Utils/geom/svg/style/utils/SVGLineStyleUtils.swift
+++ Sources/Utils/geom/svg/style/utils/SVGLineStyleUtils.swift
@@ -36,41 +36,53 @@ class SVGLineStyleUtils{
         let userSpaceOnUse:Bool = gradient.gradientUnits == "userSpaceOnUse"/*The gradientUnits attribute takes two familiar values, userSpaceOnUse and objectBoundingBox, which determine whether the gradient scales with the element that references it or not. It determines the scale of x1, y1, x2, y2.*/
         switch gradient{
             case let linearGradient as SVGLinearGradient:/*gradient is SVGRadialGradient */
-                var p1:CGPoint = /*userSpaceOnUse && !gradient.x1.isNaN && !gradient.y1.isNaN ? */CGPoint(linearGradient.x1,linearGradient.y1).copy()/* :nil*/
-                var p2:CGPoint = /*userSpaceOnUse && !gradient.x2.isNaN && !gradient.y2.isNaN ? */CGPoint(linearGradient.x2,linearGradient.y2).copy()/* :nil*/
-                if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
-                    if(gradient.gradientTransform != nil){
-                        p1 = p1.applying(gradient.gradientTransform!)
-                        p2 = p2.applying(gradient.gradientTransform!)
-                    }
-                    p1 -= shape.frame.origin
-                    p2 -= shape.frame.origin
-                }else{/*objectBoundingBox*/
-                    //if(gradient.gradientTransform != nil){fatalError("transform here is not supported yet")}//you need to implement individual scaling of gradients first
-                    let boundingBox:CGRect = shape.frame.copy()//we could use the SVGStyleUtils.boundingBox which uses the Outline of the stroked line but this is already calculated and used in the frame, so we reuse that
-                    p1.x = boundingBox.width * (p1.x / 100)//this and the following lines of code can be compacted into 1 line
-                    p1.y = boundingBox.height * (p1.y / 100)
-                    p2.x = boundingBox.width * (p2.x / 100)
-                    p2.y = boundingBox.height * (p2.y / 100)
-                    //fatalError("relative values for gradient stroke isnt implemented yet, see similar code for gradient fill to impliment this")
-                }
-                return LinearGraphicsGradient(gradient.colors,gradient.offsets,nil/*gradient.gradientTransform*/,p1,p2)
+                return Utils.linearGradient(shape, linearGradient, userSpaceOnUse)
             case let radialGradient as SVGRadialGradient:/*radial*/
-                //Swift.print("radialGradient: " + "\(radialGradient)")
-                let startCenter:CGPoint = CGPoint(!radialGradient.fx.isNaN ? radialGradient.fx : radialGradient.cx,!radialGradient.fy.isNaN ? radialGradient.fy : radialGradient.cy)/*if fx or fy isnt found use cx and cy as replacments*/
-                let endCenter:CGPoint = CGPoint(radialGradient.cx,radialGradient.cy)
-                var transformation:CGAffineTransform = CGAffineTransform.identity
-                if(radialGradient.gradientTransform != nil) {
-                    transformation = radialGradient.gradientTransform!.copy()
-                }
-                if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
-                    transformation.concat(CGAffineTransform(translationX: -shape.frame.origin.x, y: -shape.frame.origin.y))//swift 3 upgrade
-                }else{fatalError("relative values for gradient stroke isnt implemented yet, see similar code for gradient fill to impliment this")}
-                let startRadius:CGFloat = 0
-                let endRadius:CGFloat = radialGradient.r
-                return RadialGraphicsGradient(radialGradient.colors,radialGradient.offsets,transformation/*nil*/,startCenter,endCenter,startRadius,endRadius)
+                return Utils.radialGradient(shape, radialGradient, userSpaceOnUse)
             default:
                 fatalError("gradient type not supported")
         }
+    }
+}
+
+private class Utils{
+    /**
+     *
+     */
+    static func linearGradient(_ shape:Shape,_ gradient:SVGLinearGradient,_ userSpaceOnUse:Bool)->IGraphicsGradient {
+        var p1:CGPoint = /*userSpaceOnUse && !gradient.x1.isNaN && !gradient.y1.isNaN ? */CGPoint(linearGradient.x1,linearGradient.y1).copy()/* :nil*/
+        var p2:CGPoint = /*userSpaceOnUse && !gradient.x2.isNaN && !gradient.y2.isNaN ? */CGPoint(linearGradient.x2,linearGradient.y2).copy()/* :nil*/
+        if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
+            if(gradient.gradientTransform != nil){
+                p1 = p1.applying(gradient.gradientTransform!)
+                p2 = p2.applying(gradient.gradientTransform!)
+            }
+            p1 -= shape.frame.origin
+            p2 -= shape.frame.origin
+        }else{/*objectBoundingBox*/
+            //if(gradient.gradientTransform != nil){fatalError("transform here is not supported yet")}//you need to implement individual scaling of gradients first
+            let boundingBox:CGRect = shape.frame.copy()//we could use the SVGStyleUtils.boundingBox which uses the Outline of the stroked line but this is already calculated and used in the frame, so we reuse that
+            p1.x = boundingBox.width * (p1.x / 100)//this and the following lines of code can be compacted into 1 line
+            p1.y = boundingBox.height * (p1.y / 100)
+            p2.x = boundingBox.width * (p2.x / 100)
+            p2.y = boundingBox.height * (p2.y / 100)
+            //fatalError("relative values for gradient stroke isnt implemented yet, see similar code for gradient fill to impliment this")
+        }
+        return LinearGraphicsGradient(gradient.colors,gradient.offsets,nil/*gradient.gradientTransform*/,p1,p2)
+    }
+    static func radialGradient(_ shape:Shape,_ radialGradient:SVGRadialGradient,_ userSpaceOnUse:Bool)->IGraphicsGradient {
+        //Swift.print("radialGradient: " + "\(radialGradient)")
+        let startCenter:CGPoint = CGPoint(!radialGradient.fx.isNaN ? radialGradient.fx : radialGradient.cx,!radialGradient.fy.isNaN ? radialGradient.fy : radialGradient.cy)/*if fx or fy isnt found use cx and cy as replacments*/
+        let endCenter:CGPoint = CGPoint(radialGradient.cx,radialGradient.cy)
+        var transformation:CGAffineTransform = CGAffineTransform.identity
+        if(radialGradient.gradientTransform != nil) {
+            transformation = radialGradient.gradientTransform!.copy()
+        }
+        if(userSpaceOnUse){/*we offset the p1,p2 to operate in the 0,0 space that the path is drawn in, inside frame*/
+            transformation.concat(CGAffineTransform(translationX: -shape.frame.origin.x, y: -shape.frame.origin.y))//swift 3 upgrade
+        }else{fatalError("relative values for gradient stroke isnt implemented yet, see similar code for gradient fill to impliment this")}
+        let startRadius:CGFloat = 0
+        let endRadius:CGFloat = radialGradient.r
+        return RadialGraphicsGradient(radialGradient.colors,radialGradient.offsets,transformation/*nil*/,startCenter,endCenter,startRadius,endRadius)
     }
 }
