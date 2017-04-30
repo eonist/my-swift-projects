0190cee0c59f1ce7a5be2946d6852c877b2e9f41
Eonist <30n1st@gmail.com>
2017-04-30 23:42:30 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/Event.swift

--- Sources/Utils/misc/event/Event.swift
+++ Sources/Utils/misc/event/Event.swift
@@ -3,6 +3,7 @@ import Foundation
  * TODO: Implement the immediate variable if its needed (it would be a way to get assert the 1-level down immediate child an event came from, rather than the origin child which can be many levels deeper in the hierarchy)
  * NOTE: Event could be a struct, most Event classes are really simple and their passed data could be accessed via origin, maybe try using a struct for Element iOS
  * TODO: Event should extend string or stringconvertible etc. So that one can do onEvent(.update) the subclasses can do .mouseDown etc
+ * FUTURE: ⚠️️ The idea with events and enum is that you make Event generic so it can take many types of enums
  */
 class Event{
     static var update:String = "eventUpdate"/*Ideally I would name this change but apparently then subclasses can name their const the same*/
