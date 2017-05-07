6440e3871fd4f19edcd9f7cc12cbe9c0c2427910
Eonist <30n1st@gmail.com>
2017-05-07 15:41:42 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/context/Graphic.swiftSources/Utils/misc/view/InteractiveView2.swift

--- Sources/Utils/misc/view/InteractiveView2.swift
+++ Sources/Utils/misc/view/InteractiveView2.swift
@@ -26,6 +26,7 @@ class InteractiveView2:FlippedView,IInteractiveView{//TODO: rename this with app
     override init(frame frameRect:NSRect) {
         super.init(frame:frameRect)//<--maybe: MTLSystemCreateDefaultDevice()
         self.wantsLayer = true/*if true then view is layer backed*/
+        self.layerContentsRedrawPolicy = .onSetNeedsDisplay//= NSViewLayerContentsRedrawOnSetNeedsDisplay
         layer = CALayer()/*needs to be layer-hosted so that we dont get clipping of children*/
         layer!.masksToBounds = false/*This is the variable that makes subchildren mask its parents frame, set it to false and they wont mask*/
         event = eventCall/*By default we assign the propegation closure to the event, this event may be overridden in other classes, which leads to the event beeing redirected, one can always assign the default behaviour back */
