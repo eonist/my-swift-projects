dcfb64f530cb8c2d12875bb409bda8ed9acac9b4
Eonist <30n1st@gmail.com>
2017-05-08 16:24:01 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/context/Graphic.swift

--- Sources/Utils/geom/context/Graphic.swift
+++ Sources/Utils/geom/context/Graphic.swift
@@ -50,6 +50,9 @@ class Graphic:InteractiveView2,IGraphic,CALayerDelegate{//swift 3 update, NSView
         
         Swift.print("localPos(): " + "\(localPos())")
         
+        let rect = window?.convertToScreen(NSRect(0,0,100,50))
+        Swift.print("rect: " + "\(rect)")
+        
         localPoint -= fillShape.frame.origin//<--quick fix, when margin or offset is applied, they act on the frame not the path. They shouldn't but they do so this is a quick fix. Resolve this later and do it better, one could argu that moving frame is cheaper than rerendering shape
         let isPointInside:Bool = fillShape.path.contains(localPoint)
         return isPointInside ? self : super.hitTest(aPoint)/*return nil will tell the parent that there was no hit on this view*/
