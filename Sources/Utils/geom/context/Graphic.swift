44717638cf011d749a457074103074ad58df1309
Eonist <30n1st@gmail.com>
2017-05-08 12:59:23 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/context/Graphic.swift

--- Sources/Utils/geom/context/Graphic.swift
+++ Sources/Utils/geom/context/Graphic.swift
@@ -25,7 +25,7 @@ class Graphic:InteractiveView2,IGraphic,CALayerDelegate{//swift 3 update, NSView
         layer?.addSublayer(lineShape)
         self.fillShape.delegate = self/* ⚠️️ IMPORTANT ⚠️️: this is needed in order to be able to retrive the context and use it whithin the decoratable methods, or else the context would reside isolated inside the Graphic.fillShape, and Graphic.lineShape*/
         self.lineShape.delegate = self
-        self.layerContentsRedrawPolicy = .onSetNeedsDisplay/*Supposedly this makes anim fast, may or may not have an effect, try diable and enable it from time to time*/
+        //self.layerContentsRedrawPolicy = .onSetNeedsDisplay/*Supposedly this makes anim fast, may or may not have an effect, try diable and enable it from time to time*/
     }
     /**
      * Stops implicit animation from happening
