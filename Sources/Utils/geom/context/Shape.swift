37d6b9babb1e03a2ce95d1d1eac8b6675e0c62ba
Eonist <30n1st@gmail.com>
2017-05-08 13:04:27 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/context/Shape.swiftSources/Utils/misc/view/InteractiveView2.swift

--- Sources/Utils/geom/context/Shape.swift
+++ Sources/Utils/geom/context/Shape.swift
@@ -13,16 +13,15 @@ class Shape:CALayer{
         super.init()
         self.contentsScale = 2.0/*<--Makes lines smooth on retina screens, you may wan't to consider toggeling this on non-retina displays. From apple docs: The default value of this property is 1.0. For layers attached to a view, the view changes the scale factor automatically to a value that is appropriate for the current screen. For layers you create and manage yourself, you must set the value of this property yourself based on the resolution of the screen and the content you are providing. Core Animation uses the value you specify as a cue to determine how to render your content.*/
     }
+    
+    override func hitTest(_ p:CGPoint) -> CALayer? {
+        return super.hitTest(p)
+    }
+    override func setNeedsDisplay() {
+        super.setNeedsDisplay()
+    }
+    override func draw(in ctx:CGContext) {
+        super.draw(in:ctx)
+    }
     required init?(coder aDecoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
-
-/*
- override func hitTest(_ p:CGPoint) -> CALayer? {
-    return super.hitTest(p)
- }
- override func setNeedsDisplay() {
-    super.setNeedsDisplay()
- }
- override func draw(in ctx:CGContext) {
-    super.draw(in:ctx)
- }*/
