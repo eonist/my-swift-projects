6b62a8b7b3f6627b767fb96683f28724dee03650
Eonist <30n1st@gmail.com>
2017-05-08 12:38:32 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/view/IView.swift

--- Sources/Utils/misc/view/IView.swift
+++ Sources/Utils/misc/view/IView.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class IView {
-
+protocol IView:class{/*the :class enables you to set variables via extension etc*/
+    var layer:CALayer?{get}//TODO:remove?
+    var bounds:NSRect{get}
+    var frame:NSRect{get set}
+    func addSubview(_ aView: NSView)
 }
