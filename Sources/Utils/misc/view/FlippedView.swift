0534b39c17dd813cfb4a048d8c06e7621667b9c1
Eonist <30n1st@gmail.com>
2017-05-08 12:33:34 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/view/FlippedView.swiftSources/Utils/misc/view/InteractiveView2.swift

--- Sources/Utils/misc/view/FlippedView.swift
+++ Sources/Utils/misc/view/FlippedView.swift
@@ -1,12 +1,13 @@
 import Cocoa
 import Foundation
 
-protocol IView:class{/*the :class enables you to set variables via extension etc*/
-    var layer:CALayer?{get}//TODO:remove?
-    var bounds:NSRect{get}
-    var frame:NSRect{get set}
-    func addSubview(_ aView: NSView)
-}
 class FlippedView:NSView{
     override var isFlipped:Bool {return true}/*Organizes your view from top to bottom*/
 }
+
+/*protocol IView:class{/*the :class enables you to set variables via extension etc*/
+ var layer:CALayer?{get}//TODO:remove?
+ var bounds:NSRect{get}
+ var frame:NSRect{get set}
+ func addSubview(_ aView: NSView)
+ }*/
