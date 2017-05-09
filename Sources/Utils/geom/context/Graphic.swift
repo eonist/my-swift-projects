0850d623ce69deb7dcee715e6429693325bd3238
Eonist <30n1st@gmail.com>
2017-05-09 12:18:25 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/context/Graphic.swift

--- Sources/Utils/geom/context/Graphic.swift
+++ Sources/Utils/geom/context/Graphic.swift
@@ -54,7 +54,7 @@ class Graphic:InteractiveView2,IGraphic,CALayerDelegate{//swift 3 update, NSView
      * Converts global p to local p
      */
     func globToLoc(_ p:CGPoint)->CGPoint{
-        
+        Swift.print("winMousePos: " + "\()")
         let flippedPoint = localPos()//convert(p, from: self.window?.contentView)//self.convert(p,from:nil)/*Converts the point to flipped geometry*/
         Swift.print("flippedPoint: " + "\(flippedPoint)")
         let offset = globalPos()
