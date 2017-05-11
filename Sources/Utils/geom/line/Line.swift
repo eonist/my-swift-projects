97333ea3a60feb8ebcadff5192780eab25bec051
Eonist <30n1st@gmail.com>
2017-05-11 13:43:45 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/line/Line.swiftSources/Utils/geom/line/utils/LineAsserter.swift

--- Sources/Utils/geom/line/Line.swift
+++ Sources/Utils/geom/line/Line.swift
@@ -6,7 +6,7 @@ typealias Line = CGLine /*legacySupport*/
  * NOTE: Used to be named: PrimitiveLine
  * TODO: rename to CGLine (Occupying the name Line for something so simple isn't necessary)
  */
-class CGLine {
+struct CGLine {
     var p1:CGPoint
     var p2:CGPoint
     init(_ p1:CGPoint,_ p2:CGPoint) {
--- Sources/Utils/geom/line/utils/LineAsserter.swift
+++ Sources/Utils/geom/line/utils/LineAsserter.swift
@@ -5,7 +5,7 @@ typealias CGLineAsserter = LineAsserter
  * There is about 20 other lineAssert methods in legacy code
  */
 class LineAsserter {
-    static func intersects(_ a:Line,_ b:Line) -> Bool {// :TODO: rename to isSomeName
+    static func intersects(_ a:Line,_ b:Line) -> Bool {// :TODO: ⚠️️ rename to isSomeName
         return PointAsserter.intersects(a.p1, a.p2, b.p1, b.p2)
     }
     static func oppositeDirectional(_ bisector:CGLine,_ a:CGLine,_ b:CGLine) -> Bool {// :TODO: rename to isSomeName
