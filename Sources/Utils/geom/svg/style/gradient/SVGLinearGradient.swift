9c210429e95ffd6f7d45b5b0a300afd07cf1c941
Eonist <30n1st@gmail.com>
2017-05-11 23:52:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/gradient/SVGLinearGradient.swift

--- Sources/Utils/geom/svg/style/gradient/SVGLinearGradient.swift
+++ Sources/Utils/geom/svg/style/gradient/SVGLinearGradient.swift
@@ -16,17 +16,7 @@ class SVGLinearGradient:SVGGradientDecoratable {
 		self.gradient = SVGGradient.init(offsets, colors, spreadMethod, id, gradientUnits, gradientTransform)
 	}
 }
-protocol SVGGradientDecoratable:ISVGGradient{
-    var gradient:ISVGGradient {get set}
-}
-extension SVGGradientDecoratable{
-    var element:ISVGElement {get{return gradient.element} set{gradient.element = newValue}}
-    var offsets:[CGFloat] {get{return gradient.offsets} set{gradient.offsets = newValue}}
-    var colors:[CGColor] {get{return gradient.colors} set{gradient.colors = newValue}}
-    var spreadMethod:String {get{return gradient.spreadMethod} set{gradient.spreadMethod = newValue}}
-    var gradientUnits:String {get{return gradient.gradientUnits} set{gradient.gradientUnits = newValue}}
-    var gradientTransform:CGAffineTransform? {get{return gradient.gradientTransform} set{gradient.gradientTransform = newValue}}
-}
+
 /**
  * Convenience
  */
