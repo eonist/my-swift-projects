526f1a7b9919d098e20f23c8cb2cc2d4dffa5d25
Eonist <30n1st@gmail.com>
2017-05-11 21:57:49 +0200

    Files modified: 3
    Modified 3 files:Sources/Utils/geom/svg/path/command/SVGPathCommand.swiftSources/Utils/geom/svg/path/utils/SVGPathParser.swiftSources/Utils/geom/svg/path/utils/SVGPathUtils.swift

--- Sources/Utils/geom/svg/path/command/SVGPathCommand.swift
+++ Sources/Utils/geom/svg/path/command/SVGPathCommand.swift
@@ -14,11 +14,11 @@ enum SVGPathCommand:String {
     case v = "v"/*Vertical*/
     case z = "z"/*Return to last Move*/
 }
-extension SVGPathCommand{
-    /**
-     * Possibly a bug in swift
-     */
-    static func initiate(rawValue: SVGPathCommand.RawValue) -> SVGPathCommand? {
-        return SVGPathCommand.init(rawValue: rawValue)
-    }
-}
+/*extension SVGPathCommand{
+ /**
+ * Possibly a bug in swift
+ */
+ static func initiate(rawValue: SVGPathCommand.RawValue) -> SVGPathCommand? {
+ return SVGPathCommand.init(rawValue: rawValue)
+ }
+ }*/
--- Sources/Utils/geom/svg/path/utils/SVGPathParser.swift
+++ Sources/Utils/geom/svg/path/utils/SVGPathParser.swift
@@ -64,7 +64,7 @@ class SVGPathParser {
 			let command:String = commands[e]
 			let isLowerCase:Bool = StringAsserter.lowerCase(command)
 			var pos:CGPoint = isLowerCase ? prevP.copy() : CGPoint()
-			switch SVGPathCommand.initiate(rawValue:command.lowercased()) {
+			switch SVGPathCommand(rawValue:command.lowercased()) {
 				case .some(.m), .some(.l): /*lineTo,moveTo*/
 					pos += CGPoint(params[i+0],params[i+1])
 					i += 2
