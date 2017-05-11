0631c8624483d4b60f90ed4c302d2e358a25a96e
Eonist <30n1st@gmail.com>
2017-05-11 19:00:43 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/utils/SVGStyleModifier.swift

--- Sources/Utils/geom/svg/style/utils/SVGStyleModifier.swift
+++ Sources/Utils/geom/svg/style/utils/SVGStyleModifier.swift
@@ -5,7 +5,7 @@ class SVGStyleModifier {
      * Merges if a value isn't present already
      * TODO: ⚠️️ a boolean flag should toggle overide if a value exists already
      */
-    static func merge(_ a:SVGStyle,_ b:SVGStyle){
+    static func merge(_ a:inout SVGStyle,_ b:SVGStyle){
         if((!(a.fill is Double) && !(b.fill is Double)) || ((a.fill as! Double).isNaN && !(b.fill as! Double).isNaN)) {a.fill = b.fill}/*Fill*/
         if((a.fillOpacity == nil || a.fillOpacity!.isNaN) && (b.fillOpacity != nil && !b.fillOpacity!.isNaN)) {a.fillOpacity = b.fillOpacity}
         if(a.fillRule == nil && b.fillRule != nil) {a.fillRule = b.fillRule}
