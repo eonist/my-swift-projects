87ede19b1290f718551e727deffb8d73e0fb5ec6
Eonist <30n1st@gmail.com>
2017-05-11 17:52:43 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/svg/path/command/SVGPathCommand.swiftSources/Utils/geom/svg/path/utils/SVGPathUtils.swift

--- Sources/Utils/geom/svg/path/command/SVGPathCommand.swift
+++ Sources/Utils/geom/svg/path/command/SVGPathCommand.swift
@@ -3,15 +3,15 @@ import Foundation
  * The SVG commands represented here are from the official SVG syntax
  * TODO: Great candidate for enum 👌
  */
-class SVGPathCommand {
-	static var m:String = "m"/*Move*/
-	static var l:String = "l"/*Line*/
-	static var c:String = "c"/*Cubic-Curve*/
-	static var s:String = "s"/*smooth Cubic curve command*/
-	static var q:String = "q"/*Quad-curve*/
-	static var t:String = "t"/*smooth quadratic curve command*/
-	static var a:String = "a"/*Arc*/
-	static var h:String = "h"/*Horizontal*/
-	static var v:String = "v"/*Vertical*/
-	static var z:String = "z"/*Return to last Move*/
+enum SVGPathCommand:String {
+    case m = "m"/*Move*/
+    case l = "l"/*Line*/
+    case c = "c"/*Cubic-Curve*/
+    case s = "s"/*smooth Cubic curve command*/
+    case q = "q"/*Quad-curve*/
+    case t = "t"/*smooth quadratic curve command*/
+    case a = "a"/*Arc*/
+    case h = "h"/*Horizontal*/
+    case v = "v"/*Vertical*/
+    case z = "z"/*Return to last Move*/
 }
