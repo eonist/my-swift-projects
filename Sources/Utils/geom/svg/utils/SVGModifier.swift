e8172e2f66808e0b08db74494ef0c24abf72329a
Eonist <30n1st@gmail.com>
2017-05-12 00:08:44 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/utils/SVGModifier.swift

--- Sources/Utils/geom/svg/utils/SVGModifier.swift
+++ Sources/Utils/geom/svg/utils/SVGModifier.swift
@@ -36,7 +36,7 @@ class SVGModifier {
 	static func scale(_ element:ISVGElement,_ pivot:CGPoint, _ scale:CGPoint) {
         switch(element){
 			
-			case var elmt as SVGGradient:SVGGradientModifier.scale(elmt as! SVGGradient, pivot, scale);break;/*The individual style.gradient.transform instances are scaled so why do we need to scale this? It may be usefull for export purpouses*/
+			case var elmt as SVGGradient:SVGGradientModifier.scale(&elmt , pivot, scale);break;/*The individual style.gradient.transform instances are scaled so why do we need to scale this? It may be usefull for export purpouses*/
             default: break;
 		}
         if let element = element as? SVGGraphic {SVGModifier.update(element)}
