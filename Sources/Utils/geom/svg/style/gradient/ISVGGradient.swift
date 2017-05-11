f7721e470c598dca8eab2399f5b897bc5f794d80
Eonist <30n1st@gmail.com>
2017-05-11 23:39:44 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/gradient/ISVGGradient.swift

--- Sources/Utils/geom/svg/style/gradient/ISVGGradient.swift
+++ Sources/Utils/geom/svg/style/gradient/ISVGGradient.swift
@@ -4,6 +4,7 @@ import Foundation
  * NOTE: The opacity of the color of this stop-point. If opacity changes from one stop-point with 1 to another stop-point with opacity 0, then the color will gradually become more and more transparent.
  */
 protocol ISVGGradient:ISVGElement {
+    var element:ISVGElement {get}
     var offsets:[CGFloat]{get set}/*How far into the shape this color starts (if the first color of the gradient) or stops (if the last color of the gradient). Specified as percentages of the shape (really the gradient vector) the gradient is applied to. For instance, 10% means that the color should start / stop 10% into the shape.*/
     var colors:[CGColor]{get set}/*The color of this stop-point. The color the gradient changes from / to.*/
     var spreadMethod:String{get set}/*This value defines how the gradient is spread out through the shape. There are 3 possible values: pad, repeat, and reflect. 'pad' means that the first/last color of the gradient is padded (spread out) before and after the gradient. ''repeat' means that the gradient is repeated throughout the shape. 'reflect' means that gradient is mirrored in the shape. The spread method is only relevant if the gradient does not fill out the shape completely (see offset attributes of the <stop> elements.*/
@@ -11,5 +12,5 @@ protocol ISVGGradient:ISVGElement {
     var gradientTransform:CGAffineTransform?{get set}/*You can transform (e.g. rotate) the gradient before it is applied. See SVG Transformation for more details.*/
 }
 extension ISVGGradient{
-    
+    override var id: String {get {return element.id} set {element.id = newValue}}
 }
