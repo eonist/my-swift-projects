5f522c6f7b86e71c337f2ce7d635bc23d02008ac
Eonist <30n1st@gmail.com>
2017-05-12 00:13:58 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/utils/SVGModifier.swift

--- Sources/Utils/geom/svg/utils/SVGModifier.swift
+++ Sources/Utils/geom/svg/utils/SVGModifier.swift
@@ -35,7 +35,7 @@ class SVGModifier {
 	 */
 	static func scale(_ element:ISVGElement,_ pivot:CGPoint, _ scale:CGPoint) {
         switch(element){
-        case is SVGPolyLine:PointModifier.scalePoints(&(element as! SVGPolyLine).points, pivot, scale);/*SVGPolyLine,SVGPolygon*/break;
+        case let element as SVGPolyLine:PointModifier.scalePoints(&element.points, pivot, scale);/*SVGPolyLine,SVGPolygon*/break;
         
 			case var element as SVGGradient:SVGGradientModifier.scale(&element , pivot, scale);break;/*The individual style.gradient.transform instances are scaled so why do we need to scale this? It may be usefull for export purpouses*/
             default: break;
