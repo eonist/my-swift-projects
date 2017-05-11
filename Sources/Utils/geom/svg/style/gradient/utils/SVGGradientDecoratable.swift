cb399ff8a7483c571d9272b0509a7bf6e1824d06
Eonist <30n1st@gmail.com>
2017-05-11 23:52:49 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/gradient/utils/SVGGradientDecoratable.swift

--- Sources/Utils/geom/svg/style/gradient/utils/SVGGradientDecoratable.swift
+++ Sources/Utils/geom/svg/style/gradient/utils/SVGGradientDecoratable.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class SVGGradientDecoratable {
-
+protocol SVGGradientDecoratable:ISVGGradient{
+    var gradient:ISVGGradient {get set}
+}
+extension SVGGradientDecoratable{
+    var element:ISVGElement {get{return gradient.element} set{gradient.element = newValue}}
+    var offsets:[CGFloat] {get{return gradient.offsets} set{gradient.offsets = newValue}}
+    var colors:[CGColor] {get{return gradient.colors} set{gradient.colors = newValue}}
+    var spreadMethod:String {get{return gradient.spreadMethod} set{gradient.spreadMethod = newValue}}
+    var gradientUnits:String {get{return gradient.gradientUnits} set{gradient.gradientUnits = newValue}}
+    var gradientTransform:CGAffineTransform? {get{return gradient.gradientTransform} set{gradient.gradientTransform = newValue}}
 }
