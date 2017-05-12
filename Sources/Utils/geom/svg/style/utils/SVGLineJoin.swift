c12f0489f764c772803960c63253d9b374394c88
Eonist <30n1st@gmail.com>
2017-05-12 15:43:56 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/utils/SVGLineJoin.swift

--- Sources/Utils/geom/svg/style/utils/SVGLineJoin.swift
+++ Sources/Utils/geom/svg/style/utils/SVGLineJoin.swift
@@ -1,5 +1,19 @@
 import Foundation
 
-class SVGLineJoin {
-
+enum SVGLineJoin:String{
+    case miter = "miter"
+    case round = "round"
+    case bevel = "bevel"
+}
+extension CGLineJoin{
+    static func lineJoin(_ lineJoin:SVGLineJoin) -> CGLineJoin {
+        switch lineJoin {
+        case .miter:
+            return .miter
+        case .round:
+            return .round
+        case .bevel:
+            return .bevel
+        }
+    }
 }
