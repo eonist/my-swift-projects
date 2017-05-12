4ac4d23b7140501bc6b438efc6672d1b59968abc
Eonist <30n1st@gmail.com>
2017-05-12 22:06:56 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/text/utils/TextFormatConstants.swift

--- Sources/Utils/text/utils/TextFormatConstants.swift
+++ Sources/Utils/text/utils/TextFormatConstants.swift
@@ -16,5 +16,6 @@ enum TextFormatConstants:String {
     case leading = "leading"/*line-spacing*/
 }
 extension TextFormatConstants{
-    var textFormatPropertyNames:[String] {return [TextFormatConstants.size.rawValue,TextFormatConstants.font.rawValue,TextFormatConstants.align.rawValue,TextFormatConstants.selectable.rawValue,TextFormatConstants.background.rawValue,TextFormatConstants.backgroundColor.rawValue,TextFormatConstants.color.rawValue,TextFormatConstants.type.rawValue,TextFormatConstants.border.rawValue,TextFormatConstants.multiline.rawValue,TextFormatConstants.wordWrap.rawValue,TextFormatConstants.scrollable.rawValue,TextFormatConstants.leading.rawValue]}
+    var textFormatPropertyNames:[String] {
+        return 
 }
