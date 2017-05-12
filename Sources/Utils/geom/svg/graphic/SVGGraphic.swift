ca5f0cdec74506d0616d31928dba154a09564abe
Eonist <30n1st@gmail.com>
2017-05-12 18:00:05 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/graphic/SVGGraphic.swift

--- Sources/Utils/geom/svg/graphic/SVGGraphic.swift
+++ Sources/Utils/geom/svg/graphic/SVGGraphic.swift
@@ -36,24 +36,19 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      * NOTE: This method gets its call from the Graphic instance through a functional selector. Which gets it's call through a instance selector. The call is fired when OSX deems it right to be fired. This is initiated by setNeedsDisplay calls on the line and the fill shape (This )
      */
     func draw(_ layer:CALayer, in ctx:CGContext) {/*The context is passed from the layers, so that we get access to the context from this class and the classes that inherit this class*/
-        //Swift.print("SVGGraphic.drawLayer()")
         if(layer === fillShape){
-            //Swift.print("fillShape: ")
             fillShape.graphics.context = ctx
             if(style != nil){fill()}
         }else if(layer === lineShape){
-            //Swift.print("lineShape")
             lineShape.graphics.context = ctx
             if(style != nil){line()}
         }
     }
     func fill(){
-        //Swift.print("SVGGraphic.fill()")
         beginFill()
         stylizeFill()
     }
     func line(){
-        //Swift.print("SVGGraphic.line()")
         applyLineStyle()
         stylizeLine()
     }
@@ -63,7 +58,6 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      * NOTE: we dont check to se if style is not nil, since that is being done by the caller of this method
      */
     func beginFill(){
-        //Swift.print("SVGGraphic.beginFill()" + "\(style!.fill))")
         if(/*style != nil && */style!.fill is Double/* && style!.fill != "none"*/ && !(style!.fill as! Double).isNaN) {
             let color:NSColor = SVGFillStyleUtils.fillColor(style!)
             fillShape.graphics.fill(color)/*Stylize the fill*/
@@ -91,7 +85,6 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      * The draw call is overriden in SVGRect SVGCircle etc and takes care of setting the path to the Shape instances
      */
     func draw(){
-        //Swift.print("SVGGraphic.draw()")
         /*if(style != nil){/*this should porbably have a more complex assert for the sake of optimization*/
             drawLine()
             drawFill()
@@ -102,20 +95,16 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      */
     /*
     func drawLine(){
-        Swift.print("SVGGraphic.drawLine()")
         //fatalError("must be overriden in subclass")
     }
     func drawFill(){
-        Swift.print("SVGGraphic.drawFill()")
         //fatalError("must be overriden in subclass")
     }
     */
     func stylizeFill(){
-        //Swift.print("SVGGraphic.stylizeFill()")
         GraphicsModifier.stylize(fillShape.path,fillShape.graphics)//realize style on the graphic
     }
     func stylizeLine(){
-        //Swift.print("SVGGraphic.stylizeLine()")
         GraphicsModifier.stylizeLine(lineShape.path,lineShape.graphics)//realize style on the graphic
     }
     /**
@@ -123,13 +112,8 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      * TODO: the logic inside this method should be in the Shape, and this method should just forward to the shape
      */
     override func hitTest(_ aPoint: NSPoint) -> NSView? {
-        //Swift.print("hitTest in graphic" + "\(aPoint)")
-        //you have to convert the aPoint to localspace
-        let localPoint = localPos()//convertPoint(aPoint, fromView: self.window?.contentView)//convertPoint(winMousePos, fromView: nil)//
-        //Swift.print("localPoint: " + "\(localPoint)")
-        //Swift 3 upgrade, caution used different contains method before, but the bellow should work
+        let localPoint = localPos()//*you have to convert the aPoint to localspace*/
         let isPointInside:Bool = fillShape.path.contains(localPoint)//CGPathContainsPoint(fillShape.path,nil,,true)
-        //Swift.print("isPointInside: " + "\(isPointInside)")
         return isPointInside ? self : nil/*return nil will tell the parent that there was no hit on this view*/
     }
     /**
@@ -139,8 +123,6 @@ class SVGGraphic:SVGView,CALayerDelegate,ISVGGraphic{
      * NOTE: we could keep the trackingArea in graphic so its always easy to access, but i dont think it needs to be easily accesible atm.
      */
     override func updateTrackingAreas() {
-        //Swift.print("SVGGraphic.updateTrackingArea: " + "\(fillShape.frame)")
-        //Swift.print("\(NSViewParser.parents(self))" + ".updateTrackingArea: " + "\(fillShape.frame)")
         if(trackingArea != nil) {self.removeTrackingArea(trackingArea!)}//remove old trackingArea if it exists
         trackingArea = NSTrackingArea(rect: fillShape.frame, options: [NSTrackingAreaOptions.activeAlways, NSTrackingAreaOptions.mouseMoved,NSTrackingAreaOptions.mouseEnteredAndExited], owner: self, userInfo: nil)
         self.addTrackingArea(trackingArea!)//<---this will be in the Skin class in the future and the owner will be set to Element to get interactive events etc
