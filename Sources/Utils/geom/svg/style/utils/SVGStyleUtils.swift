ee65599a81e3733395e5d1fe84dc68b2fef57258
Eonist <30n1st@gmail.com>
2017-05-12 16:12:04 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/style/utils/SVGStyleUtils.swift

--- Sources/Utils/geom/svg/style/utils/SVGStyleUtils.swift
+++ Sources/Utils/geom/svg/style/utils/SVGStyleUtils.swift
@@ -8,20 +8,15 @@ class SVGStyleUtils {
      * TODO: try to find a method in swift that can extract enum values by providing a string
      */
     static func lineCap(_ lineCap:String?)->CGLineCap{
-        let strokeLineCap:String = lineCap != nil && lineCap != "" ? lineCap! : "butt"/*<-this was none, but it doesnt need to be since we wont extract this value from */
-        guard let lineCap:SVGLineCap = SVGLineCap(rawValue:strokeLineCap) else {
+        let strokeLineCap:String? = lineCap != nil && lineCap != "" ? lineCap : "butt"/*<-this was none, but it doesnt need to be since we wont extract this value from */
+        guard let lineCapStr:String = strokeLineCap, let lineCap:SVGLineCap = SVGLineCap(rawValue:lineCapStr) else {
             fatalError("this lineCap type is not supported: \(strokeLineCap)")
         }
         return SVGLineCap.lineCap(lineCap)
-        
-        if(strokeLineCap == "butt"){return CGLineCap.butt}
-        else if(strokeLineCap == "round"){return CGLineCap.round}
-        else if(strokeLineCap == "square"){return CGLineCap.square}
-        else{fatalError("this lineCap type is not supported")}
     }
     static func lineJoin(_ lineJoin:String?)->CGLineJoin{
-        let strokeLineJoin:String = lineJoin != nil && lineJoin! != "" ? lineJoin! : "miter"
-        guard let lineJoin:SVGLineJoin = SVGLineJoin(rawValue:strokeLineJoin) else{
+        let strokeLineJoin:String? = lineJoin != nil && lineJoin != "" ? lineJoin! : "miter"
+        guard let lineJoinStr:String = strokeLineJoin, let lineJoin:SVGLineJoin = SVGLineJoin(rawValue:lineJoinStr) else{
             fatalError("this strokeLineJoin type is not supported: \(strokeLineJoin)")
         }
         return SVGLineJoin.lineJoin(lineJoin)
