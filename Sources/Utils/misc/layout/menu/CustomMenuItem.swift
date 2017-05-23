774e05170261e5ed12c31c21be9f9da531d33504
Eonist <30n1st@gmail.com>
2017-05-23 16:30:35 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/menu/CustomMenuItem.swift

--- Sources/Utils/misc/layout/menu/CustomMenuItem.swift
+++ Sources/Utils/misc/layout/menu/CustomMenuItem.swift
@@ -2,12 +2,12 @@ import Cocoa
 
 class CustomMenuItem:NSMenuItem{
     init(_ title:String = "", _ keyEquivalent:String = "") {
-        super.init(title: title, action:#selector(onSelect), keyEquivalent: keyEquivalent)
+        super.init(title: title, action:#selector(self.onSelect), keyEquivalent: keyEquivalent)
         target = self/*target specifies where the selector should work, in this case in this class scope*/
         self.isEnabled = true
     }
     func onSelect(event:AnyObject) {
-        //override in subclass (optional)
+        Swift.print("override in superClass")
     }
     /**
      * Return true if you want to enable the menu item, false will disable it
