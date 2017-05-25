705219d218051f8b19487cd79b0e7b8c2f63ff1d
Eonist <30n1st@gmail.com>
2017-05-25 12:51:34 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/json/JSONParser.swift

--- Sources/Utils/json/JSONParser.swift
+++ Sources/Utils/json/JSONParser.swift
@@ -1,5 +1,38 @@
 import Foundation
 
-class JSONParser {
-
+/**
+ * NOTE: See TreeConverter.tree(json) for how you can convert json into data object you can more easily traverse, you can then do JSON -> Tree -> XML
+ * TODO: ⚠️️ You can also look at the XML classes and reflection classes for how to make more json <-> convert methods
+ */
+class JSONParser{
+    /**
+     * Array with any
+     */
+    static func arr(_ json:Any?)->[Any]?{
+        return json as? [Any]
+    }
+    /**
+     * Dict
+     */
+    static func dict(_ json:Any?)->[String: Any]?{
+        return json as? [String: Any]
+    }
+    /**
+     * Int
+     */
+    static func int(_ json:Any?)->Int?{
+        return json as? Int
+    }
+    /**
+     * String
+     */
+    static func str(_ json:Any?)->String?{
+        return json as? String
+    }
+    /**
+     * Array with dict
+     */
+    static func dictArr(_ json:Any?)->[[String:Any]]?{
+        return json as? [[String:Any]]//array with dict
+    }
 }
