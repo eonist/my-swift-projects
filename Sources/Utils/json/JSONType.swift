0494f72df4cbe0e2fa690ad0eaec8b0146a1d24f
Eonist <30n1st@gmail.com>
2017-05-25 13:50:47 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/json/JSONType.swift

--- Sources/Utils/json/JSONType.swift
+++ Sources/Utils/json/JSONType.swift
@@ -10,19 +10,19 @@ enum JSONType {
 extension JSONType{
     static func type(_ json:Any?) -> JSONType{
         switch true{
-        case JSONAsserter.isStr(json):
-            return .str
-        case JSONAsserter.isInt(json):
-            return .int
-        case JSONAsserter.isDict(json):
-            return .dict
-        case JSONAsserter.isDictArr(json):
-            return .dictArr
-        case JSONAsserter.isArr(json):
-            return .arr
-        default:
-            fatalError("type not supported: \(type(of:json))")
-            break;
+            case JSONAsserter.isStr(json):
+                return .str
+            case JSONAsserter.isInt(json):
+                return .int
+            case JSONAsserter.isDict(json):
+                return .dict
+            case JSONAsserter.isDictArr(json):
+                return .dictArr
+            case JSONAsserter.isArr(json):
+                return .arr
+            default:
+                fatalError("type not supported: \(type(of:json))")
+                break;
         }
     }
 }
