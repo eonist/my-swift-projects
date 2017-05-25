7fa4ddb0a34a7e88df9429b96a90548bd477d55f
Eonist <30n1st@gmail.com>
2017-05-25 12:51:34 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:Sources/Utils/json/JSONUtils.swift

--- Sources/Utils/json/JSONUtils.swift
+++ Sources/Utils/json/JSONUtils.swift
@@ -26,42 +26,7 @@ extension JSONType{
         }
     }
 }
-/**
- * NOTE: See TreeConverter.tree(json) for how you can convert json into data object you can more easily traverse, you can then do JSON -> Tree -> XML
- * TODO: ⚠️️ You can also 
- */
-class JSONParser{
-    /**
-     * Array with any
-     */
-    static func arr(_ json:Any?)->[Any]?{
-        return json as? [Any]
-    }
-    /**
-     * Dict
-     */
-    static func dict(_ json:Any?)->[String: Any]?{
-        return json as? [String: Any]
-    }
-    /**
-     * Int
-     */
-    static func int(_ json:Any?)->Int?{
-        return json as? Int
-    }
-    /**
-     * String
-     */
-    static func str(_ json:Any?)->String?{
-        return json as? String
-    }
-    /**
-     * Array with dict
-     */
-    static func dictArr(_ json:Any?)->[[String:Any]]?{
-        return json as? [[String:Any]]//array with dict
-    }
-}
+
 class JSONAsserter{
     static func isDict(_ json:Any?) -> Bool{
         return JSONParser.dict(json) != nil
