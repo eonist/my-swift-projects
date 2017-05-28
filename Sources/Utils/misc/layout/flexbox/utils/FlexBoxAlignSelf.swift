f637f1b99c85e6bed4ea92758821e0ce0f821b69
Eonist <30n1st@gmail.com>
2017-05-29 00:13:46 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift

--- Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
+++ Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
@@ -3,18 +3,21 @@ import Foundation
 class FlexBoxAlignSelf{
     static func flexStart<T:Flexible>(_ item:T, _ container:CGRect){
         //for each, set item y to container y
+        item.pos = Align.alignmentPoint(item.size, container.size, Alignment.top, Alignment.top)
     }
     /**
      *
      */
     static func flexEnd<T:Flexible>(_ item:T, _ container:CGRect){
         //for each item, set item y to alignPoint, bottom
+        item.pos = Align.alignmentPoint(item.size, container.size, Alignment.bottom, Alignment.bottom)
     }
     /**
      *
      */
     static func center<T:Flexible>(_ item:T, _ container:CGRect){
         //for each item, set item y to alignPoint, center
+        item.pos = Align.alignmentPoint(item.size, container.size, Alignment.center, Alignment.center)
     }
     /**
      *
