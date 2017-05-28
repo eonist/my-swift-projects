772afb3638e8e7f5be5b15150fe4d76ecb3feaa3
Eonist <30n1st@gmail.com>
2017-05-28 22:20:00 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBox.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -5,8 +5,8 @@ class FlexBox {
 
 }
 enum FlexBoxType:String{
-    case flexEnd = "flexEnd"
-    case flexStart = "flexStart"
+    case flexStart = "flexStart"/*aligns from start to end*/
+    case flexEnd = "flexEnd"/*aligns from end to start*/
     case center = "center"
     case spacebetween = "spaceBetween"
     case spaceAround = "spaceAround"
