249ccbd15750415d4ceb6e710baf84032d1b6e5b
Eonist <30n1st@gmail.com>
2017-05-29 00:06:40 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
@@ -34,10 +34,43 @@ class FlexBoxModifier{
                 JustifyUtils.spaceBetween(items,container)
         }
     }
+    /**
+     * Vertically aligns a single item
+     */
+    static func alignSelf<T:Flexible>(_ items:T, _ type:FlexBoxType.AlignItems, _ container:CGRect){
+        switch type{
+            case .flexStart:
+                JustifyUtils.flexStart(items,container)
+            case .flexEnd:
+                JustifyUtils.flexEnd(items, container)
+            case .center:
+                JustifyUtils.center(items,container)
+            case .stretch:
+                JustifyUtils.spaceBetween(items,container)
+        }
+    }
 }
-private class AlignItems{
+private class AlignSelf{
     static func flexStart<T:Flexible>(_ items:[T], _ container:CGRect){
-        //
+        //for each, set item y to container y
+    }
+    /**
+     *
+     */
+    static func flexEnd<T:Flexible>(_ items:[T], _ container:CGRect){
+        //for each item, set item y to alignPoint, bottom
+    }
+    /**
+     *
+     */
+    static func center<T:Flexible>(_ items:[T], _ container:CGRect){
+        //for each item, set item y to alignPoint, center
+    }
+    /**
+     *
+     */
+    static func stretch<T:Flexible>(_ items:[T], _ container:CGRect){
+        //for each, set item.y to container.y, set item.height to container.height
     }
 }
 private class JustifyUtils{
