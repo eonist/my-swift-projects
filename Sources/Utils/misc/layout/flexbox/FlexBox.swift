28fd01dc9679b55d729bdb6db91db9f8b48edd3a
Eonist <30n1st@gmail.com>
2017-05-29 00:50:00 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBox.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -2,4 +2,19 @@ import Foundation
 //FlexContainer ->
 //FlexItem -> init size, size,
 class FlexBox {}
+enum FlexWrapType:String{
+    case noWrap = "noWrap"
+    case wrap = "wrap"
+    //wrapReverse, not supported yet
+}
+struct FlexContainer{
+    var flexibles:[Flexible]
+    var wrapType:FlexWrapType
+    
+}
+struct FlexItem{
+    var flexible:Flexible
+    var grow:Int = 0
+    var shring:Int = 1
+}
 typealias Flexible = IPositional & ISizeable
