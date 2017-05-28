83ce611199e16a7008a8c8177aa422885c2e2d53
Eonist <30n1st@gmail.com>
2017-05-28 23:17:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
@@ -4,14 +4,14 @@ class FlexBoxModifier{
     /**
      * TODO: Possibly use FlexItem here that decorates something
      */
-    static func justifyContent<T:IPositional>(_ items:[T], _ type:FlexBoxType.Justify, _ containerSize:CGSize) where T:ISizeable{
+    static func justifyContent<T:IPositional>(_ items:[T], _ type:FlexBoxType.Justify, _ container:CGRect) where T:ISizeable{
         switch type{
             case .flexStart:
                 //Swift.print("flexStart")
                 JustifyUtils.justifyFlexStart(items)
             case .flexEnd:
                 //Swift.print("flexEnd")
-                JustifyUtils.justifyFlexEnd(items, containerSize)
+                JustifyUtils.justifyFlexEnd(items, container)
             case .center:
                 Swift.print("center")
             case .spacebetween:
@@ -25,8 +25,8 @@ class JustifyUtils{
     /**
      * Aligns from start to end
      */
-    static func justifyFlexStart<T:IPositional>(_ items:[T]) where T:ISizeable{
-        var x:CGFloat = 0//interim x
+    static func justifyFlexStart<T:IPositional>(_ items:[T], _ container:CGRect) where T:ISizeable{
+        var x:CGFloat = container.x//interim x
         items.forEach{ item in
             item.x = x
             x += item.width
@@ -35,11 +35,20 @@ class JustifyUtils{
     /**
      * Aligns from end to start
      */
-    static func justifyFlexEnd<T:IPositional>(_ items:[T], _ containerSize:CGSize) where T:ISizeable{
-        var x:CGFloat = containerSize.width/*interim x*/
+    static func justifyFlexEnd<T:IPositional>(_ items:[T], _ container:CGRect) where T:ISizeable{
+        var x:CGFloat = container.width/*interim x*/
         items.reversed().forEach{ item in/*Move backwards*/
             x -= item.width
             item.x = x
         }
+    }
+    /**
+     *
+     */
+    static func center(){
+        //find the totalW of all items
+        //Use Align.center to find x
+        //create new Rect
+        //Use justifyFlexStart and lay items out left to right with new rect as offset
     }
 }
