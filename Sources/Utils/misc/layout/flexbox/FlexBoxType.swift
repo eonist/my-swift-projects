98e2605dac05425cf43cc434b9c72a1fdf98a510
Eonist <30n1st@gmail.com>
2017-05-28 23:00:09 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxType.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxType.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxType.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class FlexBoxType {
-
+enum FlexBoxType{
+    enum Justify:String{
+        case flexStart = "flexStart"/*Aligns from start to end*/
+        case flexEnd = "flexEnd"/*Aligns from end to start*/
+        case center = "center"/*Aligns one item after the other and centers their total position*/
+        case spacebetween = "spaceBetween"/*Aligns all items from the absolute start to absolute end and adds equa spacing between them*/
+        case spaceAround = "spaceAround"/*Same as spaceBetween but does not pit to sides but rather add equal spacing there as well*/
+    }
 }
