3b4a8d35d62f6d904c513e8b222ad939534dfad1
Eonist <30n1st@gmail.com>
2017-05-29 00:09:19 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swiftSources/Utils/misc/layout/flexbox/utils/JustifyUtils.swift

--- Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
+++ Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-class FlexBoxAlignSelf {
-
+class FlexBoxAlignSelf{
+    static func flexStart<T:Flexible>(_ item:T, _ container:CGRect){
+        //for each, set item y to container y
+    }
+    /**
+     *
+     */
+    static func flexEnd<T:Flexible>(_ item:T, _ container:CGRect){
+        //for each item, set item y to alignPoint, bottom
+    }
+    /**
+     *
+     */
+    static func center<T:Flexible>(_ item:T, _ container:CGRect){
+        //for each item, set item y to alignPoint, center
+    }
+    /**
+     *
+     */
+    static func stretch<T:Flexible>(_ item:T, _ container:CGRect){
+        //for each, set item.y to container.y, set item.height to container.height
+    }
 }
--- Sources/Utils/misc/layout/flexbox/utils/JustifyUtils.swift
+++ Sources/Utils/misc/layout/flexbox/utils/JustifyUtils.swift
@@ -1,5 +1,66 @@
 import Foundation
 
-class JustifyContent {
-
+class JustifyUtils{
+    /**
+     * Aligns from start to end
+     */
+    static func flexStart<T:Flexible>(_ items:[T], _ container:CGRect){
+        var x:CGFloat = container.x//interim x
+        items.forEach{ item in
+            item.x = x
+            x += item.width
+        }
+    }
+    /**
+     * Aligns from end to start
+     */
+    static func flexEnd<T:Flexible>(_ items:[T], _ container:CGRect) {
+        var x:CGFloat = container.width/*interim x*/
+        items.reversed().forEach{ item in/*Move backwards*/
+            x -= item.width
+            item.x = x
+        }
+    }
+    /**
+     * Aligns one item after the other and centers their total position
+     */
+    static func center<T:Flexible>(_ items:[T], _ container:CGRect) {
+        //find the totalW of all items
+        let totW:CGFloat = items.reduce(0){$0 + $1.size.width}
+        //Use Align.center to find x
+        let p:CGPoint = Align.alignmentPoint(CGSize(totW,0), container.size, Alignment.centerCenter, Alignment.centerCenter, CGPoint())
+        //create new Rect
+        let newRect = CGRect(p.x,container.y,container.width,container.height)
+        //Use justifyFlexStart and lay items out left to right with new rect as offset
+        flexStart(items, newRect)
+    }
+    /**
+     * Aligns all items from the absolute start to absolute end and adds equa spacing between them
+     */
+    static func spaceBetween<T:Flexible>(_ items:[T], _ container:CGRect) {
+        let totW:CGFloat = items.reduce(0){$0 + $1.size.width}/*find the totalW of all items*/
+        let totVoid:CGFloat = container.width - totW/*find totVoid by doing w - totw*/
+        let numOfVoids:CGFloat = CGFloat(items.count - 1)/*then divide this voidSpace with .count - 1 and*/
+        let itemVoid:CGFloat = totVoid / numOfVoids/*iterate of each item and inserting itemVoid in + width*/
+        var x:CGFloat = container.x//interim x
+        items.forEach{ item in
+            item.x = x
+            x += item.width + itemVoid
+        }
+    }
+    /**
+     * Same as spaceBetween but does not pit to sides but rather add equal spacing there as well
+     */
+    static func spaceAround<T:Flexible>(_ items:[T], _ container:CGRect) {
+        let totW:CGFloat = items.reduce(0){$0 + $1.size.width}/*find the totalW of all items*/
+        let totVoid:CGFloat = container.width - totW/*find totVoid by doing w - totw*/
+        let numOfVoids:CGFloat = CGFloat(items.count)/*then divide this voidSpace with .count - 1 and*/
+        let itemVoid:CGFloat = totVoid / numOfVoids/*iterate of each item and inserting itemVoid in + width*/
+        let edgeVoid:CGFloat = itemVoid/2
+        var x:CGFloat = container.x+edgeVoid//interim x
+        items.forEach{ item in
+            item.x = x
+            x += item.width + itemVoid
+        }
+    }
 }
