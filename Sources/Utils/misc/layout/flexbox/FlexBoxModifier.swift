dec027e9b1a9e40baacf3c04940036914dfe8fbe
Eonist <30n1st@gmail.com>
2017-05-28 23:02:32 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift
@@ -4,7 +4,7 @@ class FlexBoxModifier{
     /**
      * TODO: Possibly use FlexItem here that decorates something
      */
-    static func justifyContent<T:IPositional>(_ items:[T], _ type:FlexBoxType.Justify) where T:ISizeable{
+    static func justifyContent<T:IPositional>(_ items:[T], _ type:FlexBoxType.Justify, _ containerSize:CGSize) where T:ISizeable{
         switch type{
             case .flexStart:
                 Swift.print("flexStart")
@@ -30,5 +30,17 @@ class JustifyUtils{
             item.x = x
             x += item.width
         }
+    }
+    static func justifyFlexEnd<T:IPositional>(_ items:[T], _ containerSize:CGSize) where T:ISizeable{
+        
+        //move backwards?
+        if let last = items.last{
+            var x:CGFloat = containerSize.width - last.width//interim x
+            items.reversed().forEach{ item in
+                item.x = x
+                x += item.width
+            }
+        }
+        
     }
 }
