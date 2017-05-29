70117dbe8c9ac0e4f438d82b65a9e73b0b84cfff
Eonist <30n1st@gmail.com>
2017-05-29 23:52:47 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxContainer.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxContainer.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxContainer.swift
@@ -3,12 +3,14 @@ import Foundation
 struct FlexBoxContainer{
     var rect:CGRect
     var flexibles:[Flexible]
-    var justifyContent:FlexBoxType.AlignItems
+    var justifyContent:FlexBoxType.Justify
+    var alignItems:FlexBoxType.AlignItems
     //var wrapType:FlexWrapType
     //var flexDir:FlowFlexDirType,row,collumn,reverseRow,collumnReverse
-    init(_ rect:CGRect, _ flexibles:[Flexible] = [], _ justifyContent:FlexBoxType.AlignItems = .flexStart){
+    init(_ rect:CGRect, _ flexibles:[Flexible] = [], _ justifyContent:FlexBoxType.Justify = .flexStart, _ alignItems:FlexBoxType.AlignItems = .flexStart){
         self.rect = rect
         self.flexibles = flexibles
         self.justifyContent = justifyContent
+        self.alignItems = alignItems
     }
 }
