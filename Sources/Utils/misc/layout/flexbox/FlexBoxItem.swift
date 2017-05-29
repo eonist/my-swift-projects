dd311e2dc1ad0390d83075482e22e1439f093e60
Eonist <30n1st@gmail.com>
2017-05-29 16:24:22 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBoxItem.swift

--- Sources/Utils/misc/layout/flexbox/FlexBoxItem.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBoxItem.swift
@@ -2,6 +2,7 @@ import Foundation
 
 /**
  * TODO: Needs init size, size
+ * NOTE: Shorthand: Flex:1 0 auto. This shorthand is broken down into three flexbox properties in this order: flex-grow, flex-shrink, and flex-basis.
  * IMPORTANT: ⚠️️ Set grow and shrink to 0 if you want to fix the width of an item
  * IMPORTANT: ⚠️️ set basis to 25% and grow 1. And then add 3 tiems. each of them will actually take up 33.3%, and a fourth and they take up 25%
  */
@@ -13,8 +14,8 @@ struct FlexItem{
      * CONCEPT: if you have 3 items in a 300px container. and they are grow:1 then their space will be: 1/3 aka 100px. If 1 is grow:2 then its width will be as if there are 4 items. so 300/4*2 and the two other will be: 300/4*1
      * IMPORTANT: if there is a fixed item in the container, the remainder space will be container - fixed-
      */
-    var grow:Int = 0/*The element will not grow if there's space available. It will only use the space it needs.*/
-    var shrink:Int = 1/*If there's not enough space available in the container's main axis, the element will shrink by a factor of 1, and will wrap its content.*/
+    var grow:Int = 1/*The element will not grow if there's space available. It will only use the space it needs.*/
+    var shrink:Int = 0/*If there's not enough space available in the container's main axis, the element will shrink by a factor of 1, and will wrap its content.*/
     /**
      * Flex-basis: When flex-direction is row, flex-basis controls width, But when flex-direction is column, flex-basis controls height.
      */
