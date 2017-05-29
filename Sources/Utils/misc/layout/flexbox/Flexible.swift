8ba950ed214a164887b147b4060217914657d19b
Eonist <30n1st@gmail.com>
2017-05-29 16:37:26 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/layout/flexbox/FlexBoxItem.swiftSources/Utils/misc/layout/flexbox/Flexible.swift

--- Sources/Utils/misc/layout/flexbox/Flexible.swift
+++ Sources/Utils/misc/layout/flexbox/Flexible.swift
@@ -2,6 +2,8 @@ import Foundation
 
 typealias Flexible = IPositional & ISizeable
 
-extension Flexible{
-    var rect:CGRect {return CGRect(flexible.x,flexible.y,flexible.width,flexible.height)}
+class FlexibleUtils{
+    static func rect(_ flexible:Flexible)->CGRect {
+        return CGRect(flexible.x,flexible.y,flexible.width,flexible.height)
+    }
 }
