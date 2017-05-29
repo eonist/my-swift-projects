0fe6830c49782d8667a6d47f3ea7c6945d1dee24
Eonist <30n1st@gmail.com>
2017-05-29 16:26:59 +0200

    Files modified: 3
    Modified 3 files:Sources/Utils/misc/layout/flexbox/FlexBox.swiftSources/Utils/misc/layout/flexbox/FlexBoxItem.swiftSources/Utils/misc/layout/flexbox/utils/FlexBoxGrowUtils.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -5,7 +5,7 @@ import Foundation
 /**
  * display:flex;
  * NOTE: has flex formulas: https://chriswrightdesign.com/experiments/flexbox-adventures/
- * NOTE: Shorthand: Flex:1 0 auto. This shorthand is broken down into three flexbox properties in this order: flex-grow, flex-shrink, and flex-basis.
+ * 
  */
 class FlexBox {}
 
--- Sources/Utils/misc/layout/flexbox/utils/FlexBoxGrowUtils.swift
+++ Sources/Utils/misc/layout/flexbox/utils/FlexBoxGrowUtils.swift
@@ -8,3 +8,8 @@ class FlexBoxGrowUtils {
         
     }
 }
+//The way I understand it (Grow):
+    //remainder space is key
+    //grow means if there is 3 items and you want 1 to have 2 /3 of the space you set it to two and the other two to 0.5, f you set them to 1 then you get 4 sort of. THe grow should be the percentage IMO
+    //shrink in conjunction with grow is hard
+    //
