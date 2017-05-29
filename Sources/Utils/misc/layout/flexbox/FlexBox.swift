a5b029f8dd81637993710d421814a97e1d84fbe9
Eonist <30n1st@gmail.com>
2017-05-29 10:59:13 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBox.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -15,11 +15,13 @@ struct FlexContainer{
 }
 /**
  * TODO: Needs init size, size
+ * Grow: defined how much of the remainder of the space it should use. Canvas: 400, text: 300 : button.grow:1 = 100, grow:2 = 50?
  */
 struct FlexItem{
     var flexible:Flexible
     var grow:Int = 0/*The element will not grow if there's space available. It will only use the space it needs.*/
     var shrink:Int = 1/*If there's not enough space available in the container's main axis, the element will shrink by a factor of 1, and will wrap its content.*/
+    var basis:Int? = nil//nil means auto
     //var flexBasis:CGFloat "auto" or px,ems auto means get width from child width. text,img etc
 }
 typealias Flexible = IPositional & ISizeable
