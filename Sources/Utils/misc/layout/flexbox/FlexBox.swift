5b2fbe9b401f7b314a9379eb5b36fbf6c64fffcb
Eonist <30n1st@gmail.com>
2017-05-29 11:36:24 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBox.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -18,15 +18,20 @@ struct FlexContainer{
 }
 /**
  * TODO: Needs init size, size
- * Grow: defined how much of the remainder of the space it should use. Canvas: 400, text: 300 : button.grow:1 = 100, grow:2 = 50?
- * Flex-basis: When flex-direction is row, flex-basis controls width, But when flex-direction is column, flex-basis controls height.
  * IMPORTANT: ⚠️️ Set grow and shrink to 0 if you want to fix the width of an item
  * IMPORTANT: ⚠️️ set basis to 25% and grow 1. And then add 3 tiems. each of them will actually take up 33.3%, and a fourth and they take up 25%
  */
 struct FlexItem{
     var flexible:Flexible
+    /**
+     * Grow: defined how much of the remainder of the space it should use. Canvas: 400, text: 300 : button.grow:1 = 100, grow:2 = 50?
+     * NOTE: flex-grow:0 simply means that the item won't be resized during item size calculation to accommodate flex container's full main axis size.
+     */
     var grow:Int = 0/*The element will not grow if there's space available. It will only use the space it needs.*/
     var shrink:Int = 1/*If there's not enough space available in the container's main axis, the element will shrink by a factor of 1, and will wrap its content.*/
+    /**
+     * Flex-basis: When flex-direction is row, flex-basis controls width, But when flex-direction is column, flex-basis controls height.
+     */
     var basis:Int? = nil//nil means auto
     //var flexBasis:CGFloat "auto" or px,ems auto means get width from child width. text,img etc
 }
