d9b9b191beaf7a82d7c780de92dfea1c8fc63e50
Eonist <30n1st@gmail.com>
2017-05-29 22:49:45 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift

--- Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
+++ Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift
@@ -5,25 +5,25 @@ class FlexBoxAlignSelf{
      * set item y to container y
      */
     static func flexStart<T:Flexible>(_ item:T, _ container:CGRect){
-        item.y = Align.alignmentPoint(item.size, container.size, Alignment.topCenter, Alignment.topCenter).y
+        item.y = Align.alignmentPoint(item.size, container.size, Alignment.topCenter, Alignment.topCenter,container.topLeft).y
     }
     /**
      * set item y to alignPoint, bottom
      */
     static func flexEnd<T:Flexible>(_ item:T, _ container:CGRect){
-        item.y = Align.alignmentPoint(item.size, container.size, Alignment.bottomCenter, Alignment.bottomCenter).y
+        item.y = Align.alignmentPoint(item.size, container.size, Alignment.bottomCenter, Alignment.bottomCenter,container.topLeft).y
     }
     /**
      * set item y to alignPoint, center
      */
     static func center<T:Flexible>(_ item:T, _ container:CGRect){
-        item.y = Align.alignmentPoint(item.size, container.size, Alignment.centerCenter, Alignment.center).y
+        item.y = Align.alignmentPoint(item.size, container.size, Alignment.centerCenter, Alignment.center,container.topLeft).y
     }
     /**
      * set item.y to container.y, set item.height to container.height
      */
     static func stretch<T:Flexible>(_ item:T, _ container:CGRect){
-        item.y = Align.alignmentPoint(item.size, container.size, Alignment.topCenter, Alignment.topCenter).y
+        item.y = Align.alignmentPoint(item.size, container.size, Alignment.topCenter, Alignment.topCenter,container.topLeft).y
         item.height = container.size.height
     }
 }
