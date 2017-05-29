df818c58fadc347ca07f0528b02fef422169afce
Eonist <30n1st@gmail.com>
2017-05-29 15:31:41 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/flexbox/FlexBox.swift

--- Sources/Utils/misc/layout/flexbox/FlexBox.swift
+++ Sources/Utils/misc/layout/flexbox/FlexBox.swift
@@ -44,5 +44,7 @@ typealias Flexible = IPositional & ISizeable
 
 //The way I understand it:
     //remainder space is key
-    //grow means if there is 2
+    //grow means if there is 3 items and you want 1 to have 2 /3 of the space you set it to two and the other two to 0.5, f you set them to 1 then you get 4 sort of. THe grow should be the percentage IMO
+    //shrink in conjunction with grow is hard
+    //
 
