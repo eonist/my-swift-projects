9c857ab09287554326a46353ca59774779469c75
Eonist <30n1st@gmail.com>
2017-05-31 12:42:19 +0200

    Files deleted: 11
    Deleted 11 files: Sources/Utils/misc/layout/flexbox/FlexBoxContainer.swift Sources/Utils/misc/layout/flexbox/FlexBoxItem.swift Sources/Utils/misc/layout/flexbox/FlexBoxModifier.swift Sources/Utils/misc/layout/flexbox/FlexBoxType.swift Sources/Utils/misc/layout/flexbox/Flexible.swift Sources/Utils/misc/layout/flexbox/protocol/FlexBoxContainerKind.swift Sources/Utils/misc/layout/flexbox/protocol/FlexBoxItemKind.swift Sources/Utils/misc/layout/flexbox/protocol/utils/FlexBoxContainerKindExtensions.swift Sources/Utils/misc/layout/flexbox/utils/FlexBoxAlignSelf.swift Sources/Utils/misc/layout/flexbox/utils/FlexBoxGrowUtils.swift Sources/Utils/misc/layout/flexbox/utils/JustifyUtils.swift

--- Sources/Utils/misc/layout/flexbox/protocol/FlexBoxContainerKind.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol FlexBoxContainerKind {
-    var rect:CGRect {get}
-    var flexBoxItems:[FlexBoxItemKind] {get set}
-    var justifyContent:FlexBoxType.Justify {get}
-    var alignItems:FlexBoxType.AlignType {get}
-}
--- Sources/Utils/misc/layout/flexbox/protocol/FlexBoxItemKind.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol FlexBoxItemKind {
-    var initRect:CGRect {get}
-    var flexible:Flexible {get}
-    var grow:CGFloat {get set}
-    var shrink:CGFloat {get}
-    var basis:CGFloat? {get}
-}
