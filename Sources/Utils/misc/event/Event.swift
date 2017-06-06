b9c3f4892bc8f150a8b95fa507c6cd0c3cfe2f83
Eonist <30n1st@gmail.com>
2017-06-06 19:10:08 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/Event.swift

--- Sources/Utils/misc/event/Event.swift
+++ Sources/Utils/misc/event/Event.swift
@@ -4,13 +4,13 @@ import Foundation
  * NOTE: Event could be a struct, most Event classes are really simple and their passed data could be accessed via origin, maybe try using a struct for Element iOS
  * TODO: Event should extend string or stringconvertible etc. So that one can do onEvent(.update) the subclasses can do .mouseDown etc
  * FUTURE: ⚠️️ The idea with events and enum is that you make Event generic so it can take many types of enums
- * TODO: ⚠️️ origin should be optional
+ * TODO: ⚠️️ Origin should be optional
  */
 class Event{
     static var update:String = "eventUpdate"/*Ideally I would name this change but apparently then subclasses can name their const the same*/
     var type:String
     var origin:AnyObject/*origin sender of event, this could also be weak if you discover a memory leak*///TODO:this should be of type IEventSender
-    var immediate:AnyObject/*previouse sender of event*///TODO:this should be of type IEventSender
+    var immediate:AnyObject/*previouse sender of event, TODO: ⚠️️ this should be of type IEventSender*/
     init(_ type:String = "", _ origin:AnyObject){
         self.type = type
         self.origin = origin
