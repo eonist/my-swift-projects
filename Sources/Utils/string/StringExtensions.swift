65426cd656a23084f83e67d57d85301e9c10d0a5
Eonist <30n1st@gmail.com>
2017-06-11 21:00:14 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/StringExtensions.swift

--- Sources/Utils/string/StringExtensions.swift
+++ Sources/Utils/string/StringExtensions.swift
@@ -7,8 +7,6 @@ extension String {
     init(_ value:CGFloat){self.init(describing:value)}/*Brings back simple String casting which was removed in swift 3*/
     init(_ value:Int){self.init(describing:value)}/*Brings back simple String casting which was removed in swift 3*/
     init(_ value:Bool){self.init(describing:value)}/*Brings back simple String casting which was removed in swift 3*/
-    
-    var tildePath:String {return NSString(string: self).expandingTildeInPath}/*Convenince*/
     /**
      * EXAMPLE: "this is cool".split(" ")//output: ["this","is","cool"]
      */
@@ -27,15 +25,6 @@ extension String {
     func replace(_ pattern:String,_ replacement:String)->String{/*Convenince*/
         return RegExp.replace(self,pattern, replacement)
     }
-    var count:Int{return self.characters.count}/*Convenince*/
-    var cgFloat:CGFloat{return CGFloat(Double(self)!)}//TODO:you should also do the same for the Any type
-    var double:Double{return Double(self)!}
-    /**
-     * CAUTION: if you do "0xFF0000FF".uint it will give the wrong value, use UInt(Double("")!) instead for cases like that
-     */
-    var uint:UInt{return UInt(Float(self)!)}
-    var int:Int{return Int(self)!}
-    
     func encode()->String?{/*Convenince*/
         return StringParser.encode(self)
     }
@@ -60,7 +49,14 @@ extension String {
     func idx(_ index:Int) -> String.Index{
         return StringParser.idx(self, index)
     }
-    var bool:Bool {return StringParser.boolean(self)}
+    func array<T>(_ cast:(_ char:Character)->T)->[T]{
+        return StringParser.array(self, cast)
+    }
+    /**
+     * CAUTION: if you do "0xFF0000FF".uint it will give the wrong value, use UInt(Double("")!) instead for cases like that
+     */
+    var uint:UInt{return UInt(Float(self)!)}
+    
     /**
      * EXAMPLE: "<p>text</p>".xml//Output: xml
      */
@@ -68,11 +64,21 @@ extension String {
     var url:URL {return FilePathParser.path(self)}/*Convenince*/
     var lineCount:Int{return StringParser.lineCount(self)}
     var content:String? {return FileParser.content(self.tildePath)}
-
-    func array<T>(_ cast:(_ char:Character)->T)->[T]{
-        return StringParser.array(self, cast)
-    }
+    var bool:Bool {return StringParser.boolean(self)}
     var nsColor:NSColor{return StringParser.nsColor(self)}
+    var int:Int{return Int(self)!}
+    var tildePath:String {return NSString(string: self).expandingTildeInPath}/*Convenince*/
+    var count:Int{return self.characters.count}/*Convenince*/
+    var cgFloat:CGFloat{return CGFloat(Double(self)!)}//TODO:you should also do the same for the Any type
+    var double:Double{return Double(self)!}
+    var json:Any? {
+        guard let data:Data = self.data(using: String.Encoding.utf8, allowLossyConversion: false) else{
+            return nil
+        }
+        let json:Any? = try? JSONSerialization.jsonObject(with: data, options: [])
+        return json
+    }
+    
 }
 extension NSString{
     var string:String {return String(self)}/*Convenience*/
