1a5e98714a7d94c73703ca03eb41d71969875ca9
Eonist <30n1st@gmail.com>
2017-06-12 13:52:51 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/class/reflection/UnWrappable.swift

--- Sources/Utils/misc/class/reflection/UnWrappable.swift
+++ Sources/Utils/misc/class/reflection/UnWrappable.swift
@@ -46,7 +46,7 @@ extension UnWrappable{
     static func unWrap<T:UnWrappable>(_ xml:XML,_ key:String) -> [T?]{
         guard let child:XML = xml.firstNode(key) else{return [T?]()}
         if child.hasChildren {
-            return child.children!.map {
+            return XMLParser.children(xml).map {
                 guard let subChild:XML = $0 as? XML else{fatalError("SubChild must be XML")}
                 return subChild.hasSimpleContent ? T.unWrap(subChild.value) : T.unWrap(subChild) //$0.hasComplexContent ? .. : nil
             }
