66cf7fcea3584b13191c30eb149d3c0672aede76
Eonist <30n1st@gmail.com>
2017-06-14 12:38:46 +0200

    Files modified: 1, Files deleted: 1
    Deleted 1 file: Sources/Utils/misc/shell/keychain/KeyChain.swiftModified 1 file:Sources/Utils/misc/shell/keychain/KeyChainParser.swift

--- Sources/Utils/misc/shell/keychain/KeyChainParser.swift
+++ Sources/Utils/misc/shell/keychain/KeyChainParser.swift
@@ -50,3 +50,13 @@ extension Data {
         return KeyChainUtils.stringValue(self)
     }
 }
+extension KeyChainParser{
+    /**
+     * Returns password for PARAM: accountName
+     * EXAMPLE: KeyChainParser.password("eonist")//123abc
+     * NOTE: this methods exists because of: convenience and clearity of code
+     */
+    static func password(_ accountName:String)->String?{
+        return KeyChainParser.load(accountName)?.stringValue
+    }
+}
