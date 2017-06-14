eb4c40c5ec960e41e6e8625f43a9c800a4bf0a29
Eonist <30n1st@gmail.com>
2017-06-14 12:10:36 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/keychain/KeyChainModifier.swift

--- Sources/Utils/misc/shell/keychain/KeyChainModifier.swift
+++ Sources/Utils/misc/shell/keychain/KeyChainModifier.swift
@@ -5,7 +5,7 @@ class KeyChainModifier {
      * Save keychain data for key
      * TODO: move to KeyChainModifier.swift
      */
-    static func save(key: String, _ data:NSData) -> Bool {
+    static func save(_ key: String, _ data:NSData) -> Bool {
         let query = [kSecClass as String : kSecClassGenericPassword as String, kSecAttrAccount as String : key,  kSecValueData as String   : data ]  as CFDictionary
         SecItemDelete(query)
         let status:OSStatus = SecItemAdd(query as CFDictionary, nil)
@@ -14,7 +14,7 @@ class KeyChainModifier {
     /**
      * Deletes a keychain item for key
      */
-    static func delete(key:String) -> Bool {
+    static func delete(_ key:String) -> Bool {
         let query:CFDictionary = [ kSecClass as String : kSecClassGenericPassword,kSecAttrAccount as String : key ] as CFDictionary
         let status:OSStatus = SecItemDelete(query)
         return status == noErr
