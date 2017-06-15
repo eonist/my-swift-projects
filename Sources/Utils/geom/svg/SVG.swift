36ee7a532d3d3a55fc6895b407e880c7a364ed87
Eonist <30n1st@gmail.com>
2017-06-15 14:28:42 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/svg/SVG.swift

--- Sources/Utils/geom/svg/SVG.swift
+++ Sources/Utils/geom/svg/SVG.swift
@@ -1,6 +1,6 @@
 import Foundation
 /**
- * SVG class is where other svg elements are added
+ * SVG class is where other svg elements are added 
  * TODO: ⚠️️Implement xmlns:xlink,viewBox,enable-background,xml:space
  * //preserveAspectRatio="xMinYMin meet"
  * //viewBox="0 0 90 90"
