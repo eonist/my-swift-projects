74b75b9e6ebd58b8d347ab4a3c3503191d91a795
Eonist <30n1st@gmail.com>
2017-06-18 12:56:09 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/utils/GitTypes.swift

--- Sources/Utils/misc/shell/git/utils/GitTypes.swift
+++ Sources/Utils/misc/shell/git/utils/GitTypes.swift
@@ -3,3 +3,12 @@ import Foundation
 typealias GitKey = (user:String,  pass:String)
 typealias GitRepo = (localPath:String,  remotePath:String,  branch:String)
 typealias GitMsg = (title:String,  desc:String)
+
+class GitTypes{
+    /**
+     * Describe
+     */
+    static func describe(_ repo:GitRepo,_ key:GitKey){
+        Swift.print("(" + "localPath: \(repo.localPath) , remotePath:  \(repo.remotePath), user: \(key.user), pass: \(key.pass), branch:  \(repo.branch) )")
+    }
+}
