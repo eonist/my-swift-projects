e9be5fab6aa2ff25e6dff79b2d403556aa8ccb13
Eonist <30n1st@gmail.com>
2017-06-18 09:38:16 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/utils/GitTypes.swift

--- Sources/Utils/misc/shell/git/utils/GitTypes.swift
+++ Sources/Utils/misc/shell/git/utils/GitTypes.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class GitTypes {
-
-}
+typealias GitKey = (user:String,  pass:String)
+typealias GitRepo = (localPath:String,  remotePath:String,  branch:String)
+typealias GitMsg = (title:String,  desc:String)
