885fb05669a8ac5b8b47c134ce8042b51ebe9bfa
Eonist <30n1st@gmail.com>
2017-06-18 12:34:59 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/Git.swift

--- Sources/Utils/misc/shell/git/Git.swift
+++ Sources/Utils/misc/shell/git/Git.swift
@@ -5,4 +5,19 @@ import Foundation
  */
 class Git {
     static var path:String = "/usr/bin/"/*To execute git commands we need to call the git commands from this path*/
+    /**/
+    static var git:String = "git"
+    static var add:String = "add"
+    static var commit:String = "commit"
+    static var reset:String = "reset"
+    static var pull:String = "pull"
+    static var push:String = "push"
+    static var initiate:String = "init"
+    static var rm:String = "rm"
+    static var origin:String = "origin"
+    static var remote:String = "remote"
+    static var clone:String = "clone"
+    static var update:String = "update"
+    static var fetch:String = "fetch"
+    static var checkout:String = "checkout"
 }
