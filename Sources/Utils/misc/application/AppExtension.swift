87b7f6822b7c0ffc2215a2fb693e95cac915d741
Eonist <30n1st@gmail.com>
2017-06-23 18:00:16 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/application/AppExtension.swift

--- Sources/Utils/misc/application/AppExtension.swift
+++ Sources/Utils/misc/application/AppExtension.swift
@@ -32,10 +32,11 @@ extension NSApplication{
 private class Utils{
     /**
      * NOTE: Reducing for-loops is a great way to maintain readability and maintain code modularity. Here is a trick were we use closure blocks to encapsulate the method call. The for loop is the same but the method call is different. This approach is great when you need the code within the for-loop to be the same but you want to have the code within different methods to be different
+     * TODO: Use .first and create a typealias for the action method
      */
     static func performAction(_ windows:[NSWindow], _ action:(NSWindow)->Bool)->NSWindow?{
         for window in windows{
-            if(action(window)){
+            if action(window) {
                 return window
             }
         }
