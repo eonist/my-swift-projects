a87674cc8c410db9b70f11332d420e73b8732c3c
Eonist <30n1st@gmail.com>
2017-07-14 17:20:43 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/RegExp.swift

--- Sources/Utils/string/regexp/RegExp.swift
+++ Sources/Utils/string/regexp/RegExp.swift
@@ -7,7 +7,6 @@ import Foundation
  * m - Control the behavior of "^" and "$" in a pattern. By default these will only match at the start and end, respectively, of the input text. If this flag is set, "^" and "$" will also match at the start and end of each line within the input text.
  * Para - Controls the behavior of \b in a pattern. If set, word boundaries are found according to the definitions of word found in Unicode UAX 29, Text Boundaries. By default, word boundaries are identified by means of a simple classification of characters as either “word” or “non-word”, which approximates traditional regular expression behavior. The results obtained with the two options can be quite different in runs of spaces and other non-word characters.
  */
-
 public class RegExp{
     /**
      * Asserts if a match exists
