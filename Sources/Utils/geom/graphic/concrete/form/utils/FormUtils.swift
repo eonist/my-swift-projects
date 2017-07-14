f072a26f19c096bf26f57f3bcb9a87b22384ff44
Eonist <30n1st@gmail.com>
2017-07-14 14:18:41 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swift

--- Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swift
+++ Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swift
@@ -2,11 +2,12 @@ import Foundation
 
 class PolyLineGraphicUtils{
     /**
+     * Returns a path that conceptually is a PolyLine
      * NOTE: Rename to pathByPoints?, 🚫 as swift supports method overloading, you don't need that specific naming, the context given by this class name also maginfies its readability
      */
     static func path(_ points:[CGPoint]) -> IPath {
         return (1..<points.count).indices.reduce(Path([PathCommand.moveTo.rawValue],[points[0].x,points[0].y])) { path,i in
-            var path = path
+            var path = path/*Temporarily store the interim value*/
             path.commands.append(PathCommand.lineTo.rawValue)
             let p:CGPoint = points[i]
             path.pathData += [p.x,p.y]
@@ -14,4 +15,3 @@ class PolyLineGraphicUtils{
         }
     }
 }
-
