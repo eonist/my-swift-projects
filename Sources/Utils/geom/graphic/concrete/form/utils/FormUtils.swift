2df57c4fac391b4daeb41d70e391da4662635b29
Eonist <30n1st@gmail.com>
2017-07-14 14:16:01 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swiftSources/Utils/geom/path/IPath.swift

--- Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swift
+++ Sources/Utils/geom/graphic/concrete/form/utils/FormUtils.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class PolyLineGraphicUtils{
     /**
-     * NOTE: Rename to pathByPoints?, 🚫 as swift supports method overloading, you don't need that specific naming
+     * NOTE: Rename to pathByPoints?, 🚫 as swift supports method overloading, you don't need that specific naming, the context given by this class name also maginfies its readability
      */
     static func path(_ points:[CGPoint]) -> IPath {
         return (1..<points.count).indices.reduce(Path([PathCommand.moveTo.rawValue],[points[0].x,points[0].y])) { path,i in
--- Sources/Utils/geom/path/IPath.swift
+++ Sources/Utils/geom/path/IPath.swift
@@ -1,5 +1,7 @@
 import Foundation
-
+/**
+ * TODO: Consider renaming IPath to PathDrawable or PathKind, more intune with the swift naming convention
+ */
 protocol IPath{/*<-When you extend class you can mutate variables directly when casting to IPath, why because scruts don't allow this and so on..*/
     var commands:[Int]{get set}
     var pathData:[CGFloat]{get set}
