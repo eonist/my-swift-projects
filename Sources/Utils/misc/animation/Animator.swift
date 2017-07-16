62c5d7e2d770b3db15d7d980dea02502e46b9498
Eonist <30n1st@gmail.com>
2017-07-16 14:54:36 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/Animator.swift

--- Sources/Utils/misc/animation/Animator.swift
+++ Sources/Utils/misc/animation/Animator.swift
@@ -1,23 +1,24 @@
 import Cocoa
 /**
- * TODO: You can store the active animator instance count in the AnimatableView to know when to stop the cvdisplaylink
- * TODO: Take a look at other animation libs
- * TODO: Add onComplete selector callback method on init and as a variable, do the same with method, use optional to assert if they exist or not
- * TODO: Seek,reverse,repeate,autoRepeat
- * TODO: Implement a way so that the animator can be reused so that you can target it and disable it
+ * TODO: ⚠️️ You can store the active animator instance count in the AnimatableView to know when to stop the cvdisplaylink
+ * TODO: ⚠️️ Take a look at other animation libs
+ * TODO: ⚠️️ Add onComplete selector callback method on init and as a variable, do the same with method, use optional to assert if they exist or not
+ * TODO: ⚠️️ Seek,reverse,repeate,autoRepeat
+ * TODO: ⚠️️ Implement a way so that the animator can be reused so that you can target it and disable it
  */
+typealias FrameTick = (CGFloat)->Void
+typealias EasingEquation = (CGFloat,CGFloat,CGFloat,CGFloat)->CGFloat
 class Animator:BaseAnimation{
     let fps:CGFloat = 60//<--TODO: this should be derived from a device variable
     var duration:CGFloat/*In seconds*/
     var from:CGFloat/*From this value*/
     var to:CGFloat/*To this value*/
-    var callBack:(CGFloat)->Void/*the closure method that is called on every "frame-tick" and that changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var callBack:FrameTick/*the closure method that is called on every "frame-tick" and that changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var framesToEnd:CGFloat/*totFrameCount*/
     var currentFrameCount:CGFloat = 0/*curFrameCount*/
     var easing:(CGFloat,CGFloat,CGFloat,CGFloat)->CGFloat/*variable for holding the easing method*/
     //isActive used by the AnimatiableView to assert if an animator is active or not
-    //@escaping was auto added when migrating to swift 3, may break things:
-    init(_ animatable:IAnimatable, _ duration:CGFloat = 0.5, _ from:CGFloat, _ to:CGFloat, _ callBack:@escaping (CGFloat)->Void, _ easing:@escaping (CGFloat,CGFloat,CGFloat,CGFloat)->CGFloat = Linear.ease){
+    init(_ animatable:IAnimatable, _ duration:CGFloat = 0.5, _ from:CGFloat, _ to:CGFloat, _ callBack:@escaping FrameTick, _ easing:@escaping  EasingEquation = Linear.ease){
         self.duration = duration
         self.from = from
         self.to = to
