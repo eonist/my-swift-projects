c43ee1c9ce4e636d96b0a773dc25c2cee2d49a3b
Eonist <30n1st@gmail.com>
2017-07-16 21:50:03 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/animation/movers/Mover.swiftSources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -1,28 +1,27 @@
 import Foundation
 
-class Spring {
-    
-    }
-
 class Spring:BaseAnimation {
     
     var spring:CGFloat = 0.02
+    var friction:CGFloat = 0.95
     var targetX:CGFloat = 0
     var vx:CGFloat = 0/*velocity*/
-    var friction:CGFloat = 0.95
-
-    
+ 
     var value:CGFloat = 0/*the value that should be applied to the target*/
-   
+
     var callBack:FrameTick/*the closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat, _ spring:CGFloat = 0.02){
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat, _ spring:CGFloat = 0.02, _ friction:CGFloat = 0.95){
         self.value = value
         self.spring = spring
         self.callBack = callBack
         super.init(animatable)
     }
     func updatePosition(_ direct:Bool = false) {
-        value += velocity
+        let dx:CGFloat = targetX - value
+        let ax:CGFloat = dx * spring
+        vx += ax
+        vx *= friction
+        value += vx
     }
     override func onFrame(){
         updatePosition()
