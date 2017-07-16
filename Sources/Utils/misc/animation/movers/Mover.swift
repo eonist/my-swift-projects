01d82c03b645d6fa75f575266c7a263514f758a0
Eonist <30n1st@gmail.com>
2017-07-16 15:42:40 +0200

    Files modified: 3
    Modified 3 files:Sources/Utils/misc/animation/movers/Mover.swiftSources/Utils/misc/animation/movers/RubberBand.swiftSources/Utils/misc/animation/movers/SnapFriction.swift

--- Sources/Utils/misc/animation/movers/Mover.swift
+++ Sources/Utils/misc/animation/movers/Mover.swift
@@ -2,8 +2,8 @@ import Cocoa
 
 class Mover:BaseAnimation {
     var value:CGFloat = 0/*the value that should be applied to the target*/
-    var velocity:CGFloat
-    var callBack:(CGFloat)->Void/*the closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var velocity:CGFloat/*The amount increased on each frame-tick of the animation*/
+    var callBack:FrameTick/*the closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     init(_ animatable:IAnimatable, _ callBack:@escaping FrameTick,_ value:CGFloat, _ velocity:CGFloat = 0){
         self.value = value
         self.velocity = velocity
--- Sources/Utils/misc/animation/movers/RubberBand.swift
+++ Sources/Utils/misc/animation/movers/RubberBand.swift
@@ -24,7 +24,7 @@ class RubberBand:Mover{//TODO: rename to Elastic
     var result:CGFloat = 0/*output value, this is the value that external callers can use, its the var value after friction etc has been applied, it cannot be set from outside but can only be read from outside*/
     var hasStopped:Bool = true/*indicates that the motion has stopped*/
     
-    init(_ callBack:@escaping CallBack,_ maskFrame:Frame, _ contentFrame:Frame,_ config:Config) {
+    init(_ callBack:@escaping FrameTick,_ maskFrame:Frame, _ contentFrame:Frame,_ config:Config) {
         self.maskFrame = maskFrame
         self.contentFrame = contentFrame
         self.config = config
