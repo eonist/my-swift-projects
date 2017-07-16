12ed3a4f2b0282f5010c5ba6997138365c6bdbe6
Eonist <30n1st@gmail.com>
2017-07-16 22:20:53 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -7,18 +7,19 @@ class Spring:BaseAnimation {
         //also make a stop metod that stops the anim if the velocity goes bellow a number, also sets final point to target point etc
         //you can even make multiple spring classes or use generics to support point,size,cgfloat etc, or some other solution
     
-    var spring:CGFloat = 0.02
-    var friction:CGFloat = 0.95
+    var spring:CGFloat
+    var friction:CGFloat
     var targetX:CGFloat = 0
     var velocityX:CGFloat = 0/*velocity*/
  
-    var value:CGFloat = 0/*the value that should be applied to the target*/
+    var value:CGFloat/*the value that should be applied to the target*/
 
     var callBack:FrameTick/*the closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat = 0, _ spring:CGFloat = 0.02, _ friction:CGFloat = 0.95){
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat = 0, _ spring:CGFloat = 0.02, _ friction:CGFloat = 0.90){
         self.value = value
         self.spring = spring
         self.callBack = callBack
+        self.friction = friction
         super.init(animatable)
     }
     func updatePosition(_ direct:Bool = false) {
