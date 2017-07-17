a508b0f766f8c68f1f0f02c8e559168466df9d05
Eonist <30n1st@gmail.com>
2017-07-17 12:54:12 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -2,26 +2,24 @@ import Foundation
 
 class Spring:BaseAnimation {
     /*Config values*/
-    var spring:CGFloat
-    var friction:CGFloat
+    var config:(spring:CGFloat,friction:CGFloat)
     /*Interim values*/
     var targetValue:CGFloat = 0/*Where it should go*/
     var velocity:CGFloat = 0/*Velocity*/
     var value:CGFloat/*The value that should be applied to the target*/
     /*Event related*/
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat = 0, _ spring:CGFloat = 0.02, _ friction:CGFloat = 0.90){
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat = 0, config:(spring:CGFloat, friction:CGFloat) = (spring:0.02,friction:0.90)) {
         self.value = value/*Set the init value*/
-        self.spring = spring
         self.callBack = callBack
-        self.friction = friction
+        self.config = config
         super.init(animatable)
     }
     func updatePosition() {
         let dx:CGFloat = targetValue - value
-        let ax:CGFloat = dx * spring
+        let ax:CGFloat = dx * config.spring
         velocity += ax
-        velocity *= friction
+        velocity *= config.friction
         value += velocity
         checkForStop()
     }
