898cfc29dc1b8f161e1be8077e0d4aa295fed8b7
Eonist <30n1st@gmail.com>
2017-07-17 23:15:53 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Easer.swift

--- Sources/Utils/misc/animation/movers/Easer.swift
+++ Sources/Utils/misc/animation/movers/Easer.swift
@@ -3,7 +3,7 @@ import Foundation
 class Easer<T:NumberKind>:BaseAnimation {
     typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
     /*Config values*/
-    var config:(easing:T,friction:T)
+    var easing:T
     /*Interim values*/
     var targetValue:T /*Where value should go to*/
     var velocity:T/*Velocity*/
@@ -12,17 +12,17 @@ class Easer<T:NumberKind>:BaseAnimation {
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var stopVelocity:T
     
-    init(_ callBack:@escaping FrameTick,  _ config:(easing:T) , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
+    init(_ callBack:@escaping FrameTick,  _ easing:T , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
         self.callBack = callBack
-        self.config = config
+        self.easing = easing
         self.stopVelocity = initVals.stopVelocity
         super.init()
     }
     func updatePosition() {
-        velocity = (targetValue - value) * config.easing
+        velocity = (targetValue - value) * easing
         value = value + velocity
         if assertStop {stop()}
     }
