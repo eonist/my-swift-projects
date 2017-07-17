49541746dbed8bb839eaf1efa7d271c2c5d1dabf
Eonist <30n1st@gmail.com>
2017-07-17 13:11:37 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -2,21 +2,21 @@ import Foundation
 
 class Spring<T:NumberKind>:BaseAnimation {
     /*Config values*/
-    var config:(spring:CGFloat,friction:CGFloat)
+    var config:(spring:T,friction:T)
     /*Interim values*/
     var targetValue:T /*Where it should go*/
-    var velocity:CGFloat = 0/*Velocity*/
+    var velocity:T/*Velocity*/
     var value:T/*The value that should be applied to the target*/
     /*Event related*/
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, config:(spring:CGFloat, friction:CGFloat) = (spring:0.02,friction:0.90), _ value:T) {
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, config:(spring:T, friction:T)/* = (spring:0.02,friction:0.90)*/, _ value:T) {
         self.value = value/*Set the init value*/
         self.callBack = callBack
         self.config = config
         super.init(animatable)
     }
     func updatePosition() {
-        let dx = targetValue - value
+        let dx = (targetValue - value)
         let ax = dx * config.spring
         velocity = velocity + ax
         velocity = velocity * config.friction
@@ -35,3 +35,6 @@ class Spring<T:NumberKind>:BaseAnimation {
         callBack(value)
     }
 }
+
+//Continue here:
+    //Figure out FrameTick in gerics
