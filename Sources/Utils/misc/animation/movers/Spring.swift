fbf764dd0678ab2e992371b9501b6f12f4673c3e
Eonist <30n1st@gmail.com>
2017-07-17 16:27:20 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -13,13 +13,13 @@ class Spring<T:NumberKind>:BaseAnimation {
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var stopAssert:(T)->Bool
     
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, _ stopAssert:StopAssert = Spring.defaultStopAssert, _ config:(spring:T, friction:T)/* = (spring:0.02,friction:0.90)*/, _ initVals:(value:T,targetValue:T,velocity:T)) {
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, _ stopAssert:StopAssert?, _ config:(spring:T, friction:T)?, _ initVals:(value:T,targetValue:T,velocity:T)?) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
         self.callBack = callBack
-        self.config = config
-        self.stopAssert = /*stopAssert ?? */Spring.defaultStopAssert
+        self.config = config ?? (spring:0.02,friction:0.90)
+        self.stopAssert = stopAssert ?? Spring.defaultStopAssert
         super.init(animatable)
     }
     func updatePosition() {
