715acae0047aed56afcd049b63d5582562245f26
Eonist <30n1st@gmail.com>
2017-07-17 23:11:03 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Easer.swift

--- Sources/Utils/misc/animation/movers/Easer.swift
+++ Sources/Utils/misc/animation/movers/Easer.swift
@@ -1,15 +1,9 @@
 import Foundation
 
-class Easer {
-
-}
-
-
-
-class Easer:BaseAnimation {
+class Easer<T:NumberKind>:BaseAnimation {
     typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
     /*Config values*/
-    var config:(spring:T,friction:T)
+    var config:(easing:T,friction:T)
     /*Interim values*/
     var targetValue:T /*Where value should go to*/
     var velocity:T/*Velocity*/
@@ -18,7 +12,7 @@ class Easer:BaseAnimation {
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var stopVelocity:T
     
-    init(_ callBack:@escaping FrameTick,  _ config:(spring:T, friction:T) , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
+    init(_ callBack:@escaping FrameTick,  _ config:(easing:T, friction:T) , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
@@ -29,7 +23,7 @@ class Easer:BaseAnimation {
     }
     func updatePosition() {
         let d = (targetValue - value)
-        let a = d * config.spring
+        let a = d * config.easing
         velocity = velocity + a
         velocity = velocity * config.friction
         value = value + velocity
