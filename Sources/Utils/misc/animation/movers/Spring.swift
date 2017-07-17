22580f91b1dfb743691d21f473265c8417829020
Eonist <30n1st@gmail.com>
2017-07-17 22:39:05 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -2,7 +2,6 @@ import Foundation
 
 class Spring<T:NumberKind>:BaseAnimation {
     typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
-    typealias StopAssert = (T)->Bool
     /*Config values*/
     var config:(spring:T,friction:T)
     /*Interim values*/
@@ -13,7 +12,7 @@ class Spring<T:NumberKind>:BaseAnimation {
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var stopVelocity:T
     
-    init(callBack:@escaping FrameTick,  _ config:(spring:T, friction:T) , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
+    init(_ callBack:@escaping FrameTick,  _ config:(spring:T, friction:T) , _ initVals:(value:T,targetValue:T,velocity:T,stopVelocity:T)) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
