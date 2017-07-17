39de4e1cf973e060c85e17bf58911f651c05eedd
Eonist <30n1st@gmail.com>
2017-07-17 15:06:50 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -10,11 +10,9 @@ class Spring<T:NumberKind>:BaseAnimation {
     var value:T/*The value that should be applied to the target*/
     /*Event related*/
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    
-    
     var stopAssert:(T)->Bool
     
-    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, config:(spring:T, friction:T)/* = (spring:0.02,friction:0.90)*/, initVals:(value:T,targetValue:T,velocity:T)) {
+    init(_ animatable:Animatable, _ callBack:@escaping FrameTick, _ config:(spring:T, friction:T)/* = (spring:0.02,friction:0.90)*/, _ initVals:(value:T,targetValue:T,velocity:T)) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
