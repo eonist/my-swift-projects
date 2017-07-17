78a805f61589614f5a56c5848588dff56ee695c0
Eonist <30n1st@gmail.com>
2017-07-17 12:02:40 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/math/number/generic/NumberKind.swift

--- Sources/Utils/math/number/generic/NumberKind.swift
+++ Sources/Utils/math/number/generic/NumberKind.swift
@@ -9,21 +9,25 @@ protocol NumberKind {
     static func /(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
 }
-extension Int: NumberKind {}
-extension CGFloat: NumberKind {}
+extension Int:NumberKind {}
+extension CGFloat:NumberKind {}
 //extension Float: NumberKind {}
 //extension Double: NumberKind {}
 //extension UInt: NumberKind {}
 
-func add<T: NumberKind>(x: T, y: T) -> T {
+
+func add<T:NumberKind>(x: T, y: T) -> T {
     return x + y
 }
 func substract<T: NumberKind>(x: T, y: T) -> T {
     return x - y
 }
-func divide<T: NumberKind>(x: T, y: T) -> T {
+func divide<T:NumberKind>(x: T, y: T) -> T {
     return x / y
 }
-func multiply<T: NumberKind>(x: T, y: T) -> T {
+func multiply<T:NumberKind>(x: T, y: T) -> T {
     return x * y
+}
+func * <T:NumberKind>(left: T, right: T) -> T {
+    return left * (right)
 }
