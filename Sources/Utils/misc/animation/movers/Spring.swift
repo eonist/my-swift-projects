243f36d31bae049eb2d56a3af5cbb6f053ff554b
Eonist <30n1st@gmail.com>
2017-07-17 12:31:56 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/Spring.swift

--- Sources/Utils/misc/animation/movers/Spring.swift
+++ Sources/Utils/misc/animation/movers/Spring.swift
@@ -9,7 +9,7 @@ class Spring:BaseAnimation {
     var velocity:CGFloat = 0/*Velocity*/
     var value:CGFloat/*The value that should be applied to the target*/
     /*Event related*/
-    var callBack:FrameTick/*the closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     init(_ animatable:Animatable, _ callBack:@escaping FrameTick,_ value:CGFloat = 0, _ spring:CGFloat = 0.02, _ friction:CGFloat = 0.90){
         self.value = value/*Set the init value*/
         self.spring = spring
