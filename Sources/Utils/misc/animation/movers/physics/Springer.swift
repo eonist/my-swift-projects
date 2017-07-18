d40954f56b3e8e2a8d083241fca131929e64ef9c
Eonist <30n1st@gmail.com>
2017-07-18 19:25:59 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Springer.swift

--- Sources/Utils/misc/animation/movers/physics/Springer.swift
+++ Sources/Utils/misc/animation/movers/physics/Springer.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-class Springer<T:ArithmeticKind>:BaseAnimation,PhysicsAnimationKind {
+class Springer/*<T:ArithmeticKind>*/:BaseAnimation,PhysicsAnimationKind {
     /*Signatures*/
-    typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
-    typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
-    typealias Config = (spring:T,friction:T)
+    //typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
+    //typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
+    //typealias Config = (spring:T,friction:T)
     /*Config values*/
     var initValues:InitValues//default: (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))
-    var config:Config//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
+    var config:PhysicsAnimationKind.Config//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
     /*CallBack related*/
     var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     
