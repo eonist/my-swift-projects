0127d33149c1d83599d61b88d059c58f04c9f5d3
Eonist <30n1st@gmail.com>
2017-07-18 23:17:55 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/EventSendable.swift

--- Sources/Utils/misc/event/EventSendable.swift
+++ Sources/Utils/misc/event/EventSendable.swift
@@ -3,8 +3,8 @@ import Foundation
  * All elements in the hierarchy that want's to propegate the event must implement this protocol 
  * TODO: Rename to: EventSendable?
  */
-typealias EventSendable = IEventSender//Legacy support
-protocol IEventSender:class {
+typealias IEventSender = EventSendable//Legacy support
+protocol EventSendable:class {
     var event:EventCallBack{get set}
     func onEvent(_ event:Event)
 }
