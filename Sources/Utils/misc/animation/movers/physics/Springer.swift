687f8c0fef35b5157f79fe8c53b1a33c490305ae
Eonist <30n1st@gmail.com>
2017-07-18 19:30:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Springer.swift

--- Sources/Utils/misc/animation/movers/physics/Springer.swift
+++ Sources/Utils/misc/animation/movers/physics/Springer.swift
@@ -1,17 +1,19 @@
 import Foundation
 
 class Springer/*<T:ArithmeticKind>*/:BaseAnimation,PhysicsAnimationKind {
+    typealias argType = type
+
     /*Signatures*/
     //typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
     //typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
     //typealias Config = (spring:T,friction:T)
     /*Config values*/
     var initValues:(value:argType,targetValue:argType,velocity:argType,stopVelocity:argType)//default: (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))
-    var config:(argType)->Void//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
+    var config:(spring:argType,friction:argType)//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
     /*CallBack related*/
     var callBack:(argType)->Void/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     
-    init(_ callBack:@escaping FrameTick,  _ initValues:InitValues, _ config:(argType)->Void) {
+    init(_ callBack:@escaping (argType)->Void,  _ initValues:(value:argType,targetValue:argType,velocity:argType,stopVelocity:argType), _ config:(spring:argType,friction:argType)) {
         self.initValues = initValues
         self.callBack = callBack
         self.config = config
