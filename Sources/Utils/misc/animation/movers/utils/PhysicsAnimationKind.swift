fee75877da65a670ec9534b3a5fb5148044bd5d0
Eonist <30n1st@gmail.com>
2017-07-18 14:54:46 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/utils/PhysicsAnimationKind.swift

--- Sources/Utils/misc/animation/movers/utils/PhysicsAnimationKind.swift
+++ Sources/Utils/misc/animation/movers/utils/PhysicsAnimationKind.swift
@@ -1,12 +1,15 @@
 import Foundation
 
 protocol PhysicsAnimationKind {
-    var config:(spring:T,friction:T) {get set}
+    associatedtype argType
     
-    var targetValue:T {get set} /*Where value should go to*/
-    var velocity:T {get set}/*Velocity*/
-    var value:T {get set}/*The value that should be applied to the target*/
+    
+    var config:(spring:argType,friction:argType) {get set}
+    
+    var targetValue:argType {get set} /*Where value should go to*/
+    var velocity:argType {get set}/*Velocity*/
+    var value:argType {get set}/*The value that should be applied to the target*/
     /*Event related*/
-    var callBack:Springer<T>.FrameTick {get set}/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
-    var stopVelocity:T {get set}
+    var callBack:Springer<argType>.FrameTick {get set}/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var stopVelocity:argType {get set}
 }
