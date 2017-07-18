f94eb845823b1f5b71846bc01c3b810753c9b510
Eonist <30n1st@gmail.com>
2017-07-18 23:43:57 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Easer.swift

--- Sources/Utils/misc/animation/movers/physics/Easer.swift
+++ Sources/Utils/misc/animation/movers/physics/Easer.swift
@@ -8,22 +8,11 @@ class Easer<T>:BaseAnimation,PhysicsAnimationKind {
     typealias argType = T
     var easing:T//CGPoint(0.2,0.2)
     var initValues:InitValues
-    var config:Config/*Config values*/
     var callBack:FrameTickSignature
-    init(_ callBack:@escaping FrameTickSignature,  _ initValues:InitValues, _ config:Config) {
+    init(_ callBack:@escaping FrameTickSignature,  _ initValues:InitValues, _ easing:T) {
         self.initValues = initValues
         self.callBack = callBack
-        self.config = config
-        super.init()
-    }
-
-    init(_ callBack:@escaping Springer.FrameTick,  _ easing:T , _ initVals:Springer.InitValues) {
-        self.value = initVals.value/*Set the init value*/
-        self.targetValue = initVals.targetValue
-        self.velocity = initVals.velocity
-        self.callBack = callBack
         self.easing = easing
-        self.stopVelocity = initVals.stopVelocity
         super.init()
     }
     func updatePosition() {
@@ -39,4 +28,3 @@ class Easer<T>:BaseAnimation,PhysicsAnimationKind {
         callBack(value)
     }
 }
-/**/
