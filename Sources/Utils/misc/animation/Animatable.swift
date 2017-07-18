083844785bba0d85efdd72dc918dd13284c5afbd
Eonist <30n1st@gmail.com>
2017-07-18 14:38:51 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/Animatable.swift

--- Sources/Utils/misc/animation/Animatable.swift
+++ Sources/Utils/misc/animation/Animatable.swift
@@ -2,8 +2,8 @@ import Cocoa
 /**
  * TODO: ⚠️️ This should possibly be renamed to IAnimatableView
  */
-typealias Animatable = IAnimatable//legacy support
-protocol IAnimatable:class {
+typealias IAnimatable = Animatable//legacy support
+protocol Animatable:class {
     func onFrame()
     var animators:[BaseAnimation]{get set}
     var displayLink:CVDisplayLink{get}
