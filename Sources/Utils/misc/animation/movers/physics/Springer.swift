1150287623c926de37e990e495fb20ec3791bb75
Eonist <30n1st@gmail.com>
2017-07-18 20:45:12 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Springer.swift

--- Sources/Utils/misc/animation/movers/physics/Springer.swift
+++ Sources/Utils/misc/animation/movers/physics/Springer.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 class Springer:BaseAnimation,PhysicsAnimationKind {
-    typealias argType = CGFloat
+    typealias theType = CGFloat
     /*Signatures*/
     //typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
     //typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
     //typealias Config = (spring:T,friction:T)
     /*Config values*/
-    var initValues:InitValues//(value:argType,targetValue:argType,velocity:argType,stopVelocity:argType)//default: (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))
-    var config:(spring:argType,friction:argType)//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
+    var initValues:(value:theType,targetValue:theType,velocity:theType,stopVelocity:theType)//default: (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))
+    var config:(spring:theType,friction:theType)//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
     /*CallBack related*/
-    var callBack:(argType)->Void/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var callBack:(theType)->Void/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     
-    init(_ callBack:@escaping (argType)->Void,  _ initValues:(value:argType,targetValue:argType,velocity:argType,stopVelocity:argType), _ config:(spring:argType,friction:argType)) {
+    init(_ callBack:@escaping (theType)->Void,  _ initValues:(value:theType,targetValue:theType,velocity:theType,stopVelocity:theType), _ config:(spring:theType,friction:theType)) {
         self.initValues = initValues
         self.callBack = callBack
         self.config = config
