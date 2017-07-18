80559ea209b2d85f35e0dd5f33f5da1c970f0816
Eonist <30n1st@gmail.com>
2017-07-18 19:05:43 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Springer.swift

--- Sources/Utils/misc/animation/movers/physics/Springer.swift
+++ Sources/Utils/misc/animation/movers/physics/Springer.swift
@@ -2,14 +2,14 @@ import Foundation
 
 class Springer<T:ArithmeticKind>:BaseAnimation,PhysicsAnimationKind {
     /*Signatures*/
-    typealias FrameTick = (argType)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
+    typealias FrameTick = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
     typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
     typealias Config = (spring:T,friction:T)
     /*Config values*/
     var initValues:InitValues//default: (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))
     var config:Config//default: (CGPoint(0.02,0.02),CGPoint(0.90,0.90))
     /*Event related*/
-    var callBack:(argType)->Void/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var callBack:FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     
     init(_ callBack:@escaping FrameTick,  _ initValues:InitValues, _ config:Config) {
         self.initValues = initValues
