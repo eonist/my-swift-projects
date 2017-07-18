e0a95741efa916f9b3e680c192d263b7b62c3fba
Eonist <30n1st@gmail.com>
2017-07-18 20:38:10 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/movers/physics/Easer.swift

--- Sources/Utils/misc/animation/movers/physics/Easer.swift
+++ Sources/Utils/misc/animation/movers/physics/Easer.swift
@@ -4,7 +4,7 @@ import Foundation
  * NOTE: The FrameTick and the InitValues typaliases are the same in Springer and Easer so we just reuse them
  * TODO: ⚠️️ The math can be refactored if you add += *= to ArithmeticKind
  */
-class Easer<T:ArithmeticKind>:BaseAnimation {
+/*class Easer<T:ArithmeticKind>:BaseAnimation {
     /*Config values*/
     var easing:T//CGPoint(0.2,0.2)
     /*Interim values*/
@@ -12,10 +12,10 @@ class Easer<T:ArithmeticKind>:BaseAnimation {
     var velocity:T/*Velocity*/
     var value:T/*The value that should be applied to the target*/
     /*Event related*/
-    var callBack:Springer<T>.FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+    var callBack:Springer.FrameTick/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
     var stopVelocity:T
     
-    init(_ callBack:@escaping Springer<T>.FrameTick,  _ easing:T , _ initVals:Springer<T>.InitValues) {
+    init(_ callBack:@escaping Springer.FrameTick,  _ easing:T , _ initVals:Springer.InitValues) {
         self.value = initVals.value/*Set the init value*/
         self.targetValue = initVals.targetValue
         self.velocity = initVals.velocity
@@ -37,3 +37,4 @@ class Easer<T:ArithmeticKind>:BaseAnimation {
         callBack(value)
     }
 }
+*/
