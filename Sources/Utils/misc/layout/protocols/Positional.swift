816edc40b41eed57d971c5448f0a81898badca94
Eonist <30n1st@gmail.com>
2017-07-19 22:55:34 +0200

    'Files renamed: 1'
    '
    
    Renamed 1 file:
     Sources/Utils/misc/layout/protocols/IPositional.swift -> Sources/Utils/misc/layout/protocols/Positional.swift
    
    '

--- Sources/Utils/misc/layout/protocols/Positional.swift
+++ Sources/Utils/misc/layout/protocols/Positional.swift
@@ -2,14 +2,15 @@ import Foundation
 /**
  * TODO: Rename to Positional
  */
-protocol IPositional:class {//<--new extends class makes it castable w/o creating a copy -> struct has issues with casting etc...
+typealias IPositional = Positional
+protocol Positional:class {/*<--new extends class makes it castable w/o creating a copy -> struct has issues with casting etc...*/
     func setPosition(_ position:CGPoint)
     func getPosition() -> CGPoint
 }
 /**
  * CAUTION: These extensions can only be used if you don't need to cast the instance to ISizeable
  */
-extension IPositional{
+extension Positional{
     /*pos was recently moved to the extension since it isn't needed as a variable in the protocol*/
     var pos:CGPoint{/*<-- this is named pos, because the name position is effectivly blocked when using implicit getter and setter names*/
         get{
