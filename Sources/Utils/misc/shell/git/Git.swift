b89423ed336cc374c5d47dd10ae1a62b13f325d7
Eonist <30n1st@gmail.com>
2017-07-19 23:56:53 +0200

    'Files modified: 1'
    '
    
    Modified 1 file:
    Sources/Utils/misc/shell/git/Git.swift
    '

--- Sources/Utils/misc/shell/git/Git.swift
+++ Sources/Utils/misc/shell/git/Git.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /**
- * NOTE: was /usr/local/git/bin/ but that was a fix for an early 🐛
+ * NOTE: Was /usr/local/git/bin/ but that was a fix for an early 🐛
  */
 class Git {
     static var path:String = "/usr/bin/"/*To execute git commands we need to call the git commands from this path*/
