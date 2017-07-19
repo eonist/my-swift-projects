87e4ef96120f0875c765c165dad3d89899eb1903
Eonist <30n1st@gmail.com>
2017-07-19 22:15:53 +0200

    ', Files renamed: 1'
    '
    
    Renamed 1 file:
     Sources/Utils/geom/style/line/IGradientLineStyle.swift -> Sources/Utils/geom/style/line/GradientLineStylable.swift
    
    '

--- Sources/Utils/geom/style/line/GradientLineStylable.swift
+++ Sources/Utils/geom/style/line/GradientLineStylable.swift
@@ -1,9 +1,9 @@
 import Foundation
-
-protocol IGradientLineStyle:ILineStyle {
+typealias IGradientLineStyle = GradientLineStylable
+protocol GradientLineStylable:ILineStyle {
     var gradient:IGradient{get set}
 }
-extension IGradientLineStyle{
+extension GradientLineStylable{
     func mix(_ colors:[CGColor])->GradientLineStyle{
         let c = copy()
         c.gradient.colors = colors
