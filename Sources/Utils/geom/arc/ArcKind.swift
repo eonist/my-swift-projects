6948c45e1fe15ea8f7600dc777c9e922dbcd0429
Eonist <30n1st@gmail.com>
2017-07-19 21:18:31 +0200

    'Files modified: 1'
    '
    
    Modified 1 file:
    Sources/Utils/geom/arc/ArcKind.swift
    '

--- Sources/Utils/geom/arc/ArcKind.swift
+++ Sources/Utils/geom/arc/ArcKind.swift
@@ -11,7 +11,7 @@ protocol ArcKind {
     var end:CGPoint{get set}
     var center:CGPoint?{get set}
 }
-extension IArc{
+extension ArcKind{
     var x:CGFloat {get {return center!.x} set {center!.x = newValue}}/*Convenience*/
     var y:CGFloat {get {return center!.y} set {center!.y = newValue}}/*Convenience*/
     var pathData:[CGFloat] {return BasicArcParser.pathData(self)}
@@ -21,7 +21,7 @@ private class BasicArcParser{
      * Returns the the pathData that represents an Arc Instance (sans the start point)
      * NOTE: Arc-path-data-structure: xRadii,yRadii,rotation,largeArcFlag,sweepFlag,end.x,end.y,center.x,center.y
      */
-    static func pathData(_ arc:IArc)->[CGFloat] {
+    static func pathData(_ arc:ArcKind)->[CGFloat] {
         return [arc.xRadii,arc.yRadii,arc.rotation,arc.largeArcFlag ? 1:0,arc.sweepFlag ? 1:0,arc.end.x,arc.end.y,arc.center!.x,arc.center!.y]
     }
 }
