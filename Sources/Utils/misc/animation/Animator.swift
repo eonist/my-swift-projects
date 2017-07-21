22e9496e615698443ee34a39e66b019467922cea
Eonist <30n1st@gmail.com>
2017-07-21 20:21:43 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/Animator.swift

--- Sources/Utils/misc/animation/Animator.swift
+++ Sources/Utils/misc/animation/Animator.swift
@@ -2,12 +2,9 @@ import Cocoa
 /**
  * This class animates something from A to B with a easing curve attached
  * NOTE: This animation class is more like stock animation, less interuptable than "physics based animation"
- * TODO: ⚠️️ You can store the active animator instance count in the AnimProxy to know when to stop the cvdisplaylink
  * TODO: ⚠️️ Take a look at other animation libs 👈
  * TODO: ⚠️️ Add onComplete selector callback method on init and as a variable, do the same with method, use optional to assert if they exist or not
  * TODO: ⚠️️ Seek,reverse,repeate,autoRepeat
- * TODO: ⚠️️ Implement a way so that the animator can be reused so that you can target it and disable it
- * TODO: ⚠️️ Unify the variables and the init method
  */
 class Animator:BaseAnimation{
     var frameTick:FrameTick/*The closure method that is called on every "frame-tick" and that changes the property, you can use a var closure or a regular method, probably even an inline closure*/
@@ -39,15 +36,4 @@ class Animator:BaseAnimation{
         self.easing = easing
         super.init(animatable)
     }
-}
-typealias FrameTick = (CGFloat)->Void/*the callBack signature for onFrame ticks*/
-extension Animator {
-    typealias InitValues = (duration:CGFloat,from:CGFloat,to:CGFloat)/*Signature for initValues*/
-    static var initValues:InitValues = (duration:0.5,from:0,to:1)/*Default init values*/
-    static var fps:CGFloat = 60//<--TODO: ⚠️️ this should be derived from a device variable
-    var duration:CGFloat {get{return initValues.duration}set{initValues.duration = newValue}}/*In seconds*/
-    var from:CGFloat {get{return initValues.from}set{initValues.from = newValue}}/*From this value*/
-    var to:CGFloat {get{return initValues.to}set{initValues.to = newValue}}/*To this value*/
-    var framesToEnd:CGFloat {return Animator.fps * duration}/*totFrameCount*/
-    
 }
