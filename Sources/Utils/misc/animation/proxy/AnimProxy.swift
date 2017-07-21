997e61ce58a892a974773d2df631e4e8e50b802c
Eonist <30n1st@gmail.com>
2017-07-22 00:15:50 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/proxy/AnimProxy.swift

--- Sources/Utils/misc/animation/proxy/AnimProxy.swift
+++ Sources/Utils/misc/animation/proxy/AnimProxy.swift
@@ -4,13 +4,11 @@ import Cocoa
  * NOTE: This view class serves as a basis for frame animation.
  * NOTE: Override the onFrame method to do frame animations
  * NOTE: Start and stop with CVDisplayLinkStart(displayLink) and CVDisplayLinkStop(displayLink) and CVDisplayLinkIsRunning(displayLink) to assert if the displayLink is running
- * TODO: ⚠️️ You can probably use NSObject instead of NSView. As NSObject has the performSelector method
- * TODO: ⚠️️ Package these classes as it's own lib. Kinetic. Bump. mc2. Other names? 🚫 👉 I think Animation is an appropriate name 👈 maybe AnimManager? or 👉 AnimProxy? 👈 👌
  */
 class AnimProxy:NSObject,AnimProxyKind{/*Apparently the class needs to be NSView in order for the performSelector to work*///<---TODO: ⚠️️ you can probably delete the IAnimatable, as nothing extends this class anyway
     static let shared = AnimProxy()
     lazy var displayLink:CVDisplayLink = self.setUpDisplayLink()/*This is the instance that enables frame animation, lazying this value will probably haunt me later, playing with fire*/
-    var animators:[FrameAnimator] = []/*Stores the animators*/
+    lazy var animators:[FrameAnimator] = []/*Stores the animators*/
     /**
      * Fires on every screen refresh at 60 FPS, or device speed
      */
