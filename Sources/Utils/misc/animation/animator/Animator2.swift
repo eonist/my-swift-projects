f90b454e966299f5298c449f9be0f0543ad8abe9
Eonist <30n1st@gmail.com>
2017-07-21 23:29:26 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/Animator2.swift

--- Sources/Utils/misc/animation/animator/Animator2.swift
+++ Sources/Utils/misc/animation/animator/Animator2.swift
@@ -3,8 +3,11 @@ import Foundation
  * .onComplete .onFrame .onStop .onStart
  */
 class Animator2 {
-    typealias FrameTick = (CGFloat) -> Animator2 /*Makes the return type less verbose*/
-    var onFrameTick:FrameTick?
+    typealias FrameTick = ((CGFloat) -> Animator2)?
+    //(CGFloat) -> Animator2 /*Makes the return type less verbose*/
+    var onFrameTick:FrameTick {
+        return 
+    }()
     init(){
         self.onFrameTick = onFrame
     }
