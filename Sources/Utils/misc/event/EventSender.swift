aa560ff6a85111ddc57e4cdb988ed7a8ad32a4c0
Eonist <30n1st@gmail.com>
2017-07-22 00:18:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/EventSender.swift

--- Sources/Utils/misc/event/EventSender.swift
+++ Sources/Utils/misc/event/EventSender.swift
@@ -16,7 +16,6 @@ class EventSender:EventSendable {
     /**
      * EXAMPLE: override onEvent in a subClass then assert origin === thumb && event.type == ButtonEvent.down
      * IMPORTANT: ⚠️️ Remember to add immediate to the outgoing event.
-     * TODO: ⚠️️ it may be usefull to implement the immediate attaching functionality in the bellow method
      */
     func onEvent(_ event:Event){//override in subclass and call super
         self.event!(event.setImmediate(self))//the setImmediate attaches the immediate instance to the event.
