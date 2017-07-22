764d2fcfee2c227b4e2cf5f953e3ebbc4e01851a
Eonist <30n1st@gmail.com>
2017-07-22 17:40:41 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/Animator2.swift

--- Sources/Utils/misc/animation/animator/Animator2.swift
+++ Sources/Utils/misc/animation/animator/Animator2.swift
@@ -42,11 +42,12 @@ class Animator2:FrameAnimator {
      }
     
     typealias Completed = () -> Void
-    lazy var completed:Completed = {_ in }
-    typealias Completed2 = (CGFloat) -> Void
-    lazy var completed:Completed2 = {value in }
+    lazy var completed:Completed = onComplete
     
-    
+    func onComplete(closure: @escaping Completed) -> Self{
+        completed = closure/*assign the closure*/
+        return self/*Always return self so we can chain*/
+    }
     /**
      *
      */
