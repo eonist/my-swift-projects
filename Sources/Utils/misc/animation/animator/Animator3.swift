7899e9bf84b9ff1ef0de0c12290284ecc91f59ee
Eonist <30n1st@gmail.com>
2017-07-22 18:23:19 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/Animator3.swift

--- Sources/Utils/misc/animation/animator/Animator3.swift
+++ Sources/Utils/misc/animation/animator/Animator3.swift
@@ -6,11 +6,11 @@ import Foundation
  //if you need to stop the entire anim chain you need to store each successive anim in an array and stop the one that is running, you can create utilitity methods that does this for you
  //Later you can maybe create a class that is called AnimSeq, which can sequence anim from a json file, akin to your legacy project
  
- let anim1 = Anim((dur:2.75,from:0,to:1)) {
+ let anim1 = Anim(config:(dur:2.75,from:0,to:1)) {
     //onFrame anim here, move X forward
  }.pause(at:1.25,for:2){//pauses the anim for a little bit
     //do some things, fetch data etc
- }.onComplete(Anim(dur:1.5,from:1,to:0,repeat:3)){//adds a new anim block to the completed callBack
+ }.onComplete(config:(dur:2.75,from:0,to:1,repeat:3)){//adds a new anim block to the completed callBack
     //onFrame anim here, rotate 360deg , this animation is repeated 3 times
  }.completed = {
     //this is the final complete call in the chain
