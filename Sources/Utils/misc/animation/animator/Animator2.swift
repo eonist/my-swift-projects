34bc3d022837f75be8a98ca49987cf6c29d5b35f
Eonist <30n1st@gmail.com>
2017-07-22 12:26:13 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/Animator2.swift

--- Sources/Utils/misc/animation/animator/Animator2.swift
+++ Sources/Utils/misc/animation/animator/Animator2.swift
@@ -5,11 +5,25 @@ import Foundation
  */
 class Animator2:FrameAnimator {
     var frameTick:FrameTick
+    var currentFrameCount:CGFloat = 0/*curFrameCount, this is need in order to know when the animation is complete*/
+    private var framesToEnd:CGFloat {return Animator.fps * duration}/*totFrameCount*/
     //(CGFloat) -> Animator2 /*Makes the return type less verbose*/
     init(initValues:Animator.InitValues, closure: @escaping FrameTick) {
         self.frameTick = closure
         //return TestingClass()
         super.init(AnimProxy.shared)
+    }
+    /**
+     * Fires on every frame tick
+     */
+    override func onFrame(){
+        let val:CGFloat = easing(currentFrameCount, from, to-from, framesToEnd)
+        frameTick(val)/*Call the callBack onFrame method*/
+        if(currentFrameCount == framesToEnd){
+            stop()/*Stop the animation*/
+            super.onEvent(AnimEvent(AnimEvent.completed,self))/*Notify listeners that the animation completed*/
+        }
+        self.currentFrameCount += 1
     }
     /*func pause(durInSec:CGFloat, closure: (Int) -> Void) -> Self {
      closure(value)/*Call the method*/
