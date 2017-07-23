7d5608fffbc4c5fb4fa081b7480dacdea032d8db
Eonist <30n1st@gmail.com>
2017-07-23 18:32:41 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/color/cg/ColorParser.swift

--- Sources/Utils/geom/color/cg/ColorParser.swift
+++ Sources/Utils/geom/color/cg/ColorParser.swift
@@ -25,21 +25,29 @@ class ColorParser {
      *
      */
     static func interpolate(_ a:NSColor,_ b:NSColor) ->NSColor{
-        a.inter
+//        let ciColor:CIColor = CIColor(color: nsColor)!
+//        return RGB(ciColor.red*255,ciColor.green*255,ciColor.blue*255)
         
-                
-                const CGFloat *startComponent = CGColorGetComponents(self.CGColor);
-                const CGFloat *endComponent = CGColorGetComponents(color.CGColor);
-                
-                float startAlpha = CGColorGetAlpha(self.CGColor);
-                float endAlpha = CGColorGetAlpha(color.CGColor);
-                
-                float r = startComponent[0] + (endComponent[0] - startComponent[0]) * factor;
-                float g = startComponent[1] + (endComponent[1] - startComponent[1]) * factor;
-                float b = startComponent[2] + (endComponent[2] - startComponent[2]) * factor;
-                float a = startAlpha + (endAlpha - startAlpha) * factor;
-                
-                return [UIColor colorWithRed:r green:g blue:b alpha:a];
-        }
+        a.redComponent
+        
+        
+//
+//        a.inter
+//            - (UIColor *)colorByInterpolatingWith:(UIColor *)color factor:(CGFloat)factor {
+//                factor = MIN(MAX(t, 0.0), 1.0);
+//                
+//                const CGFloat *startComponent = CGColorGetComponents(self.CGColor);
+//                const CGFloat *endComponent = CGColorGetComponents(color.CGColor);
+//                
+//                float startAlpha = CGColorGetAlpha(self.CGColor);
+//                float endAlpha = CGColorGetAlpha(color.CGColor);
+//                
+//                float r = startComponent[0] + (endComponent[0] - startComponent[0]) * factor;
+//                float g = startComponent[1] + (endComponent[1] - startComponent[1]) * factor;
+//                float b = startComponent[2] + (endComponent[2] - startComponent[2]) * factor;
+//                float a = startAlpha + (endAlpha - startAlpha) * factor;
+//                
+//                return [UIColor colorWithRed:r green:g blue:b alpha:a];
+//        }
     }
 }
