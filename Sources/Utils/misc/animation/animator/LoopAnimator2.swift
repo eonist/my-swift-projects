5e948dc6611e53ad59df1c8ef021727d224cb1e4
Eonist <30n1st@gmail.com>
2017-07-23 14:38:34 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/LoopAnimator2.swift

--- Sources/Utils/misc/animation/animator/LoopAnimator2.swift
+++ Sources/Utils/misc/animation/animator/LoopAnimator2.swift
@@ -1,5 +1,35 @@
 import Foundation
-
-class LoopAnimator2 {
-
+/**
+ * Makes it possible to create looping animations, n-loops or infinite-loops
+ * NOTE: use stop() to stop the animation if the animation is infinite, with n-loops the animation stops when the last repeat has run
+ * PARAM: duration: in seconds
+ * PARAM: callBack: is the callback ref that is called on every "frame tick"
+ */
+class LoopAnimator2:Animator2{
+    var repeatCount:Int/*<--zero means infinite, not at the moment it seems*/
+    var curRepeatCount:Int = 0
+    init(initValues:Animator.InitValues, easing:@escaping EasingEquation = Easing.linear.ease, closure: @escaping FrameTick = {_ in}) {
+        
+    }
+    init(_ animatable:AnimProxyKind, _ repeatCount:Int = 0,_ duration:CGFloat = 0.5, _ from:CGFloat, _ to:CGFloat, _ callBack:@escaping FrameTick, _ easing:@escaping EasingEquation = Linear.ease){
+        self.repeatCount = repeatCount
+        super.init(animatable, duration, from, to, callBack, easing)
+    }
+    /**
+     * Fires on every frame tick
+     */
+//    override func onFrame(){
+//        let val:CGFloat = easing(currentFrameCount, from, to-from, framesToEnd)
+//        frameTick(val)/*call the FrameTick method*/
+//        if(currentFrameCount >= framesToEnd){
+//            self.currentFrameCount = 0/*reset*/
+//            if(curRepeatCount >= repeatCount){/*The loop ended*/
+//                curRepeatCount = 0/*reset*/
+//                stop()/*stop animation*/
+//                super.onEvent(AnimEvent(AnimEvent.completed,self))
+//            }
+//            curRepeatCount += 1
+//        }
+//        self.currentFrameCount += 1
+//    }
 }
