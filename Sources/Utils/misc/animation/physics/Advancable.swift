cd7f7857c9a8caae24dfafe8068d21ad2b470e27
Eonist <30n1st@gmail.com>
2017-07-23 17:10:21 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Advancable.swift

--- Sources/Utils/misc/animation/physics/Advancable.swift
+++ Sources/Utils/misc/animation/physics/Advancable.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class Advancable {
-
+protocol Advancable{
+    associatedtype argType
+    func add(_ value: argType) -> argType
+    func substract(_ value: argType) -> argType
+    func multiply(_ value: argType) -> argType
+    func isNear(_ value:argType,_ epsilon:argType) -> Bool
 }
