eddefd47807d233d0373c00d09a13a17bf47093b
Eonist <30n1st@gmail.com>
2017-07-23 14:03:31 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Easer2.swift

--- Sources/Utils/misc/animation/physics/Easer2.swift
+++ Sources/Utils/misc/animation/physics/Easer2.swift
@@ -1,5 +1,46 @@
 import Foundation
 
-class Easer2 {
 
+class Easer2<T>:FrameAnimator,PhysicsAnimationKind {
+    typealias argType = T
+    var easing:argType
+    var initValues:InitValues
+    var callBack:FrameTickSignature
+    init(_ callBack:@escaping FrameTickSignature,  _ initValues:InitValues, _ easing:argType) {
+        self.initValues = initValues
+        self.callBack = callBack
+        self.easing = easing
+        super.init()
+    }
+    override func onFrame(){
+        self.updatePosition()
+        self.callBack(value)
+    }
+    func updatePosition() {
+        fatalError("Must be overriden in subClass")
+    }
+}
+
+protocol Advanceable{
+    associatedtype argType
+    func add(x: argType, y: argType) -> argType
+    func substract(x: argType, y: argType) -> argType
+    func multiply(x: argType, y: argType) -> argType
+    func isNear(a:argType,b:argType,epsilon:argType) -> Bool
+}
+
+extension CGFloat:Advanceable{
+    typealias argType = CGFloat
+    func isNear(a: CGFloat, b: CGFloat, epsilon: CGFloat) -> Bool {
+        return a.isNear(b, epsilon)
+    }
+    func multiply(x: CGFloat, y: CGFloat) -> CGFloat {
+        return x * y
+    }
+    func substract(x: CGFloat, y: CGFloat) -> CGFloat {
+        return x - y
+    }
+    func add(x: CGFloat, y: CGFloat) -> CGFloat {
+        return x + y
+    }
 }
