3ee5c8af42424f54312a18d58ef66e86f0812dd1
Eonist <30n1st@gmail.com>
2017-07-23 17:42:24 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Easer.swift

--- Sources/Utils/misc/animation/physics/Easer.swift
+++ Sources/Utils/misc/animation/physics/Easer.swift
@@ -3,6 +3,7 @@ import Foundation
  * NOTE: This class was attempted with regular OOP but it was not scalable, every time you subclassed the BaseClass you would have setup all the var's again
  * NOTE: The FrameTick and the InitValues typaliases are the same in Springer and Easer so we just reuse them
  * NOTE: This is the Base class
+ * NOTE: You could add RectEaser aswell which would have 4 values to ease. After this you dont need many more subclasses as you can use NUmberEaser for color for instance. Maybe if you want to do 3d you need support for 3,6,12 values etc. These can be added later
  */
 class Easer<T>:FrameAnimator,PhysicsAnimationKind {
     typealias argType = T
