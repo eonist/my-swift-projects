07e2dae3f83b6f1e3edf9dfc327ebcfe5c03ceff
Eonist <30n1st@gmail.com>
2017-07-23 17:13:04 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/animation/physics/Advancable.swiftSources/Utils/misc/animation/physics/PhysicsAnimator.swift

--- Sources/Utils/misc/animation/physics/Advancable.swift
+++ Sources/Utils/misc/animation/physics/Advancable.swift
@@ -7,3 +7,18 @@ protocol Advancable{
     func multiply(_ value: argType) -> argType
     func isNear(_ value:argType,_ epsilon:argType) -> Bool
 }
+extension CGFloat:Advancable{
+    typealias argType = CGFloat
+    func isNear(_ value: CGFloat, _ epsilon: CGFloat) -> Bool {
+        return self.isNear(value, epsilon)
+    }
+    func multiply(_ value: CGFloat) -> CGFloat {
+        return self * value
+    }
+    func substract(_ value: CGFloat) -> CGFloat {
+        return self - value
+    }
+    func add(_ value: CGFloat) -> CGFloat {
+        return self + value
+    }
+}
--- Sources/Utils/misc/animation/physics/PhysicsAnimator.swift
+++ Sources/Utils/misc/animation/physics/PhysicsAnimator.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-class PhysicsAnimator {
-
+class PhysicsAnimator<T:Advancable>:FrameAnimator {
+    var easing:T
+    var initValues:InitValues
+    var callBack:FrameTickSignature
+    init(_ callBack:@escaping FrameTickSignature,  _ initValues:InitValues, _ easing:T) {
+        self.initValues = initValues
+        self.callBack = callBack
+        self.easing = easing
+        super.init()
+    }
+    override func onFrame(){
+        self.updatePosition(value)
+        self.callBack(value)
+    }
+    func updatePosition<T:Advancable>(_ value:T) {
+        let velocity = targetValue.substract(value).multiply(easing)
+        value = value.add(velocity)
+        if assertStop {stop()}
+    }
+    var assertStop:Bool {
+        return velocity.isNear(stopVelocity, 10e-5.cgFloat)
+    }
 }
