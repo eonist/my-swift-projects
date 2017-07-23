6b24f014bb929e1f2e0123f050e12ee98a1533d4
Eonist <30n1st@gmail.com>
2017-07-23 18:27:51 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/geom/color/cg/ColorParser.swift

--- Sources/Utils/geom/color/cg/ColorParser.swift
+++ Sources/Utils/geom/color/cg/ColorParser.swift
@@ -26,5 +26,20 @@ class ColorParser {
      */
     static func interpolate(_ a:NSColor,_ b:NSColor) ->NSColor{
         a.inter
+        
+                
+                const CGFloat *startComponent = CGColorGetComponents(self.CGColor);
+                const CGFloat *endComponent = CGColorGetComponents(color.CGColor);
+                
+                float startAlpha = CGColorGetAlpha(self.CGColor);
+                float endAlpha = CGColorGetAlpha(color.CGColor);
+                
+                float r = startComponent[0] + (endComponent[0] - startComponent[0]) * factor;
+                float g = startComponent[1] + (endComponent[1] - startComponent[1]) * factor;
+                float b = startComponent[2] + (endComponent[2] - startComponent[2]) * factor;
+                float a = startAlpha + (endAlpha - startAlpha) * factor;
+                
+                return [UIColor colorWithRed:r green:g blue:b alpha:a];
+        }
     }
 }
