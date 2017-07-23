a094c97c4d145e1e1bfd82fe50224d776be54631
Eonist <30n1st@gmail.com>
2017-07-23 14:43:31 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/animator/LoopAnimator2.swift

--- Sources/Utils/misc/animation/animator/LoopAnimator2.swift
+++ Sources/Utils/misc/animation/animator/LoopAnimator2.swift
@@ -8,28 +8,25 @@ import Foundation
 class LoopAnimator2:Animator2{
     var repeatCount:Int/*<--zero means infinite, not at the moment it seems*/
     var curRepeatCount:Int = 0
-    init(initValues:Animator.InitValues, easing:@escaping EasingEquation = Easing.linear.ease, closure: @escaping FrameTick = {_ in}) {
-        
-    }
-    init(_ animatable:AnimProxyKind, _ repeatCount:Int = 0,_ duration:CGFloat = 0.5, _ from:CGFloat, _ to:CGFloat, _ callBack:@escaping FrameTick, _ easing:@escaping EasingEquation = Linear.ease){
+    init(initValues:Animator.InitValues, _ repeatCount:Int = 0, easing:@escaping EasingEquation = Easing.linear.ease, closure: @escaping FrameTick = {_ in}) {
         self.repeatCount = repeatCount
-        super.init(animatable, duration, from, to, callBack, easing)
+        super.init(initValues: initValues, easing: easing, closure: closure)
     }
     /**
      * Fires on every frame tick
      */
-//    override func onFrame(){
-//        let val:CGFloat = easing(currentFrameCount, from, to-from, framesToEnd)
-//        frameTick(val)/*call the FrameTick method*/
-//        if(currentFrameCount >= framesToEnd){
-//            self.currentFrameCount = 0/*reset*/
-//            if(curRepeatCount >= repeatCount){/*The loop ended*/
-//                curRepeatCount = 0/*reset*/
-//                stop()/*stop animation*/
-//                super.onEvent(AnimEvent(AnimEvent.completed,self))
-//            }
-//            curRepeatCount += 1
-//        }
-//        self.currentFrameCount += 1
-//    }
+    override func onFrame(){
+        let val:CGFloat = easing(currentFrameCount, from, to-from, framesToEnd)
+        frameTick(val)/*call the FrameTick method*/
+        if(currentFrameCount >= framesToEnd){
+            self.currentFrameCount = 0/*reset*/
+            if(curRepeatCount >= repeatCount){/*The loop ended*/
+                curRepeatCount = 0/*reset*/
+                stop()/*stop animation*/
+                completed()
+            }
+            curRepeatCount += 1
+        }
+        self.currentFrameCount += 1
+    }
 }
