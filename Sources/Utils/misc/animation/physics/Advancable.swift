27c033b2947426c0cd3279cbecbc97a85a5abf43
Eonist <30n1st@gmail.com>
2017-07-23 17:23:57 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Advancable.swift

--- Sources/Utils/misc/animation/physics/Advancable.swift
+++ Sources/Utils/misc/animation/physics/Advancable.swift
@@ -9,16 +9,16 @@ protocol Advancable{
 }
 extension CGFloat:Advancable{
     typealias argType = CGFloat
-    func isNear(_ value: CGFloat, _ epsilon: CGFloat) -> Bool {
+    func isNear(_ value: argType, _ epsilon: argType) -> Bool {
         return self.isNear(value, epsilon)
     }
-    func multiply(_ value: CGFloat) -> CGFloat {
+    func multiply(_ value: argType) -> argType {
         return self * value
     }
-    func substract(_ value: CGFloat) -> CGFloat {
+    func substract(_ value: argType) -> argType {
         return self - value
     }
-    func add(_ value: CGFloat) -> CGFloat {
+    func add(_ value: argType) -> argType {
         return self + value
     }
 }
