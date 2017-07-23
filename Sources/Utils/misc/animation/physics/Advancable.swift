74c48e920a18e85f645831c0c14ce020ab947f99
Eonist <30n1st@gmail.com>
2017-07-23 17:28:49 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Advancable.swift

--- Sources/Utils/misc/animation/physics/Advancable.swift
+++ Sources/Utils/misc/animation/physics/Advancable.swift
@@ -6,8 +6,8 @@ protocol Advancable{
     func substract(_ value: argType) -> argType
     func multiply(_ value: argType) -> argType
     func isNear(_ value:argType,_ epsilon:argType) -> Bool
-    func updatePosition()
-    var assertStop:Bool {get}
+    static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType)
+    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool
 }
 extension CGFloat:Advancable{
     typealias argType = CGFloat
@@ -23,12 +23,12 @@ extension CGFloat:Advancable{
     func add(_ value: argType) -> argType {
         return self + value
     }
-    func updatePosition(velocity:argType,value:argType,easing:argType) {
-        velocity = targetValue.substract(value).multiply(easing)
-        value = value.add(velocity)
-        
+    static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType){
+        let velocity:argType = targetValue.substract(value).multiply(easing)
+        let value:argType = value.add(velocity)
+        return (value,velocity)
     }
-    var assertStop:Bool {
+    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool {
         return velocity.isNear(stopVelocity, 10e-5.cgFloat)
     }
 }
