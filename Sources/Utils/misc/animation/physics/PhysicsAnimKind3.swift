1e6459502a4729b02a4efea127230d4f0decc48f
Eonist <30n1st@gmail.com>
2017-07-24 15:31:09 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/PhysicsAnimKind3.swift

--- Sources/Utils/misc/animation/physics/PhysicsAnimKind3.swift
+++ Sources/Utils/misc/animation/physics/PhysicsAnimKind3.swift
@@ -7,19 +7,19 @@ import Foundation
  * This class exists so that Springer and Easer can have fewer vars, by instead accessing vars through getters and setters that derive their values from tuples
  */
 protocol PhysicsAnimKind3:class{//TODO: ⚠️️ Rename to PhysicsAnimKind
-    associatedtype argType
+    associatedtype T: Advancable3
     /*Signatures*/
-    typealias FrameTickSignature = (argType)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
-    typealias InitValues = (value:argType,targetValue:argType,velocity:argType,stopVelocity:argType)
-    /**/
-    var initValues:InitValues {get set}
-    /**/
-    var targetValue:argType {get set} /*Where value should go to*/
-    var velocity:argType {get set}/*Velocity*/
-    var value:argType {get set}/*The value that should be applied to the target*/
-    var stopVelocity:argType {get set}
-    /*Event related*/
-    var callBack:(argType)->Void {get set}/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
+//    typealias FrameTickSignature = (argType)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
+//    typealias InitValues = (value:argType,targetValue:argType,velocity:argType,stopVelocity:argType)
+//    /**/
+//    var initValues:InitValues {get set}
+//    /**/
+//    var targetValue:argType {get set} /*Where value should go to*/
+//    var velocity:argType {get set}/*Velocity*/
+//    var value:argType {get set}/*The value that should be applied to the target*/
+//    var stopVelocity:argType {get set}
+//    /*Event related*/
+//    var callBack:(argType)->Void {get set}/*The closure method that is called on every "frame-tick" and changes the property, you can use a var closure or a regular method, probably even an inline closure*/
 }
 /**
  * Getters and setters for the value tuples
