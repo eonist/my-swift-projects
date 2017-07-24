c6758d056db8c28160528cd38cd74d4e2803137f
Eonist <30n1st@gmail.com>
2017-07-24 19:56:17 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable4.swift

--- Sources/Utils/misc/animation/physics/4/Advancable4.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable4.swift
@@ -16,17 +16,18 @@ protocol Advancable4 {
 }
 
 extension CGFloat: Advancable4 {
+    static var initValues: (value: CGFloat, targetValue: CGFloat, velocity: CGFloat, stopVelocity: CGFloat) = (value:0,targetValue:0,velocity:0,stopVelocity:0)/*Convenient*/
+
     static let defaultEpsilon: CGFloat = 10e-5
     static let defaultConfig:CGFloat = (0.2)/*Convenient*/
-    static let initValues:(value:CGFloat,targetValue:CGFloat,velocity:CGFloat,stopVelocity:CGFloat) = (value:0,targetValue:0,velocity:0,stopVelocity:0)/*Convenient*/
+    
     
     func isNear( value:  CGFloat,  epsilon: CGFloat) -> Bool {
         return self.isNear(value,epsilon)
     }
 }
 extension CGPoint: Advancable4 {
-   
-
+    static var initValues: (value: CGPoint, targetValue: CGPoint, velocity: CGPoint, stopVelocity: CGPoint) = (value:CGPoint(0,0),targetValue:CGPoint(0,0),velocity:CGPoint(0,0),stopVelocity:CGPoint(0,0))/*Convenient*/
     static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
     static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
     //static let defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
