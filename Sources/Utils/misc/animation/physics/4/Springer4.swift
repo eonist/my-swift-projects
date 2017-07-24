2460ccc03ee4c7373470672c83b8d3e725f19157
Eonist <30n1st@gmail.com>
2017-07-24 22:04:07 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Springer4.swift

--- Sources/Utils/misc/animation/physics/4/Springer4.swift
+++ Sources/Utils/misc/animation/physics/4/Springer4.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-class Springer {
-
+class Springer4<T: Advancable4>:Easer4 {
+    typealias Config = (spring:T,friction:T)/*Signatures*/
+    var config:Config/*Config values*/
+    init(_ initValues:InitValues, _ config:Config,_ callBack:@escaping FrameTickSignature) {
+        self.config = config
+        super.init()
+    }
+    override func updatePosition() {
+        let d = (targetValue - value)
+        let a = d * config.spring
+        velocity += a
+        velocity *= config.friction
+        value +=  velocity
+        if assertStop {stop()}
+    }
+}
+/*Convenient*/
+var initPointSpringConfig:(spring:CGPoint,friction:CGPoint) {/*Convenient default init values*/
+    return (spring:CGPoint(0.02,0.02),friction:CGPoint(0.90,0.90))
+}
+var initSpringerConfig:(spring:CGFloat,friction:CGFloat) {/*Convenient default init values*/
+    return (spring:0.02,friction:0.90)
 }
