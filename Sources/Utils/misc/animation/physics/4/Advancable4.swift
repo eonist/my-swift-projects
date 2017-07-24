7a678fc0d0f3a156c27c26f0645bf90a0ef3e0a7
Eonist <30n1st@gmail.com>
2017-07-24 18:48:53 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/animation/physics/4/Advancable4.swiftSources/Utils/misc/animation/physics/4/Easer4.swift

--- Sources/Utils/misc/animation/physics/4/Advancable4.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable4.swift
@@ -4,3 +4,8 @@ protocol Advancable4 {
     static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType)
     static func assertStop(velocity:argType,stopVelocity:argType) -> Bool
 }
+extension Advancable4{
+    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool {
+        return velocity.isNear(stopVelocity, 10e-5.cgFloat)
+    }
+}
--- Sources/Utils/misc/animation/physics/4/Easer4.swift
+++ Sources/Utils/misc/animation/physics/4/Easer4.swift
@@ -15,4 +15,18 @@ class Easer4<T: Advancable4>:FrameAnimator, PhysicsAnimKind4{
         self.updatePosition(value)
         self.callBack(value)
     }
+    func updatePosition() {
+        velocity = (targetValue - value) * easing
+        value += velocity
+        if assertStop {stop()}
+
+    }
+    var assertStop:Bool {
+        return velocity.isNear(stopVelocity, 10e-5.cgFloat)
+    }
+    override func updatePosition() {
+            }
+    var assertStop:Bool {
+        return velocity.isNear(stopVelocity, 10e-5.cgFloat)
+    }
 }
