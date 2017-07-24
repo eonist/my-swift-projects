db0c2020a729b585166c5b1276d9013157723ec0
Eonist <30n1st@gmail.com>
2017-07-24 18:46:19 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Easer4.swift

--- Sources/Utils/misc/animation/physics/4/Easer4.swift
+++ Sources/Utils/misc/animation/physics/4/Easer4.swift
@@ -1,8 +1,18 @@
 import Foundation
 
-class Easer4<T: Advancable4> FrameAnimator, PhysicsAnimKind4{
-    
+class Easer4<T: Advancable4>:FrameAnimator, PhysicsAnimKind4{
     var easing:T
     var initValues:PhysicsAnimKind4.InitValues
     var callBack:FrameTickSignature
+    
+    init(_ initValues:PhysicsAnimKind4.InitValues, _ easing:T, _ callBack:@escaping FrameTickSignature) {
+        self.initValues = initValues
+        self.callBack = callBack
+        self.easing = easing
+        super.init()
+    }
+    override func onFrame(){
+        self.updatePosition(value)
+        self.callBack(value)
+    }
 }
