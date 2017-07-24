56ea6278220ca22f60864bf2ddf31fc5544620a2
Eonist <30n1st@gmail.com>
2017-07-24 22:26:50 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable4.swift

--- Sources/Utils/misc/animation/physics/4/Advancable4.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable4.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 protocol Advancable4 {
-    
-    
     static func +(lhs: Self, rhs: Self) -> Self
     static func -(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
@@ -16,12 +14,8 @@ protocol Advancable4 {
 }
 
 extension CGFloat: Advancable4 {
-    
-
     static let defaultEpsilon: CGFloat = 10e-5
     static let defaultConfig:CGFloat = (0.2)/*Convenient*/
-    
-    
     func isNear( value:  CGFloat,  epsilon: CGFloat) -> Bool {
         return self.isNear(value,epsilon)
     }
@@ -29,7 +23,6 @@ extension CGFloat: Advancable4 {
 extension CGPoint: Advancable4 {
     static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
     static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
-    //static let defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
     func isNear( value:  CGPoint,  epsilon: CGPoint) -> Bool {
         return self.isNear(value,epsilon.x)
     }
