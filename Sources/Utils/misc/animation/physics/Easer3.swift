e3b648e7b9c97172287d74bc3e5f48aa8f518a52
Eonist <30n1st@gmail.com>
2017-07-24 15:47:07 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/Easer3.swift

--- Sources/Utils/misc/animation/physics/Easer3.swift
+++ Sources/Utils/misc/animation/physics/Easer3.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-class Easer3<T: Advancable>: FrameAnimator, PhysicsAnimation {
-    
+class Easer3<T: Advancable3>: FrameAnimator, PhysicsAnimKind3 {
+    let initial: AnimationState<T>
+    let target: AnimationState<T>
+    var current: AnimationState<T>
 
+    var damping: T
+    var epsilon: T = T.defaultEpsilon
 }
