6d643f4f850e19a4e12e0901cfe6aea76d5a10f8
Eonist <30n1st@gmail.com>
2017-07-24 18:29:41 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable4.swift

--- Sources/Utils/misc/animation/physics/4/Advancable4.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable4.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-class Advancable {
-
+protocol Advancable4 {
+    static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType)
+    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool
 }
