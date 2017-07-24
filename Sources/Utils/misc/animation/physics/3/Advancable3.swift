0f6ff44209848caf56cf5e6e3711a2066a4970cd
Eonist <30n1st@gmail.com>
2017-07-24 15:31:09 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/3/Advancable3.swift

--- Sources/Utils/misc/animation/physics/3/Advancable3.swift
+++ Sources/Utils/misc/animation/physics/3/Advancable3.swift
@@ -1,5 +1,45 @@
 import Foundation
 
-class Advancable3 {
-
+protocol Advancable3{
+    static func +(lhs: Self, rhs: Self) -> Self
+    static func -(lhs: Self, rhs: Self) -> Self
+    static func *(lhs: Self, rhs: Self) -> Self
+    
+    func isNear(_ value: Self, within epsilon: Self) -> Bool
+//    
+//    static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType)
+//    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool
 }
+
+extension CGFloat: Advancable3 {
+    static let defaultEpsilon: CGFloat = 10e-5
+    
+    func isNear(_ value:  CGFloat, within epsilon: CGFloat) -> Bool {
+        return abs(self - value) < epsilon//TODO: 
+    }
+}
+
+//extension CGFloat:Advancable3{
+//    typealias argType = CGFloat
+//    func isNear(_ value: argType, _ epsilon: argType) -> Bool {
+//        return self.isNear(value, epsilon)
+//    }
+//    func multiply(_ value: argType) -> argType {
+//        return self * value
+//    }
+//    func substract(_ value: argType) -> argType {
+//        return self - value
+//    }
+//    func add(_ value: argType) -> argType {
+//        return self + value
+//    }
+//    static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType){
+//        let velocity:argType = targetValue.substract(value).multiply(easing)
+//        let value:argType = value.add(velocity)
+//        return (value,velocity)
+//    }
+//    static func assertStop(velocity:argType,stopVelocity:argType) -> Bool {
+//        return velocity.isNear(stopVelocity, 10e-5.cgFloat)
+//    }
+//}
+
