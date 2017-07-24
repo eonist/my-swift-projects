5542b010f67e91a2ee040b3458f5639084257f03
Eonist <30n1st@gmail.com>
2017-07-24 18:38:04 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/animation/physics/4/Easer4.swiftSources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift

--- Sources/Utils/misc/animation/physics/4/Easer4.swift
+++ Sources/Utils/misc/animation/physics/4/Easer4.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 class Easer4<T: Advancable3> FrameAnimator, PhysicsAnimKind4{
+    
     var easing:T
     var initValues:InitValues
     var callBack:FrameTickSignature
--- Sources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift
+++ Sources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class PhysicsAnimKind4 {
-
+protocol PhysicsAnimKind4:class {
+    associatedtype T: Advancable3
+    typealias FrameTickSignature = (T)->Void/*generic call back signature, use Spring.FrameTick outside this class*/
+    typealias InitValues = (value:T,targetValue:T,velocity:T,stopVelocity:T)
 }
