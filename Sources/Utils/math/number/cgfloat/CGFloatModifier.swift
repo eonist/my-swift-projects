71733468b43517ce0e698131dec5ebe3ddd35a70
Eonist <30n1st@gmail.com>
2017-07-26 00:08:51 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/math/number/cgfloat/CGFloatModifier.swift

--- Sources/Utils/math/number/cgfloat/CGFloatModifier.swift
+++ Sources/Utils/math/number/cgfloat/CGFloatModifier.swift
@@ -22,6 +22,7 @@ class CGFloatModifier {
     /**
      * NOTE: This is an alternative: let duration = String(format: "%.01f", 3.32323242)
      * NOTE: a problem with CGFloat is that all CGFloat values have a trailing decimalpoint. (Even if you return 10, then end value will be 10.0) a way around this is to convert to a string, then your probably better of using the method mentioned above
+     * PARAM: places: decimalPlaces
      */
     static func toFixed(_ value:CGFloat, _ places:Int) -> CGFloat {
         let divisor = pow(10, CGFloat(places))
