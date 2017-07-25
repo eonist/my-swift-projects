4881d34c39ac5f28cd514c4efeccbfa06eb6343c
Eonist <30n1st@gmail.com>
2017-07-25 14:47:25 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/view/InteractiveViable.swift

--- Sources/Utils/misc/view/InteractiveViable.swift
+++ Sources/Utils/misc/view/InteractiveViable.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class InteractiveViable {
-
+typealias IInteractiveView = InteractiveViewable
+protocol InteractiveViewable:EventSendable {
+    //func onEvent(event:Event)
+    func mouseMoved(_ event:MouseEvent)
+    func mouseOver(_ event:MouseEvent)
+    func mouseOut(_ event:MouseEvent)
+    func mouseDown(_ event:MouseEvent)
+    func mouseUpInside(_ event:MouseEvent)
+    func mouseUpOutside(_ event:MouseEvent)
+    func mouseUp(_ event:MouseEvent)/*Convenient*/
 }
