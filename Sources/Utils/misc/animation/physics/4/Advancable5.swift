b5a2cd12d16e214120f66603be4d17e3c0aa4a34
Eonist <30n1st@gmail.com>
2017-07-26 18:33:08 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable5.swift

--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -5,29 +5,25 @@ protocol Advancable5 {
     static func -(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
     static var defaultEpsilon: Self { get }
-    //static var defaultConfig: Self { get }
     func isNear( value: Self, epsilon: Self) -> Bool
     
 }
 
 extension CGFloat: Advancable5 {
     static let defaultEpsilon: CGFloat = 10e-5
-    //static let defaultConfig:CGFloat = (0.2)/*Convenient*/
     func isNear( value:  CGFloat,  epsilon: CGFloat) -> Bool {
         return self.isNear(value,epsilon)
     }
 }
 extension CGPoint: Advancable5 {
-    //static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
     static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
     func isNear( value:  CGPoint,  epsilon: CGPoint) -> Bool {
         return self.isNear(value,epsilon.x)
     }
 }
 extension CGSize: Advancable5 {
-    //static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
     static var defaultEpsilon: CGSize = CGSize(10e-5,10e-5)
     func isNear( value:  CGSize,  epsilon: CGSize) -> Bool {
-        return self.isNear(value,epsilon.x)
+        return self.w.isNear(value.w,epsilon.w) && self.h.isNear(value.h,epsilon.h)
     }
 }
