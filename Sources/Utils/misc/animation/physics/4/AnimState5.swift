50bff7ce9d81f0abb5ebbeb700fd0f841113f682
Eonist <30n1st@gmail.com>
2017-07-26 17:55:03 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/AnimState5.swift

--- Sources/Utils/misc/animation/physics/4/AnimState5.swift
+++ Sources/Utils/misc/animation/physics/4/AnimState5.swift
@@ -1,17 +1,17 @@
 import Foundation
 
-struct AnimationState5<T> {
+struct AnimaState5<T> {
     let value: T
     let targetValue: T
     let velocity: T
     let stopVelocity: T
 }
 
-extension AnimationState5 {
-    static func initPointValues() ->  AnimationState5<CGPoint>  {
+extension AnimaState5 {
+    static func initPointValues() ->  Self<CGPoint>  {
         return .init(value: CGPoint(0,0), targetValue: CGPoint(0,0), velocity: CGPoint(0,0), stopVelocity: CGPoint(0,0))
     }
-    static func initValues() ->  AnimationState5<CGFloat>  {
+    static func initValues() ->  Self<CGFloat>  {
         return .init(value: 0.0, targetValue: 0.0, velocity: 0.0, stopVelocity: 0.0)
     }
 }
