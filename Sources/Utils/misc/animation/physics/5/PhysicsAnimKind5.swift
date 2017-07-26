1719988fd1ffdc98091f42ebd1bdc09f9386ac6b
Eonist <30n1st@gmail.com>
2017-07-26 18:23:15 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/PhysicsAnimKind5.swift

--- Sources/Utils/misc/animation/physics/5/PhysicsAnimKind5.swift
+++ Sources/Utils/misc/animation/physics/5/PhysicsAnimKind5.swift
@@ -3,8 +3,8 @@ import Foundation
 /**
  * TODO: ⚠️️ Try to get rid of the :class by making the var's mutable somehow
  * This class exists so that Springer and Easer can have fewer vars, by instead accessing vars through getters and setters that derive their values from tuples
- * TODO: Rename initValues to config, as initvalues could indicate the start values, which it is not
- * TODO: Consider using other names from regex proposal
+ * TODO: ⚠️️ Rename initValues to config, as initvalues could indicate the start values, which it is not
+ * TODO: ⚠️️ Consider using other names from regex proposal
  */
 protocol PhysicsAnimKind5:class {
     associatedtype T: Advancable5
