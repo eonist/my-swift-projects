12299b866c2068c7ef348fda02494dac626afd14
Eonist <30n1st@gmail.com>
2017-07-26 22:46:15 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/AnimState5.swift

--- Sources/Utils/misc/animation/physics/4/AnimState5.swift
+++ Sources/Utils/misc/animation/physics/4/AnimState5.swift
@@ -12,20 +12,20 @@ struct AnimState5<T> {
         self.stopVelocity = stopVelocity
     }
 }
-
 extension AnimState5 {
+    //default states
     static var initValues:AnimState5<CGFloat>  {
         return AnimState5<CGFloat>.init(0, 0, 0, 0)
     }
     static var initPointValues:AnimState5<CGPoint>  {
         return AnimState5<CGPoint>(CGPoint(0,0), CGPoint(0,0), CGPoint(0,0), CGPoint(0,0))
     }
-    
-    
-    static var initConfig:CGFloat { return (0.2) }
-    static var initPointConfig:CGPoint { return CGPoint(0.2,0.2) }
-    static var initRectConfig:CGRect { return CGRect(0.2,0.2,0.2,0.2) }
     static var initRectValues:AnimState5<CGRect>  {
         return AnimState5<CGRect>(CGRect(), CGRect(), CGRect(), CGRect())
     }
+    //default configs
+    static var initConfig:CGFloat { return (0.2) }
+    static var initPointConfig:CGPoint { return CGPoint(0.2,0.2) }
+    static var initRectConfig:CGRect { return CGRect(0.2,0.2,0.2,0.2) }
+    
 }
