03cb87d228fb67ccb21b0d47d4ead8fbff77ec93
Eonist <30n1st@gmail.com>
2017-07-26 21:16:01 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/AnimState5.swift

--- Sources/Utils/misc/animation/physics/4/AnimState5.swift
+++ Sources/Utils/misc/animation/physics/4/AnimState5.swift
@@ -5,21 +5,21 @@ struct AnimState5<T> {
     var targetValue: T
     var velocity: T
     var stopVelocity: T
-//    init(value: T,targetValue: T,velocity: T,stopVelocity:T){
-//        self.value = value
-//        self.targetValue = targetValue
-//        self.velocity = velocity
-//        self.stopVelocity = stopVelocity
-//    }
+    init(_ value: T,_ targetValue: T,_ velocity: T,_ stopVelocity:T){
+        self.value = value
+        self.targetValue = targetValue
+        self.velocity = velocity
+        self.stopVelocity = stopVelocity
+    }
 }
 
-//extension AnimState5 {
-//    static func initPointValues() ->  AnimState5<CGPoint>  {
-//        return AnimState5<CGPoint>(value: CGPoint(0,0), targetValue: CGPoint(0,0), velocity: CGPoint(0,0), stopVelocity: CGPoint(0,0))
-//    }
-//    static func initValues() ->  AnimState5<CGFloat>  {
-//        return AnimState5<CGPoint>.init(value: 0, targetValue: 0, velocity: 0, stopVelocity: 0)
-//    }
-//    static var initConfig:CGFloat { return (0.2) }
-//    static var initPointConfig:CGPoint { return CGPoint(0.2,0.2) }
-//}
+extension AnimState5 {
+    static func initPointValues() ->  AnimState5<CGPoint>  {
+        return AnimState5<CGPoint>(CGPoint(0,0), CGPoint(0,0), CGPoint(0,0), CGPoint(0,0))
+    }
+    static func initValues() ->  AnimState5<CGFloat>  {
+        return AnimState5<CGPoint>.init(0, 0, 0, 0)
+    }
+    static var initConfig:CGFloat { return (0.2) }
+    static var initPointConfig:CGPoint { return CGPoint(0.2,0.2) }
+}
