b88881d986b8299e616c46b52c48d5877fb1d5a4
Eonist <30n1st@gmail.com>
2017-07-26 17:41:06 +0200

    New files added: 1

--- Sources/Utils/misc/animation/physics/4/AnimationState4.swift
+++ Sources/Utils/misc/animation/physics/4/AnimationState4.swift
@@ -6,6 +6,9 @@ struct AnimationConfig4<T> {
     let velocity: T
     let stopVelocity: T
 }
+extension AnimationConfig4{
+    
+}
 //(value:T,targetValue:T,velocity:T,stopVelocity:T)
 
 //var initPointValues: Easer4<CGPoint>.InitValues = (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))//
