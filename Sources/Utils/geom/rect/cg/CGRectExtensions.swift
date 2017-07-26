366f1b589eb94284c2871fce3edd13daf49bd72b
Eonist <30n1st@gmail.com>
2017-07-26 18:40:07 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/geom/rect/cg/CGRectExtensions.swiftSources/Utils/misc/animation/physics/4/Advancable5.swift

--- Sources/Utils/geom/rect/cg/CGRectExtensions.swift
+++ Sources/Utils/geom/rect/cg/CGRectExtensions.swift
@@ -112,3 +112,9 @@ extension CGRect {
 public func +(a: CGRect, b: CGPoint) -> CGRect { return CGRect(a.x+b.x, a.y+b.y,a.width,a.height)}//Adds the coordinates of point p to the coordinates of this point to create a new point
 public func +=(a: inout CGRect, b: CGPoint) {a.x += b.x;a.y += b.y;}//modifies a by adding b, could also have used: offsetBy()
 public func -=(a: inout CGRect, b: CGPoint) {a.x -= b.x;a.y -= b.y;}//modifies a by adding b, could also have used: offsetBy()
+
+public func +(a: CGRect, b: CGRect) -> CGRect { return CGRect(a.origin + b.origin,a.size + b.size)}
+public func -(a: CGRect, b: CGRect) -> CGRect { return CGRect(a.origin - b.origin,a.size - b.size)}
+public func *(a: CGRect, b: CGRect) -> CGRect { return CGRect(a.origin * b.origin,a.size * b.size)}
+//public func -(a: CGSize, b: CGSize) -> CGSize { return CGSize(a.width - b.width,a.height - b.height)}
+//public func * (a: CGSize, b: CGSize) -> CGSize {return CGSize(a.w*b.w, a.h*b.h)}
--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -24,16 +24,12 @@ extension CGPoint: Advancable5 {
 extension CGSize: Advancable5 {
     static var defaultEpsilon: CGSize = CGSize(10e-5,10e-5)
     func isNear( value:  CGSize,  epsilon: CGSize) -> Bool {
-        return self.w.isNear(value.w,epsilon.w) && self.h.isNear(value.h,epsilon.h)
+        return self.isNear(value,epsilon.w)
     }
 }
 extension CGRect: Advancable5 {
-    static func +(lhs: CGRect, rhs: CGRect) -> CGRect {
-        <#code#>
-    }
-
     static var defaultEpsilon: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
     func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
-        return self.w.isNear(value.w,epsilon.w) && self.h.isNear(value.h,epsilon.h)
+        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
     }
 }
