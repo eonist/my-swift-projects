823a610b3e8b10f2ba7495e147e805efe04e3111
Eonist <30n1st@gmail.com>
2017-07-26 17:43:49 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/misc/animation/physics/4/Advancable5.swiftSources/Utils/misc/animation/physics/4/AnimationState5.swift

--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -1,26 +1,23 @@
 import Foundation
 
-protocol Advancable4 {
+protocol Advancable5 {
     static func +(lhs: Self, rhs: Self) -> Self
     static func -(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
-    
     static var defaultEpsilon: Self { get }
     static var defaultConfig: Self { get }
-    //static var initValues:(value:Self,targetValue:Self,velocity:Self,stopVelocity:Self) {get}
     func isNear( value: Self, epsilon: Self) -> Bool
-    //static func updatePosition(velocity:argType,value:argType,targetValue:argType,easing:argType) -> (value:argType,velocity:argType)
-    //static func assertStop(velocity:argType,stopVelocity:argType) -> Bool
+    
 }
 
-extension CGFloat: Advancable4 {
+extension CGFloat: Advancable5 {
     static let defaultEpsilon: CGFloat = 10e-5
     static let defaultConfig:CGFloat = (0.2)/*Convenient*/
     func isNear( value:  CGFloat,  epsilon: CGFloat) -> Bool {
         return self.isNear(value,epsilon)
     }
 }
-extension CGPoint: Advancable4 {
+extension CGPoint: Advancable5 {
     static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
     static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
     func isNear( value:  CGPoint,  epsilon: CGPoint) -> Bool {
--- Sources/Utils/misc/animation/physics/4/AnimationState5.swift
+++ Sources/Utils/misc/animation/physics/4/AnimationState5.swift
@@ -1,17 +1,17 @@
 import Foundation
 
-struct AnimationState4<T> {
+struct AnimationState5<T> {
     let value: T
     let targetValue: T
     let velocity: T
     let stopVelocity: T
 }
 
-extension AnimationState4 {
-    static func initPointValues() ->  AnimationState4<CGPoint>  {
-        return AnimationState4<CGPoint>.init(value: CGPoint(0,0), targetValue: CGPoint(0,0), velocity: CGPoint(0,0), stopVelocity: CGPoint(0,0))
+extension AnimationState5 {
+    static func initPointValues() ->  AnimationState5<CGPoint>  {
+        return .init(value: CGPoint(0,0), targetValue: CGPoint(0,0), velocity: CGPoint(0,0), stopVelocity: CGPoint(0,0))
     }
-    static func initValues() ->  AnimationState4<CGFloat>  {
-        return AnimationState4<CGFloat>.init(value: 0.0, targetValue: 0.0, velocity: 0.0, stopVelocity: 0.0)
+    static func initValues() ->  AnimationState5<CGFloat>  {
+        return .init(value: 0.0, targetValue: 0.0, velocity: 0.0, stopVelocity: 0.0)
     }
 }
