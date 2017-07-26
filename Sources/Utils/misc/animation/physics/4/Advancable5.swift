1e2902adb635dd92323771c44c9a27439f56dde3
Eonist <30n1st@gmail.com>
2017-07-26 18:30:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable5.swift

--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -5,7 +5,7 @@ protocol Advancable5 {
     static func -(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
     static var defaultEpsilon: Self { get }
-    static var defaultConfig: Self { get }
+    //static var defaultConfig: Self { get }
     func isNear( value: Self, epsilon: Self) -> Bool
     
 }
@@ -18,9 +18,16 @@ extension CGFloat: Advancable5 {
     }
 }
 extension CGPoint: Advancable5 {
-    static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
-    //static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
+    //static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
+    static var defaultEpsilon: CGPoint = CGPoint(10e-5,10e-5)
     func isNear( value:  CGPoint,  epsilon: CGPoint) -> Bool {
+        return self.isNear(value,epsilon.x)
+    }
+}
+extension CGSize: Advancable5 {
+    //static var defaultConfig: CGPoint = CGPoint(0.2,0.2)
+    static var defaultEpsilon: CGSize = CGSize(10e-5,10e-5)
+    func isNear( value:  CGSize,  epsilon: CGSize) -> Bool {
         return self.isNear(value,epsilon.x)
     }
 }
