9543851f2cf949bfded35460e979913227e4de0c
Eonist <30n1st@gmail.com>
2017-07-27 01:13:01 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable5.swift

--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -14,21 +14,22 @@ protocol Advancable5 {
 }
 
 extension CGFloat: Advancable5 {
-    static let defaultEpsilon: CGFloat = 10e-5
-    static let defaultStopVelocity: CGFloat = 0
-    static let defaultValue: CGFloat = 0
-    static let defaultVelocity: CGFloat = 0
-    static let defaultTargetValue: CGFloat = 0
+    static var defaults: AnimState5<CGFloat>  = AnimState5<CGFloat>()
+//    static let defaultEpsilon: CGFloat = 10e-5
+//    static let defaultStopVelocity: CGFloat = 0
+//    static let defaultValue: CGFloat = 0
+//    static let defaultVelocity: CGFloat = 0
+//    static let defaultTargetValue: CGFloat = 0
     func isNear( value:  CGFloat,  epsilon: CGFloat) -> Bool {
         return self.isNear(value,epsilon)
     }
 }
 extension CGPoint: Advancable5 {
-    static var defaultEpsilon: CGPoint = CGPoint(CGFloat.defaultEpsilon,CGFloat.defaultEpsilon)
-    static let defaultStopVelocity: CGPoint = CGPoint(0,0)
-    static let defaultValue: CGPoint = CGPoint(0,0)
-    static let defaultVelocity: CGPoint = CGPoint(0,0)
-    static let defaultTargetValue: CGPoint = CGPoint(0,0)
+//    static var defaultEpsilon: CGPoint = CGPoint(CGFloat.defaultEpsilon,CGFloat.defaultEpsilon)
+//    static let defaultStopVelocity: CGPoint = CGPoint(0,0)
+//    static let defaultValue: CGPoint = CGPoint(0,0)
+//    static let defaultVelocity: CGPoint = CGPoint(0,0)
+//    static let defaultTargetValue: CGPoint = CGPoint(0,0)
     func isNear( value:  CGPoint,  epsilon: CGPoint) -> Bool {
         return self.isNear(value,epsilon.x)
     }
@@ -36,24 +37,24 @@ extension CGPoint: Advancable5 {
 extension CGSize: Advancable5 {
     
     
-    static var defaultEpsilon: CGSize = CGSize(CGFloat.defaultEpsilon,CGFloat.defaultEpsilon)
-    static let defaultStopVelocity: CGSize = CGSize(0,0)
-    static let defaultValue: CGSize = CGSize(0,0)
-    static let defaultVelocity: CGSize = CGSize(0,0)
-    static let defaultTargetValue: CGSize = CGSize(0,0)
+//    static var defaultEpsilon: CGSize = CGSize(CGFloat.defaultEpsilon,CGFloat.defaultEpsilon)
+//    static let defaultStopVelocity: CGSize = CGSize(0,0)
+//    static let defaultValue: CGSize = CGSize(0,0)
+//    static let defaultVelocity: CGSize = CGSize(0,0)
+//    static let defaultTargetValue: CGSize = CGSize(0,0)
     func isNear( value:  CGSize,  epsilon: CGSize) -> Bool {
         return self.isNear(value,epsilon.w)
     }
 }
 extension CGRect: Advancable5 {
-    static var defaultState:AnimState5<CGRect>  =  AnimState5<CGRect>(CGRect(), CGRect(), CGRect(), CGRect(), CGRect())
-    
-    static var defaultEasing:CGRect = CGRect(0.2,0.2,0.2,0.2)
-    static let defaultStopVelocity: CGRect = CGRect(0,0,0,0)
-    static var defaultEpsilon: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
-    static let defaultValue: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
-    static let defaultVelocity: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
-    static let defaultTargetValue: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
+    static var defaults:AnimState5<CGRect>  =  AnimState5<CGRect>(CGRect(), CGRect(), CGRect(), CGRect(), CGRect(10e-5,10e-5,10e-5,10e-5))
+//    
+//    
+//    static let defaultStopVelocity: CGRect = CGRect(0,0,0,0)
+//    static var defaultEpsilon: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
+//    static let defaultValue: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
+//    static let defaultVelocity: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
+//    static let defaultTargetValue: CGRect = CGRect(CGPoint.defaultEpsilon,CGSize.defaultEpsilon)
     func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
         return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
     }
