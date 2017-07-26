225ad9096398e3479ce328d0aabe45dbbd45254c
Eonist <30n1st@gmail.com>
2017-07-26 17:41:07 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/AnimationState4.swift

--- Sources/Utils/misc/animation/physics/4/AnimationState4.swift
+++ Sources/Utils/misc/animation/physics/4/AnimationState4.swift
@@ -1,14 +1,17 @@
 import Foundation
 
-struct AnimationConfig4<T> {
+struct AnimationState4<T> {
     let value: T
     let targetValue: T
     let velocity: T
     let stopVelocity: T
 }
-extension AnimationConfig4{
-    
-}
-//(value:T,targetValue:T,velocity:T,stopVelocity:T)
 
-//var initPointValues: Easer4<CGPoint>.InitValues = (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0))//
+extension AnimationState4 {
+    static func initPointValues() ->  AnimationState4<CGPoint>  {
+        return AnimationState4<CGPoint>.init(value: CGPoint(0,0), targetValue: CGPoint(0,0), velocity: CGPoint(0,0), stopVelocity: CGPoint(0,0))
+    }
+    static func initValues() ->  AnimationState4<CGFloat>  {
+        return AnimationState4<CGFloat>.init(value: 0.0, targetValue: 0.0, velocity: 0.0, stopVelocity: 0.0)
+    }
+}
