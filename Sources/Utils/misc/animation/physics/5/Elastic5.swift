1f2275f369aa5f306e9c5a2ec89fe890a6aff970
Eonist <30n1st@gmail.com>
2017-07-27 22:45:42 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Elastic5.swift

--- Sources/Utils/misc/animation/physics/5/Elastic5.swift
+++ Sources/Utils/misc/animation/physics/5/Elastic5.swift
@@ -2,9 +2,9 @@ import Foundation
 /**
  * NOTE: Another name for this could be: RubberBand or ConstrainedEaser
  */
-class Elastic5<CGRect>:Easer5<CGRect> {
+class Elastic5<T:Advancable5>:Easer5<T> {
     var direct:Bool = false
-    typealias Frame = (min:T,len:T)//TODO:rename to  boundries
+    typealias Frame = (min:CGFloat,len:CGFloat)//TODO:rename to  boundries
     var maskFrame:Frame/*Represents the visible part of the content*/
     var contentFrame:Frame/*Represents the total size of the content*/
     init(_ state:AnimState5<T>, _ easing:T, _ maskFrame:Frame, _ contentFrame:Frame, _ onFrame:@escaping FrameTickSignature) {
