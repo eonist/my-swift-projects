3764209297fd576117f5aadd067b439ee49c4cdd
Eonist <30n1st@gmail.com>
2017-07-27 12:30:50 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Easer5.swift

--- Sources/Utils/misc/animation/physics/5/Easer5.swift
+++ Sources/Utils/misc/animation/physics/5/Easer5.swift
@@ -9,7 +9,7 @@ class Easer5<T: Advancable5>:FrameAnimator2, PhysicsAnimKind5{
     var easing:T/*This can be customized by setting the value but not via init*/
     var state:AnimState5<T>
     var onFrame:FrameTickSignature//TODO: ⚠️️ rename to onFrameTick,onFrameCallback?
-    var onComplete:()->Void = {}
+    var onComplete:()->Void = {}//add external onComplete closures when needed
     
     init(_ state:AnimState5<T>, _ easing:T, _ onFrame:@escaping FrameTickSignature) {
         self.state = state
@@ -27,6 +27,7 @@ class Easer5<T: Advancable5>:FrameAnimator2, PhysicsAnimKind5{
         if assertStop {
             state.value = state.targetValue//set the final value
             stop()//stop the animation
+            onComplete()
         }
     }
     var assertStop:Bool {
