c637f6ed672e80d69283734ab749bbb1e6b9c3a2
Eonist <30n1st@gmail.com>
2017-07-27 23:17:53 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Elastic5.swift

--- Sources/Utils/misc/animation/physics/5/Elastic5.swift
+++ Sources/Utils/misc/animation/physics/5/Elastic5.swift
@@ -53,8 +53,9 @@ class Elastic5:Easer5<CGRect> {
         //Swift.print("applyBottomBoundary")
         if(direct){/*surface is slipping the further you pull*/
             let totLen = (contentFrame.len - maskFrame.len)/*tot length of items - length of mask*/
-            let normalizedValue:CGFloat = totLen + value/*goes from 0 to -100*/
-            result = -totLen + CustomFriction.constraintValueWithLog(normalizedValue,-limit)//<--Creates the illusion that the surface under the thumb is slipping
+            let normalizedValue:CGFloat = totLen + value.y/*goes from 0 to -100*/
+            result = value
+            result.y = -totLen + CustomFriction.constraintValueWithLog(normalizedValue,-limit)//<--Creates the illusion that the surface under the thumb is slipping
         }/*else{/*Springs back to limit*/
             let dist = maskFrame.len - (value + contentFrame.len)/*distanceToGoal*/
             velocity += (dist * spring)
