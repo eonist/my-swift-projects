ed007d907e01b194ea6c9fd521ba0254d2fc91d8
Eonist <30n1st@gmail.com>
2017-07-27 22:50:38 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Elastic5.swift

--- Sources/Utils/misc/animation/physics/5/Elastic5.swift
+++ Sources/Utils/misc/animation/physics/5/Elastic5.swift
@@ -2,30 +2,43 @@ import Foundation
 /**
  * NOTE: Another name for this could be: RubberBand or ConstrainedEaser
  */
-class Elastic5<T:Advancable5>:Easer5<T> {
+class Elastic5:Easer5<CGRect> {
     var direct:Bool = false
     typealias Frame = (min:CGFloat,len:CGFloat)//TODO:rename to  boundries
     var maskFrame:Frame/*Represents the visible part of the content*/
     var contentFrame:Frame/*Represents the total size of the content*/
     init(_ state:AnimState5<T>, _ easing:T, _ maskFrame:Frame, _ contentFrame:Frame, _ onFrame:@escaping FrameTickSignature) {
-        self.state = state
-        self.onFrame = onFrame
-        self.easing = easing
         self.maskFrame = maskFrame
         self.contentFrame = contentFrame
         super.init(state,easing,onFrame)
     }
-    override func updatePosition() where T == CGRect {
-//        if direct {
-//            if value > maskFrame.min {
-//                applyTopBoundary()
-//            }
-//            else if((value + contentFrame.len) < maskFrame.len){
-//                applyBottomBoundary()
-//            }
-//        }else{
-//            super.updatePosition()
-//        }
-        
+    override func updatePosition() {
+        if direct {
+            if value.y > maskFrame.min {
+                //applyTopBoundary()
+            }
+            else if((value.y + contentFrame.len) < maskFrame.len){
+                //applyBottomBoundary()
+            }
+        }else{
+            super.updatePosition()
+        }
+    }
+    /**
+     * When the min val reaches beyond max
+     * PARAM: direct: toggles the directManipulation mode
+     */
+    func applyTopBoundary(_ direct:Bool){/*Surface is slipping the further you pull*/
+        //Swift.print("applyTopBoundary")
+        let distToGoal:CGFloat = value - maskFrame.min
+        if(direct){/*surface is slipping the further you pull*/
+            result = maskFrame.min + CustomFriction.constraintValueWithLog(distToGoal,limit - maskFrame.min /*topMargin*/)//<--Creates the illusion that the surface under the thumb is slipping
+        }else{/*Springs back to limit*/
+            velocity -= (distToGoal * spring)
+            velocity *= springEasing//TODO: try to apply log10 instead of the regular easing
+            value += velocity
+            if(value.isNear(maskFrame.min, 1)){checkForStop(direct)}
+            result = value
+        }
     }
 }
