b1f81cf862f0a6c84f8eb8f35d0770e6fa0c7025
Eonist <30n1st@gmail.com>
2017-07-27 21:44:34 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/Advancable5.swift

--- Sources/Utils/misc/animation/physics/4/Advancable5.swift
+++ Sources/Utils/misc/animation/physics/4/Advancable5.swift
@@ -4,6 +4,7 @@ protocol Advancable5 {
     static func +(lhs: Self, rhs: Self) -> Self
     static func -(lhs: Self, rhs: Self) -> Self
     static func *(lhs: Self, rhs: Self) -> Self
+    
     static var defaults:AnimState5<Self> {get}
     func isNear( value: Self, epsilon: Self) -> Bool
 }
