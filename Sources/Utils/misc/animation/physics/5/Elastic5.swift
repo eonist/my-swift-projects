da21ba81c8bc88d2c0d9fec9279d00a2fbe53017
Eonist <30n1st@gmail.com>
2017-07-27 21:26:00 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Elastic5.swift

--- Sources/Utils/misc/animation/physics/5/Elastic5.swift
+++ Sources/Utils/misc/animation/physics/5/Elastic5.swift
@@ -1,5 +1,18 @@
 import Foundation
-
-class Elastic5 {
-
+/**
+ * NOTE: Another name for this could be: RubberBand or ConstrainedEaser
+ */
+class Elastic5<T:Advancable5>:Easer5<T> {
+    var direct:Bool = false
+    typealias Frame = (min:T,len:T)
+    var maskFrame:Frame/*Represents the visible part of the content*/
+    override func updatePosition() {
+        if direct {
+            if value > maskFrame.min {applyTopBoundary()}
+            else if((value + contentFrame.len) < maskFrame.len){applyBottomBoundary()}
+        }else{
+            super.updatePosition()
+        }
+        
+    }
 }
