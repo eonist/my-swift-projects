02af6049827122210032cdeac3003a66414c0aa5
Eonist <30n1st@gmail.com>
2017-07-27 15:45:21 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/view/utils/mouse/MouseUtils.swift

--- Sources/Utils/misc/view/utils/mouse/MouseUtils.swift
+++ Sources/Utils/misc/view/utils/mouse/MouseUtils.swift
@@ -3,6 +3,7 @@ import Cocoa
 class MouseUtils {
     /**
      * Returns a Point for the mouse position in the coordinate space of the PARAM: view
+     * NOTE: use window.content.localPos() to get position in a window
      */
     static func point(_ view:NSView) -> CGPoint {
         return view.localPos()
