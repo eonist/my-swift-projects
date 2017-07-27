3f100a3103b1fb6b3a2720684902b1a8f2874b45
Eonist <30n1st@gmail.com>
2017-07-27 15:52:57 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/event/nsevent/EventHandler.swift

--- Sources/Utils/misc/event/nsevent/EventHandler.swift
+++ Sources/Utils/misc/event/nsevent/EventHandler.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class EventHandler {
-
+    func addEventHandler(){
+        
+    }
+    func removeEventHandler
+    if(leftMouseDraggedEventListener == nil) {
+        leftMouseDraggedEventListener = NSEvent.addLocalMonitorForEvents(matching: [.leftMouseDragged], handler: onModalDrag)
+    }
+    if(leftMouseDraggedEventListener != nil){
+        NSEvent.removeMonitor(leftMouseDraggedEventListener!)
+        leftMouseDraggedEventListener = nil//<--this part may not be needed
+    }/*We remove a global mouse move event listener*/
+    
 }
