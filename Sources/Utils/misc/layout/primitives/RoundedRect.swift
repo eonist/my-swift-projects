0bf8d9219ef86982f8c91bf0f56fd5aaad676325
Eonist <30n1st@gmail.com>
2017-07-30 01:34:48 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/primitives/RoundedRect.swift

--- Sources/Utils/misc/layout/primitives/RoundedRect.swift
+++ Sources/Utils/misc/layout/primitives/RoundedRect.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct RoundedRect{
     var rect:CGRect
-    var fillet:Fillet
-    init(_ rect:CGRect = CGRect(),_ fillet:Fillet = Fillet()) {
+    var fillet:CGFloat
+    init(_ rect:CGRect = CGRect(),_ fillet:CGFloat = 0) {
         self.rect = rect
         self.fillet = fillet
     }
@@ -15,20 +15,20 @@ extension RoundedRect{
     var y: CGFloat {get{return rect.y} set{rect.y = newValue}}
     var w: CGFloat {get{return rect.w} set{rect.w = newValue}}
     var h: CGFloat {get{return rect.h} set{rect.h = newValue}}
-    init(_ origin:CGPoint = CGPoint(),_ size:CGSize = CGSize(),_ fillet:Fillet = Fillet()){
+    init(_ origin:CGPoint = CGPoint(),_ size:CGSize = CGSize(),_ fillet:CGFloat = 0){
         self.init(CGRect(origin,size), fillet)
     }
-    init(_ x:CGFloat = 0,_ y:CGFloat = 0,_ w:CGFloat = 0,_ h:CGFloat = 0,_ fillet:Fillet = Fillet()){
+    init(_ x:CGFloat = 0,_ y:CGFloat = 0,_ w:CGFloat = 0,_ h:CGFloat = 0,_ fillet:CGFloat = 0){
         self.init(CGRect(CGPoint(x,y),CGSize()), fillet)
     }
     enum DefaultEasing{
-        static var easing:RoundedRect { return RoundedRect(0.2,0.2,0.2,0.2,Fillet(0.2)) }
+        static var easing:RoundedRect { return RoundedRect(0.2,0.2,0.2,0.2,0.2) }
     }
 }
 extension RoundedRect:Advancable5 {
-    static var defaults:AnimState5<RoundedRect>  =  AnimState5<RoundedRect>(RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
+    static var defaults:AnimState5<RoundedRect>  =  AnimState5<RoundedRect>(RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(10e-5,10e-5,10e-5,10e-5,10e-5))
     func isNear( value:  RoundedRect,  epsilon: RoundedRect) -> Bool {
-        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x) && 
+        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x) && self.fillet.isNear(value.fillet,epsilon.fillet)
     }
 }
 func +(a: RoundedRect, b: RoundedRect) -> RoundedRect { return RoundedRect(a.origin + b.origin,a.size + b.size,a.fillet+b.fillet)}
