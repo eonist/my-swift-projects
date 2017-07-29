c40e8335d24df636a44d6ebb50123ffd86a5b0e2
Eonist <30n1st@gmail.com>
2017-07-29 23:49:33 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/primitives/RoundedRect.swift

--- Sources/Utils/misc/layout/primitives/RoundedRect.swift
+++ Sources/Utils/misc/layout/primitives/RoundedRect.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class RoundedRect {
-
+class RoundedRect:CGRect{
+    var fillet:Fillet
+    init(_ rect:CGRect,_ fillet:Fillet) {
+        self.fillet = fillet
+        super.init(rect.x,rect.y,rect.w,rect.h)
+    }
 }
