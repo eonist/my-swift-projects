1a9a56e40eafd43ac6819cb07956a30025ed4f82
Eonist <30n1st@gmail.com>
2017-07-30 00:09:07 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/primitives/RoundedRect.swift

--- Sources/Utils/misc/layout/primitives/RoundedRect.swift
+++ Sources/Utils/misc/layout/primitives/RoundedRect.swift
@@ -3,10 +3,16 @@ import Foundation
 struct RoundedRect{
     var rect:CGRect
     var fillet:Fillet
-    var origin: CGPoint {get{return rect.origin} set{rect.origin = newValue}}
-    var size: CGSize {get{return rect.size} set{rect.size = newValue}}
     init(_ rect:CGRect,_ fillet:Fillet) {
         self.rect = rect
         self.fillet = fillet
+    }
+    
+}
+extension RoundedRect{
+    var origin: CGPoint {get{return rect.origin} set{rect.origin = newValue}}
+    var size: CGSize {get{return rect.size} set{rect.size = newValue}}
+    init(_ origin:CGPoint,_ size:CGSize,_ fillet:Fillet){
+        self.init(CGRect(origin,size), fillet)
     }
 }
