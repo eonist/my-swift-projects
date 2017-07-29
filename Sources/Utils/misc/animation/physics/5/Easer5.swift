5ccffe9b781c3df1345b7e2140c3e1ddf3eddd19
Eonist <30n1st@gmail.com>
2017-07-30 01:15:08 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/5/Easer5.swift

--- Sources/Utils/misc/animation/physics/5/Easer5.swift
+++ Sources/Utils/misc/animation/physics/5/Easer5.swift
@@ -39,7 +39,7 @@ class Easer5<T:Advancable5>:FrameAnimator2, PhysicsAnimKind5{
         return state.velocity.isNear(value:stopVelocity, epsilon:epsilon)
     }
 }
-struct DefaultEasing{
+enum DefaultEasing{
     static var value:CGFloat { return (0.2) }
     static var point:CGPoint { return CGPoint(0.2,0.2) }
     static var rect:CGRect { return CGRect(0.2,0.2,0.2,0.2) }
