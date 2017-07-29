66fb9a87f7a7c21cf9533486bd87bdb478b9fdd0
Eonist <30n1st@gmail.com>
2017-07-30 00:48:36 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/primitives/RoundedRect.swift

--- Sources/Utils/misc/layout/primitives/RoundedRect.swift
+++ Sources/Utils/misc/layout/primitives/RoundedRect.swift
@@ -3,7 +3,7 @@ import Foundation
 struct RoundedRect{
     var rect:CGRect
     var fillet:Fillet
-    init(_ rect:CGRect,_ fillet:Fillet) {
+    init(_ rect:CGRect = CGRect(),_ fillet:Fillet = Fillet()) {
         self.rect = rect
         self.fillet = fillet
     }
@@ -12,17 +12,17 @@ struct RoundedRect{
 extension RoundedRect{
     var origin: CGPoint {get{return rect.origin} set{rect.origin = newValue}}
     var size: CGSize {get{return rect.size} set{rect.size = newValue}}
-    init(_ origin:CGPoint,_ size:CGSize,_ fillet:Fillet){
+    init(_ origin:CGPoint = CGPoint(),_ size:CGSize = CGSize(),_ fillet:Fillet = Fillet()){
         self.init(CGRect(origin,size), fillet)
     }
-    init(_ x:CGFloat,_ y:CGFloat,_ w:CGFloat,_ h:CGFloat,_ fillet:Fillet){
+    init(_ x:CGFloat = 0,_ y:CGFloat = 0,_ w:CGFloat = 0,_ h:CGFloat = 0,_ fillet:Fillet = Fillet()){
         self.init(CGRect(CGPoint(x,y),CGSize()), fillet)
     }
 }
 
-//extension RoundRect:Advancable5 {
-//    static var defaults:AnimState5<RoundRect>  =  AnimState5<RoundRect>(RoundRect(), RoundRect(), RoundRect(), RoundRect(), RoundRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
-//    func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
-//        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
-//    }
-//}
+extension RoundedRect:Advancable5 {
+    static var defaults:AnimState5<RoundedRect>  =  AnimState5<RoundedRect>(RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
+    func isNear( value:  RoundedRect,  epsilon: RoundedRect) -> Bool {
+        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
+    }
+}
