54cfd513f768a7e40d3ab276dbb46e51b6858351
Eonist <30n1st@gmail.com>
2017-07-30 00:45:40 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/layout/primitives/RoundedRect.swift

--- Sources/Utils/misc/layout/primitives/RoundedRect.swift
+++ Sources/Utils/misc/layout/primitives/RoundedRect.swift
@@ -19,3 +19,10 @@ extension RoundedRect{
         self.init(CGRect(CGPoint(x,y),CGSize()), fillet)
     }
 }
+
+//extension RoundRect:Advancable5 {
+//    static var defaults:AnimState5<RoundRect>  =  AnimState5<RoundRect>(RoundRect(), RoundRect(), RoundRect(), RoundRect(), RoundRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
+//    func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
+//        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
+//    }
+//}
