4241d87e80e6d9ec8b1adb9ef85c588f37f914e0
Eonist <30n1st@gmail.com>
2017-07-30 13:55:36 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/view/utils/CALayerExtensions.swift

--- Sources/Utils/misc/view/utils/CALayerExtensions.swift
+++ Sources/Utils/misc/view/utils/CALayerExtensions.swift
@@ -7,4 +7,10 @@ extension CALayer {
     func rotate(_ rotation:CGFloat,_ pivot:CGPoint){
         CALayerModifier.rotate(self, rotation, pivot)
     }
+    /**
+     * New, great in conjunction wtih closure callback's
+     */
+    func setPos(_ point:CGPoint){
+        self.position = point
+    }
 }
