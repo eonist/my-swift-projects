90c39f01aaccc572b3e95171ee7954e54fb2170d
Eonist <30n1st@gmail.com>
2017-07-30 13:22:14 +0200

    Files modified: 3
    Modified 3 files:Sources/Utils/geom/context/Graphic.swiftSources/Utils/misc/view/InteractiveView.swiftSources/Utils/misc/view/utils/NSViewExtensions.swift

--- Sources/Utils/misc/view/InteractiveView.swift
+++ Sources/Utils/misc/view/InteractiveView.swift
@@ -26,6 +26,19 @@ class InteractiveView:FlippedView,InteractiveViewable{//TODO: rename this with a
         self.wantsLayer = true/*if true then view is layer backed*/
         layer = CALayer()/*needs to be layer-hosted so that we don't get clipping of children*/
         layer?.masksToBounds = false/*This is the variable that makes subchildren mask its parents frame, set it to false and they won't mask*/
+        
+        layer.actions = ["sublayers":NSNull()]
+        
+        NSDictionary *newActions = @{
+            @"onOrderIn": [NSNull null],
+            @"onOrderOut": [NSNull null],
+            @"sublayers": [NSNull null],
+            @"contents": [NSNull null],
+            @"bounds": [NSNull null]
+        };
+        
+        layer.actions = newActions;
+        
         self.layerContentsRedrawPolicy = .onSetNeedsDisplay/*Supposedly this makes anim fast, may or may not have an effect, try diable and enable it from time to time*/
     }
     /**
--- Sources/Utils/misc/view/utils/NSViewExtensions.swift
+++ Sources/Utils/misc/view/utils/NSViewExtensions.swift
@@ -124,7 +124,7 @@ extension NSView {
 
 /**
  * Disable implicit animation
- * TODO: ⚠️️ Figure out how to disaple auto anim in NSViews i think its: layer.actions = ["sublayers":NSNull()] see:https://stackoverflow.com/questions/5833488/how-to-disable-calayer-implicit-animations/43560444#43560444   and:  https://stackoverflow.com/questions/2244147/disabling-implicit-animations-in-calayer-setneedsdisplayinrect?rq=1
+ * TODO: ⚠️️ Figure out how to disaple auto anim in NSViews i think it's: layer.actions = ["sublayers":NSNull()] see:https://stackoverflow.com/questions/5833488/how-to-disable-calayer-implicit-animations/43560444#43560444   and:  https://stackoverflow.com/questions/2244147/disabling-implicit-animations-in-calayer-setneedsdisplayinrect?rq=1
  * EXAMPLE: disableAnim{view.isHidden = true}//Default animation is now disabled, Basically whatever you put inside the closure is not animated
  */
 func disableAnim(_ closure:()->Void){
