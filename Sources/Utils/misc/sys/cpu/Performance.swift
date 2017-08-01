d85a61f5ca0d515c5e42fab1a3967484f0a312dc
Eonist <30n1st@gmail.com>
2017-08-01 16:03:26 +0200

    'Files modified: 1'
    'Added 1 file:
    Sources/Utils/misc/sys/cpu/Performance.swift
    
    '

--- Sources/Utils/misc/sys/cpu/Performance.swift
+++ Sources/Utils/misc/sys/cpu/Performance.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-class Performance {
-
+/**
+ * EXAMPLE: testPerformance("Adding styles took: "){CSSFileParser.cssString(url)}//Adding styles took 2.4secs
+ * PARAM: startTime: performance test start time
+ */
+func testPerformance(_ context:String = "", _ startTime:NSDate = NSDate(), _ closure:@escaping ()->Void){
+    closure()/*Executes the closure*/
+    Swift.print(context + " \(abs(startTime.timeIntervalSinceNow))" + " Secs")/*Prints performance test end time*/
+}
+/**
+ * Allows you to test performance and also return a value
+ * EXAMPLE: let result:String = testPerformance{readData()}//0.028 Secs
+ */
+func testPerformance<T>(_ context:String = "", _ startTime:NSDate = NSDate(), _ closure:@escaping ()->T) -> T{
+    defer {/*execute just before code execution leaves the current block of code*/
+        Swift.print(context + " \(abs(startTime.timeIntervalSinceNow))" + " Secs")/*Prints performance test end time*/
+    }
+    return closure()/*Executes the closure*/
 }
