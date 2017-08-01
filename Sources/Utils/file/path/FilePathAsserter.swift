97cc384487be8fec9644dc4c7bf9c28d4c2a57f7
Eonist <30n1st@gmail.com>
2017-08-02 01:14:35 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/path/FilePathAsserter.swift

--- Sources/Utils/file/path/FilePathAsserter.swift
+++ Sources/Utils/file/path/FilePathAsserter.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class FilePathAsserter {
-
+    /**
+     * Tests if a path is absolute /User/John/ or relative : ../../ or styles/design/
+     */
+    static func isAbsolute(path:String, pathSeperator:String = "/") -> Bool{
+        return path.hasPrefix(pathSeperator)
+    }
 }
