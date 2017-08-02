50ae4fe5705ac611de90f3a7742efc986b1840a3
Eonist <30n1st@gmail.com>
2017-08-02 19:02:38 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/utils/RegExpPattern.swift

--- Sources/Utils/string/regexp/utils/RegExpPattern.swift
+++ Sources/Utils/string/regexp/utils/RegExpPattern.swift
@@ -16,4 +16,37 @@ enum RegExpPattern {
         let pattern:String = /*g1 +*/ g2 + g3 + g4 + g5 /*+ g6*/
         return pattern
     }()
+    /**
+     * Returns an URL regular expression pattern
+     */
+    static func urlPattern() -> String {
+        let patternString:String =  "\\b"+//the border between a word character and a non word charactre
+									"("+//Group1 start
+										"(" +//Group2 staret
+											"https?" +//http or https (? makes the sMatches the previouse expression if it exists)
+											"|" +//or
+											"telenet" +
+											"|" +//or
+											"gopher" +
+											"|" +//or
+											"file" +
+											"|" +//or
+											"wais" +
+											"|" +//or
+											"ftp" +
+										")" +//Group2 end
+										"\\:"+//Subseeded by ":"
+										"[\\w\\/\\#~:.\\?+=&%@!\\-]+?" + //subseeded by one of the characters and must Match 1 or more times, but as few times as possible (Lazy)
+									")"+//Group1 end
+									"(" +//Group3 Start
+										"?="+
+										"[\\.\\:\\?\\-]*"+		//for possible punct char,  match 0 or more occurrences of the preceding item (Greedy quantifier)
+										"(" +//Start of group4
+											"?:[^\\w/\\#~:.?+=&%@!\\-]"+//Invalid characters, Non-capturing group - Groups subpattern, but do not capture subMatch (does not consume the subseeding pattern)
+											"|" +//or
+											"$" +// end of string
+										")"+//Group4 end			
+									")"//Group3 end
+		return patternString
+    }
 }
