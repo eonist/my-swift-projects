34064d650bd2ae6710f42ead63ddf209a59bf9f0
Eonist <30n1st@gmail.com>
2017-08-02 18:37:07 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/utils/RegExpParser.swift

--- Sources/Utils/string/regexp/utils/RegExpParser.swift
+++ Sources/Utils/string/regexp/utils/RegExpParser.swift
@@ -1,11 +1,14 @@
 import Foundation
 
 public class RegExpParser{
-    /**
-     * Must be in the format: 12-30-1968 (mm-dd-yyyy)
-     * NOTE: works with: "12-30-1968" and "12/30/1968" syntax
-     */
-    static let usDatePattern:String = "(\\d\\d)[-\\/](\\d\\d)[-\\/](\\d\\d(?:\\d\\d)?)"//g
+    enum Pattern{
+        /**
+         * Must be in the format: 12-30-1968 (mm-dd-yyyy)
+         * NOTE: works with: "12-30-1968" and "12/30/1968" syntax
+         */
+        static let usDate:String = "(\\d\\d)[-\\/](\\d\\d)[-\\/](\\d\\d(?:\\d\\d)?)"//g
+    }
+    
     /**
      * Returns an array of computer date formated dates from PARAM: usDate
      * EXAMPLE:
@@ -15,7 +18,7 @@ public class RegExpParser{
      * NOTE: this could be made more advance by including the posibilities for single digit months and days, but that is out of the scope of this example
      */
     static func computerDate(_ usDate:String)->String {
-        let pattern:String = usDatePattern
+        let pattern:String = Pattern.usDate
         var output:String = ""
         usDate.matches(pattern).forEach {
             let month:String = $0.value(usDate, 1)
@@ -24,6 +27,16 @@ public class RegExpParser{
             output = year + "/" + month + "/" + day
         }
         return output
+    }
+    /**
+     * Computes and returns an array comprised of url protocol(s) from @param input
+     * @param input: a string comprised of url(s)
+     * @example:"http://www.forta.com/\n " +"https://mail.forta.com/\n " +"ftp://ftp.forta.com/\n";//http, https, ftp
+     * @Note: More url patterns in RegExpDescriber.url
+     * @Note also see RegExpParser.htmlLinks
+     */
+    static func urlProtocol(input:String) -> Array<String> {
+        return input.match(".+(?=:)")
     }
 }
 /*
