288d3cdafedfe8911e866d61ced9360fe67c1ce4
Eonist <30n1st@gmail.com>
2017-08-02 16:48:17 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/utils/RegExpPattern.swift

--- Sources/Utils/string/regexp/utils/RegExpPattern.swift
+++ Sources/Utils/string/regexp/utils/RegExpPattern.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-class RegExpPattern {
+enum RegExpPattern {
     /**
      * NOTE: Used in the svg path parameter parser for instance
      * NOTE: swift doesn't do conditionals in regex like this: \-?\d*?(\.?)((?1)\d+?)  so the bellow code was made to do the same thing
      * NOTE: you may need to add more
      */
-    static var digitAssertPattern:String{
+    static let digitAssertPattern:String = {
         //let g1:String = "(?=\\s|^)"/*must be preceeded by a space char or the begining*/
         let g2:String = "\\-*\\d+?"/*optional minus sign followed by one or more digits*/
         let g3:String = "(?=\\.\\d|\\s|,|\\-|$)"/*\\- was added recently, it may cause bugs*/   /*Must be followed by a dot with a digit or space, comma or end*/
@@ -15,5 +15,5 @@ class RegExpPattern {
         //let g6:String = "(?=$)"/*followed by an end or */
         let pattern:String = /*g1 +*/ g2 + g3 + g4 + g5 /*+ g6*/
         return pattern
-    }
+    }()
 }
