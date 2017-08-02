10a206b6a447a66b7ed0fdd8155b28a0db64412a
Eonist <30n1st@gmail.com>
2017-08-02 13:09:39 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/path/FilePathModifier.swift

--- Sources/Utils/file/path/FilePathModifier.swift
+++ Sources/Utils/file/path/FilePathModifier.swift
@@ -2,16 +2,11 @@ import Foundation
 
 class FilePathModifier {
     /**
-     * New
+     * EXAMPLE: FilePathModifier.normalize("/Users/John/Desktop/temp/../test.txt".tildePath)///Users/John/Desktop/test.txt
      */
-    static func normalize(url:String) -> String{
-        let normalizedURL:String = {
-            if FilePathAsserter.isAbsolute(path: url) {
-                return url
-            }else {
-                return ""
-            }
-        }()
-        return normalizedURL
+    static func normalize(_ urlStr:String) -> String{
+        let url:URL = FilePathParser.path(urlStr)
+        let normalizedURL:URL = url.standardized
+        return FilePathParser.path(normalizedURL)
     }
 }
