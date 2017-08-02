349fe9c91e3fa1982ffe9e47fb5cb0dcfffa6d32
Eonist <30n1st@gmail.com>
2017-08-02 23:45:23 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/path/FilePathModifier.swift

--- Sources/Utils/file/path/FilePathModifier.swift
+++ Sources/Utils/file/path/FilePathModifier.swift
@@ -9,4 +9,25 @@ class FilePathModifier {
         let normalizedURL:URL = url.standardized
         return FilePathParser.path(normalizedURL)
     }
+    /**
+     * PARAM: baseURL: must be absolute: "Users/John/Desktop/temp"
+     * EXAMPLE: Swift.print(expand("/Users/John/Desktop/temp"))///Users/John/Desktop/temp
+     * EXAMPLE: Swift.print(expand("~/Desktop/test.txt"))///Users/John/Desktop/test.txt
+     * EXAMPLE: Swift.print(expand("/temp/colors/star.svg",baseURL:"/Users/John/Desktop"))///Users/John/Desktop/temp/colors/star.svg
+     * EXAMPLE: Swift.print(expand("star.svg",baseURL:"/Users/John/Desktop"))///Users/John/Desktop/star.svg
+     * IMPORTANT: ⚠️️ Tilde paths can't have backlash syntax like ../../ etc
+     */
+    static func expand(_ filePath:String, baseURL:String = "") -> String{
+        if FilePathAsserter.isTildePath(filePath) {
+            return filePath.tildePath
+        }else if FilePathAsserter.isBacklash(filePath) {//isRelative
+            return FilePathModifier.normalize(baseURL + filePath)//returns absolute path
+        }else if FileAsserter.exists(filePath){//absolute path that exists
+            return filePath
+        }else if FilePathAsserter.isAbsolute(filePath){//absolute but doesn't exists
+            return baseURL + filePath
+        }else{//must be just I.E: "star.svg"
+            return baseURL + "/" + filePath
+        }
+    }
 }
