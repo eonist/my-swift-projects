7d2fd9e8f3c3fd95b4d4a1d36063f1b1f103885a
Eonist <30n1st@gmail.com>
2017-08-02 21:59:15 +0200

    Files modified: 2
    Modified 2 files:Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swiftSources/Utils/string/regexp/utils/RegExpPattern.swift

--- Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swift
+++ Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swift
@@ -1,6 +1,42 @@
 import Foundation
 
 class RegExpAdvanceParser {
+    enum Pattern{
+        /**
+         * Returns an URL regular expression pattern
+         */
+        static let urlPattern:String = {
+            var str:String = ""
+            str += "\\b"//the border between a word character and a non word charactre
+            str += "("//Group1 start
+            str += "(" //Group2 staret
+            str += "https?" //http or https (? makes the sMatches the previouse expression if it exists)
+            str += "|" //or
+            str += "telenet"
+            str += "|" //or
+            str += "gopher"
+            str += "|" //or
+            str += "file"
+            str += "|" //or
+            str += "wais"
+            str += "|" //or
+            str += "ftp"
+            str += ")" //Group2 end
+            str += "\\:"//Subseeded by ":"
+            str += "[\\w\\/\\#~:.\\?+=&%@!\\-]+?"  //subseeded by one of the characters and must Match 1 or more times, but as few times as possible (Lazy)
+            str += ")"//Group1 end
+            str += "(" //Group3 Start
+            str += "?="
+            str += "[\\.\\:\\?\\-]*"		//for possible punct char,  match 0 or more occurrences of the preceding item (Greedy quantifier)
+            str += "(" //Start of group4
+            str += "?:[^\\w/\\#~:.?+=&%@!\\-]"//Invalid characters, Non-capturing group - Groups subpattern, but do not capture subMatch (does not consume the subseeding pattern)
+            str += "|" //or
+            str += "$" // end of string
+            str += ")"//Group4 end
+            str += ")"//Group3 end
+            return str
+        }()
+    }
     /**
      * Computes and returns words that has the characters @param string followed by one or more characters in @param followedByOneOrMore
      * @example:
--- Sources/Utils/string/regexp/utils/RegExpPattern.swift
+++ Sources/Utils/string/regexp/utils/RegExpPattern.swift
@@ -16,38 +16,5 @@ enum RegExpPattern {
         let pattern:String = /*g1 +*/ g2 + g3 + g4 + g5 /*+ g6*/
         return pattern
     }()
-    /**
-     * Returns an URL regular expression pattern
-     */
-    static let urlPattern:String = {
-        var str:String = ""
-            str += "\\b"//the border between a word character and a non word charactre
-                str += "("//Group1 start
-                    str += "(" //Group2 staret
-                        str += "https?" //http or https (? makes the sMatches the previouse expression if it exists)
-                        str += "|" //or
-                        str += "telenet" 
-                        str += "|" //or
-                        str += "gopher" 
-                        str += "|" //or
-                        str += "file" 
-                        str += "|" //or
-                        str += "wais" 
-                        str += "|" //or
-                        str += "ftp" 
-                    str += ")" //Group2 end
-                    str += "\\:"//Subseeded by ":"
-                    str += "[\\w\\/\\#~:.\\?+=&%@!\\-]+?"  //subseeded by one of the characters and must Match 1 or more times, but as few times as possible (Lazy)
-                str += ")"//Group1 end
-                str += "(" //Group3 Start
-                    str += "?="
-                    str += "[\\.\\:\\?\\-]*"		//for possible punct char,  match 0 or more occurrences of the preceding item (Greedy quantifier)
-                    str += "(" //Start of group4
-                        str += "?:[^\\w/\\#~:.?+=&%@!\\-]"//Invalid characters, Non-capturing group - Groups subpattern, but do not capture subMatch (does not consume the subseeding pattern)
-                        str += "|" //or
-                        str += "$" // end of string
-                    str += ")"//Group4 end			
-                str += ")"//Group3 end
-		return str
-    }()
+
 }
