6cda8b766907084a7a88988d631e3d4fe98a281e
Eonist <30n1st@gmail.com>
2017-08-02 18:46:20 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/utils/RegExpParser.swift

--- Sources/Utils/string/regexp/utils/RegExpParser.swift
+++ Sources/Utils/string/regexp/utils/RegExpParser.swift
@@ -86,6 +86,27 @@ public class RegExpParser{
         let pattern:String = "\\b[a-z]+\\b"//Adding i-flag here would omitt the lowercase rule
         return input.match(pattern)
     }
+    /**
+     * Computes and returns an array with lowercaseWords from @param input
+     */
+    static func lowerCaseWordsWithExecution(input:String)-> [String] {
+        var pattern:String = "\\b[a-z]+\\b"
+        var words: [String] = []
+        var lastIndex:Int = -1;
+        while(lastIndex != 0){
+            var word:String = pattern.exec(input);
+            lastIndex = pattern.lastIndex;
+            if(word != null) words.push(word);
+        }
+        return words;
+        
+        RegExp.matches(str, pattern).forEach {
+            Swift.print("match.numberOfRanges: " + "\($0.numberOfRanges)")/*The first item is the entire match*/
+            let content = (str as NSString).substringWithRange($0.rangeAtIndex(0))/*the entire match*/
+            let name = $0.value(str, 1)/*capturing group 1*/
+            let value = $0.value(str, 2)/*capturing group 2*/
+        }//Outputs: name: green, value: 00FF00...and so on
+    }
 }
 /*
 	Add these from legacy:
