c5062f16f640b99b96e69fe4f6b4297e35ef9812
Eonist <30n1st@gmail.com>
2017-08-02 23:41:45 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/path/FilePathAsserter.swift

--- Sources/Utils/file/path/FilePathAsserter.swift
+++ Sources/Utils/file/path/FilePathAsserter.swift
@@ -7,6 +7,9 @@ class FilePathAsserter {
     static func isAbsolute(_ path:String, pathSeperator:String = "/") -> Bool{
         return path.hasPrefix(pathSeperator)
     }
+    static func isBacklash(_ path:String) -> Bool{//the name is not great, improve later
+        return path.hasPrefix("../")
+    }
     /**
      * New, naive approche
      */
