bc681972ce11e83f45bf941525a2a82516085a6f
Eonist <30n1st@gmail.com>
2017-08-03 00:53:42 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/RegExp.swift

--- Sources/Utils/string/regexp/RegExp.swift
+++ Sources/Utils/string/regexp/RegExp.swift
@@ -108,6 +108,45 @@ public class RegExp{
         //TODO: ⚠️️ research enumerateMatches, it takes a method and enumerate all matches.
         //NSRegularExpression.replacementString has an offset, which I think you can use
     }
+    
+    static func replaceMatches<T: Sequence>(in source:String, matches:T, using replacer:(Match) -> String?) -> String where T.Iterator.Element : Match {
+        
+        RegExp.matches(str, "(\\w+?)\\:([A-Z0-9]+?)(?: |$)").forEach {
+            Swift.print("match.numberOfRanges: " + "\($0.numberOfRanges)")/*The first item is the entire match*/
+            let content = (str as NSString).substringWithRange($0.rangeAtIndex(0))/*the entire match*/
+            let name = $0.value(str, 1)/*capturing group 1*/
+            let value = $0.value(str, 2)/*capturing group 2*/
+        }
+        
+        var result = ""
+        var lastRange:StringRange = source.startIndex ..< source.startIndex
+        for match in matches {
+            result += source.substring(with: lastRange.upperBound ..< match.range.lowerBound)
+            if let replacement = replacer(match) {
+                result += replacement
+            } else {
+                result += source.substring(with: match.range)
+            }
+            lastRange = match.range
+        }
+        result += source.substring(from: lastRange.upperBound)
+        return result
+    }
+    
+    
+    /**
+     Replaces all occurances of the pattern using supplied replacer function.
+     
+     - parameters:
+     - source: String to be matched to the pattern
+     - replacer: Function that takes a match and returns a replacement. If replacement is nil, the original match gets inserted instead
+     - returns: A string, where all the occurances of the pattern were replaced
+     */
+//    static func replaceAll(_ source:String,  replacer:(Match) -> String?) -> String {
+//        let matches = findAll(in: source)
+//        return replaceMatches(in: source, matches: matches, using: replacer)
+//    }
+    
 }
 extension NSTextCheckingResult{
     func value(_ str:String, _ key:Int)->String{//Convenience
