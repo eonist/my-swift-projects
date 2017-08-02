e9c9b0849cb6485ff1f47985e8e6a5d24b3af9ff
Eonist <30n1st@gmail.com>
2017-08-02 02:05:15 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/path/FilePathModifier.swift

--- Sources/Utils/file/path/FilePathModifier.swift
+++ Sources/Utils/file/path/FilePathModifier.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 class FilePathModifier {
-
+    /**
+     * New
+     */
+    static func normalize(url:String) -> String{
+        let normalizedURL:String = {
+            if FilePathAsserter.isAbsolute(path: url) {
+                return url
+            }else {
+                return ""
+            }
+        }()
+        return normalizedURL
+    }
 }
