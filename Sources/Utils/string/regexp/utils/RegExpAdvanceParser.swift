73bcad12d5a1d67752c32af90dab187a00a95d97
Eonist <30n1st@gmail.com>
2017-08-02 20:14:37 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swift

--- Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swift
+++ Sources/Utils/string/regexp/utils/RegExpAdvanceParser.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class RegExpAdvanceParser {
-
+    /**
+     * Computes and returns words that has the characters @param string followed by one or more characters in @param followedByOneOrMore
+     * @example:
+     * wordsThatStartAndFollowWithOneOrMany("target tarzan mortar tarrif", "ta", "r"))// target,tarzan,tarrif
+     */
+    static func wordsThatStartAndFollowWithOneOrMany(_ input:String, _start:String, oneOrMany:String) -> [String] {
+        let pattern:String = "\\b" + start + oneOrMany + "+?\\.*?\\b"
+        return input.match(pattern)
+    }
 }
