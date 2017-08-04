7e25828c67d158a91cb53941028c633c7f526d1d
Eonist <30n1st@gmail.com>
2017-08-04 17:08:10 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/sys/cpu/ThreadGroup.swift

--- Sources/Utils/misc/sys/cpu/ThreadGroup.swift
+++ Sources/Utils/misc/sys/cpu/ThreadGroup.swift
@@ -1,11 +1,11 @@
 import Foundation
 /**
- * Sometimes DispatchGroup doesn't work when you need nested groups etc. ThreadGroup is simple in design and can work as a substitute.
+ * Sometimes DispatchGroup doesn't work when you need nested groups etc. Mixing async and sync code eetc 
+ * ThreadGroup is simple in design and can work as a substitute.
  * NOTE: private (set) var value : Int32 = 0; OSAtomicIncrement32(&value) could be faster
  */
 class ThreadGroup {
     typealias CompletionHandler = ()->Void
-    //private var index:Int = 0//currentIndex
     private (set) var index : Int32 = 0
     var onComplete:CompletionHandler
     private var count:Int32 = 0//when count reaches this count, onAllComplete is executed
