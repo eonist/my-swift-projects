96249699019086cb09a0c990226755bb174c8177
Eonist <30n1st@gmail.com>
2017-08-04 14:15:15 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/sys/cpu/ThreadGroup.swift

--- Sources/Utils/misc/sys/cpu/ThreadGroup.swift
+++ Sources/Utils/misc/sys/cpu/ThreadGroup.swift
@@ -2,27 +2,20 @@ import Foundation
 
 class ThreadGroup {
     typealias CompletionHandler = ()->Void
-    private var index:Int = 0
+    private var index:Int = 0//currentIndex
     private var allComplete:CompletionHandler
-    private var count:Int
+    private var count:Int//when count reaches this count, onAllComplete is executed
     init(_ count:Int,onAllComplete:@escaping CompletionHandler){
         self.count = count
         self.allComplete = onAllComplete
     }
+    /**
+     * IMPORTANT: onComplete must be called on the main thread
+     */
     func onComplete(){
         index += 1
-        //Swift.print("refreshRepo.onComplete() i: \(idx)")
-        if index == 12 {
+        if index == count {
             allComplete()
         }
-//        allComplete()//execute the closure
     }
 }
-//let repos = RepoUtils.repoListFlattenedOverridden/*creates array from xml or cache*/
-//var idx:Int = 0
-//func onComplete(){/*TODO: ⚠️️ You can probably use DispatchGroup here aswell. but in the spirit of moving on*/
-//    
-//}
-//repos.forEach { repo in
-//    RefreshUtils.refreshRepo(self.commitDP!,repo,onComplete)//🚪⬅️️ 🚧 0~1000's of a-sync 💼->🐚->🌵 calls
-//}
