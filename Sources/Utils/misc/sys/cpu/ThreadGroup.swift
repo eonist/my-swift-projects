d2f6b9f36ee556622859f0ea0e49e12317297f7d
Eonist <30n1st@gmail.com>
2017-08-04 15:47:37 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/sys/cpu/ThreadGroup.swift

--- Sources/Utils/misc/sys/cpu/ThreadGroup.swift
+++ Sources/Utils/misc/sys/cpu/ThreadGroup.swift
@@ -6,15 +6,18 @@ class ThreadGroup {
     typealias CompletionHandler = ()->Void
     private var index:Int = 0//currentIndex
     private var allComplete:CompletionHandler
-    private var count:Int//when count reaches this count, onAllComplete is executed
-    init(_ count:Int,onAllComplete:@escaping CompletionHandler = {fatalError("must have completion handler attached")}){
-        self.count = count
+    private var count:Int = 0//when count reaches this count, onAllComplete is executed
+    init(onAllComplete:@escaping CompletionHandler = {fatalError("must have completion handler attached")}){
+//      self.count = count
         self.allComplete = onAllComplete
+    }
+    func enter(){
+        count += 1
     }
     /**
      * IMPORTANT: onComplete must be called on the main thread
      */
-    func onComplete(){
+    func exit(){
         index += 1
         if index == count {
             allComplete()
