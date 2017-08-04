74d6b3f5092f8dfdaa7b21c5294c040181297c0a
Eonist <30n1st@gmail.com>
2017-08-04 17:28:06 +0200

    'Files modified: 1'
    'Added 1 file:
    Sources/Utils/misc/sys/cpu/ThreadGroup.swift
    
    '

--- Sources/Utils/misc/sys/cpu/ThreadGroup.swift
+++ Sources/Utils/misc/sys/cpu/ThreadGroup.swift
@@ -2,13 +2,12 @@ import Foundation
 /**
  * Sometimes DispatchGroup doesn't work when you need nested groups etc. Mixing async and sync code eetc 
  * ThreadGroup is simple in design and can work as a substitute.
- * NOTE: private (set) var value : Int32 = 0; OSAtomicIncrement32(&value) could be faster
  */
 class ThreadGroup {
     typealias CompletionHandler = ()->Void
-    private (set) var index : Int32 = 0
+    private (set) var index : Int = 0
     var onComplete:CompletionHandler
-    private var count:Int32 = 0//when count reaches this count, onAllComplete is executed
+    private var count:Int = 0//when count reaches this count, onAllComplete is executed
     init(onComplete:@escaping CompletionHandler = {fatalError("must have completion handler attached")}){
         self.onComplete = onComplete
     }
@@ -16,15 +15,21 @@ class ThreadGroup {
      * Seems to work in both bg and main threads
      */
     func enter(){
-        OSAtomicIncrement32(&count)/*OSAtom... makes Int threadsafe*/
+        main.async {
+            self.count += 1
+        }
     }
     /**
      * Seems to work in both bg and main threads
      */
-    func leave(que:DispatchQueue = main){
-        OSAtomicIncrement32(&index)
-        if self.index == self.count {
-            self.onComplete()
+    func leave(){
+        
+        main.async {
+            self.index += 1
+            if self.index == self.count {
+                self.onComplete()
+            }
         }
+        
     }
 }
