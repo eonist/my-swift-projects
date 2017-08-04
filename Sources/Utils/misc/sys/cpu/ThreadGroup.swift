91bfa746fdada50c6da8b118f40c577f7e81cc49
Eonist <30n1st@gmail.com>
2017-08-04 14:10:26 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/sys/cpu/ThreadGroup.swift

--- Sources/Utils/misc/sys/cpu/ThreadGroup.swift
+++ Sources/Utils/misc/sys/cpu/ThreadGroup.swift
@@ -3,23 +3,24 @@ import Foundation
 class ThreadGroup {
     typealias CompletionHandler = ()->Void
     var index:Int = 0
-    private var complete:CompletionHandler
-    init(onComplete:@escaping CompletionHandler, onAllComplete:CompletionHandler){
-        self.complete = onComplete
+    private var allComplete:CompletionHandler
+    init(onAllComplete:@escaping CompletionHandler){
+        self.allComplete = onAllComplete
     }
     func onComplete(){
-        complete()//execute the closure
+        index += 1
+        //Swift.print("refreshRepo.onComplete() i: \(idx)")
+        if index == 12 {
+            allComplete()
+        }
+//        allComplete()//execute the closure
     }
 }
-let repos = RepoUtils.repoListFlattenedOverridden/*creates array from xml or cache*/
-var idx:Int = 0
-func onComplete(){/*TODO: ⚠️️ You can probably use DispatchGroup here aswell. but in the spirit of moving on*/
-    idx += 1
-    //Swift.print("refreshRepo.onComplete() i: \(idx)")
-    if idx == repos.count {
-        allRefreshesCompleted()
-    }
-}
-repos.forEach { repo in
-    RefreshUtils.refreshRepo(self.commitDP!,repo,onComplete)//🚪⬅️️ 🚧 0~1000's of a-sync 💼->🐚->🌵 calls
-}
+//let repos = RepoUtils.repoListFlattenedOverridden/*creates array from xml or cache*/
+//var idx:Int = 0
+//func onComplete(){/*TODO: ⚠️️ You can probably use DispatchGroup here aswell. but in the spirit of moving on*/
+//    
+//}
+//repos.forEach { repo in
+//    RefreshUtils.refreshRepo(self.commitDP!,repo,onComplete)//🚪⬅️️ 🚧 0~1000's of a-sync 💼->🐚->🌵 calls
+//}
