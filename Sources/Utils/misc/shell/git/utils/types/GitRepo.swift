eca0edad3413fca8ed18fef9c575f8f5e1ef07be
Eonist <30n1st@gmail.com>
2017-08-05 20:18:49 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/utils/types/GitRepo.swift

--- Sources/Utils/misc/shell/git/utils/types/GitRepo.swift
+++ Sources/Utils/misc/shell/git/utils/types/GitRepo.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-class GitType {
-
+struct GitRepo{
+    var localPath:String,  remotePath:String, branch:String
+    init(_ localPath:String,_ remotePath:String,_ branch:String){self.localPath = localPath;self.remotePath = remotePath;self.branch = branch;}
 }
