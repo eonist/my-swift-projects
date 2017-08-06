f0e16f2e61a9779e6c30e8be4774f8a1c5edc3b1
Eonist <30n1st@gmail.com>
2017-08-06 13:08:17 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift

--- Sources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift
+++ Sources/Utils/misc/animation/physics/4/PhysicsAnimKind4.swift
@@ -30,4 +30,4 @@ extension PhysicsAnimKind4{
     var value:T {get{return initValues.value}set{initValues.value = newValue}}
     var stopVelocity:T {get{return initValues.stopVelocity}set{initValues.stopVelocity = newValue}}
 }
-*/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+*/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbvvvbbbbbnbbbnbbnbnbbvnvb bnvb 
