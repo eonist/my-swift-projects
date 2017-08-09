f872ae4eb2e42a135bdb4ebdabfc88ea96ee2148
Eonist <30n1st@gmail.com>
2017-08-09 20:21:17 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/text/TextField.swift

--- Sources/Utils/text/TextField.swift
+++ Sources/Utils/text/TextField.swift
@@ -22,7 +22,7 @@ class TextField:NSTextField,Trackable{
     }
     /**/
     override func mouseDown(with theEvent:NSEvent) {
-        Swift.print("TextField.mouseDown")
+//        Swift.print("TextField.mouseDown")
         self.window!.makeFirstResponder(self)//resigns the NSTextField caret focus
         
         NSEvent.addMonitor(&monitor,.leftMouseDown,onMouseDownOutside)/*we add a global mouse move event listener*/
@@ -30,7 +30,7 @@ class TextField:NSTextField,Trackable{
     }
     /**/
     func onMouseDownOutside(_ event:NSEvent) -> Void/*NSEvent?*/{
-        Swift.print("TextField.onMouseDownOutside event.type: \(event.type)")
+//        Swift.print("TextField.onMouseDownOutside event.type: \(event.type)")
         let p = window?.mouseLocationOutsideOfEventStream//self.locationInWindow
         if(hitTest(p!) == nil){//if you click outside the NSTextField then this will take care of resiging the caret of the text
 //            Swift.print("you click outside")
@@ -48,7 +48,7 @@ class TextField:NSTextField,Trackable{
 //        return event
     }
     override func mouseEntered(with event: NSEvent) {
-                Swift.print("mouseEntered")
+//                Swift.print("mouseEntered")
         if self.isSelectable {
             addCursorRect(frame, cursor:NSCursor.iBeam())
         }
@@ -57,13 +57,13 @@ class TextField:NSTextField,Trackable{
     //        addCursorRect(frame, cursor:NSCursor.arrow())
     //    }
     override func mouseExited(with event: NSEvent) {
-                Swift.print("TextField.mouseExited")
+//                Swift.print("TextField.mouseExited")
 //        if self.isSelectable {
         
             discardCursorRects()
             resetCursorRects()
-        addCursorRect(frame, cursor:NSCursor.arrow())
-        Swift.print("set arrow cursor")
+            addCursorRect(frame, cursor:NSCursor.arrow())
+//            Swift.print("set arrow cursor")
 //        }
         
     }
@@ -75,7 +75,7 @@ class TextField:NSTextField,Trackable{
      * TODO: ⚠️️ you don't have to store the trackingarea in this class you can get and set the trackingarea from NSView
      */
     override func updateTrackingAreas() {
-        self.createTrackingArea([.activeAlways,.mouseEnteredAndExited,.mouseMoved])
+        self.createTrackingArea([.activeAlways,.mouseEnteredAndExited])
         
         super.updateTrackingAreas()
     }
@@ -90,36 +90,36 @@ class TextField:NSTextField,Trackable{
 //        return super.resignFirstResponder()
 //    }
     override func textStorageWillProcessEditing(_ notification: Notification) {
-        Swift.print("textStorageWillProcessEditing")
+//        Swift.print("textStorageWillProcessEditing")
     }
     override func textStorageDidProcessEditing(_ notification: Notification) {
-        Swift.print("textStorageDidProcessEditing")
+//        Swift.print("textStorageDidProcessEditing")
     }
     override func textDidEndEditing(_ notification: Notification) {
-        Swift.print("textDidEndEditing")
+//        Swift.print("textDidEndEditing")
         
     }
     override func controlTextDidEndEditing(_ obj: Notification) {
-        Swift.print("controlTextDidEndEditing")
+//        Swift.print("controlTextDidEndEditing")
     }
     override func textDidBeginEditing(_ notification: Notification) {
-        Swift.print("textDidBeginEditing \(self.stringValue)")
+//        Swift.print("textDidBeginEditing \(self.stringValue)")
     }
     override func textShouldEndEditing(_ textObject: NSText) -> Bool {
-        Swift.print("textShouldEndEditing")
+//        Swift.print("textShouldEndEditing")
         return super.textShouldEndEditing(textObject)
     }
     override func textShouldBeginEditing(_ textObject: NSText) -> Bool {
-        Swift.print("textShouldBeginEditing \(self.stringValue)")
+//        Swift.print("textShouldBeginEditing \(self.stringValue)")
         return super.textShouldBeginEditing(textObject)
     }
     override func textDidChange(_ notification:Notification) {
-        Swift.print("textDidChange \(self.stringValue)")
+//        Swift.print("textDidChange \(self.stringValue)")
         if(self.superview is EventSendable){
-            Swift.print("superview is EventSendable")
+//            Swift.print("superview is EventSendable")
             (self.superview as! EventSendable).event!(TextFieldEvent(Event.update,self))
         }else{
-            Swift.print("superview is NOT EventSendable")
+//            Swift.print("superview is NOT EventSendable")
         }
         super.textDidChange(notification)
     }
