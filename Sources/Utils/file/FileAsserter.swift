f04a5c9136a6b1e8ced8c07d4377fad662597232
Eonist <30n1st@gmail.com>
2017-08-09 23:26:57 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/file/FileAsserter.swift

--- Sources/Utils/file/FileAsserter.swift
+++ Sources/Utils/file/FileAsserter.swift
@@ -6,4 +6,16 @@ class FileAsserter{
     static func exists(_ path:String) -> Bool {
         return FileManager().fileExists(atPath: path)
     }
+    /**
+     * New
+     */
+    static func hasContent() -> Bool{
+        return {
+            if let content = FileParser.contentOfDir(repoItem.localPath){
+                return !content.isEmpty
+            }else {
+                return false
+            }
+        }
+    }
 }
