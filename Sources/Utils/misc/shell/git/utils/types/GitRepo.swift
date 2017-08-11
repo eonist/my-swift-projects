7af0d02c7cb8a95b754486fdb80f6f27d8332bfc
Eonist <30n1st@gmail.com>
2017-08-11 14:29:31 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/utils/types/GitRepo.swift

--- Sources/Utils/misc/shell/git/utils/types/GitRepo.swift
+++ Sources/Utils/misc/shell/git/utils/types/GitRepo.swift
@@ -8,3 +8,20 @@ struct GitRepo{
         self.branch = branch;
     }
 }
+extension GitRepo{
+    /**
+     * New
+     * PARAM: remotePath: https://github.com/john/demo.git
+     * PARAM: localPath: Users/John/dev/demo
+     */
+    static func gitRepo(localPath:String, remotePath:String, branch:String)-> GitRepo {
+        let remotePath:String = {
+            if self.remote.test("^https://.+$") {
+                return self.remote.subString(8, self.remote.count)}/*support for partial and full url,strip away the https://, since this will be added later*/
+            else {
+                return self.remote
+            }
+        }()
+        return GitRepo(self.local, remotePath, self.branch)
+    }//temp
+}
