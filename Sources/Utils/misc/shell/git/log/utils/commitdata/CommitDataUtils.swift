f3d0c31bc9bad2f926a6b8559060bc2eb7f1d399
Eonist <30n1st@gmail.com>
2017-08-12 22:26:23 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/shell/git/log/utils/commitdata/CommitDataUtils.swift

--- Sources/Utils/misc/shell/git/log/utils/commitdata/CommitDataUtils.swift
+++ Sources/Utils/misc/shell/git/log/utils/commitdata/CommitDataUtils.swift
@@ -6,7 +6,7 @@ class CommitDataUtils {
      * EXAMPLE: GitLogParser.commitData("Hash:4caecd \n Author:Eonist \n Date:2015-12-03 16:59:09 +0100 \n Subject:'abc' \n Body:'123'")//Output: a CommitData instance
      * NOTE: the log data is formated using --pretty=format
      */
-    static func conform(raw input:String) -> CommitData{
+    static func convert(raw input:String) -> CommitData{
         /*Divide*/
         let firstIdx:Int = input.indexOf("\n")
         let firstPart:String = input.subString(0,firstIdx)
