1514b312df1dafc097b7dbb4ee869b9b03de1628
Eonist <30n1st@gmail.com>
2017-08-12 12:05:21 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/date/timer/SimpleTimer.swift

--- Sources/Utils/misc/date/timer/SimpleTimer.swift
+++ Sources/Utils/misc/date/timer/SimpleTimer.swift
@@ -6,34 +6,31 @@ import Foundation
  * NOTE: Set a NSDate to measure the time like this: var startTime:NSDate = NSDate();abs(startTime!.timeIntervalSinceNow) 
  * NOTE: Or easier: let d = CACurrentMediaTime(); /*Do heavy computing*/let d1 = CACurrentMediaTime()-d; print(d1)//0.452 sec
  * EXAMPLE:
- * let timer = SimpleTimer(0.5,true,self,#selector(update))
+ * let timer = SimpleTimer(0.5,true,callBack)
  * timer.start()
  * @objc func update() {Swift.print("tick")}//method must be in the public or scope
  * IMPORTANT: because your object has a property to store the timer, and the timer calls a method on the object, you have a strong reference cycle that means neither object can be freed. To fix this, make sure you invalidate the timer when you're done with it, such as when your view is about to disappear: timer.invalidate()
  */
 class SimpleTimer {/*was named Timer, but since swift 3, NSTimer is now Timer*/
+    typealias Tick = ()->Void
     var timer:Timer?
     var interval:TimeInterval
     var repeats:Bool
-    var target:AnyObject
-    var selector:ObjectiveC.Selector
+    var tick:Tick
     
-    init( interval:TimeInterval, repeats:Bool = false, target:AnyObject, selector:ObjectiveC.Selector){
+    init( interval:TimeInterval, repeats:Bool = false, onTick:@escaping Tick){/*, target:AnyObject*//*, selector:ObjectiveC.Selector*/
         self.interval = interval
         self.repeats = repeats
-        self.target = target
-        self.selector = selector
+        self.tick = onTick
     }
     func start(){
-        timer = Timer.scheduledTimer(timeInterval: interval, target: target, selector: selector, userInfo: nil, repeats: true)//swift 3 upgrade
+        timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(update), userInfo: nil, repeats: true)//swift 3 upgrade
     }
     func stop(){
         if(timer != nil){timer!.invalidate()}
     }
+    @objc func update() {//method must be in the public or scope
+        Swift.print("tick")
+        tick()
+    }
 }
-
-/*
-init(_ timeInterval:NSTimeInterval,_ target:AnyObject,_ aSelector:ObjectiveC.Selector,_ userInfo:AnyObject?,_ repeats:Bool ){
-    super.init(timeInterval: timeInterval, target: target, selector: aSelector, userInfo: userInfo, repeats: repeats)
-}
-*/
