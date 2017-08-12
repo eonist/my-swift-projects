9209598b12d6996c00db0a68670df930c3540dd5
Eonist <30n1st@gmail.com>
2017-08-12 12:17:58 +0200

    Files modified: 1
    Modified 1 file:Sources/Utils/misc/date/timer/SimpleTimer.swift

--- Sources/Utils/misc/date/timer/SimpleTimer.swift
+++ Sources/Utils/misc/date/timer/SimpleTimer.swift
@@ -1,20 +1,15 @@
-import CoreFoundation
-import CoreGraphics
-import Darwin
 import Foundation
 /**
  * NOTE: Set a NSDate to measure the time like this: var startTime:NSDate = NSDate();abs(startTime!.timeIntervalSinceNow) 
  * NOTE: Or easier: let d = CACurrentMediaTime(); /*Do heavy computing*/let d1 = CACurrentMediaTime()-d; print(d1)//0.452 sec
- * EXAMPLE:
- * let timer = SimpleTimer(0.5,true,callBack)
- * timer.start()
- * @objc func update() {Swift.print("tick")}//method must be in the public or scope
- * IMPORTANT: because your object has a property to store the timer, and the timer calls a method on the object, you have a strong reference cycle that means neither object can be freed. To fix this, make sure you invalidate the timer when you're done with it, such as when your view is about to disappear: timer.invalidate()
+ * EXAMPLE:SimpleTimer(interval: 3,repeats: true){print("tick")}.start()
+ * IMPORTANT: ⚠️️ @objc func update() {Swift.print("tick")}//method must be in the public or scope
+ * IMPORTANT: ⚠️️ because your object has a property to store the timer, and the timer calls a method on the object, you have a strong reference cycle that means neither object can be freed. To fix this, make sure you invalidate the timer when you're done with it, such as when your view is about to disappear: timer.invalidate()
  */
-class SimpleTimer {/*was named Timer, but since swift 3, NSTimer is now Timer*/
+class SimpleTimer {/*<--was named Timer, but since swift 3, NSTimer is now Timer*/
     typealias Tick = ()->Void
     var timer:Timer?
-    var interval:TimeInterval
+    var interval:TimeInterval /*seconds*/
     var repeats:Bool
     var tick:Tick
     
