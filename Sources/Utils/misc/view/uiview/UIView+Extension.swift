c641e596325cdf41b7da762740279f4b68371496
eonist <30n1st@gmail.com>
2018-05-14 15:27:22 +0200

    Adds rootController support

--- Sources/Utils/misc/view/uiview/UIView+Extension.swift
+++ Sources/Utils/misc/view/uiview/UIView+Extension.swift
@@ -8,10 +8,29 @@ extension UIView{
      */
     public static func vc<T:UIViewController>(vcKind:T.Type? = nil) -> T?{
         guard let appDelegate:AppDelegate = UIApplication.shared.delegate as? AppDelegate else {return nil}
-        if let viewController = appDelegate.window?.rootViewController?.presentedViewController as? T {
-            return viewController
-        }else if let viewControllers = appDelegate.window?.rootViewController?.childViewControllers  {
-            return viewControllers.lazy.flatMap{$0 as? T}.first
+        if let vc = appDelegate.window?.rootViewController as? T {
+            return vc
+        }else if let vc = appDelegate.window?.rootViewController?.presentedViewController as? T {
+            return vc
+        }else if let vc = appDelegate.window?.rootViewController?.childViewControllers  {
+            return vc.lazy.flatMap{$0 as? T}.first
+        }
+        return nil
+    }
+    public static func topMostController() -> UIViewController? {
+        var topController: UIViewController? = UIApplication.sharedApplication().keyWindow?.rootViewController?
+        while topController.presentedViewController != nil {
+            topController = topController.presentedViewController!
+        }
+        return topController
+    }
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
         }
         return nil
     }
