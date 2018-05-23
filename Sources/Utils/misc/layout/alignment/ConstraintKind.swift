71cf76e8ce89a130032742f019963218f80af1a2
eonist <30n1st@gmail.com>
2018-05-23 15:41:03 +0200

    Enhances ConstraintKind

--- Sources/Utils/misc/layout/alignment/ConstraintKind.swift
+++ Sources/Utils/misc/layout/alignment/ConstraintKind.swift
@@ -5,29 +5,43 @@ protocol ConstraintKind:class{
     var anchor:(x:NSLayoutConstraint,y:NSLayoutConstraint)? {get set}
     var size:(w:NSLayoutConstraint,h:NSLayoutConstraint)? {get set}
 }
-
+extension ConstraintKind{
+    /**
+     * Convenient
+     */
+    func setConstraint(anchor:(x:NSLayoutConstraint,y:NSLayoutConstraint),size:(w:NSLayoutConstraint,h:NSLayoutConstraint)) {
+        self.anchor = anchor
+        self.size = size
+    }
+}
 /**
  * Animation
  */
 extension ConstraintKind where Self:UIView{
-    typealias AnimComplete = ()-> Void
-    static func defaultOnComplete() {Swift.print("default anim completed closure")}
     /**
      * Animates a UIView that adhers to ConstraintKind (hor)
      */
-    func animate(to:CGFloat, align:HorizontalAlign = .left, alignTo:HorizontalAlign = .left, onComplete:@escaping AnimComplete = Self.defaultOnComplete){
-        let anim = UIViewPropertyAnimator(duration: 0.3, curve: .easeOut, animations: {
-            self.update(offset: to, align: align, alignTo: alignTo)// Set the new constraints
-        })
-        anim.addCompletion{_ in onComplete() }
-        anim.startAnimation()
+    func animate(to:CGFloat, align:HorizontalAlign, alignTo:HorizontalAlign, onComplete:@escaping AnimComplete = Self.defaultOnComplete){
+        UIView.animate({self.update(offset: to, align: align, alignTo: alignTo)},onComplete:onComplete)
     }
     /**
      * Anim for ver
      */
-    func animate(to:CGFloat, align:VerticalAlign, alignTo:VerticalAlign, onComplete:@escaping AnimComplete = Self.defaultOnComplete){
+    func animate(to:CGFloat, align:VerticalAlign = .top, alignTo:VerticalAlign = .top, onComplete:@escaping AnimComplete = Self.defaultOnComplete){
+        UIView.animate({self.update(offset: to, align: align, alignTo: alignTo)},onComplete:onComplete)
+    }
+    
+}
+extension UIView{
+    typealias AnimComplete = ()-> Void
+    typealias AnimUpdate = ()-> Void
+    static func defaultOnComplete() {Swift.print("default anim completed closure")}
+    /**
+     * Animate
+     */
+    static func animate(_ onUpdate:@escaping AnimUpdate,onComplete:@escaping AnimComplete = UIView.defaultOnComplete){
         let anim = UIViewPropertyAnimator(duration: 0.3, curve: .easeOut, animations: {
-            self.update(offset: to, align: align, alignTo: alignTo)// Set the new constraints
+            onUpdate()
         })
         anim.addCompletion{_ in onComplete() }
         anim.startAnimation()
