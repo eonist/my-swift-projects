f28335097307c0c0b7723cf068858e53bfefe051
eonist <30n1st@gmail.com>
2018-06-07 00:53:03 +0200

    Minor fix

--- Sources/Utils/array/iterator/DemoIterator.swift
+++ Sources/Utils/array/iterator/DemoIterator.swift
@@ -1,17 +1,18 @@
+import Foundation
 /**
  * Demonstrates async iterator
  */
 class DemoIterator:ArrayIterator<DemoItem> {
     var complete:()->Void
-    init(array: Array<T>,onComplete:@escaping Completed) {
+    init(array: Array<T>,onComplete:@escaping ()->Void) {
         self.complete = onComplete
         super.init(array: array)
     }
-    func iterate(callBack:(item:DemoItem, success:Bool)->Void){
+    func iterate(callBack:@escaping (_ item:DemoItem, _ success:Bool)->Void){
         if hasNext() {
             let item:DemoItem = next()
             DispatchQueue.global(qos: .background).async {
-               sleep(2.6)/*Simulates some remote service taking 2.6sec*/
+               sleep(UInt32(Int(2.6)))/*Simulates some remote service taking 2.6sec*/
                let eitherOr:Bool = arc4random_uniform(2) == 0/*heads or tails*/
                callBack(item,eitherOr)
             }
@@ -24,14 +25,17 @@ struct DemoItem{}
 /**
  * Example use:
  */
-var arrIterator = ArrayIterator(array:[DemoItem(),DemoItem(),DemoItem()]){
-   print("All done: \(validItems.count)")
+var validItems:[DemoItem] = []
+var arrIterator:DemoIterator = DemoIterator(array:[DemoItem(),DemoItem(),DemoItem()]){
+   print("All done: \(validItems.count)")//between 0 and 3
 }
-var validItems:[]
-arrIterator.iterate{ item,success in
-   if success {
-      validItems.append(item)
-   } else {
-      iterate()
-   }
+func iterate(){
+    arrIterator.iterate{ item,success in
+        if success {
+            validItems.append(item)
+        } else {
+            iterate()
+        }
+    }
 }
+//iterate()
