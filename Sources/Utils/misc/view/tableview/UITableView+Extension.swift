0f805666972141d708e9d224cc924ab393129f98
Eonist <30n1st@gmail.com>
2018-12-04 14:56:18 +0100

    Update UITableView+Extension.swift

--- Sources/Utils/misc/view/tableview/UITableView+Extension.swift
+++ Sources/Utils/misc/view/tableview/UITableView+Extension.swift
@@ -1,31 +1,6 @@
 #if os(iOS)
 import UIKit
-extension UITableView:UITableViewDataSource {
-    /**
-     * EXAMPLE: tableView.cells()//list of cells in a tableview
-     */
-//    static var cells:[UITableViewCell]{
-//        var cells:[UITableViewCell] = []
-//        (0..<tableView.numberOfSections).indices.forEach { sectionIndex in
-//            (0..<tableView.numberOfRows(inSection: sectionIndex)).indices.forEach { rowIndex in
-//                if let cell:UITableViewCell = tableView.cellForRow(at: IndexPath(row: rowIndex, section: sectionIndex)) {
-//                    cells.append(cell)
-//                }
-//            }
-//        }
-//    }
-    /**
-     * Returns totalRowHeight (height of all rows)
-     * TODO: ⚠️️ Upgrade this with NumberOfSections instead
-     */
-//    var totalRowHeight:CGFloat {
-//        return sections.enumerated().map { arg -> [(section:Int,row:Int)] in
-//            return arg.element.data.indices.map{ i in return (section:arg.offset,row:i)}
-//            }.flatMap{$0}.reduce(0){
-//                return $0 + self(self, heightForRowAt: .init(row: $1.row, section: $1.section))
-//        }
-//    }
-}
+extension UITableView:UITableViewDataSource {}
 extension UITableViewDataSource where Self:UITableView {
     /**
      * EXAMPLE: tableView.cells//list of cells in a tableview
@@ -38,5 +13,32 @@ extension UITableViewDataSource where Self:UITableView {
         }.flatMap{$0}
     }
 }
-
 #endif
+
+/**
+ * Returns totalRowHeight (height of all rows)
+ * TODO: ⚠️️ Upgrade this with NumberOfSections instead
+ */
+//    var totalRowHeight:CGFloat {
+//        return sections.enumerated().map { arg -> [(section:Int,row:Int)] in
+//            return arg.element.data.indices.map{ i in return (section:arg.offset,row:i)}
+//            }.flatMap{$0}.reduce(0){
+//                return $0 + self(self, heightForRowAt: .init(row: $1.row, section: $1.section))
+//        }
+//    }
+
+
+
+/**
+ * EXAMPLE: tableView.cells()//list of cells in a tableview
+ */
+//    static var cells:[UITableViewCell]{
+//        var cells:[UITableViewCell] = []
+//        (0..<tableView.numberOfSections).indices.forEach { sectionIndex in
+//            (0..<tableView.numberOfRows(inSection: sectionIndex)).indices.forEach { rowIndex in
+//                if let cell:UITableViewCell = tableView.cellForRow(at: IndexPath(row: rowIndex, section: sectionIndex)) {
+//                    cells.append(cell)
+//                }
+//            }
+//        }
+//    }
