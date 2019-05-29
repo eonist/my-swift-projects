545baa4443437e16613ccb69ea5dc06157814fdd
andre.jorgensen <andre.jorgensen@tidal.com>
2019-05-29 10:45:25 +0200

    Update UIView+Extension.swift

--- Sources/Utils/misc/view/uiview/UIView+Extension.swift
+++ Sources/Utils/misc/view/uiview/UIView+Extension.swift
@@ -6,6 +6,22 @@ import UIKit.UIView
  * Controller related
  */
 extension UIView{
+  /**
+     * Traverses the entire UIView hirearchy and collects views that are of speccific PARAM: type
+     * TODO: ⚠️️ this can be written more elegantly with flatmap
+     */
+    func descendants<T>(type: T.Type? = nil) -> [T] {
+        var subViewsOfType: [T] = []
+        self.subviews.forEach {
+            if let subView: T = ($0 as? T) {
+                subViewsOfType.append(subView)
+            }
+            if !$0.subviews.isEmpty {
+                subViewsOfType += $0.descendants(type: type)
+            }
+        }
+        return subViewsOfType
+    }
    /**
     * Returns a ViewController of a class Kind
     * EXAMPLE: UIView.vc(vcKind: CustomViewController.self)//ref to an instance of CustomViewController
