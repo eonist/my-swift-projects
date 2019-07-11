4077609c5e4ab0eb9f45d30b026318e62738c2ca
Eonist <30n1st@gmail.com>
2019-07-11 14:22:01 +0200

    Remove git classes
    move to own repo

--- Sources/Utils/misc/shell/git/log/utils/CommitData.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct CommitData {
-    /**
-     * PARAM: subject is the same as title, in git terminology
-     */
-    let hash:String,author:String,date:String,subject:String,body:String
-}
--- Sources/Utils/misc/shell/git/utils/types/GitKey.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-struct GitKey{
-    var user:String,  pass:String
-    init(_ user:String,_ pass:String){self.user = user;self.pass = pass;}
-}
